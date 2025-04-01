613a93214f435494c36512fc714c552e41c167f8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-02 01:59:43 +0530

    Create file

---
+++ 3453-SeparateSquaresI/version/python3/3453-SeparateSquaresI_20250402_015937.py
@@ -0,0 +1,36 @@
+# Last updated: 2/4/2025, 1:59:37 am
+# This solution uses a float based binary search
+class Solution:
+    def separateSquares(self, squares: List[List[int]]) -> float:
+        n = len(squares)
+        left = 0
+        right = 0
+
+        for x, y, l in squares:
+            left = min(y, left)
+            right = max(y+l, right)
+        
+        precision = 1 / (10**5)
+        while right - left > precision:
+            mid = (left+right) / 2
+
+            top = 0
+            bottom = 0
+
+            for x, y, l in squares:
+                if y < mid and y+l > mid:
+                    b = mid-y
+                    t = l - b
+                    top += t * l
+                    bottom += b * l
+                elif y < mid:
+                    bottom += l*l
+                else:
+                    top += l*l
+            
+            if top > bottom:
+                left = mid
+            else:
+                right = mid
+
+        return left
