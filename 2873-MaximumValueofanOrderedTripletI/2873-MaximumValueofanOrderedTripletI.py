6f3b9dc4e928828e55398e2495b5411375e07aa5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-02 11:27:44 +0530

    File updated at 2/4/2025, 11:27:38 am

--- 2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.py
+++ 2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.py
@@ -1,15 +1,12 @@
-# Last updated: 2/4/2025, 11:26:48 am
+# Last updated: 2/4/2025, 11:27:38 am
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         ret = 0
         n = len(nums)
-        arr = [-float('inf')] * n
-        for i in range(n):
-            for j in range(i+1, n):
-                arr[j] = max(nums[i] - nums[j], arr[j])
 
         for i in range(n):
             for j in range(i+1, n):
-                ret = max(arr[i] * nums[j], ret)
+                for k in range(j+1, n):
+                    ret = max((nums[i] - nums[j]) * nums[k], ret)
         
         return ret if ret > 0 else 0
