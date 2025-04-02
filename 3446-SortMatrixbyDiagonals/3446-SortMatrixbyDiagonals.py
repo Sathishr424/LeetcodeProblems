a40e8de4d14ed06c081ac0027f3031827f3e7c4f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-03 01:13:00 +0530

    Create file

---
+++ 3446-SortMatrixbyDiagonals/3446-SortMatrixbyDiagonals.py
@@ -0,0 +1,31 @@
+# Last updated: 3/4/2025, 1:12:59 am
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+
+        for i in range(n):
+            j = 0
+            arr = []
+            while i+j < n:
+                arr.append(grid[i+j][j])
+                j += 1
+            arr.sort(reverse=True)
+            j -= 1
+            while j >= 0:
+                grid[i+j][j] = arr.pop()
+                j -= 1
+        
+        for i in range(1, n):
+            j = 0
+            arr = []
+            while i+j < n:
+                arr.append(grid[j][i+j])
+                j += 1
+            arr.sort()
+            j -= 1
+            while j >= 0:
+                grid[j][i+j] = arr.pop()
+                j -= 1
+        
+        return grid
+                
