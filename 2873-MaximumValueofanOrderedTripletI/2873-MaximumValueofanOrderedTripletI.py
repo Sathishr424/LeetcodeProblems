bffb1ac5635ef59b2cb7b39cc25e652f36299c0c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-02 11:40:13 +0530

    File updated at 2/4/2025, 11:40:08 am

--- 2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.py
+++ 2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.py
@@ -1,20 +1,18 @@
-# Last updated: 2/4/2025, 11:36:55 am
+# Last updated: 2/4/2025, 11:40:08 am
+inf = float('inf')
+
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         ret = 0
         
-        stack = []
-        maxi = -float('inf')
+        first = -float('inf')
+        second = -float('inf')
 
         for i in range(len(nums)):
-            while stack and stack[-1] < nums[i]:
-                stack.pop()
-            
-            ret = max(ret, maxi * nums[i])
+            ret = max(second * nums[i], ret)
 
-            if stack:
-                maxi = max(stack[0] - nums[i], maxi)
-            
-            stack.append(nums[i])
+            second = max(first - nums[i], second)
+            first = max(first, nums[i])
+
         
         return ret if ret > 0 else 0
