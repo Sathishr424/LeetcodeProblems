edbab1ebb5419adc6e3e127e9ba994f492a43a32
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-03 12:01:56 +0530

    File updated at 3/4/2025, 12:01:55 pm

--- 2874-MaximumValueofanOrderedTripletII/2874-MaximumValueofanOrderedTripletII.py
+++ 2874-MaximumValueofanOrderedTripletII/2874-MaximumValueofanOrderedTripletII.py
@@ -1,14 +1,15 @@
-# Last updated: 2/4/2025, 4:56:42 pm
+# Last updated: 3/4/2025, 12:01:55 pm
+_max = lambda x, y: x if x > y else y
+
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        highestSeen = 0
-        highestDiff = 0
-        ans = 0
-        for num in nums:
-            if highestDiff*num > ans:
-                ans = highestDiff*num
-            if highestSeen-num > highestDiff:
-                highestDiff = highestSeen-num
-            if num > highestSeen:
-                highestSeen = num
-        return ans
+        ret = 0
+        second = -float('inf')
+        first = -float('inf')
+
+        for i in range(len(nums)):
+            ret = _max(ret, second * nums[i])
+            second = _max(second, first - nums[i])
+            first = _max(first, nums[i])
+        
+        return ret
