e2acba6b32a9897ae1d3a8cb3fd19b079d8f09e7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-04 14:05:58 +0530

    File updated at 4/4/2025, 2:05:57 pm

--- 1123-LowestCommonAncestorofDeepestLeaves/1123-LowestCommonAncestorofDeepestLeaves.py
+++ 1123-LowestCommonAncestorofDeepestLeaves/1123-LowestCommonAncestorofDeepestLeaves.py
@@ -1,4 +1,4 @@
-# Last updated: 4/4/2025, 2:02:54 pm
+# Last updated: 4/4/2025, 2:05:57 pm
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,18 +7,12 @@
 #         self.right = right
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        max_level = 0
         def rec(node, level):
-            nonlocal max_level
-            if node == None: return
-
-            max_level = max(max_level, level)
-            
-            left = rec(node.left, level+1)
-            right = rec(node.right, level+1)
+            if node == None: return level-1
+            return max(rec(node.left, level+1), rec(node.right, level+1))
         
-        rec(root, 0)
-
+        max_level = rec(root, 0)
+        
         def rec2(node, level):
             if node == None: return None
 
