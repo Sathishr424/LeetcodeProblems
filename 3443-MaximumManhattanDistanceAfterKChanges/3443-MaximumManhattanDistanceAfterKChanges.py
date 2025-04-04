1103af50165e683b8e2b981997d5882732499aed
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-04 14:31:31 +0530

    File updated at 4/4/2025, 2:31:30 pm

--- 3443-MaximumManhattanDistanceAfterKChanges/3443-MaximumManhattanDistanceAfterKChanges.py
+++ 3443-MaximumManhattanDistanceAfterKChanges/3443-MaximumManhattanDistanceAfterKChanges.py
@@ -1,37 +1,35 @@
-# Last updated: 4/4/2025, 2:29:01 pm
+# Last updated: 4/4/2025, 2:31:30 pm
 cmax = lambda x, y: x if x > y else y
+
+def getDirection(d, y, x):
+    if d == 'N': return y-1, x
+    if d == 'S': return y+1, x
+    if d == 'W': return y, x-1
+    if d == 'E': return y, x+1
+
+def process(s, t, s2, t2, rem, st):
+    res = 0
+    x = 0
+    y = 0
+    for d in st:
+        if rem > 0:
+            if d == s:
+                rem -= 1
+                d = t
+            elif d == s2:
+                rem -= 1
+                d = t2
+        
+        x, y = getDirection(d, x, y)
+        res = cmax(res, abs(x) + abs(y))
+    return res
+
 class Solution:
     def maxDistance(self, st: str, k: int) -> int:
-        def getDirection(d, y, x):
-            if d == 'N': return y-1, x
-            if d == 'S': return y+1, x
-            if d == 'W': return y, x-1
-            if d == 'E': return y, x+1
-
-        res = 0
-
-        def process(s, t, s2, t2, rem):
-            nonlocal res
-            x = 0
-            y = 0
-            for d in st:
-                if rem > 0:
-                    if d == s:
-                        rem -= 1
-                        d = t
-                    elif d == s2:
-                        rem -= 1
-                        d = t2
-                
-                x, y = getDirection(d, x, y)
-                res = cmax(res, abs(x) + abs(y))
-        
-        process('S', 'N', 'W', 'E', k)
-        process('S', 'N', 'E', 'W', k)
-        process('N', 'S', 'E', 'W', k)
-        process('N', 'S', 'W', 'E', k)
-
-        return res        
+        return max(process('S', 'N', 'W', 'E', k, st),
+        process('S', 'N', 'E', 'W', k, st),
+        process('N', 'S', 'E', 'W', k, st),
+        process('N', 'S', 'W', 'E', k, st))
         
 
         
