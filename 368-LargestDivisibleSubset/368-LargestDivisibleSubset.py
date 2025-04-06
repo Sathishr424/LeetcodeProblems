d012ef67c07a53da73ca7e2be3c6e54e5bc59eaa
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-06 21:57:07 +0530

    File updated at 6/4/2025, 9:57:03 pm

--- 368-LargestDivisibleSubset/368-LargestDivisibleSubset.py
+++ 368-LargestDivisibleSubset/368-LargestDivisibleSubset.py
@@ -1,4 +1,4 @@
-# Last updated: 6/4/2025, 9:55:54 pm
+# Last updated: 6/4/2025, 9:57:03 pm
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         n = len(nums)
@@ -9,11 +9,13 @@ class Solution:
 
         for i in range(n-1, -1, -1):
             num = nums[i]
-            dp[num] = [num]
+            dp[num] = []
             for j in range(i+1, n):
-                if nums[j] % num  == 0 and len(dp[nums[j]]) >= len(dp[num]):
-                    dp[num] = [num] + dp[nums[j]]
+                if nums[j] % num  == 0 and len(dp[nums[j]]) > len(dp[num]):
+                    dp[num] = dp[nums[j]]
             
+            dp[num] = [num] + dp[num]
+
             if len(dp[num]) > len(ret):
                 ret = dp[num]
 
