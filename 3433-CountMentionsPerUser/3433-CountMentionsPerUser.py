4c6046a9c1f29334d54e7e39d4f773ea09e6eb33
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-06 05:38:42 +0530

    File updated at 6/4/2025, 5:38:39 am

--- 3433-CountMentionsPerUser/3433-CountMentionsPerUser.py
+++ 3433-CountMentionsPerUser/3433-CountMentionsPerUser.py
@@ -1,4 +1,4 @@
-# Last updated: 6/4/2025, 5:36:37 am
+# Last updated: 6/4/2025, 5:38:39 am
 class Solution:
     def countMentions(self, users: int, events: List[List[str]]) -> List[int]:
         n = users
@@ -12,24 +12,13 @@ class Solution:
                 new_events.append([0, int(event[1]), event[2]])
 
         new_events.sort(key=lambda x: (x[1], x[0]))
-
-        online_users = [True] * n
-        offline = []
         offline_hash = defaultdict(int)
 
         for event, timestamp, id in new_events:
             if event == 1:
                 if id == 'HERE':
-                    # print(offline, timestamp, id)
-                    while offline and offline[0][0] <= timestamp:
-                        t, id = heapq.heappop(offline)
-
-                        if t >= offline_hash[id]:
-                            online_users[id] = True
-                            offline_hash[id] = 0
-                        
                     for i in range(n):
-                        res[i] += online_users[i]
+                        res[i] += offline_hash[i] <= timestamp
                 elif id == 'ALL':
                     for i in range(n):
                         res[i] += 1
@@ -38,8 +27,6 @@ class Solution:
                         res[int(u[2:])] += 1
             else:
                 id = int(id)
-                heapq.heappush(offline, (timestamp+60, id))
-                online_users[id] = False
                 offline_hash[id] = max(offline_hash[id], timestamp+60)
 
         return res
