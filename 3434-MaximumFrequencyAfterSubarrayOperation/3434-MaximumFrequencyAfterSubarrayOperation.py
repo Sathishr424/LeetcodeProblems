2d86c8e2872e8b5dc8266c2a2c6135b74ee6346e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-06 05:35:06 +0530

    File updated at 6/4/2025, 5:35:03 am

--- 3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.py
+++ 3434-MaximumFrequencyAfterSubarrayOperation/3434-MaximumFrequencyAfterSubarrayOperation.py
@@ -1,26 +1,30 @@
-# Last updated: 6/4/2025, 5:32:33 am
+# Last updated: 6/4/2025, 5:35:03 am
+cmin = lambda x, y: x if x < y else y
+cmax = lambda x, y: x if x > y else y
+
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         n = len(nums)
+        m = 51
 
         res = 0
-        freq = [0] * 51
 
-        nums_start = {}
-        nums_end = {}
+        freq = [0] * m
+        nums_start = [n] * m
+        nums_end = [0] * m
         
         for i in range(n):
             num = nums[i]
         
-            if num not in nums_start:
-                nums_start[num] = i
+            nums_start[num] = cmin(i, nums_start[num])
             
             nums_end[num] = i
             
             freq[num] += 1
 
-        for num in nums_start:
+        for num in range(1, m):
             f = freq[num]
+            if f == 0: continue
 
             start = nums_start[num]
             end = nums_end[num]
@@ -30,14 +34,14 @@ class Solution:
 
             for i in range(start, end+1):
                 if nums[i] == k:
-                    ans = max(ans-1, -1)
+                    ans = cmax(ans-1, -1)
                 elif nums[i] == num:
-                    ans = max(ans+1, 1)
+                    ans = cmax(ans+1, 1)
                 else:
-                    ans = max(ans, 0)
+                    ans = cmax(ans, 0)
                 
-                maxi = max(maxi, ans)
+                maxi = cmax(maxi, ans)
         
-            res = max(res, maxi+freq[k])
+            res = cmax(res, maxi+freq[k])
         
         return res
