4b375c2d537b24fe938690606ffbd450ee95dde3
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-06 21:47:07 +0530

    File updated at 6/4/2025, 9:47:02 pm

--- 368-LargestDivisibleSubset/368-LargestDivisibleSubset.py
+++ 368-LargestDivisibleSubset/368-LargestDivisibleSubset.py
@@ -1,36 +1,21 @@
-# Last updated: 6/4/2025, 7:32:53 am
+# Last updated: 6/4/2025, 9:47:02 pm
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         n = len(nums)
 
+        dp = defaultdict(list)
+        ret = []
         nums.sort()
 
-        visited = defaultdict(list)
+        for i in range(n-1, -1, -1):
+            num = nums[i]
+            dp[num] = [num]
+            for j in range(i+1, n):
+                if nums[j] % num  == 0:
+                    if len(dp[nums[j]])+1 > len(dp[num]):
+                        dp[num] = [num] + dp[nums[j]]
+            if len(dp[num]) > len(ret):
+                ret = dp[num]
 
-        ret = [nums[0]]
-
-        def rec(num, index):
-            if index == n: return []
-            if visited[num]: return visited[num]
-
-            arr = []
-            if nums[index] % num == 0:
-                tmp = rec(nums[index], index+1)
-                if len(tmp)+1 > len(arr):
-                    arr = [nums[index]] + tmp
-            
-            tmp = rec(num, index+1)
-            
-            if len(tmp) > len(arr):
-                arr = tmp
-            
-            visited[num] = arr
-            return arr
-
-        for i, num in enumerate(nums):
-            arr = rec(num, i+1)
-            if len(arr)+1 > len(ret):
-                ret = [num] + arr
-        
         return ret
-            
+
