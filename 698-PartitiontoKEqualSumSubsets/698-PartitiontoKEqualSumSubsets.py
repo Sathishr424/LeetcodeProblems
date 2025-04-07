9ef31bc8ae5c231cda353bb32d25c405c388aee5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-07 23:15:15 +0530

    File updated at 7/4/2025, 11:15:10 pm

--- 698-PartitiontoKEqualSumSubsets/698-PartitiontoKEqualSumSubsets.py
+++ 698-PartitiontoKEqualSumSubsets/698-PartitiontoKEqualSumSubsets.py
@@ -1,32 +1,45 @@
-# Last updated: 7/4/2025, 11:14:52 pm
+# Last updated: 7/4/2025, 11:15:10 pm
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        total = sum(nums)
-        target = total // k
+        tot = sum(nums)
+        part = tot // k
 
-        if total / k != target: return False
+        if tot / k != part: return False
 
-        n = len(nums)
+        used = defaultdict(int)
 
-        mask = 1 << n
-        full_mask = (1 << (n+1)) - 1
+        for num in nums:
+            used[num] += 1
 
-        def rec(index, tot, done):
-            nonlocal mask
-            if index == n: return False
+        dict_arr = sorted(used.keys())
+        # print(dict_arr, tot, part)
+        n = len(dict_arr)
 
-            for i in range(index, n):
-                if mask & (1 << i) == 0 and tot+nums[i] <= target:
-                    curr = tot+nums[i]
-                    tmp = mask
-                    mask |= 1 << i
-                    if curr == target:
-                        if done+1 == k: return mask == full_mask
-                        return rec(0, 0, done+1)
-                    elif rec(i+1, tot+nums[i], done): return True
-                    mask = tmp
+        def rec(index, tot, parts, arr):
+            nonlocal used
+            if index == len(arr): return False
 
+            for i in range(index, len(arr)):
+                if arr[i]+tot <= part:
+                    if used[arr[i]] == 0: continue
+                    used[arr[i]] -= 1
+                    if arr[i]+tot == part:
+                        if parts+1 == k: return True
+            
+                        new_arr = []
+                        for num in arr:
+                            if used[num] > 0:
+                                new_arr.append(num)
+                        if rec(0, 0, parts+1, new_arr): return True
+                    else:
+
+                        if rec(i+(used[arr[i]] == 0), tot+arr[i], parts, arr): return True
+                    used[arr[i]] += 1
+                else: return False
+            
             return False
-        
-        return rec(0, 0, 0)
+            
+
+        return rec(0, 0, 0, dict_arr)
+
 
