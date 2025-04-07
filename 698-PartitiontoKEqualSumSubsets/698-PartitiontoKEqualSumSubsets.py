254f99ca92cd8a085914ed63e345f4fe3021438e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-07 21:49:08 +0530

    File updated at 7/4/2025, 9:49:03 pm

--- 698-PartitiontoKEqualSumSubsets/698-PartitiontoKEqualSumSubsets.py
+++ 698-PartitiontoKEqualSumSubsets/698-PartitiontoKEqualSumSubsets.py
@@ -1,4 +1,4 @@
-# Last updated: 7/4/2025, 7:20:39 pm
+# Last updated: 7/4/2025, 9:49:03 pm
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         total = sum(nums)
@@ -8,36 +8,20 @@ class Solution:
         target = total // k
         n = len(nums)
 
-        dp = [[] for _ in range(target+1)]
-        
-        mask = 1 << n
         full_mask = (1 << (n+1)) - 1
 
-        dp[0].append(mask)
+        @cache
+        def rec(tot, mask, done):
+            if tot == target:
+                done += 1
+                if done == k: 
+                    return mask == full_mask 
+                return rec(0, mask, done)
 
-        for i, num in enumerate(nums):
-            for tot in range(target-num, -1, -1):
-                for m in dp[tot]:
-                    dp[tot+num].append(m | (1 << i))
-
-        arr = dp[target]
-
-        dp = {}
-        def rec(j, index, rem, check):
-            if check in dp: return dp[check]
-            if rem == 0:
-                return check == full_mask
-            if index == len(arr): return False
-
-            for i in range(index, len(arr)):
-                if arr[j] & arr[i] == mask and rec(i, i+1, rem-1, check | arr[i]):
-                    dp[check] = True
+            for i in range(n):
+                if mask & (1 << i) == 0 and nums[i]+tot <= target and rec(tot+nums[i], mask | (1 << i), done):
                     return True
             
-            dp[check] = False
             return False
-        
-        for i in range(len(arr)):
-            if rec(i, i+1, k-1, mask | arr[i]): return True
-        
-        return False
+
+        return rec(0, 1 << n, 0)
