8f620cc78c68f3f2da4a6fbf4ee231185ce91c0a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-07 21:59:41 +0530

    File updated at 7/4/2025, 9:59:36 pm

--- 698-PartitiontoKEqualSumSubsets/698-PartitiontoKEqualSumSubsets.py
+++ 698-PartitiontoKEqualSumSubsets/698-PartitiontoKEqualSumSubsets.py
@@ -1,4 +1,4 @@
-# Last updated: 7/4/2025, 9:55:07 pm
+# Last updated: 7/4/2025, 9:59:36 pm
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         total = sum(nums)
@@ -11,21 +11,23 @@ class Solution:
         full_mask = (1 << (n+1)) - 1
 
         dp = {}
+        for i in range(1 << n, full_mask+1):
+            dp[i] = False
 
         def rec(index, tot, mask, done):
-            if mask in dp: return False
             if tot == target:
                 done += 1
                 if done == k: return mask == full_mask 
                 return rec(0, 0, mask, done)
             
+            if dp[mask]: return False
             index = index % n
 
             for i in range(index, n):
                 if mask & (1 << i) == 0 and nums[i]+tot <= target and rec(i+1, tot+nums[i], mask | (1 << i), done):
                     return True
 
-            dp[mask] = False
+            dp[mask] = True
 
             return False
 
