10eb39e539bd90b6dd6cc882633375d612fd7ed4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-08 06:39:18 +0530

    File updated at 8/4/2025, 6:39:12 am

--- 945-MinimumIncrementtoMakeArrayUnique/945-MinimumIncrementtoMakeArrayUnique.py
+++ 945-MinimumIncrementtoMakeArrayUnique/945-MinimumIncrementtoMakeArrayUnique.py
@@ -1,22 +1,20 @@
-# Last updated: 8/4/2025, 6:38:11 am
+# Last updated: 8/4/2025, 6:39:12 am
 class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
         res = 0
-        last = max(nums)
+        last = max(nums) + 1
 
-        arr = [0] * (last+2)
+        arr = [0] * (last+1)
 
         for num in nums:
             arr[num] += 1
         
-        for num in range(last+1):
+        for num in range(last):
             if arr[num] > 1:
                 arr[num+1] += arr[num]-1
                 res += arr[num]-1
-                arr[num] = 1
-        # print(arr, arr[last+1])
-        res += arr[last+1] * (arr[last+1]-1) // 2
-        return res
+
+        return res + arr[last] * (arr[last]-1) // 2
 
         
 
