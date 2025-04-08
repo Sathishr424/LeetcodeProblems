95b779539b22bf2ac861c34abba66046e2490afa
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-09 03:02:47 +0530

    File updated at 9/4/2025, 3:02:39 am

--- 85-MaximalRectangle/85-MaximalRectangle.py
+++ 85-MaximalRectangle/85-MaximalRectangle.py
@@ -1,4 +1,4 @@
-# Last updated: 9/4/2025, 3:01:39 am
+# Last updated: 9/4/2025, 3:02:39 am
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         m = len(matrix)
@@ -6,15 +6,14 @@ class Solution:
 
         vis = [[0] * n for _ in range(m)]
         
-        ret = 0
         for i in range(m):
             for j in range(n-1, -1, -1):
                 if matrix[i][j] == '1':
                     vis[i][j] = vis[i][j+1] + 1 if j+1 < n else 1
         
+        stack = []
+        ret = 0
         for j in range(n):
-            stack = []
-            index = 0
             for i in range(m):
                 index = i
                 while stack and stack[-1][1] > vis[i][j]:
