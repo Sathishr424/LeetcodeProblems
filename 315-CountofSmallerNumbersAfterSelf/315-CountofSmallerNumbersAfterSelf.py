0428a3b5c6cfbedfa1b5f22ce2bc776db36ec25a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-08 07:41:24 +0530

    File updated at 8/4/2025, 7:41:19 am

--- 315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.py
+++ 315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.py
@@ -1,13 +1,13 @@
-# Last updated: 8/4/2025, 7:23:07 am
+# Last updated: 8/4/2025, 7:41:19 am
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         arr = SortedList()
         arr.add(nums[-1])
-        ret = [0]
+        ret = [0] * len(nums)
 
         for i in range(len(nums)-2, -1, -1):
             index = bisect_right(arr, nums[i]-1)
-            ret.append(index)
+            ret[i] = index
             arr.add(nums[i])
         
-        return ret[::-1]
+        return ret
