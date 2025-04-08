ca2633a9e3bc3e54fa329918035c0fb23f50b0f5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-08 05:55:44 +0530

    File updated at 8/4/2025, 5:55:38 am

--- 3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct.py
+++ 3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct.py
@@ -1,13 +1,13 @@
-# Last updated: 8/4/2025, 5:54:44 am
+# Last updated: 8/4/2025, 5:55:38 am
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         n = len(nums)
 
         there = {}
         for i in range(n-1, -1, -1):
-            if nums[i] not in there:
-                there[nums[i]] = 1
-            else:
+            if nums[i] in there:
                 return ceil((i+1) / 3)
+            
+            there[nums[i]] = 1
         
         return 0
