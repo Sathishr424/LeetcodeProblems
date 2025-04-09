d23624762a4b57671b6f9f1643fc406841f0c1d5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-09 12:51:19 +0530

    File updated at 9/4/2025, 12:51:12 pm

--- 3375-MinimumOperationstoMakeArrayValuesEqualtoK/3375-MinimumOperationstoMakeArrayValuesEqualtoK.py
+++ 3375-MinimumOperationstoMakeArrayValuesEqualtoK/3375-MinimumOperationstoMakeArrayValuesEqualtoK.py
@@ -1,21 +1,13 @@
-# Last updated: 9/4/2025, 12:44:08 pm
+# Last updated: 9/4/2025, 12:51:12 pm
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        nums.sort()
-
         counts = defaultdict(int)
+        mini = nums[0]
         for num in nums:
             counts[num] += 1
+            mini = min(num, mini)
         
-        n = len(nums)
-
-        op = 0
-        for i in range(n-2, -1, -1):
-            if counts[nums[i+1]] == n-i-1:
-                op += 1
-                counts[nums[i]] += counts[nums[i+1]]
-
-        if counts[nums[0]] == n and nums[0] >= k:
-            return op + (nums[0] != k)
+        if mini >= k:
+            return len(counts) - (mini == k)
         
         return -1
