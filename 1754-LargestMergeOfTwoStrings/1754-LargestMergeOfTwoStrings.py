5d83c581f0a7eb981075fb1268106cd694f27d37
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-09 22:26:20 +0530

    File updated at 9/4/2025, 10:26:11 pm

--- 1754-LargestMergeOfTwoStrings/1754-LargestMergeOfTwoStrings.py
+++ 1754-LargestMergeOfTwoStrings/1754-LargestMergeOfTwoStrings.py
@@ -1,44 +1,26 @@
-# Last updated: 9/4/2025, 10:23:39 pm
+# Last updated: 9/4/2025, 10:26:11 pm
 class Solution:
     def largestMerge(self, word1: str, word2: str) -> str:
-        ret = []
+        ret = ''
 
         l = 0
         r = 0
 
-        # a
-        # ab
-
-        # aac
-        # aab
-
-        while l < len(word1) and r < len(word2):
-            if word1[l] > word2[r]:
-                ret.append(word1[l])
-                l += 1
-            elif word1[l] < word2[r]:
-                ret.append(word2[r])
-                r += 1
+        while word1 and word2:
+            if word1[0] > word2[0]:
+                ret += word1[0]
+                word1 = word1[1:]
+            elif word1[0] < word2[0]:
+                ret += word2[0]
+                word2 = word2[1:]
             else:
-                if word1[l:] > word2[r:]:
-                    ret.append(word1[l])
-                    l += 1
+                if word1 > word2:
+                    ret += word1[0]
+                    word1 = word1[1:]
                 else:
-                    ret.append(word2[r])
-                    r += 1
-            # print(word1[l:])
-            # print(word2[r:])
-            # print(''.join(ret))
-
-            # print()
-        while l < len(word1):
-            ret.append(word1[l])
-            l += 1
+                    ret += word2[0]
+                    word2 = word2[1:]
         
-        while r < len(word2):
-            ret.append(word2[r])
-            r += 1
-        
-        return ''.join(ret)
+        return ret + word1 + word2
 
 
