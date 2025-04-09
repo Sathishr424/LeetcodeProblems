a86b86ee2f0608685cedb3df3babe1da37ec3db7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-09 21:54:39 +0530

    File updated at 9/4/2025, 9:54:31 pm

--- 1081-SmallestSubsequenceofDistinctCharacters/1081-SmallestSubsequenceofDistinctCharacters.py
+++ 1081-SmallestSubsequenceofDistinctCharacters/1081-SmallestSubsequenceofDistinctCharacters.py
@@ -1,19 +1,17 @@
-# Last updated: 9/4/2025, 9:54:00 pm
-alp = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n'
-: 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
-
+# Last updated: 9/4/2025, 9:54:31 pm
 class Solution:
     def smallestSubsequence(self, s: str) -> str:
         hash = defaultdict(int)
         for a in s: hash[a] += 1
         ret = []
         his = {}
+
         for a in s:
             if a in his:
                 hash[a] -= 1
                 continue
             j = len(ret)-1
-            while j >= 0 and alp[ret[j]] > alp[a] and hash[ret[j]] >= 1:
+            while j >= 0 and ret[j] > a and hash[ret[j]] >= 1:
                 del his[ret.pop()]
                 j -= 1
             ret.append(a)
