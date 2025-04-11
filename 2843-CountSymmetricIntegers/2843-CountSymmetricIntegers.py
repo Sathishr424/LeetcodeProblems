e8e77ff9942d5ced3176ff5da5ef175cfa7b826e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-11 16:09:49 +0530

    File updated at 11/4/2025, 4:09:48 pm

--- 2843-CountSymmetricIntegers/2843-CountSymmetricIntegers.py
+++ 2843-CountSymmetricIntegers/2843-CountSymmetricIntegers.py
@@ -1,11 +1,10 @@
-# Last updated: 11/4/2025, 4:09:08 pm
+# Last updated: 11/4/2025, 4:09:48 pm
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
         ret = 0
         for num in range(low, high+1):
             n = len(str(num))
             if n % 2 != 0: continue
-            half = n // 2
             
             prefix = []
             tot = 0
@@ -17,7 +16,7 @@ class Solution:
                 tot += rem
                 prefix.append(tot)
             
-            half = prefix[half-1]
+            half = prefix[n // 2 - 1]
             if prefix[-1] - half == half: ret += 1
         
         return ret
