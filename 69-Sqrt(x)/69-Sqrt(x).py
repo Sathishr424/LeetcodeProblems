8a6492b64020ec4d10084720f283bbab1fd422a9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-13 21:23:50 +0530

    File updated at 13/4/2025, 9:23:50 pm

--- 69-Sqrt(x)/69-Sqrt(x).py
+++ 69-Sqrt(x)/69-Sqrt(x).py
@@ -1,6 +1,9 @@
-# Last updated: 13/4/2025, 9:20:57 pm
+# Last updated: 13/4/2025, 9:23:50 pm
 class Solution:
     def mySqrt(self, x: int) -> int:
+        if x == 0: return 0
+        if x == 1: return 1
+        
         l = 1
         r = x // 2 + 1
 
@@ -8,9 +11,9 @@ class Solution:
             mid = (l + r) // 2
             val = mid * mid
 
-            if val >= x:
+            if val > x:
                 r = mid
             else:
                 l = mid + 1
         
-        return l if l*l == x else l-1
+        return l-1
