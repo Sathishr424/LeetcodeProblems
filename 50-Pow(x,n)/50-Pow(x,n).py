ad8a6cdbd021791b92ccb095947d07c9e0d963e7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-13 19:10:18 +0530

    File updated at 13/4/2025, 7:10:18 pm

--- 50-Pow(x,n)/50-Pow(x,n).py
+++ 50-Pow(x,n)/50-Pow(x,n).py
@@ -1,17 +1,16 @@
-# Last updated: 13/4/2025, 7:09:52 pm
+# Last updated: 13/4/2025, 7:10:18 pm
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n < 0:
             x = 1/x
             n *= -1
         
-        @cache
         def rec(n):
             if n == 0: return 1
             elif n == 1: return x
 
             if n % 2 == 0:
-                return rec(n//2) * rec(n//2)
+                return rec(n//2) ** 2
             else:
                 return rec(n//2) ** 2 * x
         
