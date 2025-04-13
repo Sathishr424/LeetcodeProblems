a23972845697509e5a94d8df5c835eeb031d7109
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-13 09:27:57 +0530

    File updated at 13/4/2025, 9:27:56 am

--- 1922-CountGoodNumbers/1922-CountGoodNumbers.py
+++ 1922-CountGoodNumbers/1922-CountGoodNumbers.py
@@ -1,18 +1,21 @@
-# Last updated: 13/4/2025, 9:27:12 am
+# Last updated: 13/4/2025, 9:27:56 am
 mod = 10 ** 9 + 7
 
+@cache
+def good(n):
+    if n == 0: return 1
+    elif n == 1: return 5
+    
+    i = 4
+    ret = 20
+    prev = 2
+    while i <= n:
+        ret = ret * ret % mod
+        prev = i
+        i += i
+    
+    return (ret * good(n - prev)) % mod
+
 class Solution:
-    @cache
     def countGoodNumbers(self, n: int) -> int:
-        if n == 0: return 1
-        elif n == 1: return 5
-        
-        i = 4
-        ret = 20
-        prev = 2
-        while i <= n:
-            ret = ret * ret % mod
-            prev = i
-            i += i
-        
-        return (ret * self.countGoodNumbers(n - prev)) % mod
+        return good(n)
