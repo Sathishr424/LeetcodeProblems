024280b47ff9fc3b23a2a961e68af71ca33498ca
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-13 21:00:25 +0530

    File updated at 13/4/2025, 9:00:25 pm

--- 372-SuperPow/372-SuperPow.py
+++ 372-SuperPow/372-SuperPow.py
@@ -1,20 +1,20 @@
-# Last updated: 13/4/2025, 8:59:28 pm
+# Last updated: 13/4/2025, 9:00:25 pm
 mod = 1337
 
-@cache
-def pow(x, n):
-    if n == 0: return 1
+# @cache
+# def pow(x, n):
+#     if n == 0: return 1
 
-    ans = pow(x, n//2)
-    ans = ans * ans % mod
+#     ans = pow(x, n//2)
+#     ans = ans * ans % mod
     
-    return ans * x % mod if n % 2 else ans
+#     return ans * x % mod if n % 2 else ans
 
 class Solution:
     def superPow(self, a: int, b: List[int]) -> int:
         ret = 1
 
         for n in b:
-            ret = pow(ret, 10) * pow(a, n) % mod
+            ret = pow(ret, 10, mod) * pow(a, n, mod) % mod
 
         return ret
