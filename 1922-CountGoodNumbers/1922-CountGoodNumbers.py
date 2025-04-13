4053cce6a083affed498d867440c266737ab3d96
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-13 19:46:36 +0530

    File updated at 13/4/2025, 7:46:36 pm

--- 1922-CountGoodNumbers/1922-CountGoodNumbers.py
+++ 1922-CountGoodNumbers/1922-CountGoodNumbers.py
@@ -1,17 +1,18 @@
-# Last updated: 13/4/2025, 9:28:12 am
+# Last updated: 13/4/2025, 7:46:36 pm
 mod = 10 ** 9 + 7
+
+@cache
+def pow(x, n):
+    if n == 0: return 1
+
+    ans = pow(x, n // 2)
+    ans = ans * ans % mod
+
+    return ans * x % mod if n % 2 else ans
 
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        if n == 0: return 1
-        elif n == 1: return 5
-        
-        i = 4
-        ret = 20
-        prev = 2
-        while i <= n:
-            ret = ret * ret % mod
-            prev = i
-            i += i
-        
-        return (ret * self.countGoodNumbers(n - prev)) % mod
+        evens = (n + 1) // 2
+        odds = n // 2
+
+        return pow(5, evens) * pow(4, odds) % mod
