1b419d75208d8440a6bbec0438cc14852f1d1cf8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-13 18:48:41 +0530

    Create file

---
+++ 50-Pow(x,n)/50-Pow(x,n).py
@@ -0,0 +1,27 @@
+# Last updated: 13/4/2025, 6:48:40 pm
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1/x
+            n *= -1
+        
+        neg = 1
+        if x < 0: 
+            x *= -1
+            if n % 2 == 1: neg = -1
+        
+        def helper(n):
+            if n == 0: return 1
+            elif n == 1: return x
+
+            ret = x*x
+            i = 4
+            prev = 2
+
+            while i <= n:
+                ret *= ret
+                prev = i
+                i += i
+
+            return ret * helper(n - prev)
+        return helper(n) * neg
