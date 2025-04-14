d984b9fdc19f8598b66d02361015cd4a6941fe3d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-14 12:21:50 +0530

    File updated at 14/4/2025, 12:21:49 pm

--- 1534-CountGoodTriplets/1534-CountGoodTriplets.py
+++ 1534-CountGoodTriplets/1534-CountGoodTriplets.py
@@ -1,16 +1,21 @@
-# Last updated: 14/4/2025, 11:51:00 am
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+# Last updated: 14/4/2025, 12:21:49 pm
+__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("10"))
 
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
-        l = len(arr)
         ret = 0
-        for i in range(l-2):
-            for j in range(i+1, l-1):
-                if abs(arr[i] - arr[j]) > a:
-                    continue
-                for k in range(j+1, l):
-                    if abs(arr[j] - arr[k]) <= b and \
-                    abs(arr[i] - arr[k]) <= c:
-                        ret += 1
+        n = len(arr)
+
+        for i in range(n-2):
+            m = SortedList()
+            if abs(arr[i] - arr[i+1]) <= a:
+                m.add(arr[i+1])
+            for j in range(i+2, n):
+                if abs(arr[i] - arr[j]) <= c:
+                    left = bisect_left(m, arr[j] - b)
+                    right = bisect_right(m, b + arr[j])
+                    ret += right-left
+                if abs(arr[i] - arr[j]) <= a:
+                    m.add(arr[j])
+        
         return ret
