7fe4cd61d3f830f7ce6abb1c7306dd679bb958ea
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-14 18:12:30 +0530

    Create file

---
+++ 47-PermutationsII/47-PermutationsII.py
@@ -0,0 +1,25 @@
+# Last updated: 14/4/2025, 6:12:29 pm
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        vis = [1] * n
+
+        ret = []
+        def rec(p):
+            if len(p) == n:
+                ret.append(p + [])
+                return
+            
+            prev = -11
+            for i in range(n):
+                if vis[i] and nums[i] != prev:
+                    prev = nums[i]
+                    vis[i] = 0
+                    p.append(nums[i])
+                    rec(p)
+                    p.pop()
+                    vis[i] = 1
+        
+        rec([])
+        return ret
