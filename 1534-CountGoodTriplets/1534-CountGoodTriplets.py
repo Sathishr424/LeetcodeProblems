a32019ad67df398191f3f6fa86e1efd2b88ba33f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-14 11:45:46 +0530

    Create file

---
+++ 1534-CountGoodTriplets/1534-CountGoodTriplets.py
@@ -0,0 +1,19 @@
+# Last updated: 14/4/2025, 11:45:45 am
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        ret = 0
+        n = len(arr)
+
+        for i in range(n-2):
+            m = SortedList()
+            if abs(arr[i] - arr[i+1]) <= a:
+                m.add(arr[i+1])
+            for j in range(i+2, n):
+                if abs(arr[i] - arr[j]) <= c:
+                    left = bisect_left(m, arr[j] - b)
+                    right = bisect_left(m, b + arr[j] + 1)
+                    ret += right-left
+                if abs(arr[i] - arr[j]) <= a:
+                    m.add(arr[j])
+        
+        return ret
