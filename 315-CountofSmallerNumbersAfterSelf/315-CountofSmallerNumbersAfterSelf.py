f0a52ece3111cf2081d1daaf92166ee2aa446199
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-15 23:19:37 +0530

    File updated at 15/4/2025, 11:19:35 pm

--- 315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.py
+++ 315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.py
@@ -1,4 +1,4 @@
-# Last updated: 15/4/2025, 11:13:03 pm
+# Last updated: 15/4/2025, 11:19:35 pm
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         n = len(nums)
@@ -13,43 +13,16 @@ class Solution:
                 index += 1
         
         n = len(freq)
-
-        tree = [0] * (n*4)
-        cache = [-1] * n
-
-        def buildTree(left, right, index):
-            if left == right:
-                cache[left] = index
-                return
-
-            mid = (left+right) // 2
-            buildTree(left, mid, index*2+1)
-            buildTree(mid+1, right, index*2+2)
-        
-        buildTree(0, n-1, 0)
-
-        def query(r, left, right, index):
-            if left > r: return 0
-
-            if right <= r:
-                return tree[index]
-
-            mid = (left+right) // 2
-
-            return query(r, left, mid, index*2+1) + query(r, mid+1, right, index*2+2)
+        arr = SortedList()
 
         for j in range(len(nums)-1, -1, -1):
             num = nums[j]
             i = freq[num]
 
-            ret[j] = query(i-1, 0, n-1, 0)
-
-            index = cache[i]
-            tree[index] += 1
-
-            while index > 0:
-                index = (index+1) // 2 - 1
-                tree[index] = tree[index*2+1] + tree[index*2+2]
-            
+            index = bisect_left(arr, i)
+            arr.add(i)
+            # print(arr, i, index)
+            ret[j] = index
+        
         return ret
         
