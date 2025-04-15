6e5c0180f546cf7856a7a071985b4be3e08552ea
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-15 21:47:01 +0530

    File updated at 15/4/2025, 9:46:58 pm

--- 315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.py
+++ 315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.py
@@ -1,9 +1,21 @@
-# Last updated: 15/4/2025, 9:41:06 pm
+# Last updated: 15/4/2025, 9:46:58 pm
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         n = len(nums)
         tree = [0] * (n*4)
         ret = [0] * n
+        cache = [-1] * n
+
+        def buildTree(left, right, index):
+            if left == right:
+                cache[left] = index
+                return
+
+            mid = (left+right) // 2
+            buildTree(left, mid, index*2+1)
+            buildTree(mid+1, right, index*2+2)
+        
+        buildTree(0, n-1, 0)
 
         def query(l, left, right, index):
             if right < l: return 0
@@ -23,20 +35,7 @@ class Solution:
         arr.sort()
 
         for num, i in arr:
-            index = 0
-            l = 0
-            r = n-1
-
-            while l < r:
-                mid = (l+r) // 2
-                index *= 2
-                if i > mid:
-                    l = mid+1
-                    index += 2
-                else:
-                    r = mid
-                    index += 1
-            
+            index = cache[i]
             tree[index] = 1
 
             while index > 0:
