6b4577c643f92b9ac4b9c89a45282ff4832c813e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-15 17:27:52 +0530

    File updated at 15/4/2025, 5:27:49 pm

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
@@ -1,13 +1,13 @@
-# Last updated: 15/4/2025, 5:22:02 pm
+# Last updated: 15/4/2025, 5:27:49 pm
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        stack = []
-
-        for num in nums:
-            index = bisect_left(stack, num)
-            if index < len(stack):
-                stack[index] = num
-            else:
-                stack.append(num)
+        n = len(nums)
+        @cache
+        def rec(index):
+            ans = 0
+            for i in range(index+1, n):
+                if nums[i] > nums[index]: ans = max(ans, rec(i) + 1)
+            
+            return ans
         
-        return len(stack)
+        return max([rec(i) + 1 for i in range(n-1, -1, -1)])
