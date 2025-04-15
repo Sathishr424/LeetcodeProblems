67d701578bbc8a6c51d0153e84116730a8bfda74
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-16 02:52:06 +0530

    File updated at 16/4/2025, 2:52:03 am

--- 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
+++ 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
@@ -1,52 +1,26 @@
-# Last updated: 16/4/2025, 2:46:41 am
+# Last updated: 16/4/2025, 2:52:03 am
 class Solution:
     def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
-
-        tree = [0] * (n * 4)
-        cache = [0] * n
-
         relation = [0] * n
 
         for i, num in enumerate(nums2):
             relation[num] = i
         
         arr = []
+        left = SortedList()
         for num in nums1:
             arr.append(relation[num])
+            left.add(relation[num])
 
-        def buildTree(left, right, index):
-            if left == right:
-                cache[left] = index
-                return
-
-            mid = (left+right) // 2
-            buildTree(left, mid, index*2+1)
-            buildTree(mid+1, right, index*2+2)
-        
-        buildTree(0, n-1, 0)
-
-        def query(l, r, left, right, index):
-            if left > r or right < l: return 0
-
-            if left >= l and right <= r: return tree[index]
-            mid = (left+right) // 2
-
-            return query(l, r, left, mid, index*2+1) + query(l, r, mid+1, right, index*2+2)
-        
+        right = SortedList()
         ret = 0
-
         for num in arr[::-1]:
-            right = query(num+1, n-1, 0, n-1, 0)
-            left = query(0, num-1, 0, n-1, 0)
+            left_index = bisect_left(left, num)
+            right_index = bisect_left(right, num)
 
-            ret += right * (num-left)
-
-            index = cache[num]
-            tree[index] = 1
-
-            while index > 0:
-                index = (index+1) // 2 - 1
-                tree[index] = tree[index*2+1] + tree[index*2+2]
-
+            ret += left_index * (len(right) - right_index)
+            right.add(num)
+            left.remove(num)
+        
         return ret
