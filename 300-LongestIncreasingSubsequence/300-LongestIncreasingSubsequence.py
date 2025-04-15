3cafdd5857ae2b2dbbd68232569b1215e10b0055
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-15 17:22:05 +0530

    Create file

---
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
@@ -0,0 +1,13 @@
+# Last updated: 15/4/2025, 5:22:02 pm
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        stack = []
+
+        for num in nums:
+            index = bisect_left(stack, num)
+            if index < len(stack):
+                stack[index] = num
+            else:
+                stack.append(num)
+        
+        return len(stack)
