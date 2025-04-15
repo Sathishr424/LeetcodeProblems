fc88b7df345d889708a0bbfa79afc6e397955712
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-16 02:54:54 +0530

    File updated at 16/4/2025, 2:54:50 am

--- 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
+++ 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
@@ -1,26 +1,52 @@
-# Last updated: 16/4/2025, 2:52:03 am
+# Last updated: 16/4/2025, 2:54:50 am
 class Solution:
     def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
+
+        tree = [0] * (n * 4)
+        cache = [0] * n
+
         relation = [0] * n
 
         for i, num in enumerate(nums2):
             relation[num] = i
         
         arr = []
-        left = SortedList()
         for num in nums1:
             arr.append(relation[num])
-            left.add(relation[num])
 
-        right = SortedList()
-        ret = 0
-        for num in arr[::-1]:
-            left_index = bisect_left(left, num)
-            right_index = bisect_left(right, num)
+        def buildTree(left, right, index):
+            if left == right:
+                cache[left] = index
+                return
 
-            ret += left_index * (len(right) - right_index)
-            right.add(num)
-            left.remove(num)
+            mid = (left+right) // 2
+            buildTree(left, mid, index*2+1)
+            buildTree(mid+1, right, index*2+2)
         
+        buildTree(0, n-1, 0)
+
+        def query(l, r, left, right, index):
+            if left > r or right < l: return 0
+
+            if left >= l and right <= r: return tree[index]
+            mid = (left+right) // 2
+
+            return query(l, r, left, mid, index*2+1) + query(l, r, mid+1, right, index*2+2)
+        
+        ret = 0
+
+        for num in arr:
+            right = query(num+1, n-1, 0, n-1, 0)
+            left = query(0, num-1, 0, n-1, 0)
+
+            ret += left * ((n-num-1)-right)
+
+            index = cache[num]
+            tree[index] = 1
+
+            while index > 0:
+                index = (index+1) // 2 - 1
+                tree[index] = tree[index*2+1] + tree[index*2+2]
+
         return ret
