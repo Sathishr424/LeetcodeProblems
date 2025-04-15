74d403b4f42287c807fd6f48c35f91fa8df8c29e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-15 23:13:05 +0530

    File updated at 15/4/2025, 11:13:03 pm

--- 315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.py
+++ 315-CountofSmallerNumbersAfterSelf/315-CountofSmallerNumbersAfterSelf.py
@@ -1,9 +1,20 @@
-# Last updated: 15/4/2025, 10:13:10 pm
+# Last updated: 15/4/2025, 11:13:03 pm
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        tree = [0] * (n*4)
         ret = [0] * n
+
+        freq = {}
+        index = 0
+
+        for num in sorted(nums):
+            if num not in freq:
+                freq[num] = index
+                index += 1
+        
+        n = len(freq)
+
+        tree = [0] * (n*4)
         cache = [-1] * n
 
         def buildTree(left, right, index):
@@ -17,32 +28,28 @@ class Solution:
         
         buildTree(0, n-1, 0)
 
-        def query(l, left, right, index):
-            if right < l: return 0
+        def query(r, left, right, index):
+            if left > r: return 0
 
-            if left >= l and right < n:
+            if right <= r:
                 return tree[index]
 
             mid = (left+right) // 2
 
-            return query(l, left, mid, index*2+1) + query(l, mid+1, right, index*2+2)
+            return query(r, left, mid, index*2+1) + query(r, mid+1, right, index*2+2)
 
-        arr = []
+        for j in range(len(nums)-1, -1, -1):
+            num = nums[j]
+            i = freq[num]
 
-        for i, num in enumerate(nums):
-            arr.append((num, i))
-        
-        arr.sort()
-
-        for _, i in arr:
-            ret[i] = query(i+1, 0, n-1, 0)
+            ret[j] = query(i-1, 0, n-1, 0)
 
             index = cache[i]
-            tree[index] = 1
+            tree[index] += 1
 
             while index > 0:
                 index = (index+1) // 2 - 1
                 tree[index] = tree[index*2+1] + tree[index*2+2]
-        
+            
         return ret
         
