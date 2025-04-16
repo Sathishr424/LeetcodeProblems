5c10c86570f0da5f4a62aac537a216c3e5d8e61b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-17 00:29:59 +0530

    File updated at 17/4/2025, 12:29:54 am

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,35 +1,32 @@
-# Last updated: 16/4/2025, 11:36:17 pm
+# Last updated: 17/4/2025, 12:29:54 am
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
 
-        p_index = bisect_left(nums, 1)
-        if p_index == 0: return []
-
+        n = len(nums)
         ret = []
-        if bisect_right(nums, 0) - bisect_left(nums, 0) >= 3: ret.append([0, 0, 0])
 
-        pos = {}
-        neg = {}
-        for i in range(p_index, n):
-            pos[nums[i]] = 1
-        
-        for i in range(0, p_index):
-            if i > 0 and nums[i] == nums[i-1]: continue
-            neg[nums[i]] = 1
-            for j in range(i+1, p_index):
-                if j > i+1 and nums[j] == nums[j-1]: continue
-                s = -(nums[i]+nums[j])
-                if s in pos:
-                    ret.append([nums[i], nums[j], s])
-        
-        for i in range(p_index, n):
-            if i > 0 and nums[i] == nums[i-1]: continue
+        compression = {}
+        index = 0
+        for num in nums:
+            if num not in compression:
+                compression[num] = index
+                index += 1
+
+        added = {}
+        for i in range(n):
             for j in range(i+1, n):
-                if j > i+1 and nums[j] == nums[j-1]: continue
-                s = -(nums[i]+nums[j])
-                if s in neg:
-                    ret.append([nums[i], nums[j], s])
-
+                s = target - (nums[i]+nums[j])
+                sums = {}
+                for k in range(j+1, n):
+                    diff = s-nums[k]
+                    if diff in sums:
+                        mask = hash((compression[nums[i]], compression[nums[j]], compression[diff], compression[nums[k]]))
+                        if mask not in added:
+                            ret.append([nums[i], nums[j], diff, nums[k]])
+                            added[mask] = 1
+                    sums[nums[k]] = 1
+        
         return ret
+
+
