929ddf441dd8885e3e0e7b8675401f3c5a1aeb24
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-16 15:09:17 +0530

    Create file

---
+++ 2537-CounttheNumberofGoodSubarrays/2537-CounttheNumberofGoodSubarrays.py
@@ -0,0 +1,31 @@
+# Last updated: 16/4/2025, 3:09:13 pm
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        pairs = defaultdict(int)
+        total = 0
+
+        stack = deque([])
+        ret = 0
+        good = 0
+
+        for i in range(n):
+            num = nums[i]
+
+            total -= pairs[num] * (pairs[num]-1) // 2
+            pairs[num] += 1
+            total += pairs[num] * (pairs[num]-1) // 2
+            stack.append(i)
+
+            while stack and total >= k:
+                left = stack[0]
+                right = n-i-1
+                ret += left+right+1-good
+                good += 1
+                num = nums[stack.popleft()]
+
+                total -= pairs[num] * (pairs[num]-1) // 2
+                pairs[num] -= 1
+                total += pairs[num] * (pairs[num]-1) // 2
+            
+        return ret
