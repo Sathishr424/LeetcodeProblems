ef44533a8cca71654032e9fee426b4f38f60ef4f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-17 00:51:18 +0530

    File updated at 17/4/2025, 12:51:13 am

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,10 +1,17 @@
-# Last updated: 17/4/2025, 12:48:15 am
+# Last updated: 17/4/2025, 12:51:13 am
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
 
         n = len(nums)
         ret = []
+
+        compression = {}
+        index = 0
+        for num in nums:
+            if num not in compression:
+                compression[num] = index
+                index += 1
 
         for i in range(n):
             if i > 0 and nums[i] == nums[i-1]: continue
@@ -14,11 +21,12 @@ class Solution:
                 sums = {}
                 masks = {}
                 for k in range(j+1, n):
-                    if nums[k] in masks: continue
                     diff = s-nums[k]
                     if diff in sums:
-                        ret.append([nums[i], nums[j], diff, nums[k]])
-                        masks[nums[k]] = 1
+                        mask = hash((diff, nums[k]))
+                        if mask not in masks:
+                            ret.append([nums[i], nums[j], diff, nums[k]])
+                            masks[mask] = 1
                     
                     sums[nums[k]] = 1
         
