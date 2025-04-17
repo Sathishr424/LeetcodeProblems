22cd2005c3f72a6a60c1c1f81541f826426d8b45
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-17 17:38:11 +0530

    File updated at 17/4/2025, 5:38:06 pm

--- 1649-CreateSortedArraythroughInstructions/1649-CreateSortedArraythroughInstructions.py
+++ 1649-CreateSortedArraythroughInstructions/1649-CreateSortedArraythroughInstructions.py
@@ -1,61 +1,39 @@
-# Last updated: 17/4/2025, 11:37:23 am
-mod = 10 ** 9 + 7
+# Last updated: 17/4/2025, 5:38:06 pm
+mod = 10**9 + 7
 class Solution:
-    def createSortedArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        smaller = [0] * n
-        larger = [0] * n
-        cost = 0
+    def createSortedArray(self, ins: List[int]) -> int:
+        n = len(ins)
+        m = n+1
 
-        def mergeSort(arr):
-            if len(arr) == 1: return
-            mid = len(arr) // 2
-            
-            left = arr[:mid]
-            right = arr[mid:]
-            
-            mergeSort(left)
-            mergeSort(right)
+        compression = {}
+        index = 1
+        for num in sorted(ins):
+            if num not in compression:
+                compression[num] = index
+                index += 1
 
-            i = 0
-            j = 0
-            k = 0
-            cnt = 0
-            
-            while i < len(left) and j < len(right):
-                if left[i][0] <= right[j][0]:
-                    arr[k] = left[i]
-                    if i > 0 and left[i][0] == left[i-1][0]: cnt += 1
-                    else: cnt = 1
-                    i += 1
-                else:
-                    arr[k] = right[j]
-                    if i-1 >= 0 and left[i-1][0] == right[j][0]:
-                        smaller[right[j][1]] += i-cnt
-                    else:
-                        smaller[right[j][1]] += i
-                    larger[right[j][1]] += len(left) - i
-                    j += 1
-                
-                k += 1
-            
-            while i < len(left):
-                arr[k] = left[i]
-                i += 1
-                k += 1
-                
-            while j < len(right):
-                arr[k] = right[j]
-                if i-1 >= 0 and left[i-1][0] == right[j][0]:
-                    smaller[right[j][1]] += i-cnt
-                else:
-                    smaller[right[j][1]] += i
-                j += 1
-                k += 1
-        mergeSort([(num, i) for i, num in enumerate(nums)])
+        def update(index):
+            while index < m:
+                tree[index] += 1
+                index += index & (-index)
         
-        for i in range(n):
-            cost = (cost + min(smaller[i], larger[i])) % mod
+        def query(index):
+            s = 0
+            while index:
+                s += tree[index]
+                index -= index & (-index)
+            return s
 
-        return cost
+        tree = [0] * (n+1)
+        cost = 0
+        hash = defaultdict(int)
+
+        for i, num in enumerate(ins):
+            left = query(compression[num]-1)
+            right = i - left - hash[num]
             
+            cost = (cost + min(left, right)) % mod
+            update(compression[num])
+            hash[num] += 1
+        
+        return cost
