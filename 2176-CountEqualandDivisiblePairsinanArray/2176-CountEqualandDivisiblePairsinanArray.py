5d1b84d6bc19eaf60ea4d6d92c316010654e486b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-17 08:15:25 +0530

    File updated at 17/4/2025, 8:15:20 am

--- 2176-CountEqualandDivisiblePairsinanArray/2176-CountEqualandDivisiblePairsinanArray.py
+++ 2176-CountEqualandDivisiblePairsinanArray/2176-CountEqualandDivisiblePairsinanArray.py
@@ -1,10 +1,13 @@
-# Last updated: 17/4/2025, 8:14:02 am
+# Last updated: 17/4/2025, 8:15:20 am
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
         n = len(nums)
+        hash = defaultdict(list)
         ret = 0
-        for i in range(n):
-            for j in range(i+1, n):
-                if nums[i] == nums[j] and i * j % k == 0: ret += 1
+        for i, num in enumerate(nums):
+            for j in hash[num]:
+                if i * j % k == 0: ret += 1
+            
+            hash[num].append(i)
         
         return ret
