4d253b80aaf889d0130d1938d5f332fdb8c6966e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-17 19:27:29 +0530

    File updated at 17/4/2025, 7:27:23 pm

--- 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
+++ 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
@@ -1,8 +1,7 @@
-# Last updated: 17/4/2025, 8:38:24 am
+# Last updated: 17/4/2025, 7:27:23 pm
 class Solution:
     def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
-
         relation = [0] * n
 
         for i, num in enumerate(nums2):
@@ -12,49 +11,27 @@ class Solution:
         for num in nums1:
             arr.append(relation[num])
 
-        smaller = [0] * n
-        larger = [0] * n
-
-        def mergeSort(arr):
-            nonlocal smaller, larger
-            if len(arr) == 1: return
-            mid = len(arr) // 2
-            
-            left = arr[:mid]
-            right = arr[mid:]
-            
-            mergeSort(left)
-            mergeSort(right)
-            
-            i = 0
-            j = 0
-            k = 0
-            
-            while i < len(left) and j < len(right):
-                if left[i][0] < right[j][0]:
-                    arr[k] = left[i]
-                    larger[left[i][1]] += len(right) - j
-                    i += 1
-                else:
-                    arr[k] = right[j]
-                    smaller[right[j][1]] += i
-                    j += 1
-                
-                k += 1
-            
-            while i < len(left):
-                arr[k] = left[i]
-                i += 1
-                k += 1
-                
-            while j < len(right):
-                arr[k] = right[j]
-                smaller[right[j][1]] += i
-                j += 1
-                k += 1
-        mergeSort([(num, i) for i, num in enumerate(arr)])
+        m = n+1
+        tree = [0] * (m+1)
         ret = 0
-        for i in range(n):
-            ret += smaller[i] * larger[i]
 
+        def query(index):
+            s = 0
+            while index:
+                s += tree[index]
+                index -= index & -index
+            return s
+        
+        def update(index):
+            while index <= m:
+                tree[index] += 1
+                index += index & -index
+        
+        for i, num in enumerate(arr):
+            left = query(num)
+            right = (n-num-1)-(i-left)
+            ret += left * right
+            update(num+1)
+        
         return ret
+        
