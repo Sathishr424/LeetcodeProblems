d8e637ef87f704a43c4169fdb014ebc5e456578f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-18 22:51:43 +0530

    File updated at 18/4/2025, 10:51:42 pm

--- 503-NextGreaterElementII/503-NextGreaterElementII.py
+++ 503-NextGreaterElementII/503-NextGreaterElementII.py
@@ -1,4 +1,4 @@
-# Last updated: 18/4/2025, 10:50:22 pm
+# Last updated: 18/4/2025, 10:51:42 pm
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         n = len(nums)
@@ -7,10 +7,10 @@ class Solution:
 
         for i in range(n*2 - 1):
             i %= n
-            while stack and stack[-1][0] < nums[i]:
-                ret[stack.pop()[1]] = nums[i]
+            while stack and nums[stack[-1]] < nums[i]:
+                ret[stack.pop()] = nums[i]
             
-            stack.append((nums[i], i))
+            stack.append(i)
 
         return ret
             
