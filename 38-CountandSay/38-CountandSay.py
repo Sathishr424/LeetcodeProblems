f4d396e1d70cfb7fd8af44e9173cff4275ff08a2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-18 06:48:11 +0530

    File updated at 18/4/2025, 6:48:11 am

--- 38-CountandSay/38-CountandSay.py
+++ 38-CountandSay/38-CountandSay.py
@@ -1,4 +1,4 @@
-# Last updated: 18/4/2025, 6:46:31 am
+# Last updated: 18/4/2025, 6:48:11 am
 class Solution:
     def countAndSay(self, n: int) -> str:
         st = '1'
@@ -7,13 +7,13 @@ class Solution:
             prev = st[0]
             cnt = 1
             new_st = ''
-            for char in st[1:]:
-                if char == prev:
+            for j in range(1, len(st)):
+                if st[j] == prev:
                     cnt += 1
                 else:
                     new_st += f"{cnt}{prev}"
                     cnt = 1
-                prev = char
+                prev = st[j]
             new_st += f"{cnt}{prev}"
             st = new_st
             i += 1
