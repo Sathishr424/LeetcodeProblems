2218eb6dbb57acb389dae36e3858982c1c756c12
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-18 20:30:57 +0530

    File updated at 18/4/2025, 8:30:56 pm

--- 1754-LargestMergeOfTwoStrings/1754-LargestMergeOfTwoStrings.py
+++ 1754-LargestMergeOfTwoStrings/1754-LargestMergeOfTwoStrings.py
@@ -1,16 +1,31 @@
-# Last updated: 9/4/2025, 10:27:50 pm
+# Last updated: 18/4/2025, 8:30:56 pm
 class Solution:
     def largestMerge(self, word1: str, word2: str) -> str:
-        ret = ''
+        m = len(word1)
+        n = len(word2)
 
-        while word1 and word2:
-            if word1 > word2:
-                ret += word1[0]
-                word1 = word1[1:]
-            else:
-                ret += word2[0]
-                word2 = word2[1:]
+        def greater(l, r):
+            while l < m and r < n and word1[l] == word2[r]:
+                l += 1
+                r += 1
+            return r == len(word2) or (l < len(word1) and word1[l] > word2[r])
         
-        return ret + word1 + word2
-
-
+        ret = []
+        l = 0
+        r = 0
+        while l < m and r < n:
+            if greater(l, r):
+                ret.append(word1[l])
+                l += 1
+            else:
+                ret.append(word2[r])
+                r += 1
+        
+        while l < m:
+            ret.append(word1[l])
+            l += 1
+        while r < n:
+            ret.append(word2[r])
+            r += 1
+        
+        return ''.join(ret)
