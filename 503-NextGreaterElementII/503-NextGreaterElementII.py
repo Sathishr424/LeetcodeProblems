99eec90ab7a7a19571453c9476e1307b260a30f3
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-19 02:45:48 +0530

    File updated at 19/4/2025, 2:45:47 am

--- 503-NextGreaterElementII/503-NextGreaterElementII.py
+++ 503-NextGreaterElementII/503-NextGreaterElementII.py
@@ -1,14 +1,14 @@
-# Last updated: 19/4/2025, 2:43:49 am
+# Last updated: 19/4/2025, 2:45:47 am
 class Solution:
     def carFleet(self, target: int, pos: List[int], speed: List[int]) -> int:
-        arr = []
+        sorted_pos_and_speed = []
         for i in range(len(pos)):
-            arr.append((pos[i], speed[i]))
-        arr.sort()
+            sorted_pos_and_speed.append((pos[i], speed[i]))
+        sorted_pos_and_speed.sort()
 
         stack = []
 
-        for p, s in arr:
+        for p, s in sorted_pos_and_speed:
             d = (target-p) / s
             while stack and stack[-1] <= d:
                 stack.pop()
