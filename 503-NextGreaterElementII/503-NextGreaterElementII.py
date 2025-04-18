712bae9bc331143b90dc9fbd3d310fca4730bfb3
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-18 23:24:38 +0530

    File updated at 18/4/2025, 11:24:36 pm

--- 503-NextGreaterElementII/503-NextGreaterElementII.py
+++ 503-NextGreaterElementII/503-NextGreaterElementII.py
@@ -1,4 +1,4 @@
-# Last updated: 18/4/2025, 11:16:23 pm
+# Last updated: 18/4/2025, 11:24:36 pm
 inf = float('inf')
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
@@ -12,21 +12,22 @@ class Solution:
                 smaller[stack.pop()] = nums[i]
             
             stack.append(i)
-        
+
         stack = []
         for i in range(n-1, -1, -1):
             while stack and nums[stack[-1]] < nums[i]:
                 greater[stack.pop()] = nums[i]
             
             stack.append(i)
-        
+
         ret = 0
-        for i in range(n):
-            if greater[i] != inf or smaller[i] != inf: break
+        left = 0
+        while left < n and greater[left] == inf and smaller[left] == inf:
+            left += 1
         
-        left = i
-        for i in range(n-1, -1, -1):
-            if greater[i] != inf or smaller[i] != inf: return i-left+1
+        right = n-1
+        while right >= 0 and greater[right] == inf and smaller[right] == inf:
+            right -= 1
         
-        return 0
+        return max(right-left+1, 0)
 
