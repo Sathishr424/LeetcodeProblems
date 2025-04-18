9c9c30e1b47a64d4184305287c35e75c91d431ee
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-18 06:52:25 +0530

    File updated at 18/4/2025, 6:52:24 am

--- 38-CountandSay/38-CountandSay.py
+++ 38-CountandSay/38-CountandSay.py
@@ -1,20 +1,23 @@
-# Last updated: 18/4/2025, 6:51:19 am
-class Solution:
-    @cache
-    def countAndSay(self, n: int) -> str:
-        if n == 1: return '1'
+# Last updated: 18/4/2025, 6:52:24 am
+@cache
+def rec(n):
+    if n == 1: return '1'
 
-        before = self.countAndSay(n-1)
-        prev = before[0]
-        cnt = 1
-        ret = ''
-        for i in range(1, len(before)):
-            if before[i] == prev:
-                cnt += 1
-            else:
-                ret += f"{cnt}{prev}"
-                cnt = 1
-            prev = before[i]
-                
-        ret += f"{cnt}{prev}"
-        return ret
+    before = rec(n-1)
+    prev = before[0]
+    cnt = 1
+    ret = ''
+    for i in range(1, len(before)):
+        if before[i] == prev:
+            cnt += 1
+        else:
+            ret += f"{cnt}{prev}"
+            cnt = 1
+        prev = before[i]
+            
+    ret += f"{cnt}{prev}"
+    return ret
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        return rec(n)
