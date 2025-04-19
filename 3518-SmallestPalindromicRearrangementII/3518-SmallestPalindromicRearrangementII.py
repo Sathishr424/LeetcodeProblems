fbef664064e0415ffc980b4afc12a0c28dae3946
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-20 01:15:12 +0530

    File updated at 20/4/2025, 1:15:09 am

--- 3518-SmallestPalindromicRearrangementII/3518-SmallestPalindromicRearrangementII.py
+++ 3518-SmallestPalindromicRearrangementII/3518-SmallestPalindromicRearrangementII.py
@@ -1,4 +1,4 @@
-# Last updated: 20/4/2025, 1:07:50 am
+# Last updated: 20/4/2025, 1:15:09 am
 @cache
 def fact(x):
     if x <= 1: return 1
@@ -8,7 +8,7 @@ MAX = 10**6
 log_fact = [0] * (MAX+1)
 for i in range(1, MAX+1): 
     log_fact[i] = log_fact[i-1] + log2(i)
-# print(log_fact)
+
 class Solution:
     def smallestPalindrome(self, s: str, k: int) -> str:
         n = len(s)
@@ -20,9 +20,11 @@ class Solution:
         half = n//2
         counts = {}
 
-        def getPerm(arr, k, b):
-            n = len(arr)
+        def getPerm(k, b):
+            n = sum(counts.values())
             if n == 0: return ''
+
+            c = sorted(counts.keys())
 
             f = log_fact[n]
 
@@ -30,20 +32,19 @@ class Solution:
                 f -= log_fact[counts[char]]
             
             if f >= 32:
-                x = b // fact(counts[arr[0]]) * fact(counts[arr[0]]-1)
-                counts[arr[0]] -= 1
-                return arr[0] + getPerm(arr[1:], k, x)
+                x = b // fact(counts[c[0]]) * fact(counts[c[0]]-1)
+                counts[c[0]] -= 1
+                if counts[c[0]] == 0: del counts[c[0]]
+                return c[0] + getPerm(k, x)
 
-            for i in range(n):
-                if i > 0 and arr[i] == arr[i-1]: continue
-                char = arr[i]
-
+            for char in c:
                 x = b // fact(counts[char]) * fact(counts[char]-1)
                 f = fact(n-1) // x
 
                 if f >= k:
                     counts[char] -= 1
-                    return char + getPerm(arr[:i] + arr[i+1:], k, x)
+                    if counts[char] == 0: del counts[char]
+                    return char + getPerm(k, x)
 
                 k -= f
             return ''
@@ -52,15 +53,11 @@ class Solution:
         for char in uniq:
             if uniq[char] % 2:
                 odd = char
-            counts[char] = uniq[char] // 2
-            bottom *= fact(counts[char])
+            if uniq[char] > 1:
+                counts[char] = uniq[char] // 2
+                bottom *= fact(counts[char])
 
-        arr = []
-        for char in sorted(counts.keys()):
-            for i in range(counts[char]):
-                arr.append(char)
-
-        st = getPerm(arr, k, bottom)
+        st = getPerm(k, bottom)
         if len(st) != half: return ''
         
         return st + odd + st[::-1]
