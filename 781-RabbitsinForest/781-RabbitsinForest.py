c7e8c261c7d5b241bcef0c14db101e1c8c070c00
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-20 13:19:59 +0530

    File updated at 20/4/2025, 1:19:55 pm

--- 781-RabbitsinForest/781-RabbitsinForest.py
+++ 781-RabbitsinForest/781-RabbitsinForest.py
@@ -1,13 +1,21 @@
-# Last updated: 20/4/2025, 1:19:27 pm
+# Last updated: 20/4/2025, 1:19:55 pm
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
+        answers.sort()
+        prev = answers[0]
+        cnt = 1
         ret = 0
-        freq = Counter(answers)
+        
+        def calc(num, cnt):
+            return ceil(cnt / num) * num
 
-        for num in freq:
-            cnt = freq[num]
-            num += 1
-
-            ret += ceil(cnt / num) * num
+        for i in range(1, len(answers)):
+            num = answers[i]
+            if num == prev:
+                cnt += 1
+            else:
+                ret += calc(prev+1, cnt)
+                cnt = 1
+            prev = num
                 
-        return ret
+        return ret + calc(prev+1, cnt)
