00f265d75e12e23174aba7bc6e82b669bd9913fe
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-21 00:22:32 +0530

    File updated at 21/4/2025, 12:22:28 am

--- 3470-PermutationsIV/3470-PermutationsIV.py
+++ 3470-PermutationsIV/3470-PermutationsIV.py
@@ -1,4 +1,4 @@
-# Last updated: 21/4/2025, 12:11:01 am
+# Last updated: 21/4/2025, 12:22:28 am
 @cache
 def fact(x, y):
     if x == 0 or y == 0: return 1
@@ -6,22 +6,14 @@ def fact(x, y):
 
 class Solution:
     def permute(self, n: int, k: int) -> List[int]:
-        def perm(evens, odds, k, is_odd):
-            n = len(evens) + len(odds)
-
-            if is_odd:
-                for i, num in enumerate(evens):
-                    f = fact(len(odds), len(evens)-1)
-                    if f >= k:
-                        return [num] + perm(evens[:i] + evens[i+1:], odds, k, False)
-                    k -= f
-            else:
-                for i, num in enumerate(odds):
-                    f = fact(len(evens), len(odds)-1)
-                    if f >= k:
-                        return [num] + perm(evens, odds[:i] + odds[i+1:], k, True)
-                    k -= f
+        def perm(evens, odds, k):
+            for i, num in enumerate(evens):
+                f = fact(len(odds), len(evens)-1)
+                if f >= k:
+                    return [num] + perm(odds, evens[:i] + evens[i+1:], k)
+                k -= f
             return []
+        
         odds = []
         evens = []
         nums = []
@@ -30,21 +22,19 @@ class Solution:
             if i % 2: odds.append(i)
             else: evens.append(i)
         if n % 2:
-            return perm(evens, odds, k, False)
+            return perm(odds, evens, k)
         else:
             for i, num in enumerate(nums):
                 if num % 2:
                     f = fact(len(evens), len(odds)-1)
                     if f >= k:
                         i = i // 2
-                        return [num] + perm(evens, odds[:i] + odds[i+1:], k, True)
+                        return [num] + perm(evens, odds[:i] + odds[i+1:], k)
                 else:
                     f = fact(len(odds), len(evens)-1)
                     if f >= k:
                         i = i // 2
-                        return [num] + perm(evens[:i] + evens[i+1:], odds, k, False)
+                        return [num] + perm(odds, evens[:i] + evens[i+1:], k)
                 k -= f
         
         return []
-
-            
