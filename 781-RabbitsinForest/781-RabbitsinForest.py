512e240a2ab5df1d22af6a3ab7a0f1618c4dfdcc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-20 13:13:08 +0530

    File updated at 20/4/2025, 1:13:04 pm

--- 781-RabbitsinForest/781-RabbitsinForest.py
+++ 781-RabbitsinForest/781-RabbitsinForest.py
@@ -1,17 +1,21 @@
-# Last updated: 20/4/2025, 1:06:39 pm
+# Last updated: 20/4/2025, 1:13:04 pm
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
+        answers.sort()
+        prev = answers[0]
+        cnt = 1
         ret = 0
+        
+        def calc(num, cnt):
+            return cnt // num * num + (num if cnt % num else 0)
 
-        freq = defaultdict(int)
-
-        for num in answers:
-            freq[num+1] += 1
-
-        for num in freq:
-            cnt = freq[num]
-
-            ret += cnt // num * num
-            if cnt % num: ret += num
+        for i in range(1, len(answers)):
+            num = answers[i]
+            if num == prev:
+                cnt += 1
+            else:
+                ret += calc(prev+1, cnt)
+                cnt = 1
+            prev = num
                 
-        return ret
+        return ret + calc(prev+1, cnt)
