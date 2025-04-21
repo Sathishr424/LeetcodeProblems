30cc1c26fa6c82ed8694b5655f9562bc047887ed
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-21 13:51:49 +0530

    Create file

---
+++ 2145-CounttheHiddenSequences/2145-CounttheHiddenSequences.py
@@ -0,0 +1,17 @@
+# Last updated: 21/4/2025, 1:51:45 pm
+class Solution:
+    def numberOfArrays(self, diff: List[int], lower: int, upper: int) -> int:
+        prev = diff[0]
+        mini = prev
+        maxi = prev
+        for d in diff:
+            curr = prev+d
+            mini = min(curr, mini)
+            maxi = max(curr, maxi)
+            prev = curr
+
+        l = lower-mini
+        r = maxi+l
+        
+        return max(0, upper-r+1)
+            
