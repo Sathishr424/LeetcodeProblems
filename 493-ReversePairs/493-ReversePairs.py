91f352ea3eb5fe966e4917d4ad75d2af95913bbc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-22 02:13:59 +0530

    File updated at 22/4/2025, 2:13:53 am

--- 493-ReversePairs/493-ReversePairs.py
+++ 493-ReversePairs/493-ReversePairs.py
@@ -1,41 +1,11 @@
-# Last updated: 22/4/2025, 2:11:43 am
+# Last updated: 22/4/2025, 2:13:53 am
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        compression = {}
-        index = 1
-
-        arr = []
-        for num in nums:
-            arr.append(num)
-            arr.append(num * 2)
+        A = SortedList()
+        ans = 0
+        for i, n in enumerate(nums):
+            j = A.bisect_right(2*n)
+            ans += (i - j)
+            A.add(n)
         
-        for num in sorted(arr):
-            if num not in compression:
-                compression[num] = index
-                index += 1
-        
-        n = index + 1
-        m = n + 1
-        tree = [0] * m
-
-        def add(index):
-            while index < m:
-                tree[index] += 1
-                index += index & -index
-        
-        def query(index):
-            s = 0
-            while index:
-                s += tree[index]
-                index -= index & -index
-            return s
-
-        ret = 0
-        added = defaultdict(int)
-        for num in nums[::-1]:
-            ret += query(compression[num]) - added[num]
-            
-            add(compression[num * 2])
-            added[num*2] += 1
-        
-        return ret
+        return ans
