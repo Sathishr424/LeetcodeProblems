d73fceed6fe21bfd3b8f5994de3bea3eb77e4401
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-22 02:18:39 +0530

    File updated at 22/4/2025, 2:18:33 am

--- 493-ReversePairs/493-ReversePairs.py
+++ 493-ReversePairs/493-ReversePairs.py
@@ -1,11 +1,11 @@
-# Last updated: 22/4/2025, 2:18:02 am
+# Last updated: 22/4/2025, 2:18:33 am
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
         arr = SortedList()
         ret = 0
-        
+
         for i, num in enumerate(nums):
-            ret += i - bisect_right(arr, num*2)
+            ret += i - arr.bisect_right(num*2)
             arr.add(num)
         
         return ret
