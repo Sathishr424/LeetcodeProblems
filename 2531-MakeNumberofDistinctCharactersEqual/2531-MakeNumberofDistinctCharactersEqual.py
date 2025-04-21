514fd937eb65372297bdc3eb2fc719c69ef4449d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-22 01:11:21 +0530

    File updated at 22/4/2025, 1:11:15 am

--- 2531-MakeNumberofDistinctCharactersEqual/2531-MakeNumberofDistinctCharactersEqual.py
+++ 2531-MakeNumberofDistinctCharactersEqual/2531-MakeNumberofDistinctCharactersEqual.py
@@ -1,38 +1,26 @@
-# Last updated: 22/4/2025, 1:04:35 am
+# Last updated: 22/4/2025, 1:11:15 am
 class Solution:
     def isItPossible(self, word1: str, word2: str) -> bool:
-        m = len(word1)
-        n = len(word2)
-
-        x = defaultdict(int)
-        y = defaultdict(int)
-
-        for char in word1:
-            x[char] += 1
-        
-        for char in word2:
-            y[char] += 1
-
-        x_uniq = len(x)
-        y_uniq = len(y)
-
-        for x_char in x:
-            x_cnt = x_uniq
-            y_cnt = y_uniq
-
-            if x_char in y and x_cnt == y_cnt: return True
-
-            x_cnt -= x[x_char] == 1
-            y_cnt += x_char not in y
-
-            for y_char in y:
-                if y_char == x_char: continue
-                y_ = y_cnt
-                x_ = x_cnt
-                
-                if y[y_char] == 1: y_ -= 1
-                if y_char not in x: x_ += 1
-
-                if y_ == x_: return True
-
+        a = Counter(word1)
+        b = Counter(word2)
+        difference = len(b) - len(a)
+        if abs(difference) > 2:
+            return False
+        for aChoice in a:
+            outerDifference = difference
+            if a[aChoice] == 1:
+                outerDifference += 1
+            for bChoice in b:
+                innerDifference = outerDifference
+                if aChoice == bChoice:
+                    innerDifference = difference
+                else:
+                    if aChoice not in b:
+                        innerDifference += 1
+                    if bChoice not in a:
+                        innerDifference -= 1
+                    if b[bChoice] == 1:
+                        innerDifference -= 1
+                if innerDifference == 0:
+                    return True
         return False
