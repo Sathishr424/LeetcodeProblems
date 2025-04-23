734db53f213c43ead7c958a3c89c45b478f7bccf
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-23 22:32:35 +0530

    File updated at 23/4/2025, 10:32:34 pm

--- 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.py
+++ 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.py
@@ -1,32 +1,19 @@
-# Last updated: 23/4/2025, 10:30:49 pm
-cmax = lambda x, y: x if x > y else y
+# Last updated: 23/4/2025, 10:32:34 pm
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         nums.sort()
-        # prefixCnt - ((maxi - currElement) * cnt)
-
-        ret = 1
-        prefix = [0]
-        for num in nums:
-            prefix.append(prefix[-1] + num)
+        left = 0
+        ans = 0
+        curr = 0
         
-        def bn_l(l, r, index):
-            while l < r:
-                mid = (l + r) // 2
-
-                diff = index - mid
-                s = prefix[index] - prefix[mid]
-                s = abs((nums[index] * diff) - s)
-
-                if s <= k:
-                    r = mid
-                else:
-                    l = mid + 1
+        for right in range(len(nums)):
+            target = nums[right]
+            curr += target
             
-            return l
+            while (right - left + 1) * target - curr > k:
+                curr -= nums[left]
+                left += 1
+            
+            ans = max(ans, right - left + 1)
 
-        for i in range(len(nums)):
-            left = bn_l(0, i, i)
-            ret = cmax(ret, i-left+1)
-        
-        return ret
+        return ans
