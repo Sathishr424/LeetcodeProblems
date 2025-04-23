f126a7e69d2e8ef6adcbcac69f8d82211dac681e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-23 07:57:31 +0530

    File updated at 23/4/2025, 7:57:31 am

--- 2507-SmallestValueAfterReplacingWithSumofPrimeFactors/2507-SmallestValueAfterReplacingWithSumofPrimeFactors.py
+++ 2507-SmallestValueAfterReplacingWithSumofPrimeFactors/2507-SmallestValueAfterReplacingWithSumofPrimeFactors.py
@@ -1,25 +1,23 @@
-# Last updated: 23/4/2025, 5:04:20 am
+# Last updated: 23/4/2025, 7:57:31 am
 N = 10**5
 
 is_prime = [True] * (N+1)
 is_prime[0] = False
 is_prime[1] = False
 
-primes = []
-
 for i in range(2, int(N**0.5) + 1):
     if not is_prime[i]: continue
     for j in range(i*i, N+1, i):
         is_prime[j] = False
 
+primes = []
 for i in range(2, N+1):
     if is_prime[i]: primes.append(i)
 
 class Solution:
     def smallestValue(self, n: int) -> int:
-        prev_n = -1
-        while not is_prime[n] and n != prev_n:
-            prev_n = n
+        if n == 4: return 4
+        while not is_prime[n]:
             new_n = 0
             for num in primes:
                 if num > n: break
