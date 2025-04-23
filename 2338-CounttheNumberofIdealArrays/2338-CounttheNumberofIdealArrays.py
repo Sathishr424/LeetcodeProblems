1fa6369fa72c2b3bc621b9672cc853c382346cbf
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-23 21:59:41 +0530

    File updated at 23/4/2025, 9:59:40 pm

--- 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.py
+++ 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.py
@@ -1,23 +1,9 @@
-# Last updated: 23/4/2025, 9:54:55 pm
+# Last updated: 23/4/2025, 9:59:40 pm
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         n = len(nums)
-
         nums.sort()
-
-        # k = 10
-        # 2, 3, 4, 12, 32, 44, 66, 120
-        # 2, 1
-        
-        # 2, 3, 4, 8, 10 => prefixCnt - ((maxi - currElement) * cnt)
-        # 4, 1, 0, 4, 6
-
-        # 4, 1, 0, 0, 1, 2, 1, 2
-
-        # 2, 1
-        # 8, 7, 6, 2, 0
-
-        # 20-15 = 5, 20-2 = 18
+        # prefixCnt - ((maxi - currElement) * cnt)
 
         ret = 1
         maxi = nums[-1] + 1
@@ -28,9 +14,10 @@ class Solution:
         def bn_l(l, r, index, val, rem):
             while l < r:
                 mid = (l + r) // 2
+                
                 diff = index - mid
                 s = prefix[index] - prefix[mid]
-                s = s - (maxi - nums[index]) * diff
+                s = s - (val * diff)
 
                 if s <= rem:
                     r = mid
@@ -42,9 +29,10 @@ class Solution:
         def bn_r(l, r, index, val, rem):
             while l < r:
                 mid = (l + r) // 2
+
                 diff = index - mid
-                s = prefix[mid] - prefix[index] + (maxi-nums[mid])
-                s = s - (maxi - nums[index]) * diff
+                s = prefix[mid+1] - prefix[index]
+                s = s - (val * diff)
 
                 if s >= rem:
                     r = mid
@@ -52,15 +40,14 @@ class Solution:
                     l = mid + 1
             
             return l
-        # print(nums)
+
         for i in range(1, n):
             num = nums[i]
 
-            left = bn_l(0, i, i, num, k)
+            left = bn_l(0, i, i, maxi-num, k)
             s = abs(prefix[i] - prefix[left])
-            right = bn_r(i, n-1, i, num, k-s)
+            right = bn_r(i, n-1, i, maxi-num, k-s)
 
             ret = max(ret, right-left+1)
-            # print(i, num, (left, right), (right-left))
         
         return ret
