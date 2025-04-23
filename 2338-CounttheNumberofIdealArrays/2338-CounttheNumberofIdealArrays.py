3d3c57a84065799e0e1713d02cf4fa5248f9a56d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-23 22:28:44 +0530

    File updated at 23/4/2025, 10:28:42 pm

--- 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.py
+++ 2338-CounttheNumberofIdealArrays/2338-CounttheNumberofIdealArrays.py
@@ -1,4 +1,4 @@
-# Last updated: 23/4/2025, 10:27:16 pm
+# Last updated: 23/4/2025, 10:28:42 pm
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
@@ -25,28 +25,11 @@ class Solution:
                     l = mid + 1
             
             return l
-        
-        def bn_r(l, r, index, num, rem):
-            while l < r:
-                mid = (l + r) // 2
 
-                diff = index - mid
-                s = prefix[mid+1] - prefix[index]
-                s = abs((num * diff) - s)
-
-                if s > rem:
-                    r = mid
-                else:
-                    l = mid + 1
-            
-            return l
-
-        for i in range(1, n):
+        for i in range(n):
             num = nums[i]
 
             left = bn_l(0, i, i, num, k)
-            s = abs(prefix[i] - prefix[left])
-            right = bn_r(i+1, n, i, num, k-s)
-            ret = cmax(ret, right-left)
+            ret = cmax(ret, i-left+1)
         
         return ret
