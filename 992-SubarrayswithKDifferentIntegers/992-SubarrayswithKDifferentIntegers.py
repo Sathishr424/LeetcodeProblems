a044d100e6a59e9ac284234a2107c9b3a2fe3490
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-24 11:18:25 +0530

    File updated at 24/4/2025, 11:18:25 am

--- 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
+++ 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
@@ -1,38 +1,37 @@
-# Last updated: 24/4/2025, 11:16:45 am
+# Last updated: 24/4/2025, 11:18:25 am
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
         n = len(nums)
         ret = 0
 
-        uniq = [0] * (n+1)
+        uniq = [[0, 0] for _ in range(n+1)]
         matches = 0
         left = 0
         prev = 0
 
-        cnts = [0] * (n+1)
         cnt_matches = 0
         cnt_left = 0
 
         for i in range(n):
-            cnts[nums[i]] += 1
-            cnt_matches += cnts[nums[i]] == 1
+            uniq[nums[i]][1] += 1
+            cnt_matches += uniq[nums[i]][1] == 1
 
             if cnt_matches > k:
                 while cnt_left < i and cnt_matches > k:
                     num = nums[cnt_left]
-                    cnts[num] -= 1
-                    cnt_matches -= cnts[num] == 0
+                    uniq[num][1] -= 1
+                    cnt_matches -= uniq[num][1] == 0
                     cnt_left += 1
                 prev = 0
             
-            uniq[nums[i]] += 1
-            matches += uniq[nums[i]] == 1
+            uniq[nums[i]][0] += 1
+            matches += uniq[nums[i]][0] == 1
 
             if matches == k:
                 while left <= i and matches == k:
                     num = nums[left]
-                    uniq[num] -= 1
-                    matches -= uniq[num] == 0
+                    uniq[num][0] -= 1
+                    matches -= uniq[num][0] == 0
                     left += 1
                     prev += 1
  
