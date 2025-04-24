295a3b77ae41ffdf041bd3a50539846cea7509c2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-24 15:45:10 +0530

    File updated at 24/4/2025, 3:45:10 pm

--- 187-RepeatedDNASequences/187-RepeatedDNASequences.py
+++ 187-RepeatedDNASequences/187-RepeatedDNASequences.py
@@ -1,13 +1,12 @@
-# Last updated: 24/4/2025, 3:37:14 pm
+# Last updated: 24/4/2025, 3:45:10 pm
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         n = len(s)
         if n <= 10: return []
         k = 10
         visited = defaultdict(int)
-        visited[s[:10]] += 1
-        for i in range(k, n):
-            visited[s[i-k+1:i+1]] += 1
+        for i in range(n-k+1):
+            visited[s[i:i+k]] += 1
         ret = []
         for st in visited:
             if visited[st] > 1:
