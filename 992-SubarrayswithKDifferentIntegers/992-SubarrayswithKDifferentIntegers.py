17c094d12f34ad154235b8fbeb6f320fbbbb0708
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-24 11:15:31 +0530

    File updated at 24/4/2025, 11:15:31 am

--- 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
+++ 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
@@ -1,42 +1,28 @@
-# Last updated: 24/4/2025, 11:09:00 am
+# Last updated: 24/4/2025, 11:15:31 am
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        ret = 0
+        return self.slidingWindowAtMost(nums, k) - self.slidingWindowAtMost(nums, k - 1)
 
-        uniq = [0] * (n+1)
-        matches = 0
+    # Helper function to count the number of subarrays with at most k distinct elements.
+    def slidingWindowAtMost(self, nums: List[int], distinctK: int) -> int:
+        # To store the occurrences of each element.
+        freq_map = defaultdict(int)
         left = 0
-        prev = 0
+        total_count  = 0
 
-        cnts = [0] * (n+1)
-        cnt_matches = 0
-        cnt_left = 0
+        # Right pointer of the sliding window iterates through the array.
+        for right in range(len(nums)):
+            freq_map[nums[right]] += 1
 
-        for i in range(n):
-            cnts[nums[i]] += 1
-            if cnts[nums[i]] == 1: cnt_matches += 1
+            # If the number of distinct elements in the window exceeds k,
+            # we shrink the window from the left until we have at most k distinct elements.
+            while len(freq_map) > distinctK:
+                freq_map[nums[left]] -= 1
+                if freq_map[nums[left]] == 0:
+                    del freq_map[nums[left]]
+                left += 1
 
-            if cnt_matches > k:
-                while cnt_left < i and cnt_matches > k:
-                    num = nums[cnt_left]
-                    cnts[num] -= 1
-                    if cnts[num] == 0: cnt_matches -= 1
-                    cnt_left += 1
-                prev = 0
-            
-            uniq[nums[i]] += 1
-            if uniq[nums[i]] == 1: matches += 1
-
-            if matches == k:
-                while left <= i and matches == k:
-                    num = nums[left]
-                    uniq[num] -= 1
-                    if uniq[num] == 0: matches -= 1
-                    left += 1
-                    prev += 1
- 
-            ret += prev
-        
-        return ret
-        
+            # Update the total count by adding the length of the current subarray.
+            total_count  += right - left + 1
+        print(total_count)
+        return total_count 
