13446f4ed2015db2855257201dd428ae14c4cfca
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-24 16:04:04 +0530

    File updated at 24/4/2025, 4:04:03 pm

--- 187-RepeatedDNASequences/187-RepeatedDNASequences.py
+++ 187-RepeatedDNASequences/187-RepeatedDNASequences.py
@@ -1,14 +1,42 @@
-# Last updated: 24/4/2025, 3:45:10 pm
+# Last updated: 24/4/2025, 4:04:03 pm
+k = 10
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         n = len(s)
-        if n <= 10: return []
-        k = 10
+        if n <= k: return []
+        base = 5
+        mod = 10**9 + 7
+
+        link = {'A': 1, 'C': 2, 'G': 3, 'T': 4}
+
+        cache = pow(base, 9, mod)
+
+        def rolling_hash_add(num, val):
+            return ((num * base % mod) + val) % mod
+        
+        def rolling_hash_delete(num, val):
+            return (num - (val * cache % mod)) % mod
+        
+        num = 0
         visited = defaultdict(int)
-        for i in range(n-k+1):
-            visited[s[i:i+k]] += 1
+
+        for i in range(k):
+            num = rolling_hash_add(num, link[s[i]])
+        
+        substrings = {}
+        visited[num] += 1
+        substrings[num] = s[:10]
+
+        for i in range(k, n):
+            num = rolling_hash_delete(num, link[s[i-k]])
+            num = rolling_hash_add(num, link[s[i]])
+            visited[num] += 1
+            if visited[num] == 1:
+                substrings[num] = s[i-k+1:i+1]
+
         ret = []
-        for st in visited:
-            if visited[st] > 1:
-                ret.append(st)
+        for num in visited:
+            if visited[num] > 1:
+                ret.append(substrings[num])
+        
         return ret
