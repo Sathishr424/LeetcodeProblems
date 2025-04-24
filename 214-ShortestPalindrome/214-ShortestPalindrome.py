8a2e8b9062087a2c2aa9e4f57de2c422573834b4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-24 19:22:39 +0530

    File updated at 24/4/2025, 7:22:38 pm

--- 214-ShortestPalindrome/214-ShortestPalindrome.py
+++ 214-ShortestPalindrome/214-ShortestPalindrome.py
@@ -1,55 +1,50 @@
-# Last updated: 24/4/2025, 7:12:27 pm
+# Last updated: 24/4/2025, 7:22:38 pm
 mod= 10**9 + 7
 base = 28
+inverseBase = pow(base, mod-2, mod)
 
-def modInverse(num):
-    return pow(num, mod-2, mod)
+def addNum(num, val):
+    return ( (num * base % mod) + val ) % mod
+
+def deleteRight(num, val):
+    num -= val
+    return num * inverseBase % mod
+
+def deleteLeft(num, val, distance):
+    return (num - ( val * pow(base, distance, mod) % mod) ) % mod
+
+def getAlpIndex(val):
+    return ord(val) - 96
 
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         n = len(s)
-
-        def addNum(num, val):
-            return ((num * base % mod) + val) % mod
-        
-        def addLeft(num, val, distance):
-            return (num + (val * pow(base, distance, mod) % mod)) % mod
-        
-        def deleteRight(num, val):
-            num -= val
-            return num * modInverse(base) % mod
-        
-        def deleteLeft(num, val, distance):
-            return (num - ( val * pow(base, distance, mod) % mod) ) % mod
         
         left = 0
         right = 0
 
         for i in range(n // 2):
-            left = addNum(left, ord(s[i]) - 96)
-            right = addNum(right, ord(s[n-i-1]) - 96)
-
-        alt = True
-        if n % 2:
-            alt = False
+            left = addNum(left, getAlpIndex(s[i]))
+            right = addNum(right, getAlpIndex(s[n-i-1]))
 
         if left == right: return s
+
+        isOddTurn = n % 2 != 1
         
         l_pos = [0, n//2 - 1]
         r_pos = [(n+1) // 2, n-1]
 
         while l_pos[1] >= 0:
-            # print(l_pos, r_pos, s[:l_pos[1]+1], s[r_pos[0]:r_pos[1]+1], (left, right), alt)
-            right = deleteLeft(right, ord(s[r_pos[1]]) - 96, r_pos[1] - r_pos[0])
+            right = deleteLeft(right, getAlpIndex(s[r_pos[1]]), r_pos[1] - r_pos[0])
             r_pos[1] -= 1
-            if not alt:
+            if not isOddTurn:
                 r_pos[0] -= 1
-                right = addNum(right, ord(s[r_pos[0]]) - 96)
-                alt = True
+                right = addNum(right, getAlpIndex(s[r_pos[0]]))
+                isOddTurn = True
             else:
-                left = deleteRight(left, ord(s[l_pos[1]]) - 96)
+                left = deleteRight(left, getAlpIndex(s[l_pos[1]]))
                 l_pos[1] -= 1
-                alt = False
+                isOddTurn = False
             if left == right: return s[r_pos[1]+1:][::-1] + s
 
         return ''
