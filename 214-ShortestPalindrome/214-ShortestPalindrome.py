26d09643d689995b7d4e4f2aff6ba2f27f838525
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-25 01:36:42 +0530

    File updated at 25/4/2025, 1:36:40 am

--- 214-ShortestPalindrome/214-ShortestPalindrome.py
+++ 214-ShortestPalindrome/214-ShortestPalindrome.py
@@ -1,34 +1,30 @@
-# Last updated: 25/4/2025, 1:35:33 am
+# Last updated: 25/4/2025, 1:36:40 am
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         n = len(s)
 
-        def kmp(st):
-            m = len(st)
-            lps = [0] * m
-            i = 1
-            j = 0
+        lps = [0] * n
+        i = 1
+        j = 0
 
-            while i < m:
-                if st[i] == st[j]:
-                    j += 1
-                    lps[i] = j
-                elif j > 0:
-                    j = lps[j-1]
-                    continue
-                i += 1
-            return lps
+        while i < n:
+            if s[i] == s[j]:
+                j += 1
+                lps[i] = j
+            elif j > 0:
+                j = lps[j-1]
+                continue
+            i += 1
         
-        x = kmp(s)
         j = 0
         i = 0
         st = s[::-1]
-        
+
         while i < n:
             if st[i] == s[j]:
                 j += 1
             elif j > 0:
-                j = x[j-1]
+                j = lps[j-1]
                 continue
             i += 1
 
