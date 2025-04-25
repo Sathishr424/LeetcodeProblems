9cacafeb297843660403143be44b3196ee70c431
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-25 15:44:05 +0530

    File updated at 25/4/2025, 3:44:03 pm

--- 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.py
+++ 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.py
@@ -1,4 +1,4 @@
-# Last updated: 25/4/2025, 3:43:10 pm
+# Last updated: 25/4/2025, 3:44:03 pm
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         ret = 0
@@ -6,18 +6,17 @@ class Solution:
         s = 0
         prefix = defaultdict(int)
         prefix[0] = 1
-
         for i in range(n):
-            
             s += nums[i]
-
             if nums[i] % k == 0: ret += 1
-
-            if prefix[s % k]:
-                ret += prefix[s % k]
-                if s % k == (s-nums[i]) % k: ret -= 1
             
-            prefix[s % k] += 1
+            rem = s % k
+
+            if prefix[rem]:
+                ret += prefix[rem]
+                if rem == (s-nums[i]) % k: ret -= 1
+            
+            prefix[rem] += 1
         
         return ret
 
