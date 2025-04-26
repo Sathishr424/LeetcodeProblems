37eca0baa3424705acdf1658a677c8e6a0cd6c55
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-26 22:52:36 +0530

    File updated at 26/4/2025, 10:52:32 pm

--- 3527-FindtheMostCommonResponse/3527-FindtheMostCommonResponse.py
+++ 3527-FindtheMostCommonResponse/3527-FindtheMostCommonResponse.py
@@ -1,19 +1,21 @@
-# Last updated: 26/4/2025, 10:29:09 pm
+# Last updated: 26/4/2025, 10:52:32 pm
 class Solution:
     def findCommonResponse(self, responses: List[List[str]]) -> str:
         cnts = defaultdict(int)
 
+        ret = ''
         for i, res in enumerate(responses):
             uniq = {}
             for s in res:
                 if s not in uniq:
                     cnts[s] += 1
                     uniq[s] = 1
+                    ret = s
 
-        arr = sorted(cnts.keys())
-        ret = arr[0]
-        for s in arr:
+        for s in cnts.keys():
             if cnts[s] > cnts[ret]:
+                ret = s
+            elif cnts[s] == cnts[ret] and s < ret:
                 ret = s
 
         return ret
