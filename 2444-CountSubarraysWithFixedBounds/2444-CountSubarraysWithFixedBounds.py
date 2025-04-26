ab4e21cc349a4afc1b0b227ff89ae12d77b3b3b4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-26 09:07:53 +0530

    File updated at 26/4/2025, 9:07:50 am

--- 2444-CountSubarraysWithFixedBounds/2444-CountSubarraysWithFixedBounds.py
+++ 2444-CountSubarraysWithFixedBounds/2444-CountSubarraysWithFixedBounds.py
@@ -1,8 +1,7 @@
-# Last updated: 26/4/2025, 8:58:44 am
+# Last updated: 26/4/2025, 9:07:50 am
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
         n = len(nums)
-        
         """
         [1,3,5,2,7,5]
         [5,3,5,1,2,1,5,8,7,1,2,5,2]
@@ -11,19 +10,31 @@ class Solution:
         prev = 0
         left = 0
         ret = 0
-        arr = SortedList()
+        mnk = 0
+        mxk = 0
 
         for i, num in enumerate(nums):
-            arr.add(num)
 
-            if arr[0] < minK or arr[-1] > maxK:
+            if num == minK:
+                mnk += 1
+
+            if num == maxK:
+                mxk += 1
+
+            if num < minK or num > maxK:
                 prev = 0
-                arr.clear()
                 left = i+1
+                mnk = 0
+                mxk = 0
                 continue
-            # print(arr, arr[0], arr[-1])
-            while arr and arr[0] == minK and arr[-1] == maxK:
-                arr.remove(nums[left])
+
+            while mnk > 0 and mxk > 0:
+                num = nums[left]
+                if num == minK:
+                    mnk -= 1
+                if num == maxK:
+                    mxk -= 1
+                
                 left += 1
                 prev += 1
             
