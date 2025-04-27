9181eb85e35672e9dea2e511fd05488ce79f3091
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-27 17:28:20 +0530

    File updated at 27/4/2025, 5:28:16 pm

--- 3532-PathExistenceQueriesinaGraphI/3532-PathExistenceQueriesinaGraphI.py
+++ 3532-PathExistenceQueriesinaGraphI/3532-PathExistenceQueriesinaGraphI.py
@@ -1,8 +1,7 @@
-# Last updated: 27/4/2025, 5:25:03 pm
+# Last updated: 27/4/2025, 5:28:16 pm
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
         parents = [i for i in range(n)]
-        sizes = [1] * n
 
         def find(x):
             if x != parents[x]:
@@ -10,21 +9,14 @@ class Solution:
             return parents[x]
         
         def union(x, y):
-            node1 = find(x)
-            node2 = find(y)
-
-            if node1 == node2: return True
-            if sizes[node2] > sizes[node1]:
-                node1, node2 = node2, node1
-            
-            sizes[node1] += sizes[node2]
-            parents[node2] = node1
-            return False
+            parents[find(y)] = find(x)
         
         for i in range(1, n):
             if nums[i] - nums[i-1] <= maxDiff:
                 union(i-1, i)
+        
         ret = []
+
         for x, y in queries:
             ret.append(find(x) == find(y))
         
