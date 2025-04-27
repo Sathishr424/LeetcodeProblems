6525d4c99ee9e1f6c16c4ecadf3f80941c4f4f4e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-27 17:11:58 +0530

    File updated at 27/4/2025, 5:11:55 pm

--- 3531-CountCoveredBuildings/3531-CountCoveredBuildings.py
+++ 3531-CountCoveredBuildings/3531-CountCoveredBuildings.py
@@ -1,15 +1,18 @@
-# Last updated: 27/4/2025, 4:57:11 pm
+# Last updated: 27/4/2025, 5:11:55 pm
+cmin = lambda x, y: x if x < y else y
+cmax = lambda x, y: x if x > y else y
+
 class Solution:
     def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
         horizontal = [[float('inf'), -float('inf')] for _ in range(n+1)]
         vertical = [[float('inf'), -float('inf')] for _ in range(n+1)]
 
         for i, j in buildings:
-            vertical[j][0] = min(vertical[j][0], i)
-            vertical[j][1] = max(vertical[j][1], i)
+            vertical[j][0] = cmin(vertical[j][0], i)
+            vertical[j][1] = cmax(vertical[j][1], i)
 
-            horizontal[i][0] = min(horizontal[i][0], j)
-            horizontal[i][1] = max(horizontal[i][1], j)
+            horizontal[i][0] = cmin(horizontal[i][0], j)
+            horizontal[i][1] = cmax(horizontal[i][1], j)
         
         ret = 0
         for i, j in buildings:
