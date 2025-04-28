07f55ca19635db79b56bd29690d3695c08f69164
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-28 16:46:57 +0530

    File updated at 28/4/2025, 4:46:53 pm

--- 2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.py
+++ 2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.py
@@ -1,32 +1,25 @@
-# Last updated: 28/4/2025, 4:43:22 pm
+# Last updated: 28/4/2025, 4:46:53 pm
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         """
         [2,1,4,3,5]
         """
         ret = 0
-        prefix = [0]
-
+        left = 0
+        cnt = 0
+        s = 0
+        prev = 0
         for num in nums:
-            prefix.append(prefix[-1] + num)
-        
-        n = len(nums)
+            cnt += 1
+            s += num
 
-        for i in range(n):
-            l = 0
-            r = i+1
-            p = prefix[i+1]
+            while s * cnt >= k:
+                cnt -= 1
+                s -= nums[left]
+                left += 1
+                prev -= 1
             
-            while l < r:
-                mid = (l+r) // 2
-
-                if (p - prefix[mid])*(i-mid+1) < k:
-                    r = mid
-                else:
-                    l = mid+1
-
-            ret += i-l+1
-
+            prev += 1
+            ret += prev
+        
         return ret
-                    
-
