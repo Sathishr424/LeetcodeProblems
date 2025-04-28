9136a3e11401eff135d1cbac54b390e5d63965e4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-28 16:49:31 +0530

    File updated at 28/4/2025, 4:49:26 pm

--- 2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.py
+++ 2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.py
@@ -1,4 +1,4 @@
-# Last updated: 28/4/2025, 4:46:53 pm
+# Last updated: 28/4/2025, 4:49:26 pm
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         """
@@ -8,7 +8,7 @@ class Solution:
         left = 0
         cnt = 0
         s = 0
-        prev = 0
+        
         for num in nums:
             cnt += 1
             s += num
@@ -17,9 +17,7 @@ class Solution:
                 cnt -= 1
                 s -= nums[left]
                 left += 1
-                prev -= 1
-            
-            prev += 1
-            ret += prev
+
+            ret += cnt
         
         return ret
