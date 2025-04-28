4acfd29b2338107c77b4adfd53f14446dd0f84a9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-28 18:55:36 +0530

    File updated at 28/4/2025, 6:55:31 pm

--- 1483-KthAncestorofaTreeNode/1483-KthAncestorofaTreeNode.py
+++ 1483-KthAncestorofaTreeNode/1483-KthAncestorofaTreeNode.py
@@ -1,19 +1,14 @@
-# Last updated: 28/4/2025, 6:53:38 pm
+# Last updated: 28/4/2025, 6:55:31 pm
 class TreeAncestor:
     def __init__(self, n: int, parent: List[int]):
-        i = 1
-        m = 0
-        while i <= n:
-            i *= 2
-            m += 1
-        
+        m = len(bin(n)) - 2
         self.logs = [[-1] * n for _ in range(m)]
 
         for node in range(1, n):
             self.logs[0][node] = parent[node]
 
         for k in range(1, m):
-            for node in range(n):
+            for node in range(1, n):
                 if self.logs[k-1][node] == -1: continue
                 self.logs[k][node] = self.logs[k-1][self.logs[k-1][node]]
 
@@ -22,7 +17,7 @@ class TreeAncestor:
         while k:
             if k % 2:
                 node = self.logs[p][node]
-                if node == -1: break
+                if node == -1: return -1
             k //= 2
             p += 1
         
