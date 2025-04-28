28df829d6a3d5d780ab025197241d0b8b924714a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-28 18:53:44 +0530

    File updated at 28/4/2025, 6:53:38 pm

--- 1483-KthAncestorofaTreeNode/1483-KthAncestorofaTreeNode.py
+++ 1483-KthAncestorofaTreeNode/1483-KthAncestorofaTreeNode.py
@@ -1,38 +1,31 @@
-# Last updated: 28/4/2025, 6:52:36 pm
+# Last updated: 28/4/2025, 6:53:38 pm
 class TreeAncestor:
-
     def __init__(self, n: int, parent: List[int]):
-        self.n = n
-        self.logs = []
-
         i = 1
         m = 0
         while i <= n:
-            self.logs.append(defaultdict(lambda: -1))
             i *= 2
             m += 1
+        
+        self.logs = [[-1] * n for _ in range(m)]
 
         for node in range(1, n):
             self.logs[0][node] = parent[node]
 
         for k in range(1, m):
-            for node in range(1, n):
+            for node in range(n):
+                if self.logs[k-1][node] == -1: continue
                 self.logs[k][node] = self.logs[k-1][self.logs[k-1][node]]
-        
-        # print([dict(l) for l in self.logs])
 
     def getKthAncestor(self, node: int, k: int) -> int:
-        if node == 0: return -1
-        tmp = node, k
-        arr = []
         p = 0
         while k:
             if k % 2:
-                arr.append((2 ** p, node, self.logs[p][node]))
                 node = self.logs[p][node]
+                if node == -1: break
             k //= 2
             p += 1
-        # print(tmp, arr)
+        
         return node
 
 
