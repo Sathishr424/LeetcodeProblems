070c152071304f12cd0723d1e9a1adcc18b2e8c3
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-04-29 13:48:33 +0530

    File updated at 29/4/2025, 1:48:26 pm

--- 3534-PathExistenceQueriesinaGraphII/3534-PathExistenceQueriesinaGraphII.py
+++ 3534-PathExistenceQueriesinaGraphII/3534-PathExistenceQueriesinaGraphII.py
@@ -1,4 +1,4 @@
-# Last updated: 29/4/2025, 1:47:51 pm
+# Last updated: 29/4/2025, 1:48:26 pm
 m = 19
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[int]:
@@ -17,6 +17,7 @@ class Solution:
         furthest = [0] * n
         for i in range(n):
             furthest[i] = bisect_right(arr, (arr[i][0]+maxDiff, float('inf'))) - 1
+            if furthest[i] == i: furthest[i] = -1
 
         logs = [[-1] * n for _ in range(m)]
 
@@ -25,6 +26,7 @@ class Solution:
 
         for i in range(1, m):
             for j in range(n):
+                if logs[i-1][j] == -1: continue
                 logs[i][j] = logs[i-1][logs[i-1][j]]
 
         for x, y in queries:
@@ -42,15 +44,11 @@ class Solution:
             p = 0
             node = x
             
-            while True:
-                if logs[p][node] >= y or logs[p][node] == node:
-                    if p == 0: break
-                    p -= 1
-                else:
-                    node = logs[p][node]
-                    cnt += 2 ** p
-                    p += 1
-            
+            for i in range(m-1, -1, -1):
+                if logs[i][node] < y and logs[i][node] != -1:
+                    node = logs[i][node]
+                    cnt += 1 << i
+                    
             ret.append(cnt+1 if logs[p][node] >= y else -1)
 
         return ret
