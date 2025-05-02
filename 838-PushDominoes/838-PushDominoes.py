81b7a3302fe92301709d9ffd50719192fae46aab
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-02 16:39:55 +0530

    File updated at 2/5/2025, 4:39:52 pm

--- 838-PushDominoes/838-PushDominoes.py
+++ 838-PushDominoes/838-PushDominoes.py
@@ -1,38 +1,37 @@
-# Last updated: 2/5/2025, 4:37:48 pm
-inf = 10**5 + 1
-cmin = lambda x, y: x if x < y else y
+# Last updated: 2/5/2025, 4:39:52 pm
 class Solution:
     def pushDominoes(self, dom: str) -> str:
         n = len(dom)
         dom = list(dom)
-
-        dp = [inf] * n
-        f = inf
-
-        for i in range(n):
-            if dom[i] == 'R':
-                f = 1
-            elif dom[i] == 'L':
-                f = inf
-            else:
-                f = cmin(inf, f + 1)
+        
+        def simulate(l, r):
+            leftForce = 0
+            rightForce = 0
+            if l-1 >= 0:
+                leftForce = -1 if dom[l-1] == 'L' else 1
+            if r+1 < n:
+                rightForce = -1 if dom[r+1] == 'L' else 1
             
-            dp[i] = f
+            if leftForce <= 0 and rightForce >= 0: return
+            elif leftForce > 0 and rightForce < 0:
+                while l < r:
+                    dom[l] = 'R'
+                    dom[r] = 'L'
+                    l += 1
+                    r -= 1
+            else:
+                for i in range(l, r+1):
+                    dom[i] = 'L' if leftForce + rightForce < 0 else 'R'
+
+        start = -1
+        for i in range(n):
+            if dom[i] == '.':
+                if start == -1: start = i
+            else:
+                if start != -1:
+                    simulate(start, i-1)
+                    start = -1
         
-        f = inf
-        
-        for i in range(n-1, -1, -1):
-            if dom[i] == 'L': 
-                f = 1
-            elif dom[i] == 'R':
-                f = inf
-            else: 
-                f = cmin(inf, f + 1)
-                
-                if f < dp[i]:
-                    dom[i] = 'L'
-                elif f > dp[i]:
-                    dom[i] = 'R'
-        
+        if start != -1: simulate(start, n-1)
+
         return ''.join(dom)
-        
