18b36b6c7e3487ddc391ec1d346e84f2e5ad25f5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-02 17:06:03 +0530

    File updated at 2/5/2025, 5:06:00 pm

--- 2071-MaximumNumberofTasksYouCanAssign/2071-MaximumNumberofTasksYouCanAssign.py
+++ 2071-MaximumNumberofTasksYouCanAssign/2071-MaximumNumberofTasksYouCanAssign.py
@@ -1,30 +1,31 @@
-# Last updated: 2/5/2025, 4:45:22 pm
+# Last updated: 2/5/2025, 5:06:00 pm
 class Solution:
     def maxTaskAssign(self, tasks: List[int], workers: List[int], pills: int, strength: int) -> int:
         n = len(tasks)
         m = len(workers)
 
         tasks.sort()
-        workers.sort(reverse=True)
+        workers.sort()
 
         left = 0
         right = n
 
         def isGood(mid):
             p = pills
-            sl = SortedList(workers[:mid+1])
+            sl = workers[-mid-1:]
 
             for i in range(mid+1):
                 if not sl: return False
                 task = tasks[mid-i]
                 if sl[-1] < task:
                     if p == 0: return False
-                    index = sl.bisect_left(task-strength)
+                    a = task-strength
+                    index = bisect_left(sl, a)
                     if index == len(sl): return False
+                    del sl[index]
                     p -= 1
-                    sl.remove(sl[index])
                 else:
-                    sl.remove(sl[-1])
+                    sl.pop()
             
             return True
 
