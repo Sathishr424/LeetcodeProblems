68170c7b44fdd9805bc83a199e356ecd124ff671
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-03 11:33:02 +0530

    File updated at 3/5/2025, 11:32:58 am

--- 1007-MinimumDominoRotationsForEqualRow/1007-MinimumDominoRotationsForEqualRow.py
+++ 1007-MinimumDominoRotationsForEqualRow/1007-MinimumDominoRotationsForEqualRow.py
@@ -1,4 +1,4 @@
-# Last updated: 3/5/2025, 11:30:12 am
+# Last updated: 3/5/2025, 11:32:58 am
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
         n = len(tops)
@@ -16,35 +16,19 @@ class Solution:
             bottom_freq[num] += 1
             if bottom_freq[num] > bottom_freq[max_bottom_freq]:
                 max_bottom_freq = num
-        
-        # print(max_top_freq, max_bottom_freq)
 
-        match = True
-        rotation = 0
-        for i in range(n):
-            if tops[i] != max_top_freq:
-                if bottoms[i] != max_top_freq:
-                    match = False
-                    break
-                rotation += 1
-        # print(match, rotation)
-        ret = rotation if match else -1
+        def checkRotation(x, y, z):
+            rotation = 0
+            for i in range(n):
+                if x[i] != z:
+                    if y[i] != z: return float('inf')
+                    rotation += 1
 
-        rotation = 0
-        match = True
-        for i in range(n):
-            if bottoms[i] != max_bottom_freq:
-                if tops[i] != max_bottom_freq:
-                    match = False
-                    break
-                rotation += 1
-        
-        # print(match, rotation)
-        if match:
-            if ret == -1: ret = rotation
-            else: ret = min(rotation, ret)
+            return rotation
 
-        return ret
+        ret = min(checkRotation(tops, bottoms, max_top_freq), checkRotation(bottoms, tops, max_bottom_freq))
+
+        return ret if ret != float('inf') else -1
         
         
 
