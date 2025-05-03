104473501aa44756210ff7c2e6ef1af2f49f74f5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-03 17:21:43 +0530

    File updated at 3/5/2025, 5:21:39 pm

--- 1986-MinimumNumberofWorkSessionstoFinishtheTasks/1986-MinimumNumberofWorkSessionstoFinishtheTasks.py
+++ 1986-MinimumNumberofWorkSessionstoFinishtheTasks/1986-MinimumNumberofWorkSessionstoFinishtheTasks.py
@@ -1,4 +1,4 @@
-# Last updated: 3/5/2025, 5:20:39 pm
+# Last updated: 3/5/2025, 5:21:39 pm
 cmin = lambda x, y: x if x < y else y
 inf = 15
 class Solution:
@@ -7,11 +7,9 @@ class Solution:
         full_mask = (1 << (n+1)) - 1
         start_mask = 1 << n
 
-        diff = full_mask - start_mask
+        dp = [[inf] * (sTime+1) for _ in range(full_mask+1)]
 
-        dp = [[inf] * (sTime+1) for _ in range(diff+1)]
-
-        dp[0][0] = 0
+        dp[start_mask][0] = 0
     
         for mask in range(start_mask, full_mask):
             for time in range(sTime+1):
@@ -19,9 +17,9 @@ class Solution:
                     if mask & (1 << i) == 0:
                         new_mask = mask | (1 << i)
                         if time >= tasks[i]:
-                            dp[new_mask-start_mask][time-tasks[i]] = cmin(dp[new_mask-start_mask][time-tasks[i]], dp[mask-start_mask][time])
+                            dp[new_mask][time-tasks[i]] = cmin(dp[new_mask][time-tasks[i]], dp[mask][time])
                         else:
-                            dp[new_mask-start_mask][sTime-tasks[i]] = cmin(dp[new_mask-start_mask][sTime-tasks[i]], dp[mask-start_mask][time] + 1)
+                            dp[new_mask][sTime-tasks[i]] = cmin(dp[new_mask][sTime-tasks[i]], dp[mask][time] + 1)
         
-        return min(dp[full_mask-start_mask])
+        return min(dp[full_mask])
 
