73d48ecb49074202fd11978e6a4d973727af0606
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-04 09:42:50 +0530

    File updated at 4/5/2025, 9:42:45 am

--- 3537-FillaSpecialGrid/3537-FillaSpecialGrid.py
+++ 3537-FillaSpecialGrid/3537-FillaSpecialGrid.py
@@ -1,4 +1,4 @@
-# Last updated: 4/5/2025, 9:35:31 am
+# Last updated: 4/5/2025, 9:42:45 am
 class Solution:
     def specialGrid(self, N: int) -> List[List[int]]:
         if N == 0: return [[0]]
@@ -6,38 +6,21 @@ class Solution:
 
         ret = [[0] * m for _ in range(m)]
 
-        def fill(rowStart, rows, colStart, cols, val):
-            j = 0
-            while j < cols:
-                if j % 2 == 0:
-                    i = 0
-                    while i < rows:
-                        ret[rowStart+i][colStart+j] = val
-                        val -= 1
-                        i += 1
-                else:
-                    i = rows-1
-                    while i >= 0:
-                        ret[rowStart+i][colStart+j] = val
-                        val -= 1
-                        i -= 1
-                j += 1
-            return val
-
         def processFill(i, j, rows, val):
-            if rows == 2:
-                return fill(i, rows, j, rows, val)
-            else:
-                half = rows//2
-                for k in range(0, rows, rows//2):
-                    val = processFill(i, j, half, val)
-                    i += half
-                j += k
+            if rows == 1:
+                ret[i][j] = val
+                return ret[i][j]-1
+            
+            half = rows//2
+            for k in range(0, rows, rows//2):
+                val = processFill(i, j, half, val)
+                i += half
+            j += k
+            i -= half
+            for k in range(0, rows, rows//2):
+                val = processFill(i, j, half, val)
                 i -= half
-                for k in range(0, rows, rows//2):
-                    val = processFill(i, j, half, val)
-                    i -= half
-                return val
+            return val
                     
         processFill(0, 0, m, 2**(2*N) - 1)
         return ret
