d8ce9867baf3b8734c9a8f1156cd94927949bb11
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-04 09:47:06 +0530

    File updated at 4/5/2025, 9:47:01 am

--- 3537-FillaSpecialGrid/3537-FillaSpecialGrid.py
+++ 3537-FillaSpecialGrid/3537-FillaSpecialGrid.py
@@ -1,9 +1,7 @@
-# Last updated: 4/5/2025, 9:42:45 am
+# Last updated: 4/5/2025, 9:47:01 am
 class Solution:
     def specialGrid(self, N: int) -> List[List[int]]:
-        if N == 0: return [[0]]
-        m = 2**N
-
+        m = 1 << N
         ret = [[0] * m for _ in range(m)]
 
         def processFill(i, j, rows, val):
@@ -12,17 +10,17 @@ class Solution:
                 return ret[i][j]-1
             
             half = rows//2
-            for k in range(0, rows, rows//2):
+            for _ in range(2):
                 val = processFill(i, j, half, val)
                 i += half
-            j += k
+            j += half
             i -= half
-            for k in range(0, rows, rows//2):
+            for _ in range(2):
                 val = processFill(i, j, half, val)
                 i -= half
             return val
-                    
-        processFill(0, 0, m, 2**(2*N) - 1)
+        
+        processFill(0, 0, m, (1 << (2*N)) - 1)
         return ret
                 
 
