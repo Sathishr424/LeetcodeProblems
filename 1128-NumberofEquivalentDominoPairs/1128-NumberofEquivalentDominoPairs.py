5bdc69f1fd8356c2adc550e11911f3a7813f3251
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-04 09:31:04 +0530

    File updated at 4/5/2025, 9:30:59 am

--- 1128-NumberofEquivalentDominoPairs/1128-NumberofEquivalentDominoPairs.py
+++ 1128-NumberofEquivalentDominoPairs/1128-NumberofEquivalentDominoPairs.py
@@ -1,15 +1,14 @@
-# Last updated: 4/5/2025, 9:30:10 am
+# Last updated: 4/5/2025, 9:30:59 am
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        pairs = defaultdict(int)
+        pairs = [[0] * 10 for _ in range(10)]
         ret = 0
         for x, y in dominoes:
             if x > y:
                 x, y = y, x
             
-            key = f"{x},{y}"
-            ret += pairs[key]
-            pairs[key] += 1
+            ret += pairs[x][y]
+            pairs[x][y] += 1
         
         return ret
 
