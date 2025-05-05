71efe1711fe11b2515df88640f866dcb0d93c85d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-05 12:16:58 +0530

    File updated at 5/5/2025, 12:16:53 pm

--- 790-DominoandTrominoTiling/790-DominoandTrominoTiling.py
+++ 790-DominoandTrominoTiling/790-DominoandTrominoTiling.py
@@ -1,21 +1,21 @@
-# Last updated: 5/5/2025, 12:15:38 pm
+# Last updated: 5/5/2025, 12:16:53 pm
 mod = 10**9 + 7
 # [2, 1, 3, 3, 4, 4], (6, 8, 10, 12, 14, 18, ....) (vertival and horizontal)
 # (5, 7, 9, 11, ....) (vertival and horizontal)
+N = 1000
+
+dp = [0] * (N+1)
+dp[0] = 1
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, N+1):
+    dp[i] = dp[i-1]
+    dp[i] = (dp[i] + dp[i-2]) % mod
+    
+    for num in range(3, i+1):
+        dp[i] = (dp[i] + (dp[i-num] * 2 % mod)) % mod
 
 class Solution:
     def numTilings(self, n: int) -> int:
-        if n == 1: return 1
-        dp = [0] * (n+1)
-        dp[0] = 1
-        dp[1] = 1
-        dp[2] = 2
-
-        for i in range(3, n+1):
-            dp[i] = dp[i-1]
-            dp[i] = (dp[i] + dp[i-2]) % mod
-            
-            for num in range(3, i+1):
-                dp[i] = (dp[i] + (dp[i-num] * 2 % mod)) % mod
-
         return dp[n]
