1cfca0ee27d7c75a4d499d4bbc686fed4fa3c617
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-05 12:01:12 +0530

    Create file

---
+++ 790-DominoandTrominoTiling/790-DominoandTrominoTiling.py
@@ -0,0 +1,30 @@
+# Last updated: 5/5/2025, 12:01:06 pm
+mod = 10**9 + 7
+ways = [1, 2, 3, 3, 4, 4]
+
+# [2, 1, 3, 3, 4, 4], (6, 8, 10, 12, 14, 18, ....) (vertival and horizontal)
+# (5, 7, 9, 11, ....) (vertival and horizontal)
+
+class Solution:
+    def numTilings(self, N: int) -> int:
+        
+        @cache
+        def rec(n):
+            print(n)
+            if n <= 1: return 1
+            elif n == 2: return 2
+            
+            ret = 0
+            for num in ways:
+                if num > n: break
+                ret = (ret + rec(n-num)) % mod
+            
+            for num in range(5, n+1, 2):
+                ret = (ret + (rec(n-num) * 2 % mod)) % mod
+            
+            for num in range(6, n+1, 2):
+                ret = (ret + (rec(n-num) * 2 % mod)) % mod
+
+            return ret
+        
+        return rec(N)
