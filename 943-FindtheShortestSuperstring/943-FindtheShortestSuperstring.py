55c50b0987477acbceb7d371fc7cfe974aa82447
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-05 16:30:06 +0530

    File updated at 5/5/2025, 4:30:01 pm

--- 943-FindtheShortestSuperstring/943-FindtheShortestSuperstring.py
+++ 943-FindtheShortestSuperstring/943-FindtheShortestSuperstring.py
@@ -1,29 +1,4 @@
-# Last updated: 5/5/2025, 4:24:54 pm
-def getAlp(a):
-    return ord(a) - 96
-
-base = 27
-mod = 10**9 + 7
-
-base_powers = [1] * (20 + 1)
-for i in range(1, 20 + 1):
-    base_powers[i] = base_powers[i - 1] * base % mod
-
-def overlap_append_rolling_hash(st, s):
-    max_len = min(len(st), len(s))
-
-    match_ = 0
-    hash_st = 0
-    hash_s = 0
-
-    for i in range(1, max_len + 1):
-        hash_st = (getAlp(st[-i]) * base_powers[i - 1] + hash_st) % mod
-        hash_s = (hash_s * base + getAlp(s[i - 1])) % mod
-        if hash_st == hash_s:
-            match_ = i
-
-    return st + s[match_:]
-
+# Last updated: 5/5/2025, 4:30:01 pm
 def overlap_append(a: str, b: str) -> str:
     m = len(a)
     n = len(b)
@@ -39,14 +14,12 @@ def overlap_append(a: str, b: str) -> str:
         
     return a + b 
 
+        
+d = 'z' * 241
+
 class Solution:
     def shortestSuperstring(self, words: List[str]) -> str:
-        # words = test_words
         n = len(words)
-        
-        d = 'z'
-        for word in words:
-            d += 'z' * len(word)
         
         full_mask = (1 << (n+1)) - 1
         start_mask = 1 << n
