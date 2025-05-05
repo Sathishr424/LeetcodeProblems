c3aa0f73889b3203a4a272449c3a5c63f19d89cc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-05 17:44:36 +0530

    File updated at 5/5/2025, 5:44:31 pm

--- 943-FindtheShortestSuperstring/943-FindtheShortestSuperstring.py
+++ 943-FindtheShortestSuperstring/943-FindtheShortestSuperstring.py
@@ -1,4 +1,4 @@
-# Last updated: 5/5/2025, 4:48:17 pm
+# Last updated: 5/5/2025, 5:44:31 pm
 def overlap_append(a: str, b: str) -> str:
     m = len(a)
     n = len(b)
@@ -13,29 +13,43 @@ def overlap_append(a: str, b: str) -> str:
         if start == m: return a[:i] + b
         
     return a + b 
-
         
 d = 'z' * 241
 
 class Solution:
     def shortestSuperstring(self, words: List[str]) -> str:
         n = len(words)
-        memo = {}
+        start_mask = 1 << n
+        full_mask = (1 << (n+1)) - 1
 
-        def rec(mask, st):
-            if mask == 0: return st
+        dp = {}
+        
+        for i in range(n):
+            key = f'{start_mask | (1 << i)},{words[i]}'
+            dp[key] = words[i]
+        
+        for i in range(n-1):
+            new_dp = {}
+            for mask in dp:
+                st = dp[mask]
+                mask = int(mask.split(',')[0])
+
+                for k in range(n):
+                    if mask & (1 << k) == 0:
+                        
+                        new_mask = mask | (1 << k)
+                        new_merge = overlap_append(st, words[k])
+
+                        key = f'{new_mask},{words[k]}'
+                        if key not in new_dp or len(new_merge) < len(new_dp[key]):
+                            new_dp[key] = new_merge
             
-            key = f"{mask},{st}"
-            if key in memo: return memo[key]
-
-            ans = d
-            for i in range(n):
-                if (mask >> i) & 1:
-                    curr = rec(mask & ~(1 << i), words[i])
-                    merged = overlap_append(st, curr)
-                    if len(merged) < len(ans):
-                        ans = merged
-            memo[key] = ans
-            return ans
-
-        return rec((1 << n) - 1, '')
+            dp = new_dp
+            # print([[format(m, f'0{n+1}b'), s] for m, s in dp])
+        ret = d
+        for mask in dp:
+            if len(dp[mask]) < len(ret):
+                ret = dp[mask]
+        # print([[format(m, f'0{n+1}b'), s] for m, s in dp])
+        # print(ret)
+        return ret
