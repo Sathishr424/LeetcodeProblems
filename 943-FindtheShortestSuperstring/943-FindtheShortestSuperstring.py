95b0b30ea202666f859995b5ea61ca16e3f734d9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-05 16:48:23 +0530

    File updated at 5/5/2025, 4:48:17 pm

--- 943-FindtheShortestSuperstring/943-FindtheShortestSuperstring.py
+++ 943-FindtheShortestSuperstring/943-FindtheShortestSuperstring.py
@@ -1,4 +1,4 @@
-# Last updated: 5/5/2025, 4:30:01 pm
+# Last updated: 5/5/2025, 4:48:17 pm
 def overlap_append(a: str, b: str) -> str:
     m = len(a)
     n = len(b)
@@ -20,27 +20,22 @@ d = 'z' * 241
 class Solution:
     def shortestSuperstring(self, words: List[str]) -> str:
         n = len(words)
-        
-        full_mask = (1 << (n+1)) - 1
-        start_mask = 1 << n
-
         memo = {}
-        ret = d
 
         def rec(mask, st):
-            if mask == full_mask: return st
+            if mask == 0: return st
             
             key = f"{mask},{st}"
             if key in memo: return memo[key]
 
             ans = d
             for i in range(n):
-                if mask & (1 << i) == 0:
-                    curr = rec(mask | (1 << i), words[i])
+                if (mask >> i) & 1:
+                    curr = rec(mask & ~(1 << i), words[i])
                     merged = overlap_append(st, curr)
                     if len(merged) < len(ans):
                         ans = merged
             memo[key] = ans
             return ans
 
-        return rec(start_mask, '')
+        return rec((1 << n) - 1, '')
