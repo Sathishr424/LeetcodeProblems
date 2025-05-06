8d4c745ef3ed6a040a154ea502e552c758285640
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-06 14:27:48 +0530

    File updated at 6/5/2025, 2:27:47 pm

--- 3533-ConcatenatedDivisibility/3533-ConcatenatedDivisibility.py
+++ 3533-ConcatenatedDivisibility/3533-ConcatenatedDivisibility.py
@@ -1,4 +1,4 @@
-# Last updated: 6/5/2025, 2:20:53 pm
+# Last updated: 6/5/2025, 2:27:47 pm
 class Solution:
     def concatenatedDivisibility(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
@@ -13,11 +13,11 @@ class Solution:
 
         memo = {}
 
-        def rec(mask, num, rem, need):
+        def rec(mask, num, rem, need, whole_num, so_far):
             if mask == 0:
                 return num, []
             
-            key = (mask, num, need)
+            key = (mask, so_far)
             if key in memo: return memo[key]
 
             ans = float('inf')
@@ -27,7 +27,7 @@ class Solution:
                 if (mask >> i) & 1:
                     num2 = nums[i] * pow(10, rem - digits[i], k) % k
 
-                    new_num, arr = rec(mask & ~(1 << i), num2, rem - digits[i], (need + num) % k)
+                    new_num, arr = rec(mask & ~(1 << i), num2, rem - digits[i], (need + num) % k, whole_num+num2, (whole_num+num2) % k)
 
                     if (need + num + new_num) % k == 0:
                         ans = (num + new_num) % k
@@ -37,4 +37,4 @@ class Solution:
             memo[key] = [ans, ret_arr]
             return ans, ret_arr
         
-        return rec((1 << n)-1, 0, total, 0)[1]
+        return rec((1 << n) - 1, 0, total, 0, 0, 0)[1]
