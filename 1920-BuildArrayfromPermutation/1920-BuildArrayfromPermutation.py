19c427be9ba36bbadfde8dab0e43766949145ee5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-06 13:17:37 +0530

    File updated at 6/5/2025, 1:17:36 pm

--- 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.py
+++ 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.py
@@ -1,8 +1,12 @@
-# Last updated: 6/5/2025, 12:37:30 pm
+# Last updated: 6/5/2025, 1:17:36 pm
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        ans = []
-        for i in range(len(nums)):
-            ans.append(nums[nums[i]])
+        n = len(nums)
         
-        return ans
+        for i in range(n):
+            nums[i] = (nums[nums[i]] % 1000) * 1000 + nums[i]
+
+        for i in range(n):
+            nums[i] = nums[i] // 1000
+        
+        return nums
