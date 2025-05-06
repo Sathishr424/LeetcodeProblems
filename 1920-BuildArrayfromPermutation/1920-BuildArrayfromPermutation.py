b9ccd576012deaa47e250802e3741cde84e88458
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-06 12:37:31 +0530

    Create file

---
+++ 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.py
@@ -0,0 +1,8 @@
+# Last updated: 6/5/2025, 12:37:30 pm
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        
+        return ans
