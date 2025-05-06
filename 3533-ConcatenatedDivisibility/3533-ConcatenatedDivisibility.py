5811bfd135e240d16e0bfeea24249f21477943e4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-06 19:19:13 +0530

    File updated at 6/5/2025, 7:19:12 pm

--- 3533-ConcatenatedDivisibility/3533-ConcatenatedDivisibility.py
+++ 3533-ConcatenatedDivisibility/3533-ConcatenatedDivisibility.py
@@ -1,54 +1,49 @@
-# Last updated: 6/5/2025, 7:15:59 pm
-def compare(x, y):
-    for i in range(len(x)):
-        if x[i] < y[i]: return True
-        elif y[i] < x[i]: return False
-    
-    return False
-
+# Last updated: 6/5/2025, 7:19:12 pm
 class Solution:
     def concatenatedDivisibility(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
         nums.sort()
-        digits = [0] * n
 
-        for i in range(n):
-            digits[i] = len(str(nums[i]))
-        
-        total = sum(digits)
+        digits = []
+        total = 0
+        for i, num in enumerate(nums):
+            digits.append(len(str(num)))
+            total += digits[-1]
 
         pows = [0] * total
         for i in range(total):
             pows[i] = pow(10, i, k)
-        
+
         start_mask = (1 << n) - 1
 
-        dp = [[[] for _ in range(k)] for _ in range(start_mask+1)]
+        dp = [[None for _ in range(k)] for _ in range(start_mask+1)]
 
         for i in range(n):
             new_mask = start_mask & ~(1 << i)
             num = nums[i] * pows[total - digits[i]] % k
 
-            dp[new_mask][num % k] = [nums[i], (total - digits[i])]
+            dp[new_mask][num % k] = ([nums[i]], total - digits[i])
+
+        def compare(x, y):
+            return x < y
 
         for mask in range(start_mask, 0, -1):
-            for rem in range(k):
+            for whole_num in range(k):
+                if dp[mask][whole_num] == None: continue
                 
-                if not dp[mask][rem]: continue
-                arr = dp[mask][rem][:-1]
-                d = dp[mask][rem][-1]
-
+                arr, rem = dp[mask][whole_num]
                 for i in range(n):
                     if (mask >> i) & 1 == 0: continue
 
                     new_mask = mask & ~(1 << i)
+                    
+                    l = digits[i]
+                    r = ((nums[i] * pows[rem - l] % k) + whole_num) % k
 
-                    r = ((nums[i] * pows[d - digits[i]] % k) + rem) % k
+                    if dp[new_mask][r] == None or compare(arr, dp[new_mask][r][0]):
+                        dp[new_mask][r] = (arr + [nums[i]], rem - l)
 
-                    if not dp[new_mask][r] or compare(arr, dp[new_mask][r]):
-                        dp[new_mask][r] = arr + [nums[i], d-digits[i]]
-
-        return dp[0][0][:-1] if dp[0][0] else []
+        return dp[0][0][0] if dp[0][0] != None else []
 
 
 
