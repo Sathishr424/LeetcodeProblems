99fe802c7d85b9c56ec5fad2380446ea61f50461
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-06 17:58:34 +0530

    File updated at 6/5/2025, 5:58:33 pm

--- 3533-ConcatenatedDivisibility/3533-ConcatenatedDivisibility.py
+++ 3533-ConcatenatedDivisibility/3533-ConcatenatedDivisibility.py
@@ -1,4 +1,4 @@
-# Last updated: 6/5/2025, 4:53:51 pm
+# Last updated: 6/5/2025, 5:58:33 pm
 class Solution:
     def concatenatedDivisibility(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
@@ -17,26 +17,51 @@ class Solution:
         for i in range(total):
             pows[i] = pow(10, i, k)
 
-        def rec(mask, num, rem, whole_num):
-            if mask == 0: return num, []
+        start_mask = (1 << n) - 1
+
+        dp = [[[[], total] for _ in range(k)] for _ in range(start_mask+1)]
+
+        for i in range(n):
+            new_mask = start_mask & ~(1 << i)
+            num = nums[i] * pows[total - digits[i]] % k
+
+            dp[new_mask][num % k] = [[nums[i]], total - digits[i]]
+
+        def compare(x, y):
+            for i in range(len(x)):
+                if x[i] < y[i]: return True
+                elif y[i] < x[i]: return False
             
-            key = (mask, whole_num)
-            if key in memo: return memo[key]
+            return False
 
-            ans = float('inf')
-            ret_arr = []
+        for mask in range(start_mask, 0, -1):
+            for whole_num in range(k):
+                arr, rem = dp[mask][whole_num]
+                if rem == total: continue
+                for i in range(n):
+                    if (mask >> i) & 1 == 0: continue
 
-            for i in range(n):
-                if (mask >> i) & 1:
-                    new_num = nums[i] * pows[rem - digits[i]] % k
-                    new_num, arr = rec(mask & ~(1 << i), new_num, rem - digits[i], (whole_num + new_num) % k)
+                    new_mask = mask & ~(1 << i)
+                    l = digits[i]
 
-                    if (whole_num + new_num) % k == 0:
-                        ans = (num + new_num) % k
-                        ret_arr = [nums[i]] + arr
-                        break
-            
-            memo[key] = [ans, ret_arr]
-            return ans, ret_arr
-        
-        return rec((1 << n) - 1, 0, total, 0)[1]
+                    num = nums[i] * pows[rem - l] % k
+                    r = (num + whole_num) % k
+
+                    if dp[new_mask][r][1] == total:
+                        dp[new_mask][r] = [arr + [nums[i]], rem - l]
+                    else:
+                        new_arr = arr + [nums[i]]
+                        old_arr = dp[new_mask][r][0]
+                        
+                        # print(new_arr, old_arr)
+
+                        if compare(new_arr, old_arr):
+                            dp[new_mask][r] = [new_arr, rem - l]
+
+        # print(dp[0])
+        return dp[0][0][0]
+
+
+
+
+                        
