41d0611081837ed6524063213241bddc5e982589
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-08 03:09:49 +0530

    Create file

---
+++ 2730-FindtheLongestSemi-RepetitiveSubstring/2730-FindtheLongestSemi-RepetitiveSubstring.py
@@ -0,0 +1,15 @@
+# Last updated: 8/5/2025, 3:09:46 am
+class Solution:
+    def longestSemiRepetitiveSubstring(self, s: str) -> int:
+        n = len(s)
+
+        start = 0
+        new_start = 0
+        ret = 1
+        for i in range(1, n):
+            if s[i] == s[i-1]:
+                start = new_start
+                new_start = i
+            ret = max(ret, i-start+1)
+
+        return ret
