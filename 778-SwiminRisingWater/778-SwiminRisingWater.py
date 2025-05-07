989ce2f92f04b5bc07c93138c9dc72b4ed724e17
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-07 12:51:38 +0530

    File updated at 7/5/2025, 12:51:37 pm

--- 778-SwiminRisingWater/778-SwiminRisingWater.py
+++ 778-SwiminRisingWater/778-SwiminRisingWater.py
@@ -1,5 +1,28 @@
-# Last updated: 7/5/2025, 12:45:16 pm
-DIR = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+# Last updated: 7/5/2025, 12:51:37 pm
+DIR = [(0, 1), (1, 0)]
+
+class Union:
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
+        self.sizes = [1] * n
+    
+    def find(self, x):
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+    
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y: return True
+
+        if self.sizes[y] > self.sizes[x]:
+            x, y = y, x
+
+        self.sizes[x] += self.sizes[y]
+        self.parents[y] = self.parents[x]
+        return False
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
@@ -12,25 +35,24 @@ class Solution:
 
         l = 0
         r = maxi
+        finalPos = n*n
 
         def isGood(mid):
             if grid[0][0] > mid: return False
-            stack = [(0, 0)]
-            visited = {(0,0): 1}
+            uj = Union(finalPos)
+            
+            for i in range(n):
+                for j in range(n):
+                    if grid[i][j] <= mid:
+                        pos = i*n + j
+                        for ni, nj in DIR:
+                            ni += i
+                            nj += j
 
-            while stack:
-                i, j = stack.pop()
-                if i == n-1 and j == n-1: return True
-
-                for ni, nj in DIR:
-                    ni += i
-                    nj += j
-
-                    if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] <= mid and (ni, nj) not in visited:
-                        visited[(ni, nj)] = 1
-                        stack.append((ni, nj))
-
-            return False
+                            if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] <= mid:
+                                uj.union(pos, ni*n + nj)
+            
+            return uj.find(0) == uj.find(finalPos-1)
 
         while l < r:
             mid = (l+r) // 2
