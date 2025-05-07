19884a5b13f0c951c52fc8e8261c85c038dab27a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-07 12:42:47 +0530

    File updated at 7/5/2025, 12:42:45 pm

--- 778-SwiminRisingWater/778-SwiminRisingWater.py
+++ 778-SwiminRisingWater/778-SwiminRisingWater.py
@@ -1,23 +1,43 @@
-# Last updated: 7/5/2025, 11:56:48 am
+# Last updated: 7/5/2025, 12:42:45 pm
 DIR = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
 
-        stack = [(grid[0][0], 0, 0)]
+        maxi = 0
+        for i in range(n):
+            for j in range(n):
+                maxi = max(maxi, grid[i][j])
+
+        l = 0
+        r = maxi
+
+        def isGood(mid):
+            if grid[0][0] > mid: return False
+            stack = [(0, 0)]
+            visited = {(0,0): 1}
+
+            while stack:
+                i, j = stack.pop()
+                if i == n-1 and j == n-1: return True
+
+                for ni, nj in DIR:
+                    ni += i
+                    nj += j
+
+                    if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] <= mid and (ni, nj) not in visited:
+                        visited[(ni, nj)] = 1
+                        stack.append((ni, nj))
+
+            return False
+
+        while l < r:
+            mid = (l+r) // 2
+
+            if isGood(mid):
+                r = mid
+            else:
+                l = mid + 1
         
-        while stack:
-            depth, i, j = heapq.heappop(stack)
-            if i == n-1 and j == n-1: return depth
-
-            for ni, nj in DIR:
-                ni += i
-                nj += j
-
-                if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] != -1:
-                    heapq.heappush(stack, (max(depth, grid[ni][nj]), ni, nj))
-                    grid[ni][nj] = -1
-        
-        return grid[-1][-1]
-
+        return l
