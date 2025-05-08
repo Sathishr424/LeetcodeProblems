9907dd3efb886dd5f7c71830f8182c5ee770b502
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-08 11:08:29 +0530

    Create file

---
+++ 1631-PathWithMinimumEffort/1631-PathWithMinimumEffort.py
@@ -0,0 +1,31 @@
+# Last updated: 8/5/2025, 11:08:25 am
+DIR = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m = len(heights)
+        n = len(heights[0])
+
+        dist = [[float('inf')] * n for _ in range(m)]
+        dist[0][0] = 0
+
+        stack = [(0, 0, 0)]
+    
+        while stack:
+            diff, i, j = heapq.heappop(stack)
+
+            if dist[i][j] < diff: continue
+
+            h = heights[i][j]
+
+            for ni, nj in DIR:
+                ni += i
+                nj += j
+
+                if 0 <= ni < m and 0 <= nj < n:
+                    currDiff = max(diff, abs(h-heights[ni][nj]))
+                    if currDiff < dist[ni][nj]:
+                        dist[ni][nj] = currDiff
+                        heapq.heappush(stack, (currDiff, ni, nj))
+        
+        return dist[-1][-1]
