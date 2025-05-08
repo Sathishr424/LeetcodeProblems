71bc64c53ac2249b636f22879e617a07afaebd62
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-09 02:17:39 +0530

    File updated at 9/5/2025, 2:17:35 am

--- 2732-FindaGoodSubsetoftheMatrix/2732-FindaGoodSubsetoftheMatrix.py
+++ 2732-FindaGoodSubsetoftheMatrix/2732-FindaGoodSubsetoftheMatrix.py
@@ -1,64 +1,25 @@
-# Last updated: 9/5/2025, 2:05:34 am
-class Node:
-    def __init__(self):
-        self.one = None
-        self.zero = None
-        self.index = -1
-
-class Trie:
-    def __init__(self):
-        self.node = Node()
-    
-    def check(self, num, node):
-        if node == None: return -1
-
-        while num != 1:
-            if num & 1:
-                num >>= 1
-                if not node.zero: return -1
-                node = node.zero
-            else:
-                num >>= 1
-                index = self.check(num, node.one)
-                if index != -1: return index
-
-                return self.check(num, node.zero)
-        
-        return node.index
-
-    def insert(self, num, index):
-        node = self.node
-        while num != 1:
-            if num & 1:
-                if node.one == None:
-                    node.one = Node()
-                node = node.one
-            else:
-                if node.zero == None:
-                    node.zero = Node()
-                node = node.zero
-            num >>= 1
-        node.index = index
-
+# Last updated: 9/5/2025, 2:17:35 am
 class Solution:
     def goodSubsetofBinaryMatrix(self, grid: List[List[int]]) -> List[int]:
         m = len(grid)
         n = len(grid[0])
 
-        trie = Trie()
-        start_mask = 1 << n
+        seen = {}
 
         for i in range(m):
-            mask = start_mask
-
+            mask = 0
             for j in range(n):
-                mask += grid[i][j] << (n-j-1)
-            
-            if mask == start_mask: return [i]
+                mask += grid[i][j] << (n - j - 1)
 
-            index = trie.check(mask, trie.node)
-            if index != -1: return [index, i]
+            # If the bitmask is zero, we immediately have a valid subset
+            if mask == 0: 
+                return [i]
 
-            trie.insert(mask, i)
+            for prev_mask, prev_index in seen.items():
+                if prev_mask & mask == 0:
+                    return [prev_index, i]
+
+            seen[mask] = i
         
         return []
+        
