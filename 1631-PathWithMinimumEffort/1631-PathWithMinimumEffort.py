69c11e2f6b9e829cdc811d902e934a54e796f8de
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-08 11:15:35 +0530

    File updated at 8/5/2025, 11:15:31 am

--- 1631-PathWithMinimumEffort/1631-PathWithMinimumEffort.py
+++ 1631-PathWithMinimumEffort/1631-PathWithMinimumEffort.py
@@ -1,4 +1,4 @@
-# Last updated: 8/5/2025, 11:08:54 am
+# Last updated: 8/5/2025, 11:15:31 am
 DIR = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 class Solution:
@@ -6,26 +6,41 @@ class Solution:
         m = len(heights)
         n = len(heights[0])
 
-        dist = [[float('inf')] * n for _ in range(m)]
-        dist[0][0] = 0
+        mini = heights[0][0]
+        maxi = heights[0][0]
 
-        stack = [(0, 0, 0)]
-    
-        while stack:
-            diff, i, j = heapq.heappop(stack)
-
-            if dist[i][j] < diff: continue
-
-            h = heights[i][j]
-
-            for ni, nj in DIR:
-                ni += i
-                nj += j
-
-                if 0 <= ni < m and 0 <= nj < n:
-                    currDiff = max(diff, abs(h-heights[ni][nj]))
-                    if currDiff < dist[ni][nj]:
-                        dist[ni][nj] = currDiff
-                        heapq.heappush(stack, (currDiff, ni, nj))
+        for i in range(m):
+            for j in range(n):
+                mini = min(heights[i][j], mini)
+                maxi = max(heights[i][j], maxi)
         
-        return dist[-1][-1]
+        def isGood(diff):
+            q = [(0, 0)]
+            visited = [[False] * n for _ in range(m)]
+
+            while q:
+                i, j = q.pop()
+                if i == m-1 and j == n-1: return True
+
+                for ni, nj in DIR:
+                    ni += i
+                    nj += j
+                    
+                    if 0 <= ni < m and 0 <= nj < n and not visited[ni][nj] and abs(heights[i][j] - heights[ni][nj]) <= diff:
+                        visited[ni][nj] = True
+                        q.append((ni, nj))
+            
+            return False
+        
+        l = 0
+        r = maxi-mini
+
+        while l < r:
+            mid = (l+r) // 2
+
+            if isGood(mid):
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
