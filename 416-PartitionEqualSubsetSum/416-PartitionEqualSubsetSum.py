bc6038eb9ea0efadd938cbe51429940492f08834
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-09 20:20:43 +0530

    File updated at 9/5/2025, 8:20:39 pm

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
@@ -1,4 +1,4 @@
-# Last updated: 9/5/2025, 8:20:11 pm
+# Last updated: 9/5/2025, 8:20:39 pm
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
@@ -10,7 +10,7 @@ class Solution:
         @cache
         def rec(index, tot):
             if index == n: return False
-            if tot == half: return True
+            if tot >= half: return tot == half
 
             return rec(index+1, tot+nums[index]) or rec(index+1, tot)
         
