3559fd14dd552ad32d1199aafbbd2b5d73b3b9c2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-09 18:37:06 +0530

    File updated at 9/5/2025, 6:37:02 pm

--- 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
+++ 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
@@ -1,11 +1,10 @@
-# Last updated: 9/5/2025, 6:35:43 pm
+# Last updated: 9/5/2025, 6:37:02 pm
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = [0] * (amount+1)
         dp[0] = 1
-        coins.sort()
+
         for coin in coins:
-            for i in range(amount):
-                if coin+i > amount: break
+            for i in range(amount-coin+1):
                 dp[coin+i] += dp[i]
         return dp[amount]
