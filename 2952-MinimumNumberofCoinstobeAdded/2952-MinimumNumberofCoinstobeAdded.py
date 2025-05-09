fe58a1bd0d9c72ff8275eb667ecf407474f26c7b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-09 15:34:04 +0530

    File updated at 9/5/2025, 3:33:59 pm

--- 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
+++ 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
@@ -1,15 +1,16 @@
-# Last updated: 9/5/2025, 3:32:52 pm
+# Last updated: 9/5/2025, 3:33:59 pm
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def lengthOfLongestSubsequence(self, nums: List[int], target: int) -> int:
         n = len(nums)
         nums.sort()
 
-        dp = [-float('inf') for _ in range(target+1)]
+        dp = [-1 for _ in range(target+1)]
         dp[0] = 0
 
         for i, num in enumerate(nums):
             for t in range(target-num, -1, -1):
-                dp[t+num] = cmax(dp[t+num], dp[t] + 1)
+                if dp[t] >= 0:
+                    dp[t+num] = cmax(dp[t+num], dp[t] + 1)
         
-        return cmax(-1, dp[target])
+        return dp[target]
