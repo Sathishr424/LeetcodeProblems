64111ccd4cb8a5e783d8de1c54f2d26c0a25349a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-09 15:18:59 +0530

    File updated at 9/5/2025, 3:18:55 pm

--- 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
+++ 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
@@ -1,18 +1,20 @@
-# Last updated: 9/5/2025, 3:08:18 pm
+# Last updated: 9/5/2025, 3:18:55 pm
+cmax = lambda x, y: x if x > y else y
 class Solution:
     def lengthOfLongestSubsequence(self, nums: List[int], target: int) -> int:
         n = len(nums)
+        nums.sort()
+        dp = [[-float('inf')] * (target+1) for _ in range(n+1)]
+        dp[0][0] = 0
 
-        dp = [-float('inf') for _ in range(target+1)]
-        dp[0] = 0
-
-        # [0, 1, 1, 2, 1, 1, 2, 2]
+        ans = -float('inf')
         
-        for i, num in enumerate(nums):
-            prev = dp + []
+        for i in range(1, n+1):
+            num = nums[i-1]
             for t in range(target):
-                if t+num <= target:
-                    dp[t+num] = max(prev[t+num], prev[t] + 1)
-            # print(dp)
+                if t+num > target: break
+                dp[i][t] = cmax(dp[i-1][t], dp[i][t])
+                dp[i][t+num] = cmax(dp[i-1][t+num], dp[i-1][t] + 1)
+            ans = cmax(ans, dp[i][target])
                     
-        return dp[target] if dp[target] != -float('inf') else -1
+        return ans if ans != -float('inf') else -1
