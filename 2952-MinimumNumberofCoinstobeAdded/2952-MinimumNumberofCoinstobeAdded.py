b09d2bba8a6342b6c778d0a83830744ab649aba8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-09 18:35:48 +0530

    File updated at 9/5/2025, 6:35:43 pm

--- 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
+++ 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
@@ -1,14 +1,11 @@
-# Last updated: 9/5/2025, 3:43:25 pm
+# Last updated: 9/5/2025, 6:35:43 pm
 class Solution:
-    def lengthOfLongestSubsequence(self, nums: List[int], target: int) -> int:
-        nums.sort()
-
-        dp = [-1 for _ in range(target+1)]
-        dp[0] = 0
-
-        for i, num in enumerate(nums):
-            for t in range(target-num, -1, -1):
-                if dp[t] >= 0 and dp[t] + 1 > dp[t+num]:
-                    dp[t+num] = dp[t] + 1
-        
-        return dp[target]
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount+1)
+        dp[0] = 1
+        coins.sort()
+        for coin in coins:
+            for i in range(amount):
+                if coin+i > amount: break
+                dp[coin+i] += dp[i]
+        return dp[amount]
