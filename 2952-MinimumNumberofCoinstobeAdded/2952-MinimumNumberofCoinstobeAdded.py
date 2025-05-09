35d535e65731c60c131774b444b7ec798b566d49
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-09 15:08:23 +0530

    File updated at 9/5/2025, 3:08:18 pm

--- 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
+++ 2952-MinimumNumberofCoinstobeAdded/2952-MinimumNumberofCoinstobeAdded.py
@@ -1,21 +1,18 @@
-# Last updated: 9/5/2025, 2:42:32 pm
+# Last updated: 9/5/2025, 3:08:18 pm
 class Solution:
     def lengthOfLongestSubsequence(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        nums.sort()
-        dp = [[-1] * target for _ in range(n)]
-        
-        def rec(index, total):
-            if total == target: return 0
-            elif total > target or index == n: return -float('inf')
-            elif dp[index][total] != -1: return dp[index][total]
-            
-            ans = max(rec(index+1, total+nums[index]) + 1, rec(index+1, total))
-        
-            dp[index][total] = ans
-            return ans
-        
-        ret = rec(0, 0)
-        if ret == -float('inf'): return -1
-        return ret
 
+        dp = [-float('inf') for _ in range(target+1)]
+        dp[0] = 0
+
+        # [0, 1, 1, 2, 1, 1, 2, 2]
+        
+        for i, num in enumerate(nums):
+            prev = dp + []
+            for t in range(target):
+                if t+num <= target:
+                    dp[t+num] = max(prev[t+num], prev[t] + 1)
+            # print(dp)
+                    
+        return dp[target] if dp[target] != -float('inf') else -1
