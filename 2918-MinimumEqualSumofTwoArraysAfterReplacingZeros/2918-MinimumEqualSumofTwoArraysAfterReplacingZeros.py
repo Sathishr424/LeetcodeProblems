8a63a2b606acd9c23c15b998537cd512ec73b114
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-10 12:36:56 +0530

    File updated at 10/5/2025, 12:36:56 pm

--- 2918-MinimumEqualSumofTwoArraysAfterReplacingZeros/2918-MinimumEqualSumofTwoArraysAfterReplacingZeros.py
+++ 2918-MinimumEqualSumofTwoArraysAfterReplacingZeros/2918-MinimumEqualSumofTwoArraysAfterReplacingZeros.py
@@ -1,20 +1,29 @@
-# Last updated: 10/5/2025, 12:31:19 pm
+# Last updated: 10/5/2025, 12:36:56 pm
 class Solution:
     def minSum(self, nums1: List[int], nums2: List[int]) -> int:
-        sum_1 = sum_old_1 = sum(nums1)
-        sum_2 = sum_old_2 = sum(nums2)
+        sum_1 = 0
+        sum_2 = 0
+        z1 = 0
+        z2 = 0
 
         for num in nums1:
             if num == 0:
-                sum_1 += 1
-        
+                z1 += 1
+            else:
+                sum_1 += num
+
         for num in nums2:
             if num == 0:
-                sum_2 += 1
-        
+                z2 += 1
+            else:
+                sum_2 += num
+
+        sum_1 += z1
+        sum_2 += z2
+
         if sum_1 == sum_2: return sum_1
-        elif sum_1 > sum_2 and sum_old_2 < sum_2: return sum_1
-        elif sum_2 > sum_1 and sum_old_1 < sum_1: return sum_2
+        elif sum_1 > sum_2 and z2: return sum_1
+        elif sum_2 > sum_1 and z1: return sum_2
         return -1
 
 
