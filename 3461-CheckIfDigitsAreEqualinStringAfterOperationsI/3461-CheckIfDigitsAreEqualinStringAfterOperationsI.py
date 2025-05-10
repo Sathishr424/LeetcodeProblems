334709cfbc0e324075810878551536ba7e3a9439
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-10 19:24:04 +0530

    File updated at 10/5/2025, 7:24:04 pm

--- 3461-CheckIfDigitsAreEqualinStringAfterOperationsI/3461-CheckIfDigitsAreEqualinStringAfterOperationsI.py
+++ 3461-CheckIfDigitsAreEqualinStringAfterOperationsI/3461-CheckIfDigitsAreEqualinStringAfterOperationsI.py
@@ -1,11 +1,44 @@
-# Last updated: 10/5/2025, 1:41:20 pm
+# Last updated: 10/5/2025, 7:24:04 pm
+fact = [1] * 6
+
+for i in range(1, 6):
+    fact[i] = i * fact[i-1]
+
 class Solution:
     def hasSameDigits(self, s: str) -> bool:
         n = len(s)
         dp = [int(i) for i in s]
 
-        for i in range(1, n-1):
-            for j in range(n-1, i-1, -1):
-                dp[j] = (dp[j] + dp[j-1]) % 10
+        left = dp[0] + dp[-2]
+        right = dp[1] + dp[-1]
+
+        row = n-2
+
+        def lucasMod(x, y, mod):
+            a = 1
+            while x and y:
+                rem_x = x % mod
+                rem_y = y % mod
+
+                b = fact[rem_x] // (fact[rem_y] * fact[rem_x - rem_y])
+                a = a * b % mod
+
+                x //= mod
+                y //= mod
+
+            return a
         
-        return dp[-1] == dp[-2]
+        def getCoeff(r, c):
+            l2 = lucasMod(r, c, 2)
+            l5 = lucasMod(r, c, 5)
+
+            for i in range(10):
+                if i % 2 == l2 and i % 5 == l5: return i
+
+        for col in range(1, row):
+            coeff = getCoeff(row, col)
+
+            left = (left + dp[col] * coeff) % 10
+            right = (right + dp[n-col-1] * coeff) % 10
+
+        return left == right
