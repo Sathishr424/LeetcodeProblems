f7f651071b6f0bff3c1fb7cbe7fed01bcb4bfac9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-11 07:55:46 +0530

    File updated at 11/5/2025, 7:55:46 am

--- 1550-ThreeConsecutiveOdds/1550-ThreeConsecutiveOdds.py
+++ 1550-ThreeConsecutiveOdds/1550-ThreeConsecutiveOdds.py
@@ -1,10 +1,10 @@
-# Last updated: 11/5/2025, 7:54:57 am
+# Last updated: 11/5/2025, 7:55:46 am
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
         n = len(arr)
         if n < 3: return False
 
         for i in range(n-2):
-            if (arr[i] % 2) + (arr[i+1] % 2) + (arr[i+2] % 2) == 3: return True
+            if arr[i] % 2 and arr[i+1] % 2 and arr[i+2] % 2: return True
         
         return False
