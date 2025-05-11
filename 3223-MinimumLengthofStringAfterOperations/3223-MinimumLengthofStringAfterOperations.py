f43ad107075a8160b30fd19f5424b88e6901fca3
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-11 10:28:41 +0530

    File updated at 11/5/2025, 10:28:40 am

--- 3223-MinimumLengthofStringAfterOperations/3223-MinimumLengthofStringAfterOperations.py
+++ 3223-MinimumLengthofStringAfterOperations/3223-MinimumLengthofStringAfterOperations.py
@@ -1,13 +1,14 @@
-# Last updated: 11/5/2025, 10:27:37 am
+# Last updated: 11/5/2025, 10:28:40 am
 class Solution:
     def minimumLength(self, s: str) -> int:
-        freq = defaultdict(int)
+        freq = [0] * 26
 
         for char in s:
-            freq[char] += 1
+            freq[ord(char) - 97] += 1
+        
         ret = 0
-        for char in freq:
-            cnt = freq[char]
+        for i in range(26):
+            cnt = freq[i]
             while cnt >= 3:
                 cnt -= 2
             ret += cnt
