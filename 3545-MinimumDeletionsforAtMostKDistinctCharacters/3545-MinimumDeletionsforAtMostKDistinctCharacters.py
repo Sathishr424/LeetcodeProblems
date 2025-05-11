22a280a0b1a1696f18d115de4bd57fd0db5abf0a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-11 10:21:38 +0530

    File updated at 11/5/2025, 10:21:38 am

--- 3545-MinimumDeletionsforAtMostKDistinctCharacters/3545-MinimumDeletionsforAtMostKDistinctCharacters.py
+++ 3545-MinimumDeletionsforAtMostKDistinctCharacters/3545-MinimumDeletionsforAtMostKDistinctCharacters.py
@@ -1,4 +1,4 @@
-# Last updated: 11/5/2025, 10:20:51 am
+# Last updated: 11/5/2025, 10:21:38 am
 class Solution:
     def minDeletion(self, s: str, k: int) -> int:
         freq = defaultdict(int)
@@ -7,7 +7,6 @@ class Solution:
             freq[char] += 1
 
         arr = sorted(freq.values(), reverse=True)
-        cnt = sum(freq.values())
         ret = 0
         while len(arr) > k:
             ret += arr.pop()
