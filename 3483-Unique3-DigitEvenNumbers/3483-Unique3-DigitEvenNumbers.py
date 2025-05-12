c2e2b43f737364c1b19d24aaf60d7baeeb612452
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-12 13:37:36 +0530

    File updated at 12/5/2025, 1:37:33 pm

--- 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
@@ -1,4 +1,4 @@
-# Last updated: 12/5/2025, 1:30:19 pm
+# Last updated: 12/5/2025, 1:37:33 pm
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
         freq = [0] * 10
@@ -7,16 +7,17 @@ class Solution:
         
         ret = 0
 
-        for i in range(1, 10):
-            if freq[i] == 0: continue
-            freq[i] -= 1
-            for j in range(10):
-                if freq[j] == 0: continue
-                freq[j] -= 1
-                for k in range(0, 10, 2):
-                    if freq[k]: ret += 1
-                freq[j] += 1
-            freq[i] += 1
+        def check(num):
+            if num == 0: return True
+            rem = num % 10
+            if freq[rem] == 0: return False
+            freq[rem] -= 1
+            ans = check(num // 10)
+            freq[rem] += 1
+            return ans
 
+        for num in range(100, 999, 2):
+            ret += check(num)
+        
         return ret
         
