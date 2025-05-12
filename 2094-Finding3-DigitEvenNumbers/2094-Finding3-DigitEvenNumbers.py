0ad77a2595ec9fd56d4ea4ecf3036eaf6e6526a0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-12 13:35:23 +0530

    File updated at 12/5/2025, 1:35:20 pm

--- 2094-Finding3-DigitEvenNumbers/2094-Finding3-DigitEvenNumbers.py
+++ 2094-Finding3-DigitEvenNumbers/2094-Finding3-DigitEvenNumbers.py
@@ -1,4 +1,4 @@
-# Last updated: 12/5/2025, 1:25:14 pm
+# Last updated: 12/5/2025, 1:35:20 pm
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
         freq = [0] * 10
@@ -7,16 +7,20 @@ class Solution:
         
         ret = []
 
-        for i in range(1, 10):
-            if freq[i] == 0: continue
-            freq[i] -= 1
-            for j in range(10):
-                if freq[j] == 0: continue
-                freq[j] -= 1
-                for k in range(0, 10, 2):
-                    if freq[k]:
-                        ret.append((i * 100) + (j * 10) + k)
-                freq[j] += 1
-            freq[i] += 1
+        def check(num):
+            if num == 0: return True
+            rem = num % 10
+            if freq[rem] == 0: return False
+            freq[rem] -= 1
+            ans = check(num // 10)
+            freq[rem] += 1
+            return ans
 
+        for num in range(100, 1000):
+            if num % 2: continue
+
+            if check(num): ret.append(num)
+        
         return ret
+            
+
