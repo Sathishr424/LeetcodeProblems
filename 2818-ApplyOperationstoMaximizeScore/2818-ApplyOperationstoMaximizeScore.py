1540e07c73982226ab013984414aed422d93cc03
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-12 16:20:28 +0530

    Create file

---
+++ 2818-ApplyOperationstoMaximizeScore/2818-ApplyOperationstoMaximizeScore.py
@@ -0,0 +1,73 @@
+# Last updated: 12/5/2025, 4:20:25 pm
+N = 10**5 + 1
+is_prime = [True] * N
+is_prime[0] = False
+for i in range(2, int(N ** 0.5) + 1):
+    if not is_prime[i]: continue
+
+    for num in range(i*i, N, i):
+        is_prime[num] = False
+
+primes = []
+for num in range(2, N):
+    if is_prime[num]: primes.append(num)
+
+def getPrimeScore(num):
+    if num == 1: return 0
+    if is_prime[num]: return 1
+    score = 0
+    for x in primes:
+        if num % x == 0:
+            while num % x == 0:
+                num //= x
+            score += 1
+        if num == 1: return score
+mod = 10**9 + 7
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        ret = 1
+        scores = [getPrimeScore(num) for num in nums]
+        # print(scores)
+        arr = []
+        for i, num in enumerate(nums):
+            arr.append((num, i))
+        arr.sort(reverse=True)
+
+        left = [-1] * n
+        right = [n] * n
+        stack = []
+
+        for i in range(n-1, -1, -1):
+            score = scores[i]
+            while stack and stack[-1][1] <= score:
+                left[stack.pop()[0]] = i
+            
+            stack.append((i, score))
+        
+        stack = []
+        for i in range(n):
+            score = scores[i]
+            while stack and stack[-1][1] < score:
+                right[stack.pop()[0]] = i
+            
+            stack.append((i, score))
+        # print(left)
+        # print(right)
+
+        # [1, 2, 3, 4, 5]
+
+        for num, i in arr:
+            l = i - left[i]
+            r = right[i] - i
+
+            possible = min(k, l * r)
+            # print(num, (l, r), possible)
+
+            ret = ret * pow(num, possible, mod) % mod
+
+            k -= possible
+            
+            if k == 0: break
+
+        return ret
