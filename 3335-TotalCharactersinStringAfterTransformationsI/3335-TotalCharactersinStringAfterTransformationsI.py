5d574972c862066d189a6d88db8df10a84abfaa7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-13 12:30:16 +0530

    File updated at 13/5/2025, 12:30:12 pm

--- 3335-TotalCharactersinStringAfterTransformationsI/3335-TotalCharactersinStringAfterTransformationsI.py
+++ 3335-TotalCharactersinStringAfterTransformationsI/3335-TotalCharactersinStringAfterTransformationsI.py
@@ -1,29 +1,23 @@
-# Last updated: 13/5/2025, 12:09:28 pm
+# Last updated: 13/5/2025, 12:30:12 pm
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
         mod = 10**9 + 7
-        # ab
-        # 26, 25
-        # adefg
-        #   fgh
-
         freq = [0] * 26
 
         for char in s:
             freq[ord(char) - 97] += 1
-        # print(freq)
+
         for i in range(t):
-            curr = freq[25]
+            z = freq[25]
+            freq[25] = 0
             for num in range(24, -1, -1):
                 if freq[num]:
-                    freq[num+1] = (freq[num+1] + freq[num]) % mod
+                    freq[num+1] = freq[num]
                     freq[num] = 0
             
-            if freq[25]:
-                freq[0] = (freq[0] + curr) % mod
-                freq[1] = (freq[1] + curr) % mod
-                freq[25] -= curr
-            # print(t+1, freq, sum(freq))
+            if z:
+                freq[0] = (freq[0] + z) % mod
+                freq[1] = (freq[1] + z) % mod
         
         return sum(freq) % mod
                 
