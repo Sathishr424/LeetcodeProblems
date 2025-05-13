a3a31caf968ba68d1d1a6ef222478c53bbdd6ba6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-14 01:42:56 +0530

    File updated at 14/5/2025, 1:42:51 am

--- 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
+++ 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
@@ -1,15 +1,15 @@
-# Last updated: 14/5/2025, 1:42:28 am
+# Last updated: 14/5/2025, 1:42:51 am
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
 
-        @cache
         def mas(i, j):
             return abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
 
         heap = [(0, 0)]
         ret = 0
         edges = set([i for i in range(n)])
+        
         while heap:
             cost, x = heapq.heappop(heap)
             if x not in edges: continue
