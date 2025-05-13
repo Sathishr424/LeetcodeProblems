c1ec3e45af3fff51ff84bcb9c3e7caa6bef105cc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-14 02:02:32 +0530

    File updated at 14/5/2025, 2:02:28 am

--- 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
+++ 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
@@ -1,4 +1,4 @@
-# Last updated: 14/5/2025, 1:59:27 am
+# Last updated: 14/5/2025, 2:02:28 am
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
@@ -8,20 +8,23 @@ class Solution:
 
         heap = [(0, 0)]
         ret = 0
-        edges = set([i for i in range(n)])
+        visited = [False] * n
         dis = [float('inf')] * n
+        edges = 0
 
         while heap:
             cost, x = heapq.heappop(heap)
-            if x not in edges: continue
+            if visited[x]: continue
+            visited[x] = True
             ret += cost
-            if len(edges) == 1: break
-            edges.remove(x)
+            edges += 1
+            if edges == n: break
 
-            for y in edges:
-                cost = mas(x, y)
-                if cost < dis[y]:
-                    dis[y] = cost
-                    heapq.heappush(heap, (cost, y))
+            for y in range(n):
+                if not visited[y]:
+                    cost = mas(x, y)
+                    if cost < dis[y]:
+                        dis[y] = cost
+                        heapq.heappush(heap, (cost, y))
             
         return ret
