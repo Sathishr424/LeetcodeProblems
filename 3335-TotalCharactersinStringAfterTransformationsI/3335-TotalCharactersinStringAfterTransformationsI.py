2375b3b664a3f399d95519c2e80fd31a7cfdd843
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-13 12:33:26 +0530

    File updated at 13/5/2025, 12:33:22 pm

--- 3335-TotalCharactersinStringAfterTransformationsI/3335-TotalCharactersinStringAfterTransformationsI.py
+++ 3335-TotalCharactersinStringAfterTransformationsI/3335-TotalCharactersinStringAfterTransformationsI.py
@@ -1,4 +1,4 @@
-# Last updated: 13/5/2025, 12:31:42 pm
+# Last updated: 13/5/2025, 12:33:22 pm
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
         mod = 10**9 + 7
@@ -14,8 +14,8 @@ class Solution:
                 freq[num+1] = freq[num]
                 freq[num] = 0
             
-            freq[0] = (freq[0] + z) % mod
-            freq[1] = (freq[1] + z) % mod
+            freq[0] = freq[0] + z
+            freq[1] = freq[1] + z
         
         return sum(freq) % mod
                 
