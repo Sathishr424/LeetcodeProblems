578c216165d861c2650fd7b6d22c4e8c69af45a1
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-15 03:56:34 +0530

    File updated at 15/5/2025, 3:56:27 am

--- 873-LengthofLongestFibonacciSubsequence/873-LengthofLongestFibonacciSubsequence.py
+++ 873-LengthofLongestFibonacciSubsequence/873-LengthofLongestFibonacciSubsequence.py
@@ -1,21 +1,41 @@
-# Last updated: 15/5/2025, 3:56:11 am
+# Last updated: 15/5/2025, 3:56:27 am
 N = 10**5
 mod = 10**9 + 7
-dp = [[[-1] * 2 for _ in range(3)] for _ in range(N+1)]
+dp = [[[0] * 2 for _ in range(3)] for _ in range(N)]
 
-def dfs(x, l, a):
-    if dp[x][l][a] != -1: return dp[x][l][a]
-    if x == 0: return 1
-    ans = 0
-    if l < 2: ans = (ans + dfs(x-1, l+1, a)) % mod
-    if a == 0: ans = (ans + dfs(x-1, 0, a+1)) % mod
+dp[0][0][0] = 1
+dp[0][0][1] = 1
 
-    ans = (ans + dfs(x-1, 0, a)) % mod
-    dp[x][l][a] = ans
-    return ans
+dp[0][1][0] = 1
+dp[0][1][1] = 0
 
-dfs(N, 0, 0)
+dp[0][2][0] = 0
+dp[0][2][1] = 0
+
+# l = 0, a = 0 => (0, a), (l+1, a) (0, a+1)
+# l = 1, a = 0 => (0, a), (l+1, a) (0, a+1)
+# l = 2, a = 0 => (0, a), (0, a+1)
+
+# l = 0, a = 1 => (0, a), (l+1, a)
+# l = 1, a = 1 => (0, a), (l+1, a)
+# l = 2, a = 1 => (0, a)
+
+for i in range(N-1):
+    for l in range(3):
+        for a in range(2):
+            dp[i+1][0][a] = (dp[i+1][0][a] + dp[i][l][a]) % mod
+
+            if l < 2:
+                dp[i+1][l+1][a] = (dp[i+1][l+1][a] + dp[i][l][a]) % mod
+            
+            if a == 0:
+                dp[i+1][0][a+1] = (dp[i+1][0][a+1] + dp[i][l][a]) % mod
 
 class Solution:
     def checkRecord(self, n: int) -> int:
-        return dp[n][0][0]
+        ret = 0
+        n -= 1
+        for i in range(3):
+            for j in range(2):
+                ret = (ret + dp[n][i][j]) % mod
+        return ret
