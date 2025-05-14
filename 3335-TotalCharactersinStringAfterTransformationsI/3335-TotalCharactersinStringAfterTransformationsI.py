fa900fce665d08de861f7111f34693e40ea59f6f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-14 13:26:06 +0530

    File updated at 14/5/2025, 1:26:01 pm

--- 3335-TotalCharactersinStringAfterTransformationsI/3335-TotalCharactersinStringAfterTransformationsI.py
+++ 3335-TotalCharactersinStringAfterTransformationsI/3335-TotalCharactersinStringAfterTransformationsI.py
@@ -1,9 +1,19 @@
-# Last updated: 13/5/2025, 3:24:42 pm
-mod = 10 ** 9 + 7
-dp = [1] * 26 + [0] * 100100
-for i in range(26, 100100):
-    dp[i] = (dp[i - 26] + dp[i - 26 + 1]) % mod
+# Last updated: 14/5/2025, 1:26:01 pm
+N = 10**5 + 1
+mod = 10**9 + 7
+
+dp = [[1] * 26] + [[0] * 26 for i in range(N-1)]
+
+for i in range(1, N):
+    for j in range(25):
+        dp[i][j] = (dp[i][j] + dp[i-1][j + 1]) % mod
+    dp[i][25] = (dp[i][25] + dp[i-1][0] + dp[i-1][1]) % mod
 
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
-        return sum(dp[ord(c) - ord('a') + t] for c in s) % mod
+
+        ret = 0
+        for char in s:
+            ret = (ret + dp[t][ord(char) - 97]) % mod
+
+        return ret
