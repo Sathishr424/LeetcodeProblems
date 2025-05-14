89e1385e17911408fdebfff7949eb9ea91c04440
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-14 20:19:07 +0530

    File updated at 14/5/2025, 8:19:01 pm

--- 372-SuperPow/372-SuperPow.py
+++ 372-SuperPow/372-SuperPow.py
@@ -1,4 +1,4 @@
-# Last updated: 14/5/2025, 8:18:24 pm
+# Last updated: 14/5/2025, 8:19:01 pm
 # 8 -> 4 -> 2 -> 1
 class Solution:
     def myPow(self, x: float, n: int) -> float:
@@ -10,6 +10,5 @@ class Solution:
             if n % 2: ans *= x
             return ans
         
-        if n < 0:
-            return pow(1/x, -n)
+        if n < 0: return pow(1/x, -n)
         return pow(x, n)
