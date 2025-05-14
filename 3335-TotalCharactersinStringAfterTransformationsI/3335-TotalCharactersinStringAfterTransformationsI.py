9ad8c21f3cef4916d667005b218a9f200823ad19
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-14 22:36:55 +0530

    File updated at 14/5/2025, 10:36:49 pm

--- 3335-TotalCharactersinStringAfterTransformationsI/3335-TotalCharactersinStringAfterTransformationsI.py
+++ 3335-TotalCharactersinStringAfterTransformationsI/3335-TotalCharactersinStringAfterTransformationsI.py
@@ -1,40 +1,20 @@
-# Last updated: 14/5/2025, 10:36:11 pm
+# Last updated: 14/5/2025, 10:36:49 pm
+N = 10**5 + 1
 mod = 10**9 + 7
 
-def matrix_multiplication(matrix_x, matrix_y):
-    m = len(matrix_x)
-    n = len(matrix_y[0])
-    new_matrix = [[0] * n for _ in range(m)]
-    for i in range(m):
-        for j in range(n):
-            curr = 0
-            for k in range(n):
-                curr = (curr + matrix_x[i][k] * matrix_y[k][j] % mod) % mod
-            new_matrix[i][j] = curr
-    return new_matrix
+dp = [[1] * 26] + [[0] * 26 for i in range(N-1)]
 
-def matrix_pow(matrix, power):
-    if power == 1: return matrix
-    ans = matrix_pow(matrix, power // 2)
-    ans = matrix_multiplication(ans, ans)
-    if power % 2:
-        ans = matrix_multiplication(ans, matrix)
-    return ans
+for i in range(1, N):
+    for j in range(25):
+        dp[i][j] = (dp[i][j] + dp[i-1][j + 1]) % mod
+    dp[i][25] = (dp[i][25] + dp[i-1][0] + dp[i-1][1]) % mod
 
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
-        matrix = [[0] * 26 for _ in range(26)]
 
-        for i in range(25):
-            matrix[i][i+1] = 1
-        
-        matrix[25][0] = 1
-        matrix[25][1] = 1
-
-        matrix = matrix_pow(matrix, t)
-
-        freq = [[0] * 26]
+        ret = 0
         for char in s:
-            freq[0][ord(char) - 97] += 1
+            ret += dp[t][ord(char) - 97]
+            ret %= mod
 
-        return sum(matrix_multiplication(freq, matrix)[0]) % mod
+        return ret
