ac5689097cf0f8402978a1ad6f45e4f7b5630065
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-15 01:46:43 +0530

    File updated at 15/5/2025, 1:46:36 am

--- 873-LengthofLongestFibonacciSubsequence/873-LengthofLongestFibonacciSubsequence.py
+++ 873-LengthofLongestFibonacciSubsequence/873-LengthofLongestFibonacciSubsequence.py
@@ -1,21 +1,26 @@
-# Last updated: 15/5/2025, 1:42:04 am
+# Last updated: 15/5/2025, 1:46:36 am
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
         n = len(arr)
 
-        def process(curr, prev):
+        dp = [[-1] * n for _ in range(n)]
+
+        def process(i, j):
+            if dp[i][j] != -1: return dp[i][j]
+            prev = arr[i]
+            curr = arr[j]
             num = curr - prev
             if prev > num and num in added:
-                return process(prev, arr[added[num]]) + 1
-            return 0
+                dp[i][j] = process(added[num], i) + 1
+            else:
+                dp[i][j] = 0
+            return dp[i][j]
         
         ret = 0
         added = {}
         for i in range(n):
             for j in range(i+1, n):
-                curr = arr[j]
-                prev = arr[i]
-                ans = process(curr, prev)
+                ans = process(i, j)
                 if ans > 0:
                     ret = max(ret, ans + 2)
                         
