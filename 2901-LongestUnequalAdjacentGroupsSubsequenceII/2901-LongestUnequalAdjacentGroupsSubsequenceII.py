482307bd2a5c825f8d22a4238d8b87dbc83c0eca
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-15 15:52:40 +0530

    File updated at 15/5/2025, 3:52:33 pm

--- 2901-LongestUnequalAdjacentGroupsSubsequenceII/2901-LongestUnequalAdjacentGroupsSubsequenceII.py
+++ 2901-LongestUnequalAdjacentGroupsSubsequenceII/2901-LongestUnequalAdjacentGroupsSubsequenceII.py
@@ -1,19 +1,17 @@
-# Last updated: 15/5/2025, 3:50:42 pm
+# Last updated: 15/5/2025, 3:52:33 pm
 class Solution:
     def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
         n = len(words)
 
-        distance = [[0] * n for _ in range(n)]
-
-        for i in range(n):
-            for j in range(i+1, n):
-                if len(words[i]) != len(words[j]): continue
-                diff = 0
-                
-                for k in range(len(words[i])):
-                    if words[i][k] != words[j][k]: diff += 1
-                
-                distance[i][j] = diff
+        def checkDistance(i, j):
+            if len(words[i]) != len(words[j]): return False
+            diff = 0
+            
+            for k in range(len(words[i])):
+                if words[i][k] != words[j][k]: 
+                    diff += 1
+            
+            return diff == 1
 
         dp = [[words[i]] for i in range(n)]
         ret = dp[0]
@@ -21,7 +19,7 @@ class Solution:
         for i in range(n-1, -1, -1):
             new_dp = dp[i]
             for j in range(i+1, n):
-                if len(words[i]) == len(words[j]) and groups[i] != groups[j] and distance[i][j] == 1:
+                if len(words[i]) == len(words[j]) and groups[i] != groups[j] and checkDistance(i, j):
                     if len(dp[j]) + len(dp[i]) > len(new_dp):
                         new_dp = dp[i] + dp[j]
             
