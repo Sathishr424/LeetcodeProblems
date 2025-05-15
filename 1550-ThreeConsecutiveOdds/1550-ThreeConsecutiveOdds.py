fd0ff6ac362a5b30cdb25a4024a61b72a19950d4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-15 22:34:00 +0530

    File updated at 15/5/2025, 10:33:52 pm

--- 1550-ThreeConsecutiveOdds/1550-ThreeConsecutiveOdds.py
+++ 1550-ThreeConsecutiveOdds/1550-ThreeConsecutiveOdds.py
@@ -1,9 +1,8 @@
-# Last updated: 15/5/2025, 10:31:38 pm
-N = 37
+# Last updated: 15/5/2025, 10:33:52 pm
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def countLargestGroup(self, n: int) -> int:
-        groups = [0] * N
+        groups = [0] * 37
         max_group = 0
 
         for num in range(1, n+1):
@@ -16,7 +15,7 @@ class Solution:
             max_group = cmax(max_group, groups[s])
         
         ret = 0
-        for num in range(1, N):
-            ret += groups[num] == max_group
+        for num in groups:
+            ret += num == max_group
         
         return ret
