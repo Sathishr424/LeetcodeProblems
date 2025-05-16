23544bbdf76a0cfe06650bbbd0548904ca6bd26a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-16 22:39:31 +0530

    Create file

---
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.py
@@ -0,0 +1,29 @@
+# Last updated: 16/5/2025, 10:39:30 pm
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        # abasdsadsdcsacdeeeff a.*cdeeeff
+        m = len(s)
+        n = len(p)
+        @cache
+        def rec(left, right):
+            if left == m and right == n: return True
+            if left < m and right < n:
+                if s[left] == p[right]:
+                    if right+1 < n and p[right+1] == '*' and rec(left, right+1): return True
+                    return rec(left+1, right+1)
+                elif p[right] == '.':
+                    if right+1 < n and p[right+1] == '*' and rec(left, right+1): return True
+                    return rec(left+1, right+1)
+                elif p[right] == '*':
+                    prev = right-1
+                    if rec(left, right+1): return True
+                    if prev >= 0 and (p[prev] == '.' or p[prev] == s[left]):
+                        return rec(left+1, right+1) or rec(left+1, right)
+                elif right+1 < n and p[right+1] == '*':
+                    return rec(left, right+2)
+            elif right < n:
+                if p[right] == '*': return rec(left, right+1)
+                elif right+1 < n and p[right+1] == '*': return rec(left, right+2)
+            return False
+
+        return rec(0, 0)
