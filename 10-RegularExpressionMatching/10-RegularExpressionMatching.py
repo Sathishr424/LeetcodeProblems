8920f3b5e620e787d17450e08fa40a2fc3646d84
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-16 22:56:44 +0530

    File updated at 16/5/2025, 10:56:42 pm

--- 10-RegularExpressionMatching/10-RegularExpressionMatching.py
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.py
@@ -1,4 +1,4 @@
-# Last updated: 16/5/2025, 10:40:47 pm
+# Last updated: 16/5/2025, 10:56:42 pm
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         # abasdsadsdcsacdeeeff a.*cdeeeff
@@ -6,24 +6,20 @@ class Solution:
         n = len(p)
         @cache
         def rec(left, right):
-            if left == m and right == n: return True
-            if left < m and right < n:
-                if s[left] == p[right]:
-                    if right+1 < n and p[right+1] == '*' and rec(left, right+1): return True
-                    return rec(left+1, right+1)
-                elif p[right] == '.':
-                    if right+1 < n and p[right+1] == '*' and rec(left, right+1): return True
-                    return rec(left+1, right+1)
-                elif p[right] == '*':
-                    prev = right-1
-                    if rec(left, right+1): return True
-                    if prev >= 0 and (p[prev] == '.' or p[prev] == s[left]):
-                        return rec(left+1, right+1) or rec(left+1, right)
-                elif right+1 < n and p[right+1] == '*':
-                    return rec(left, right+2)
-            elif right < n:
-                if p[right] == '*': return rec(left, right+1)
-                elif right+1 < n and p[right+1] == '*': return rec(left, right+2)
+            if right == n: return left == m
+            elif right+1 < n and p[right+1] == '*' and rec(left, right+1): return True
+            elif left == m: 
+                return p[right] == '*' and rec(left, right+1)
+            
+            if s[left] == p[right]:
+                return rec(left+1, right+1)
+            elif p[right] == '.':
+                return rec(left+1, right+1)
+            elif p[right] == '*':
+                if rec(left, right+1): return True
+                if right > 0 and (p[right-1] == '.' or p[right-1] == s[left]):
+                    return rec(left+1, right)
+
             return False
 
         return rec(0, 0)
