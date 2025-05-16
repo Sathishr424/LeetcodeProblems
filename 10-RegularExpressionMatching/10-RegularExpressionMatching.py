8d0e3b4dd279a736c43275c21489e2d7c174d61d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-16 22:40:48 +0530

    File updated at 16/5/2025, 10:40:47 pm

--- 10-RegularExpressionMatching/10-RegularExpressionMatching.py
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.py
@@ -1,10 +1,29 @@
-# Last updated: 16/5/2025, 10:40:28 pm
+# Last updated: 16/5/2025, 10:40:47 pm
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        if (len(p) == 0): return len(s) == 0
-        firstMatch = len(s) > 0 and (s[0] == p[0] or p[0] == ".")
-        
-        if (len(p) >= 2 and p[1] == "*"):
-            return (self.isMatch(s,p[2:]) or (firstMatch and self.isMatch(s[1:],p)))
-        else:
-            return firstMatch and self.isMatch(s[1:], p[1:])
+        # abasdsadsdcsacdeeeff a.*cdeeeff
+        m = len(s)
+        n = len(p)
+        @cache
+        def rec(left, right):
+            if left == m and right == n: return True
+            if left < m and right < n:
+                if s[left] == p[right]:
+                    if right+1 < n and p[right+1] == '*' and rec(left, right+1): return True
+                    return rec(left+1, right+1)
+                elif p[right] == '.':
+                    if right+1 < n and p[right+1] == '*' and rec(left, right+1): return True
+                    return rec(left+1, right+1)
+                elif p[right] == '*':
+                    prev = right-1
+                    if rec(left, right+1): return True
+                    if prev >= 0 and (p[prev] == '.' or p[prev] == s[left]):
+                        return rec(left+1, right+1) or rec(left+1, right)
+                elif right+1 < n and p[right+1] == '*':
+                    return rec(left, right+2)
+            elif right < n:
+                if p[right] == '*': return rec(left, right+1)
+                elif right+1 < n and p[right+1] == '*': return rec(left, right+2)
+            return False
+
+        return rec(0, 0)
