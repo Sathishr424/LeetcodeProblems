6015b59dce00ac695d50ec8b41f1e1e409539980
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-16 07:27:37 +0530

    File updated at 16/5/2025, 7:27:36 am

--- 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
+++ 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
@@ -1,28 +1,28 @@
-# Last updated: 14/5/2025, 2:10:53 am
+# Last updated: 16/5/2025, 7:27:36 am
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
 
-        def mas(i, j):
+        def getDistance(i, j):
             return abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+        
+        stack = [(0, 0)]
+        edges = 0
+        visited = [False] * n
+        total_cost = 0
 
-        heap = [(0, 0)]
-        ret = 0
-        edges = set([i for i in range(n)])
-        dis = [float('inf')] * n
-        dis[0] = 0
+        while stack:
+            cost, x = heapq.heappop(stack)
+            if visited[x]: continue
 
-        while heap:
-            cost, x = heapq.heappop(heap)
-            if x not in edges: continue
-            ret += cost
-            if len(edges) == 1: break
-            edges.remove(x)
+            visited[x] = True
+            total_cost += cost
+            edges += 1
+            if edges == n: return total_cost
 
-            for y in edges:
-                cost = mas(x, y)
-                if cost < dis[y]:
-                    dis[y] = cost
-                    heapq.heappush(heap, (cost, y))
-            
-        return ret
+            for y in range(n):
+                if visited[y]: continue
+                d = getDistance(x, y)
+                heapq.heappush(stack, (d, y))
+        
+        return total_cost
