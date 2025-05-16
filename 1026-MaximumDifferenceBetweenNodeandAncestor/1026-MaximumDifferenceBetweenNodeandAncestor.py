420f00c3ce49042072d60cb4afa0df603d51aeb2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-16 08:23:48 +0530

    File updated at 16/5/2025, 8:23:48 am

--- 1026-MaximumDifferenceBetweenNodeandAncestor/1026-MaximumDifferenceBetweenNodeandAncestor.py
+++ 1026-MaximumDifferenceBetweenNodeandAncestor/1026-MaximumDifferenceBetweenNodeandAncestor.py
@@ -1,20 +1,21 @@
-# Last updated: 16/5/2025, 8:22:42 am
+# Last updated: 16/5/2025, 8:23:48 am
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-N = 10**5
+cmax = lambda x, y: x if x > y else y
+cmin = lambda x, y: x if x < y else y
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
         def traverse(node, maxi, mini):
             if node == None: return 0
 
-            ans = max(abs(maxi - node.val), abs(mini - node.val))
-            left = traverse(node.left, max(maxi, node.val), min(mini, node.val))
-            right = traverse(node.right, max(maxi, node.val), min(mini, node.val))
+            ans = cmax(abs(maxi - node.val), abs(mini - node.val))
+            left = traverse(node.left, cmax(maxi, node.val), cmin(mini, node.val))
+            right = traverse(node.right, cmax(maxi, node.val), cmin(mini, node.val))
 
-            return max(ans, left, right)
+            return cmax(ans, cmax(left, right))
         
         return traverse(root, root.val, root.val)
