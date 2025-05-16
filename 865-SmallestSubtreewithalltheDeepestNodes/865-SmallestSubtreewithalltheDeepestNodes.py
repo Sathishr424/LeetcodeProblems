c03223b038b4843f16fcb6d7841039ef4c6b0be5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-16 08:51:32 +0530

    File updated at 16/5/2025, 8:51:32 am

--- 865-SmallestSubtreewithalltheDeepestNodes/865-SmallestSubtreewithalltheDeepestNodes.py
+++ 865-SmallestSubtreewithalltheDeepestNodes/865-SmallestSubtreewithalltheDeepestNodes.py
@@ -1,4 +1,4 @@
-# Last updated: 4/4/2025, 2:07:11 pm
+# Last updated: 16/5/2025, 8:51:32 am
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,25 +7,22 @@
 #         self.right = right
 class Solution:
     def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def rec(node, level):
-            if node == None: return level-1
-            return max(rec(node.left, level+1), rec(node.right, level+1))
-        
-        max_level = rec(root, 0)
-        
-        def rec2(node, level):
-            if node == None: return None
+        deapest_level = 0
+        deapest_node = root
 
-            if level == max_level: return node
-
-            left = rec2(node.left, level+1)
-            right = rec2(node.right, level+1)
+        def traverse(node, level):
+            nonlocal deapest_level, deapest_node
+            if node == None: return level
+            level += 1
+            left = traverse(node.left, level)
+            right = traverse(node.right, level)
+            if left >= deapest_level and left == right:
+                deapest_node = node
+                deapest_level = left
+                return left
+            else:
+                return max(left, right)
             
-            if left and right: return node
-            elif left: return left
-            elif right: return right
-            
-            return None
-        
-        return rec2(root, 0)
+        traverse(root, 0)
+        return deapest_node
         
