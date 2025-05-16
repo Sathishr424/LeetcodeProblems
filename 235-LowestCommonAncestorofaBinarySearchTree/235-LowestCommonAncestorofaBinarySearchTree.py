08a36406cd2685a9f3764404722dc310b41e5984
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-16 10:18:57 +0530

    File updated at 16/5/2025, 10:18:56 am

--- 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 16/5/2025, 10:01:02 am
+# Last updated: 16/5/2025, 10:18:56 am
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -8,17 +8,29 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs(node):
-            if node == None: return None
-
-            if node.val == p.val or node.val == q.val:
-                return node
-            
-            left = dfs(node.left)
-            right = dfs(node.right)
-
-            if left and right: return node
-            return left or right or None
+        if p.val > q.val:
+            p, q = q, p
         
-        return dfs(root)
+        def dfs(node, x):
+            if node == None: return None
+            if node.val == x: return node
 
+            if x > node.val:
+                return dfs(node.right, x)
+            else:
+                return dfs(node.left, x)
+        
+        def helper(node):
+            if p.val <= node.val and q.val >= node.val:
+                left = dfs(node, p.val)
+                right = dfs(node, q.val)
+
+                if left and right: return node
+                elif left: return left
+                return right
+            elif p.val > node.val:
+                return helper(node.right)
+            else:
+                return helper(node.left)
+
+        return helper(root)
