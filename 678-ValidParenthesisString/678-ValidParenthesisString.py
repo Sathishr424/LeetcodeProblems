76cddc145746992094c0d2328302d43e7f47472b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-17 17:05:46 +0530

    File updated at 17/5/2025, 5:05:44 pm

--- 678-ValidParenthesisString/678-ValidParenthesisString.py
+++ 678-ValidParenthesisString/678-ValidParenthesisString.py
@@ -1,11 +1,12 @@
-# Last updated: 17/5/2025, 5:02:30 pm
+# Last updated: 17/5/2025, 5:05:44 pm
 class Solution:
     def checkValidString(self, s: str) -> bool:
         n = len(s)
+
         @cache
         def rec(index, open):
             if index == n: return open == 0
-            if open < 0: return False
+            if open < 0 or n-index < open: return False
             if s[index] == '(':
                 return rec(index+1, open+1)
             elif s[index] == ')':
