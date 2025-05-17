fec4bd784b5388164525b5d285bb8e245795ad12
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-17 11:53:06 +0530

    File updated at 17/5/2025, 11:53:04 am

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -1,4 +1,4 @@
-# Last updated: 17/5/2025, 11:42:55 am
+# Last updated: 17/5/2025, 11:53:04 am
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
@@ -6,13 +6,15 @@ class Solution:
         """
         n = len(nums)
         left = 0
+        right = n-1
         
-        for i in range(n):
+        i = 0
+        while i < n and i <= right:
             if nums[i] == 0:
                 nums[i], nums[left] = nums[left], nums[i]
                 left += 1
-        
-        for i in range(left, n):
-            if nums[i] == 1:
-                nums[i], nums[left] = nums[left], nums[i]
-                left += 1
+            elif nums[i] == 2:
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
+                continue
+            i += 1
