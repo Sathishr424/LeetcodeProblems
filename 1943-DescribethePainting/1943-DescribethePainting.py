857cf2189100a1fbab13d51e98f014f6eaa9d4cc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-19 17:10:39 +0530

    File updated at 19/5/2025, 5:10:34 pm

--- 1943-DescribethePainting/1943-DescribethePainting.py
+++ 1943-DescribethePainting/1943-DescribethePainting.py
@@ -1,30 +1,32 @@
-# Last updated: 19/5/2025, 5:08:45 pm
+# Last updated: 19/5/2025, 5:10:34 pm
 class Solution:
     def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
-        starts = defaultdict(int)
-        ends = defaultdict(int)
-
         first = segments[0][0]
         last = segments[0][0]
 
         for x, y, color in segments:
-            starts[x] += color
-            ends[y] += color
             first = min(first, x)
             last = max(last, y)
+        
+        start = [0] * (last+1)
+        end = [0] * (last+1)
+
+        for x, y, color in segments:
+            start[x] += color
+            end[y] += color
         
         ret = []
         prev = first
         mix = 0
         for index in range(first, last+1):
-            if index in ends:
+            if end[index]:
                 ret.append([prev, index, mix])
-                mix -= ends[index]
+                mix -= end[index]
                 prev = index
-            if index in starts:
+            if start[index]:
                 if prev != index and mix > 0:
                     ret.append([prev, index, mix])
-                mix += starts[index]
+                mix += start[index]
                 prev = index
 
         return ret
