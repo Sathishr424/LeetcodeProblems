1b518d715bfa7203e28e81fa66b6a309784d77f9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-20 16:47:25 +0530

    File updated at 20/5/2025, 4:47:19 pm

--- 3489-ZeroArrayTransformationIV/3489-ZeroArrayTransformationIV.py
+++ 3489-ZeroArrayTransformationIV/3489-ZeroArrayTransformationIV.py
@@ -1,24 +1,23 @@
-# Last updated: 20/5/2025, 4:40:05 pm
+# Last updated: 20/5/2025, 4:47:19 pm
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
         n = len(nums)
         m = len(queries)
         
         def rec(index, need):
+            if need == 0: return -1
             sums = [False] * (need+1)
             sums[0] = True
 
             for i, (x, y, val) in enumerate(queries):
-                if x <= index and y >= index:
-                    for s in range(need-val, -1, -1):
-                        if sums[s]:
-                            if s+val == need: return i
-                            sums[s+val] = True
+                if x <= index and y >= index and need-val >= 0:
+                    if sums[need-val]: return i
+                    for s in range(need-1, val-1, -1):
+                        sums[s] = sums[s] or sums[s-val]
             return m
         
         ret = -1
         for i, num in enumerate(nums):
-            if num == 0: continue
             ret = max(ret, rec(i, num))
         
         return -1 if ret >= m else ret+1
