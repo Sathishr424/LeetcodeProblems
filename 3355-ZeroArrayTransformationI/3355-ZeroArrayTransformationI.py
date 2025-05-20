18ffc417557d6c387b5b756e6aad0eaefd3c84f8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-20 13:39:17 +0530

    File updated at 20/5/2025, 1:39:11 pm

--- 3355-ZeroArrayTransformationI/3355-ZeroArrayTransformationI.py
+++ 3355-ZeroArrayTransformationI/3355-ZeroArrayTransformationI.py
@@ -1,25 +1,19 @@
-# Last updated: 20/5/2025, 1:38:44 pm
+# Last updated: 20/5/2025, 1:39:11 pm
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
         n = len(nums)
-        start = defaultdict(int)
-        end = defaultdict(int)
-        first = queries[0][0]
-        last = queries[0][1]
+
+        line = [0] * (n+1)
 
         for x, y in queries:
-            start[x] += 1
-            end[y] += 1
+            line[x] += 1
+            line[y+1] -= 1
         
         s = 0
-
+        zero = True
         for i in range(n):
-            s += start[i]
-
-            nums[i] -= s
-            if nums[i] > 0: return False
-
-            s -= end[i]
+            s += line[i]
+            if nums[i] - s > 0: return False
 
         return True
             
