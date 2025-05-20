342ef70e8fcf6a2854f04151aaf8b3e630475de5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-20 16:01:53 +0530

    File updated at 20/5/2025, 4:01:47 pm

--- 3489-ZeroArrayTransformationIV/3489-ZeroArrayTransformationIV.py
+++ 3489-ZeroArrayTransformationIV/3489-ZeroArrayTransformationIV.py
@@ -1,29 +1,25 @@
-# Last updated: 20/5/2025, 3:22:10 pm
+# Last updated: 20/5/2025, 4:01:47 pm
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        
         n = len(nums)
         m = len(queries)
-        maxi = max(nums)
-
-        dp = [[[-1] * n for _ in range(maxi+1)] for _ in range(m)]
         
-        def rec(index, need, k):
-            if need == 0: return k-1
-            if k == m: return m
-            if dp[k][need][index] != -1: return dp[k][need][index]
-
-            ans = rec(index, need, k+1)
-            x, y, val = queries[k]
-            if x <= index and y >= index and need - val >= 0:
-                ans = min(ans, rec(index, need-val, k+1))
-            dp[k][need][index] = ans
-            return ans
+        def rec(index, need):
+            there = {0: 0}
+            for i, (x, y, val) in enumerate(queries):
+                if x <= index and y >= index and need-val >= 0:
+                    new_there = {}
+                    for num in there:
+                        new_there[num] = there[num]
+                        if num+val == need: return i
+                        new_there[num + val] = i
+                    there = new_there
+            return m
         
         ret = -1
         for i, num in enumerate(nums):
             if num == 0: continue
-            ret = max(ret, rec(i, num, 0))
+            ret = max(ret, rec(i, num))
         
         return -1 if ret >= m else ret+1
             
