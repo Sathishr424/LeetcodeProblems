f9abefb463bafecee4c04621225376da0c6f267e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-21 08:03:44 +0530

    File updated at 21/5/2025, 8:03:38 am

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.py
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.py
@@ -1,4 +1,4 @@
-# Last updated: 21/5/2025, 6:30:20 am
+# Last updated: 21/5/2025, 8:03:38 am
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
@@ -7,19 +7,39 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
 
-        rows = {}
-        cols = {}
+        first_row = False
+        first_col = False
+
+        for j in range(n):
+            if matrix[0][j] == 0:
+                first_row = True
+                break
+        
+        for i in range(m):
+            if matrix[i][0] == 0:
+                first_col = True
+                break
 
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
-                    rows[i] = 1
-                    cols[j] = 1
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1, m):
+            if matrix[i][0] != 0: continue
+            for j in range(1, n):
+                matrix[i][j] = 0
         
-        for row in rows:
-            for j in range(n):
-                matrix[row][j] = 0
+        for j in range(1, n):
+            if matrix[0][j] != 0: continue
+            for i in range(1, m):
+                matrix[i][j] = 0
         
-        for col in cols:
-            for i in range(m):
-                matrix[i][col] = 0
+        if first_row:
+            for j in range(1, n):
+                matrix[0][j] = 0
+
+        if first_col:
+            for i in range(1, m):
+                matrix[i][0] = 0
