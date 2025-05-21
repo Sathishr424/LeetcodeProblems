009921dbf64013c4ff05682ee0a87e4cc9a586ce
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-21 06:07:25 +0530

    File updated at 21/5/2025, 6:07:18 am

--- 2145-CounttheHiddenSequences/2145-CounttheHiddenSequences.py
+++ 2145-CounttheHiddenSequences/2145-CounttheHiddenSequences.py
@@ -1,19 +1,13 @@
-# Last updated: 21/5/2025, 6:06:49 am
-cmin = lambda x, y: x if x < y else y
-cmax = lambda x, y: x if x > y else y
+# Last updated: 21/5/2025, 6:07:18 am
 class Solution:
     def numberOfArrays(self, diff: List[int], lower: int, upper: int) -> int:
-        mini = lower
-        maxi = lower
-        
-        prev = lower
+        ret = 0
+        arr = [lower]
         for num in diff:
-            curr = prev+num
-            mini = cmin(mini, curr)
-            maxi = cmax(maxi, curr)
-            prev = curr
+            arr.append(arr[-1]+num)
         
-        diff = lower-mini
-        maxi += diff
+        mini = min(arr)
+        maxi = max(arr)
+        maxi += lower-mini
         
-        return cmax(0, upper-maxi+1)
+        return max(0, upper-maxi+1)
