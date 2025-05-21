42c8f37ab55a41e4e61e9cbb3dd8a38f57ab55bc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-22 01:33:57 +0530

    File updated at 22/5/2025, 1:33:49 am

--- 1508-RangeSumofSortedSubarraySums/1508-RangeSumofSortedSubarraySums.py
+++ 1508-RangeSumofSortedSubarraySums/1508-RangeSumofSortedSubarraySums.py
@@ -1,4 +1,4 @@
-# Last updated: 22/5/2025, 1:30:34 am
+# Last updated: 22/5/2025, 1:33:49 am
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
         m = len(nums1)
@@ -17,15 +17,11 @@ class Solution:
             if key in visited: continue
             ret.append([nums1[l], nums2[r]])
             visited[key] = 1
+            
             if r+1 < n:
                 heapq.heappush(heap, (nums1[l] + nums2[r+1], l, r+1))
-            elif l+1 < m:
-                heapq.heappush(heap, (nums1[l+1] + nums2[r], l+1, r))
-            
             if l+1 < m:
                 heapq.heappush(heap, (nums1[l+1] + nums2[r], l+1, r))
-            elif r+1 < n:
-                heapq.heappush(heap, (nums1[l] + nums2[r+1], l, r+1))
             
             k -= 1
         
