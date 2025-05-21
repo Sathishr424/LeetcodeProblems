80f943c6b4c01c389a5acc8a29cbcaccd028dbdd
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-21 22:23:21 +0530

    File updated at 21/5/2025, 10:23:14 pm

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -1,38 +1,44 @@
-# Last updated: 21/5/2025, 10:12:28 pm
+# Last updated: 21/5/2025, 10:23:14 pm
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeSort(self, left, right):
-        new_list = ListNode()
-        head = new_list
-
-        while left and right:
-            if left.val < right.val:
-                new_list.next = ListNode(left.val)
-                left = left.next
-            else:
-                new_list.next = ListNode(right.val)
-                right = right.next
-            new_list = new_list.next
-        
-        if left: new_list.next = left
-        else: new_list.next = right
-        return head.next
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        n = len(lists)
-        if n == 0: return None
-        elif n == 1: return lists[0]
+        new_lists = []
+        for l in lists:
+            if l: new_lists.append(l)
+        
+        new_lists.sort(key=lambda x: x.val)
+        lists = new_lists
+        if len(lists) == 0: return None
 
+        def mergeSort(left, right):
+            new_list = ListNode()
+            head = new_list
+
+            while left and right:
+                if left.val < right.val:
+                    new_list.next = ListNode(left.val)
+                    left = left.next
+                else:
+                    new_list.next = ListNode(right.val)
+                    right = right.next
+                new_list = new_list.next
+            
+            if left: new_list.next = left
+            else: new_list.next = right
+            return head.next
+        
         while len(lists) > 1:
-            right = lists.pop()
-            lists[-1] = self.mergeSort(lists[-1], right)
-            index = random.randrange(0, len(lists))
-            lists[index], lists[-1] = lists[-1], lists[index]
+            new_lists = []
+
+            while len(lists) > 1:
+                new_lists.append(mergeSort(lists.pop(), lists.pop()))
+            if lists:
+                new_lists.append(lists[0])
+            
+            lists = new_lists
         
         return lists[0]
-        
-        
