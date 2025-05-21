6939d06b89c8b585cae180a86f64e71583fef7bc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-21 21:27:53 +0530

    Create file

---
+++ 1508-RangeSumofSortedSubarraySums/1508-RangeSumofSortedSubarraySums.py
@@ -0,0 +1,13 @@
+# Last updated: 21/5/2025, 9:27:46 pm
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        mod = 10**9 + 7
+        sums = []
+        for i in range(n):
+            s = 0
+            for j in range(i, n):
+                s += nums[j]
+                sums.append(s)
+        sums.sort()
+
+        return sum(sums[left-1:right]) % mod
