05e73649af685c9260f732c22f9230d224b92994
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-22 23:31:12 +0530

    File updated at 22/5/2025, 11:31:11 pm

--- 3459-LengthofLongestV-ShapedDiagonalSegment/3459-LengthofLongestV-ShapedDiagonalSegment.py
+++ 3459-LengthofLongestV-ShapedDiagonalSegment/3459-LengthofLongestV-ShapedDiagonalSegment.py
@@ -1,4 +1,4 @@
-# Last updated: 22/5/2025, 11:28:41 pm
+# Last updated: 22/5/2025, 11:31:11 pm
 class Solution:
     def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
         m = len(grid)
@@ -24,13 +24,15 @@ class Solution:
             i2 += i
             j2 += j
 
-            if 0 <= i2 < m and 0 <= j2 < n and (grid[i2][j2] != grid[i][j] and grid[i2][j2] != 1):
+            next_ = 0 if grid[i][j] == 2 else 2
+
+            if 0 <= i2 < m and 0 <= j2 < n and grid[i2][j2] == next_:
                 ans = max(ans, dfs(i2, j2, d, t) + 1)
             if t == 0:
                 i2, j2 = DIR[DIR_hash[DIR[d]]]
                 i2 += i
                 j2 += j
-                if 0 <= i2 < m and 0 <= j2 < n and (grid[i2][j2] != grid[i][j] and grid[i2][j2] != 1):
+                if 0 <= i2 < m and 0 <= j2 < n and grid[i2][j2] == next_:
                     ans = max(ans, dfs(i2, j2, DIR_hash[DIR[d]], t+1) + 1)
             
             # visited[i][j][d][t] = ans
