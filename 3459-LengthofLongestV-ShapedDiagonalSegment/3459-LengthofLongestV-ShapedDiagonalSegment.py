4e7d8d55d12e9d3e06c8353cdd98211f3c7b4279
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-23 00:47:27 +0530

    File updated at 23/5/2025, 12:47:26 am

--- 3459-LengthofLongestV-ShapedDiagonalSegment/3459-LengthofLongestV-ShapedDiagonalSegment.py
+++ 3459-LengthofLongestV-ShapedDiagonalSegment/3459-LengthofLongestV-ShapedDiagonalSegment.py
@@ -1,4 +1,5 @@
-# Last updated: 23/5/2025, 12:03:42 am
+# Last updated: 23/5/2025, 12:47:26 am
+cmax = lambda x, y: x if x > y else y
 class Solution:
     def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
         m = len(grid)
@@ -23,13 +24,13 @@ class Solution:
             next_ = 0 if grid[i][j] == 2 else 2
 
             if 0 <= i2 < m and 0 <= j2 < n and grid[i2][j2] == next_:
-                ans = max(ans, dfs(i2, j2, d * 2 + t) + 1)
+                ans = cmax(ans, dfs(i2, j2, d * 2 + t) + 1)
             if t == 0:
                 i2, j2 = DIR[DIR_hash[d]]
                 i2 += i
                 j2 += j
                 if 0 <= i2 < m and 0 <= j2 < n and grid[i2][j2] == next_:
-                    ans = max(ans, dfs(i2, j2, DIR_hash[d] * 2 + 1) + 1)
+                    ans = cmax(ans, dfs(i2, j2, DIR_hash[d] * 2 + 1) + 1)
             
             visited[i][j][z] = ans
             return ans
@@ -38,12 +39,12 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] != 1: continue
-                ret = max(ret, 1)
+                ret = cmax(ret, 1)
                 for d, (i2, j2) in enumerate(DIR):
                     i2 += i
                     j2 += j
 
                     if 0 <= i2 < m and 0 <= j2 < n and grid[i2][j2] == 2:
-                        ret = max(ret, dfs(i2, j2, d * 2) + 1)
+                        ret = cmax(ret, dfs(i2, j2, d * 2) + 1)
 
         return ret
