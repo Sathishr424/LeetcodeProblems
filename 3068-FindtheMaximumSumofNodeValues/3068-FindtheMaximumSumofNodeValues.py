f064912e02d7cc92fb6a2a1b003bfffc58a0a376
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-23 11:01:24 +0530

    File updated at 23/5/2025, 11:01:24 am

--- 3068-FindtheMaximumSumofNodeValues/3068-FindtheMaximumSumofNodeValues.py
+++ 3068-FindtheMaximumSumofNodeValues/3068-FindtheMaximumSumofNodeValues.py
@@ -1,4 +1,4 @@
-# Last updated: 23/5/2025, 10:59:48 am
+# Last updated: 23/5/2025, 11:01:24 am
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]], second=0) -> int:
         n = len(nums)
@@ -9,30 +9,30 @@ class Solution:
         for num in nums:
             xnum = num ^ k
             if xnum > num:
-                right.append((num, xnum))
+                right.append(num)
             else:
-                left.append((num, xnum))
+                left.append(num)
         
-        left.sort(key=lambda x: x[0] - x[1])
-        right.sort(key=lambda x: x[1] - x[0])
+        left.sort(key=lambda x: x - (x ^ k))
+        right.sort(key=lambda x: (x ^ k) - x)
 
         ret = 0
-        for x, _ in left:
+        for x in left:
             ret += x
         
         while len(right) > 1:
-            ret += right.pop()[1]
-            ret += right.pop()[1]
+            ret += right.pop() ^ k
+            ret += right.pop() ^ k
 
         new_ret = ret
         if right:
-            ret += right[0][0]
+            ret += right[0]
             if left:
-                new_ret -= left[0][0]
-                new_ret += left[0][1]
+                new_ret -= left[0]
+                new_ret += left[0] ^ k
 
-                new_ret += right[0][1]
+                new_ret += right[0] ^ k
             else:
-                new_ret += right[0][0]
+                new_ret += right[0]
         
         return max(new_ret, ret)
