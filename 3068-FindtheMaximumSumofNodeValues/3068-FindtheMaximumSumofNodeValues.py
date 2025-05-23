be23647f3e1f3ba60f4ecba97059e97da4db4282
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-23 10:59:48 +0530

    File updated at 23/5/2025, 10:59:48 am

--- 3068-FindtheMaximumSumofNodeValues/3068-FindtheMaximumSumofNodeValues.py
+++ 3068-FindtheMaximumSumofNodeValues/3068-FindtheMaximumSumofNodeValues.py
@@ -1,50 +1,38 @@
-# Last updated: 23/5/2025, 10:53:50 am
+# Last updated: 23/5/2025, 10:59:48 am
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]], second=0) -> int:
         n = len(nums)
         
-        heap = []
+        left = []
+        right = []
+
         for num in nums:
-            heapq.heappush(heap, (-((num ^ k) - num), num ^ k, num))
-        # print(heap)
-        ret = 0
-        later = []
-        another = []
-        while heap:
-            while heap and heap[0][1] < heap[0][2]:
-                later.append(heapq.heappop(heap)[1:])
-            if not heap: break
-
-            x = heapq.heappop(heap)
-            while heap and heap[0][1] < heap[0][2]:
-                later.append(heapq.heappop(heap)[1:])
-            
-            if not heap:
-                another.append(x[1:])
-                break
-            
-            y = heapq.heappop(heap)
-            ret += x[1]
-            ret += y[1]
-        
-        later.sort(key=lambda x: x[1] - x[0])
-        
-        # print(ret)
-        for xnum, num in later:
-            ret += num
-        
-        # print(later)
-        # print(another)
-        new_ret = ret
-        if another:
-            ret += another[0][1]
-            if later:
-                new_ret -= later[0][1]
-                new_ret += later[0][0]
-
-                new_ret += another[0][0]
+            xnum = num ^ k
+            if xnum > num:
+                right.append((num, xnum))
             else:
-                new_ret += another[0][1]
+                left.append((num, xnum))
+        
+        left.sort(key=lambda x: x[0] - x[1])
+        right.sort(key=lambda x: x[1] - x[0])
 
+        ret = 0
+        for x, _ in left:
+            ret += x
+        
+        while len(right) > 1:
+            ret += right.pop()[1]
+            ret += right.pop()[1]
 
-        return max(ret, new_ret)
+        new_ret = ret
+        if right:
+            ret += right[0][0]
+            if left:
+                new_ret -= left[0][0]
+                new_ret += left[0][1]
+
+                new_ret += right[0][1]
+            else:
+                new_ret += right[0][0]
+        
+        return max(new_ret, ret)
