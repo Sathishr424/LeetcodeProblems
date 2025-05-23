d31ea7f9d9a16315b67d0b54db1df26ec48e7672
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-23 18:37:19 +0530

    File updated at 23/5/2025, 6:37:18 pm

--- 3463-CheckIfDigitsAreEqualinStringAfterOperationsII/3463-CheckIfDigitsAreEqualinStringAfterOperationsII.py
+++ 3463-CheckIfDigitsAreEqualinStringAfterOperationsII/3463-CheckIfDigitsAreEqualinStringAfterOperationsII.py
@@ -1,9 +1,9 @@
-# Last updated: 23/5/2025, 6:35:56 pm
+# Last updated: 23/5/2025, 6:37:18 pm
 class Solution:
     def hasSameDigits(self, s: str) -> bool:
         n = len(s)
+
         def lucas(row, col):
-            print(row, col)
             p_5 = getCoeff(row, col, 5)
             p_2 = getCoeff(row, col, 2)
 
@@ -16,10 +16,8 @@ class Solution:
             while row and col:
                 r = row % mod
                 c = col % mod
-                if r >= c:
-                    ans *= factorial(r) // (factorial(c) * factorial(r - c))
-                else:
-                    return 0
+                if r < c: return 0
+                ans *= factorial(r) // (factorial(c) * factorial(r - c))
      
                 ans %= mod
                 row //= mod
@@ -30,6 +28,7 @@ class Solution:
         row = n-2
         left = int(s[0])
         right = int(s[-1])
+
         for col in range(1, n-1):
             e = lucas(row, col)
             left = (left + int(s[col]) * e % 10) % 10
