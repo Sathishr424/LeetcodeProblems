3b65fbd36c6258700b5a97c7d37c4e93620c303e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-24 15:57:02 +0530

    File updated at 24/5/2025, 3:56:59 pm

--- 2942-FindWordsContainingCharacter/2942-FindWordsContainingCharacter.py
+++ 2942-FindWordsContainingCharacter/2942-FindWordsContainingCharacter.py
@@ -1,11 +1,12 @@
-# Last updated: 24/5/2025, 3:56:21 pm
+# Last updated: 24/5/2025, 3:56:59 pm
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         ret = []
         for i, word in enumerate(words):
-            index = random.randrange(len(word))
-            for j in range(index, index+len(word)):
-                if word[j  % len(word)] == x:
+            m = len(word)
+            index = random.randrange(m)
+            for j in range(index, index+m):
+                if word[j  % m] == x:
                     ret.append(i)
                     break
         
