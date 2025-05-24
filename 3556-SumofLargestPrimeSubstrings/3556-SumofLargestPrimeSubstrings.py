b2dfde347abb2a06c52819c34c6966cbf8afdefe
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-24 22:19:46 +0530

    File updated at 24/5/2025, 10:19:46 pm

--- 3556-SumofLargestPrimeSubstrings/3556-SumofLargestPrimeSubstrings.py
+++ 3556-SumofLargestPrimeSubstrings/3556-SumofLargestPrimeSubstrings.py
@@ -1,18 +1,18 @@
-# Last updated: 24/5/2025, 10:19:27 pm
+# Last updated: 24/5/2025, 10:19:46 pm
+@cache
+def isPrime(num):
+    if num == 1: return False
+    if num == 2: return True
+    if num == 3: return True
+    if num % 2 == 0: return False
+    
+    for i in range(3, int(num ** 0.5) + 1, 2):
+        if num % i == 0: return False
+    
+    return True
+
 class Solution:
     def sumOfLargestPrimes(self, s: str) -> int:
-        @cache
-        def isPrime(num):
-            if num == 1: return False
-            if num == 2: return True
-            if num == 3: return True
-            if num % 2 == 0: return False
-            
-            for i in range(3, int(num ** 0.5) + 1, 2):
-                if num % i == 0: return False
-            
-            return True
-            
         n = len(s)
         primes = {}
         for i in range(n):
