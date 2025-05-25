f227e15b93f2e668fab239ce9d7f157177347b3c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-25 11:31:23 +0530

    File updated at 25/5/2025, 11:31:23 am

--- 2131-LongestPalindromebyConcatenatingTwoLetterWords/2131-LongestPalindromebyConcatenatingTwoLetterWords.py
+++ 2131-LongestPalindromebyConcatenatingTwoLetterWords/2131-LongestPalindromebyConcatenatingTwoLetterWords.py
@@ -1,8 +1,8 @@
-# Last updated: 25/5/2025, 11:30:43 am
+# Last updated: 25/5/2025, 11:31:23 am
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         there = defaultdict(int)
-
+        
         for word in words:
             there[word] += 1
         
@@ -10,6 +10,7 @@ class Solution:
         odd = False
 
         for word in there:
+            if there[word] == 0: continue
             reverse_word = word[::-1]
             if word[0] == word[1]:
                 odd = odd or (there[word] % 2 == 1)
