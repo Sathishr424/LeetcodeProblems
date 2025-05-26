bb530feb6008c354da0a14b2e95554d4333beade
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-26 23:14:05 +0530

    Create 3563-LexicographicallySmallestStringAfterAdjacentRemovals

---
+++ 3563-LexicographicallySmallestStringAfterAdjacentRemovals/3563-LexicographicallySmallestStringAfterAdjacentRemovals.py
@@ -0,0 +1,37 @@
+# Last updated: 26/5/2025, 11:14:04 pm
+# abcdefghihklmnoopqrstwxyz
+@cache
+def isPair(x, y):
+    x = ord(x) - 97
+    y = ord(y) - 97
+
+    if (x + 1) % 26 == y or (x - 1) % 26 == y:
+        return True
+    return False
+
+class Solution:
+    def lexicographicallySmallestString(self, s: str) -> str:
+        n = len(s)     
+        @cache
+        def dfs(i, j):
+            if i == j: return False
+            elif i > j: return True
+            if isPair(s[i], s[j]) and dfs(i+1, j-1):
+                return True
+            for k in range(i+1, j, 2):
+                if dfs(i, k) and dfs(k+1, j): return True
+            return False
+
+        @cache
+        def rec(index):
+            if index == n: return ''
+
+            ans = s[index] + rec(index+1)
+
+            for i in range(index+1, n, 2):
+                if dfs(index, i):
+                    ans = min(ans, rec(i+1))
+            
+            return ans
+        
+        return rec(0)
