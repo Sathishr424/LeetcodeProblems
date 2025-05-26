85309e1455c6b0c6608e58eeaf4d99bdafd71ec9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-26 13:43:09 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.py
@@ -0,0 +1,21 @@
+# Last updated: 26/5/2025, 1:43:07 pm
+cmax = lambda x, y: x if x > y else y
+cmin = lambda x, y: x if x < y else y
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+        ret = nums[0]
+
+        mn = ret
+        mx = ret
+
+        for i in range(1, n):
+            new_mn = nums[i] * mn
+            new_mx = nums[i] * mx
+            mn = min(nums[i], new_mn, new_mx)
+            mx = max(nums[i], new_mn, new_mx)
+            
+            ret = max(mx, ret)
+        
+        return ret
+        
