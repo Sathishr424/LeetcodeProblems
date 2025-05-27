35969be88decfb8f6f91950deeed40a8065b0a2c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-27 06:19:17 +0530

    File updated at 27/5/2025, 6:19:16 am

--- 2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.py
+++ 2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.py
@@ -1,4 +1,4 @@
-# Last updated: 27/5/2025, 6:17:53 am
+# Last updated: 27/5/2025, 6:19:16 am
 class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
-        return n * (n+1) // 2 - n // m * (n // m+1) // 2 * m * 2
+        return n * (n+1) // 2 - n // m * (n // m+1) * m
