142b6ae7fb1ec79eab46029b6159398f88f4c069
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-28 03:23:07 +0530

    File updated at 28/5/2025, 3:23:05 am

--- 31-NextPermutation/31-NextPermutation.py
+++ 31-NextPermutation/31-NextPermutation.py
@@ -1,22 +1,39 @@
-# Last updated: 14/4/2025, 5:28:30 pm
+# Last updated: 28/5/2025, 3:23:05 am
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        start = 0
-        end = n-1
+        if len(nums) == 1: return nums
+        # 1, 2, 3, 4, 5
+        # 1, 2, 3, 5, 4
 
+        # 1, 3, 4, 5, 2
+        # 1, 3, 2, 4, 5
+
+        # 1, 4, 2, 3, 5
+        # 1, 4, 2, 5, 3 => 3, 2, 5
+        # 1, 4, 3, 2, 5
+        # 1, 4, 3, 5, 2
+        # 1, 4, 5, 2, 3
+        # 1, 4, 5, 3, 2 => 5, 4, 3, 2
+        # 1, 5, 2, 3, 4
+
+        # 3, 6, 5, 4, 2, 1
+
+        # 1, 5, 4, 3, 2
+
+        n = len(nums)
+        index = 0
         for i in range(n-2, -1, -1):
             if nums[i] < nums[i+1]:
-                for j in range(n-1, i, -1):
+                for j in range(n-1, -1, -1):
                     if nums[j] > nums[i]:
                         nums[i], nums[j] = nums[j], nums[i]
-                        start = i+1
                         break
+                index = i+1
                 break
         
-        for k in range(start, (start+end)//2 + 1):
-            nums[k], nums[end] = nums[end], nums[k]
-            end -= 1    
+        left = index
+        right = n-1
+        for i in range((right-left+1)//2):
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
