a52a9955cc1c6aa30f614c3d6ea0121b7f2e68f3
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-27 16:39:08 +0530

    File updated at 27/5/2025, 4:39:06 pm

--- 3469-FindMinimumCosttoRemoveArrayElements/3469-FindMinimumCosttoRemoveArrayElements.py
+++ 3469-FindMinimumCosttoRemoveArrayElements/3469-FindMinimumCosttoRemoveArrayElements.py
@@ -1,4 +1,5 @@
-# Last updated: 27/5/2025, 4:38:04 pm
+# Last updated: 27/5/2025, 4:39:06 pm
+cmax = lambda x, y: x if x > y else y
 class Solution:
     def minCost(self, nums: List[int]) -> int:
         # 1 2 3 4 5 6 7
@@ -13,15 +14,15 @@ class Solution:
             if n-index < 2:
                 return max([nums[prev_index]] + nums[index:])
             
-            one = dfs(index+2, index) + max(nums[prev_index], nums[index+1])
-            two = dfs(index+2, prev_index) + max(nums[index], nums[index+1])
-            three = dfs(index+2, index+1) + max(nums[prev_index], nums[index])
+            one = dfs(index+2, index) + cmax(nums[prev_index], nums[index+1])
+            two = dfs(index+2, prev_index) + cmax(nums[index], nums[index+1])
+            three = dfs(index+2, index+1) + cmax(nums[prev_index], nums[index])
             ans = min(one, two, three)
             memo[index][prev_index] = ans
             return ans
     
-        one = dfs(3, 0) + max(nums[1], nums[2])
-        two = dfs(3, 1) + max(nums[0], nums[2])
-        three = dfs(3, 2) + max(nums[0], nums[1])
+        one = dfs(3, 0) + cmax(nums[1], nums[2])
+        two = dfs(3, 1) + cmax(nums[0], nums[2])
+        three = dfs(3, 2) + cmax(nums[0], nums[1])
 
         return min(one, two, three)
