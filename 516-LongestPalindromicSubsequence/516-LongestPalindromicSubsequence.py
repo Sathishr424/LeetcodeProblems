7f564fdd226c3856711bdf6af325d7c4ff563f88
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-31 23:18:19 +0530

    File updated at 31/5/2025, 11:18:18 pm

--- 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
@@ -1,13 +1,17 @@
-# Last updated: 31/5/2025, 10:09:37 pm
+# Last updated: 31/5/2025, 11:18:18 pm
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        @cache
-        def dfs(l, r):
-            if l == r: return 1
-            elif l > r: return 0
+        n = len(s)
+        dp = [[0] * (n+2) for _ in range(n+2)]
+        ret = 1
 
-            if s[l] == s[r]:
-                return dfs(l+1, r-1) + 2
-            return max(dfs(l+1, r), dfs(l, r-1))
-        
-        return dfs(0, len(s)-1)
+        for i in range(1, n+1):
+            for j in range(n, i-1, -1):
+                if s[i-1] == s[j-1]:
+                    # print(i, j, (i-1, j-1), dp[i-1])
+                    dp[i][j] = dp[i-1][j+1] + (1 if i == j else 2)
+                    ret = max(ret, dp[i][j])
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j+1])
+                
+        return ret
