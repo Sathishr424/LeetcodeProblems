c6b57b047cf4de1aed8c76167bee18249cbbe13f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-31 21:51:20 +0530

    File updated at 31/5/2025, 9:51:20 pm

--- 3470-PermutationsIV/3470-PermutationsIV.py
+++ 3470-PermutationsIV/3470-PermutationsIV.py
@@ -1,40 +1,48 @@
-# Last updated: 21/4/2025, 12:24:40 am
+# Last updated: 31/5/2025, 9:51:20 pm
 @cache
-def fact(x, y):
-    if x == 0 or y == 0: return 1
-    return x * fact(y, x-1)
+def getComb(x, y):
+    if x == 0 and y == 0: return 1
+    elif x == 0: return 0
+    return x * getComb(y, x-1)
 
 class Solution:
     def permute(self, n: int, k: int) -> List[int]:
-        def perm(left, right, k):
-            for i, num in enumerate(left):
-                f = fact(len(right), len(left)-1)
-                if f >= k:
-                    return [num] + perm(right, left[:i] + left[i+1:], k)
-                k -= f
-            return []
-        
+        # 1, 2, 3, 4 => 2, 2
+        # 4, 4
         odds = []
         evens = []
-        nums = []
+
         for i in range(1, n+1):
-            nums.append(i)
-            if i % 2: odds.append(i)
-            else: evens.append(i)
+            if i % 2:
+                odds.append(i)
+            else:
+                evens.append(i)
         
-        if n % 2:
-            return perm(odds, evens, k)
-        else:
-            for i, num in enumerate(nums):
-                if num % 2:
-                    f = fact(len(evens), len(odds)-1)
-                else:
-                    f = fact(len(odds), len(evens)-1)
-                
-                if f >= k:
-                    if num % 2: odds, evens = evens, odds
-                    i = i // 2
-                    return [num] + perm(odds, evens[:i] + evens[i+1:], k)
-                k -= f
+        def dfs(evens, odds, k):
+            f = getComb(len(odds), len(evens)-1)
+            p = 0
+            for i in range(len(evens)):
+                if p+f >= k:
+                    return [evens[i]] + dfs(odds, evens[:i] + evens[i+1:], k-p)
+                p += f
+            
+            return []
         
-        return []
+        p = 0
+        ret = []
+        for i in range(1, n+1):
+            half = (i-1)//2
+            if i % 2 == 0:
+                f = getComb(len(odds), len(evens)-1)
+                if f + p >= k:
+                    ret = [evens[half]] + dfs(odds, evens[:half] + evens[half+1:], k-p)
+                    break
+            else:
+                f = getComb(len(evens), len(odds)-1)
+                if f + p >= k:
+                    odds, evens = evens, odds
+                    ret = [evens[half]] + dfs(odds, evens[:half] + evens[half+1:], k-p)
+                    break
+            p += f
+
+        return ret
