acf4cb86defb466c1fc1e29b4385fe9864ca8a43
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-31 23:30:44 +0530

    File updated at 31/5/2025, 11:30:44 pm

--- 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
@@ -1,17 +1,20 @@
-# Last updated: 31/5/2025, 11:20:21 pm
+# Last updated: 31/5/2025, 11:30:44 pm
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)
-        dp = [[0] * (n+2) for _ in range(n+2)]
+        dp = [0] * (n+2)
         ret = 1
 
         for i in range(1, n+1):
+            prev = dp[n+1]
             for j in range(n, i-1, -1):
+                curr = dp[j]
                 if s[i-1] == s[j-1]:
-                    dp[i][j] = dp[i-1][j+1] + (1 if i == j else 2)
-                    ret = cmax(ret, dp[i][j])
+                    dp[j] = prev + (1 if i == j else 2)
+                    ret = cmax(ret, dp[j])
                 else:
-                    dp[i][j] = cmax(dp[i-1][j], dp[i][j+1])
+                    dp[j] = cmax(dp[j+1], dp[j])
+                prev = curr
                 
         return ret
