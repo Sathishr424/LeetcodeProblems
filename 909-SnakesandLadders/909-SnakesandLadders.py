d154cde1fcfa3f58e68de3e4db45fab659945fa4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-05-31 15:24:02 +0530

    File updated at 31/5/2025, 3:24:02 pm

--- 909-SnakesandLadders/909-SnakesandLadders.py
+++ 909-SnakesandLadders/909-SnakesandLadders.py
@@ -1,15 +1,11 @@
-# Last updated: 31/5/2025, 3:23:03 pm
-cmin = lambda x, y: x if x < y else y
+# Last updated: 31/5/2025, 3:24:02 pm
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
         area = n * n
 
-        stack = [0]
-        pos = 0
-
         grid = []
-        moves = 0
+        pos = 0
 
         def addToGrid(i, j):
             if board[i][j] == -1:
@@ -27,17 +23,24 @@ class Solution:
                     addToGrid(i, j)
                     pos += 1
         
+        stack = [0]
+        moves = 0
+        visited = [0] * area
         while stack:
             new_stack = []
             for pos in stack:
-                for new_pos in range(pos+1, cmin(n*n, pos+7)):
-                    if grid[new_pos] == -1: continue
-                    
+                for roll in range(1, 7):
+                    new_pos = pos + roll
+                    if new_pos >= area: break
+                    if visited[new_pos]: continue
                     if grid[new_pos] == area-1: return moves + 1
+                    visited[new_pos] = 1
+
                     new_stack.append(grid[new_pos])
-                    grid[new_pos] = -1
-            
             stack = new_stack
             moves += 1
         
         return -1
+
+                
+
