70464f40908ba77d807bb1cbf4a4ba82fb5e35e2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-01 19:34:09 +0530

    File updated at 1/6/2025, 7:34:08 pm

--- 2929-DistributeCandiesAmongChildrenII/2929-DistributeCandiesAmongChildrenII.py
+++ 2929-DistributeCandiesAmongChildrenII/2929-DistributeCandiesAmongChildrenII.py
@@ -1,4 +1,4 @@
-# Last updated: 1/6/2025, 7:33:05 pm
+# Last updated: 1/6/2025, 7:34:08 pm
 cmin = lambda x, y: x if x < y else y
 cmax = lambda x, y: x if x > y else y
 class Solution:
@@ -7,16 +7,14 @@ class Solution:
 
         start = cmax(0, n - (limit * 2))
         end = cmin(n, limit)
+
         ret = 0
+
         for i in range(start, end+1):
             rem = n - i
             s = cmax(0, rem - limit)
             e = cmin(rem, limit)
 
-            diff = cmin(limit - s, e) + 1
-
-            ret += diff
+            ret += cmin(limit - s, e) + 1
 
         return ret
-
-        
