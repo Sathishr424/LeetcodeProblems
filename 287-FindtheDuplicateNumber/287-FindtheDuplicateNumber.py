033a50ebc5b71e2374caefd497d9d136c93c5920
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-01 22:47:48 +0530

    File updated at 1/6/2025, 10:47:47 pm

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
@@ -1,30 +1,22 @@
-# Last updated: 1/6/2025, 9:01:39 pm
+# Last updated: 1/6/2025, 10:47:47 pm
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         n = len(nums)
         
         l = int(log2(n-1)) + 1
-        
-        cnt = [0] * l
-        for num in nums:
-            index = l-1
-            while num:
-                cnt[index] += num % 2
-                num //= 2
-                index -= 1
-
-        for num in range(1, n):
-            index = l-1
-            while num:
-                cnt[index] -= num % 2
-                num //= 2
-                index -= 1
-
         ret = 0
-        index = 0
-        while cnt:
-            if cnt.pop() > 0:
-                ret += 2 ** index
-            index += 1
+        
+        for bit in range(l+1):
+            cnt = 0
+            for num in nums:
+                if num & (1 << bit):
+                    cnt += 1
+            
+            for num in range(1, n):
+                if num & (1 << bit):
+                    cnt -= 1
+            
+            if cnt > 0:
+                ret += 2 ** bit
         
         return ret
