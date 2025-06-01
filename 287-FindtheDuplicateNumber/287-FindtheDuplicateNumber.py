39781408c1001b41d3b6eeb6cd75a0cfe409245a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-01 22:51:45 +0530

    File updated at 1/6/2025, 10:51:44 pm

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
@@ -1,4 +1,4 @@
-# Last updated: 1/6/2025, 10:47:47 pm
+# Last updated: 1/6/2025, 10:51:44 pm
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         n = len(nums)
@@ -9,14 +9,12 @@ class Solution:
         for bit in range(l+1):
             cnt = 0
             for num in nums:
-                if num & (1 << bit):
-                    cnt += 1
+                cnt += num & (1 << bit)
             
             for num in range(1, n):
-                if num & (1 << bit):
-                    cnt -= 1
+                if cnt == 0: break
+                cnt -= num & (1 << bit)
             
-            if cnt > 0:
-                ret += 2 ** bit
+            if cnt: ret += 1 << bit
         
         return ret
