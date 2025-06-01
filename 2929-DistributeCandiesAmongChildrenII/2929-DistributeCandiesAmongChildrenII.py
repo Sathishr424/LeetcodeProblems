d41718fcfc1e63c77b78bf2f0a357ccb50934c19
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-01 19:41:31 +0530

    File updated at 1/6/2025, 7:41:30 pm

--- 2929-DistributeCandiesAmongChildrenII/2929-DistributeCandiesAmongChildrenII.py
+++ 2929-DistributeCandiesAmongChildrenII/2929-DistributeCandiesAmongChildrenII.py
@@ -1,15 +1,13 @@
-# Last updated: 1/6/2025, 7:40:08 pm
+# Last updated: 1/6/2025, 7:41:30 pm
 cmin = lambda x, y: x if x < y else y
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
         start = cmax(0, n - (limit * 2))
         end = cmin(n, limit)
-
         ret = 0
 
-        for i in range(start, end+1):
-            rem = n - i
+        for rem in range(n-start, n-end-1, -1):
             s = cmax(0, rem - limit)
             e = cmin(rem, limit)
 
