c68274745a371c6df6833d8473aa6db46a21e0fd
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-02 13:11:22 +0530

    File updated at 2/6/2025, 1:11:20 pm

--- 135-Candy/135-Candy.py
+++ 135-Candy/135-Candy.py
@@ -1,4 +1,4 @@
-# Last updated: 2/6/2025, 1:08:23 pm
+# Last updated: 2/6/2025, 1:11:20 pm
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
@@ -6,21 +6,19 @@ class Solution:
         left = [0] * n
         right = [0] * n
 
-        stack = [1, ratings[0]]
+        cnt = 1
         for i in range(1, n):
-            if stack[-1] >= ratings[i]:
-                stack = [0, -1]
-            left[i] = stack[0]
-            stack[0] += 1
-            stack[-1] = ratings[i]
+            if ratings[i-1] >= ratings[i]:
+                cnt = 0
+            left[i] = cnt
+            cnt += 1
 
-        stack = [1, ratings[-1]]
+        cnt = 1
         for i in range(n-2, -1, -1):
-            if stack[-1] >= ratings[i]:
-                stack = [0, -1]
-            right[i] = stack[0]
-            stack[0] += 1
-            stack[-1] = ratings[i]
+            if ratings[i+1] >= ratings[i]:
+                cnt = 0
+            right[i] = cnt
+            cnt += 1
 
         ret = 0
         for i in range(n):
