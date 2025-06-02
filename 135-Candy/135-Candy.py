5545fa9e7d1ca5eea4a39fccd589c69ec3e9b30e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-02 13:14:24 +0530

    File updated at 2/6/2025, 1:14:22 pm

--- 135-Candy/135-Candy.py
+++ 135-Candy/135-Candy.py
@@ -1,27 +1,24 @@
-# Last updated: 2/6/2025, 1:11:20 pm
+# Last updated: 2/6/2025, 1:14:22 pm
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
 
-        left = [0] * n
-        right = [0] * n
+        candies = [0] * n
 
         cnt = 1
         for i in range(1, n):
             if ratings[i-1] >= ratings[i]:
                 cnt = 0
-            left[i] = cnt
+            candies[i] = cnt
             cnt += 1
 
         cnt = 1
+        ret = candies[-1] + 1
         for i in range(n-2, -1, -1):
             if ratings[i+1] >= ratings[i]:
                 cnt = 0
-            right[i] = cnt
+            candies[i] = max(candies[i], cnt)
+            ret += candies[i] + 1
             cnt += 1
-
-        ret = 0
-        for i in range(n):
-            ret += max(left[i], right[i]) + 1
 
         return ret
