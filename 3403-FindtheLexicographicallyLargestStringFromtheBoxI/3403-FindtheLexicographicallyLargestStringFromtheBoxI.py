ad8f67c017f8caac5b15bd860e19ef9efd5e368c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-04 02:23:48 +0530

    File updated at 4/6/2025, 2:23:44 am

--- 3403-FindtheLexicographicallyLargestStringFromtheBoxI/3403-FindtheLexicographicallyLargestStringFromtheBoxI.py
+++ 3403-FindtheLexicographicallyLargestStringFromtheBoxI/3403-FindtheLexicographicallyLargestStringFromtheBoxI.py
@@ -1,36 +1,38 @@
-# Last updated: 4/6/2025, 2:20:00 am
+# Last updated: 4/6/2025, 2:23:44 am
 class Solution:
     def answerString(self, word: str, numFriends: int) -> str:
         if numFriends == 1: return word
         n = len(word)
-
         indexes = []
 
         maxi = 0
         for i, char in enumerate(word):
             if char > word[maxi]:
                 maxi = i
-            
+        
         for i, char in enumerate(word):
             if char == word[maxi]:
                 indexes.append(i)
-            
+        
         split = n - (numFriends-1)
 
-        def checkMax(x, y):
-            for i in range(min(len(x), len(y))):
-                if x[i] > y[i]: return True
-                elif y[i] > x[i]: return False
+        def checkMax(i, j):
+            k = min(n, i+split)
+            l = min(n, j+split)
+            while i < k and j < l:
+                if word[i] > word[j]: return True
+                elif word[j] > word[i]: return False
+                i += 1
+                j += 1
             
             return False
 
-        index = indexes[0]
-        ret = word[index:index+split]
+        start = indexes[0]
+        
         for index in indexes[1:]:
-            curr = word[index:index+split]
-            if checkMax(curr, ret):
-                ret = curr
+            if checkMax(index, start):
+                start = index
 
-        return ret
+        return word[start:start+split]
 
             
