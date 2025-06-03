06c13d99f1e8f2b1c751d14607767ded8a19a636
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-03 11:40:30 +0530

    File updated at 3/6/2025, 11:40:27 am

--- 1298-MaximumCandiesYouCanGetfromBoxes/1298-MaximumCandiesYouCanGetfromBoxes.py
+++ 1298-MaximumCandiesYouCanGetfromBoxes/1298-MaximumCandiesYouCanGetfromBoxes.py
@@ -1,34 +1,31 @@
-# Last updated: 3/6/2025, 11:37:29 am
+# Last updated: 3/6/2025, 11:40:27 am
 class Solution:
     def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
         n = len(status)
-        opened = {}
-        yet_to_open = {}
+        ret = 0
+        yet_to_open = [0] * n
+        opened = [0] * n
 
         def openBox(box):
-            if box in opened: return
+            if opened[box]: return 0
             if status[box] == 1:
+                ans = candies[box]
                 opened[box] = 1
 
                 for b in keys[box]:
                     status[b] = 1
-                    if b in yet_to_open:
-                        openBox(b)
+                    if yet_to_open[b]:
+                        ans += openBox(b)
 
                 for b in containedBoxes[box]:
-                    openBox(b)
+                    ans += openBox(b)
+                return ans
             else:
                 yet_to_open[box] = 1
+                return 0
 
         for box in initialBoxes:
-            openBox(box)
-
-        ret = 0
-        # print(opened)
-        # print(yet_to_open)
-
-        for box in opened:
-            ret += candies[box]
+            ret += openBox(box)
         
         return ret
 
