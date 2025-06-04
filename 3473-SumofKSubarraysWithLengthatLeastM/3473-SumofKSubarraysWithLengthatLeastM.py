903c76e10c1e5687e68fbea4ea9227b2fd1b53b7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-04 22:30:37 +0530

    File updated at 4/6/2025, 10:30:37 pm

--- 3473-SumofKSubarraysWithLengthatLeastM/3473-SumofKSubarraysWithLengthatLeastM.py
+++ 3473-SumofKSubarraysWithLengthatLeastM/3473-SumofKSubarraysWithLengthatLeastM.py
@@ -1,34 +1,31 @@
-# Last updated: 4/6/2025, 10:08:27 pm
+# Last updated: 4/6/2025, 10:30:37 pm
 class Solution:
     def maxSum(self, nums: List[int], k: int, m: int) -> int:
-        prefix = []
-        p = 0
         n = len(nums)
 
+        prefix = [0]
         for num in nums:
-            p += num
-            prefix.append(p)
+            prefix.append(prefix[-1] + num)
         
-        inf = -float('inf')
-
+        inf = float('inf')
+        
         @cache
-        def rec(index, rem, extend):
-            if rem == 0 and index == n: return 0
-            if index >= n: return inf
-
-            ans = rec(index+1, rem, False)
+        def rec(index, k, expand):
+            if k == 0 and (not expand or index == n): return 0
+            # print(index, k, expand)
+            ans = -inf
+            if index+1 <= n-(k*m):
+                ans = rec(index+1, k, False)
             
-            prev = prefix[index] - nums[index]
+            if expand and index+1 <= n-(k*m):
+                ans = max(ans, rec(index+1, k, True) + nums[index])
+            if k > 0:
+                t = prefix[index+m] - prefix[index]
 
-            if extend:
-                ans = max(ans, rec(index+1, rem, (index+1)+(rem*m) < n) + nums[index])
+                ans = max(ans, rec(index+m, k-1, True) + t)
             
-            if index+m <= n:
-                ans = max(ans, rec(index+m, rem-1, (index+m)+((rem-1)*m) < n) + (prefix[index+m-1] - prev))
             return ans
         
         ans = rec(0, k, False)
         rec.cache_clear()
         return ans
-
-        
