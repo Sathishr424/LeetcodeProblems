c9916692170ee21cb36684bbc2e6303670fec0c0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-04 22:34:37 +0530

    File updated at 4/6/2025, 10:34:37 pm

--- 3473-SumofKSubarraysWithLengthatLeastM/3473-SumofKSubarraysWithLengthatLeastM.py
+++ 3473-SumofKSubarraysWithLengthatLeastM/3473-SumofKSubarraysWithLengthatLeastM.py
@@ -1,4 +1,4 @@
-# Last updated: 4/6/2025, 10:31:38 pm
+# Last updated: 4/6/2025, 10:34:37 pm
 class Solution:
     def maxSum(self, nums: List[int], k: int, m: int) -> int:
         n = len(nums)
@@ -8,24 +8,25 @@ class Solution:
             prefix.append(prefix[-1] + num)
         
         inf = float('inf')
+
+        dp = [[[-inf, -inf] for _ in range(k+1)] for _ in range(n+1)]
         
-        @cache
         def rec(index, k, expand):
-            if k == 0 and (not expand or index == n): return 0
+            if dp[index][k][expand] != -inf: return dp[index][k][expand]
+            if k == 0 and (expand == 0 or index == n): return 0
 
             ans = -inf
             if index+1 <= n-(k*m):
-                ans = rec(index+1, k, False)
+                ans = rec(index+1, k, 0)
             
             if expand and index+1 <= n-(k*m):
-                ans = max(ans, rec(index+1, k, True) + nums[index])
+                ans = max(ans, rec(index+1, k, 1) + nums[index])
             if k > 0:
                 t = prefix[index+m] - prefix[index]
 
-                ans = max(ans, rec(index+m, k-1, True) + t)
-            
+                ans = max(ans, rec(index+m, k-1, 1) + t)
+            dp[index][k][expand] = ans
             return ans
         
-        ans = rec(0, k, False)
-
+        ans = rec(0, k, 0)
         return ans
