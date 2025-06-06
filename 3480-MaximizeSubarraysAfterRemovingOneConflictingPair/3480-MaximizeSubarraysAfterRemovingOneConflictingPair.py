29706570d42f1ecdee5f3e095fb87294301ee566
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-07 03:02:28 +0530

    File updated at 7/6/2025, 3:02:25 am

--- 3480-MaximizeSubarraysAfterRemovingOneConflictingPair/3480-MaximizeSubarraysAfterRemovingOneConflictingPair.py
+++ 3480-MaximizeSubarraysAfterRemovingOneConflictingPair/3480-MaximizeSubarraysAfterRemovingOneConflictingPair.py
@@ -1,4 +1,4 @@
-# Last updated: 7/6/2025, 2:57:13 am
+# Last updated: 7/6/2025, 3:02:25 am
 class Solution:
     def maxSubarrays(self, n: int, cp: List[List[int]]) -> int:
         m = len(cp)
@@ -8,18 +8,15 @@ class Solution:
                 cp[i][0], cp[i][1] = cp[i][1], cp[i][0]
 
         cp.sort()
-        # print(cp)
         better = [0] * m
 
         start = [x for x, _ in cp]
-        
         end = deque([])
-        end.append(m-1)
-        secondEnd = deque([])
-        secondEnd.append(m)
+
+        cp.append([0, n+1])
 
         sl = [m-1, m]
-        cp.append([0, n+1])
+        end.appendleft(sl + [])
 
         for i in range(m-2, -1, -1):
             if cp[i][1] <= cp[sl[0]][1]:
@@ -28,17 +25,16 @@ class Solution:
             elif cp[i][1] < cp[sl[1]][1]:
                 sl[1] = i
             
-            end.appendleft(sl[0])
-            secondEnd.appendleft(sl[1])
-        
+            end.appendleft(sl + [])
+
         cnt = 0
         for i in range(n, 0, -1):
             index = bisect_left(start, i)
             if index < m:
-                index_2 = end[index]
+                index_2 = end[index][0]
                 diff = cp[index_2][1] - i
                 cnt += diff
-                better[index_2] += (cp[secondEnd[index]][1] - i) - diff
+                better[index_2] += (cp[ end[index][1] ][1] - i) - diff
             else:
                 cnt += n-i+1
         
