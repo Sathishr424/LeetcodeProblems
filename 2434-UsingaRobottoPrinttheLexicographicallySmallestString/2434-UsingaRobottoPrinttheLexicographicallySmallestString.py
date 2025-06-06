aef99a011f950f0446a70c94bc99462b495c6a3c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-06 13:23:43 +0530

    File updated at 6/6/2025, 1:23:41 pm

--- 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
+++ 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
@@ -1,15 +1,36 @@
-# Last updated: 6/6/2025, 1:23:25 pm
+# Last updated: 6/6/2025, 1:23:41 pm
 class Solution:
     def robotWithString(self, s: str) -> str:
-        cnt = Counter(s)
-        stack = []
-        res = []
-        minCharacter = "a"
-        for c in s:
-            stack.append(c)
-            cnt[c] -= 1
-            while minCharacter != "z" and cnt[minCharacter] == 0:
-                minCharacter = chr(ord(minCharacter) + 1)
-            while stack and stack[-1] <= minCharacter:
-                res.append(stack.pop())
-        return "".join(res)
+        n = len(s)
+        ret = []
+        t = []
+        index = 0
+        
+        freq = [0] * 26
+
+        for i in range(n):
+            a = ord(s[i]) - 97
+            freq[a] += 1
+        
+        def addItToT(to_find):
+            nonlocal index, ret
+            for i in range(index, n):
+                a = ord(s[i]) - 97
+                freq[a] -= 1
+                if a == to_find:
+                    ret.append(s[i])
+                    index = i + 1
+                    break
+                t.append(s[i])
+        
+        while index < n:
+            i = 0
+            while i < 26 and freq[i] == 0: 
+                i += 1
+
+            if not t or ord(t[-1]) - 97 > i:
+                addItToT(i)
+            else:
+                ret.append(t.pop())
+        
+        return ''.join(ret + t[::-1])
