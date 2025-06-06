14e0aa4b1c0e30b122866e381b94e48ff953176b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-06 13:15:19 +0530

    File updated at 6/6/2025, 1:15:17 pm

--- 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
+++ 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
@@ -1,4 +1,4 @@
-# Last updated: 6/6/2025, 1:08:54 pm
+# Last updated: 6/6/2025, 1:15:17 pm
 class Solution:
     def robotWithString(self, s: str) -> str:
         n = len(s)
@@ -24,23 +24,14 @@ class Solution:
                 t.append(s[i])
         
         while index < n:
-            # print('index:', index)
-            # print('freq:', freq)
-            # print('T:', t)
-            # print('ret:', ret)
-            # print()
             for i in range(26):
-                if not t:
-                    if freq[i]: 
-                        addItToT(i)
-                        break
+                if freq[i] == 0: continue
+                
+                if not t or ord(t[-1]) - 97 > i:
+                    addItToT(i)
                 else:
-                    if ord(t[-1]) - 97 <= i:
-                        ret += t.pop()
-                        break
-                    elif freq[i]:
-                        addItToT(i)
-                        break
+                    ret += t.pop()
+                break
         
         while t: ret += t.pop()
         return ret
