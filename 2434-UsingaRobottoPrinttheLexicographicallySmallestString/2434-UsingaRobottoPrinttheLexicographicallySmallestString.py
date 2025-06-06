754be9a7f3a89bb9d070c162d7db5673fd494612
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-06 13:08:57 +0530

    File updated at 6/6/2025, 1:08:54 pm

--- 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
+++ 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
@@ -1,30 +1,50 @@
-# Last updated: 6/6/2025, 12:50:11 pm
+# Last updated: 6/6/2025, 1:08:54 pm
 class Solution:
     def robotWithString(self, s: str) -> str:
         n = len(s)
         ret = ''
-        
-        heap = []
-        for i in range(n):
-            heap.append((s[i], i))
-        heapq.heapify(heap)
-
         t = []
         index = 0
-
-        while index < n:
-            while heap[0][1] < index:
-                heapq.heappop(heap)
-            if not t or heap[0][0] < t[-1]:
-                curr, i = heapq.heappop(heap)
-                ret += curr
-                for j in range(index, i):
-                    t.append(s[j])
-                index = i+1
-            else:
-                ret += t.pop()
         
-        while t:
-            ret += t.pop()
+        freq = [0] * 26
 
+        for i in range(n):
+            a = ord(s[i]) - 97
+            freq[a] += 1
+        
+        def addItToT(to_find):
+            nonlocal index, ret
+            for i in range(index, n):
+                a = ord(s[i]) - 97
+                freq[a] -= 1
+                if a == to_find:
+                    ret += s[i]
+                    index = i + 1
+                    break
+                t.append(s[i])
+        
+        while index < n:
+            # print('index:', index)
+            # print('freq:', freq)
+            # print('T:', t)
+            # print('ret:', ret)
+            # print()
+            for i in range(26):
+                if not t:
+                    if freq[i]: 
+                        addItToT(i)
+                        break
+                else:
+                    if ord(t[-1]) - 97 <= i:
+                        ret += t.pop()
+                        break
+                    elif freq[i]:
+                        addItToT(i)
+                        break
+        
+        while t: ret += t.pop()
         return ret
+
+
+
+            
