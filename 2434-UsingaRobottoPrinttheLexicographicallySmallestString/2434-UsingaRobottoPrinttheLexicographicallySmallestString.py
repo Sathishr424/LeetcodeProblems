e1119171ab7ad700505477de54cd98fe0daba27c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-06 12:50:14 +0530

    File updated at 6/6/2025, 12:50:11 pm

--- 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
+++ 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
@@ -1,8 +1,8 @@
-# Last updated: 6/6/2025, 12:49:46 pm
+# Last updated: 6/6/2025, 12:50:11 pm
 class Solution:
     def robotWithString(self, s: str) -> str:
         n = len(s)
-        ret = []
+        ret = ''
         
         heap = []
         for i in range(n):
@@ -17,14 +17,14 @@ class Solution:
                 heapq.heappop(heap)
             if not t or heap[0][0] < t[-1]:
                 curr, i = heapq.heappop(heap)
-                ret.append(curr)
+                ret += curr
                 for j in range(index, i):
                     t.append(s[j])
                 index = i+1
             else:
-                ret.append(t.pop())
+                ret += t.pop()
         
         while t:
-            ret.append(t.pop())
+            ret += t.pop()
 
-        return ''.join(ret)
+        return ret
