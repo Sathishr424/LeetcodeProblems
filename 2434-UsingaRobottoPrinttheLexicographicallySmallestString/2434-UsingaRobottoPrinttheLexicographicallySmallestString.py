6ae2b1a4a70ab50b2a47505165ccac4924f52989
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-06 13:19:47 +0530

    File updated at 6/6/2025, 1:19:44 pm

--- 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
+++ 2434-UsingaRobottoPrinttheLexicographicallySmallestString/2434-UsingaRobottoPrinttheLexicographicallySmallestString.py
@@ -1,27 +1,40 @@
-# Last updated: 6/6/2025, 1:18:29 pm
+# Last updated: 6/6/2025, 1:19:44 pm
 class Solution:
     def robotWithString(self, s: str) -> str:
         n = len(s)
         ret = ''
-        
-        heap = []
-        for i in range(n):
-            heap.append((s[i], i))
-        heapq.heapify(heap)
-
         t = []
         index = 0
+        
+        freq = [0] * 26
 
+        for i in range(n):
+            a = ord(s[i]) - 97
+            freq[a] += 1
+        
+        def addItToT(to_find):
+            nonlocal index, ret
+            for i in range(index, n):
+                a = ord(s[i]) - 97
+                freq[a] -= 1
+                if a == to_find:
+                    ret += s[i]
+                    index = i + 1
+                    break
+                t.append(s[i])
+        
         while index < n:
-            while heap[0][1] < index:
-                heapq.heappop(heap)
-            if not t or heap[0][0] < t[-1]:
-                curr, i = heapq.heappop(heap)
-                ret += curr
-                for j in range(index, i):
-                    t.append(s[j])
-                index = i+1
+            i = 0
+            while i < 26 and freq[i] == 0: 
+                i += 1
+
+            if not t or ord(t[-1]) - 97 > i:
+                addItToT(i)
             else:
                 ret += t.pop()
-
+        
         return ret + ''.join(t[::-1])
+
+
+
+            
