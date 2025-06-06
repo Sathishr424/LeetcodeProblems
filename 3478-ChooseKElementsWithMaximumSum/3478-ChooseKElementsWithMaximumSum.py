f7e17b63b2953c1291a00a2d7a6ec446491e591b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-06 18:45:01 +0530

    File updated at 6/6/2025, 6:44:58 pm

--- 3478-ChooseKElementsWithMaximumSum/3478-ChooseKElementsWithMaximumSum.py
+++ 3478-ChooseKElementsWithMaximumSum/3478-ChooseKElementsWithMaximumSum.py
@@ -1,4 +1,4 @@
-# Last updated: 6/6/2025, 6:39:58 pm
+# Last updated: 6/6/2025, 6:44:58 pm
 class Solution:
     def findMaxSum(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
         n = len(nums1)
@@ -8,23 +8,22 @@ class Solution:
         for i in range(n):
             arr.append((nums1[i], i))
         arr.sort(key=lambda x: x[0])
-        
+
         heap = []
         total = 0
 
         for index in range(n):
             num, i = arr[index]
-            prev = total
+
+            if num == arr[index-1][0]:
+                ret[i] = ret[arr[index-1][1]]
+            else:
+                ret[i] = total
 
             total += nums2[i]
             heapq.heappush(heap, nums2[i])
 
             if len(heap) > k:
                 total -= heapq.heappop(heap)
-
-            if num == arr[index-1][0]:
-                ret[i] = ret[arr[index-1][1]]
-            else:
-                ret[i] = prev
 
         return ret
