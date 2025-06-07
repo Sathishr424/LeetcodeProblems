e07d24e135435d28a4b96a05bcf694d718573a40
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-07 16:19:44 +0530

    File updated at 7/6/2025, 4:19:41 pm

--- 3170-LexicographicallyMinimumStringAfterRemovingStars/3170-LexicographicallyMinimumStringAfterRemovingStars.py
+++ 3170-LexicographicallyMinimumStringAfterRemovingStars/3170-LexicographicallyMinimumStringAfterRemovingStars.py
@@ -1,8 +1,8 @@
-# Last updated: 7/6/2025, 4:18:31 pm
+# Last updated: 7/6/2025, 4:19:41 pm
 class Solution:
     def clearStars(self, s: str) -> str:
         n = len(s)
-        # abacacb*abav**abab
+
         deleted = [0] * n
         heap = []
         for i in range(n):
@@ -12,11 +12,11 @@ class Solution:
             else:
                 heapq.heappush(heap, (s[i], -i))
         
-        ret = ''
+        ret = []    
 
         for i in range(n):
             if deleted[i] == 0:
-                ret += s[i]
+                ret.append(s[i])
         
-        return ret
+        return ''.join(ret)
                 
