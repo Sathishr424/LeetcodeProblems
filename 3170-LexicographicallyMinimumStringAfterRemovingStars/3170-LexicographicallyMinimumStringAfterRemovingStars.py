29b0ab62ab1ac79924f43b22fa6b41d813c23059
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-07 16:24:49 +0530

    File updated at 7/6/2025, 4:24:46 pm

--- 3170-LexicographicallyMinimumStringAfterRemovingStars/3170-LexicographicallyMinimumStringAfterRemovingStars.py
+++ 3170-LexicographicallyMinimumStringAfterRemovingStars/3170-LexicographicallyMinimumStringAfterRemovingStars.py
@@ -1,16 +1,19 @@
-# Last updated: 7/6/2025, 4:19:41 pm
+# Last updated: 7/6/2025, 4:24:46 pm
 class Solution:
     def clearStars(self, s: str) -> str:
         n = len(s)
 
         deleted = [0] * n
-        heap = []
+        freq = [[] for _ in range(26)]
         for i in range(n):
             if s[i] == '*':
                 deleted[i] = 1
-                deleted[-heapq.heappop(heap)[1]] = 1
+                for i in range(26):
+                    if freq[i]:
+                        deleted[freq[i].pop()] = 1
+                        break
             else:
-                heapq.heappush(heap, (s[i], -i))
+                freq[ord(s[i]) - 97].append(i)
         
         ret = []    
 
