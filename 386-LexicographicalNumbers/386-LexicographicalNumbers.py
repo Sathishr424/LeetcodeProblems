862313f635126e458b76efcb025e53ce2dd8eb31
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-08 21:38:51 +0530

    File updated at 8/6/2025, 9:38:49 pm

--- 386-LexicographicalNumbers/386-LexicographicalNumbers.py
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.py
@@ -1,37 +1,45 @@
-# Last updated: 8/6/2025, 9:28:20 pm
+# Last updated: 8/6/2025, 9:38:49 pm
 class Node:
     def __init__(self):
-        self.childs = {}
+        self.childs = [None for _ in range(10)]
 
 class Trie:
     def __init__(self):
         self.node = Node()
     
+    def insert_(self, val, node):
+        if val == 0: return node
+        
+        curr = val % 10
+        node = self.insert_(val // 10, node)
+
+        if node.childs[curr] == None:
+            node.childs[curr] = Node()
+        
+        return node.childs[curr]
+    
     def insert(self, val):
         node = self.node
         for i in range(len(val)):
-            if val[i] in node.childs:
+            if node.childs[val[i]] != None:
                 node = node.childs[val[i]]
             else:
                 node.childs[val[i]] = Node()
                 node = node.childs[val[i]]
     
     def getAll(self, node, curr, ret):
-        for child in node.childs:
-            ret.append(curr + int(child))
-            self.getAll(node.childs[child], (curr + int(child)) * 10, ret)
+        for child in range(10):
+            if node.childs[child] != None:
+                ret.append(curr + child)
+                self.getAll(node.childs[child], (curr + child) * 10, ret)
 
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         ret = []
         trie = Trie()
         for i in range(1, n+1):
-            trie.insert(str(i))
+            trie.insert_(i, trie.node)
         
         trie.getAll(trie.node, 0, ret)
         
         return ret
-
-
-            
-            
