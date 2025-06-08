ee6770b98e4f795791c602f73d357759ac0e8a45
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-08 21:28:21 +0530

    File updated at 8/6/2025, 9:28:20 pm

--- 386-LexicographicalNumbers/386-LexicographicalNumbers.py
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.py
@@ -1,62 +1,34 @@
-# Last updated: 8/6/2025, 7:47:25 pm
+# Last updated: 8/6/2025, 9:28:20 pm
+class Node:
+    def __init__(self):
+        self.childs = {}
+
+class Trie:
+    def __init__(self):
+        self.node = Node()
+    
+    def insert(self, val):
+        node = self.node
+        for i in range(len(val)):
+            if val[i] in node.childs:
+                node = node.childs[val[i]]
+            else:
+                node.childs[val[i]] = Node()
+                node = node.childs[val[i]]
+    
+    def getAll(self, node, curr, ret):
+        for child in node.childs:
+            ret.append(curr + int(child))
+            self.getAll(node.childs[child], (curr + int(child)) * 10, ret)
+
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         ret = []
-        # 1, 10, 100, 101, 102, 103, 104, ... 109
-        # 1, 10, 100, 101, 102, 110
-
-        # 10, 100, 1000, 
-
-        # 1000, 1009, 1010, 11
-        # 1
-
-        ret = []
-        num = 1
-        stack = []
-        used = []
-        total_used = 0
-        while num <= n:
-            stack.append(num)
-            used.append(0)
-            ret.append(num)
-            num *= 10
+        trie = Trie()
+        for i in range(1, n+1):
+            trie.insert(str(i))
         
-        def checkSmaller(x,  y):
-            x = str(x)
-            y = str(y)
-
-            for i in range(min(len(x), len(y))):
-                if x[i] < y[i]: return True
-                elif y[i] < x[i]: return False
-            
-            return x[i] < y[i]
-
-        for i in range(len(stack)):
-            stack[i] += 1
-            if stack[i] > min(n, 10**(i+1) - 1):
-                used[i] = 1
-                total_used += 1
-        
-        m = len(stack)
-        while total_used < len(stack):
-            smaller = 0
-            for i in range(m):
-                if used[i] == 0:
-                    smaller = i
-                    break;
-            
-            for i in range(1, m):
-                if used[i] == 0 and checkSmaller(stack[i], stack[smaller]):
-                    smaller = i
-            
-            ret.append(stack[smaller])
-            # print(stack, smaller, ret[-5:])
-            stack[smaller] += 1
-            
-
-            if stack[smaller] > min(n, 10**(smaller+1) - 1):
-                used[smaller] = 1
-                total_used += 1
+        trie.getAll(trie.node, 0, ret)
         
         return ret
 
