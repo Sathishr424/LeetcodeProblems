1bd52e9b9f6ce147a9461895d8f76436194a7458
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-09 04:08:00 +0530

    Create 3578-CountPartitionsWithMax-MinDifferenceatMostK

---
+++ 3578-CountPartitionsWithMax-MinDifferenceatMostK/3578-CountPartitionsWithMax-MinDifferenceatMostK.py
@@ -0,0 +1,31 @@
+# Last updated: 9/6/2025, 4:07:58 am
+class Solution:
+    def countPartitions(self, nums: List[int], k: int) -> int:
+        mod = 10**9 + 7
+
+        n = len(nums)
+        sl = SortedList()
+
+        cnt = 0
+        cnts = [1] * (n+1)
+        ret = 0
+        add = 1
+        for i in range(n-1, -1, -1):
+            sl.add(nums[i])
+
+            while sl[-1] - sl[0] > k:
+                add -= cnts[i+cnt+1]
+                sl.remove(nums[i+cnt])
+                cnt -= 1
+            
+            z = ret
+            ret += add
+            cnts[i] = ret
+            add += z
+            cnt += 1
+
+        return ret % mod
+            
+            
+
+
