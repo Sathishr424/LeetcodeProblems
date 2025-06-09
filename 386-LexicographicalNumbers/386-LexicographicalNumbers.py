e43f46d8ec9141439c73874d4cfd278e063b99fd
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-09 20:48:51 +0530

    File updated at 9/6/2025, 8:48:50 pm

--- 386-LexicographicalNumbers/386-LexicographicalNumbers.py
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.py
@@ -1,45 +1,17 @@
-# Last updated: 8/6/2025, 9:38:49 pm
-class Node:
-    def __init__(self):
-        self.childs = [None for _ in range(10)]
-
-class Trie:
-    def __init__(self):
-        self.node = Node()
-    
-    def insert_(self, val, node):
-        if val == 0: return node
-        
-        curr = val % 10
-        node = self.insert_(val // 10, node)
-
-        if node.childs[curr] == None:
-            node.childs[curr] = Node()
-        
-        return node.childs[curr]
-    
-    def insert(self, val):
-        node = self.node
-        for i in range(len(val)):
-            if node.childs[val[i]] != None:
-                node = node.childs[val[i]]
-            else:
-                node.childs[val[i]] = Node()
-                node = node.childs[val[i]]
-    
-    def getAll(self, node, curr, ret):
-        for child in range(10):
-            if node.childs[child] != None:
-                ret.append(curr + child)
-                self.getAll(node.childs[child], (curr + child) * 10, ret)
-
+# Last updated: 9/6/2025, 8:48:50 pm
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         ret = []
-        trie = Trie()
-        for i in range(1, n+1):
-            trie.insert_(i, trie.node)
+        def dfs(curr):
+            if curr > n: return
+            for i in range(10):
+                new_num = curr + i
+                if new_num > n: break
+                ret.append(new_num)
+                dfs(new_num * 10)
         
-        trie.getAll(trie.node, 0, ret)
-        
+        for i in range(1, 10):
+            if i > n: break
+            ret.append(i)
+            dfs(i * 10)
         return ret
