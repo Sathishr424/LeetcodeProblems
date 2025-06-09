8fcae580e6e5e387ccf90e61b0a26b22668c606b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-10 01:58:46 +0530

    File updated at 10/6/2025, 1:58:45 am

--- 440-K-thSmallestinLexicographicalOrder/440-K-thSmallestinLexicographicalOrder.py
+++ 440-K-thSmallestinLexicographicalOrder/440-K-thSmallestinLexicographicalOrder.py
@@ -1,18 +1,28 @@
-# Last updated: 9/6/2025, 2:04:53 pm
-class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        ret = []
-        def dfs(num):
-            if num > n: return
-            for i in range(10):
-                if num <= n:
-                    ret.append(num)
-                    dfs(num * 10)
-                    num += 1
-        
-        for num in range(1, 10):
-            if num > n: break
-            ret.append(num)
-            dfs(num * 10)
-        
-        return ret
+# Last updated: 10/6/2025, 1:58:45 am
+class Solution(object):
+    def findKthNumber(self, n, k):
+        curr = 1
+        k -= 1
+
+        while k > 0:
+            step = self._count_steps(n, curr, curr + 1)
+            # If the steps are less than or equal to k, we skip this prefix's subtree
+            if step <= k:
+                # Move to the next prefix and decrease k by the number of steps we skip
+                curr += 1
+                k -= step
+            else:
+                # Move to the next level of the tree and decrement k by 1
+                curr *= 10
+                k -= 1
+
+        return curr
+
+    # To count how many numbers exist between prefix1 and prefix2
+    def _count_steps(self, n, prefix1, prefix2):
+        steps = 0
+        while prefix1 <= n:
+            steps += min(n + 1, prefix2) - prefix1
+            prefix1 *= 10
+            prefix2 *= 10
+        return steps
