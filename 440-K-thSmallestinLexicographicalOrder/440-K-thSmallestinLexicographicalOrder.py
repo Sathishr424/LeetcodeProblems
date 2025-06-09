84559973231b0b08d06a532ba7805c5e262679bf
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-10 03:09:56 +0530

    File updated at 10/6/2025, 3:09:55 am

--- 440-K-thSmallestinLexicographicalOrder/440-K-thSmallestinLexicographicalOrder.py
+++ 440-K-thSmallestinLexicographicalOrder/440-K-thSmallestinLexicographicalOrder.py
@@ -1,37 +1,38 @@
-# Last updated: 10/6/2025, 3:04:48 am
+# Last updated: 10/6/2025, 3:09:55 am
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
         def dfs(num, rem):
             if rem == 0: return num // 10
-            # print(num, rem, l_)
-            x = 0
+
+            cnt = 0
             for i in range(10):
                 new_num = num + i
+                possible = getPossible(new_num)
 
-                z = getL(new_num)
-                prev = x
-                x += z
-                if x >= rem:
-                    # print((num, rem, z), new_num, i, (x, z))
-                    return dfs(new_num * 10, rem-prev-1)
-                # print(new_num, (x, z))
+                if cnt + possible >= rem:
+                    return dfs(new_num * 10, rem-cnt-1)
+                
+                cnt += possible
 
-        def getL(num):
-            dig = 1
-            add = 1
-            while num * (dig * 10) + (dig * 10 - 1) <= n:
-                dig *= 10
-                add += dig
+        def getPossible(num):
+            digits = 1
+            possible = 1
+
+            while num * digits + (digits - 1) <= n:
+                digits *= 10
+                possible += digits
             
-            b = num * dig * 10
-            return ( add ) + ((n % b) + 1 if n - (n % b) == b else 0)
+            possible -= digits
+            digits //= 10
 
-        x = 0
+            balance = num * digits * 10
+            return possible + ((n % balance) + 1 if n - (n % balance) == balance else 0)
+
+        cnt = 0
         for i in range(1, 10):
-            new_l = getL(i)
-            if x + new_l >= k:
-                return dfs(i * 10, k-x-1)
-            x += new_l
-            # print(i, x, new_l)
+            possible = getPossible(i)
+            if cnt + possible >= k:
+                return dfs(i * 10, k-cnt-1)
+            cnt += possible
 
         return 0
