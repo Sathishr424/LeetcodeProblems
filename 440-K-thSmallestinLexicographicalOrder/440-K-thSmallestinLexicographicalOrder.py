e6a960bdbfe24f274b8828de88f0eafb301b0e0a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-10 03:04:50 +0530

    File updated at 10/6/2025, 3:04:48 am

--- 440-K-thSmallestinLexicographicalOrder/440-K-thSmallestinLexicographicalOrder.py
+++ 440-K-thSmallestinLexicographicalOrder/440-K-thSmallestinLexicographicalOrder.py
@@ -1,28 +1,37 @@
-# Last updated: 10/6/2025, 1:58:45 am
-class Solution(object):
-    def findKthNumber(self, n, k):
-        curr = 1
-        k -= 1
+# Last updated: 10/6/2025, 3:04:48 am
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        def dfs(num, rem):
+            if rem == 0: return num // 10
+            # print(num, rem, l_)
+            x = 0
+            for i in range(10):
+                new_num = num + i
 
-        while k > 0:
-            step = self._count_steps(n, curr, curr + 1)
-            # If the steps are less than or equal to k, we skip this prefix's subtree
-            if step <= k:
-                # Move to the next prefix and decrease k by the number of steps we skip
-                curr += 1
-                k -= step
-            else:
-                # Move to the next level of the tree and decrement k by 1
-                curr *= 10
-                k -= 1
+                z = getL(new_num)
+                prev = x
+                x += z
+                if x >= rem:
+                    # print((num, rem, z), new_num, i, (x, z))
+                    return dfs(new_num * 10, rem-prev-1)
+                # print(new_num, (x, z))
 
-        return curr
+        def getL(num):
+            dig = 1
+            add = 1
+            while num * (dig * 10) + (dig * 10 - 1) <= n:
+                dig *= 10
+                add += dig
+            
+            b = num * dig * 10
+            return ( add ) + ((n % b) + 1 if n - (n % b) == b else 0)
 
-    # To count how many numbers exist between prefix1 and prefix2
-    def _count_steps(self, n, prefix1, prefix2):
-        steps = 0
-        while prefix1 <= n:
-            steps += min(n + 1, prefix2) - prefix1
-            prefix1 *= 10
-            prefix2 *= 10
-        return steps
+        x = 0
+        for i in range(1, 10):
+            new_l = getL(i)
+            if x + new_l >= k:
+                return dfs(i * 10, k-x-1)
+            x += new_l
+            # print(i, x, new_l)
+
+        return 0
