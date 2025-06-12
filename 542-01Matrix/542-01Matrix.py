db3623a60d2c396d5b8d5fb4e23bc5596db336dc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:48:36 +0530

    Create 542-01Matrix

---
+++ 542-01Matrix/542-01Matrix.py
@@ -0,0 +1,20 @@
+# Last updated: 12/6/2025, 5:48:33 am
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+
+        def getVal(x, y):
+            if x == -1 or x == m or y == -1 or y == n: return float('inf')
+            return mat[x][y]
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] != 0:
+                    mat[i][j] = min(getVal(i-1, j), getVal(i, j-1)) + 1
+
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if mat[i][j] != 0:
+                    mat[i][j] = min(mat[i][j], getVal(i+1, j) + 1, getVal(i, j+1) + 1)
+        return mat
