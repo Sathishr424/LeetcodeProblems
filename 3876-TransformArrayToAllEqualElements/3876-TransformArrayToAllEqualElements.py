ac1c2d7ce9a3a0400f596ec1c5e56db1ab9674ca
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:33:14 +0530

    Create 3876-TransformArrayToAllEqualElements

---
+++ 3876-TransformArrayToAllEqualElements/3876-TransformArrayToAllEqualElements.py
@@ -0,0 +1,17 @@
+# Last updated: 12/6/2025, 5:33:10 am
+class Solution:
+    def canMakeEqual(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        
+        def check(m):
+            new_nums = nums + []
+            op = k
+            for i in range(n-1):
+                if new_nums[i] != m:
+                    new_nums[i] *= -1
+                    new_nums[i+1] *= -1
+                    op -= 1
+            return new_nums[-1] == m and op >= 0
+        
+        return check(-1) or check(1)
+            
