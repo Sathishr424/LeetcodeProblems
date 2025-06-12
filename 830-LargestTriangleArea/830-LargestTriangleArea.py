ba7773b0955321e282c926e82431f3a0aa4dc7ff
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:46:25 +0530

    Create 830-LargestTriangleArea

---
+++ 830-LargestTriangleArea/830-LargestTriangleArea.py
@@ -0,0 +1,5 @@
+# Last updated: 12/6/2025, 5:46:21 am
+class Solution:
+    def largestTriangleArea(self, p):
+        return max(0.5 * abs(i[0] * j[1] + j[0] * k[1] + k[0] * i[1]- j[0] * i[1] - k[0] * j[1] - i[0] * k[1])
+            for i, j, k in itertools.combinations(p, 3))
