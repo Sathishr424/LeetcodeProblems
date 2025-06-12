3ce5e6eb4ee3cc441806cd7804d1f6f018a4dd3a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:35:25 +0530

    Create 3628-FindMinimumTimeToReachLastRoomIi

---
+++ 3628-FindMinimumTimeToReachLastRoomIi/3628-FindMinimumTimeToReachLastRoomIi.py
@@ -0,0 +1,26 @@
+# Last updated: 12/6/2025, 5:35:21 am
+DIR = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+alt_arr = [0, 2, 1]
+
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        m = len(moveTime)
+        n = len(moveTime[0])
+
+        stack = [(0, 0, 0, 1)]
+        moveTime[0][0] = -1
+
+        while stack:
+            time, i, j, alt = heapq.heappop(stack)
+
+            for ni, nj in DIR:
+                ni += i
+                nj += j
+
+                if 0 <= ni < m and 0 <= nj < n and moveTime[ni][nj] != -1:
+                    newTime = max(moveTime[ni][nj], time) + alt
+                    if ni == m-1 and nj == n-1: return newTime
+                    heapq.heappush(stack, (newTime, ni, nj, alt_arr[alt]))
+                    moveTime[ni][nj] = -1
+        
+        return 0
