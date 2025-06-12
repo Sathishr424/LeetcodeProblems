20f3c4f573c9d0f3032e1306cacb1962ee903dc5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:40:34 +0530

    Create 1772-CreateSortedArrayThroughInstructions

---
+++ 1772-CreateSortedArrayThroughInstructions/1772-CreateSortedArrayThroughInstructions.py
@@ -0,0 +1,31 @@
+# Last updated: 12/6/2025, 5:40:30 am
+mod = 10**9 + 7
+class Solution:
+    def createSortedArray(self, ins: List[int]) -> int:
+        m = max(ins)
+
+        def update(index):
+            while index <= m:
+                tree[index] += 1
+                index += index & (-index)
+        
+        def query(index):
+            s = 0
+            while index:
+                s += tree[index]
+                index -= index & (-index)
+            return s
+
+        tree = [0] * (m+1)
+        cost = 0
+        hash = defaultdict(int)
+
+        for i, num in enumerate(ins):
+            left = query(num-1)
+            right = i - left - hash[num]
+
+            cost = (cost + min(left, right)) % mod
+            update(num)
+            hash[num] += 1
+        
+        return cost
