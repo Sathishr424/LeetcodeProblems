08b132f489364a5ee74e18a2823043c03612c719
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:35:35 +0530

    Create 3483-AlternatingGroupsIi

---
+++ 3483-AlternatingGroupsIi/3483-AlternatingGroupsIi.py
@@ -0,0 +1,18 @@
+# Last updated: 12/6/2025, 5:35:31 am
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        n = len(colors)
+        alt = 1
+        ret = 0
+
+        for i in range(1, n+k-1):
+            if alt == k:
+                ret += 1
+                alt -= 1
+            i = i % n
+            if colors[i] != colors[i-1]:
+                alt += 1
+            else:
+                alt = 1
+        
+        return ret+(alt==k)
