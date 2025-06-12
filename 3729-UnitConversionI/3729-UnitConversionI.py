ccf892b57dbc120b59d0b4451192ceb3043fb111
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:34:57 +0530

    Create 3729-UnitConversionI

---
+++ 3729-UnitConversionI/3729-UnitConversionI.py
@@ -0,0 +1,25 @@
+# Last updated: 12/6/2025, 5:34:54 am
+class Solution:
+    def baseUnitConversions(self, conversions: List[List[int]]) -> List[int]:
+        mod = 10**9 + 7
+        n = len(conversions) + 1
+        graph = defaultdict(lambda: defaultdict(int))
+
+        for x, y, w in conversions:
+            graph[x][y] = w
+        
+        ret = [0] * n
+
+        visited = [0] * n
+        def dfs(x, s):
+            if visited[x] == 1: return
+            visited[x] = 1
+            ret[x] = s
+            for y in graph[x]:
+                if visited[y] == 0:
+                    dfs(y, s * graph[x][y] % mod)
+
+        for i in range(n):
+            dfs(i, 1)
+
+        return ret
