b4671fc5938aea544e3baf3c287ea0a4549379be
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:55:21 +0530

    Create 15-3sum

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,35 @@
+# Last updated: 12/6/2025, 5:55:18 am
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        nums.sort()
+
+        p_index = bisect_left(nums, 1)
+        if p_index == 0: return []
+
+        ret = []
+        if bisect_right(nums, 0) - bisect_left(nums, 0) >= 3: ret.append([0, 0, 0])
+
+        pos = {}
+        neg = {}
+        for i in range(p_index, n):
+            pos[nums[i]] = 1
+        
+        for i in range(0, p_index):
+            if i > 0 and nums[i] == nums[i-1]: continue
+            neg[nums[i]] = 1
+            for j in range(i+1, p_index):
+                if j > i+1 and nums[j] == nums[j-1]: continue
+                s = -(nums[i]+nums[j])
+                if s in pos:
+                    ret.append([nums[i], nums[j], s])
+        
+        for i in range(p_index, n):
+            if i > 0 and nums[i] == nums[i-1]: continue
+            for j in range(i+1, n):
+                if j > i+1 and nums[j] == nums[j-1]: continue
+                s = -(nums[i]+nums[j])
+                if s in neg:
+                    ret.append([nums[i], nums[j], s])
+
+        return ret
