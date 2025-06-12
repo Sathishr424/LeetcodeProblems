bcae9f6267b539d68ac87867b7e6e1c919bfd27e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:33:42 +0530

    Create 3843-PartitionArrayIntoTwoEqualProductSubsets

---
+++ 3843-PartitionArrayIntoTwoEqualProductSubsets/3843-PartitionArrayIntoTwoEqualProductSubsets.py
@@ -0,0 +1,24 @@
+# Last updated: 12/6/2025, 5:33:39 am
+class Solution:
+    def checkEqualPartitions(self, nums: List[int], target: int) -> bool:
+        n = len(nums)
+        memo = {}
+        
+        def rec(mask, tot, rem):
+            if mask in memo: return memo[mask]
+            if tot == target:
+                return rem == target
+
+            for i in range(n):
+                if mask & (1 << i) == 0 and rec(mask | (1 << i), tot * nums[i], rem // nums[i]): 
+                    memo[mask] = True
+                    return True
+            
+            memo[mask] = False
+            return False
+        
+        total = 1
+        for num in nums:
+            total *= num
+        
+        return rec(0, 1, total)
