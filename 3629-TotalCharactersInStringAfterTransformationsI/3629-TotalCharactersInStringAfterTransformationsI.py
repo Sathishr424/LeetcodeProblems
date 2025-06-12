96f44c2634625b18d6b67f0315611d3c480f853e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:35:23 +0530

    Create 3629-TotalCharactersInStringAfterTransformationsI

---
+++ 3629-TotalCharactersInStringAfterTransformationsI/3629-TotalCharactersInStringAfterTransformationsI.py
@@ -0,0 +1,20 @@
+# Last updated: 12/6/2025, 5:35:20 am
+N = 10**5 + 1
+mod = 10**9 + 7
+
+dp = [[1] * 26] + [[0] * 26 for i in range(N-1)]
+
+for i in range(1, N):
+    for j in range(25):
+        dp[i][j] = (dp[i][j] + dp[i-1][j + 1]) % mod
+    dp[i][25] = (dp[i][25] + dp[i-1][0] + dp[i-1][1]) % mod
+
+class Solution:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+
+        ret = 0
+        for char in s:
+            ret += dp[t][ord(char) - 97]
+            ret %= mod
+
+        return ret
