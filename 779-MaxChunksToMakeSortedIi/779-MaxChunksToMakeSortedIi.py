1dd7a7667cdbe80883f4f7a8cdab37c1ace3356c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:46:58 +0530

    Create 779-MaxChunksToMakeSortedIi

---
+++ 779-MaxChunksToMakeSortedIi/779-MaxChunksToMakeSortedIi.py
@@ -0,0 +1,21 @@
+# Last updated: 12/6/2025, 5:46:54 am
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        n = len(arr)
+        prefixSum = []
+        tot = 0
+        prefixSum.append(tot)
+        for num in sorted(arr):
+            tot += num
+            prefixSum.append(tot)
+        def rec(start):
+            if start == n: return 0
+            tot = 0
+            for i in range(start, n):
+                tot += arr[i]
+                if abs(prefixSum[start] - prefixSum[i+1]) == tot:
+                    return 1 + rec(i+1)
+            return 1
+        
+        return rec(0)
+        
