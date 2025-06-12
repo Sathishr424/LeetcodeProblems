5bc0563b03dcbbecc39a6e6d0522fdd5908b687c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:38:03 +0530

    Create 2456-ConstructSmallestNumberFromDiString

---
+++ 2456-ConstructSmallestNumberFromDiString/2456-ConstructSmallestNumberFromDiString.py
@@ -0,0 +1,31 @@
+# Last updated: 12/6/2025, 5:38:00 am
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        n = len(pattern)
+
+        m = n+1
+        vis = [True] * (m+1)
+        max_ans = '9' * m
+
+        def rec(index, st, prev):
+            if index == m: return st
+            ans = max_ans
+            if pattern[index-1] == 'I':
+                l = prev+1
+                r = m+1
+            else:
+                l = 1
+                r = prev
+            for i in range(l, r):
+                if vis[i]:
+                    vis[i] = False
+                    ans = min(ans, rec(index+1, st+str(i), i))
+                    vis[i] = True
+            return ans
+        
+        ans = max_ans
+        for i in range(1, m+1):
+            vis[i] = False
+            ans = min(ans, rec(1, str(i), i))
+            vis[i] = True
+        return ans
