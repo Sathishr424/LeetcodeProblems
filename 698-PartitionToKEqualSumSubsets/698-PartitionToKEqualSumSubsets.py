4cca6a457fdaa3feda8ac74be8c3efacd5609862
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:47:36 +0530

    Create 698-PartitionToKEqualSumSubsets

---
+++ 698-PartitionToKEqualSumSubsets/698-PartitionToKEqualSumSubsets.py
@@ -0,0 +1,38 @@
+# Last updated: 12/6/2025, 5:47:32 am
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total = sum(nums)
+        target = total // k
+
+        if total / k != target: return False
+
+        n = len(nums)
+
+        full_mask = (1 << (n+1)) - 1
+
+        dp = {}
+        nums.sort()
+        
+        def rec(index, tot, mask, done):
+            if mask in dp: return False
+            
+            index = index % n
+
+            for i in range(index, n):
+                curr = nums[i]+tot
+                if curr <= target:
+                    if mask & (1 << i) == 0:
+                        if curr == target:
+                            if done+1 == k: return mask | (1 << i) == full_mask 
+                            return rec(0, 0, mask | (1 << i), done+1)
+
+                        elif rec(i+1, curr, mask | (1 << i), done):
+                            return True
+                else:
+                    break
+            
+            dp[mask] = True
+
+            return False
+
+        return rec(0, 0, 1 << n, 0)
