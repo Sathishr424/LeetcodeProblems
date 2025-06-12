73f8e0e5c09b7619a9a00a656f3940397b27fb0f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:35:00 +0530

    Create 3722-SumOfKSubarraysWithLengthAtLeastM

---
+++ 3722-SumOfKSubarraysWithLengthAtLeastM/3722-SumOfKSubarraysWithLengthAtLeastM.py
@@ -0,0 +1,31 @@
+# Last updated: 12/6/2025, 5:34:56 am
+class Solution:
+    def maxSum(self, nums: List[int], k: int, m: int) -> int:
+        n = len(nums)
+
+        prefix = [0]
+        for num in nums:
+            prefix.append(prefix[-1] + num)
+        
+        inf = float('inf')
+        
+        @cache
+        def rec(index, k, expand):
+            if k == 0 and (not expand or index == n): return 0
+
+            ans = -inf
+            if index+1 <= n-(k*m):
+                ans = rec(index+1, k, False)
+            
+            if expand and index+1 <= n-(k*m):
+                ans = max(ans, rec(index+1, k, True) + nums[index])
+            if k > 0:
+                t = prefix[index+m] - prefix[index]
+
+                ans = max(ans, rec(index+m, k-1, True) + t)
+            
+            return ans
+        
+        ans = rec(0, k, False)
+        rec.cache_clear()
+        return ans
