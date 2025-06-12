c2e4ed3d5ccacaf2457314d4d6bae1d8817fc059
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:48:16 +0530

    Create 594-LongestHarmoniousSubsequence

---
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.py
@@ -0,0 +1,14 @@
+# Last updated: 12/6/2025, 5:48:13 am
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        hash = defaultdict(int)
+        ret = 0
+        for num in nums:
+            hash[num] += 1
+        
+        for num in nums:
+            if hash[num+1]:
+                ret = max(ret, hash[num+1] + hash[num])
+        return ret
