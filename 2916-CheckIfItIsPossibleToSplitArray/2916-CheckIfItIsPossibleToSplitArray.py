44e8e2e6ee52a317f7f7cecc164d10f623ba3dc2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:36:35 +0530

    Create 2916-CheckIfItIsPossibleToSplitArray

---
+++ 2916-CheckIfItIsPossibleToSplitArray/2916-CheckIfItIsPossibleToSplitArray.py
@@ -0,0 +1,19 @@
+# Last updated: 12/6/2025, 5:36:32 am
+class Solution:
+    def canSplitArray(self, nums: List[int], m: int) -> bool:
+        n = len(nums)
+        if n <= 2: return True
+
+        prefix = [0]
+        for num in nums:
+            prefix.append(prefix[-1] + num)
+
+        @cache
+        def rec(l, r):
+            if l == r: return 1
+        
+            if prefix[r+1] - prefix[l] >= m:
+                return max(rec(l+1, r), rec(l, r-1)) + 1
+            return 0
+
+        return rec(0, n-1) == n
