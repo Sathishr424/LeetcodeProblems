de9c00215f003b6eb4fd97e07df80d94e8f646d6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:33:31 +0530

    Create 3856-MaximumWeightedKEdgePath

---
+++ 3856-MaximumWeightedKEdgePath/3856-MaximumWeightedKEdgePath.py
@@ -0,0 +1,20 @@
+# Last updated: 12/6/2025, 5:33:27 am
+cmax = lambda x, y: x if x > y else y
+class Solution:
+    def maxWeight(self, n: int, edges: List[List[int]], k: int, t: int) -> int:
+        graph = defaultdict(dict)
+
+        for x, y, w in edges:
+            graph[x][y] = w
+
+        @cache
+        def dfs(x, rem, tot):
+            if rem == 0 and tot > 0: return 0
+            if rem <= 0 or tot <= 0: return -float('inf')
+            ans = -float('inf')
+            for y in graph[x]:
+                ans = cmax(ans, dfs(y, rem-1, tot-graph[x][y]) + graph[x][y])
+            return ans
+        
+        ans = max([dfs(x, k, t) for x in range(n)])
+        return cmax(-1, ans)
