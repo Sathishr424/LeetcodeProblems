fae928223f98e7aab1fa5daa4f38d54805a439d2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:33:52 +0530

    Create 3827-ImplementRouter

---
+++ 3827-ImplementRouter/3827-ImplementRouter.py
@@ -0,0 +1,43 @@
+# Last updated: 12/6/2025, 5:33:49 am
+class Router:
+    def __init__(self, memoryLimit: int):
+        self.limit = memoryLimit
+        self.hash = defaultdict(lambda: defaultdict(deque))
+        self.packets = deque([])
+        self.destToSource = defaultdict(dict)
+
+    def addPacket(self, source: int, destination: int, timestamp: int) -> bool:
+        if self.hash[source][destination] and self.hash[source][destination][-1] == timestamp: return False
+
+        if len(self.packets) == self.limit:
+            s, d = self.packets.popleft()
+            self.hash[s][d].popleft()
+        
+        self.destToSource[destination][source] = 1
+        
+        self.hash[source][destination].append(timestamp)
+
+        self.packets.append((source, destination))
+        return True
+
+    def forwardPacket(self) -> List[int]:
+        if self.packets:
+            s, d = self.packets.popleft()
+            return [s, d, self.hash[s][d].popleft()]
+        return []
+
+    def getCount(self, destination: int, startTime: int, endTime: int) -> int:
+        res = 0
+        for source in self.destToSource[destination]:
+            arr = self.hash[source][destination]
+
+            res += bisect_right(arr, endTime)-bisect_left(arr, startTime)
+
+        return res
+
+
+# Your Router object will be instantiated and called as such:
+# obj = Router(memoryLimit)
+# param_1 = obj.addPacket(source,destination,timestamp)
+# param_2 = obj.forwardPacket()
+# param_3 = obj.getCount(destination,startTime,endTime)
