c81db34ac4cbe5bed76be4e3a8847dafb90be3c7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:38:48 +0530

    Create 2237-LongestPalindromeByConcatenatingTwoLetterWords

---
+++ 2237-LongestPalindromeByConcatenatingTwoLetterWords/2237-LongestPalindromeByConcatenatingTwoLetterWords.py
@@ -0,0 +1,16 @@
+# Last updated: 12/6/2025, 5:38:45 am
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        there = Counter(words)
+        
+        ret = 0
+        odd = False
+
+        for word in there.keys():
+            if word[0] == word[1]:
+                odd = odd or (there[word] % 2 == 1)
+                ret += there[word] - (there[word] % 2)
+            else:
+                ret += min(there[word], there[word[1] + word[0]])
+
+        return (ret + odd) * 2
