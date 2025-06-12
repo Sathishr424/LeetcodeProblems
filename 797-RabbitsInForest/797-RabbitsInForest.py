010a6d716048d3ad6225747c0e3f62fa9b2af4e2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:46:44 +0530

    Create 797-RabbitsInForest

---
+++ 797-RabbitsInForest/797-RabbitsInForest.py
@@ -0,0 +1,21 @@
+# Last updated: 12/6/2025, 5:46:41 am
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        answers.sort()
+        prev = answers[0]
+        cnt = 1
+        ret = 0
+        
+        def calc(num, cnt):
+            return ceil(cnt / num) * num
+
+        for i in range(1, len(answers)):
+            num = answers[i]
+            if num == prev:
+                cnt += 1
+            else:
+                ret += calc(prev+1, cnt)
+                cnt = 1
+            prev = num
+                
+        return ret + calc(prev+1, cnt)
