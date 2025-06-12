67750b9b52b356e15064053760316ab14d66a8e1
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:50:36 +0530

    Create 327-CountOfRangeSum

---
+++ 327-CountOfRangeSum/327-CountOfRangeSum.py
@@ -0,0 +1,27 @@
+# Last updated: 12/6/2025, 5:50:33 am
+class Solution:
+    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+        n = len(nums)
+
+        prefixes = [0] * n
+        s = 0
+        for i in range(n-1, -1, -1):
+            s += nums[i]
+            prefixes[i] = s
+
+        sl = SortedList()
+        ret = 0
+
+        for i in range(n):
+            sl.add(prefixes[i])
+            curr = prefixes[i] - nums[i]
+
+            l = sl.bisect_left(curr+lower)
+            r = sl.bisect_right(curr+upper)
+
+            ret += r - l
+        
+        return ret
+            
+
+        
