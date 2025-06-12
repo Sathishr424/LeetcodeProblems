b4d6fce4120f0572d02ef13ac636e0349a420c07
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:40:58 +0530

    Create 1706-MinCostToConnectAllPoints

---
+++ 1706-MinCostToConnectAllPoints/1706-MinCostToConnectAllPoints.py
@@ -0,0 +1,31 @@
+# Last updated: 12/6/2025, 5:40:54 am
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+
+        def getDistance(i, j):
+            return abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+        
+        stack = [(0, 0)]
+        edges = 0
+        visited = [False] * n
+        dis = [float('inf')] * n
+        total_cost = 0
+
+        while stack:
+            cost, x = heapq.heappop(stack)
+            if visited[x]: continue
+
+            visited[x] = True
+            total_cost += cost
+            edges += 1
+            if edges == n: return total_cost
+
+            for y in range(n):
+                if visited[y]: continue
+                d = getDistance(x, y)
+                if dis[y] > d:
+                    dis[y] = d
+                    heapq.heappush(stack, (d, y))
+        
+        return total_cost
