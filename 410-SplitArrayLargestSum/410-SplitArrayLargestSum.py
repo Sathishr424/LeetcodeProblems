092f0e357049dfa4e74332a81f3a81e325aa8a74
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:49:47 +0530

    Create 410-SplitArrayLargestSum

---
+++ 410-SplitArrayLargestSum/410-SplitArrayLargestSum.py
@@ -0,0 +1,25 @@
+# Last updated: 12/6/2025, 5:49:43 am
+class Solution:
+    def splitArray(self, weights: List[int], days: int) -> int:
+        l = max(weights)
+        r = sum(weights)
+        ret = r
+
+        while l <= r:
+            ship_cap = (l+r) // 2
+
+            day = 1
+            load = 0
+            for w in weights:
+                if load+w > ship_cap:
+                    day += 1
+                    load = 0
+                load += w
+            
+            if day <= days:
+                ret = ship_cap
+                r = ship_cap-1
+            else:
+                l = ship_cap+1
+        
+        return ret
