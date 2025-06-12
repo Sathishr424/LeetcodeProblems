e0fd0e71022be21ab08bc25d3c8c21e35108e6f2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:36:20 +0530

    Create 3079-MinimumEdgeWeightEquilibriumQueriesInATree

---
+++ 3079-MinimumEdgeWeightEquilibriumQueriesInATree/3079-MinimumEdgeWeightEquilibriumQueriesInATree.py
@@ -0,0 +1,87 @@
+# Last updated: 12/6/2025, 5:36:16 am
+cmax = lambda x, y: x if x > y else y
+class Solution:
+    def minOperationsQueries(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
+        graph = defaultdict(dict)
+        max_w = 0
+
+        for x, y, w in edges:
+            graph[x][y] = w
+            graph[y][x] = w
+            max_w = cmax(max_w, w)
+        
+        max_w += 1
+        
+        freq = [[0] * max_w for _ in range(n)]
+        depths = [0] * n
+        parents = [-1] * n
+
+        root = 0
+        visited = [False] * n
+        visited[root] = True
+
+        def dfs(x, depth):
+            depths[x] = depth
+
+            for y in graph[x]:
+                if not visited[y]:
+                    visited[y] = True
+                    parents[y] = x
+                    for i in range(1, max_w):
+                        freq[y][i] += freq[x][i]
+                    freq[y][graph[x][y]] += 1
+                    dfs(y, depth+1)
+
+        dfs(root, 0)
+        N = int(log2(max(depths) + 1)) + 1
+
+        logs = [[-1] * n for _ in range(N)]
+
+        for i in range(n):
+            logs[0][i] = parents[i]
+
+        for i in range(1, N):
+            for j in range(n):
+                if logs[i-1][j] == -1: continue
+                logs[i][j] = logs[i-1][logs[i-1][j]]
+
+        def kthNode(x, k):
+            if k == 0: return x
+            for i in range(N-1, -1, -1):
+                if k >= (1 << i):
+                    k -= (1 << i)
+                    x = logs[i][x]
+            return x
+
+        def lca(x, y):
+            if x == y: return x
+            for i in range(N-1, -1, -1):
+                if logs[i][x] != logs[i][y]:
+                    x = logs[i][x]
+                    y = logs[i][y]
+            return logs[0][y]
+        
+        ret = []
+        for x, y in queries:
+            depth_x = depths[x]
+            depth_y = depths[y]
+
+            if depth_x > depth_y:
+                x, y = y, x
+                depth_x, depth_y = depth_y, depth_x
+            
+            diff = depth_y - depth_x
+            new_y = kthNode(y, diff)
+            
+            ancestor = lca(x, new_y)
+            
+            maxi = 0
+            total = 0
+            for i in range(1, max_w):
+                curr = (freq[x][i] + freq[y][i]) - (freq[ancestor][i] * 2)
+                maxi = cmax(maxi, curr)
+                total += curr
+
+            ret.append(total - maxi)
+
+        return ret
