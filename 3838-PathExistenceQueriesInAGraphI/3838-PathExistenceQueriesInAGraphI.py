1ef5aada441fd23e41ff86644fbc296bc7ab8967
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:33:45 +0530

    Create 3838-PathExistenceQueriesInAGraphI

---
+++ 3838-PathExistenceQueriesInAGraphI/3838-PathExistenceQueriesInAGraphI.py
@@ -0,0 +1,21 @@
+# Last updated: 12/6/2025, 5:33:41 am
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        parents = [i for i in range(n)]
+
+        def find(x):
+            return parents[x]
+        
+        def union(x, y):
+            parents[find(y)] = find(x)
+        
+        for i in range(1, n):
+            if nums[i] - nums[i-1] <= maxDiff:
+                union(i-1, i)
+        
+        ret = []
+
+        for x, y in queries:
+            ret.append(find(x) == find(y))
+        
+        return ret
