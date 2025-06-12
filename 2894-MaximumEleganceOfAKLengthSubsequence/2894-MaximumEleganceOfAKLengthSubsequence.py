6c035318928010a6ef3c27b43071b4404b872266
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:36:40 +0530

    Create 2894-MaximumEleganceOfAKLengthSubsequence

---
+++ 2894-MaximumEleganceOfAKLengthSubsequence/2894-MaximumEleganceOfAKLengthSubsequence.py
@@ -0,0 +1,38 @@
+# Last updated: 12/6/2025, 5:36:37 am
+class Solution:
+    def findMaximumElegance(self, items: List[List[int]], k: int) -> int:
+        n = len(items)
+        items.sort(key=lambda x: x[0], reverse=True)
+
+        uniq = defaultdict(int)
+        total = 0
+        stack = []
+
+        for i in range(k):
+            total += items[i][0]
+            uniq[items[i][1]] += 1
+
+            if uniq[items[i][1]] > 1:
+                stack.append(items[i])
+
+        ret = total + (len(uniq) ** 2)
+
+        for i in range(k, n):
+            if items[i][1] not in uniq:
+                while stack and uniq[ stack[-1][1] ] == 1:
+                    stack.pop()
+                
+                if not stack: break
+
+                profit, cat = stack.pop()
+
+                uniq[ cat ] -= 1
+
+                total -= profit
+                total += items[i][0]
+                
+                uniq[items[i][1]] += 1
+
+                ret = max(ret, total + (len(uniq) ** 2))
+            
+        return ret
