704551b8d7ab66983bffe3b55be8a6a5911f47b8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:45:57 +0530

    Create 896-SmallestSubtreeWithAllTheDeepestNodes

---
+++ 896-SmallestSubtreeWithAllTheDeepestNodes/896-SmallestSubtreeWithAllTheDeepestNodes.py
@@ -0,0 +1,28 @@
+# Last updated: 12/6/2025, 5:45:54 am
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        deapest_level = 0
+        deapest_node = root
+
+        def traverse(node, level):
+            nonlocal deapest_level, deapest_node
+            if node == None: return level
+            level += 1
+            left = traverse(node.left, level)
+            right = traverse(node.right, level)
+            if left >= deapest_level and left == right:
+                deapest_node = node
+                deapest_level = left
+                return left
+            else:
+                return max(left, right)
+            
+        traverse(root, 0)
+        return deapest_node
+        
