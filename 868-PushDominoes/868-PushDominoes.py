8c0e7ebda571015882d81dedc455cbc526222fd1
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:46:11 +0530

    Create 868-PushDominoes

---
+++ 868-PushDominoes/868-PushDominoes.py
@@ -0,0 +1,37 @@
+# Last updated: 12/6/2025, 5:46:07 am
+class Solution:
+    def pushDominoes(self, dom: str) -> str:
+        n = len(dom)
+        dom = list(dom)
+
+        def simulate(l, r):
+            leftForce = 0
+            rightForce = 0
+            if l-1 >= 0:
+                leftForce = -1 if dom[l-1] == 'L' else 1
+            if r+1 < n:
+                rightForce = -1 if dom[r+1] == 'L' else 1
+            
+            if leftForce <= 0 and rightForce >= 0: return
+            elif leftForce > 0 and rightForce < 0:
+                while l < r:
+                    dom[l] = 'R'
+                    dom[r] = 'L'
+                    l += 1
+                    r -= 1
+            else:
+                a = 'L' if leftForce + rightForce < 0 else 'R'
+                for i in range(l, r+1): dom[i] = a
+
+        start = -1
+        for i in range(n):
+            if dom[i] == '.':
+                if start == -1: start = i
+            else:
+                if start != -1:
+                    simulate(start, i-1)
+                    start = -1
+        
+        if start != -1: simulate(start, n-1)
+
+        return ''.join(dom)
