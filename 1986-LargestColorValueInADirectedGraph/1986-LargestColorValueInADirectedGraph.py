159ec6e5ed6784f4898d2c9ed9f5a439ccda5a97
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:39:40 +0530

    Create 1986-LargestColorValueInADirectedGraph

---
+++ 1986-LargestColorValueInADirectedGraph/1986-LargestColorValueInADirectedGraph.py
@@ -0,0 +1,36 @@
+# Last updated: 12/6/2025, 5:39:37 am
+cmax = lambda x, y: x if x > y else y
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        n = len(colors)
+        graph = defaultdict(list)
+
+        for x, y in edges:
+            graph[x].append(y)
+
+        max_freq = [[0] * 26 for _ in range(n)]
+        ret = 0
+        visited = [False] * n
+
+        def dfs(x, vis):
+            if x in vis: return False
+            if visited[x]: return True
+
+            vis[x] = 1
+            visited[x] = True
+
+            for y in graph[x]:
+                if not dfs(y, vis): return False
+                for i in range(26):
+                    max_freq[x][i] = cmax(max_freq[x][i], max_freq[y][i])
+
+            max_freq[x][ord(colors[x]) - 97] += 1
+            
+            del vis[x]
+            return True
+        
+        for x in range(n):
+            if not dfs(x, {}): return -1
+            ret = cmax(ret, max_freq[x][ord(colors[x]) - 97])
+
+        return ret
