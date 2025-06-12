0994230f7aa8585196942a5abbecc6f6bf592c6f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:39:16 +0530

    Create 2114-MinimumNumberOfWorkSessionsToFinishTheTasks

---
+++ 2114-MinimumNumberOfWorkSessionsToFinishTheTasks/2114-MinimumNumberOfWorkSessionsToFinishTheTasks.py
@@ -0,0 +1,23 @@
+# Last updated: 12/6/2025, 5:39:12 am
+inf = 15
+class Solution:
+    def minSessions(self, tasks: List[int], sessionTime: int) -> int:
+        n = len(tasks)
+
+        full_mask = (1 << (n+1)) - 1
+        dp = [[inf] * sessionTime for _ in range(full_mask)]
+
+        def rec(mask, remTime):
+            if mask == full_mask: return 0
+            if dp[mask][remTime] != inf: return dp[mask][remTime] 
+            ans = inf
+            for i in range(n):
+                if mask & (1 << i) == 0:
+                    if tasks[i] <= remTime:
+                        ans = min(ans, rec(mask | (1 << i), remTime - tasks[i]))
+                    else:
+                        ans = min(ans, rec(mask | (1 << i), sessionTime - tasks[i]) + 1)
+            dp[mask][remTime]  = ans
+            return ans
+        
+        return rec(1 << n, 0)
