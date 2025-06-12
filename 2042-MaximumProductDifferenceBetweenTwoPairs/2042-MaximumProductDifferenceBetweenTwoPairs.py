a242f3a576a115f83d7e1793198b75a6d49260bf
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:39:30 +0530

    Create 2042-MaximumProductDifferenceBetweenTwoPairs

---
+++ 2042-MaximumProductDifferenceBetweenTwoPairs/2042-MaximumProductDifferenceBetweenTwoPairs.py
@@ -0,0 +1,19 @@
+# Last updated: 12/6/2025, 5:39:26 am
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        first = 0
+        second = 0
+        for num in nums:
+            if num > first:
+                second = first
+                first = num
+            elif num > second:
+                second = num
+        left = first * second
+        for num in nums:
+            if num < first:
+                second = first
+                first = num
+            elif num < second:
+                second = num
+        return left - (first * second)
