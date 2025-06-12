e817908770e75a7854b2bb7f88d3c0781bcc3f2a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-12 05:34:21 +0530

    Create 3786-LongestPalindromicSubsequenceAfterAtMostKOperations

---
+++ 3786-LongestPalindromicSubsequenceAfterAtMostKOperations/3786-LongestPalindromicSubsequenceAfterAtMostKOperations.py
@@ -0,0 +1,23 @@
+# Last updated: 12/6/2025, 5:34:18 am
+class Solution:
+    def longestPalindromicSubsequence(self, s: str, k: int) -> int:
+        n = len(s)
+
+        dp = [[[-1] * (k+1) for _ in range(n)] for _ in range(n)]
+
+        def dfs(l, r, k):
+            if dp[l][r][k] != -1: return dp[l][r][k]
+            if l == r: return 1
+            elif l > r: return 0
+
+            ans = max(dfs(l+1, r, k), dfs(l, r-1, k))
+
+            diff = abs(ord(s[l]) - ord(s[r]))
+            diff = min(26 - diff, diff)
+
+            if diff <= k:
+                ans = max(ans, dfs(l+1, r-1, k-diff) + 2)
+            dp[l][r][k] = ans
+            return ans
+        
+        return dfs(0, n-1, k)
