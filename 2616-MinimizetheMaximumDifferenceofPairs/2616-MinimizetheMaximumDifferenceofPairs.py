0c091c761538b26a8d69e83d394aadc094361262
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-13 09:03:00 +0530

    File updated at 13/6/2025, 9:03:00 am

--- 2616-MinimizetheMaximumDifferenceofPairs/2616-MinimizetheMaximumDifferenceofPairs.py
+++ 2616-MinimizetheMaximumDifferenceofPairs/2616-MinimizetheMaximumDifferenceofPairs.py
@@ -1,39 +1,35 @@
-# Last updated: 13/6/2025, 8:48:52 am
+# Last updated: 13/6/2025, 9:03:00 am
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
-        nums.sort()
-        ret = 0
         n = len(nums)
+        nums.sort()
         print(nums)
-
-        # 5, 7, 8, 10
-        # [1, 2, 1, 3, 0, 2, 1, 1, 2, 3]
 
         l = 0
         r = (nums[-1] - nums[0]) + 1
 
         while l < r:
             mid = (l + r) // 2
-            # print(l, mid, r)
 
-            stack = deque([])
-            
+            print(l, mid, r)
+
+            start = 0
             cnt = 0
-            for i in range(n):
-                while stack and nums[i] - stack[0] > mid:
-                    stack.popleft()
-                    if stack:
-                        stack.popleft()
-                        cnt += 1
-                stack.append(nums[i])
-                # print(stack, cnt)
+            for i in range(1, n):
+                if nums[i] - nums[start] > mid:
+                    diff = i - start
+                    cnt += diff // 2
+                    if diff % 2 == 0 or nums[i] - nums[i-1] > mid:
+                        start = i
+                    else:
+                        start = i-1
+                # print((i, nums[i]), start, cnt)
             
-            while stack:
-                stack.popleft()
-                if stack:
-                    stack.popleft()
-                    cnt += 1
-
+            # end = n-1
+            # while end > start and nums[end] - nums[start] > mid:
+            #     end -= 1
+            
+            cnt += (n - start) // 2
             # print(cnt)
 
             if cnt >= p:
@@ -42,3 +38,4 @@ class Solution:
                 l = mid + 1
         
         return l
+        # [3, 3, 3, 5, 6, 6, 6, 7, 8, 11]
