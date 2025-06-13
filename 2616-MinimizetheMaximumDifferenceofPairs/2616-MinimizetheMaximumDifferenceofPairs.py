77c457b46497841b1980db4984b62af138c3e2be
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-13 09:08:35 +0530

    File updated at 13/6/2025, 9:08:35 am

--- 2616-MinimizetheMaximumDifferenceofPairs/2616-MinimizetheMaximumDifferenceofPairs.py
+++ 2616-MinimizetheMaximumDifferenceofPairs/2616-MinimizetheMaximumDifferenceofPairs.py
@@ -1,19 +1,15 @@
-# Last updated: 13/6/2025, 9:08:16 am
+# Last updated: 13/6/2025, 9:08:35 am
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
+        n = len(nums)
         nums.sort()
         ret = 0
-        n = len(nums)
-
-        # 5, 7, 8, 10
-        # [1, 2, 1, 3, 0, 2, 1, 1, 2, 3]
 
         l = 0
         r = (nums[-1] - nums[0]) + 1
 
         while l < r:
             mid = (l + r) // 2
-            # print(l, mid, r)
 
             stack = deque([])
             
@@ -25,15 +21,12 @@ class Solution:
                         stack.popleft()
                         cnt += 1
                 stack.append(nums[i])
-                # print(stack, cnt)
             
             while stack:
                 stack.popleft()
                 if stack:
                     stack.popleft()
                     cnt += 1
-
-            # print(cnt)
 
             if cnt >= p:
                 r = mid
