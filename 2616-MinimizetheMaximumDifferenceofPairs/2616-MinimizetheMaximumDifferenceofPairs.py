3482a481b9bfb3242312764f9d9e9f1a8f70b682
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-13 09:05:12 +0530

    File updated at 13/6/2025, 9:05:11 am

--- 2616-MinimizetheMaximumDifferenceofPairs/2616-MinimizetheMaximumDifferenceofPairs.py
+++ 2616-MinimizetheMaximumDifferenceofPairs/2616-MinimizetheMaximumDifferenceofPairs.py
@@ -1,4 +1,4 @@
-# Last updated: 13/6/2025, 9:03:26 am
+# Last updated: 13/6/2025, 9:05:11 am
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
         n = len(nums)
@@ -7,23 +7,25 @@ class Solution:
         l = 0
         r = (nums[-1] - nums[0]) + 1
 
-        while l < r:
-            mid = (l + r) // 2
-
+        def isGood(target):
             start = 0
             cnt = 0
             for i in range(1, n):
-                if nums[i] - nums[start] > mid:
+                if nums[i] - nums[start] > target:
                     diff = i - start
                     cnt += diff // 2
-                    if diff % 2 == 0 or nums[i] - nums[i-1] > mid:
+                    if diff % 2 == 0 or nums[i] - nums[i-1] > target:
                         start = i
                     else:
                         start = i-1
             
             cnt += (n - start) // 2
+            return cnt >= p
 
-            if cnt >= p:
+        while l < r:
+            mid = (l + r) // 2
+
+            if isGood(mid):
                 r = mid
             else:
                 l = mid + 1
