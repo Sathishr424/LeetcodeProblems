c2c40244d0c630a049e25207d04386a3fe4d928a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-13 09:08:53 +0530

    File updated at 13/6/2025, 9:08:52 am

--- 2616-MinimizetheMaximumDifferenceofPairs/2616-MinimizetheMaximumDifferenceofPairs.py
+++ 2616-MinimizetheMaximumDifferenceofPairs/2616-MinimizetheMaximumDifferenceofPairs.py
@@ -1,34 +1,31 @@
-# Last updated: 13/6/2025, 9:08:35 am
+# Last updated: 13/6/2025, 9:08:52 am
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
         n = len(nums)
         nums.sort()
-        ret = 0
 
         l = 0
         r = (nums[-1] - nums[0]) + 1
 
+        def isGood(target):
+            start = 0
+            cnt = 0
+            for i in range(1, n):
+                if nums[i] - nums[start] > target:
+                    diff = i - start
+                    cnt += diff // 2
+                    if diff % 2 == 0 or nums[i] - nums[i-1] > target:
+                        start = i
+                    else:
+                        start = i-1
+            
+            cnt += (n - start) // 2
+            return cnt >= p
+
         while l < r:
             mid = (l + r) // 2
 
-            stack = deque([])
-            
-            cnt = 0
-            for i in range(n):
-                while stack and nums[i] - stack[0] > mid:
-                    stack.popleft()
-                    if stack:
-                        stack.popleft()
-                        cnt += 1
-                stack.append(nums[i])
-            
-            while stack:
-                stack.popleft()
-                if stack:
-                    stack.popleft()
-                    cnt += 1
-
-            if cnt >= p:
+            if isGood(mid):
                 r = mid
             else:
                 l = mid + 1
