c44dbbeefb5376ac2976a5623f29f9ad2f62a33e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-14 14:38:36 +0530

    File updated at 14/6/2025, 2:38:34 pm

--- 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
+++ 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
@@ -1,16 +1,16 @@
-# Last updated: 14/6/2025, 2:36:46 pm
+# Last updated: 14/6/2025, 2:38:34 pm
 class Solution:
     def mostFrequent(self, nums: List[int], key: int) -> int:
-        keys = defaultdict(int)
+        targets = defaultdict(int)
         ret = 0
 
         for i in range(len(nums)-1):
             if nums[i] == key:
-                keys[nums[i+1]] += 1
+                targets[nums[i+1]] += 1
                 ret = nums[i+1]
 
-        for num in keys:
-            if keys[num] > keys[ret]:
+        for num in targets:
+            if targets[num] > targets[ret]:
                 ret = num
         
         return ret
