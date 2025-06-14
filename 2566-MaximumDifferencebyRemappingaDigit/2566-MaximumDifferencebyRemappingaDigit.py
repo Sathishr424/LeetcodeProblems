e6d9de349576fc9b7bc09667f1c3fbbd79bb1936
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-14 13:21:01 +0530

    File updated at 14/6/2025, 1:21:00 pm

--- 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
+++ 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
@@ -1,10 +1,10 @@
-# Last updated: 14/6/2025, 1:20:47 pm
+# Last updated: 14/6/2025, 1:21:00 pm
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         n = len(str1)
         m = len(str2)
         
-        div = m
+        div = min(n, m)
         while div:
             if n % div == 0 and m % div == 0:
                 st = str2[:div]
