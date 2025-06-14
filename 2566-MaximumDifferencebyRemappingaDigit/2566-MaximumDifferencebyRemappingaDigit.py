f0fe5339a14d4afd26c2d58b7590d40f0c6110b4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-14 13:20:48 +0530

    File updated at 14/6/2025, 1:20:47 pm

--- 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
+++ 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
@@ -1,9 +1,6 @@
-# Last updated: 14/6/2025, 1:12:16 pm
+# Last updated: 14/6/2025, 1:20:47 pm
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if len(str2) > len(str1):
-            str1, str2 = str2, str1
-        
         n = len(str1)
         m = len(str2)
         
