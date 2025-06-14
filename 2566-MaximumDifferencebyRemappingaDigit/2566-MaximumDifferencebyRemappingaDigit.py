32fe916c2cace35a0cd132a83824d035da43ce17
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-14 15:31:39 +0530

    File updated at 14/6/2025, 3:31:38 pm

--- 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
+++ 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
@@ -1,26 +1,20 @@
-# Last updated: 14/6/2025, 3:25:15 pm
+# Last updated: 14/6/2025, 3:31:38 pm
 def gcd(x, y):
-    rem = x % y
-    if rem == 0: return y
-    return gcd(y, rem)
+    if y == 0: return x
+    return gcd(y, x % y)
+
+# 45 => 10 . 2 + 5
+# 10 => 5 . 2 + 0
 
 class Solution:
     def subarrayGCD(self, nums: List[int], k: int) -> int:
         n = len(nums)
         ret = 0
 
-        for num in nums:
-            if num == k: ret += 1
-
-        for i in range(n-1):
-            num = nums[i]
-            for j in range(i+1, n):
-                if num > nums[j]:
-                    num = gcd(num, nums[j])
-                else:
-                    num = gcd(nums[j], num)
-                
-                ret += num == k
+        for i in range(n):
+            num = 0
+            for j in range(i, n):
+                ret += (num := gcd(nums[j], num)) == k
         
         return ret
             
