b34c26a399c634f198d0aaf8213f9cceec813475
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-14 15:21:35 +0530

    File updated at 14/6/2025, 3:21:33 pm

--- 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
+++ 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
@@ -1,8 +1,24 @@
-# Last updated: 14/6/2025, 2:41:54 pm
-class Solution:
-    def average(self, salary: List[int]) -> float:
-        s = sum(salary)
-        s -= min(salary)
-        s -= max(salary)
+# Last updated: 14/6/2025, 3:21:33 pm
+def gcd(x, y):
+    rem = x % y
+    if rem == 0: return y
+    return gcd(y, rem)
 
-        return s / (len(salary) - 2)
+class Solution:
+    def subarrayGCD(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        ret = 0
+
+        for num in nums:
+            if num == k: ret += 1
+
+        for i in range(n-1):
+            num = nums[i]
+            for j in range(i+1, n):
+                g = gcd(max(num, nums[j]), min(num, nums[j]))
+                if g == k:
+                    ret += 1
+                num = g
+        
+        return ret
+            
