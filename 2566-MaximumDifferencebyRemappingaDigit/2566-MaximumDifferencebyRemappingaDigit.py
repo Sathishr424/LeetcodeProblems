a066f57177720b85c659552044b530c01a1548dd
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-14 14:28:53 +0530

    File updated at 14/6/2025, 2:28:52 pm

--- 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
+++ 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.py
@@ -1,15 +1,12 @@
-# Last updated: 14/6/2025, 1:55:54 pm
+# Last updated: 14/6/2025, 2:28:52 pm
 def gcd(x, y):
     rem = x % y
     if rem == 0: return y
-
     return gcd(y, rem)
 
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1 + str2 != str2 + str1: return ''
-        n = len(str1)
-        m = len(str2)
-        if m > n:
-            n, m = m, n
-        return str1[0:gcd(n, m)]
+    def findGCD(self, nums: List[int]) -> int:
+        x = max(nums)
+        y = min(nums)
+
+        return gcd(x, y)
