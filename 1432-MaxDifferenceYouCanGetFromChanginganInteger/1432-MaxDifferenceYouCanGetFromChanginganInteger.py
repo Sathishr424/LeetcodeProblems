2ce68e16bbc7c5140480d7e1bb6a761528c57be9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-15 08:12:23 +0530

    File updated at 15/6/2025, 8:12:20 am

--- 1432-MaxDifferenceYouCanGetFromChanginganInteger/1432-MaxDifferenceYouCanGetFromChanginganInteger.py
+++ 1432-MaxDifferenceYouCanGetFromChanginganInteger/1432-MaxDifferenceYouCanGetFromChanginganInteger.py
@@ -1,43 +1,37 @@
-# Last updated: 15/6/2025, 8:09:28 am
+# Last updated: 15/6/2025, 8:12:20 am
 class Solution:
     def maxDiff(self, num: int) -> int:
         digit = -1
-        def getMaxi(num):
-            nonlocal digit
-            if num == 0: return 0
-
-            new_num = getMaxi(num // 10)
-            rem = num % 10
-            if digit == -1:
-                if rem != 9:
-                    digit = rem
-            
-            if rem == digit:
-                return new_num * 10 + 9
-            return new_num * 10 + rem
+        find = 9
+        replace = 9
         
-        replace = 1
-
-        def getMini(num):
+        def rec(num):
             nonlocal digit, replace
             if num == 0: return 0
 
-            new_num = getMini(num // 10)
+            new_num = rec(num // 10)
             rem = num % 10
+
             if digit == -1:
                 if new_num == 0:
-                    if rem > 1: digit = rem
-                elif rem > 1:
-                    digit = rem
-                    replace = 0
+                    if rem != find:
+                        digit = rem
+                else:
+                    if find == 1:
+                        if rem > 1:
+                            digit = rem
+                            replace = 0
+                    elif rem != find:
+                        digit = rem
             
             if rem == digit:
                 return new_num * 10 + replace
-            return new_num * 10 + rem
             
+            return new_num * 10 + rem
         
-        maxi = getMaxi(num)
+        maxi = rec(num)
         digit = -1
-        mini = getMini(num)
-        # print(maxi, mini)
+        find = 1
+        replace = 1
+        mini = rec(num)
         return maxi - mini
