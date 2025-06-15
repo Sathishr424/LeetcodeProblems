de19385bfb2d1f6244389076bba923f9d0a3608b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-16 03:52:43 +0530

    File updated at 16/6/2025, 3:52:40 am

--- 3499-MaximizeActiveSectionwithTradeI/3499-MaximizeActiveSectionwithTradeI.py
+++ 3499-MaximizeActiveSectionwithTradeI/3499-MaximizeActiveSectionwithTradeI.py
@@ -1,4 +1,4 @@
-# Last updated: 16/6/2025, 3:51:00 am
+# Last updated: 16/6/2025, 3:52:40 am
 class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
         n = len(s)
@@ -10,7 +10,7 @@ class Solution:
         ret = total
         i = 0
         zeros = 0
-        ones = 0
+        ones = False
         while i < n:
             if s[i] == '0':
                 j = i + 1
@@ -22,15 +22,13 @@ class Solution:
                     ret = max(ret, total + zeros + curr)
 
                 zeros = curr
-                ones = 0
+                ones = False
                 i = j
             else:
-                j = i + 1
-                while j < n and s[j] == '1':
-                    j += 1
+                ones = True
+                i += 1
+                while i < n and s[i] == '1':
+                    i += 1
                 
-                ones = j - i
-                i = j
-        
         return ret
                 
