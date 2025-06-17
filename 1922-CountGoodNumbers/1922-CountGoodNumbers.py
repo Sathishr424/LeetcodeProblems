df69f0b49e6cf907bfe2e3a4d0ee475d2f25bfb4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-17 20:05:46 +0530

    File updated at 17/6/2025, 8:05:44 pm

--- 1922-CountGoodNumbers/1922-CountGoodNumbers.py
+++ 1922-CountGoodNumbers/1922-CountGoodNumbers.py
@@ -1,10 +1,5 @@
-# Last updated: 17/6/2025, 8:05:14 pm
+# Last updated: 17/6/2025, 8:05:44 pm
+mod = 10 ** 9 + 7
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        if n == 1: return 5
-        mod = 10 ** 9 + 7
-
-        ret = pow(5, (n + 1) // 2, mod)
-        ret = ret * pow(4, n // 2, mod) % mod
-
-        return ret
+        return pow(5, (n + 1) // 2, mod) * pow(4, n // 2, mod) % mod
