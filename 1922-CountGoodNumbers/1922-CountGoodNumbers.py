86d7429df11ebada2fa46361b968452074ab84a7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-17 20:09:30 +0530

    File updated at 17/6/2025, 8:09:28 pm

--- 1922-CountGoodNumbers/1922-CountGoodNumbers.py
+++ 1922-CountGoodNumbers/1922-CountGoodNumbers.py
@@ -1,5 +1,18 @@
-# Last updated: 17/6/2025, 8:05:44 pm
+# Last updated: 17/6/2025, 8:09:28 pm
 mod = 10 ** 9 + 7
+
+def cPow(x, n):
+    if n == 0: return 1
+    if n == 1: return x
+    half = n // 2
+
+    ans = cPow(x, half)
+    ans = ans * ans % mod
+
+    if n % 2:
+        ans = ans * x % mod
+    return ans
+
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        return pow(5, (n + 1) // 2, mod) * pow(4, n // 2, mod) % mod
+        return cPow(5, (n + 1) // 2) * cPow(4, n // 2) % mod
