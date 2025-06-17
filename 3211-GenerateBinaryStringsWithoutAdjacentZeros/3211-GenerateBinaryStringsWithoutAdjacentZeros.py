23bac8640db05d0a8b3ca0d9ef53af4a21a62841
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-17 20:21:05 +0530

    File updated at 17/6/2025, 8:21:02 pm

--- 3211-GenerateBinaryStringsWithoutAdjacentZeros/3211-GenerateBinaryStringsWithoutAdjacentZeros.py
+++ 3211-GenerateBinaryStringsWithoutAdjacentZeros/3211-GenerateBinaryStringsWithoutAdjacentZeros.py
@@ -1,23 +1,24 @@
-# Last updated: 17/6/2025, 8:19:43 pm
+# Last updated: 17/6/2025, 8:21:02 pm
+@cache
+def rec(is_one, index):
+    if index == 0: return ['']
+    ans = []
+    index -= 1
+    if is_one:
+        curr = rec(False, index)
+        for val in curr:
+            ans.append('0' + val)
+        curr = rec(True, index)
+        for val in curr:
+            ans.append('1' + val)
+    else:
+        curr = rec(True, index)
+        for val in curr:
+            ans.append('1' + val)
+    return ans
+
 class Solution:
     def validStrings(self, n: int) -> List[str]:
         ret = []
         
-        @cache
-        def rec(is_one, index):
-            if index == n: return ['']
-            ans = []
-            if is_one:
-                curr = rec(False, index + 1)
-                for val in curr:
-                    ans.append('0' + val)
-                curr = rec(True, index + 1)
-                for val in curr:
-                    ans.append('1' + val)
-            else:
-                curr = rec(True, index + 1)
-                for val in curr:
-                    ans.append('1' + val)
-            return ans
-        
-        return rec(True, 0)
+        return rec(True, n)
