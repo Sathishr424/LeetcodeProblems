58fe136ce17eff3b7e3e7a375b870c9e150da5f7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-17 10:31:42 +0530

    File updated at 17/6/2025, 10:31:41 am

--- 699-FallingSquares/699-FallingSquares.py
+++ 699-FallingSquares/699-FallingSquares.py
@@ -1,4 +1,4 @@
-# Last updated: 17/6/2025, 10:29:11 am
+# Last updated: 17/6/2025, 10:31:41 am
 class Solution:
     def fallingSquares(self, pos: List[List[int]]) -> List[int]:
         n = len(pos)
@@ -9,7 +9,7 @@ class Solution:
         def collision(left, right):
             if left[0] > right[0]:
                 left, right = right, left
-            # print(left, right)
+
             if right[0] >= left[0] and right[0] < left[1]:
                 return True
             
@@ -18,14 +18,13 @@ class Solution:
         max_height = 0
         for i in range(n):
             height = pos[i][1]
-            for j in range(len(plane)):
+            index = plane.bisect_left((pos[i][0] + pos[i][1], 0, 0))
+            for j in range(min(len(plane)-1, index), -1, -1):
                 if collision((pos[i][0], pos[i][0] + pos[i][1]), plane[j]):
-                    # print('collision', plane[j], pos[i])
                     height = max(height, plane[j][2] + pos[i][1])
             
             max_height = max(max_height, height)
             plane.add((pos[i][0], pos[i][0] + pos[i][1], height))
-            # print(plane, height)
             ret.append(max_height)
         
         return ret
