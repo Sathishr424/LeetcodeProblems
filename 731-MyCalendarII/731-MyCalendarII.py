670c70b577fedc176f357c0d0fab084d861f33b2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-17 08:57:46 +0530

    File updated at 17/6/2025, 8:57:45 am

--- 731-MyCalendarII/731-MyCalendarII.py
+++ 731-MyCalendarII/731-MyCalendarII.py
@@ -1,43 +1,30 @@
-# Last updated: 17/6/2025, 8:50:06 am
+# Last updated: 17/6/2025, 8:57:45 am
 class MyCalendarTwo:
     def __init__(self):
         self.arr = SortedList()
 
     def book(self, startTime: int, endTime: int) -> bool:
-        # (1, 5) (4, 8)
-        # (4, 8) (5, 7)
         def collision(left, right):
-            res = False
             if left[0] > right[0]:
                 left, right = right, left
             
             if right[0] >= left[0] and right[0] < left[1]:
-                res = True
-            
-            # print(left, right, res)
+                return True
 
-            return res
+            return False
         
         curr = (startTime, endTime)
         intersect = -1
-        res = True
         index = self.arr.bisect_left((endTime, 0))
-        # print(index, curr)
-        # print(self.arr)
-        for i in range(min(index, len(self.arr) - 1), -1, -1):
-            # if self.arr[i][1] <= startTime: break
-            if collision(self.arr[i], curr):
-                if intersect != -1 and collision(self.arr[intersect], self.arr[i]):
-                    res = False
-                    break
-                intersect = i
-        
-        # print(self.arr)
-        # print((startTime, endTime), res, '====>')
 
-        if res:
-            self.arr.add(curr)
-        return res
+        for i in range(min(index, len(self.arr) - 1), -1, -1):
+            if collision(curr, self.arr[i]):
+                if intersect != -1 and collision(self.arr[intersect], self.arr[i]):
+                    return False
+                intersect = i
+
+        self.arr.add(curr)
+        return True
 
 
 # Your MyCalendarTwo object will be instantiated and called as such:
