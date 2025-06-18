9d5a3678870ec0106ee2753d43ee56860ada1a31
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-19 00:10:41 +0530

    File updated at 19/6/2025, 12:10:38 am

--- 715-RangeModule/715-RangeModule.py
+++ 715-RangeModule/715-RangeModule.py
@@ -1,9 +1,9 @@
-# Last updated: 19/6/2025, 12:06:58 am
+# Last updated: 19/6/2025, 12:10:38 am
 N = 10**9
 cmax = lambda x, y: x if x > y else y
 class SegNode:
     def __init__(self, l, r):
-        self.max = 0
+        self.track = False
         self.l = l
         self.r = r
         self.lazy = -1
@@ -18,14 +18,14 @@ class SegTree:
         if node.r < s or node.l > e: return
         if node.l >= s and node.r <= e:
             node.lazy = 0
-            node.max = 0
+            node.track = False
             return
         
         self.push(node)
         self.remove(node.left, s, e)
         self.remove(node.right, s, e)
 
-        node.max = min(node.left.max, node.right.max)
+        node.track = node.left.track and node.right.track
     
     def push(self, node):
         mid = (node.l + node.r) // 2
@@ -35,16 +35,17 @@ class SegTree:
             node.right = SegNode(mid+1, node.r)
         
         if node.lazy != -1:
-            node.left.max = node.lazy
-            node.right.max = node.lazy
-            node.left.lazy = node.lazy
-            node.right.lazy = node.lazy
+            val = True if node.lazy == 1 else False
+            node.left.track = val
+            node.right.track = val
+            node.left.lazy = val
+            node.right.lazy = val
             node.lazy = -1
 
     def query(self, node, s, e):
         if node.r < s or node.l > e: return True
         if node.l >= s and node.r <= e:
-            return True if node.max >= 1 else False
+            return node.track
         
         self.push(node)
         return self.query(node.left, s, e) and self.query(node.right, s, e)
@@ -53,14 +54,14 @@ class SegTree:
         if node.r < s or node.l > e: return
         if node.l >= s and node.r <= e:
             node.lazy = 1
-            node.max = 1
+            node.track = True
             return
         
         self.push(node)
         self.add(node.left, s, e)
         self.add(node.right, s, e)
 
-        node.max = min(node.left.max, node.right.max)
+        node.track = node.left.track and node.right.track
     
 class RangeModule:
     def __init__(self):
