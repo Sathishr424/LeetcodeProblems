6cbfde5210c0af84ceed38d6d8354d70e5672cac
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-19 08:58:00 +0530

    File updated at 19/6/2025, 8:57:56 am

--- 3343-CountNumberofBalancedPermutations/3343-CountNumberofBalancedPermutations.py
+++ 3343-CountNumberofBalancedPermutations/3343-CountNumberofBalancedPermutations.py
@@ -1,4 +1,4 @@
-# Last updated: 19/6/2025, 8:54:49 am
+# Last updated: 19/6/2025, 8:57:56 am
 mod = 10 ** 9 + 7
 @cache
 def inverse(x):
@@ -32,22 +32,26 @@ class Solution:
 
         combined = f * f2 % mod
 
-        @cache
+        dp = [[[-1] * 361 for _ in range(half+1)] for _ in range(11)]
+
         def howmany(index, cnt, tot):
+            if dp[index][cnt][tot] != -1: return dp[index][cnt][tot]
             if index == 10 and cnt == 0 and tot == 0:
                 return combined
             if index == 10 or tot < 0: return 0
             ans = 0
             r = uniq[index]
             
+            new_tot = tot
+            new_cnt = cnt
             for l in range(r+1):
-                if tot < 0 or cnt < 0:
+                if new_tot < 0 or new_cnt < 0:
                     break
-                ans += inverse(l) * inverse(r-l) * howmany(index+1, cnt, tot)
+                ans += inverse(l) * inverse(r-l) * howmany(index+1, new_cnt, new_tot)
                 ans %= mod
-                tot -= index
-                cnt -= 1
-            
+                new_tot -= index
+                new_cnt -= 1
+            dp[index][cnt][tot] = ans
             return ans
             
         return howmany(0, half, target)
