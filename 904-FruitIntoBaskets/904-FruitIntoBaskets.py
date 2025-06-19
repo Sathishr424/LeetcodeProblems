42dc0ae91a836e392927d64dff2c087cb7b386ea
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-19 09:42:27 +0530

    Create 904-FruitIntoBaskets

---
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.py
@@ -0,0 +1,46 @@
+# Last updated: 19/6/2025, 9:42:23 am
+class Solution:
+    def totalFruit(self, f: List[int]) -> int:
+        n = len(f)
+
+        a = f[0]
+        i = 1
+        while i < n and f[i] == a:
+            i += 1
+        
+        if i == n: return n
+        b = f[i]
+
+        a_cnt = i
+        b_cnt = 1
+        ret = a_cnt + b_cnt
+    
+        cnt = 1
+        prev = f[i]
+
+        for i in range(i+1, n):
+            if f[i] == a:
+                a_cnt += 1
+            elif f[i] == b:
+                b_cnt += 1
+            else:
+                if prev == a:
+                    a_cnt = cnt
+                    b_cnt = 1
+                    b = f[i]
+                else:
+                    a_cnt = 1
+                    b_cnt = cnt
+                    a = f[i]
+            
+            if f[i] == prev:
+                cnt += 1
+            else:
+                prev = f[i]
+                cnt = 1
+            
+            ret = max(a_cnt + b_cnt, ret)
+        
+        return ret
+
+        
