bae57a87a39b4d6a197d025860a669a04dbbba5e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-19 10:03:42 +0530

    File updated at 19/6/2025, 10:03:38 am

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.py
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.py
@@ -1,39 +1,47 @@
-# Last updated: 19/6/2025, 10:01:20 am
+# Last updated: 19/6/2025, 10:03:38 am
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def totalFruit(self, f: List[int]) -> int:
         n = len(f)
 
+        a = f[0]
         i = 1
-        while i < n and f[i] == f[0]:
+        while i < n and f[i] == a:
             i += 1
+        
         if i == n: return n
+        b = f[i]
 
-        basket = {}
-        basket[f[0]] = i-1
-        basket[f[i]] = i
-        ret = i + 1
-        cnt = i + 1
+        a_cnt = i
+        b_cnt = 1
+        ret = a_cnt + b_cnt
+    
+        cnt = 1
+        prev = f[i]
 
         for i in range(i+1, n):
-            if f[i] not in basket:
-                x, y = list(basket.keys())
-
-                if basket[x] > basket[y]:
-                    cnt = i - basket[y]
-                    del basket[y]
-                else:
-                    cnt = i - basket[x]
-                    del basket[x]
+            if f[i] == a:
+                a_cnt += 1
+            elif f[i] == b:
+                b_cnt += 1
             else:
-                cnt += 1
+                if prev == a:
+                    a_cnt = cnt
+                    b_cnt = 1
+                    b = f[i]
+                else:
+                    a_cnt = 1
+                    b_cnt = cnt
+                    a = f[i]
             
-            basket[f[i]] = i
-            ret = cmax(ret, cnt)
+            if f[i] == prev:
+                cnt += 1
+            else:
+                prev = f[i]
+                cnt = 1
+            
+            ret = cmax(a_cnt + b_cnt, ret)
         
         return ret
-
-
-
 
         
