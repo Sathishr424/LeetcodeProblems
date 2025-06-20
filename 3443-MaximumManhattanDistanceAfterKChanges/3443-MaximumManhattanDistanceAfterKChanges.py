1ba29766d3479f7ba0447f0edcf5762146959fc0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-20 08:27:44 +0530

    File updated at 20/6/2025, 8:27:39 am

--- 3443-MaximumManhattanDistanceAfterKChanges/3443-MaximumManhattanDistanceAfterKChanges.py
+++ 3443-MaximumManhattanDistanceAfterKChanges/3443-MaximumManhattanDistanceAfterKChanges.py
@@ -1,27 +1,30 @@
-# Last updated: 20/6/2025, 8:25:03 am
+# Last updated: 20/6/2025, 8:27:39 am
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
         n = len(grid)
         inf = 200
         dis = [[inf] * n for _ in range(n)]
         DIR = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        stack = deque([])
+        stack = []
 
         for i in range(n):
             for j in range(n):
                 if grid[i][j] == 1:
-                    stack.append((i, j, 0))
-        
+                    stack.append((i, j))
+        d = 0
         while stack:
-            i, j, d = stack.popleft()
-            if dis[i][j] <= d: continue
-            dis[i][j] = d
+            new_stack = []
+            for i, j in stack:
+                if dis[i][j] <= d: continue
+                dis[i][j] = d
 
-            for i2, j2 in DIR:
-                i2 += i
-                j2 += j
-                if 0 <= i2 < n and 0 <= j2 < n and grid[i2][j2] == 0:
-                    stack.append((i2, j2, d+1))
+                for i2, j2 in DIR:
+                    i2 += i
+                    j2 += j
+                    if 0 <= i2 < n and 0 <= j2 < n and grid[i2][j2] == 0:
+                        new_stack.append((i2, j2))
+            d += 1
+            stack = new_stack
 
         ret = -1
         for i in range(n):
