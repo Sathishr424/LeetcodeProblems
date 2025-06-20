b77c38223e24dd2c5e824f3631b53e849f3a75ca
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-20 08:25:08 +0530

    File updated at 20/6/2025, 8:25:03 am

--- 3443-MaximumManhattanDistanceAfterKChanges/3443-MaximumManhattanDistanceAfterKChanges.py
+++ 3443-MaximumManhattanDistanceAfterKChanges/3443-MaximumManhattanDistanceAfterKChanges.py
@@ -1,8 +1,8 @@
-# Last updated: 20/6/2025, 8:24:18 am
+# Last updated: 20/6/2025, 8:25:03 am
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        inf = float('inf')
+        inf = 200
         dis = [[inf] * n for _ in range(n)]
         DIR = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         stack = deque([])
@@ -22,7 +22,7 @@ class Solution:
                 j2 += j
                 if 0 <= i2 < n and 0 <= j2 < n and grid[i2][j2] == 0:
                     stack.append((i2, j2, d+1))
-        # [print(row) for row in dis]
+
         ret = -1
         for i in range(n):
             for j in range(n):
