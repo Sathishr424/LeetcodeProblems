5535dae4d6556143e9ac4928e2ddadfa61c9e899
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-20 20:23:18 +0530

    File updated at 20/6/2025, 8:23:12 pm

--- 3443-MaximumManhattanDistanceAfterKChanges/3443-MaximumManhattanDistanceAfterKChanges.py
+++ 3443-MaximumManhattanDistanceAfterKChanges/3443-MaximumManhattanDistanceAfterKChanges.py
@@ -1,30 +1,31 @@
-# Last updated: 20/6/2025, 8:38:00 am
-cmax = lambda x, y: x if x > y else y
-inf = 200
+# Last updated: 20/6/2025, 8:23:12 pm
 class Solution:
-    def maxDistance(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        dis = [[inf] * n for _ in range(n)]
-        DIR = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        stack = []
+    def maxDistance(self, s: str, k: int) -> int:
+        n = len(s)
+        ret = 0
 
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    stack.append((i, j))
-        d = 1
-        ret = -1
-        while stack:
-            new_stack = []
-            for i, j in stack:
-                for i2, j2 in DIR:
-                    i2 += i
-                    j2 += j
-                    if 0 <= i2 < n and 0 <= j2 < n and grid[i2][j2] == 0 and d < dis[i2][j2]:
-                        ret = cmax(ret, d)
-                        dis[i2][j2] = d
-                        new_stack.append((i2, j2))
-            stack = new_stack
-            d += 1
-
-        return -1 if ret == inf else ret
+        def helper(h, v, k):
+            ans = 0
+            x = 0
+            y = 0
+            for char in s:
+                if char in 'WE':
+                    if k > 0 and char != h:
+                        x += 1 if h == 'E' else -1
+                        k -= 1
+                    else:
+                        x += 1 if char == 'E' else -1
+                elif char in 'SN':
+                    if k > 0 and char != v:
+                        y += 1 if v == 'N' else -1
+                        k -= 1
+                    else:
+                        y += 1 if char == 'N' else -1
+                ans = max(ans, abs(x) + abs(y))
+            return ans
+        
+        ret = max(ret, helper('W', 'N', k))
+        ret = max(ret, helper('W', 'S', k))
+        ret = max(ret, helper('E', 'N', k))
+        ret = max(ret, helper('E', 'S', k))
+        return ret
