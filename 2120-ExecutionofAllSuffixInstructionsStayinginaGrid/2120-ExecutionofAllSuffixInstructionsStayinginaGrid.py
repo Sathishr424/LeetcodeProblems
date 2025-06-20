d91587ed071e8a7aad416b128c4e05dcd793c131
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-20 10:38:24 +0530

    File updated at 20/6/2025, 10:38:19 am

--- 2120-ExecutionofAllSuffixInstructionsStayinginaGrid/2120-ExecutionofAllSuffixInstructionsStayinginaGrid.py
+++ 2120-ExecutionofAllSuffixInstructionsStayinginaGrid/2120-ExecutionofAllSuffixInstructionsStayinginaGrid.py
@@ -1,10 +1,11 @@
-# Last updated: 20/6/2025, 10:36:11 am
+# Last updated: 20/6/2025, 10:38:19 am
 class Solution:
     def executeInstructions(self, n: int, startPos: List[int], s: str) -> List[int]:
         m = len(s)
 
         pos = [[startPos[0], startPos[1]] for _ in range(m)]
-        ret = [-1] * m
+        ret = [m-i for i in range(m)]
+        used = [0] * m
         
         for i in range(m):
             d = s[i]
@@ -15,15 +16,12 @@ class Solution:
             elif d == 'U': y -= 1
             elif d == 'D': y += 1
             for j in range(i+1):
-                if ret[j] != -1: continue
+                if used[j]: continue
                 pos[j][0] += y
                 pos[j][1] += x
 
                 if pos[j][0] == n or pos[j][0] == -1 or pos[j][1] == -1 or pos[j][1] == n:
                     ret[j] = i-j
-        
-        for i in range(m):
-            if ret[i] == -1:
-                ret[i] = m-i
+                    used[j] = 1
         
         return ret
