7db555026617cff3ea6b832a3340b19c6e6f106b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-21 09:24:42 +0530

    File updated at 21/6/2025, 9:24:36 am

--- 3085-MinimumDeletionstoMakeStringK-Special/3085-MinimumDeletionstoMakeStringK-Special.py
+++ 3085-MinimumDeletionstoMakeStringK-Special/3085-MinimumDeletionstoMakeStringK-Special.py
@@ -1,20 +1,23 @@
-# Last updated: 21/6/2025, 9:22:29 am
+# Last updated: 21/6/2025, 9:24:36 am
 class Solution:
     def minimumDeletions(self, word: str, k: int) -> int:
-        freq = defaultdict(int)
+        freq = [0] * 26
         for char in word:
-            freq[char] += 1
+            freq[ord(char) - 97] += 1
 
-        arr = sorted(list(freq.values()))
+        arr = sorted([freq[i] for i in range(26) if freq[i]])
+        n = len(arr)
+        dp = [[-1] * n for _ in range(n)]
 
-        @cache
         def rec(l, r):
+            if dp[l][r] != -1: return dp[l][r]
             if l == r: return 0
 
             diff = arr[r] - arr[l]
             if diff > k:
-                return min(rec(l+1, r) + arr[l], rec(l, r-1) + diff - k)
+                dp[l][r] = min(rec(l+1, r) + arr[l], rec(l, r-1) + diff - k)
             else:
-                return 0
+                dp[l][r] = 0
+            return dp[l][r]
         
-        return rec(0, len(arr)-1)
+        return rec(0, n-1)
