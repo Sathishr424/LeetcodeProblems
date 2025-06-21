76f2e69e3295a1f2c4079cfc6be034c4f91cab30
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-21 10:51:56 +0530

    File updated at 21/6/2025, 10:51:50 am

--- 3085-MinimumDeletionstoMakeStringK-Special/3085-MinimumDeletionstoMakeStringK-Special.py
+++ 3085-MinimumDeletionstoMakeStringK-Special/3085-MinimumDeletionstoMakeStringK-Special.py
@@ -1,4 +1,4 @@
-# Last updated: 21/6/2025, 10:50:50 am
+# Last updated: 21/6/2025, 10:51:50 am
 class Solution:
     def minimumDeletions(self, word: str, k: int) -> int:
         arr = sorted(Counter(word).values())
@@ -7,10 +7,11 @@ class Solution:
         p = 0
         for i in range(n):
             best = p
-            j = n-1
-            while j > i and arr[j] - arr[i] > k:
-                best += arr[j] - arr[i] - k
-                j -= 1
+            for j in range(n-1, i, -1):
+                diff = arr[j] - arr[i]
+                if diff > k:
+                    best += diff - k
+                else: break
             ret = min(ret, best)
             p += arr[i]
 
