9ce38c8b5cae5cf814b72fc8340d884add61b023
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-21 23:34:51 +0530

    Create 3589-CountPrime-GapBalancedSubarrays

---
+++ 3589-CountPrime-GapBalancedSubarrays/3589-CountPrime-GapBalancedSubarrays.py
@@ -0,0 +1,42 @@
+# Last updated: 21/6/2025, 11:34:44 pm
+N = 10**4 * 5 + 1
+
+is_prime = [1] * N
+is_prime[1] = 0
+
+for i in range(1, int(N ** 0.5) + 1):
+    if not is_prime[i]: continue
+
+    for j in range(i*i, N, i):
+        is_prime[j] = 0
+
+class Solution:
+    def primeSubarray(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        # 2, 3, 4, 2
+        cnt = 0
+        stack = deque([])
+        ret = 0
+        prev = 0
+        cnts = defaultdict(int)
+        sl = SortedList()
+        # [5, 2, 5, 7]
+        for i in range(n):
+            if is_prime[nums[i]]:
+                while stack and abs(nums[i] - sl[0]) > k:
+                    while cnts[sl[0]]:
+                        cnts[stack.popleft()[0]] -= 1
+                    sl.remove(sl[0])
+                while stack and abs(nums[i] - sl[-1]) > k:
+                    while cnts[sl[-1]]:
+                        cnts[stack.popleft()[0]] -= 1
+                    sl.remove(sl[-1])
+                sl.add(nums[i])
+                cnts[nums[i]] += 1
+                stack.append((nums[i], prev, i))
+                prev = i+1
+            # print(nums[i], stack)
+            if len(stack) >= 2:
+                ret += stack[-2][2] - stack[0][1] + 1
+
+        return ret
