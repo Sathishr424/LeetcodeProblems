5403a7317105ef5cc7b5d3e919385168d95fd878
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-22 03:29:15 +0530

    File updated at 22/6/2025, 3:29:07 am

--- 3588-FindMaximumAreaofaTriangle/3588-FindMaximumAreaofaTriangle.py
+++ 3588-FindMaximumAreaofaTriangle/3588-FindMaximumAreaofaTriangle.py
@@ -1,4 +1,4 @@
-# Last updated: 22/6/2025, 3:28:15 am
+# Last updated: 22/6/2025, 3:29:08 am
 class Solution:
     def maxArea(self, coords: List[List[int]]) -> int:
         n = len(coords)
@@ -14,8 +14,7 @@ class Solution:
         
         h = sorted(x.keys())
         v = sorted(y.keys())
-        # print(dict(x))
-        # print(dict(y))
+        
         ret = -1
         for x_ in h:
             if len(x[x_]) > 1:
@@ -23,20 +22,17 @@ class Solution:
                 b = x[x_][-1]
 
                 base = abs(a-b)
-                # print('X', x_, base, h[0], h[-1])
                 if abs(x_ - h[0]) > 0:
                     ret = max(base * abs(x_ - h[0]), ret)
                 if abs(x_ - h[-1]) > 0:
                     ret = max(ret, base * abs(x_ - h[-1]))
-                # print((base, abs(x_ - h[0])), base * abs(x_ - h[0]), (base, abs(x_ - h[-1])), base * abs(x_ - h[-1]))
-        
+
         for y_ in v:
             if len(y[y_]) > 1:
                 a = y[y_][0]
                 b = y[y_][-1]
 
                 base = abs(a-b)
-                # print('Y', y_, base, v[0], v[-1])
                 if abs(y_ - v[0]) > 0:
                     ret = max(base * abs(y_ - v[0]), ret)
                 if abs(y_ - v[-1]) > 0:
