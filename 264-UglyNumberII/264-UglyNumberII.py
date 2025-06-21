968fe945beac321099378159afce7e1f66f5d337
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-21 14:57:45 +0530

    File updated at 21/6/2025, 2:57:39 pm

--- 264-UglyNumberII/264-UglyNumberII.py
+++ 264-UglyNumberII/264-UglyNumberII.py
@@ -1,20 +1,36 @@
-# Last updated: 21/6/2025, 2:34:52 pm
-ugly = []
-index = 1
-heap = [1]
-there = {}
-num = 1
-while index < 1691:
-    num = heapq.heappop(heap)
-    if num in there: continue
-    ugly.append(num)
-    there[num] = 1
-    index += 1
-    heapq.heappush(heap, num * 2)
-    heapq.heappush(heap, num * 3)
-    heapq.heappush(heap, num * 5)
-ugly.append(num)
-
+# Last updated: 21/6/2025, 2:57:39 pm
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        return ugly[n-1]
+        x = [2]
+        y = [3]
+        z = [5]
+        
+        x_index = 0
+        y_index = 0
+        z_index = 0
+        num = 1
+        n -= 1
+        while n:
+            if x[x_index] == y[y_index]:
+                x_index += 1
+            if x[x_index] == z[z_index]:
+                x_index += 1
+            if y[y_index] == z[z_index]:
+                y_index += 1
+            
+            if x[x_index] < y[y_index] and x[x_index] < z[z_index]:
+                num = x[x_index]
+                x_index += 1
+            elif y[y_index] < z[z_index]:
+                num = y[y_index]
+                y_index += 1
+            else:
+                num = z[z_index]
+                z_index += 1
+
+            x.append(num * 2)
+            y.append(num * 3)
+            z.append(num * 5)
+            n -= 1
+
+        return num
