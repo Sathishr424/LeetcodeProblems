6de6af435fb19ab90363f28f3377308f2b238df2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-21 18:39:47 +0530

    File updated at 21/6/2025, 6:39:40 pm

--- 264-UglyNumberII/264-UglyNumberII.py
+++ 264-UglyNumberII/264-UglyNumberII.py
@@ -1,18 +1,19 @@
-# Last updated: 21/6/2025, 6:38:50 pm
+# Last updated: 21/6/2025, 6:39:40 pm
+ugly = [1]
+i = 0
+j = 0
+k = 0
+
+while 1690 > len(ugly):
+    num = min(ugly[i] * 2, ugly[j] * 3, ugly[k] * 5)
+    ugly.append(num)
+    if num == ugly[i] * 2:
+        i += 1
+    if num == ugly[j] * 3:
+        j += 1
+    if num == ugly[k] * 5:
+        k += 1
+
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        ugly = [1]
-        i = 0
-        j = 0
-        k = 0
-        
-        while n > len(ugly):
-            num = min(ugly[i] * 2, ugly[j] * 3, ugly[k] * 5)
-            ugly.append(num)
-            if num == ugly[i] * 2:
-                i += 1
-            if num == ugly[j] * 3:
-                j += 1
-            if num == ugly[k] * 5:
-                k += 1
-        return ugly[-1]
+        return ugly[n-1]
