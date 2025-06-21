29c901e5d1feba0824acdf2a95861dba9020b0eb
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-22 03:35:07 +0530

    File updated at 22/6/2025, 3:34:59 am

--- 3588-FindMaximumAreaofaTriangle/3588-FindMaximumAreaofaTriangle.py
+++ 3588-FindMaximumAreaofaTriangle/3588-FindMaximumAreaofaTriangle.py
@@ -1,41 +1,52 @@
-# Last updated: 22/6/2025, 3:29:08 am
+# Last updated: 22/6/2025, 3:34:59 am
+inf = float('inf')
+class Node:
+    def __init__(self):
+        self.min = inf
+        self.max = 0
+        self.cnt = 0
 class Solution:
     def maxArea(self, coords: List[List[int]]) -> int:
         n = len(coords)
 
         h = []
         v = []
-        x = defaultdict(SortedList)
-        y = defaultdict(SortedList)
+        x = defaultdict(Node)
+        y = defaultdict(Node)
 
         for x_, y_ in coords:
-            x[x_].add(y_)
-            y[y_].add(x_)
+            x[x_].min = min(x[x_].min, y_)
+            x[x_].max = max(x[x_].max, y_)
+            x[x_].cnt += 1
+
+            y[y_].min = min(y[y_].min, x_)
+            y[y_].max = max(y[y_].max, x_)
+            y[y_].cnt += 1
         
         h = sorted(x.keys())
         v = sorted(y.keys())
-        
+
         ret = -1
         for x_ in h:
-            if len(x[x_]) > 1:
-                a = x[x_][0]
-                b = x[x_][-1]
+            if x[x_].cnt == 1: continue
+            a = x[x_].min
+            b = x[x_].max
 
-                base = abs(a-b)
-                if abs(x_ - h[0]) > 0:
-                    ret = max(base * abs(x_ - h[0]), ret)
-                if abs(x_ - h[-1]) > 0:
-                    ret = max(ret, base * abs(x_ - h[-1]))
+            base = b-a
+            if x_ - h[0] > 0:
+                ret = max(base * (x_ - h[0]), ret)
+            if h[-1] - x_ > 0:
+                ret = max(ret, base * (h[-1] - x_))
 
         for y_ in v:
-            if len(y[y_]) > 1:
-                a = y[y_][0]
-                b = y[y_][-1]
+            if y[y_].cnt == 1: continue
+            a = y[y_].min
+            b = y[y_].max
 
-                base = abs(a-b)
-                if abs(y_ - v[0]) > 0:
-                    ret = max(base * abs(y_ - v[0]), ret)
-                if abs(y_ - v[-1]) > 0:
-                    ret = max(ret, base * abs(y_ - v[-1]))
+            base = b-a
+            if y_ - v[0] > 0:
+                ret = max(base * (y_ - v[0]), ret)
+            if v[-1] - y_ > 0:
+                ret = max(ret, base * (v[-1] - y_))
         
         return ret
