3bacc9dd74e50dc791cb68f47b22b96e60712a2c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-22 07:48:32 +0530

    File updated at 22/6/2025, 7:48:31 am

--- 2928-DistributeCandiesAmongChildrenI/2928-DistributeCandiesAmongChildrenI.py
+++ 2928-DistributeCandiesAmongChildrenI/2928-DistributeCandiesAmongChildrenI.py
@@ -1,9 +1,9 @@
-# Last updated: 22/6/2025, 7:48:13 am
+# Last updated: 22/6/2025, 7:48:31 am
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
         # *****
         limit += 1
-        @cache
+
         def rec(k, rem):
             if k == 0:
                 if rem == 0: return 1
