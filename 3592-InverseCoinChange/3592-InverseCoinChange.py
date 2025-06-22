60b652ecc469233f6298fae08b4d2b3cbc940118
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-23 03:33:59 +0530

    File updated at 23/6/2025, 3:33:57 am

--- 3592-InverseCoinChange/3592-InverseCoinChange.py
+++ 3592-InverseCoinChange/3592-InverseCoinChange.py
@@ -1,9 +1,6 @@
-# Last updated: 23/6/2025, 3:33:35 am
+# Last updated: 23/6/2025, 3:33:57 am
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        coins.sort()
-        n = len(coins)
-        
         dp = [0] * (amount + 1)
         dp[0] = 1
 
