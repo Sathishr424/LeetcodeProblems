b4d3754e29d2a4f913961fb513c468e8762928e8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-23 04:25:28 +0530

    File updated at 23/6/2025, 4:25:26 am

--- 3592-InverseCoinChange/3592-InverseCoinChange.py
+++ 3592-InverseCoinChange/3592-InverseCoinChange.py
@@ -1,11 +1,36 @@
-# Last updated: 23/6/2025, 3:33:57 am
+# Last updated: 23/6/2025, 4:25:26 am
 class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount + 1)
-        dp[0] = 1
+    def findCoins(self, numWays: List[int]) -> List[int]:
+        n = len(numWays)
 
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        coins = []
+        for i in range(n):
+            if numWays[i]:
+                coins.append(i+1)
+        # print(dp)
+        used = {}
         for coin in coins:
-            for tot in range(amount-coin+1):
-                dp[tot + coin] += dp[tot]
+            possible = True
+            for tot in range(n-coin+1):
+                if dp[tot] == 0: continue
+                plus = tot + coin
+                if dp[plus] + dp[tot] > numWays[plus-1]:
+                    possible = False
+                    break
+            
+            if possible:
+                for tot in range(n-coin+1):
+                    if dp[tot] == 0: continue
+                    plus = tot + coin
+                    dp[plus] += dp[tot]
+                    used[coin] = 1
+            
+        # print(dp)
+        for i in range(1, n+1):
+            if dp[i] != numWays[i-1]: return []
         
-        return dp[amount]
+        return list(used.keys())
+
+
