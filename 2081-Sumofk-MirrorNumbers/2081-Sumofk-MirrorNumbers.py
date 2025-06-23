d897c3d7741a34cb35b07221e064b63117c6585f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-23 11:59:27 +0530

    File updated at 23/6/2025, 11:59:24 am

--- 2081-Sumofk-MirrorNumbers/2081-Sumofk-MirrorNumbers.py
+++ 2081-Sumofk-MirrorNumbers/2081-Sumofk-MirrorNumbers.py
@@ -1,4 +1,4 @@
-# Last updated: 23/6/2025, 11:39:27 am
+# Last updated: 23/6/2025, 11:59:24 am
 def is_palindrome(num):
     st = str(num)
     return st == st[::-1]
@@ -10,40 +10,43 @@ def getPalBaseNum(num, pal, base):
         pal //= 10
     return num
 
+p = [[0] * 31 for _ in range(10)]
+for k in range(2, 10):
+    n = 0
+    stack = deque([])
+    
+    for i in range(1, k):
+        stack.append((i, i, 1))
+    
+    wait_list = []
+    
+    prev = 1
+    while stack:
+        num, base, l = stack.popleft()
+        if l > prev:
+            for new_num in wait_list:
+                n += 1
+                p[k][n] = p[k][n-1] + new_num
+                if n == 30: break
+            if n == 30: break
+            wait_list = []
+        
+        new_num = getPalBaseNum(num, base // 10, k)
+        if is_palindrome(new_num):
+            n += 1
+            p[k][n] = p[k][n-1] + new_num
+            if n == 30: break
+        
+        new_num = getPalBaseNum(num, base, k)
+        if is_palindrome(new_num):
+            prev = l
+            wait_list.append(new_num)
+        
+        for i in range(k):
+            new_base = base * 10 + i
+            new_num = num * k + i
+            stack.append((new_num, new_base, l + 1))
+
 class Solution:
     def kMirror(self, k: int, n: int) -> int:
-        stack = deque([])
-        ret = 0
-        
-        for i in range(1, k):
-            stack.append((i, i, 1))
-        
-        wait_list = []
-        
-        prev = 1
-        while stack:
-            num, base, l = stack.popleft()
-            if l > prev:
-                for new_num in wait_list:
-                    ret += new_num
-                    n -= 1
-                    if n == 0: return ret
-                wait_list = []
-            
-            new_num = getPalBaseNum(num, base // 10, k)
-            if is_palindrome(new_num):
-                ret += new_num
-                n -= 1
-                if n == 0: break
-            
-            new_num = getPalBaseNum(num, base, k)
-            if is_palindrome(new_num):
-                prev = l
-                wait_list.append(new_num)
-            
-            for i in range(k):
-                new_base = base * 10 + i
-                new_num = num * k + i
-                stack.append((new_num, new_base, l + 1))
-        
-        return ret
+        return p[k][n]
