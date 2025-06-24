fd0775f8d3a978bd4b2eca5df18d42e145f13f54
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-25 00:22:51 +0530

    File updated at 25/6/2025, 12:22:48 am

--- 55-JumpGame/55-JumpGame.py
+++ 55-JumpGame/55-JumpGame.py
@@ -1,4 +1,4 @@
-# Last updated: 25/6/2025, 12:15:25 am
+# Last updated: 25/6/2025, 12:22:48 am
 inf = float('inf')
 cmin = lambda x, y: x if x < y else y
 
@@ -9,10 +9,21 @@ class Solution:
         dp = [inf] * n
         dp[0] = 0
 
+        stack = deque([])
+
         for i in range(n):
-            for j in range(nums[i]):
-                index = i + j + 1
-                if index == n: break
+            while stack and stack[0] < i:
+                stack.popleft()
+            
+            if stack: dp[i] = cmin(dp[i], dp[stack[0]])
+            
+            if nums[i]:
+                index = cmin(n - 1, i + nums[i])
                 dp[index] = cmin(dp[index], dp[i] + 1)
+                while stack and stack[-1] < index and dp[stack[-1]] > dp[index]:
+                    stack.pop()
+                
+                if not stack or stack[-1] < index:
+                    stack.append(index)
         
         return dp[n-1]
