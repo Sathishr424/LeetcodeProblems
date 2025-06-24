005d3faeb90899a2e39537b3fa811db7fb96609f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-24 21:08:34 +0530

    Create 3292-MinimumNumberofValidStringstoFormTargetII

---
+++ 3292-MinimumNumberofValidStringstoFormTargetII/3292-MinimumNumberofValidStringstoFormTargetII.cpp
@@ -0,0 +1,24 @@
+// Last updated: 24/6/2025, 9:08:31 pm
+public:
+    int minValidStrings(vector<string>& v, string t) {
+        int m = t.size();
+        vector<int> dp(m);
+
+        for(auto& w: v){
+            int n = w.size();
+            vector<int> p = prefix_function(w+'#'+t);
+            for(int i=0; i<m; i++){
+                dp[i] = max(dp[i], p[i+n+1]);
+            }
+        }
+
+        m--;
+        int a = 0;
+        while(m>=0 and dp[m]>0){
+            m-=dp[m];
+            a++;
+        }
+
+        return m==-1 ? a: -1;
+    }
+};
