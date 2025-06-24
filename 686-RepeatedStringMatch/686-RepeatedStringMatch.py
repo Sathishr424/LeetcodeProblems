aafd1e3a8c0e82ddcb4a4ead51307f43c85456d8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-24 17:36:28 +0530

    File updated at 24/6/2025, 5:36:25 pm

--- 686-RepeatedStringMatch/686-RepeatedStringMatch.py
+++ 686-RepeatedStringMatch/686-RepeatedStringMatch.py
@@ -1,4 +1,4 @@
-# Last updated: 24/6/2025, 5:34:41 pm
+# Last updated: 24/6/2025, 5:36:25 pm
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
         n = len(a)
@@ -7,8 +7,8 @@ class Solution:
         new_a = a + (a * ceil(m / n))
         for i in range(n):
             if a[i] == b[0]:
-                l = m - (n - i)
                 if new_a[i:i+m] == b:
+                    l = m - (n - i)
                     return ceil(l / n) + 1
         
         return -1
