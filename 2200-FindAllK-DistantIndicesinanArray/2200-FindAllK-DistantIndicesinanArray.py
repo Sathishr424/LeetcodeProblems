dff153d32a85c2eeaf49e0078ec100c0b4d6efd9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-24 14:16:43 +0530

    File updated at 24/6/2025, 2:16:40 pm

--- 2200-FindAllK-DistantIndicesinanArray/2200-FindAllK-DistantIndicesinanArray.py
+++ 2200-FindAllK-DistantIndicesinanArray/2200-FindAllK-DistantIndicesinanArray.py
@@ -1,26 +1,24 @@
-# Last updated: 24/6/2025, 2:15:14 pm
-inf = 10 ** 20
+# Last updated: 24/6/2025, 2:16:40 pm
+inf = 2001
+cmin = lambda x, y: x if x < y else y
 class Solution:
     def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
         n = len(nums)
         ret = []
         
         nearest = [0] * n
-
-        prev = inf
-        for i in range(n):
-            if nums[i] == key:
-                prev = i
-            nearest[i] = abs(i - prev)
         
         prev = inf
         for i in range(n-1, -1, -1):
             if nums[i] == key:
                 prev = i
-            nearest[i] = min(nearest[i], abs(prev - i))
+            nearest[i] = abs(i - prev)
         
+        prev = inf
         for i in range(n):
-            if nearest[i] <= k:
-                ret.append(i)
+            if nums[i] == key:
+                prev = i
+            nearest[i] = cmin(nearest[i], abs(prev - i))
+            if nearest[i] <= k: ret.append(i)
         
         return ret
