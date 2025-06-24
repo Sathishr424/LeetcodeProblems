b6ecec41aa5d7ed4d9c16c0986758825deb0b0ea
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-24 17:33:26 +0530

    File updated at 24/6/2025, 5:33:23 pm

--- 686-RepeatedStringMatch/686-RepeatedStringMatch.py
+++ 686-RepeatedStringMatch/686-RepeatedStringMatch.py
@@ -1,16 +1,16 @@
-# Last updated: 24/6/2025, 5:32:51 pm
+# Last updated: 24/6/2025, 5:33:23 pm
 inf = float('inf')
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
         n = len(a)
         m = len(b)
-        
+
         new_a = a + (a * ceil(m / n))
         ret = inf
         for i in range(n):
             if a[i] == b[0]:
                 l = m - (n - i)
                 if new_a[i:i+m] == b:
-                    ret = min(ret, ceil(l / n) + 1)
+                    return ceil(l / n) + 1
         
-        return -1 if ret == inf else ret
+        return -1
