441914ff5b87eb3f30f3d0c3767997ea3acdcf05
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-24 18:05:22 +0530

    File updated at 24/6/2025, 6:05:19 pm

--- 686-RepeatedStringMatch/686-RepeatedStringMatch.py
+++ 686-RepeatedStringMatch/686-RepeatedStringMatch.py
@@ -1,37 +1,37 @@
-# Last updated: 24/6/2025, 6:00:50 pm
-class Solution:
-    def repeatedStringMatch(self, a: str, b: str) -> int:
-        n = len(a)
-        m = len(b)
+# Last updated: 24/6/2025, 6:05:19 pm
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        def check(index):
+            return all(A[(i + index) % len(A)] == x
+                       for i, x in enumerate(B))
 
-        nn = n + (n * ceil(m / n))
+        q = (len(B) - 1) // len(A) + 1
 
-        def getKMP(word):
-            n = len(word)
-            lps = [0] * n
-            
-            i = 1
-            j = 0
-            while i < n:
-                if word[i] == word[j]:
-                    j += 1
-                    lps[i] = j
-                elif j > 0:
-                    j = lps[j - 1]
-                    continue
-                i += 1
-            return lps
-        
-        lps = getKMP(b)
-        j = 0
-        i = 0
-        while i < nn:
-            if a[i % n] == b[j]:
-                j += 1
-                if j == m: return i // n + 1
-            elif j > 0:
-                j = lps[j - 1]
-                continue
-            i += 1
-        
+        p, MOD = 113, 10**9 + 7
+        p_inv = pow(p, MOD-2, MOD)
+        power = 1
+
+        b_hash = 0
+        for x in map(ord, B):
+            b_hash += power * x
+            b_hash %= MOD
+            power = (power * p) % MOD
+
+        a_hash = 0
+        power = 1
+        for i in range(len(B)):
+            a_hash += power * ord(A[i % len(A)])
+            a_hash %= MOD
+            power = (power * p) % MOD
+
+        if a_hash == b_hash and check(0): return q
+
+        power = (power * p_inv) % MOD
+        for i in range(len(B), (q+1) * len(A)):
+            a_hash = (a_hash - ord(A[(i - len(B)) % len(A)])) * p_inv
+            a_hash += power * ord(A[i % len(A)])
+            a_hash %= MOD
+            if a_hash == b_hash and check(i - len(B) + 1):
+                return q if i < q * len(A) else q+1
+
         return -1
