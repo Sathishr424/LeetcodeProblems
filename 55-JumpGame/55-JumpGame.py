49e9506d7043dce30f5f9d0cedd8ac557fa529aa
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-25 00:04:54 +0530

    File updated at 25/6/2025, 12:04:51 am

--- 55-JumpGame/55-JumpGame.py
+++ 55-JumpGame/55-JumpGame.py
@@ -1,14 +1,11 @@
-# Last updated: 25/6/2025, 12:04:01 am
+# Last updated: 25/6/2025, 12:04:51 am
+cmax = lambda x, y: x if x > y else y
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-
         larger = 0
 
-        for i in range(n):
-            if i <= larger:
-                larger = max(larger, i + nums[i])
-            else:
-                return False
+        for i in range(len(nums)):
+            if i > larger: return False
+            larger = cmax(larger, i + nums[i])
         
         return True
