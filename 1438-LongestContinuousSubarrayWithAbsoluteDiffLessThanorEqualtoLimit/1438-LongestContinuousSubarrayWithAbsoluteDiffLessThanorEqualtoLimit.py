59cb4c9877d9908f0cbe162c3a0f6ad5661fd6fa
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-25 07:58:09 +0530

    File updated at 25/6/2025, 7:58:05 am

--- 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.py
+++ 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.py
@@ -1,31 +1,37 @@
-# Last updated: 25/6/2025, 7:53:48 am
+# Last updated: 25/6/2025, 7:58:05 am
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         n = len(nums)
-        min_heap = []
-        max_heap = []
+        min_stack = deque([])
+        max_stack = deque([])
         
         prev = 0
         ret = 1
         for i in range(n):
-            heapq.heappush(min_heap, (nums[i], i))
-            heapq.heappush(max_heap, (-nums[i], i))
+            while max_stack and max_stack[-1][0] < nums[i]:
+                max_stack.pop()
+            
+            while min_stack and min_stack[-1][0] > nums[i]:
+                min_stack.pop()
 
-            while min_heap and min_heap[0][1] < prev:
-                heapq.heappop(min_heap)
-                
-            while max_heap and max_heap[0][1] < prev:
-                heapq.heappop(max_heap)
+            while max_stack and max_stack[0][1] < prev:
+                max_stack.popleft()
             
-            while (-max_heap[0][0]) - min_heap[0][0] > limit:
+            while min_stack and min_stack[0][1] < prev:
+                min_stack.popleft()
+            
+            max_stack.append((nums[i], i))
+            min_stack.append((nums[i], i))
+
+            while max_stack[0][0] - min_stack[0][0] > limit:
                 prev += 1
-                while min_heap and min_heap[0][1] < prev:
-                    heapq.heappop(min_heap)
-                    
-                while max_heap and max_heap[0][1] < prev:
-                    heapq.heappop(max_heap)
-            
+                while max_stack and max_stack[0][1] < prev:
+                    max_stack.popleft()
+                
+                while min_stack and min_stack[0][1] < prev:
+                    min_stack.popleft()
+
             ret = cmax(ret, i - prev + 1)
         
         return ret
