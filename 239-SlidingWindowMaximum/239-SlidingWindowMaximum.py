cb588ed081ccfbad635c2abfe9ac631969955ae1
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-25 07:38:42 +0530

    File updated at 25/6/2025, 7:38:38 am

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.py
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.py
@@ -1,4 +1,4 @@
-# Last updated: 25/6/2025, 7:37:21 am
+# Last updated: 25/6/2025, 7:38:38 am
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
@@ -15,9 +15,10 @@ class Solution:
         for i in range(k, n):
             while stack and nums[stack[-1]] < nums[i]:
                 stack.pop()
+                
             stack.append(i)
-            left = i - k + 1
-            while stack and stack[0] < left:
+
+            if stack[0] <= i - k:
                 stack.popleft()
 
             ret.append(nums[stack[0]])
