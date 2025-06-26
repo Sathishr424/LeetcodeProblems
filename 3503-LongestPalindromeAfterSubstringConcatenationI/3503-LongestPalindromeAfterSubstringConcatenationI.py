b12519ca3128157cd76c4158a8419d86f9d2b851
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-27 01:24:03 +0530

    File updated at 27/6/2025, 1:23:58 am

--- 3503-LongestPalindromeAfterSubstringConcatenationI/3503-LongestPalindromeAfterSubstringConcatenationI.py
+++ 3503-LongestPalindromeAfterSubstringConcatenationI/3503-LongestPalindromeAfterSubstringConcatenationI.py
@@ -1,17 +1,35 @@
-# Last updated: 27/6/2025, 12:58:53 am
+# Last updated: 27/6/2025, 1:23:58 am
 class Solution:
-    def longestPalindrome(self, s: str) -> int:
-        freq = [0] * 58
-        for char in s:
-            freq[ord(char) - 65] += 1
+    def largestPalindromic(self, num: str) -> str:
+        freq = [0] * 10
+
+        for char in num:
+            freq[int(char)] += 1
         
-        is_odd = 0
-        ret = 0
-        for i in range(58):
-            if freq[i] % 2: 
-                is_odd = 1
-                ret += freq[i] - 1
-            else:
-                ret += freq[i]
+        to_add = []
+        odds = []
+
+        for i in range(9, -1, -1):
+            cnt = freq[i]
+            if freq[i] > 1:
+                to_add.append(i)
+                cnt %= 2
+            
+            if freq[i] % 2:
+                if len(odds) < 2: odds.append(i)
         
-        return ret + is_odd
+        can_add_zero = True
+        if freq[0] > 1 and len(to_add) == 1:
+            can_add_zero = False
+        
+        ret = ''
+        for i in to_add:
+            if i != 0 or can_add_zero:
+                ret = ret + str(i) * (freq[i] // 2)
+
+        if len(odds):
+            ret = ret + str(odds[0]) + ret[::-1]
+        else:
+            ret = ret + ret[::-1]
+        
+        return ret if len(ret) else '0'
