7d1a0c248784d6bf9032ad81d8636745a3ae04ff
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-27 00:58:58 +0530

    File updated at 27/6/2025, 12:58:53 am

--- 3503-LongestPalindromeAfterSubstringConcatenationI/3503-LongestPalindromeAfterSubstringConcatenationI.py
+++ 3503-LongestPalindromeAfterSubstringConcatenationI/3503-LongestPalindromeAfterSubstringConcatenationI.py
@@ -1,4 +1,4 @@
-# Last updated: 27/6/2025, 12:56:38 am
+# Last updated: 27/6/2025, 12:58:53 am
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         freq = [0] * 58
@@ -8,8 +8,10 @@ class Solution:
         is_odd = 0
         ret = 0
         for i in range(58):
-            if freq[i] % 2: is_odd = 1
-
-            ret += freq[i] // 2 * 2
+            if freq[i] % 2: 
+                is_odd = 1
+                ret += freq[i] - 1
+            else:
+                ret += freq[i]
         
         return ret + is_odd
