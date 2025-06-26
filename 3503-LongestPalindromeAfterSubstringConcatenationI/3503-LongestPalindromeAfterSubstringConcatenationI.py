ef058084b09ea4fc2e3126f303a92d3593f22935
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-27 00:56:44 +0530

    File updated at 27/6/2025, 12:56:38 am

--- 3503-LongestPalindromeAfterSubstringConcatenationI/3503-LongestPalindromeAfterSubstringConcatenationI.py
+++ 3503-LongestPalindromeAfterSubstringConcatenationI/3503-LongestPalindromeAfterSubstringConcatenationI.py
@@ -1,78 +1,15 @@
-# Last updated: 26/6/2025, 11:32:26 pm
-cmax = lambda x, y: x if x > y else y
+# Last updated: 27/6/2025, 12:56:38 am
 class Solution:
-    def longestPalindrome(self, s: str, t: str) -> int:
-        n = len(s)
-        m = len(t)
-
-        tot = n + m
-        st = s + t
-        ret = 1
+    def longestPalindrome(self, s: str) -> int:
+        freq = [0] * 58
+        for char in s:
+            freq[ord(char) - 65] += 1
         
-        def rec_left(i, j):
-            if i < 0 or j == tot: return 0
+        is_odd = 0
+        ret = 0
+        for i in range(58):
+            if freq[i] % 2: is_odd = 1
 
-            ans = rec_left(i-1, j)
-
-            if st[i] == st[j]:
-                cnt = 0
-                while i >= 0 and j < tot and st[i] == st[j]:
-                    i -= 1
-                    j += 1
-                    cnt += 2
-                ans = cmax(ans, cnt)
-            
-            return ans
+            ret += freq[i] // 2 * 2
         
-        def rec_right(i, j):
-            if i < 0 or j == tot: return 0
-
-            ans = rec_right(i, j + 1)
-
-            if st[i] == st[j]:
-                cnt = 0
-                while i >= 0 and j < tot and st[i] == st[j]:
-                    i -= 1
-                    j += 1
-                    cnt += 2
-                ans = cmax(ans, cnt)
-            
-            return ans
-
-        def do_left(left, right):
-            cnt = rec_left(n-1, right + 1)
-            return right - left + cnt + 1
-        
-        def do_right(left, right):
-            cnt = rec_right(left - 1, n)
-            return right - left + cnt + 1
-
-        for i in range(1, tot):
-            left = i-1
-            right = i
-            while left >= 0 and right < tot and st[left] == st[right]:
-                left -= 1
-                right += 1
-            left += 1
-            right -= 1
-            ret = cmax(ret, right - left + 1)
-            if left >= n:
-                ret = cmax(ret, do_left(left, right))
-            if right < n:
-                ret = cmax(ret, do_right(left, right))
-            
-            left = i-1
-            right = i+1
-            while left >= 0 and right < tot and st[left] == st[right]:
-                left -= 1
-                right += 1
-            left += 1
-            right -= 1
-            ret = cmax(ret, right - left + 1)
-
-            if left >= n:
-                ret = cmax(ret, do_left(left, right))
-            if right < n:
-                ret = cmax(ret, do_right(left, right))
-
-        return ret
+        return ret + is_odd
