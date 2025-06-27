63d19d863087ac6a404ef70c4794579fe95d69d9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-27 05:35:04 +0530

    File updated at 27/6/2025, 5:34:58 am

--- 3503-LongestPalindromeAfterSubstringConcatenationI/3503-LongestPalindromeAfterSubstringConcatenationI.py
+++ 3503-LongestPalindromeAfterSubstringConcatenationI/3503-LongestPalindromeAfterSubstringConcatenationI.py
@@ -1,39 +1,52 @@
-# Last updated: 27/6/2025, 5:17:18 am
+# Last updated: 27/6/2025, 5:34:58 am
+primes = [2, 3, 5, 7]
+
+def isPrime(num):
+    if num == 1: return False
+    if num == 2: return True
+    if num == 3: return True
+
+    if num % 2 == 0: return False
+
+    for i in range(3, int(num ** 0.5) + 1, 2):
+        if num % i == 0: return False
+    
+    return True
+
 class Solution:
     def primePalindrome(self, n: int) -> int:
-        def isPrime(num):
-            if num == 1: return False
-            if num == 2: return True
-            if num == 3: return True
-
-            if num % 2 == 0: return False
-
-            for i in range(3, int(num ** 0.5) + 1, 2):
-                if num % i == 0: return False
-            
-            return True
+        for num in primes:
+            if num >= n: return num
         
-        stack = deque([''])
+        def getPalindrome(num):
+            r_num = num
+            while num:
+                r_num = r_num * 10 + (num % 10)
+                num //= 10
+            return r_num
+        
+        def getPalindromeExtra(num, extra):
+            r_num = num * 10 + extra
+            while num:
+                r_num = r_num * 10 + (num % 10)
+                num //= 10
+            return r_num
+        
+        stack = deque([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
         while stack:
             num = stack.popleft()
-            # print(num, '===>')
 
-            if len(num):
-                r_num = int(num + num[::-1])
-                # print(r_num)
-                if r_num >= n and isPrime(r_num):
-                    return r_num
-                
-            for i in range(10):
-                r_num = int(num + str(i) + num[::-1])
-                # print(r_num)
-                if r_num >= n and isPrime(r_num):
-                    return r_num
+            r_num = getPalindrome(num)
+            # print(num, r_num)
+            if r_num >= n and isPrime(r_num):
+                return r_num
             
             for i in range(10):
-                stack.append(num + str(i))
+                r_num = getPalindromeExtra(num, i)
+                # print(num, r_num)
+                if r_num >= n and isPrime(r_num):
+                    return r_num
 
-
-
-            
+            for i in range(10):
+                stack.append(num * 10 + i)
