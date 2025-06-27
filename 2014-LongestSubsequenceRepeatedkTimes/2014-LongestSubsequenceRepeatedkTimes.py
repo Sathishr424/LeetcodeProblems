43f6784513859b2d811d3567b6b83ddde617d0ea
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-27 19:14:25 +0530

    File updated at 27/6/2025, 7:14:18 pm

--- 2014-LongestSubsequenceRepeatedkTimes/2014-LongestSubsequenceRepeatedkTimes.py
+++ 2014-LongestSubsequenceRepeatedkTimes/2014-LongestSubsequenceRepeatedkTimes.py
@@ -1,48 +1,58 @@
-# Last updated: 27/6/2025, 6:34:29 pm
+# Last updated: 27/6/2025, 7:14:18 pm
+def charToInt(char):
+    return ord(char) - 97
+
 class Solution:
     def longestSubsequenceRepeatedK(self, s: str, k: int) -> str:
         n = len(s)
-        indexes = defaultdict(deque)
+        freq = [0] * 26
 
-        for i, char in enumerate(s):
-            indexes[char].append(i)
-        
+        for char in s:
+            freq[charToInt(char)] += 1
+
+        next_index = [[-1] * 26 for _ in range(n + 1)]
+
+        for i in range(n-1, -1, -1):
+            for a in range(26):
+                next_index[i][a] = next_index[i+1][a]
+            next_index[i][ord(s[i]) - 97] = i
+
         char_limit = n // k
 
-        candidates = []
-        used = defaultdict(int)
+        candidates = [0] * 26
         for char in s:
-            if len(indexes[char]) >= (used[char] + 1) * k:
-                candidates.append(char)
-                used[char] += 1
-        
-        visited = {}
-        used_freq = defaultdict(int)
-        def check(st):
-            if st in visited: return
-            visited[st] = 1
-            if len(st) == char_limit: return
-            for char in candidates:
-                if (used_freq[char] + 1) * k <= len(indexes[char]):
-                    used_freq[char] += 1
-                    check(st + char)
-                    used_freq[char] -= 1
-        
-        check('')
+            a = charToInt(char)
+            if freq[a] >= k:
+                candidates[a] = 1
 
-        def check_possible(st):
-            last_index = -1
+        ret = ''
+        used = [0] * 26
+        can_use = {}
+
+        def rec(st):
+            if st in can_use: return
+            can_use[st] = 1
+            if len(st) == char_limit: return
+
+            for i in range(26):
+                if candidates[i] and (used[i] + 1) * k <= freq[i]:
+                    used[i] += 1
+                    rec(st + chr(i + 97))
+                    used[i] -= 1
+
+        rec('')
+
+        def checkPossible(st):
+            last_index = 0
             for i in range(k):
                 for char in st:
-                    l_index = bisect_left(indexes[char], last_index + 1)
-                    if l_index == len(indexes[char]):
-                        return False
-                    last_index = indexes[char][l_index]
+                    a = charToInt(char)
+                    if next_index[last_index][a] == -1: return False
+                    last_index = next_index[last_index][a] + 1
             return True
-
-        for st in sorted(visited.keys(), key=lambda x: (len(x), x), reverse=True):
-            if check_possible(st):
+            
+        for st in sorted(can_use.keys(), key=lambda x: (len(x), x), reverse=True):
+            if checkPossible(st):
                 return st
         
         return ''
-            
