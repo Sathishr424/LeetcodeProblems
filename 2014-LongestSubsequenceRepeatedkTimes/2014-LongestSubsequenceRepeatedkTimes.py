56319d9444759014d3f0218eae36530db1c53e56
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-27 22:07:46 +0530

    File updated at 27/6/2025, 10:07:38 pm

--- 2014-LongestSubsequenceRepeatedkTimes/2014-LongestSubsequenceRepeatedkTimes.py
+++ 2014-LongestSubsequenceRepeatedkTimes/2014-LongestSubsequenceRepeatedkTimes.py
@@ -1,20 +1,24 @@
-# Last updated: 27/6/2025, 10:02:49 pm
+# Last updated: 27/6/2025, 10:07:38 pm
 def charToInt(char):
     return ord(char) - 97
-
 cmax = lambda x, y: x if x > y else y
+
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
         n = len(s)
         alps = [charToInt(char) for char in s]
 
-        def rec(l, r):
-            freq = [0] * 26
-            for i in range(l, r+1):
-                freq[alps[i]] += 1
+        freq = [[0] * 26 for _ in range(n + 1)]
 
+        for i in range(1, n + 1):
+            for j in range(26):
+                freq[i][j] = freq[i-1][j]
+
+            freq[i][alps[i - 1]] += 1
+
+        def rec(l, r):
             for i in range(l, r+1):
-                if freq[alps[i]] < k:
+                if freq[r+1][alps[i]] - freq[l][alps[i]] < k:
                     return cmax(rec(l, i-1), rec(i+1, r))
             
             return r - l + 1
