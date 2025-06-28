fb7d5c25e835762f4d3b0206fea86b21f9f817bc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-28 19:31:21 +0530

    Create 3346-MaximumFrequencyofanElementAfterPerformingOperationsI

---
+++ 3346-MaximumFrequencyofanElementAfterPerformingOperationsI/3346-MaximumFrequencyofanElementAfterPerformingOperationsI.py
@@ -0,0 +1,47 @@
+# Last updated: 28/6/2025, 7:31:13 pm
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, op: int) -> int:
+        n = len(nums)
+
+        nums.sort()
+        # print(nums)
+        ret = 1
+        freq = defaultdict(int)
+
+        i = 0
+        while i < n:
+            freq[nums[i]] += 1
+            j = i + 1
+            while j < n and nums[j] == nums[i]:
+                j += 1
+            
+            dup = j - i - 1
+            right = bisect_left(nums, nums[i] + k + 1)
+            left = bisect_left(nums, nums[i] - k)
+
+            left = min(op, i - left)
+            right = min(op + dup, right - i - 1)
+            ret = max(ret, left + 1, right + 1, min(left + right + 1, op + dup + 1))
+
+            i = j
+        
+        if op < 2: return ret
+        op -= 1
+        i = 0
+        while i < n:
+            num = nums[i] + k
+            dup = freq[num]
+            right = bisect_left(nums, num + k + 1)
+            left = bisect_left(nums, num - k)
+
+            # print(i, num, (left, right, dup))
+
+            left = min(op, i - left)
+            right = min(op + dup, right - i - 1)
+            # print(left, right)
+
+            ret = max(ret, left + 1, right + 1, min(left + right + 1, op + dup + 1))
+
+            i += 1
+        
+        return ret
