738b9af337c85eecc704e109e5b98a157cc76678
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-28 18:36:38 +0530

    File updated at 28/6/2025, 6:36:30 pm

--- 3505-MinimumOperationstoMakeElementsWithinKSubarraysEqual/3505-MinimumOperationstoMakeElementsWithinKSubarraysEqual.py
+++ 3505-MinimumOperationstoMakeElementsWithinKSubarraysEqual/3505-MinimumOperationstoMakeElementsWithinKSubarraysEqual.py
@@ -1,25 +1,34 @@
-# Last updated: 28/6/2025, 9:10:49 am
+# Last updated: 28/6/2025, 6:36:30 pm
 class Solution:
-    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+    def minOperationsToMakeMedianK(self, nums: List[int], k: int) -> int:
         n = len(nums)
-
-        sl = SortedList()
-        for i in range(k):
-            sl.add(nums[i])
+        nums.sort()
+        left = n
+        for i, num in enumerate(nums):
+            if num >= k:
+                left = i
+                break
         
-        ret = []
-        if k % 2:
-            ret.append(sl[k // 2])
-        else:
-            ret.append((sl[k // 2] + sl[k // 2 - 1]) / 2)
-        
-        for i in range(k, n):
-            sl.remove(nums[i-k])
-            sl.add(nums[i])
+        mid = n // 2
+        # if left == mid and nums[left] == k: return 0
+        # if n % 2 == 0 and left == mid - 1 and nums[left + 1] == k: return 0
 
-            if k % 2:
-                ret.append(sl[k // 2])
-            else:
-                ret.append((sl[k // 2] + sl[k // 2 - 1]) / 2)
+        # [1, 2, 3, 5, _7_, 8]
+        # [1, _2_, 3, 5, 7, 8]
 
+        # [1, 2, 3, 5, _7_, 8, 9]
+        # [1, _2_, 3, 5, 7, 8, 9]
+
+        ret = 0
+        if left <= mid:
+            for i in range(left, mid + 1):
+                ret += nums[i] - k
+                nums[i] = k
+        elif left > mid:
+            for i in range(mid, left):
+                ret += k - nums[i]
+                nums[i] = k
+        # print(nums)
         return ret
+
+
