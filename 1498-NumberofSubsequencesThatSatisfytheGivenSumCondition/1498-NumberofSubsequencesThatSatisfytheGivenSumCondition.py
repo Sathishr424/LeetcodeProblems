19ed0774d688fce0329a4c2ab57787ef096d5448
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-29 07:11:08 +0530

    File updated at 29/6/2025, 7:11:06 am

--- 1498-NumberofSubsequencesThatSatisfytheGivenSumCondition/1498-NumberofSubsequencesThatSatisfytheGivenSumCondition.py
+++ 1498-NumberofSubsequencesThatSatisfytheGivenSumCondition/1498-NumberofSubsequencesThatSatisfytheGivenSumCondition.py
@@ -1,11 +1,15 @@
-# Last updated: 29/6/2025, 6:58:04 am
-def c_fact(n):
-    if n == 1: return 1
-    return c_fact(n - 1) + 1
+# Last updated: 29/6/2025, 7:11:06 am
+N = 10 ** 5 + 1
+fact = [0] * N
+fact[1] = 1
+mod = 10 ** 9 + 7
+s = 1
+for i in range(2, N):
+    fact[i] = (s + 1) % mod
+    s = (s + fact[i]) % mod
 
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
-        mod = 10 ** 9 + 7
         n = len(nums)
 
         # 2 3 5 6 7
@@ -13,16 +17,11 @@ class Solution:
         # (3) (3 5) (3 6) (3 7) (3 5 6) (3 5 6 7) (3 5 7) (3 6 7)
 
         nums.sort()
-        # print(nums)
         ret = 0
 
         for i in range(n):
             index = bisect_right(nums, target - nums[i])
-            # print(i, index, (index - i))
             if index >= i:
-                # print((1 << (index - i)) // 2)
-                ret += (1 << (index - i)) // 2
-                ret %= mod
-        
+                ret = (ret + fact[index - i]) % mod
         return ret
 
