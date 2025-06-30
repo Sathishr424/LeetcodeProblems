28e47464ff69f496e356e89ee0d778f8505367fe
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-06-30 12:42:20 +0530

    File updated at 30/6/2025, 12:42:17 pm

--- 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.py
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.py
@@ -1,14 +1,15 @@
-# Last updated: 12/6/2025, 5:48:13 am
+# Last updated: 30/6/2025, 12:42:17 pm
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
         n = len(nums)
 
-        hash = defaultdict(int)
         ret = 0
-        for num in nums:
-            hash[num] += 1
+        cnts = defaultdict(int)
+        for i, num in enumerate(nums):
+            cnts[num] += 1
+            if cnts[num-1]:
+                ret = max(ret, cnts[num-1] + cnts[num])
+            if cnts[num+1]:
+                ret = max(ret, cnts[num+1] + cnts[num])
         
-        for num in nums:
-            if hash[num+1]:
-                ret = max(ret, hash[num+1] + hash[num])
         return ret
