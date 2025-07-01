432ca65781aba33c67aeb201465da4d39efb233f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-01 06:54:19 +0530

    Create 3330-FindtheOriginalTypedStringI

---
+++ 3330-FindtheOriginalTypedStringI/3330-FindtheOriginalTypedStringI.py
@@ -0,0 +1,16 @@
+# Last updated: 1/7/2025, 6:54:17 am
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        ret = 1
+        
+        prev = 0
+        cnt = 0
+        for i in range(1, len(word)):
+            if word[i] == word[prev]:
+                cnt += 1
+            else:
+                ret += cnt
+                cnt = 0
+                prev = i
+        
+        return ret + cnt
