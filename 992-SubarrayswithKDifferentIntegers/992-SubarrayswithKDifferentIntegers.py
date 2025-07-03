3511eaf51c28c8dab4e027f1a5019eb565555891
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-04 02:17:03 +0530

    File updated at 4/7/2025, 2:17:01 am

--- 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
+++ 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
@@ -1,29 +1,32 @@
-# Last updated: 4/7/2025, 2:07:44 am
+# Last updated: 4/7/2025, 2:17:01 am
 class Solution:
-    def countAtMostK(self, arr, k):
-        n = len(arr)
-        
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+
         freq = defaultdict(int)
-        cnt = 0
-        prev = 0
         ret = 0
-        
+        prev = 0
+        cnt = 0
+        prev_2 = 0
+
         for i in range(n):
-            freq[arr[i]] += 1
-            if freq[arr[i]] == 1:
+            freq[nums[i]] += 1
+            if freq[nums[i]] == 1:
                 cnt += 1
-            
-                while cnt > k:
-                    freq[arr[prev]] -= 1
-                    if freq[arr[prev]] == 0:
-                        cnt -= 1
-                    prev += 1
-            
-            ret += i - prev + 1
+
+                if cnt > k:
+                    prev = prev_2
+                    while cnt > k:
+                        freq[nums[prev]] -= 1
+                        if freq[nums[prev]] == 0: cnt -= 1
+                        prev += 1
+                    
+                    prev_2 = prev
+            if cnt == k:
+                while freq[nums[prev_2]] > 1:
+                    freq[nums[prev_2]] -= 1
+                    prev_2 += 1
+
+                ret += prev_2 - prev + 1
         
         return ret
-    
-    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        x = self.countAtMostK(nums, k)
-        y = self.countAtMostK(nums, k - 1)
-        return x - y
