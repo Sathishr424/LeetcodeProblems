f49b98a0a331b6d588dbef24b3e06db15f9a8d7c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-04 02:02:33 +0530

    File updated at 4/7/2025, 2:02:31 am

--- 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
+++ 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
@@ -1,42 +1,35 @@
-# Last updated: 24/4/2025, 11:19:15 am
+# Last updated: 4/7/2025, 2:02:31 am
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
         n = len(nums)
+
+        freq = defaultdict(int)
         ret = 0
-
-        uniq = [0] * (n+1)
-        matches = 0
-        left = 0
         prev = 0
-
-        cnts = [0] * (n+1)
-        cnt_matches = 0
-        cnt_left = 0
+        cnt = 0
+        prev_2 = 0
 
         for i in range(n):
-            cnts[nums[i]] += 1
-            if cnts[nums[i]] == 1: cnt_matches += 1
+            freq[nums[i]] += 1
+            if freq[nums[i]] == 1:
+                cnt += 1
 
-            if cnt_matches > k:
-                while cnt_left < i and cnt_matches > k:
-                    num = nums[cnt_left]
-                    cnts[num] -= 1
-                    if cnts[num] == 0: cnt_matches -= 1
-                    cnt_left += 1
-                prev = 0
+                if cnt > k:
+                    prev = prev_2
+                    while cnt > k:
+                        freq[nums[prev]] -= 1
+                        if freq[nums[prev]] == 0: cnt -= 1
+                        prev += 1
+                    
+                    prev_2 = prev
+            if cnt == k:
+                while freq[nums[prev_2]] > 1:
+                    freq[nums[prev_2]] -= 1
+                    prev_2 += 1
+
+                ret += prev_2 - prev + 1
+
             
-            uniq[nums[i]] += 1
-            if uniq[nums[i]] == 1: matches += 1
-
-            if matches == k:
-                while left <= i and matches == k:
-                    num = nums[left]
-                    uniq[num] -= 1
-                    if uniq[num] == 0: matches -= 1
-                    left += 1
-                    prev += 1
- 
-            ret += prev
-        
+            # print((i, prev), (prev, prev_2), ret)
+        # 1, 1, 1, 2, 2, 3
         return ret
-        
