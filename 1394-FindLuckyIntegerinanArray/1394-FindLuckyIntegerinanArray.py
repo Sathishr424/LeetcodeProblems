3f01bb5a4167e348fdaf24b8fec04df4bdf962b3
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-05 08:45:40 +0530

    File updated at 5/7/2025, 8:45:38 am

--- 1394-FindLuckyIntegerinanArray/1394-FindLuckyIntegerinanArray.py
+++ 1394-FindLuckyIntegerinanArray/1394-FindLuckyIntegerinanArray.py
@@ -1,14 +1,14 @@
-# Last updated: 5/7/2025, 8:44:46 am
+# Last updated: 5/7/2025, 8:45:38 am
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
-        freq = defaultdict(int)
+        freq = [0] * 501
 
         for num in arr:
             freq[num] += 1
         
         ret = -1
-        for num in freq:
-            if num == freq[num]:
-                ret = max(ret, num)
+        for num in range(1, 501):
+            if num == freq[num] and num > ret:
+                ret = num
         
         return ret
