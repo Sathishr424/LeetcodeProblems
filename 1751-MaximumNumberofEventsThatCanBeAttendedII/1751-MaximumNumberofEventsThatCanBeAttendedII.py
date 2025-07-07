d23a904c474f6a433afdf8c3666a9f81c432f95b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-07 19:46:13 +0530

    File updated at 7/7/2025, 7:46:07 pm

--- 1751-MaximumNumberofEventsThatCanBeAttendedII/1751-MaximumNumberofEventsThatCanBeAttendedII.py
+++ 1751-MaximumNumberofEventsThatCanBeAttendedII/1751-MaximumNumberofEventsThatCanBeAttendedII.py
@@ -1,4 +1,4 @@
-# Last updated: 7/7/2025, 7:37:15 pm
+# Last updated: 7/7/2025, 7:46:07 pm
 class Solution:
     def maxValue(self, events: List[List[int]], k: int) -> int:
         events.sort()
@@ -10,12 +10,9 @@ class Solution:
             if index == n: return 0
             x, y, val = events[index]
 
-            left = bisect_left(events, [y + 1, 0, 0])
-            right = bisect_right(events, [y + 1, 0, 0])
+            next_index = bisect_left(events, [y + 1, 0, 0])
 
-            ans = max(rec(right, rem - 1) + val, rec(index + 1, rem))
-            for i in range(left, right):
-                ans = max(ans, rec(i, rem))
+            ans = max(rec(next_index, rem - 1) + val, rec(index + 1, rem))
             return ans
         
         ans = rec(0, k)
