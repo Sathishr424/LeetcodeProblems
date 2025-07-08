c2231de0483b5c020ca0d5fc2162414f75141dd0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-09 02:05:17 +0530

    File updated at 9/7/2025, 2:05:10 am

--- 2104-SumofSubarrayRanges/2104-SumofSubarrayRanges.py
+++ 2104-SumofSubarrayRanges/2104-SumofSubarrayRanges.py
@@ -1,40 +1,40 @@
-# Last updated: 9/7/2025, 2:02:10 am
+# Last updated: 9/7/2025, 2:05:10 am
+def getMinSubarraySum(nums):
+    n = len(nums)
+    
+    left = [-1] * n
+    right = [n] * n
+    stack = []
+    for i in range(n):
+        while stack and nums[stack[-1]] >= nums[i]:
+            stack.pop()
+        
+        if stack:
+            left[i] = stack[-1]
+        stack.append(i)
+    
+    stack = []
+    for i in range(n-1, -1, -1):
+        while stack and nums[stack[-1]] > nums[i]:
+            stack.pop()
+        
+        if stack:
+            right[i] = stack[-1]
+        stack.append(i)
+    
+    totalSum = 0
+
+    for i in range(n):
+        l = i - left[i]
+        r = right[i] - i
+        
+        totalSum += l * r * nums[i]
+    
+    return totalSum
+
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        def getSum(nums):
-            n = len(nums)
-            
-            left = [-1] * n
-            right = [n] * n
-            stack = []
-            for i in range(n):
-                while stack and nums[stack[-1]] >= nums[i]:
-                    stack.pop()
-                
-                if stack:
-                    left[i] = stack[-1]
-                stack.append(i)
-            
-            stack = []
-            for i in range(n-1, -1, -1):
-                while stack and nums[stack[-1]] > nums[i]:
-                    stack.pop()
-                
-                if stack:
-                    right[i] = stack[-1]
-                stack.append(i)
-            
-            totalSum = 0
-
-            for i in range(n):
-                l = i - left[i]
-                r = right[i] - i
-                
-                totalSum += l * r * nums[i]
-            
-            return totalSum
-        
-        min_ = getSum(nums)
-        max_ = getSum([-num for num in nums]) * -1
+        min_ = getMinSubarraySum(nums)
+        max_ = getMinSubarraySum([-num for num in nums]) * -1
 
         return max_ - min_
