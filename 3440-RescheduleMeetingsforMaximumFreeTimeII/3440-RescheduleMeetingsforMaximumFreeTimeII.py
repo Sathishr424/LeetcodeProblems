a710453e9c32bde9675778cd4637dcc095bec35e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-09 12:08:08 +0530

    File updated at 9/7/2025, 12:08:01 pm

--- 3440-RescheduleMeetingsforMaximumFreeTimeII/3440-RescheduleMeetingsforMaximumFreeTimeII.py
+++ 3440-RescheduleMeetingsforMaximumFreeTimeII/3440-RescheduleMeetingsforMaximumFreeTimeII.py
@@ -1,4 +1,5 @@
-# Last updated: 9/7/2025, 12:06:51 pm
+# Last updated: 9/7/2025, 12:08:01 pm
+cmax = lambda x, y: x if x > y else y
 class Solution:
     def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
         n = len(startTime)
@@ -15,7 +16,7 @@ class Solution:
 
         for i in range(k, n+1):
             curr = curr - prefix[i - k] + prefix[i]
-            ret = max(ret, curr)
+            ret = cmax(ret, curr)
         
         left = [0] * (n + 1)
         left[0] = prefix[0]
@@ -23,18 +24,13 @@ class Solution:
         right[-1] = prefix[-1]
 
         for i in range(n-1, -1, -1):
-            right[i] = max(right[i+1], prefix[i])
+            right[i] = cmax(right[i+1], prefix[i])
         for i in range(1, n+1):
-            left[i] = max(left[i-1], prefix[i])
-
-        # print(prefix)
-        # print(left)
-        # print(right)
+            left[i] = cmax(left[i-1], prefix[i])
 
         for i in range(n):
             dur = endTime[i] - startTime[i]
             if (i != 0 and dur <= left[i-1]) or (i != n-1 and dur <= right[i+2]):
-                # print(i, dur, (prefix[i], prefix[i+1]))
-                ret = max(ret, dur + prefix[i] + prefix[i+1])
-        # print([(startTime[i], endTime[i]) for i in range(n)])
+                ret = cmax(ret, dur + prefix[i] + prefix[i+1])
+
         return ret
