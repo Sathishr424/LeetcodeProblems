3372bca6fae0ee0d1808708893c08d3257494962
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-09 11:10:11 +0530

    File updated at 9/7/2025, 11:10:04 am

--- 3439-RescheduleMeetingsforMaximumFreeTimeI/3439-RescheduleMeetingsforMaximumFreeTimeI.py
+++ 3439-RescheduleMeetingsforMaximumFreeTimeI/3439-RescheduleMeetingsforMaximumFreeTimeI.py
@@ -1,23 +1,19 @@
-# Last updated: 9/7/2025, 11:08:38 am
+# Last updated: 9/7/2025, 11:10:04 am
+cmax = lambda x, y: x if x > y else y
 class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
         n = len(startTime)
-        merged = []
-        for i in range(n):
-            merged.append((startTime[i], endTime[i]))
-        
-        prefix = [merged[0][0] - 0]
+
+        prefix = [startTime[0] - 0]
         for i in range(1, n):
-            prefix.append(merged[i][0] - merged[i-1][1])
-        prefix.append(eventTime - merged[n-1][1])
-        # print(merged)
-        # print(prefix)
+            prefix.append(startTime[i] - endTime[i-1])
+        prefix.append(eventTime - endTime[n-1])
+
         k += 1
         curr = sum(prefix[:k])
         ret = curr
         for i in range(k, n + 1):
-            curr -= prefix[i - k]
-            curr += prefix[i]
-            ret = max(curr, ret)
+            curr = curr - prefix[i - k] + prefix[i]
+            ret = cmax(curr, ret)
         
         return ret
