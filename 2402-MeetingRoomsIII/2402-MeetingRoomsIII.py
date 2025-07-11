851256e62b9c74da3a2a2103b9e20ff82179edf8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-11 11:58:50 +0530

    File updated at 11/7/2025, 11:58:42 am

--- 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
+++ 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
@@ -1,4 +1,4 @@
-# Last updated: 11/7/2025, 11:58:13 am
+# Last updated: 11/7/2025, 11:58:42 am
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         m = len(meetings)
@@ -11,33 +11,31 @@ class Solution:
         heapq.heapify(meetings_heap)
 
         on_going_meetings_heap = []
+        max_used_room = 0
+
+        def assignRoom(e):
+            nonlocal max_used_room
+            for i in range(n):
+                if free_rooms[i] == 0:
+                    heapq.heappush(on_going_meetings_heap, (e, i))
+                    used[i] += 1
+                    free_rooms[i] = 1
+                    return True
+            return False
 
         while meetings_heap:
             s, e = heapq.heappop(meetings_heap)
             while on_going_meetings_heap and on_going_meetings_heap[0][0] <= s:
                 e_, r = heapq.heappop(on_going_meetings_heap)
                 free_rooms[r] = 0
-
-            can = False
-            for i in range(n):
-                if free_rooms[i] == 0:
-                    # print((s, e), 'room:', i)
-                    can = True
-                    heapq.heappush(on_going_meetings_heap, (e, i))
-                    used[i] += 1
-                    free_rooms[i] = 1
-                    break
             
-            if not can:
+            if not assignRoom(e):
                 e_, r = heapq.heappop(on_going_meetings_heap)
-                diff = e_ - s
-                # print((s, e), 'room:', r, 'modified:', (s + diff, e + diff))
-                heapq.heappush(on_going_meetings_heap, (e + diff, r))
+                heapq.heappush(on_going_meetings_heap, (e + (e_ - s), r))
                 used[r] += 1
-        
+    
         max_room = 0
         for i in range(n):
             if used[i] > used[max_room]:
                 max_room = i
-        # print(used)
         return max_room
