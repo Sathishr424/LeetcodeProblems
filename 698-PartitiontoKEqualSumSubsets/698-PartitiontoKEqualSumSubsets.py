213c9f2326249133663ca7f51d43e3b316d3f059
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-13 01:08:07 +0530

    File updated at 13/7/2025, 1:07:56 am

--- 698-PartitiontoKEqualSumSubsets/698-PartitiontoKEqualSumSubsets.py
+++ 698-PartitiontoKEqualSumSubsets/698-PartitiontoKEqualSumSubsets.py
@@ -1,17 +1,15 @@
-# Last updated: 13/7/2025, 12:58:50 am
+# Last updated: 13/7/2025, 1:07:56 am
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        @cache
-        def rec(index):
-            if index == n:
-                return 0
-
-            ans = 0
-            for i in range(index + 1, n):
-                if nums[i] > nums[index]:
-                    ans = max(ans, rec(i))
-            
-            return ans + 1
         
-        return max([rec(i) for i in range(n)])
+        stack = []
+
+        for i in range(n):
+            index = bisect_left(stack, nums[i])
+            if index == len(stack):
+                stack.append(nums[i])
+            else:
+                stack[index] = nums[i]
+        
+        return len(stack)
