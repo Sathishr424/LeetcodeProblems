358678cd0e273251d175fa66223b3eecd53860de
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-12 14:13:54 +0530

    File updated at 12/7/2025, 2:13:45 pm

--- 1900-TheEarliestandLatestRoundsWherePlayersCompete/1900-TheEarliestandLatestRoundsWherePlayersCompete.py
+++ 1900-TheEarliestandLatestRoundsWherePlayersCompete/1900-TheEarliestandLatestRoundsWherePlayersCompete.py
@@ -1,4 +1,4 @@
-# Last updated: 12/7/2025, 2:11:51 pm
+# Last updated: 12/7/2025, 2:13:45 pm
 inf = float('inf')
 cmin = lambda x, y: x if x < y else y
 cmax = lambda x, y: x if x > y else y
@@ -9,7 +9,7 @@ class Solution:
         max_round = floor(log2(n)) + 1
 
         @cache
-        def rec_earliest(mask, l, r, rnd):
+        def rec(mask, l, r, rnd):
             while l <= r and mask & (1 << l) != 0:
                 l += 1
             
@@ -18,42 +18,20 @@ class Solution:
 
             if l < r:
                 if l == firstPlayer and r == secondPlayer:
-                    return rnd + 1
+                    return rnd + 1, rnd + 1
                 elif l == firstPlayer:
-                    return rec_earliest(mask | (1 << r), l + 1, r - 1, rnd)
+                    return rec(mask | (1 << r), l + 1, r - 1, rnd)
                 elif r == secondPlayer:
-                    return rec_earliest(mask | (1 << l), l + 1, r - 1, rnd)
-                    
-                return cmin(rec_earliest(mask | (1 << l), l + 1, r - 1, rnd), rec_earliest(mask | (1 << r), l + 1, r - 1, rnd))
+                    return rec(mask | (1 << l), l + 1, r - 1, rnd)
+                l1, r1 = rec(mask | (1 << l), l + 1, r - 1, rnd)
+                l2, r2 = rec(mask | (1 << r), l + 1, r - 1, rnd)
+                return cmin(l1, l2), cmax(r1, r2)
             else:
-                if rnd + 1 > max_round: return inf
-                return rec_earliest(mask, 0, n-1, rnd + 1)
-        
-        @cache
-        def rec_latest(mask, l, r, rnd):
-            while l <= r and mask & (1 << l) != 0:
-                l += 1
-            
-            while r >= l and mask & (1 << r) != 0:
-                r -= 1
+                if rnd + 1 > max_round: return inf, -1
+                return rec(mask, 0, n-1, rnd + 1)
 
-            if l < r:
-                if l == firstPlayer and r == secondPlayer:
-                    return rnd + 1
-                elif l == firstPlayer:
-                    return rec_latest(mask | (1 << r), l + 1, r - 1, rnd)
-                elif r == secondPlayer:
-                    return rec_latest(mask | (1 << l), l + 1, r - 1, rnd)
-                    
-                return cmax(rec_latest(mask | (1 << l), l + 1, r - 1, rnd), rec_latest(mask | (1 << r), l + 1, r - 1, rnd))
-            else:
-                if rnd + 1 > max_round: return -1
-                return rec_latest(mask, 0, n-1, rnd + 1)
+        left, right = rec(0, 0, n-1, 0)
 
-        left = rec_earliest(0, 0, n-1, 0)
-        right = rec_latest(0, 0, n-1, 0)
-
-        rec_earliest.cache_clear()
-        rec_latest.cache_clear()
-
+        rec.cache_clear()
+        # print(left, right)
         return [left, right]
