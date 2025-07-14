8b275deb88cae2fb40465b77ea663e33371e756f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-14 17:32:56 +0530

    File updated at 14/7/2025, 5:32:45 pm

--- 3614-ProcessStringwithSpecialOperationsII/3614-ProcessStringwithSpecialOperationsII.py
+++ 3614-ProcessStringwithSpecialOperationsII/3614-ProcessStringwithSpecialOperationsII.py
@@ -1,25 +1,20 @@
-# Last updated: 14/7/2025, 5:32:15 pm
+# Last updated: 14/7/2025, 5:32:45 pm
 class Solution:
     def processStr(self, s: str, k: int) -> str:
         n = len(s)
-        total_cnt = 0
-        new_s = {}
+        cnt = 0
 
         for char in s:
             if char == '*':
-                total_cnt = max(0, total_cnt - 1)
+                cnt = max(0, cnt - 1)
             elif char == '#':
-                total_cnt += total_cnt
+                cnt += cnt
             elif char == '%':
                 pass
             else:
-                total_cnt += 1
-                new_s[char] = 1
+                cnt += 1
         
-        if total_cnt <= k: return '.'
-
-        index = k
-        cnt = total_cnt
+        if cnt <= k: return '.'
 
         for i in range(n-1, -1, -1):
             char = s[i]
@@ -27,10 +22,10 @@ class Solution:
                 cnt += 1
             elif char == '#':
                 cnt //= 2
-                if index >= cnt:
-                    index -= cnt
+                if k >= cnt:
+                    k -= cnt
             elif char == '%':
-                index = cnt - index - 1
+                k = cnt - k - 1
             else:
-                if index == cnt - 1: return char
+                if k == cnt - 1: return char
                 cnt -= 1
