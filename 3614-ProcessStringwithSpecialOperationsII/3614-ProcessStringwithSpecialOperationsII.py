f602f2b0da29ef4a28da0b2884a1e30955aff52e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-14 17:32:27 +0530

    File updated at 14/7/2025, 5:32:15 pm

--- 3614-ProcessStringwithSpecialOperationsII/3614-ProcessStringwithSpecialOperationsII.py
+++ 3614-ProcessStringwithSpecialOperationsII/3614-ProcessStringwithSpecialOperationsII.py
@@ -1,49 +1,36 @@
-# Last updated: 14/7/2025, 3:31:43 pm
-cmax = lambda x, y: x if x > y else y
+# Last updated: 14/7/2025, 5:32:15 pm
 class Solution:
-    def maxLen(self, n: int, edges: List[List[int]], label: str) -> int:
-        graph = defaultdict(list)
-        half = n // 2
+    def processStr(self, s: str, k: int) -> str:
+        n = len(s)
+        total_cnt = 0
+        new_s = {}
 
-        freq = defaultdict(int)
+        for char in s:
+            if char == '*':
+                total_cnt = max(0, total_cnt - 1)
+            elif char == '#':
+                total_cnt += total_cnt
+            elif char == '%':
+                pass
+            else:
+                total_cnt += 1
+                new_s[char] = 1
+        
+        if total_cnt <= k: return '.'
 
-        for char in label:
-            freq[char] += 1
+        index = k
+        cnt = total_cnt
 
-        for x, y in edges:
-            graph[x].append(y)
-            graph[y].append(x)
-        
-        def dfs(mask, x, st, index):
-            if index == len(st): return True
-            for y in graph[x]:
-                if st[index] == label[y] and mask & (1 << y) == 0:
-                    if dfs(mask | (1 << y), y, st, index + 1): return True
-            return False
-        
-        def isMatch(st):
-            # print(st)
-            for i in range(n):
-                if label[i] == st[0] and dfs(1 << i, i, st, 1):
-                    return True
-            return False
-        
-        max_size = 1
-        def rec(st):
-            nonlocal max_size
-            new_st = st + st[::-1]
-            is_pal = new_st == new_st[::-1]
-            if is_pal and len(st) * 2 > max_size and isMatch(new_st):
-                max_size = len(st) * 2
-            for char in freq:
-                if is_pal and freq[char] > 0 and len(st) * 2 + 1 > max_size and isMatch(st + char + st[::-1]):
-                    max_size = len(st) * 2 + 1
-                
-                if freq[char] > 1:
-                    freq[char] -= 2
-                    rec(st + char)
-                    freq[char] += 2
-        
-        rec('')
-        return max_size
-        
+        for i in range(n-1, -1, -1):
+            char = s[i]
+            if char == '*':
+                cnt += 1
+            elif char == '#':
+                cnt //= 2
+                if index >= cnt:
+                    index -= cnt
+            elif char == '%':
+                index = cnt - index - 1
+            else:
+                if index == cnt - 1: return char
+                cnt -= 1
