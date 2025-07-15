dc804e65d61becc4444de6e62e0a584e51fc6ec9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-15 17:33:24 +0530

    File updated at 15/7/2025, 5:33:11 pm

--- 3507-MinimumPairRemovaltoSortArrayI/3507-MinimumPairRemovaltoSortArrayI.py
+++ 3507-MinimumPairRemovaltoSortArrayI/3507-MinimumPairRemovaltoSortArrayI.py
@@ -1,31 +1,65 @@
-# Last updated: 15/7/2025, 3:16:30 pm
+# Last updated: 15/7/2025, 5:33:11 pm
+class Node:
+    def __init__(self, val, prev=None):
+        self.val = val
+        self.prev = prev
+        self.next = None
+        self.deleted = False
+        self.index = -1
+    
+    def __lt__(self, node):
+        return self.index < node.index
+
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
-        op = 0
-        def isSorted(nums):
-            for i in range(len(nums)-1):
-                if nums[i] > nums[i + 1]: return False
+        head = Node(nums[0])
+        node = head
+        node.index = 0
+
+        heap = []
+
+        for i in range(len(nums) - 1):
+            node.next = Node(nums[i + 1], node)
+            heapq.heappush(heap, (nums[i] + nums[i+1], node))
+            node = node.next
+            node.index = i + 1
+        
+        def isSorted(node):
+            # print(node)
+            while node.next:
+                if node.val > node.next.val: return False
+                node = node.next
             return True
         
-        while not isSorted(nums):
-            n = len(nums)
-            sortest = 0
-            sortest_sum = nums[0] + nums[1]
-            for i in range(n-1):
-                s = nums[i] + nums[i + 1]
-                if s < sortest_sum:
-                    sortest_sum = s
-                    sortest = i
+        op = 0
+        while not isSorted(head):
+            while heap and (heap[0][1].deleted or heap[0][1].val + heap[0][1].next.val != heap[0][0]):
+                heapq.heappop(heap)
+            s, node = heapq.heappop(heap)
+
+            index = node.index
+            # print('==>', s, node)
+            node.deleted = True
+            node.next.deleted = True
+
+            new_node = Node(s, node.prev)
+            new_node.index = index
             
-            new_nums = []
-            for i in range(n):
-                if i == sortest:
-                    new_nums.append(sortest_sum)
-                elif i == sortest + 1: continue
-                else:
-                    new_nums.append(nums[i])
-            nums = new_nums
+            if node.prev:
+                node.prev.next = new_node
+                heapq.heappush(heap, (new_node.prev.val + new_node.val, new_node.prev))
+            else:
+                head = new_node
+
+            if node.next.next:
+                node.next.next.prev = new_node
+                new_node.next = node.next.next
+
+                heapq.heappush(heap, (new_node.val + new_node.next.val, new_node))
+
             op += 1
         
         return op
 
+
+            
