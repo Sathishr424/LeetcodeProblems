678d82a80ca9bdee708e955ea435b6b162464830
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-15 14:45:22 +0530

    File updated at 15/7/2025, 2:45:09 pm

--- 3509-MaximumProductofSubsequencesWithanAlternatingSumEqualtoK/3509-MaximumProductofSubsequencesWithanAlternatingSumEqualtoK.py
+++ 3509-MaximumProductofSubsequencesWithanAlternatingSumEqualtoK/3509-MaximumProductofSubsequencesWithanAlternatingSumEqualtoK.py
@@ -1,4 +1,5 @@
-# Last updated: 15/7/2025, 2:43:19 pm
+# Last updated: 15/7/2025, 2:45:09 pm
+cmax = lambda x, y: x if x > y else y
 class Solution:
     def maxProduct(self, nums: List[int], k: int, limit: int) -> int:
         n = len(nums)
@@ -18,7 +19,7 @@ class Solution:
                 return -inf
             
             ans = rec(index + 1, is_odd, s, lim, zero_used)
-            
+
             if is_odd:
                 s -= nums[index]
             else:
@@ -26,16 +27,16 @@ class Solution:
             zero_used = zero_used or nums[index] == 0
 
             if lim * nums[index] <= limit:
-                ans = max(ans, rec(index + 1, not is_odd, s, lim * nums[index], zero_used))
+                ans = cmax(ans, rec(index + 1, not is_odd, s, lim * nums[index], zero_used))
             elif index <= zero_index:
-                ans = max(ans, rec(index + 1, not is_odd, s, 0, zero_used))
+                ans = cmax(ans, rec(index + 1, not is_odd, s, 0, zero_used))
             
             return ans
         
         ans = -inf
         for i in range(n):
             if nums[i] <= limit:
-                ans = max(ans, rec(i + 1, True, nums[i], nums[i], nums[i] == 0))
+                ans = cmax(ans, rec(i + 1, True, nums[i], nums[i], nums[i] == 0))
         if ans == -inf: return -1
         rec.cache_clear()
         return ans
