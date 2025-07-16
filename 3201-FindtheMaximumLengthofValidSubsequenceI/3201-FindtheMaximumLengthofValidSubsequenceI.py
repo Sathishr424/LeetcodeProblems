4d0d1749fac55ca42ccec82116b0fb3f1b0501a0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-16 17:55:59 +0530

    File updated at 16/7/2025, 5:55:58 pm

--- 3201-FindtheMaximumLengthofValidSubsequenceI/3201-FindtheMaximumLengthofValidSubsequenceI.py
+++ 3201-FindtheMaximumLengthofValidSubsequenceI/3201-FindtheMaximumLengthofValidSubsequenceI.py
@@ -1,4 +1,4 @@
-# Last updated: 16/7/2025, 12:22:15 pm
+# Last updated: 16/7/2025, 5:55:58 pm
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
         ret = 0
@@ -6,7 +6,7 @@ class Solution:
             dp = [0, 0]
             for num in nums:
                 curr = num % 2
-                need = (curr - add) % 2
+                need = (add - curr) % 2
                 dp[curr] = dp[need] + 1
             
             ret = max(ret, max(dp))
