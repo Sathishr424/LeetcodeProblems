58f840bb26a4d836c964a9859ea38fa3f236910a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-16 18:54:36 +0530

    File updated at 16/7/2025, 6:54:35 pm

--- 1027-LongestArithmeticSubsequence/1027-LongestArithmeticSubsequence.py
+++ 1027-LongestArithmeticSubsequence/1027-LongestArithmeticSubsequence.py
@@ -1,24 +1,15 @@
-# Last updated: 16/7/2025, 6:54:17 pm
+# Last updated: 16/7/2025, 6:54:35 pm
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def longestArithSeqLength(self, nums: List[int]) -> int:
-        n = len(nums)
         maxi = max(nums)
         ret = 0
-        m = maxi * 3 + 1
-        counter = [[0] * m for _ in range(maxi * 2 + 1)]
-        is_on_diff = [0] * (maxi * 2 + 1)
 
-        for i in range(n):
-            for j in range(i+1, n):
-                is_on_diff[nums[j] - nums[i] + maxi] = 1
-        
         for diff in range(-maxi, maxi + 1):
-            index = diff + maxi
-            if is_on_diff[index] == 0: continue
+            counter = [0] * (maxi * 3 + 1)
             for num in nums:
-                counter[index][num + maxi] = counter[index][ (num - diff + maxi) ] + 1
-            ret = cmax(ret, max(counter[index]))
+                counter[num + maxi] = counter[ (num - diff + maxi) ] + 1
+            ret = cmax(ret, max(counter))
 
         return ret
 
