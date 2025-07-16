e174b550c50a78883df708cf1b9048049c1a869d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-16 08:51:19 +0530

    Create 3201-FindtheMaximumLengthofValidSubsequenceI

---
+++ 3201-FindtheMaximumLengthofValidSubsequenceI/3201-FindtheMaximumLengthofValidSubsequenceI.py
@@ -0,0 +1,21 @@
+# Last updated: 16/7/2025, 8:51:19 am
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        # nums = [random.randrange(1, 10**7 + 1) for _ in range(2 * 10**5)]
+        n = len(nums)
+
+        @cache
+        def rec(index, to_match, balance):
+            if index == n: return 0
+
+            ans = rec(index + 1, to_match, balance)
+            if (nums[index] + balance) % 2 == to_match:
+                ans = max(ans, rec(index + 1, to_match, nums[index] % 2) + 1)
+            return ans
+        
+        ans = 0
+        for i in range(n):
+            ans = max(ans, rec(i + 1, 0, nums[i] % 2) + 1, rec(i + 1, 1, nums[i] % 2) + 1)
+        
+        rec.cache_clear()
+        return ans
