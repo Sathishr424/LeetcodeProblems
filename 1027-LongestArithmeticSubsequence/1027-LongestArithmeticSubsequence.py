bb8fa85f7b5045844e79ee15cdb7a0b7f443a56c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-16 18:41:33 +0530

    File updated at 16/7/2025, 6:41:32 pm

--- 1027-LongestArithmeticSubsequence/1027-LongestArithmeticSubsequence.py
+++ 1027-LongestArithmeticSubsequence/1027-LongestArithmeticSubsequence.py
@@ -1,11 +1,11 @@
-# Last updated: 16/7/2025, 6:36:20 pm
+# Last updated: 16/7/2025, 6:41:32 pm
 class Solution:
     def longestArithSeqLength(self, nums: List[int]) -> int:
         n = len(nums)
         maxi = max(nums)
         ret = 0
 
-        diffs = {}
+        diffs = defaultdict(int)
         for i in range(n):
             for j in range(i+1, n):
                 diffs[nums[j] - nums[i]] = 1
@@ -13,10 +13,10 @@ class Solution:
         # 4, 6 => 2
         # 6, 4 => -2
         for diff in diffs:
-            counter = defaultdict(int)
+            counter = [0] * 1501
             for i in range(n):
-                counter[nums[i]] = counter[ nums[i] - diff ] + 1
-                ret = max(ret, counter[nums[i]])
+                counter[nums[i] + 500] = counter[ (nums[i] - diff + 500) ] + 1
+                ret = max(ret, counter[nums[i] + 500])
             # print(diff, [(i, counter[i]) for i in range(maxi + 1) if counter[i] > 0])
         return ret
 
