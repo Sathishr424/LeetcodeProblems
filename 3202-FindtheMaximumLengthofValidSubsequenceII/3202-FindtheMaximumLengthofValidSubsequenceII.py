3df50b7efbb45a9f598ace81be92ac4ac8abe647
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-16 11:54:24 +0530

    File updated at 16/7/2025, 11:54:24 am

--- 3202-FindtheMaximumLengthofValidSubsequenceII/3202-FindtheMaximumLengthofValidSubsequenceII.py
+++ 3202-FindtheMaximumLengthofValidSubsequenceII/3202-FindtheMaximumLengthofValidSubsequenceII.py
@@ -1,4 +1,4 @@
-# Last updated: 16/7/2025, 11:39:31 am
+# Last updated: 16/7/2025, 11:54:24 am
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def maximumLength(self, nums: List[int], k) -> int:
@@ -8,8 +8,8 @@ class Solution:
         for match in range(k):
             counter = [0] * k
             for i in range(n):
-                need = (k - (nums[i] % k) - match) % k
                 curr = nums[i] % k
+                need = (k - curr - match) % k
                 counter[curr] = cmax(1, counter[need] + 1)
                 ret = cmax(counter[curr], ret)
         
