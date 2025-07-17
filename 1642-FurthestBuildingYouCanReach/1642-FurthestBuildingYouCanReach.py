dd2a2c4368c57767ea33c7ce56b21eacfad7a1c6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-17 19:35:57 +0530

    File updated at 17/7/2025, 7:35:56 pm

--- 1642-FurthestBuildingYouCanReach/1642-FurthestBuildingYouCanReach.py
+++ 1642-FurthestBuildingYouCanReach/1642-FurthestBuildingYouCanReach.py
@@ -1,17 +1,15 @@
-# Last updated: 17/7/2025, 7:35:31 pm
+# Last updated: 17/7/2025, 7:35:56 pm
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
         n = len(heights)
 
         max_need = []
-        bricks_needed = 0
-
         for i in range(n - 1):
             if heights[i] < heights[i + 1]:
                 need = heights[i + 1] - heights[i]
                 heapq.heappush(max_need, need)
                 if len(max_need) > ladders:
-                    bricks_needed += heapq.heappop(max_need)
-                    if bricks_needed > bricks: return i
+                    bricks -= heapq.heappop(max_need)
+                    if bricks < 0: return i
         
         return n-1
