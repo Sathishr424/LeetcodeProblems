f3e851263d0f56d0a9511b9f99d32e2c54650edc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-17 15:06:10 +0530

    File updated at 17/7/2025, 3:06:09 pm

--- 447-NumberofBoomerangs/447-NumberofBoomerangs.py
+++ 447-NumberofBoomerangs/447-NumberofBoomerangs.py
@@ -1,9 +1,8 @@
-# Last updated: 17/7/2025, 3:05:51 pm
+# Last updated: 17/7/2025, 3:06:09 pm
 class Solution:
     def numberOfBoomerangs(self, points: List[List[int]]) -> int:
         n = len(points)
 
-        @cache
         def getDis(x1, y1, x2, y2):
             diff = (x2 - x1)**2 + (y2 - y1)**2
             return sqrt(diff)
@@ -16,5 +15,5 @@ class Solution:
                 d = getDis(points[i][0], points[i][1], points[j][0], points[j][1])
                 ret += dis[i][d] * 2
                 dis[i][d] += 1
-        getDis.cache_clear()
+
         return ret
