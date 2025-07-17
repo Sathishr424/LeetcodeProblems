6fe65fb9e8bcf38cfe0ef4dfd43a804338b9d6e5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-17 20:18:16 +0530

    File updated at 17/7/2025, 8:18:15 pm

--- 1996-TheNumberofWeakCharactersintheGame/1996-TheNumberofWeakCharactersintheGame.py
+++ 1996-TheNumberofWeakCharactersintheGame/1996-TheNumberofWeakCharactersintheGame.py
@@ -1,25 +1,23 @@
-# Last updated: 17/7/2025, 8:11:24 pm
+# Last updated: 17/7/2025, 8:18:15 pm
 class Solution:
     def numberOfWeakCharacters(self, prop: List[List[int]]) -> int:
         n = len(prop)
         prop.sort(key=lambda x: -x[0])
-
-        sl = SortedList()
-        ret = 0
         # print(prop)
+        maxi = 0
+        ret = 0
         i = 0
+
         while i < n:
-            cnt = len(sl) - sl.bisect_right(prop[i][1])
-            if cnt: ret += 1
+            if maxi > prop[i][1]: ret += 1
 
             j = i + 1
             while j < n and prop[j][0] == prop[j-1][0]:
-                cnt = len(sl) - sl.bisect_right(prop[j][1])
-                if cnt: ret += 1
+                if maxi > prop[j][1]: ret += 1
                 j += 1
             
             while i < j:
-                sl.add(prop[i][1])
+                maxi = max(maxi, prop[i][1])
                 i += 1
         
         return ret
