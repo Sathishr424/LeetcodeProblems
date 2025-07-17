44d85c88e41b7938ad1ae11b8b41124d74c49b9d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-17 15:02:08 +0530

    Create 447-NumberofBoomerangs

---
+++ 447-NumberofBoomerangs/447-NumberofBoomerangs.py
@@ -0,0 +1,20 @@
+# Last updated: 17/7/2025, 3:02:08 pm
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        n = len(points)
+
+        def getDis(a, b):
+            diff = ((b[0] - a[0])**2) + ((b[1] - a[1])**2)
+            return sqrt(diff)
+
+        ret = 0
+        dis = defaultdict(lambda: defaultdict(int))
+        for i in range(n):
+            for j in range(n):
+                if j == i: continue
+                d = getDis(points[i], points[j])
+                # print((i, j), d, dict(dis))
+                ret += dis[i][d] * 2
+                dis[i][d] += 1
+        
+        return ret
