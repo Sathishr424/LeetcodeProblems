43f7c53225380c56e982d01d0d43ff434cfa0180
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-17 14:25:05 +0530

    File updated at 17/7/2025, 2:25:04 pm

--- 29-DivideTwoIntegers/29-DivideTwoIntegers.py
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.py
@@ -1,4 +1,4 @@
-# Last updated: 17/7/2025, 2:23:17 pm
+# Last updated: 17/7/2025, 2:25:04 pm
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         is_neg = 0
@@ -9,15 +9,22 @@ class Solution:
             dividend = -dividend
             is_neg += 1
         
-        def recAdd(num, add, op, op_add):
+        num = 0
+        add = divisor
+        op = 0
+        op_add = 1
+        while True:
             if num + add > dividend:
-                if op_add == 1: return op
-                return recAdd(num, divisor, op, 1)
+                if op_add == 1: break
+                add = divisor
+                op_add = 1
+                continue
             
-            return recAdd(num + add, add + add, op + op_add, op_add + op_add)
+            num += add
+            add += add
+            op += op_add
+            op_add += op_add
         
-
-        ans = recAdd(0, divisor, 0, 1)
         if is_neg == 1: 
-            return -min(ans, 2**31)
-        return min(ans, 2**31 - 1)
+            return -min(op, 2**31)
+        return min(op, 2**31 - 1)
