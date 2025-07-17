252023db2a938c2d274fa7b83ab4ad44ed76fbe9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-17 21:43:27 +0530

    File updated at 17/7/2025, 9:43:26 pm

--- 1656-DesignanOrderedStream/1656-DesignanOrderedStream.py
+++ 1656-DesignanOrderedStream/1656-DesignanOrderedStream.py
@@ -1,25 +1,22 @@
-# Last updated: 17/7/2025, 9:42:49 pm
+# Last updated: 17/7/2025, 9:43:26 pm
 class OrderedStream:
     def __init__(self, n: int):
         self.n = n
         self.arr = [''] * n
         self.used = [0] * n
-        self.output = [0] * n
 
     def insert(self, idKey: int, value: str) -> List[str]:
         idKey -= 1
         self.used[idKey] = 1
         self.arr[idKey] = value
 
-        ret = []
-
         for i in range(idKey):
             if self.used[i] == 0: return []
         
+        ret = []
         for i in range(idKey, self.n):
             if self.used[i] == 0: break
             ret.append(self.arr[i])
-            # self.output[i] = 1
         
         return ret
 
