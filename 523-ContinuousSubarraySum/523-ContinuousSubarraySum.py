194b25cc7c2941bd59566ed31f6711550fe9c896
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-17 17:55:38 +0530

    File updated at 17/7/2025, 5:55:38 pm

--- 523-ContinuousSubarraySum/523-ContinuousSubarraySum.py
+++ 523-ContinuousSubarraySum/523-ContinuousSubarraySum.py
@@ -1,29 +1,19 @@
-# Last updated: 17/7/2025, 5:54:02 pm
+# Last updated: 17/7/2025, 5:55:38 pm
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         n = len(nums)
 
-        for i in range(n):
-            nums[i] %= k
-
-        ret = 1
         counter = {}
         tot = 0
         for i in range(n):
             tot = (tot + nums[i]) % k
 
             need = (k - tot) % k
-            # print(nums[:i+1], tot, need, counter)
 
-            if tot in counter:
-                ret = max(ret, i - counter[tot])
-                # if ret >= 2: return True
-            
-            if tot == 0: 
-                ret = i + 1
-                # if ret >= 2: return True
+            if tot in counter and i - counter[tot] >= 2: return True
+            if tot == 0 and i >= 1: return True 
             
             if tot not in counter:
                 counter[tot] = i
 
-        return ret >= 2
+        return False
