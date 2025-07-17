54a629e4f5179e39c66f88edc6b258f2128adc4a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-17 18:56:53 +0530

    File updated at 17/7/2025, 6:56:53 pm

--- 665-Non-decreasingArray/665-Non-decreasingArray.py
+++ 665-Non-decreasingArray/665-Non-decreasingArray.py
@@ -1,22 +1,17 @@
-# Last updated: 17/7/2025, 6:54:02 pm
+# Last updated: 17/7/2025, 6:56:53 pm
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
         n = len(nums)
-        okay_right = [1] * n
 
-        for i in range(n-2, -1, -1):
-            if okay_right[i + 1] == 0 or nums[i] > nums[i + 1]:
-                okay_right[i] = 0
-            
+        count = 0
         for i in range(n-1):
             if nums[i] > nums[i + 1]:
                 if i == 0:
-                    if okay_right[i + 1]: return True
-                    return False
+                    count += 1
                 else:
-                    if not okay_right[i + 1]: return False
+                    count += 1
+                    if count > 1: return False
                     if i + 2 == n: return True
                     if nums[i + 2] < nums[i] and nums[i-1] > nums[i + 1]: return False
-                    return True
         
         return True
