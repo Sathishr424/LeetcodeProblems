66c8d24b42a2003ace864c780d101e01efc4b3ad
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-18 15:03:18 +0530

    File updated at 18/7/2025, 3:03:15 pm

--- 2163-MinimumDifferenceinSumsAfterRemovalofElements/2163-MinimumDifferenceinSumsAfterRemovalofElements.py
+++ 2163-MinimumDifferenceinSumsAfterRemovalofElements/2163-MinimumDifferenceinSumsAfterRemovalofElements.py
@@ -1,4 +1,4 @@
-# Last updated: 18/7/2025, 2:58:13 pm
+# Last updated: 18/7/2025, 3:03:15 pm
 cmax = lambda x, y: x if x > y else y
 cmin = lambda x, y: x if x < y else y
 class Solution:
@@ -10,27 +10,32 @@ class Solution:
         heap = []
         s = 0
         max_sum = [0] * (n + 1)
-        for i in range(n-1, n-(quater*2)-1, -1):
+
+        for i in range(n-quater+1, n):
             heapq.heappush(heap, nums[i])
             s += nums[i]
-            if len(heap) > quater:
-                s -= heapq.heappop(heap)
-            if len(heap) == quater:
-                max_sum[i] = cmax(max_sum[i + 1], s)
 
-        left_end = n - quater
+        for i in range(n-quater, n-(quater*2)-1, -1):
+            heapq.heappush(heap, nums[i])
+            s += nums[i]
+            max_sum[i] = cmax(max_sum[i + 1], s)
+            
+            s -= heapq.heappop(heap)
 
         heap = []
         s = 0
         ret = inf
-        for i in range(n-quater):
+
+        for i in range(quater-1):
             heapq.heappush(heap, -nums[i])
             s += nums[i]
-            
-            if len(heap) > quater:
-                s -= -heapq.heappop(heap)
-            if len(heap) == quater:
-                ret = cmin(ret, s - max_sum[i + 1])
+
+        for i in range(quater-1, n-quater):
+            heapq.heappush(heap, -nums[i])
+            s += nums[i]
+            ret = cmin(ret, s - max_sum[i + 1])
+
+            s += heapq.heappop(heap)
 
         return ret
                 
