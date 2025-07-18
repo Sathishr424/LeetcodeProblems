ca274a9e132f24f705accebb6660ccea197cf535
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-18 14:34:17 +0530

    File updated at 18/7/2025, 2:34:14 pm

--- 3469-FindMinimumCosttoRemoveArrayElements/3469-FindMinimumCosttoRemoveArrayElements.py
+++ 3469-FindMinimumCosttoRemoveArrayElements/3469-FindMinimumCosttoRemoveArrayElements.py
@@ -1,21 +1,22 @@
-# Last updated: 18/7/2025, 2:33:21 pm
+# Last updated: 18/7/2025, 2:34:14 pm
 class Solution:
     def minCost(self, nums: List[int]) -> int:
         n = len(nums)
 
         @cache
-        def rec(x, y, z):
+        def rec(x, y):
+            z = y + 1
             if  y >= n or z >= n:
                 curr = nums[x]
                 if y < n: curr = max(curr, nums[y])
                 if z < n: curr = max(curr, nums[z])
                 return curr
-            one = rec(z, z + 1, z + 2) + max(nums[x], nums[y])
-            two = rec(y, z + 1, z + 2) + max(nums[x], nums[z])
-            three = rec(x, z + 1, z + 2) + max(nums[y], nums[z])
+            one = rec(z, z + 1) + max(nums[x], nums[y])
+            two = rec(y, z + 1) + max(nums[x], nums[z])
+            three = rec(x, z + 1) + max(nums[y], nums[z])
 
             return min(one, two, three)
         
-        ans = rec(0, 1, 2)
+        ans = rec(0, 1)
         rec.cache_clear()
         return ans
