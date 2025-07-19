6a5fe2846fadcca37365eeb225ef61ef33511daa
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-19 14:02:25 +0530

    File updated at 19/7/2025, 2:02:21 pm

--- 1233-RemoveSub-FoldersfromtheFilesystem/1233-RemoveSub-FoldersfromtheFilesystem.py
+++ 1233-RemoveSub-FoldersfromtheFilesystem/1233-RemoveSub-FoldersfromtheFilesystem.py
@@ -1,21 +1,45 @@
-# Last updated: 19/7/2025, 1:33:15 pm
+# Last updated: 19/7/2025, 2:02:21 pm
+class Node:
+    def __init__(self):
+        self.childs = [None] * 27
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.mainNode = Node()
+    
+    def insert(self, folder):
+        node = self.mainNode
+        for char in folder:
+            if char == '/':
+                if node.is_end:
+                    print(folder, char)
+                    return
+                a = 26
+            else:
+                a = ord(char) - ord('a')
+            if node.childs[a] == None:
+                node.childs[a] = Node()
+            node = node.childs[a]
+        node.is_end = True
+    
+    def getAllMainFolders(self, node, s, ret):
+        if node.is_end:
+            ret.append(s)
+        for child in range(26):
+            if node.childs[child] == None: continue
+            self.getAllMainFolders(node.childs[child], s + chr(child + ord('a')), ret)
+        if node.childs[26] != None:
+            self.getAllMainFolders(node.childs[26], s + '/', ret)
+
 class Solution:
-    def removeSubfolders(self, folder: List[str]) -> List[str]:
-        n = len(folder)
-        folder.sort(key=lambda x: len(x))
-
-        ret = []
-        starts = {}
-
-        for i in range(n):
-            is_sub = False
-            for j in range(1, len(folder[i])):
-                if folder[i][j] == '/':
-                    if folder[i][:j] in starts:
-                        is_sub = True
-                        break
-            if not is_sub:
-                ret.append(folder[i])
-                starts[folder[i]] = 1
+    def removeSubfolders(self, folders: List[str]) -> List[str]:
+        n = len(folders)
+        folders.sort(key=lambda x: len(x))
+        trie = Trie()
+        for folder in folders:
+            trie.insert(folder)
         
+        ret = []
+        trie.getAllMainFolders(trie.mainNode, '', ret)
         return ret
