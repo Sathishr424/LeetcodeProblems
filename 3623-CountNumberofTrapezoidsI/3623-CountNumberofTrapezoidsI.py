4debfc8ceb4a99dc963b4dc3f44fa5799d79e49a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-20 09:55:38 +0530

    Create 3623-CountNumberofTrapezoidsI

---
+++ 3623-CountNumberofTrapezoidsI/3623-CountNumberofTrapezoidsI.py
@@ -0,0 +1,30 @@
+# Last updated: 20/7/2025, 9:55:38 am
+mod = 10**9 + 7
+
+@cache
+def fact(x):
+    return factorial(x)
+
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        n = len(points)
+
+        hor = defaultdict(int)
+
+        for x, y in points:
+            hor[y] += 1
+
+        if len(hor) == 1: return 0
+        ret = 0
+        prev = 0
+        # print(hor)
+        for h in hor:
+            cnt = hor[h]
+            if cnt == 1: continue
+            curr = fact(cnt) // (fact(cnt - 2) * fact(2))
+            ret += prev * curr % mod
+            ret %= mod
+            prev += curr
+            prev %= mod
+
+        return ret
