dc4320c3ac343654eca1335abea8a7b833cf5523
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-21 15:33:44 +0530

    File updated at 21/7/2025, 3:33:42 pm

--- 37-SudokuSolver/37-SudokuSolver.cpp
+++ 37-SudokuSolver/37-SudokuSolver.cpp
@@ -1,61 +1,46 @@
-// Last updated: 21/7/2025, 3:25:52 pm
-#include <vector>
-#include <string>
-using namespace std;
-
+// Last updated: 21/7/2025, 3:33:42 pm
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        vector<int> boxes(9, 0);
-        vector<int> rows(9, 0);
-        vector<int> cols(9, 0);
-        vector<pair<int, int>> need;
-        int n = 9;
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i][j] == '.') {
-                    need.push_back({i, j});
-                } else {
-                    int pos = 1 << (board[i][j] - '1');
-                    int box = (i / 3) * 3 + (j / 3);
-                    rows[i] |= pos;
-                    cols[j] |= pos;
-                    boxes[box] |= pos;
-                }
-            }
+    
+    bool isValid(vector<vector<char>>& board , int row , int col , int num){
+        for(int i=0 ; i<9 ; i++){
+            if(board[row][i]== '0'+num) return false ;
+        }
+        // col check
+        for(int j=0 ; j<9 ; j++){
+            if(board[j][col] == '0'+num) return false ;
         }
 
-        function<bool(int)> dfs = [&](int index) {
-            if (index == need.size()) return true;
+        // small 3x3 grid check
+        int x = (row/3)*3 ;
+        int y = (col/3)*3 ;
 
-            int i = need[index].first;
-            int j = need[index].second;
-            int box = (i / 3) * 3 + (j / 3);
-
-            for (int val = 0; val < 9; ++val) {
-                int pos = 1 << val;
-                if ((rows[i] & pos) == 0 && (cols[j] & pos) == 0 && (boxes[box] & pos) == 0) {
-                    int r = rows[i];
-                    int c = cols[j];
-                    int b = boxes[box];
-
-                    rows[i] |= pos;
-                    cols[j] |= pos;
-                    boxes[box] |= pos;
-                    board[i][j] = char('1' + val);
-
-                    if (dfs(index + 1)) return true;
-
-                    rows[i] = r;
-                    cols[j] = c;
-                    boxes[box] = b;
-                    board[i][j] = '.';
-                }
+        for(int i=x ; i<x+3 ; i++){
+            for(int j=y ; j<y+3 ; j++){
+                if(board[i][j]=='0'+num) return false ;
             }
-            return false;
-        };
+        }
+        return true ;
+    }
 
-        dfs(0);
+    bool helper(vector<vector<char>>& board , int row , int col){
+        if(row == 9) return true ;
+        if(col == 9) return helper(board , row+1 , 0) ;
+        if(board[row][col] != '.') return helper(board , row , col+1) ;
+
+        for(int i=1 ; i<=9 ; i++){
+            if(isValid(board , row , col , i)){
+                board[row][col] = '0' + i ;
+                bool res = helper(board , row , col+1) ;
+                if(res) return true ;
+                board[row][col] = '.' ;
+            }
+        }
+        return false ;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        helper(board , 0 , 0) ;
+        return ;
     }
 };
