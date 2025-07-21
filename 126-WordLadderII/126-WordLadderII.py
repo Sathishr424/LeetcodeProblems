b9d8243c29b4f28ada13a033ba6999787c835621
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-21 21:24:04 +0530

    File updated at 21/7/2025, 9:24:01 pm

--- 126-WordLadderII/126-WordLadderII.py
+++ 126-WordLadderII/126-WordLadderII.py
@@ -1,4 +1,4 @@
-# Last updated: 21/7/2025, 9:22:30 pm
+# Last updated: 21/7/2025, 9:24:01 pm
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
         there = defaultdict(list)
@@ -15,8 +15,7 @@ class Solution:
                 new_word = beginWord[:i] + "*" + beginWord[i+1:]
                 there[new_word].append(beginWord)
                 possible[beginWord].append(new_word)
-            
-        # print(dict(possible))
+        
         ret = []      
         def addItoRet(index, arr, s):
             if index == len(arr):
@@ -28,25 +27,24 @@ class Solution:
                     addItoRet(index + 1, arr, s)
                     s.pop()      
         
-        stack = deque([(beginWord, 1, [beginWord])])
-        smallest = {}
+        stack = deque([(beginWord, 1)])
+        can_visit = {}
         visited = {}
         shortest = inf
         while stack:
-            word, cnt, s = stack.popleft()
+            word, cnt = stack.popleft()
             if word in visited and visited[word] <= cnt: continue
             visited[word] = cnt
-            smallest[word] = s[:-1]
+            can_visit[word] = 1
             if word == endWord:
                 shortest = cnt
                 break
 
             for new_word in possible[word]:
                 for n_word in there[new_word]:
-                    stack.append((n_word, cnt + 1, s + [n_word]))
+                    stack.append((n_word, cnt + 1))
         
         if shortest == inf: return []
-        # print(smallest)
 
         visited = {}
         def dfs(word, s):
@@ -56,7 +54,7 @@ class Solution:
             if len(s) == shortest: return
             for new_word in possible[word]:
                 for n_word in there[new_word]:
-                    if n_word not in smallest or n_word in visited: continue
+                    if n_word not in can_visit or n_word in visited: continue
                     visited[n_word] = 1
                     s.append(n_word)
                     dfs(n_word, s)
