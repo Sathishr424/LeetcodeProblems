a9686948851ee561b2c0ecc6a44e9c2b38bea6a9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-21 15:17:25 +0530

    File updated at 21/7/2025, 3:17:24 pm

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,20 +1,18 @@
-# Last updated: 12/6/2025, 5:54:41 am
+# Last updated: 21/7/2025, 3:17:24 pm
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [{} for _ in range(9)]
-        cols = [{} for _ in range(9)]
-        boxes = [{} for _ in range(9)]
+        rows = [0] * 9
+        cols = [0] * 9
+        boxes = [0] * 9
 
         for i in range(9):
             for j in range(9):
-                val = board[i][j]
-                if val == '.': continue
-                elif val in rows[i]:  return False
-                elif val in cols[j]: return False
-                z = ((i//3)*3)+(j//3)
-                if val in boxes[z]: return False
-                rows[i][val] = 1
-                cols[j][val] = 1
-                boxes[z][val] = 1
+                if board[i][j] == '.': continue
+                box = (i // 3) * 3 + (j // 3)
+                pos = 1 << (int(board[i][j]) - 1)
+                if rows[i] & pos != 0 or cols[j] & pos != 0 or boxes[box] & pos != 0: return False
+                rows[i] |= pos
+                cols[j] |= pos
+                boxes[box] |= pos
+        
         return True
-
