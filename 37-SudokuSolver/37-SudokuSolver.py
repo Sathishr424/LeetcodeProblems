12aa8a55c5d91d9f7dd1a7e7a68547d8530d2c0f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-21 13:17:29 +0530

    File updated at 21/7/2025, 1:17:28 pm

--- 37-SudokuSolver/37-SudokuSolver.py
+++ 37-SudokuSolver/37-SudokuSolver.py
@@ -1,19 +1,19 @@
-# Last updated: 21/7/2025, 1:13:34 pm
+# Last updated: 21/7/2025, 1:17:28 pm
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        boxes = [0] * 10
-        rows = [0] * 10
-        cols = [0] * 10
+        boxes = [0] * 9
+        rows = [0] * 9
+        cols = [0] * 9
         need = []
         n = 9
         for i in range(n):
             for j in range(n):
                 if board[i][j] == '.': need.append((i, j))
                 else:
-                    pos = 1 << int(board[i][j])
+                    pos = 1 << (int(board[i][j]) - 1)
                     box = (i // 3) * 3 + (j // 3)
                     rows[i] |= pos
                     cols[j] |= pos
@@ -24,7 +24,7 @@ class Solution:
                 return True
             i, j = need[index]
             box = (i // 3) * 3 + (j // 3)
-            for val in range(1, 10):
+            for val in range(9):
                 pos = 1 << val
                 if rows[i] & pos == 0 and cols[j] & pos == 0 and boxes[box] & pos == 0:
                     r = rows[i]
@@ -33,7 +33,7 @@ class Solution:
                     rows[i] |= pos
                     cols[j] |= pos
                     boxes[box] |= pos
-                    board[i][j] = str(val)
+                    board[i][j] = str(val + 1)
                     if dfs(index + 1): return True
                     rows[i] = r
                     cols[j] = c
