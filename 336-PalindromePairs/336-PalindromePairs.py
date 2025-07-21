5af3f05e950ce72e1e6339d2f84acaca8d38e126
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-21 23:12:05 +0530

    File updated at 21/7/2025, 11:12:02 pm

--- 336-PalindromePairs/336-PalindromePairs.py
+++ 336-PalindromePairs/336-PalindromePairs.py
@@ -1,7 +1,7 @@
-# Last updated: 21/7/2025, 11:00:07 pm
+# Last updated: 21/7/2025, 11:12:02 pm
 class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
-        forwards = defaultdict(list)
+        forwards = {}
         ret = []
         visited = {}
 
@@ -10,21 +10,19 @@ class Solution:
                 left = word[j:][::-1]
                 curr = left + word
                 if curr == curr[::-1]:
-                    for index in forwards[left]:
-                        if index == i or (index, i) in visited: continue
-                        visited[(index, i)] = 1
+                    if left in forwards and forwards[left] != i:
+                        visited[(forwards[left], i)] = 1
             
         def checkPalReverse(i, word):
             for j in range(len(word) + 1):
                 right = word[:j][::-1]
                 curr = word + right
                 if curr == curr[::-1]:
-                    for index in forwards[right]:
-                        if index == i or (i, index) in visited: continue
-                        visited[(i, index)] = 1
+                    if right in forwards and forwards[right] != i:
+                        visited[(i, forwards[right])] = 1
         
         for i, word in enumerate(words):
-            forwards[word].append(i)
+            forwards[word] = i
         
         for i, word in enumerate(words):
             checkPalForward(i, word)
