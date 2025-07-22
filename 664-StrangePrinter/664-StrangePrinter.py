22134354f65cc5ca955bb493e54a59237dd2008b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-23 00:45:24 +0530

    File updated at 23/7/2025, 12:45:22 am

--- 664-StrangePrinter/664-StrangePrinter.py
+++ 664-StrangePrinter/664-StrangePrinter.py
@@ -1,4 +1,6 @@
-# Last updated: 23/7/2025, 12:44:30 am
+# Last updated: 23/7/2025, 12:45:22 am
+inf = 101
+cmin = lambda x, y: x if x < y else y
 class Solution:
     def strangePrinter(self, s: str) -> int:
         n = len(s)
@@ -17,9 +19,9 @@ class Solution:
                 if s[i] == s[l - 1]:
                     left = rec(l, i - 1)
                     right = rec(i, r)
-                    ans = min(ans, left + right)
+                    ans = cmin(ans, left + right)
             
-            ans = min(ans, rec(l, r) + 1)
+            ans = cmin(ans, rec(l, r) + 1)
             return ans
         
         ret = rec(0, n-1)
