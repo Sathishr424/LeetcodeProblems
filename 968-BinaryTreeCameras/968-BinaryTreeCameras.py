b586e76dde27a8b90a8e100270ce56ad2ecc3afc
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-23 19:36:08 +0530

    File updated at 23/7/2025, 7:36:04 pm

--- 968-BinaryTreeCameras/968-BinaryTreeCameras.py
+++ 968-BinaryTreeCameras/968-BinaryTreeCameras.py
@@ -1,10 +1,11 @@
-# Last updated: 23/7/2025, 7:33:23 pm
+# Last updated: 23/7/2025, 7:36:04 pm
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+cmin = lambda x, y: x if x < y else y
 class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
         @cache
@@ -15,11 +16,11 @@ class Solution:
             ans = rec(node.left, 2) + rec(node.right, 2) + 1
 
             if can >= 2:
-                ans = min(ans, rec(node.left, can - 1) + rec(node.right, can - 1))
+                ans = cmin(ans, rec(node.left, can - 1) + rec(node.right, can - 1))
             elif can == 1 and (node.left or node.right):
-                ans = min(ans, rec(node.left, 3) + rec(node.right, 1) + 1)
-                ans = min(ans, rec(node.left, 1) + rec(node.right, 3) + 1)
+                ans = cmin(ans, rec(node.left, 3) + rec(node.right, 1) + 1)
+                ans = cmin(ans, rec(node.left, 1) + rec(node.right, 3) + 1)
             
             return ans
         
-        return min(rec(root, 1), rec(root, 3) + 1)
+        return cmin(rec(root, 1), rec(root, 3) + 1)
