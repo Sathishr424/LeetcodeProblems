116c018dbc5f477c679c8e2189c631a8424496d3
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-23 15:23:04 +0530

    File updated at 23/7/2025, 3:23:01 pm

--- 834-SumofDistancesinTree/834-SumofDistancesinTree.py
+++ 834-SumofDistancesinTree/834-SumofDistancesinTree.py
@@ -1,14 +1,13 @@
-# Last updated: 23/7/2025, 3:18:18 pm
+# Last updated: 23/7/2025, 3:23:01 pm
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
+        graph = [[] for _ in range(n)]
 
         for x, y in edges:
             graph[x].append(y)
             graph[y].append(x)
 
         ret = [0] * n
-
         costs = [0] * n
         connections = [0] * n
 
@@ -27,22 +26,18 @@ class Solution:
         dfs(0, -1)
 
         def dfs2(x, par, connection, cost):
-            # print((par, x), connection, cost)
             costs[x] += cost + connection
 
             c = costs[x]
             ret[x] = c
             conn = connections[x]
-            # print(x, 'newcost', c, cost + connection)
             for y in graph[x]:
                 if y == par: continue
                 conn_ = conn - connections[y]
                 cost_ = c - (costs[y] + connections[y] + 1)
-                # print((par, x, y), conn_, cost_, (c, conn))
                 dfs2(y, x, connection + conn_, cost_)
 
-        # print(costs)
-        # print(connections)
+
         dfs2(0, -1, 0, 0)
         return ret
                 
