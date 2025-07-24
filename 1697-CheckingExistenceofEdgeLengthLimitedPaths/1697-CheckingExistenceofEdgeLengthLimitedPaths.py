57cbbfeec46448cb107a711509e3799947e5aa64
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-25 03:52:04 +0530

    Create 1697-CheckingExistenceofEdgeLengthLimitedPaths

---
+++ 1697-CheckingExistenceofEdgeLengthLimitedPaths/1697-CheckingExistenceofEdgeLengthLimitedPaths.py
@@ -0,0 +1,125 @@
+# Last updated: 25/7/2025, 3:51:58 am
+class Union:
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
+        self.sizes = [1] * n
+
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+
+        return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y: return True
+
+        if self.sizes[y] > self.sizes[x]:
+            x, y = y, x
+
+        self.sizes[x] += self.sizes[y]
+        self.parents[y] = x
+        
+        return False
+
+class Solution:
+    def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        newEdgeList = []
+        edgeList.sort(key=lambda x: x[2])
+
+        un = Union(n)
+        for x, y, d in edgeList:
+            if un.union(x, y): continue
+            newEdgeList.append([x, y, d])
+        # print(newEdgeList)
+
+        parents = [-1] * n
+
+        graph = defaultdict(dict)
+        for x, y, d in newEdgeList:
+            graph[x][y] = d
+            graph[y][x] = d
+        
+        main_parents = [-1] * n
+        depths = [0] * n
+        
+        def dfs(node, depth, parent, main_parent):
+            depths[node] = depth
+            parents[node] = parent
+            main_parents[node] = main_parent
+
+            for child in graph[node]:
+                if child == parent: continue
+                dfs(child, depth + 1, node, main_parent)
+        
+        done = {}
+        for i in range(n):
+            par = un.find(i)
+            if par in done: continue
+            dfs(par, 0, -1, par)
+            done[par] = 1
+        
+        k = floor(log2(n)) + 1
+        logs = [[[-1, 0] for _ in range(n)] for _ in range(k)]
+
+        for i in range(n):
+            if parents[i] == -1: continue
+            logs[0][i][0] = parents[i]
+            logs[0][i][1] = graph[parents[i]][i]
+        
+        for i in range(1, k):
+            for j in range(n):
+                if logs[i-1][j][0] == -1: continue
+                logs[i][j][0] = logs[i-1][ logs[i-1][j][0] ][0]
+                logs[i][j][1] = max(logs[i-1][j][1], logs[i-1][ logs[i-1][j][0] ][1])
+        
+        # print(visited)
+        # print([(parents[i], i) for i in range(n)])
+        # print(main_parents)
+        # [print(row) for row in logs]
+        ret = []
+
+        def goToDepth(x, depth):
+            if depth == 0: return x, 0
+            ans = 0
+            for i in range(k-1, -1, -1):
+                if 1 << i <= depth:
+                    ans = max(logs[i][x][1], ans)
+                    x = logs[i][x][0]
+                    depth -= 1 << i
+            return x, ans
+        
+        def meet(x, y, ans):
+            if x == y: return ans
+            for i in range(k-1, -1, -1):
+                if logs[i][x][0] == -1: continue
+                if logs[i][x][0] == logs[i][y][0]: continue
+                ans = max(ans, logs[i][x][1], logs[i][y][1])
+                x = logs[i][x][0]
+                y = logs[i][y][0]
+            
+            return max(ans, logs[0][x][1], logs[0][y][1])
+
+        for x, y, dis in queries:
+            par = main_parents[x]
+            if par == -1 or main_parents[y] != par:
+                ret.append(False)
+                continue
+            
+            x_depth = depths[x]
+            y_depth = depths[y]
+
+            if y_depth < x_depth:
+                x_depth, y_depth = y_depth, x_depth
+                x, y = y, x
+            
+            depth_need = y_depth - x_depth
+            new_y, ans = goToDepth(y, depth_need)
+            # print('==', new_y, ans, depth_need, (x, y), (x_depth, y_depth))
+            ans = meet(x, new_y, ans)
+            # print((x, y, dis), ans)
+            ret.append(ans < dis)
+
+        return ret
