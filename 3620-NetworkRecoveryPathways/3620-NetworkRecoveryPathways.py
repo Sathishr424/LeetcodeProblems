48791e36957c291fa26dc1822b1147fd3e17d496
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-24 13:44:38 +0530

    File updated at 24/7/2025, 1:44:33 pm

--- 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.py
+++ 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.py
@@ -1,24 +1,38 @@
-# Last updated: 20/7/2025, 11:28:29 am
+# Last updated: 24/7/2025, 1:44:33 pm
 class Solution:
     def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
-        graph = defaultdict(list)
+        graph = defaultdict(dict)
         n = len(online)
-
+        max_cost = 0
         for x, y, c in edges:
-            graph[x].append((y, c))
+            graph[x][y] = c
+            max_cost = max(max_cost, c)
+        
+        l = -1
+        r = max_cost
+        while l < r:
+            mid = (l + r + 1) // 2
 
-        stack = [(-inf, 0, 0)]
-        visited = {}
-        while stack:
-            cost, dis, x = heapq.heappop(stack)
-            if x == n-1: return -cost
+            stack = [(0, 0)]
+            visited = [inf] * n
+            can = False
+            while stack:
+                dis, x = heapq.heappop(stack)
+                if x == n-1:
+                    can = True
+                    break
 
-            if x in visited and visited[x][0] <= dis and visited[x][1] >= cost: continue
-            visited[x] = [dis, cost]
+                if visited[x] <= dis: continue
+                visited[x] = dis
+                
+                for y in graph[x]:
+                    if not online[y] or graph[x][y] < mid: continue
+                    if dis + graph[x][y] > k: continue
+                    heapq.heappush(stack, (dis + graph[x][y], y))
             
-            for y, c in graph[x]:
-                if not online[y]: continue
-                if dis + c > k: continue
-                heapq.heappush(stack, (-min(-cost, c), dis + c, y))
-
-        return -1
+            if can:
+                l = mid
+            else:
+                r = mid - 1
+            
+        return l
