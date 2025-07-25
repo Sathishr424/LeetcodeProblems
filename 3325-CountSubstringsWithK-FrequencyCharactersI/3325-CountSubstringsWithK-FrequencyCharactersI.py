be8b9d17e5f872ae8d03102d7da4f83b322679f6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-25 20:11:15 +0530

    File updated at 25/7/2025, 8:11:10 pm

--- 3325-CountSubstringsWithK-FrequencyCharactersI/3325-CountSubstringsWithK-FrequencyCharactersI.py
+++ 3325-CountSubstringsWithK-FrequencyCharactersI/3325-CountSubstringsWithK-FrequencyCharactersI.py
@@ -1,15 +1,29 @@
-# Last updated: 25/7/2025, 6:40:46 pm
+# Last updated: 25/7/2025, 8:11:10 pm
 class Solution:
     def numberOfSubstrings(self, s: str, k: int) -> int:
         n = len(s)
         ret = 0
-        for i in range(n):
-            freq = [0] * 26
-            for j in range(i, n):
-                a = ord(s[j]) - ord('a')
-                freq[a] += 1
-                if freq[a] == k:
-                    ret += n - j
-                    break
+
+        maxi = [[] for _ in range(26)]
+        prefix = [[0] * 26 for _ in range(n)]
         
+        for i in range(n):
+            prefix[i][ord(s[i]) - ord('a')] += 1
+            for j in range(26):
+                prefix[i][j] += prefix[i - 1][j]
+                maxi[j].append(prefix[i][j])
+
+        for i in range(n-k+1):
+            smallest = n
+            curr = ord(s[i]) - ord('a')
+            for a in range(26):
+                need = prefix[i][a] + k
+                if a == curr:
+                    need -= 1
+                index = bisect_left(maxi[a], need, lo=i + k - 1)
+                # print(i, (curr, a), chr(a + ord('a')), need, index)
+
+                smallest = min(index, smallest)
+            ret += n - smallest
+
         return ret
