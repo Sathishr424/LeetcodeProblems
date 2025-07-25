9d5fc35d6a55bfeb57b9b42d5f42d68cf16d5a32
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-26 05:17:08 +0530

    File updated at 26/7/2025, 5:17:02 am

--- 741-CherryPickup/741-CherryPickup.py
+++ 741-CherryPickup/741-CherryPickup.py
@@ -1,39 +1,39 @@
-# Last updated: 26/7/2025, 5:02:25 am
+# Last updated: 26/7/2025, 5:17:02 am
+cmax = lambda x, y: x if x > y else y
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid)
-    
         if grid[0][0] == -1: return 0
-
-        def visit(i, j, i2, j2):
-            ans = -inf
-            add = grid[i][j]
-            if i2 != i and j2 != j:
-                add += grid[i2][j2]
-            if i + 1 < m and grid[i+1][j] != -1:
-                can = False
-                if i2 + 1 < m and grid[i2+1][j2] != -1:
-                    ans = max(ans, dfs(i + 1, j, i2 + 1, j2) + add)
-                if j2 + 1 < m and grid[i2][j2+1] != -1:
-                    ans = max(ans, dfs(i + 1, j, i2, j2 + 1) + add)
-                if i2 == m-1 and j2 == m-1:
-                    ans = max(ans, dfs(i + 1, j, i2, j2) + add)
-            
-            if j + 1 < m and grid[i][j + 1] != -1:
-                if i2 + 1 < m and grid[i2+1][j2] != -1:
-                    ans = max(ans, dfs(i, j + 1, i2 + 1, j2) + add)
-                if j2 + 1 < m and grid[i2][j2+1] != -1:
-                    ans = max(ans, dfs(i, j + 1, i2, j2 + 1) + add)
-                if i2 == m-1 and j2 == m-1:
-                    ans = max(ans, dfs(i, j + 1, i2, j2) + add)
-            return ans
 
         @cache
         def dfs(i, j, i2, j2):
             if i == n-1 and j == n-1 and i2 == n-1 and j2 == n-1:
                 return grid[i][j]
-                
-            return max(visit(i, j, i2, j2), visit(i2, j2, i, j))
+            
+            ans = -inf
+            check = [
+                (i + 1, j, i2 + 1, j2),
+                (i + 1, j, i2, j2 + 1),
+                (i, j + 1, i2 + 1, j2),
+                (i, j + 1, i2, j2 + 1),
+            ]
+
+            if i == n-1 and j == n-1:
+                check.append((i, j, i2 + 1, j2))
+                check.append((i, j, i2, j2 + 1))
+            
+            if i2 == n-1 and j2 == n-1:
+                check.append((i + 1, j, i2, j2))
+                check.append((i, j + 1, i2, j2))
+            
+            add = grid[i][j]
+            if i != i2 and j != j2: add += grid[i2][j2]
+        
+            for ni, nj, ni2, nj2 in check:
+                if ni < m and nj < m and ni2 < m and nj2 < m and grid[ni][nj] != -1 and grid[ni2][nj2] != -1:
+                    ans = max(ans, dfs(ni, nj, ni2, nj2) + add)
+            
+            return ans
         
         return max(0, dfs(0, 0, 0, 0))
