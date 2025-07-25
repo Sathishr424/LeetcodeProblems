ea186424a99178dd70f34267a1baeeed9b7b61cd
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-26 02:22:42 +0530

    File updated at 26/7/2025, 2:22:36 am

--- 2435-PathsinMatrixWhoseSumIsDivisiblebyK/2435-PathsinMatrixWhoseSumIsDivisiblebyK.py
+++ 2435-PathsinMatrixWhoseSumIsDivisiblebyK/2435-PathsinMatrixWhoseSumIsDivisiblebyK.py
@@ -1,4 +1,4 @@
-# Last updated: 26/7/2025, 2:18:01 am
+# Last updated: 26/7/2025, 2:22:36 am
 mod = 10**9 + 7
 
 class Solution:
@@ -6,7 +6,7 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        dp = [[[0] * k for _ in range(n)] for _ in range(m)]
+        dp = [[[0] * k for _ in range(n + 1)] for _ in range(m + 1)]
         dp[0][0][0] = 1
 
         for i in range(m):
@@ -14,19 +14,10 @@ class Solution:
                 val = grid[i][j]
                 for d in range(k):
                     new_d = (d + val) % k
-                    if i + 1 < m:
-                        dp[i + 1][j][new_d] += dp[i][j][d]
-                        dp[i + 1][j][new_d] %= mod
-                    if j + 1 < n:
-                        dp[i][j + 1][new_d] += dp[i][j][d]
-                        dp[i][j + 1][new_d] %= mod
+                    dp[i + 1][j][new_d] += dp[i][j][d]
+                    dp[i + 1][j][new_d] %= mod
+
+                    dp[i][j + 1][new_d] += dp[i][j][d]
+                    dp[i][j + 1][new_d] %= mod
         
-        ret = 0
-        lastVal = grid[m-1][n-1]
-        lastBlock = dp[m-1][n-1]
-        for d in range(k):
-            if (d + lastVal) % k == 0:
-                ret += lastBlock[d]
-                ret %= mod
-            
-        return ret
+        return dp[m][n-1][0]
