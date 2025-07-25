312c3cce2cc9799aac4404fb8d97f0175e51ef52
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-25 21:11:36 +0530

    File updated at 25/7/2025, 9:11:30 pm

--- 224-BasicCalculator/224-BasicCalculator.py
+++ 224-BasicCalculator/224-BasicCalculator.py
@@ -1,32 +1,42 @@
-# Last updated: 12/6/2025, 5:51:28 am
+# Last updated: 25/7/2025, 9:11:30 pm
 class Solution:
-    def calculate(self, s: str) -> int:
-        n = len(s)
-        def rec(i):
-            op = 1
-            ans = 0
-            curr = 0
+    def calculate(self, calculate: str) -> int:
+        n = len(calculate)
+
+        def rec(index):
+            # print('new', index)
+            i = index
+            left = 0
+            op = ''
             while i < n:
-                if s[i] == '(':
-                    curr, i = rec(i+1)
-                    ans += curr * op
+                if calculate[i] == '(':
+                    tmp = i
+                    i, s = rec(i + 1)
+                    # print('found pranthesis', (tmp, i), calculate[tmp:i+1], left, op, s)
+                    if op == '-': left -= s
+                    elif op == '+': left += s
+                    else: left = s
+                    op = ''
+                elif calculate[i] == ')':
+                    # print("return", (i, left))
+                    return i, left
+                elif calculate[i] == '+':
+                    op = '+'
+                elif calculate[i] == '-':
+                    op = '-'
+                elif calculate[i] != ' ':
                     curr = 0
-                elif s[i] == ')':
-                    ans += curr * op
-                    return [ans, i]
-                elif s[i] == '-':
-                    ans += curr * op
-                    op = -1
-                    curr = 0
-                elif s[i] == '+':
-                    ans += curr * op
-                    op = 1
-                    curr = 0
-                elif s[i] != ' ':
-                    curr = curr * 10 + int(s[i])
+                    j = i
+                    while j < n and calculate[j] in '0123456789':
+                        curr = curr * 10 + int(calculate[j])
+                        j += 1
+                    if op == '+': left += curr
+                    elif op == '-': left -= curr
+                    else: left = curr
+                    op = ''
+                    i = j - 1
+                # print((i, calculate[index:i+1]), left, op)
                 i += 1
-            ans += curr * op
-            return [ans, i]
+            return left, i
+        
         return rec(0)[0]
-
-
