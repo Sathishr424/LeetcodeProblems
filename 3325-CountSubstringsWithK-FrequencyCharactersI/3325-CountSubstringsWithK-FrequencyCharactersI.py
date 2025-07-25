b07bf1bc0b1f3bf6bd7945ac716e12c3858d4d51
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-25 18:40:52 +0530

    File updated at 25/7/2025, 6:40:46 pm

--- 3325-CountSubstringsWithK-FrequencyCharactersI/3325-CountSubstringsWithK-FrequencyCharactersI.py
+++ 3325-CountSubstringsWithK-FrequencyCharactersI/3325-CountSubstringsWithK-FrequencyCharactersI.py
@@ -1,13 +1,14 @@
-# Last updated: 25/7/2025, 6:38:51 pm
+# Last updated: 25/7/2025, 6:40:46 pm
 class Solution:
     def numberOfSubstrings(self, s: str, k: int) -> int:
         n = len(s)
         ret = 0
         for i in range(n):
-            freq = defaultdict(int)
+            freq = [0] * 26
             for j in range(i, n):
-                freq[s[j]] += 1
-                if freq[s[j]] == k:
+                a = ord(s[j]) - ord('a')
+                freq[a] += 1
+                if freq[a] == k:
                     ret += n - j
                     break
         
