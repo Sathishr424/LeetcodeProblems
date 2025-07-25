3101be67f706a26526bfa8061325d0cee8edb7e8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-26 05:18:36 +0530

    File updated at 26/7/2025, 5:18:29 am

--- 741-CherryPickup/741-CherryPickup.py
+++ 741-CherryPickup/741-CherryPickup.py
@@ -1,9 +1,8 @@
-# Last updated: 26/7/2025, 5:17:17 am
+# Last updated: 26/7/2025, 5:18:29 am
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        m = len(grid)
         if grid[0][0] == -1: return 0
 
         @cache
@@ -18,22 +17,14 @@ class Solution:
                 (i, j + 1, i2 + 1, j2),
                 (i, j + 1, i2, j2 + 1),
             ]
-
-            # if i == n-1 and j == n-1:
-            #     check.append((i, j, i2 + 1, j2))
-            #     check.append((i, j, i2, j2 + 1))
-            
-            # if i2 == n-1 and j2 == n-1:
-            #     check.append((i + 1, j, i2, j2))
-            #     check.append((i, j + 1, i2, j2))
             
             add = grid[i][j]
             if i != i2 and j != j2: add += grid[i2][j2]
         
             for ni, nj, ni2, nj2 in check:
-                if ni < m and nj < m and ni2 < m and nj2 < m and grid[ni][nj] != -1 and grid[ni2][nj2] != -1:
-                    ans = max(ans, dfs(ni, nj, ni2, nj2) + add)
+                if ni < n and nj < n and ni2 < n and nj2 < n and grid[ni][nj] != -1 and grid[ni2][nj2] != -1:
+                    ans = cmax(ans, dfs(ni, nj, ni2, nj2) + add)
             
             return ans
         
-        return max(0, dfs(0, 0, 0, 0))
+        return cmax(0, dfs(0, 0, 0, 0))
