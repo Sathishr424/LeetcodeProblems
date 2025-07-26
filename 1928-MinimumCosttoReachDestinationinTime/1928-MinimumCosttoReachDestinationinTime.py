75e6d2a04f82e7a8dfa424c95f140e66d2433f69
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-26 06:37:46 +0530

    File updated at 26/7/2025, 6:37:39 am

--- 1928-MinimumCosttoReachDestinationinTime/1928-MinimumCosttoReachDestinationinTime.py
+++ 1928-MinimumCosttoReachDestinationinTime/1928-MinimumCosttoReachDestinationinTime.py
@@ -1,22 +1,22 @@
-# Last updated: 26/7/2025, 6:36:56 am
+# Last updated: 26/7/2025, 6:37:39 am
 class Solution:
     def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
         n = len(passingFees)
-        graph = defaultdict(list)
+        graph = [[] for _ in range(n)]
 
         for x, y, t in edges:
             graph[x].append((y, t))
             graph[y].append((x, t))
         
         stack = [(-maxTime, passingFees[0], 0)]
-        visited = {}
+        visited = [inf] * n
         ret = inf
         
         while stack:
             remTime, fee, node = heapq.heappop(stack)
             remTime *= -1
 
-            if node in visited and visited[node] < fee:  continue
+            if visited[node] < fee:  continue
             visited[node] = fee
 
             if node == n - 1: 
