6f73bb8383bc12156413086e363627994f8e8b1d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-26 06:36:07 +0530

    File updated at 26/7/2025, 6:36:00 am

--- 1928-MinimumCosttoReachDestinationinTime/1928-MinimumCosttoReachDestinationinTime.py
+++ 1928-MinimumCosttoReachDestinationinTime/1928-MinimumCosttoReachDestinationinTime.py
@@ -1,26 +1,35 @@
-# Last updated: 26/7/2025, 6:22:51 am
+# Last updated: 26/7/2025, 6:36:00 am
 class Solution:
     def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
         n = len(passingFees)
-        graph = [[] for _ in range(n)]
+        graph = defaultdict(list)
+
         for x, y, t in edges:
             graph[x].append((y, t))
             graph[y].append((x, t))
         
-        dp = [[-1] * (maxTime + 1) for _ in range(n)]
-
-        def dfs(node, remTime):
-            if dp[node][remTime] != -1: return dp[node][remTime]
-            if node == n - 1: return 0
-
-            ans = inf
-            for child, t in graph[node]:
-                if remTime - t < 0: continue
-                ans = min(ans, dfs(child, remTime - t) + passingFees[child])
-            
-            dp[node][remTime] = ans
-            return ans
+        stack = [(-maxTime, passingFees[0], 0)]
+        visited = {}
+        ret = inf
         
-        ans = dfs(0, maxTime) + passingFees[0]
-        if ans == inf: return -1
-        return ans
+        while stack:
+            remTime, fee, node = heapq.heappop(stack)
+            remTime *= -1
+            # print(remTime, fee, node)
+            if node == n - 1: 
+                ret = min(ret, fee)
+                continue
+
+            if node in visited and visited[node] < fee: 
+                # print('cant vis', remTime, fee, node)
+                continue
+            visited[node] = fee
+
+            for child, t in graph[node]:
+                if remTime - t < 0: 
+                    # print('cant', child, remTime, t)
+                    continue
+                heapq.heappush(stack, (-(remTime - t), fee + passingFees[child], child))
+        
+        if ret == inf: return -1
+        return ret
