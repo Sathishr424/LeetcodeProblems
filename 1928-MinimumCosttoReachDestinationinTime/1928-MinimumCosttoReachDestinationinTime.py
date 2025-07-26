fb9442e4a53b83d232b67bbb16ea0a94280b029f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-26 06:22:57 +0530

    File updated at 26/7/2025, 6:22:51 am

--- 1928-MinimumCosttoReachDestinationinTime/1928-MinimumCosttoReachDestinationinTime.py
+++ 1928-MinimumCosttoReachDestinationinTime/1928-MinimumCosttoReachDestinationinTime.py
@@ -1,22 +1,24 @@
-# Last updated: 26/7/2025, 6:20:53 am
+# Last updated: 26/7/2025, 6:22:51 am
 class Solution:
     def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
-        graph = defaultdict(list)
-        lastNode = 0
+        n = len(passingFees)
+        graph = [[] for _ in range(n)]
         for x, y, t in edges:
             graph[x].append((y, t))
             graph[y].append((x, t))
-            lastNode = max(lastNode, x, y)
         
-        @cache
+        dp = [[-1] * (maxTime + 1) for _ in range(n)]
+
         def dfs(node, remTime):
-            if node == lastNode: return 0
+            if dp[node][remTime] != -1: return dp[node][remTime]
+            if node == n - 1: return 0
 
             ans = inf
             for child, t in graph[node]:
                 if remTime - t < 0: continue
                 ans = min(ans, dfs(child, remTime - t) + passingFees[child])
             
+            dp[node][remTime] = ans
             return ans
         
         ans = dfs(0, maxTime) + passingFees[0]
