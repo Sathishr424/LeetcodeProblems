befb120cfb7974ba1b651798820de1a6e96d99db
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-28 20:07:00 +0530

    Create 3627-MaximumMedianSumofSubsequencesofSize3

---
+++ 3627-MaximumMedianSumofSubsequencesofSize3/3627-MaximumMedianSumofSubsequencesofSize3.py
@@ -0,0 +1,15 @@
+# Last updated: 28/7/2025, 8:06:51 pm
+class Solution:
+    def maximumMedianSum(self, nums: List[int]) -> int:
+        nums.sort()
+        # print(nums)
+
+        need = len(nums) // 3
+        ret = 0
+
+        while need:
+            nums.pop()
+            ret += nums.pop()
+            need -= 1
+        
+        return ret
