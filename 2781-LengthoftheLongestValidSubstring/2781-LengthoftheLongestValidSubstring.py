ad6f9bee89a3f342790cbe4e74b925cb9177de45
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-29 23:50:38 +0530

    File updated at 29/7/2025, 11:50:28 pm

--- 2781-LengthoftheLongestValidSubstring/2781-LengthoftheLongestValidSubstring.py
+++ 2781-LengthoftheLongestValidSubstring/2781-LengthoftheLongestValidSubstring.py
@@ -1,22 +1,15 @@
-# Last updated: 29/7/2025, 11:49:50 pm
+# Last updated: 29/7/2025, 11:50:28 pm
 cmin = lambda x, y: x if x < y else y
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def longestValidSubstring(self, word: str, forbidden: List[str]) -> int:
         n = len(word)
         remove = set(forbidden)
-
-        dp = [[0] * 10 for i in range(n)]
-
-        for i in range(n):
-            for j in range(i, min(n, i + 10)):
-                if word[i:j+1] in remove:
-                    dp[i][j - i] = 1
         
         def check(l, r):
             m = len(word)
             for i in range(r, max(l - 1, r - 10), -1):
-                if dp[i][r-i]: return i
+                if word[i:r+1] in remove: return i
             return -1
 
         ret = 0
