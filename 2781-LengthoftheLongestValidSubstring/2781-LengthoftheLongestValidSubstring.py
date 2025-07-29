7c6106c456d738edba184d18824220303b176008
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-29 23:38:09 +0530

    File updated at 29/7/2025, 11:37:59 pm

--- 2781-LengthoftheLongestValidSubstring/2781-LengthoftheLongestValidSubstring.py
+++ 2781-LengthoftheLongestValidSubstring/2781-LengthoftheLongestValidSubstring.py
@@ -1,4 +1,4 @@
-# Last updated: 29/7/2025, 11:35:43 pm
+# Last updated: 29/7/2025, 11:37:59 pm
 cmin = lambda x, y: x if x < y else y
 cmax = lambda x, y: x if x > y else y
 class Solution:
@@ -8,8 +8,8 @@ class Solution:
         
         def check(word):
             m = len(word)
-            for j in range(m-1, max(-1, m-10-1), -1):
-                if word[j:] in remove: return j
+            for i in range(m-1, -1, -1):
+                if word[i:] in remove: return i
             return -1
 
         ret = 0
@@ -17,14 +17,15 @@ class Solution:
         l = 0
         for i in range(n):
             curr += word[i]
+        
+            if len(curr) > 10:
+                curr = curr[1:]
+                l += 1
+            
             index = check(curr)
             if index != -1:
                 curr = curr[index+1:]
                 l = 0
-            if len(curr) > 10:
-                curr = curr[1:]
-                l += 1
             ret = cmax(ret, l + len(curr))
         
         return ret
-            
