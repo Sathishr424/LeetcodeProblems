3aad4966b47ad7a4ec09b9b27c2b563435429a6c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-30 07:51:16 +0530

    File updated at 30/7/2025, 7:51:16 am

--- 2419-LongestSubarrayWithMaximumBitwiseAND/2419-LongestSubarrayWithMaximumBitwiseAND.py
+++ 2419-LongestSubarrayWithMaximumBitwiseAND/2419-LongestSubarrayWithMaximumBitwiseAND.py
@@ -1,4 +1,4 @@
-# Last updated: 30/7/2025, 7:45:27 am
+# Last updated: 30/7/2025, 7:51:16 am
 cmax = lambda x, y: x if x > y else y
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
@@ -6,14 +6,36 @@ class Solution:
 
         max_num = max(nums)
 
-        cnt = 0
-        ret = 1
-        for i in range(n):
-            if nums[i] == max_num:
-                if i > 0 and nums[i] == nums[i-1]:
-                    cnt += 1
-                    ret = cmax(cnt, ret)
-                else:
-                    cnt = 1
+        k = floor(log2(n)) + 1
 
-        return ret
+        table = [[0] * n for _ in range(k)]
+
+        for i in range(n):
+            table[0][i] = nums[i]
+        
+        for power in range(1, k):
+            m = 1 << power
+            prev_m = m >> 1
+
+            for i in range(n-m+1):
+                table[power][i] = table[power - 1][i] & table[power-1][i+prev_m]
+        
+        def isGood(dis):
+            power = floor(log2(dis))
+            m = 1 << power
+            for i in range(n-dis+1):
+                if table[power][i] & table[power][(i + dis) - m] == max_num: return True
+            return False
+
+        l = 1
+        r = n
+
+        while l < r:
+            mid = (l + r + 1) // 2
+
+            if isGood(mid):
+                l = mid
+            else:
+                r = mid - 1
+
+        return l
