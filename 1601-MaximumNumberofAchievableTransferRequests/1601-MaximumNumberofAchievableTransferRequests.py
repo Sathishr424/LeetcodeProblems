5ded6d32b75347cc016962d6c0d053d35957d924
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-30 16:56:23 +0530

    File updated at 30/7/2025, 4:56:22 pm

--- 1601-MaximumNumberofAchievableTransferRequests/1601-MaximumNumberofAchievableTransferRequests.py
+++ 1601-MaximumNumberofAchievableTransferRequests/1601-MaximumNumberofAchievableTransferRequests.py
@@ -1,24 +1,25 @@
-# Last updated: 30/7/2025, 4:48:13 pm
+# Last updated: 30/7/2025, 4:56:22 pm
 class Solution:
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
         m = len(requests)
 
-        @cache
-        def rec(index, mask):
-            if index == m:
-                buildings = [0] * n
-                for i in range(m):
-                    if mask & (1 << i):
-                        buildings[requests[i][1]] += 1
-                        buildings[requests[i][0]] -= 1
-                
-                for b in buildings:
-                    if b != 0: return 0
-                
-                return bin(mask).count('1')
-            
-            ans = rec(index + 1, mask)
-            ans = max(rec(index + 1, mask | (1 << index)), ans)
-            return ans
+        best = 0
+        for mask in range((1 << m)-1, 0, -1):
+            cnt = bin(mask).count('1')
+            if cnt <= best: continue
 
-        return rec(0, 0)
+            buildings = [0] * n
+            for i in range(m):
+                if mask & (1 << i):
+                    buildings[requests[i][1]] += 1
+                    buildings[requests[i][0]] -= 1
+            
+            fine = True
+            for b in buildings:
+                if b != 0:
+                    fine = False
+                    break
+            
+            if fine: best = cnt
+
+        return best
