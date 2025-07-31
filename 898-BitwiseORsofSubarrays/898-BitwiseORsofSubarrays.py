7e87e7a1d6edd71d243f02df004d884d3db70f13
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-07-31 13:43:04 +0530

    File updated at 31/7/2025, 1:43:02 pm

--- 898-BitwiseORsofSubarrays/898-BitwiseORsofSubarrays.py
+++ 898-BitwiseORsofSubarrays/898-BitwiseORsofSubarrays.py
@@ -1,22 +1,15 @@
-# Last updated: 31/7/2025, 1:41:55 pm
+# Last updated: 31/7/2025, 1:43:02 pm
 class Solution:
     def subarrayBitwiseORs(self, nums: List[int]) -> int:
         n = len(nums)
 
         uniq = {}
-
         for i in range(n):
             val = 0
-            new_uniq = {}
             for j in range(i, n):
                 val |= nums[j]
-                if val in uniq and uniq[val] >= j: 
-                    # print((i, j), val, uniq)
+                if val in uniq and uniq[val] >= j:
                     break
-                new_uniq[val] = j
-            
-            for num in new_uniq:
-                if num not in uniq:
-                    uniq[num] = new_uniq[num]
+                uniq[val] = j
         
         return len(uniq)
