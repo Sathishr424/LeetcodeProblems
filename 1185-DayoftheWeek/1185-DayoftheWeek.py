c9124e466bda9ccc85a6a0140ab89f6f4fa817fb
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-03 19:25:19 +0530

    File updated at 3/8/2025, 7:25:16 pm

--- 1185-DayoftheWeek/1185-DayoftheWeek.py
+++ 1185-DayoftheWeek/1185-DayoftheWeek.py
@@ -1,23 +1,23 @@
-# Last updated: 3/8/2025, 6:08:16 pm
+# Last updated: 3/8/2025, 7:25:16 pm
 class Solution:
-    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
-        weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-        startDay = 5
-        months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-        days = startDay - 1
-        for y in range(1971, year):
-            if y % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
-                days += 366
-            else:
-                days += 365
+    def reverse(self, x: int) -> int:
+        ret = 0
+        is_neg = x < 0
+        x = abs(x)
+        maxi = 2 ** 31 - 1
+        rem = 0
+        while x:
+            can = maxi - ret * 10
+            rem = x % 10
+            x //= 10
+            if rem >= can:
+                if rem == can:
+                    if is_neg and x: return 0
+                else:
+                    return 0
+            ret = ret * 10 + rem
+            rem = 0
         
-        for m in range(1, month):
-            days += months[m - 1]
-        
-        days += day
-
-        if month > 2 and (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
-            days += 1
-        
-        return weekdays[days % 7]
+        if is_neg:
+            return -ret
+        return ret
