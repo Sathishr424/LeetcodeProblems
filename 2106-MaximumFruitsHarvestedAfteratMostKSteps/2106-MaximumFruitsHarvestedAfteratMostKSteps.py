0778bb53334ed667b72ef765351b1d4eed7d89d7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-03 18:21:24 +0530

    File updated at 3/8/2025, 6:21:21 pm

--- 2106-MaximumFruitsHarvestedAfteratMostKSteps/2106-MaximumFruitsHarvestedAfteratMostKSteps.py
+++ 2106-MaximumFruitsHarvestedAfteratMostKSteps/2106-MaximumFruitsHarvestedAfteratMostKSteps.py
@@ -1,33 +1,16 @@
-# Last updated: 3/8/2025, 12:51:13 pm
-cmax = lambda x, y: x if x > y else y
+# Last updated: 3/8/2025, 6:21:21 pm
 class Solution:
-    def maxTotalFruits(self, fruits: List[List[int]], startPos: int, k: int) -> int:
-        n = len(fruits)
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1: return [[1]]
+        elif numRows == 2: return [[1], [1, 1]]
+        ret = [[1],[1,1]]
 
-        prefix = [0]
-        position = []
-
-        for p, f in fruits:
-            prefix.append(prefix[-1] + f)
-            position.append(p)
-        
-        ret = 0
-        for pos in range(cmax(0, startPos - k), startPos + 1):
-            rem = k - (startPos - pos)
-            left = bisect_left(position, pos)
-            mid = bisect_left(position, startPos + 1)
-            right = cmax(mid, bisect_left(position, pos + rem + 1))
-
-            tot = prefix[right] - prefix[left]
-            ret = cmax(ret, tot)
-
-        for pos in range(startPos, startPos + k + 1):
-            rem = k - (pos - startPos)
-            left = bisect_left(position, pos - rem)
-            mid = bisect_left(position, startPos + 1)
-            right = cmax(mid, bisect_left(position, pos + 1))
-
-            tot = prefix[right] - prefix[left]
-            ret = cmax(ret, tot)
+        for i in range(2, numRows):
+            row = []
+            row.append(1)
+            for j in range(1, i):
+                row.append(ret[len(ret) - 1][j - 1] + ret[len(ret) - 1][j])
+            row.append(1)
+            ret.append(row)
         
         return ret
