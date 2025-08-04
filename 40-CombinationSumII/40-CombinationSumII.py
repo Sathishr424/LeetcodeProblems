0c405d83b576d08b72b167da48bc0b591b274562
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-05 00:17:54 +0530

    File updated at 5/8/2025, 12:17:50 am

--- 40-CombinationSumII/40-CombinationSumII.py
+++ 40-CombinationSumII/40-CombinationSumII.py
@@ -1,17 +1,14 @@
-# Last updated: 4/8/2025, 11:55:48 pm
+# Last updated: 5/8/2025, 12:17:50 am
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        ret = []
-        def rec(rem, opened, s):
-            if rem == 0:
-                if opened == 0:
-                    ret.append(s)
-                return
-            
-            if opened < rem - 1:
-                rec(rem - 1, opened + 1, s + '(')
-            if opened:
-                rec(rem - 1, opened - 1, s + ')')
+    def numTrees(self, n: int) -> int:
+        @cache
+        def rec(l, r):
+            if l >= r: return 1
+
+            ans = 0
+            for i in range(l, r):
+                ans += rec(l, i) * rec(i + 1, r)
+            return ans
         
-        rec(n * 2, 0, '')
-        return ret
+        return rec(1, n + 1)
+
