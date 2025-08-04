acbabebc52bfc62b1a74085d944db093a9425cf1
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-04 23:16:03 +0530

    File updated at 4/8/2025, 11:15:59 pm

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,31 +1,14 @@
-# Last updated: 4/8/2025, 11:09:44 pm
-mod = 10 ** 9 + 7
-N = 101
-primes = [0] * N
-primes[2] = 1
-primes[3] = 1
-
-def is_prime(num):
-    if num % 2 == 0: return 0
-    for i in range(3, int(num ** 0.5) + 1, 2):
-        if num % i == 0: return 0
-    return 1
-
-for num in range(4, N):
-    primes[num] = is_prime(num)
-
-fact = [1] * N
-for i in range(1, N):
-    fact[i] = fact[i - 1] * i % mod
-
+# Last updated: 4/8/2025, 11:15:59 pm
 class Solution:
-    def numPrimeArrangements(self, n: int) -> int:
-        prime = 0
-        non_prime = 0
-        for i in range(1, n + 1):
-            if primes[i]:
-                prime += 1
-            else:
-                non_prime += 1
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        freq = [0] * 101
 
-        return fact[prime] * fact[non_prime] % mod
+        for num in nums:
+            freq[num] += 1
+
+        ret = 0
+        for cnt in freq:
+            if cnt > 1:
+                ret += cnt * (cnt - 1) // 2
+        
+        return ret
