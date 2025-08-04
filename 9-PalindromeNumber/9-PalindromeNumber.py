5f6dd245b300e03bc893d9d864a1cf1a4cfa09b4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-04 22:47:26 +0530

    File updated at 4/8/2025, 10:47:22 pm

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,32 +1,34 @@
-# Last updated: 4/8/2025, 2:36:57 pm
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        bits = [0] * 14
+# Last updated: 4/8/2025, 10:47:22 pm
+def addition(a, b):
+    max_int = 1 << 32
+    ret = 0
+    carry = 0
+    power = 0
+    while (a or b) and power < 32:
+        a_rem = a % 2
+        b_rem = b % 2
 
-        for num in range(1, n + 1):
-            bit = 13
-            while num:
-                if num % 2:
-                    bits[bit] += 1
-                bit -= 1
-                num //= 2
-        
-        for num in nums:
-            bit = 13
-            while num:
-                if num % 2:
-                    bits[bit] -= 1
-                bit -= 1
-                num //= 2
-        
-        bit = 13
-        ret = 0
-        power = 0
-        while bit >= 0:
-            if bits[bit]:
-                ret += 1 << power
-            power += 1
-            bit -= 1
-        
-        return ret
+        if a_rem and b_rem:
+            if carry:  ret += 1 << power
+            else: carry = 1
+        elif a_rem or b_rem:
+            if not carry: ret += 1 << power
+        elif carry:
+            ret += 1 << power
+            carry = 0
+        power += 1
+        a //= 2
+        b //= 2
+    
+    if carry: ret += 1 << power
+    if a < 0: ret = ret - max_int
+    if b < 0: ret = ret - max_int
+    return ret
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        is_neg = a < 0 or b < 0
+        # if a < 0: a = addition(~a, 1)
+        # if b < 0: b = addition(~b, 1)
+        ans = addition(a, b)
+        return ans
