67927b5da283b336094a4dc5bf2d37418e620af5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-05 00:22:02 +0530

    File updated at 5/8/2025, 12:21:58 am

--- 40-CombinationSumII/40-CombinationSumII.py
+++ 40-CombinationSumII/40-CombinationSumII.py
@@ -1,14 +1,4 @@
-# Last updated: 5/8/2025, 12:17:50 am
+# Last updated: 5/8/2025, 12:21:58 am
 class Solution:
     def numTrees(self, n: int) -> int:
-        @cache
-        def rec(l, r):
-            if l >= r: return 1
-
-            ans = 0
-            for i in range(l, r):
-                ans += rec(l, i) * rec(i + 1, r)
-            return ans
-        
-        return rec(1, n + 1)
-
+        return factorial(2 * n) // (factorial(n + 1) * factorial(n))
