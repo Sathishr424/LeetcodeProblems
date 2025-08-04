d0e92a2b520d8d65b7fe7b8fa29e6e7e1bbce655
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-04 23:09:14 +0530

    File updated at 4/8/2025, 11:09:10 pm

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,30 +1,31 @@
-# Last updated: 4/8/2025, 10:48:35 pm
-def addition(a, b):
-    max_int = 1 << 32
-    ret = 0
-    carry = 0
-    power = 0
-    while power < 32:
-        a_rem = a % 2
-        b_rem = b % 2
+# Last updated: 4/8/2025, 11:09:10 pm
+mod = 10 ** 9 + 7
+N = 101
+primes = [0] * N
+primes[2] = 1
+primes[3] = 1
 
-        if a_rem and b_rem:
-            if carry:  ret += 1 << power
-            else: carry = 1
-        elif a_rem or b_rem:
-            if not carry: ret += 1 << power
-        elif carry:
-            ret += 1 << power
-            carry = 0
-        power += 1
-        a //= 2
-        b //= 2
-    
-    if carry: ret += 1 << power
-    if a < 0: ret = ret - max_int
-    if b < 0: ret = ret - max_int
-    return ret
+def is_prime(num):
+    if num % 2 == 0: return 0
+    for i in range(3, int(num ** 0.5) + 1, 2):
+        if num != i and num % i == 0: return 0
+    return 1
+
+for num in range(4, N):
+    primes[num] = is_prime(num)
+
+fact = [1] * N
+for i in range(1, N):
+    fact[i] = fact[i - 1] * i % mod
 
 class Solution:
-    def getSum(self, a: int, b: int) -> int:
-        return addition(a, b)
+    def numPrimeArrangements(self, n: int) -> int:
+        prime = 0
+        non_prime = 0
+        for i in range(1, n + 1):
+            if primes[i]:
+                prime += 1
+            else:
+                non_prime += 1
+        # print(prime, non_prime)
+        return fact[prime] * fact[non_prime] % mod
