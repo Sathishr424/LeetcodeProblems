492861062fe96927c9887b1f10b840c1aed20dd0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-04 23:45:04 +0530

    File updated at 4/8/2025, 11:45:00 pm

--- 40-CombinationSumII/40-CombinationSumII.py
+++ 40-CombinationSumII/40-CombinationSumII.py
@@ -1,15 +1,18 @@
-# Last updated: 4/8/2025, 11:37:40 pm
+# Last updated: 4/8/2025, 11:45:00 pm
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         ret = []
-        def rec(index, arr):
+        def rec(index, arr, rem):
             if len(arr) == k:
-                ret.append(arr[:])
+                if rem == 0:
+                    ret.append(arr[:])
                 return
             
-            for i in range(index, n + 1):
+            for i in range(index, 10):
+                if i > rem: break
                 arr.append(i)
-                rec(i + 1, arr)
+                rec(i + 1, arr, rem - i)
                 arr.pop()
-        rec(1, [])
+        
+        rec(1, [], n)
         return ret
