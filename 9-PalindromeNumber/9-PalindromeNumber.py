19a3f53e5bf7195d1a9cccc243c06c3b47b744c1
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-04 14:26:38 +0530

    File updated at 4/8/2025, 2:26:35 pm

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,11 +1,20 @@
-# Last updated: 4/8/2025, 2:20:40 pm
+# Last updated: 4/8/2025, 2:26:35 pm
+N = 5 * (10 ** 6) + 2
+is_prime = [True] * N
+is_prime[0] = False
+is_prime[1] = False
+
+for i in range(2, int(N ** 0.5) + 1):
+    if not is_prime[i]: continue
+    for j in range(i*i, N, i):
+        is_prime[j] = False
+
+prime_cnts = [0] * N
+for i in range(1, N):
+    prime_cnts[i] += prime_cnts[i - 1]
+    if is_prime[i - 1]:
+        prime_cnts[i] += 1
+
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0: return False
-        rev = 0
-        num = x
-        while num:
-            rev = rev * 10 + (num % 10)
-            num //= 10
-        
-        return rev == x
+    def countPrimes(self, n: int) -> int:
+        return prime_cnts[n]
