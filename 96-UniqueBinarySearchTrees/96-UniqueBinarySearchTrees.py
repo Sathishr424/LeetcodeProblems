ee069926850491cd1a5d61d124495ec9f7ba49ac
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-05 11:59:09 +0530

    File updated at 5/8/2025, 11:59:09 am

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
@@ -1,20 +1,9 @@
-# Last updated: 5/8/2025, 11:24:29 am
+# Last updated: 5/8/2025, 11:59:09 am
+def gcd(x, y):
+    rem = x % y
+    if rem == 0: return y
+    return gcd(y, rem)
+
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        m = len(str1)
-        n = len(str2)
-
-        if m < n:
-            m, n = n, m
-            str1, str2 = str2, str1
-        
-        d = gcd(m, n)
-        ret = str2[:d]
-
-        for i in range(0, m, d):
-            if str1[i:i+d] != ret: return ''
-        
-        for i in range(0, n, d):
-            if str2[i:i+d] != ret: return ''
-        
-        return ret
+    def findGCD(self, nums: List[int]) -> int:
+        return gcd(max(nums), min(nums))
