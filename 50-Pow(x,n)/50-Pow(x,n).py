3e2921d12e8fa3785b766ce4710210210bba8390
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-05 17:44:49 +0530

    File updated at 5/8/2025, 5:44:49 pm

--- 50-Pow(x,n)/50-Pow(x,n).py
+++ 50-Pow(x,n)/50-Pow(x,n).py
@@ -1,9 +1,8 @@
-# Last updated: 5/8/2025, 5:44:33 pm
+# Last updated: 5/8/2025, 5:44:49 pm
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         def cPow(num, power):
             if power == 0: return 1
-            if power == 1: return num
             half = power // 2
             ans = cPow(num, half)
             ans = ans * ans
