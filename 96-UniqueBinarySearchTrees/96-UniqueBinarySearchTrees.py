474d201a6aefe48fa7d220db529ad640a212ec9e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-05 10:43:43 +0530

    File updated at 5/8/2025, 10:43:43 am

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
@@ -1,16 +1,13 @@
-# Last updated: 5/8/2025, 10:41:57 am
+# Last updated: 5/8/2025, 10:43:43 am
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
-        def rec(i, j):
-            if i == m-1 and j == n-1:
-                return 1
-            
-            ans = 0
-            if i + 1 < m:
-                ans += rec(i + 1, j)
-            if j + 1 < n:
-                ans += rec(i, j + 1)
-            return ans
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if i + 1 < m:
+                    dp[i + 1][j] += dp[i][j]
+                if j + 1 < n:
+                    dp[i][j + 1] += dp[i][j]
         
-        return rec(0, 0)
+        return dp[m-1][n-1]
