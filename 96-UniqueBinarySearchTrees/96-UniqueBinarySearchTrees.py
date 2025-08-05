84c736ae9b3e6a9ac3f8d33b2d14e44259f799ca
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-05 14:04:38 +0530

    File updated at 5/8/2025, 2:04:37 pm

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
@@ -1,5 +1,5 @@
-# Last updated: 5/8/2025, 1:16:22 pm
-N = 1000 + 1
+# Last updated: 5/8/2025, 2:04:37 pm
+N = 10 ** 5 + 1
 is_prime = [1] * N
 is_prime[0] = 0
 is_prime[1] = 0
@@ -7,25 +7,24 @@ for i in range(2, int(N ** 0.5) + 1):
     if not is_prime[i]: continue
     for j in range(i * i, N, i):
         is_prime[j] = False
-    
+
 primes = []
 for i in range(N):
     if is_prime[i]:
         primes.append(i)
 
 class Solution:
-    def distinctPrimeFactors(self, nums: List[int]) -> int:
-        uniq = {}
-        for num in nums:
-            if is_prime[num]:
-                uniq[num] = 1
-                continue
+    def smallestValue(self, n: int) -> int:
+        while n:
+            prev_n = n
+            new_n = 0
             for p in primes:
-                if p > num: break
-                if num % p == 0:
-                    uniq[p] = 1
-                    while num % p == 0:
-                        num //= p
-                    if num == 0: break
+                if p > n: break
+                if n % p != 0: continue
+                while n % p == 0:
+                    new_n += p
+                    n //= p
+            n = new_n
+            if n == prev_n: break
+        return n
         
-        return len(uniq)
