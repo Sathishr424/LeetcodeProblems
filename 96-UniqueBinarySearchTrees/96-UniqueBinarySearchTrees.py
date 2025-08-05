54c0784765afc88023b4560eef0794c136501256
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-05 11:01:59 +0530

    File updated at 5/8/2025, 11:01:59 am

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.py
@@ -1,13 +1,8 @@
-# Last updated: 5/8/2025, 10:43:43 am
+# Last updated: 5/8/2025, 11:01:59 am
+fact = [1] * 201
+for i in range(1, 201):
+    fact[i] = fact[i - 1] * i
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0] * n for _ in range(m)]
-        dp[0][0] = 1
-        for i in range(m):
-            for j in range(n):
-                if i + 1 < m:
-                    dp[i + 1][j] += dp[i][j]
-                if j + 1 < n:
-                    dp[i][j + 1] += dp[i][j]
-        
-        return dp[m-1][n-1]
+        return fact[m + n - 2] // (fact[m - 1] * fact[n - 1])
