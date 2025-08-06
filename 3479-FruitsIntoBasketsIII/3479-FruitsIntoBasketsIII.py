8aa1975c37927846b7f1311b64f2f0e5c102f2fa
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-06 11:19:02 +0530

    File updated at 6/8/2025, 11:19:01 am

--- 3479-FruitsIntoBasketsIII/3479-FruitsIntoBasketsIII.py
+++ 3479-FruitsIntoBasketsIII/3479-FruitsIntoBasketsIII.py
@@ -1,42 +1,49 @@
-# Last updated: 6/6/2025, 8:06:17 pm
+# Last updated: 6/8/2025, 11:19:01 am
+class SegmentTree:
+    def __init__(self, baskets):
+        self.baskets = baskets
+        self.n = len(baskets)
+        self.tree = [0] * (self.n * 4)
+
+        self.build(0, self.n-1, 0)
+    
+    def build(self, l, r, index):
+        if l == r:
+            self.tree[index] = self.baskets[l]
+            return self.tree[index]
+        
+        mid = (l + r) // 2
+        left = self.build(l, mid, index * 2 + 1)
+        right = self.build(mid + 1, r, index * 2 + 2)
+
+        self.tree[index] = max(left, right)
+        return self.tree[index]
+    
+    def query(self, l, r, index, capacity):
+        if self.tree[index] < capacity:
+            return False
+        
+        if l == r:
+            self.tree[index] = 0
+            return True
+        
+        mid = (l + r) // 2
+
+        canPlace = self.query(l, mid, index * 2 + 1, capacity)
+        if not canPlace:
+            canPlace = self.query(mid + 1, r, index * 2 + 2, capacity)
+
+        self.tree[index] = max(self.tree[index * 2 + 1], self.tree[index * 2 + 2])
+        return canPlace
+
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
-        n = len(fruits)
+        n = len(baskets)
 
-        tree = [0 for _ in range(n*4)]
+        segTree = SegmentTree(baskets)
 
-        def query(l, r, index, need):
-            if tree[index] < need: return False
-            
-            if l == r:
-                tree[index] = 0
-                return True
-
-            mid = (l + r) // 2
-
-            left = query(l, mid, index*2+1, need)
-            right = False
-            if not left:
-                right = query(mid+1, r, index*2+2, need)
-            
-            tree[index] = max(tree[index*2+1], tree[index*2+2])
-            return left or right
-
-        def build(l, r, index):
-            if l == r:
-                tree[index] = baskets[l]
-                return tree[index]
-            mid = (l+r) // 2
-            left = build(l, mid, index*2+1)
-            right = build(mid+1, r, index*2+2)
-
-            tree[index] = max(left, right)
-            return tree[index]
+        placed = 0
+        for f in fruits:
+            if segTree.query(0, n-1, 0, f): placed += 1
         
-        build(0, n-1, 0)
-
-        ret = 0
-        for i in range(n):
-            ret += not query(0, n-1, 0, fruits[i])
-
-        return ret
+        return n - placed
