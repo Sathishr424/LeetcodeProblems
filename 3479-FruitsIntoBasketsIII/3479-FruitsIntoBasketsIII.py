73e21c8e0eef67b53e11c594393c7a42ff49b4c4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-06 11:39:30 +0530

    File updated at 6/8/2025, 11:39:30 am

--- 3479-FruitsIntoBasketsIII/3479-FruitsIntoBasketsIII.py
+++ 3479-FruitsIntoBasketsIII/3479-FruitsIntoBasketsIII.py
@@ -1,49 +1,30 @@
-# Last updated: 6/8/2025, 11:25:52 am
-class SegmentTree:
-    def __init__(self, baskets):
-        self.baskets = baskets
-        self.n = len(baskets)
-        self.tree = [0] * (self.n * 4)
-
-        self.build(0, self.n-1, 0)
-    
-    def build(self, l, r, index):
-        if l == r:
-            self.tree[index] = self.baskets[l]
-            return self.tree[index]
-        
-        mid = (l + r) // 2
-        left = self.build(l, mid, index * 2 + 1)
-        right = self.build(mid + 1, r, index * 2 + 2)
-
-        self.tree[index] = max(left, right)
-        return self.tree[index]
-    
-    def query(self, l, r, index, capacity):
-        if self.tree[index] < capacity:
-            return False
-        
-        if l == r:
-            self.tree[index] = 0
-            return True
-        
-        mid = (l + r) // 2
-
-        canPlace = self.query(l, mid, index * 2 + 1, capacity)
-        if not canPlace:
-            canPlace = self.query(mid + 1, r, index * 2 + 2, capacity)
-
-        self.tree[index] = max(self.tree[index * 2 + 1], self.tree[index * 2 + 2])
-        return canPlace
-
+# Last updated: 6/8/2025, 11:39:30 am
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         n = len(baskets)
+        m = int(math.sqrt(n))
+        section = (n + m - 1) // m
+        count = 0
+        maxV = [0] * section
 
-        segTree = SegmentTree(baskets)
+        for i in range(n):
+            maxV[i // m] = max(maxV[i // m], baskets[i])
 
-        un_placed = 0
-        for f in fruits:
-            if not segTree.query(0, n-1, 0, f): un_placed += 1
-        
-        return un_placed
+        for fruit in fruits:
+            unset = 1
+            for sec in range(section):
+                if maxV[sec] < fruit:
+                    continue
+                choose = 0
+                maxV[sec] = 0
+                for i in range(m):
+                    pos = sec * m + i
+                    if pos < n and baskets[pos] >= fruit and not choose:
+                        baskets[pos] = 0
+                        choose = 1
+                    if pos < n:
+                        maxV[sec] = max(maxV[sec], baskets[pos])
+                unset = 0
+                break
+            count += unset
+        return count
