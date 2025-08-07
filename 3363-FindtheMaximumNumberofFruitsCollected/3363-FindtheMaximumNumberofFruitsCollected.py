24986d4015d39cd8fb2e916fb441894d4afe68a0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-07 17:00:39 +0530

    File updated at 7/8/2025, 5:00:37 pm

--- 3363-FindtheMaximumNumberofFruitsCollected/3363-FindtheMaximumNumberofFruitsCollected.py
+++ 3363-FindtheMaximumNumberofFruitsCollected/3363-FindtheMaximumNumberofFruitsCollected.py
@@ -1,4 +1,4 @@
-# Last updated: 7/8/2025, 4:30:04 pm
+# Last updated: 7/8/2025, 5:00:37 pm
 class Solution:
     def maxCollectedFruits(self, fruits: List[List[int]]) -> int:
         n = len(fruits)
@@ -8,10 +8,12 @@ class Solution:
         j = 0
         while i < n:
             ret += fruits[i][j]
-            fruits[i][j] = 0
+            fruits[i][j] = -inf
             i += 1
             j += 1
         
+        fruits[n-1][n-1] = 0
+
         DIR = [[(1, -1), (1, 0), (1, 1)], [(-1, 1), (0, 1), (1, 1)]]
         m = n-1
         def getDis(i, j):
@@ -20,8 +22,7 @@ class Solution:
         @cache
         def dfs(i, j, rem):
             if i == n-1 and j == n-1:
-                return [(i, j)], fruits[i][j]
-            arr = []
+                return fruits[i][j]
             ans = -inf
             rem -= 1
             for i2, j2 in DIR[0]:
@@ -29,12 +30,10 @@ class Solution:
                 j2 += j
 
                 if 0 <= i2 < n and 0 <= j2 < n and getDis(i2, j2) <= rem:
-                    new_arr, a = dfs(i2, j2, rem)
+                    a = dfs(i2, j2, rem)
                     if a > ans:
                         ans = a
-                        arr = new_arr
-            arr.append((i, j))
-            return arr, ans + fruits[i][j]
+            return ans + fruits[i][j]
         
         @cache
         def dfs2(i, j, rem):
@@ -53,12 +52,9 @@ class Solution:
             
             return ans + fruits[i][j]
         
-        arr, ans = dfs(0, n-1, n-1)
+        ans = dfs(0, n-1, n-1)
         dfs.cache_clear()
         ret += ans
-        # return ans
-        for (i, j) in arr:
-            fruits[i][j] = 0
 
         ans = dfs2(n-1, 0, n-1)
         dfs2.cache_clear()
