b54a8b8532f7fc401415165e7e3cd81f963ed9d7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-08 02:33:11 +0530

    File updated at 8/8/2025, 2:33:11 am

--- 378-KthSmallestElementinaSortedMatrix/378-KthSmallestElementinaSortedMatrix.py
+++ 378-KthSmallestElementinaSortedMatrix/378-KthSmallestElementinaSortedMatrix.py
@@ -1,14 +1,16 @@
-# Last updated: 8/8/2025, 2:07:33 am
+# Last updated: 8/8/2025, 2:33:11 am
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         n = len(matrix)
 
         heap = []
 
-        for i in range(n):
-            for j in range(n):
-                heapq.heappush(heap, -matrix[i][j])
-                if len(heap) > k:
-                    heapq.heappop(heap)
+        for i in range(min(n, k)):
+            heapq.heappush(heap, (matrix[i][0], i, 0))
+
+        for _ in range(k-1):
+            num, i, j = heapq.heappop(heap)
+            if j + 1 < n:
+                heapq.heappush(heap, (matrix[i][j + 1], i, j + 1))
         
-        return -heap[0]
+        return heap[0][0]
