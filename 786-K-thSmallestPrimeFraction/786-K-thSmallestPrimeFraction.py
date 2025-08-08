7286a48f34d7e26e304cfd28fbff373010e6dc3d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-09 04:46:24 +0530

    File updated at 9/8/2025, 4:46:23 am

--- 786-K-thSmallestPrimeFraction/786-K-thSmallestPrimeFraction.py
+++ 786-K-thSmallestPrimeFraction/786-K-thSmallestPrimeFraction.py
@@ -1,19 +1,42 @@
-# Last updated: 9/8/2025, 4:45:05 am
+# Last updated: 9/8/2025, 4:46:23 am
+def maxHeap(nums, n, k):
+    heap = []
+    for i in range(n-1):
+        heapq.heappush(heap, (-nums[i] / nums[i+1], i, i+1))
+
+    for _ in range(k-1):
+        _, i, j = heapq.heappop(heap)
+
+        if j+1 < n:
+            j += 1
+            heapq.heappush(heap, (-nums[i] / nums[j], i, j))
+    
+    _, i, j = heapq.heappop(heap)
+    return [nums[i], nums[j]]
+
+def minHeap(nums, n, k):
+    heap = []
+    for i in range(n-1):
+        heapq.heappush(heap, (nums[i] / nums[n-1], i, n-1))
+
+    while k-1:
+        _, i, j = heapq.heappop(heap)
+
+        if j-1 > i:
+            j -= 1
+            heapq.heappush(heap, (nums[i] / nums[j], i, j))
+        k -= 1
+    
+    _, i, j = heapq.heappop(heap)
+    return [nums[i], nums[j]]
+
 class Solution:
     def kthSmallestPrimeFraction(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
-        k = (n * (n-1) // 2) - k + 1
-    
-        heap = []
-        for i in range(n-1):
-            heapq.heappush(heap, (-nums[i] / nums[i+1], i, i+1))
 
-        for _ in range(k-1):
-            _, i, j = heapq.heappop(heap)
-
-            if j+1 < n:
-                j += 1
-                heapq.heappush(heap, (-nums[i] / nums[j], i, j))
+        n_k = (n * (n-1) // 2) - k + 1
+        if n_k < k:
+            return maxHeap(nums, n, n_k)
         
-        _, i, j = heapq.heappop(heap)
-        return [nums[i], nums[j]]
+        return minHeap(nums, n, k)
+    
