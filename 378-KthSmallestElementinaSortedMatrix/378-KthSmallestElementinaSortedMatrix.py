6b5e8de2d950e32eae2896e24921fbe615607c0e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-09 04:10:47 +0530

    File updated at 9/8/2025, 4:10:46 am

--- 378-KthSmallestElementinaSortedMatrix/378-KthSmallestElementinaSortedMatrix.py
+++ 378-KthSmallestElementinaSortedMatrix/378-KthSmallestElementinaSortedMatrix.py
@@ -1,36 +1,29 @@
-# Last updated: 9/8/2025, 2:40:15 am
-def minHeap(matrix, k, n):
-    heap = []
-
-    for i in range(min(n, k)):
-        heapq.heappush(heap, (matrix[i][0], i, 0))
-
-    for _ in range(k-1):
-        num, i, j = heapq.heappop(heap)
-        if j + 1 < n:
-            heapq.heappush(heap, (matrix[i][j + 1], i, j + 1))
-    
-    return heap[0][0]
-
-def maxHeap(matrix, k, n):
-    heap = []
-
-    for i in range(n-1, max(-1, n-k-1), -1):
-        heapq.heappush(heap, (-matrix[i][n-1], i, n-1))
-    
-    for _ in range(k - 1):
-        _, i, j = heapq.heappop(heap)
-        if j - 1 >= 0:
-            heapq.heappush(heap, (-matrix[i][j-1], i, j-1))
-    
-    return -heapq.heappop(heap)[0]
+# Last updated: 9/8/2025, 4:10:46 am
+# [[1, 5, 9],
+#  [10,11,14],
+#  [12,13,15]]
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         n = len(matrix)
 
-        m_k = (n * n) - k + 1
-        if m_k < k:
-            k = m_k
-            return maxHeap(matrix, k, n)
-        return minHeap(matrix, k, n)
+        l = matrix[0][0]
+        r = matrix[n-1][n-1]
+
+        def smallest(num):
+            count = 0
+            for i in range(n):
+                if matrix[i][n-1] <= num:
+                    count += n
+                else:
+                    count += bisect_right(matrix[i], num)
+            return count
+
+        while l < r:
+            mid = (l + r) // 2
+            if smallest(mid) < k:
+                l = mid + 1
+            else:
+                r = mid
+        
+        return l
