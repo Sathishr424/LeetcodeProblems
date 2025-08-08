23a4044a7096a510b66aaf8b0ea667198ddd1cde
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-08 18:38:20 +0530

    File updated at 8/8/2025, 6:38:19 pm

--- 808-SoupServings/808-SoupServings.py
+++ 808-SoupServings/808-SoupServings.py
@@ -1,21 +1,21 @@
-# Last updated: 8/8/2025, 6:36:47 pm
+# Last updated: 8/8/2025, 6:38:19 pm
 class Solution:
     def soupServings(self, n: int) -> float:
         n = ceil(n / 25)
         if n > 200: return 1
         @cache
         def rec(a, b):
-            if a == 0 and b == 0:
+            if a <= 0 and b <= 0:
                 return 0.5
-            if a == 0:
+            if a <= 0:
                 return 1
-            if b == 0:
+            if b <= 0:
                 return 0
 
-            i = rec(max(0, a-4), b)
-            j = rec(max(0, a-3), max(0, b-1))
-            k = rec(max(0, a-2), max(0, b-2))
-            l = rec(max(0, a-1), max(0, b-3))
+            i = rec(a-4, b)
+            j = rec(a-3, b-1)
+            k = rec(a-2, b-2)
+            l = rec(a-1, b-3)
             ans = 0.25 * (i + j + k + l)
             return ans
         
