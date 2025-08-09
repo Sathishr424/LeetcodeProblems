9f7673e04a5566e0aa6bba47d323d675699d66b5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-09 18:48:12 +0530

    Create 3116-KthSmallestAmountWithSingleDenominationCombination

---
+++ 3116-KthSmallestAmountWithSingleDenominationCombination/3116-KthSmallestAmountWithSingleDenominationCombination.py
@@ -0,0 +1,36 @@
+# Last updated: 9/8/2025, 6:48:11 pm
+class Solution:
+    def findKthSmallest(self, coins: List[int], k: int) -> int:
+        n = len(coins)
+
+        def isGood(mid):
+            total = 0
+            
+            def rec(index, cnt, l):
+                nonlocal total
+                if index == n:
+                    if cnt == 0: return
+                    if cnt % 2:
+                        total += mid // l
+                    else:
+                        total -= mid // l
+                    return
+                
+                rec(index + 1, cnt + 1, lcm(l, coins[index]))
+                rec(index + 1, cnt, l)
+
+            rec(0, 0, 1)
+            return total >= k    
+            
+        l = 1
+        r = coins[-1] * k
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if isGood(mid):
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
