ed483909790deec418991bc59655f9e39e92c416
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-10 04:23:16 +0530

    Create 1318-MinimumFlipstoMakeaORbEqualtoc

---
+++ 1318-MinimumFlipstoMakeaORbEqualtoc/1318-MinimumFlipstoMakeaORbEqualtoc.py
@@ -0,0 +1,20 @@
+# Last updated: 10/8/2025, 4:23:15 am
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        ret = 0
+        while c or a or b:
+            rem = c % 2
+            if rem == 0:
+                if a % 2:
+                    ret += 1
+                if b % 2:
+                    ret += 1
+            else:
+                if a % 2 == 0 and b % 2 == 0:
+                    ret += 1
+            
+            c //= 2
+            a //= 2
+            b //= 2
+        
+        return ret
