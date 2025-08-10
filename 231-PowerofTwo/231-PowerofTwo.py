182d3564e1017b56d08379ddf7b339bc6dc26689
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-10 05:57:12 +0530

    File updated at 10/8/2025, 5:57:11 am

--- 231-PowerofTwo/231-PowerofTwo.py
+++ 231-PowerofTwo/231-PowerofTwo.py
@@ -1,5 +1,28 @@
-# Last updated: 9/8/2025, 7:45:24 am
-maxi = (1 << 31) - 1
+# Last updated: 10/8/2025, 5:57:11 am
 class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and n & (n-1) == 0
+    def reorderedPowerOf2(self, n: int) -> bool:
+        length = 0
+
+        tmp = n
+        digits = []
+        while tmp:
+            length += 1
+            digits.append(tmp % 10)
+            tmp //= 10
+        
+        full_mask = (1 << length) - 1
+
+        @cache
+        def rec(mask, num):
+            if mask == full_mask:
+                if num > 0 and num & (num - 1) == 0: return True
+                return False
+            
+            for i in range(length):
+                if digits[i] == 0 and num == 0: continue
+                if mask & (1 << i) == 0 and rec(mask | (1 << i), num * 10 + digits[i]):
+                    return True
+            
+            return False
+        
+        return rec(0, 0)
