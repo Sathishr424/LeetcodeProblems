f63a8933e514fa9569f35700805c682d17a22f46
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-10 11:38:54 +0530

    Create 2801-CountSteppingNumbersinRange

---
+++ 2801-CountSteppingNumbersinRange/2801-CountSteppingNumbersinRange.py
@@ -0,0 +1,126 @@
+# Last updated: 10/8/2025, 11:38:53 am
+def substract(x, y):
+    carry = 0
+    ret = ''
+    for i in range(len(x)-1, -1, -1):
+        a = int(x[i])
+        b = int(y[i])
+        if carry:
+            carry = (a - 1) < b
+            a = (a - 1) % 10
+        if b > a:
+            carry = 1
+            a = 10 + a
+        
+        ret = str(a - b) + ret
+    # print(x, y, ret)
+    return ret
+
+class Solution:
+    def countSteppingNumbers(self, low: str, high: str) -> int:
+        
+        low = substract(low, '0' * (len(low) - 1) + '1')
+        
+        l_n = len(low)
+        r_n = len(high)
+        mod = 10**9 + 7
+
+        @cache
+        def rec(rem, prev):
+            if rem == 0:
+                return 1
+            
+            ans = 1
+            if prev + 1 < 10:
+                ans += rec(rem - 1, prev + 1)
+            if prev - 1 >= 0:
+                ans += rec(rem - 1, prev - 1)
+            
+            return ans % mod
+        
+        @cache
+        def rec3(rem, prev):
+            if rem == 0:
+                return 1
+        
+            ans = 0
+            if prev + 1 < 10:
+                ans += rec3(rem - 1, prev + 1)
+            if prev - 1 >= 0:
+                ans += rec3(rem - 1, prev - 1)
+            
+            return ans % mod
+        
+        @cache
+        def rec2(rem, prev, strict):
+            if rem == r_n:
+                return 1
+        
+            ans = 0
+            if not strict:
+                if prev + 1 < 10:
+                    ans += rec2(rem + 1, prev + 1, False)
+                if prev - 1 >= 0:
+                    ans += rec2(rem + 1, prev - 1, False)
+            else:
+                if prev + 1 < 10 and prev + 1 <= int(high[rem]):
+                    ans += rec2(rem + 1, prev + 1, prev + 1 == int(high[rem]))
+                if prev - 1 >= 0 and prev - 1 <= int(high[rem]):
+                    ans += rec2(rem + 1, prev - 1, prev - 1 == int(high[rem]))
+            
+            return ans % mod
+
+        @cache
+        def rec4(rem, prev, strict):
+            if rem == l_n:
+                return 1
+        
+            ans = 0
+            if not strict:
+                if prev + 1 < 10:
+                    ans += rec4(rem + 1, prev + 1, False)
+                if prev - 1 >= 0:
+                    ans += rec4(rem + 1, prev - 1, False)
+            else:
+                if prev + 1 < 10 and prev + 1 <= int(low[rem]):
+                    ans += rec4(rem + 1, prev + 1, prev + 1 == int(low[rem]))
+                if prev - 1 >= 0 and prev - 1 <= int(low[rem]):
+                    ans += rec4(rem + 1, prev - 1, prev - 1 == int(low[rem]))
+            
+            return ans % mod
+        
+        # print("1 to high - 1 digit")
+        ans = 0
+        if r_n >= 2:
+            for i in range(1, 10):
+                ans += rec(r_n - 2, i)
+                ans %= mod
+        
+        # print("1 to high less than high all")
+        for i in range(1, int(high[0])):
+            ans += rec3(r_n - 1, i)
+            ans %= mod
+        
+        # print("1 to high less than high full")
+        ans += rec2(1, int(high[0]), True)
+        ans %= mod
+        
+        # print(ans, low)
+
+        left = 0
+        if l_n >= 2:
+            for i in range(1, 10):
+                left += rec(l_n - 2, i)
+                left %= mod
+        
+        for i in range(1, int(low[0])):
+            left += rec3(l_n - 1, i)
+            left %= mod
+        
+        if int(low[0]) > 0:
+            left += rec4(1, int(low[0]), True)
+            left %= mod
+        
+        # print(ans, left, ans - left)
+        
+        return (ans - left) % mod
