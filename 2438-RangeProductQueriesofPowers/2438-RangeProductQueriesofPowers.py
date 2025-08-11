faa0e7a1a4c802c4368947a253c4748393d7eb47
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-11 11:38:55 +0530

    File updated at 11/8/2025, 11:38:53 am

--- 2438-RangeProductQueriesofPowers/2438-RangeProductQueriesofPowers.py
+++ 2438-RangeProductQueriesofPowers/2438-RangeProductQueriesofPowers.py
@@ -1,4 +1,4 @@
-# Last updated: 11/8/2025, 11:38:24 am
+# Last updated: 11/8/2025, 11:38:53 am
 mod = 10 ** 9 + 7
 def inverse(x):
     return pow(x, mod - 2, mod)
@@ -6,17 +6,16 @@ def inverse(x):
 class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         arr = []
+        prefix = [1]
 
         for i in range(32):
             if n & (1 << i) > 0:
                 curr = 1 << i
                 arr.append(curr)
-
+                prefix.append(prefix[-1] * curr % mod)
+        
         ret = []
         for l, r in queries:
-            ans = 1
-            for i in range(l, r + 1):
-                ans = ans * arr[i] % mod
-            ret.append(ans)
+            ret.append(prefix[r + 1] * inverse(prefix[l]) % mod)
         
         return ret
