11ecc55c7922d2c975e6809fd7bcc9f4022e156c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-12 00:58:42 +0530

    File updated at 12/8/2025, 12:58:39 am

--- 187-RepeatedDNASequences/187-RepeatedDNASequences.py
+++ 187-RepeatedDNASequences/187-RepeatedDNASequences.py
@@ -1,4 +1,4 @@
-# Last updated: 12/8/2025, 12:58:02 am
+# Last updated: 12/8/2025, 12:58:39 am
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         n = len(s)
@@ -11,12 +11,11 @@ class Solution:
 
         for i in range(n-9):
             h = 0
-            p = 0
             for j in range(i, i + 10):
                 a = mp[s[j]]
-                h = h + (a * pow(base, p, mod) % mod)
+                h = (h * base % mod) + a
                 h %= mod
-                p += 1
+            
             freq[h] += 1
             if freq[h] == 2:
                 ret.append(s[i:i+10])
