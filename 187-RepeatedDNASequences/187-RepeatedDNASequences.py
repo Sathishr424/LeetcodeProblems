188a0ea050ac464452712b8cc9f81d9fbdb3a100
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-12 01:04:03 +0530

    File updated at 12/8/2025, 1:04:00 am

--- 187-RepeatedDNASequences/187-RepeatedDNASequences.py
+++ 187-RepeatedDNASequences/187-RepeatedDNASequences.py
@@ -1,7 +1,8 @@
-# Last updated: 12/8/2025, 12:58:39 am
+# Last updated: 12/8/2025, 1:04:00 am
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         n = len(s)
+        if n <= 10: return []
         base = 31
         mod = 10**9 + 7
         mp = {'A': 1, 'C': 2, 'G': 3, 'T': 4}
@@ -9,16 +10,22 @@ class Solution:
         freq = defaultdict(int)
         ret = []
 
-        for i in range(n-9):
-            h = 0
-            for j in range(i, i + 10):
-                a = mp[s[j]]
-                h = (h * base % mod) + a
-                h %= mod
-            
+        h = 0
+        for i in range(10):
+            h = h * base % mod
+            h = (h + mp[s[i]]) % mod
+        
+        freq[h] += 1
+    
+        for i in range(10, n):
+            h = h - (mp[s[i-10]] * pow(base, 9, mod) % mod)
+            h %= mod
+
+            h = h * base % mod
+            h = (h + mp[s[i]]) % mod
+
             freq[h] += 1
             if freq[h] == 2:
-                ret.append(s[i:i+10])
+                ret.append(s[i-9:i+1])
 
-        return ret
-        
+        return ret            
