44e62f9e33559ecee7690b5ce28ea609c320e99f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-13 01:03:00 +0530

    File updated at 13/8/2025, 1:02:57 am

--- 2101-DetonatetheMaximumBombs/2101-DetonatetheMaximumBombs.py
+++ 2101-DetonatetheMaximumBombs/2101-DetonatetheMaximumBombs.py
@@ -1,9 +1,9 @@
-# Last updated: 13/8/2025, 1:00:59 am
+# Last updated: 13/8/2025, 1:02:57 am
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         n = len(bombs)
 
-        def isColloide(x, y, x2, y2, r):
+        def isInRange(x, y, x2, y2, r):
             return (x - x2) ** 2 + (y - y2) ** 2 <= r ** 2
         
         graph = defaultdict(list)
@@ -22,7 +22,7 @@ class Solution:
                 if i == j: continue
                 x2, y2, _ = bombs[j]
 
-                if isColloide(x2, y2, x, y, r):
+                if isInRange(x2, y2, x, y, r):
                     graph[i].append(j)
         
         ret = 1
