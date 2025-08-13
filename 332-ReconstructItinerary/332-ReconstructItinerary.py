73f321c3d13dd028c3acaf1dd6158caf3f109c43
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-13 17:07:46 +0530

    File updated at 13/8/2025, 5:07:42 pm

--- 332-ReconstructItinerary/332-ReconstructItinerary.py
+++ 332-ReconstructItinerary/332-ReconstructItinerary.py
@@ -1,24 +1,22 @@
-# Last updated: 12/6/2025, 5:50:28 am
+# Last updated: 13/8/2025, 5:07:42 pm
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        visited = {}
-    
         graph = defaultdict(list)
-        for x, y in tickets:
-            graph[x].append(y)
+
+        for f, t in tickets:
+            graph[f].append(t)
         
-        for x in graph: graph[x].sort(reverse=True)
+        for f in graph:
+            graph[f].sort(reverse=True)
         
         ret = []
-
-        stack = ['JFK']
-
-        while stack:
-            x = stack[-1]
-            
-            if len(graph[x]) > 0:
-                stack.append(graph[x].pop())
-            else:
-                ret.append(stack.pop())
         
+        def dfs(x):
+            while graph[x]:
+                dfs(graph[x].pop())
+            
+            ret.append(x)
+        
+        dfs('JFK')
         return ret[::-1]
+
