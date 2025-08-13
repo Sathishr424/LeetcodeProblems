1ec9cd8eb6ba555b387f7425fb25071d5e1892b5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-13 20:09:22 +0530

    File updated at 13/8/2025, 8:09:18 pm

--- 753-CrackingtheSafe/753-CrackingtheSafe.py
+++ 753-CrackingtheSafe/753-CrackingtheSafe.py
@@ -1,18 +1,19 @@
-# Last updated: 13/8/2025, 8:07:47 pm
+# Last updated: 13/8/2025, 8:09:18 pm
 class Solution:
     def crackSafe(self, n: int, k: int) -> str:
         # 01201
         seen = {}
-        def dfs(s, curr, total):
+        arr = [str(i) for i in range(k)]
+        def dfs(s, total):
+            curr = s[-n:]
             if curr in seen: return None
-
             seen[curr] = 1
+
             if total == 0: return s
-            new_curr = curr[1:]
             for i in range(k):
-                ans = dfs(s + str(i), new_curr + str(i), total - 1)
+                ans = dfs(s + arr[i], total - 1)
                 if ans: return ans
             del seen[curr]
             return None
         
-        return dfs('0' * n, '0' * n, k ** n - 1)
+        return dfs('0' * n, k ** n - 1)
