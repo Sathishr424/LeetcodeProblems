196c289e74dd2d878c286334b04ade4f1a0dfe12
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-15 23:00:04 +0530

    File updated at 15/8/2025, 11:00:03 pm

--- 2111-MinimumOperationstoMaketheArrayK-Increasing/2111-MinimumOperationstoMaketheArrayK-Increasing.py
+++ 2111-MinimumOperationstoMaketheArrayK-Increasing/2111-MinimumOperationstoMaketheArrayK-Increasing.py
@@ -1,23 +1,19 @@
-# Last updated: 15/8/2025, 10:59:28 pm
+# Last updated: 15/8/2025, 11:00:03 pm
 class Solution:
     def kIncreasing(self, nums: List[int], k: int) -> int:
-        # N = 10 ** 5
-        # nums = [random.randrange(N+1) for _ in range(N)]
         n = len(nums)
         ret = 0
-
-        cnts = [0] * k
-        for i in range(n):
-            cnts[i % k] += 1
         
         for i in range(k):
             stack = []
+            cnt = 0
             for j in range(i, n, k):
                 index = bisect_left(stack, nums[j] + 1)
                 if index < len(stack):
                     stack[index] = nums[j]
                 else:
                     stack.append(nums[j])
-            ret += cnts[i] - len(stack)
+                cnt += 1
+            ret += cnt - len(stack)
 
         return ret
