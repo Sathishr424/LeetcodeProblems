04ec597790358d8475191bedae7f043c7a0d41b6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-16 11:10:52 +0530

    File updated at 16/8/2025, 11:10:51 am

--- 2615-SumofDistances/2615-SumofDistances.py
+++ 2615-SumofDistances/2615-SumofDistances.py
@@ -1,17 +1,16 @@
-# Last updated: 16/8/2025, 11:05:23 am
+# Last updated: 16/8/2025, 11:10:51 am
 class Solution:
     def distance(self, nums: List[int]) -> List[int]:
         n = len(nums)
 
         indexes = defaultdict(list)
-        curr_index = defaultdict(int)
         for i in range(n):
             indexes[nums[i]].append(i)
 
         from_left = defaultdict(list)
         from_right = defaultdict(list)
+        ret = [0] * n
         for num in indexes:
-            curr_index[num] = 0
             curr = indexes[num]
             from_left[num] = [0] * len(curr)
             for i in range(1, len(curr)):
@@ -20,16 +19,12 @@ class Solution:
                 from_left[num][i] = diff * i + prev
             
             from_right[num] = [0] * len(curr)
+            ret[curr[-1]] = from_left[num][-1]
             for i in range(len(curr)-2, -1, -1):
                 prev = from_right[num][i + 1]
                 diff = curr[i + 1] - curr[i]
                 from_right[num][i] = diff * (len(curr) - i - 1) + prev
-
-        ret = [0] * n
-        for i in range(n):
-            index = curr_index[nums[i]]
-            ret[i] = from_left[nums[i]][index] + from_right[nums[i]][index]
-            curr_index[nums[i]] += 1
+                ret[curr[i]] = from_right[num][i] + from_left[num][i]
         
         return ret
         
