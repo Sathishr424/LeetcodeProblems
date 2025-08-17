bcbacab9200dfdd59e9cae6b09b58052f28d0381
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-17 09:52:16 +0530

    Create 3654-MinimumSumAfterDivisibleSumDeletions

---
+++ 3654-MinimumSumAfterDivisibleSumDeletions/3654-MinimumSumAfterDivisibleSumDeletions.py
@@ -0,0 +1,31 @@
+# Last updated: 17/8/2025, 9:52:15 am
+class Solution:
+    def minArraySum(self, nums: List[int], k: int) -> int:
+        if k == 1: return 0
+        n = len(nums)
+
+        prefix = [0]
+        s = 0
+        for i in range(n):
+            s = (s + nums[i]) % k
+            prefix.append(s)
+
+        relation = defaultdict(list)
+        for i in range(len(prefix)):
+            relation[prefix[i]].append(i)
+
+        @cache
+        def rec(index):
+            if index >= n: return 0
+
+            ans = rec(index + 1) + nums[index]
+            i = bisect_right(relation[prefix[index]], index)
+
+            if i < len(relation[prefix[index]]):
+                ans = min(ans, rec(relation[prefix[index]][i]))
+
+            return ans
+
+        ans = rec(0)
+        rec.cache_clear()
+        return ans
