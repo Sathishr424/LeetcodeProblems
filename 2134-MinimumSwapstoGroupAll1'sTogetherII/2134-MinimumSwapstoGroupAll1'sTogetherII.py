f3490d7cb65dc0a03d7cca1789369b5526300eae
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-17 18:30:59 +0530

    File updated at 17/8/2025, 6:30:57 pm

--- 2134-MinimumSwapstoGroupAll1'sTogetherII/2134-MinimumSwapstoGroupAll1'sTogetherII.py
+++ 2134-MinimumSwapstoGroupAll1'sTogetherII/2134-MinimumSwapstoGroupAll1'sTogetherII.py
@@ -1,26 +1,23 @@
-# Last updated: 17/8/2025, 6:07:55 pm
+# Last updated: 17/8/2025, 6:30:57 pm
 class Solution:
-    def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
-        n = len(startWords)
-        m = len(targetWords)
+    def minSwaps(self, nums: List[int]) -> int:
+        # 0 0 0 0 1 1 1 1 0 0 1 1 0 0 1 1 0 0 0 0 1 1 0 1
+        n = len(nums)
 
-        ret = 0
-        there = set()
-        for word in startWords:
-            mask = 0
-            for char in word:
-                mask |= 1 << (ord(char) - ord('a'))
-            
-            for a in range(26):
-                if mask & (1 << a) == 0:
-                    there.add(mask | (1 << a))
+        need = nums.count(1)
 
-        for word in targetWords:
-            mask = 0
-            for char in word:
-                mask |= 1 << (ord(char) - ord('a'))
-            if mask in there: ret += 1
+        one = 0
+        for i in range(need):
+            one += nums[i] == 1
+        
+        ret = need - one
 
+        for i in range(need, n + n - 1):
+            one -= nums[(i - need) % n] == 1
+            one += nums[i % n] == 1
+
+            ret = min(ret, need - one)
+        
         return ret
-                    
             
+                
