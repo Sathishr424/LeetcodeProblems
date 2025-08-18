a5002c23cb2ff378179164ab5df3de36d8b1d9a6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-18 19:33:31 +0530

    File updated at 18/8/2025, 7:33:28 pm

--- 805-SplitArrayWithSameAverage/805-SplitArrayWithSameAverage.py
+++ 805-SplitArrayWithSameAverage/805-SplitArrayWithSameAverage.py
@@ -1,4 +1,4 @@
-# Last updated: 18/8/2025, 7:32:26 pm
+# Last updated: 18/8/2025, 7:33:28 pm
 class Solution:
     def splitArraySameAverage(self, nums: List[int]) -> bool:
         n = len(nums)
@@ -20,17 +20,14 @@ class Solution:
             if rec(index + 1, rem, rem_cnt) or rec(index + 1, rem - nums[index], rem_cnt-1): return True
             
             return False
-        # prec = 0.00005
+
         for i in range(1, n):
             need = round(av * i, 5)
-            # print(i, need, nums)
             if need - int(need) == 0:
-                # rem = total - need
-                if rec(0, int(need), i): 
+                rem = total - need
+                if rem / (n - i) == av and rec(0, int(need), i): 
                     return True
-            
-        
-        # print(need)
+
         rec.cache_clear()
         return False
         
