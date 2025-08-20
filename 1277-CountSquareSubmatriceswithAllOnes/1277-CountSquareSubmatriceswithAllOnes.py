92e3e226682044ab1dbcdecbe348581d9acfa1da
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-20 11:08:59 +0530

    File updated at 20/8/2025, 11:08:55 am

--- 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.py
+++ 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.py
@@ -1,4 +1,4 @@
-# Last updated: 20/8/2025, 11:06:35 am
+# Last updated: 20/8/2025, 11:08:55 am
 """
 [0,1,1,1,1,1],
 [1,1,1,1,1,1],
@@ -16,16 +16,22 @@ class Solution:
         DIR = [(0, 1), (1, 1), (1, 0)]
         tot = 0
 
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
                 if matrix[i][j] == 0: continue
                 max_area = inf
                 for i2, j2 in DIR:
                     i2 += i
                     j2 += j
 
-                    max_area = cmin(max_area, matrix[i2][j2] if i2 < m and j2 < n else 0)
+                    max_area = cmin(max_area, matrix[i2][j2])
                 matrix[i][j] = max_area + 1
                 tot += matrix[i][j]
+        
+        for i in range(n):
+            tot += matrix[m-1][i]
+        
+        for j in range(m-1):
+            tot += matrix[j][n-1]
         
         return tot
