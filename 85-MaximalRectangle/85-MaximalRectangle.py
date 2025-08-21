e051ac0e2a994a012e22f7c54ef75ec343eda8fa
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-21 15:35:37 +0530

    File updated at 21/8/2025, 3:35:36 pm

--- 85-MaximalRectangle/85-MaximalRectangle.py
+++ 85-MaximalRectangle/85-MaximalRectangle.py
@@ -1,4 +1,6 @@
-# Last updated: 21/8/2025, 3:33:47 pm
+# Last updated: 21/8/2025, 3:35:36 pm
+cmax = lambda x, y: x if x > y else y
+
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         m = len(matrix)
@@ -10,8 +12,6 @@ class Solution:
             for j in range(n-1, -1, -1):
                 if matrix[i][j] == '1':
                     h[i][j] = h[i][j + 1] + 1
-        
-        # [print(row) for row in h]
 
         max_ret = 0
         for j in range(n):
@@ -20,17 +20,17 @@ class Solution:
                 cnt = 0
                 while stack and stack[-1][0] >= h[i][j]:
                     v, c = stack.pop()
-                    cnt += c + 1
-                    max_ret = max(max_ret, v * cnt)
-                stack.append((h[i][j], cnt))
-                # print(j, stack)
+                    cnt += c
+                    max_ret = cmax(max_ret, v * cnt)
+                stack.append((h[i][j], cnt + 1))
+            
             tot_cnt = 0
             for _, c in stack:
-                tot_cnt += c + 1
+                tot_cnt += c
+            
             while stack:
                 v, c = stack.popleft()
-                # print(stack, tot_cnt, (v, c))
-                max_ret = max(max_ret, v * tot_cnt)
-                tot_cnt -= c + 1
+                max_ret = cmax(max_ret, v * tot_cnt)
+                tot_cnt -= c
 
         return max_ret
