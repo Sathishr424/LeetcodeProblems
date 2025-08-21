768236bcb274da6ae6dd011b6f18bc5b634be673
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-21 17:14:28 +0530

    File updated at 21/8/2025, 5:14:28 pm

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.py
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.py
@@ -1,20 +1,20 @@
-# Last updated: 9/4/2025, 3:06:54 am
+# Last updated: 21/8/2025, 5:14:28 pm
+cmax = lambda x, y: x if x > y else y
+
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
+        max_rect = 0
         stack = []
-        ret = 0
+        for i in range(n):
+            left = i
+            while stack and stack[-1][0] > heights[i]:
+                height, left = stack.pop()
+                max_rect = cmax(max_rect, height * (i - left))
+            stack.append((heights[i], left))
 
-        for i, h in enumerate(heights):
-            index = i
-            while stack and stack[-1][1] > h:
-                index, prev_h = stack.pop()
-                ret = max(ret, (i-index) * prev_h)
-            
-            stack.append((index, h))
-        
         while stack:
-            index, h = stack.pop()
-            ret = max(ret, (n-index) * h)
-        
-        return ret
+            height, left = stack.pop()
+            max_rect = cmax(max_rect, height * (n - left))
+
+        return max_rect
