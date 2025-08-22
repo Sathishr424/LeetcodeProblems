efb00ce21765c39851cacf1052e1f61a0823051b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-22 20:44:13 +0530

    File updated at 22/8/2025, 8:44:13 pm

--- 2156-FindSubstringWithGivenHashValue/2156-FindSubstringWithGivenHashValue.py
+++ 2156-FindSubstringWithGivenHashValue/2156-FindSubstringWithGivenHashValue.py
@@ -1,4 +1,4 @@
-# Last updated: 22/8/2025, 8:43:47 pm
+# Last updated: 22/8/2025, 8:44:13 pm
 class Solution:
     def subStrHash(self, s: str, power: int, mod: int, k: int, hashValue: int) -> str:
         n = len(s)
@@ -6,20 +6,20 @@ class Solution:
         def getAlp(char):
             return ord(char) - ord('a') + 1
         
+        cached = pow(power, k, mod)
+        
         suffix = deque([0])
         roll = 0
         for i in range(n-1, -1, -1):
             roll = roll * power % mod
             roll = (roll + getAlp(s[i])) % mod
             suffix.appendleft(roll)
-        
-        # print(list(suffix))
     
         for i in range(n-k+1):
             right = suffix[i + k]
             left = suffix[i]
             
-            diff = left - right * pow(power, k, mod) % mod
+            diff = left - right * cached % mod
             diff %= mod
             if diff == hashValue: return s[i:i+k]
         
