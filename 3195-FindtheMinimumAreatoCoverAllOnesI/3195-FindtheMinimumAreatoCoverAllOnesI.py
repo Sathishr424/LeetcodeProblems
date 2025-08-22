b6f813cf92ef21eb39f2f824a78bef51146e04d6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-22 13:13:35 +0530

    File updated at 22/8/2025, 1:13:34 pm

--- 3195-FindtheMinimumAreatoCoverAllOnesI/3195-FindtheMinimumAreatoCoverAllOnesI.py
+++ 3195-FindtheMinimumAreatoCoverAllOnesI/3195-FindtheMinimumAreatoCoverAllOnesI.py
@@ -1,4 +1,7 @@
-# Last updated: 22/8/2025, 1:12:30 pm
+# Last updated: 22/8/2025, 1:13:34 pm
+cmin = lambda x, y: x if x < y else y
+cmax = lambda x, y: x if x > y else y
+
 class Solution:
     def minimumArea(self, grid: List[List[int]]) -> int:
         m = len(grid)
@@ -9,10 +12,10 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    pos[0] = min(pos[0], i)
-                    pos[1] = max(pos[1], j)
-                    pos[2] = max(pos[2], i)
-                    pos[3] = min(pos[3], j)
+                    pos[0] = cmin(pos[0], i)
+                    pos[1] = cmax(pos[1], j)
+                    pos[2] = cmax(pos[2], i)
+                    pos[3] = cmin(pos[3], j)
 
         width = pos[1] - pos[3] + 1
         height = pos[2] - pos[0] + 1
