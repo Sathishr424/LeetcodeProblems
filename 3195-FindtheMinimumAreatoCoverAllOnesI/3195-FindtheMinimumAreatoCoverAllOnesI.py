9546b90b5323d7f9d6954352807c7d7b5ef22d2d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-22 13:12:30 +0530

    File updated at 22/8/2025, 1:12:30 pm

--- 3195-FindtheMinimumAreatoCoverAllOnesI/3195-FindtheMinimumAreatoCoverAllOnesI.py
+++ 3195-FindtheMinimumAreatoCoverAllOnesI/3195-FindtheMinimumAreatoCoverAllOnesI.py
@@ -1,16 +1,10 @@
-# Last updated: 22/8/2025, 1:11:21 pm
+# Last updated: 22/8/2025, 1:12:30 pm
 class Solution:
     def minimumArea(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
 
-        pos = [0, 0, 0, 0]
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    pos = [i, j, i, j]
-                    break
+        pos = [m, 0, 0, n]
         
         for i in range(m):
             for j in range(n):
@@ -19,12 +13,8 @@ class Solution:
                     pos[1] = max(pos[1], j)
                     pos[2] = max(pos[2], i)
                     pos[3] = min(pos[3], j)
-        
-        # start = pos[0], pos[3]
-        # end = pos[2], pos[1]
 
         width = pos[1] - pos[3] + 1
         height = pos[2] - pos[0] + 1
-        # print(pos, width, height)
 
         return height * width
