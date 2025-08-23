8527b8f136ca197f5194895846a1c281343471ec
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-23 16:19:49 +0530

    File updated at 23/8/2025, 4:19:48 pm

--- 3197-FindtheMinimumAreatoCoverAllOnesII/3197-FindtheMinimumAreatoCoverAllOnesII.py
+++ 3197-FindtheMinimumAreatoCoverAllOnesII/3197-FindtheMinimumAreatoCoverAllOnesII.py
@@ -1,11 +1,13 @@
-# Last updated: 23/8/2025, 4:18:33 pm
+# Last updated: 23/8/2025, 4:19:48 pm
+cmin = lambda x, y: x if x < y else y
+cmax = lambda x, y: x if x > y else y
+
 class Solution:
     def minimumSum(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
 
         def split(t, r, b, l):
-            # print(t, r, b, l, 'split')
             min_area = inf
             ones = 0
             for i in range(t, b + 1):
@@ -19,7 +21,7 @@ class Solution:
                     if grid[i][j] == 1:
                         rem_ones -= 1
                 if rem_ones and rem_ones < ones:
-                    min_area = min(min_area, getArea(t, r, i, l) + getArea(i+1, r, b, l))
+                    min_area = cmin(min_area, getArea(t, r, i, l) + getArea(i+1, r, b, l))
             
             rem_ones = ones
             for j in range(l, r + 1):
@@ -27,12 +29,11 @@ class Solution:
                     if grid[i][j] == 1:
                         rem_ones -= 1
                 if rem_ones and rem_ones < ones:
-                    min_area = min(min_area, getArea(t, j, b, l) + getArea(t, r, b, j+1))
+                    min_area = cmin(min_area, getArea(t, j, b, l) + getArea(t, r, b, j+1))
 
             return min_area
 
         def getArea(t, r, b, l):
-            # print('area', (t, r, b, l))
             left = inf
             top = inf
             right = 0
@@ -41,11 +42,10 @@ class Solution:
             for i in range(t, b + 1):
                 for j in range(l, r + 1):
                     if grid[i][j] == 1:
-                        top = min(top, i)
-                        right = max(right, j)
-                        bottom = max(bottom, i)
-                        left = min(left, j)
-            # print("AREA", (t, r, b, l), (top, right, bottom, left), (right - left + 1), (bottom - top + 1))
+                        top = cmin(top, i)
+                        right = cmax(right, j)
+                        bottom = cmax(bottom, i)
+                        left = cmin(left, j)
             return (right - left + 1) * (bottom - top + 1)
 
         def getAns():
@@ -62,10 +62,8 @@ class Solution:
                     if grid[i][j] == 1:
                         rem_ones -= 1
                 if rem_ones and rem_ones < ones:
-                    curr = split(0, n-1, i, 0) + getArea(i+1, n-1, m-1, 0)
-                    ans = min(ans, curr)
-                    curr = getArea(0, n-1, i, 0) + split(i+1, n-1, m-1, 0)
-                    ans = min(ans, curr)
+                    ans = cmin(ans, split(0, n-1, i, 0) + getArea(i+1, n-1, m-1, 0))
+                    ans = cmin(ans, getArea(0, n-1, i, 0) + split(i+1, n-1, m-1, 0))
             
             rem_ones = ones
             for j in range(n):
@@ -73,13 +71,10 @@ class Solution:
                     if grid[i][j] == 1:
                         rem_ones -= 1
                 if rem_ones and rem_ones < ones:
-                    curr = split(0, j, m-1, 0) + getArea(0, n-1, m-1, j+1)
-                    ans = min(ans, curr)
-                    curr = getArea(0, j, m-1, 0) + split(0, n-1, m-1, j+1)
-                    ans = min(ans, curr)
+                    ans = cmin(ans, split(0, j, m-1, 0) + getArea(0, n-1, m-1, j+1))
+                    ans = cmin(ans, getArea(0, j, m-1, 0) + split(0, n-1, m-1, j+1))
                     
             return ans
-        # [print(row) for row in grid]
-        # print(m, n)
+
         return getAns()
         
