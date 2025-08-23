74c4932ffe15acc430fd834f865d10da9032179e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-24 01:56:28 +0530

    File updated at 24/8/2025, 1:56:27 am

--- 2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods/2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods.py
+++ 2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods/2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods.py
@@ -1,18 +1,7 @@
-# Last updated: 24/8/2025, 1:54:47 am
+# Last updated: 24/8/2025, 1:56:27 am
 class Solution:
     def minimumTime(self, s: str) -> int:
-        # s = [str(random.randrange(2)) for _ in range(10**5 * 2)]
         n = len(s)
-
-        left = []
-        l = 0
-        add = 0
-        for i in range(n):
-            add += 1
-            if s[i] == '1':
-                l += add
-                add = 0
-            left.append(l)
 
         right = [0] * n
         r = 0
@@ -33,10 +22,14 @@ class Solution:
             else:
                 return rec(index + 1)
         
+        l = 0
+        add = 0
         ans = inf
         for i in range(n):
-            curr = rec(i)
-            ans = min(ans, (left[i-1] if i > 0 else 0) + rec(i))
-            # print(i, curr, left[i-1] + rec(i))
+            ans = min(ans, l + rec(i))
+            add += 1
+            if s[i] == '1':
+                l += add
+                add = 0
         
         return ans
