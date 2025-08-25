c9759f2352cbd942e505bc44cddb3dbe92ffc70f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-25 15:54:23 +0530

    File updated at 25/8/2025, 3:54:23 pm

--- 3661-MaximumWallsDestroyedbyRobots/3661-MaximumWallsDestroyedbyRobots.py
+++ 3661-MaximumWallsDestroyedbyRobots/3661-MaximumWallsDestroyedbyRobots.py
@@ -1,35 +1,4 @@
-# Last updated: 25/8/2025, 5:14:46 am
-class SegmentTree:
-    def __init__(self, n, walls):
-        self.n = n
-        self.walls = walls
-        self.tree = [0] * (self.n * 4)
-
-        self.build(0, self.n-1, 0)
-    
-    def build(self, l, r, index):
-        if l == r:
-            self.tree[index] = 1 if l in self.walls else 0
-            return self.tree[index]
-        
-        mid = (l + r) // 2
-        left = self.build(l, mid, index * 2 + 1)
-        right = self.build(mid + 1, r, index * 2 + 2)
-
-        self.tree[index] = left + right
-        return self.tree[index]
-    
-    def query(self, l, r, index, left, right):
-        if l > right or r < left:
-            return 0
-        
-        if l >= left and r <= right:
-            return self.tree[index]
-        
-        mid = (l + r) // 2
-
-        return self.query(l, mid, index * 2 + 1, left, right) + self.query(mid + 1, r, index * 2 + 2, left, right)
-
+# Last updated: 25/8/2025, 3:54:23 pm
 class Solution:
     def maxWalls(self, robots: List[int], distance: List[int], walls: List[int]) -> int:
         n = len(robots)
@@ -59,22 +28,30 @@ class Solution:
         for wall in walls:
             c_walls[compressed[wall]] = 1
         
-        tree = SegmentTree(c_index, c_walls)
+        prefix = [0]
+        w = 0
+        for i in range(c_index):
+            if i in c_walls:
+                w += 1
+            prefix.append(w)
 
         left = [0] * n
         right = [0] * n
         between = [0] * n
 
+        def query(l, r):
+            return prefix[r + 1] - prefix[l]
+
         for i in range(n):
             rob, dis = combined[i]
             l = compressed[max(robots[i-1] + 1, rob - dis) if i > 0 else rob - dis]
-            left[i] = tree.query(0, c_index-1, 0, l, compressed[rob])
+            left[i] = query(l, compressed[rob])
 
             r = compressed[min(robots[i+1] - 1, rob + dis) if i + 1 < n else rob + dis]
-            right[i] = tree.query(0, c_index-1, 0, compressed[rob], r)
+            right[i] = query(compressed[rob], r)
 
             if i + 1 < n:
-                between[i] = tree.query(0, c_index-1, 0, compressed[robots[i]], compressed[robots[i + 1]])
+                between[i] = query(compressed[robots[i]], compressed[robots[i + 1]])
 
         @cache
         def rec(index, is_left):
