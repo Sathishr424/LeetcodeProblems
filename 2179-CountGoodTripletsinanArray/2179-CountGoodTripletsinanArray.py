b9ae968f5ce5cc922c94d49674c9dd84949b942a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-25 21:55:19 +0530

    File updated at 25/8/2025, 9:55:18 pm

--- 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
+++ 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
@@ -1,35 +1,73 @@
-# Last updated: 17/4/2025, 7:34:48 pm
+# Last updated: 25/8/2025, 9:55:18 pm
+class SegmentTree:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * (self.n * 4)
+        self.indexes = [0] * self.n
+
+        self.build(0, self.n-1, 0)
+    
+    def build(self, l, r, index):
+        if l == r:
+            self.indexes[l] = index
+            return
+        
+        mid = (l + r) // 2
+        self.build(l, mid, index * 2 + 1)
+        self.build(mid + 1, r, index * 2 + 2)
+    
+    def query(self, l, r, index, left, right):
+        if l > right or r < left:
+            return 0
+        
+        if l >= left and r <= right:
+            return self.tree[index]
+        
+        mid = (l + r) // 2
+
+        return self.query(l, mid, index * 2 + 1, left, right) + self.query(mid + 1, r, index * 2 + 2, left, right)
+    
+    def update(self, index):
+        index = self.indexes[index]
+        self.tree[index] = 1
+
+        while index:
+            index = (index - 1) // 2
+            self.tree[index] = self.tree[index * 2  + 1] + self.tree[index * 2 + 2]
+
 class Solution:
     def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
-        # FenwekTree
         n = len(nums1)
-        relation = [0] * n
-
-        for i, num in enumerate(nums2):
-            relation[num] = i
-
-        m = n+1
-        tree = [0] * (m+1)
         ret = 0
 
-        def query(index):
-            s = 0
-            while index:
-                s += tree[index]
-                index -= index & -index
-            return s
-        
-        def update(index):
-            while index <= m:
-                tree[index] += 1
-                index += index & -index
-        
+        indexes = [0] * n
         for i in range(n):
-            num = relation[nums1[i]]
-            left = query(num)
-            right = (n-num-1)-(i-left)
-            ret += left * right
-            update(num+1)
+            indexes[nums1[i]] = i
+
+        tree = SegmentTree(n)
+        from_left = [0] * n
+        for i in range(n):
+            tree.update(indexes[nums2[i]])
+
+            index = indexes[nums2[i]]
+            # if index <= i:
+            cnt = tree.query(0, n-1, 0, 0, index-1)
+            from_left[i] = cnt
         
+        for i in range(len(tree.tree)):
+            tree.tree[i] = 0
+        
+        # from_right = [0] * n
+        for i in range(n-1, -1, -1):
+            tree.update(indexes[nums2[i]])
+
+            index = indexes[nums2[i]]
+
+            cnt = tree.query(0, n-1, 0, index + 1, n-1)
+            ret += from_left[i] * cnt
+            # from_right[i] = cnt
+        
+        # print(from_left)
+        # print(from_right)
+
         return ret
-        
