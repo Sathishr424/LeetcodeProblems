5baf26479a53ea95e5c802fd1092c070fa0e4a7a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-25 22:03:01 +0530

    File updated at 25/8/2025, 10:03:01 pm

--- 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
+++ 2179-CountGoodTripletsinanArray/2179-CountGoodTripletsinanArray.py
@@ -1,40 +1,4 @@
-# Last updated: 25/8/2025, 9:55:18 pm
-class SegmentTree:
-    def __init__(self, n):
-        self.n = n
-        self.tree = [0] * (self.n * 4)
-        self.indexes = [0] * self.n
-
-        self.build(0, self.n-1, 0)
-    
-    def build(self, l, r, index):
-        if l == r:
-            self.indexes[l] = index
-            return
-        
-        mid = (l + r) // 2
-        self.build(l, mid, index * 2 + 1)
-        self.build(mid + 1, r, index * 2 + 2)
-    
-    def query(self, l, r, index, left, right):
-        if l > right or r < left:
-            return 0
-        
-        if l >= left and r <= right:
-            return self.tree[index]
-        
-        mid = (l + r) // 2
-
-        return self.query(l, mid, index * 2 + 1, left, right) + self.query(mid + 1, r, index * 2 + 2, left, right)
-    
-    def update(self, index):
-        index = self.indexes[index]
-        self.tree[index] = 1
-
-        while index:
-            index = (index - 1) // 2
-            self.tree[index] = self.tree[index * 2  + 1] + self.tree[index * 2 + 2]
-
+# Last updated: 25/8/2025, 10:03:01 pm
 class Solution:
     def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
@@ -44,30 +8,17 @@ class Solution:
         for i in range(n):
             indexes[nums1[i]] = i
 
-        tree = SegmentTree(n)
         from_left = [0] * n
+        sl = SortedList()
         for i in range(n):
-            tree.update(indexes[nums2[i]])
-
-            index = indexes[nums2[i]]
-            # if index <= i:
-            cnt = tree.query(0, n-1, 0, 0, index-1)
-            from_left[i] = cnt
+            sl.add(indexes[nums2[i]])
+            from_left[i] = sl.bisect_left(indexes[nums2[i]])
         
-        for i in range(len(tree.tree)):
-            tree.tree[i] = 0
-        
-        # from_right = [0] * n
+        sl = SortedList()
         for i in range(n-1, -1, -1):
-            tree.update(indexes[nums2[i]])
+            sl.add(indexes[nums2[i]])
 
-            index = indexes[nums2[i]]
-
-            cnt = tree.query(0, n-1, 0, index + 1, n-1)
+            cnt = len(sl) - sl.bisect_left(indexes[nums2[i]]) - 1
             ret += from_left[i] * cnt
-            # from_right[i] = cnt
-        
-        # print(from_left)
-        # print(from_right)
 
         return ret
