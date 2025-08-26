ed81f93de031183fe874c9d01a16a0fe7e00ea8c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-26 10:46:34 +0530

    File updated at 26/8/2025, 10:46:33 am

--- 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.py
+++ 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.py
@@ -1,10 +1,10 @@
-# Last updated: 26/8/2025, 10:46:17 am
+# Last updated: 26/8/2025, 10:46:33 am
 class Solution:
     def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
         max_area = 0
         max_diagonal = 0
         for i, (l, w) in enumerate(dimensions):
-            s = sqrt(l * l + w * w)
+            s = l * l + w * w
             area = l * w
             if s > max_diagonal or (s == max_diagonal and area > max_area):
                 max_diagonal = s
