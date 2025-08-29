0c376fbf138fb150bd4f9f4b67a18c8130e99821
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-29 13:17:38 +0530

    File updated at 29/8/2025, 1:17:36 pm

--- 3021-AliceandBobPlayingFlowerGame/3021-AliceandBobPlayingFlowerGame.py
+++ 3021-AliceandBobPlayingFlowerGame/3021-AliceandBobPlayingFlowerGame.py
@@ -1,13 +1,7 @@
-# Last updated: 29/8/2025, 1:16:48 pm
+# Last updated: 29/8/2025, 1:17:36 pm
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
-        ret = 0
         even = m // 2
         odd = m - even
-        for i in range(1, n + 1):
-            if i % 2:
-                ret += even
-            else:
-                ret += odd
         
-        return ret
+        return (n // 2) * odd + ((n + 1) // 2) * even
