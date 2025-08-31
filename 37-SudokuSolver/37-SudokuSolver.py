f6715a4784cd35c16a346d16f2e530c9a169ffa6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-08-31 17:29:33 +0530

    File updated at 31/8/2025, 5:29:33 pm

--- 37-SudokuSolver/37-SudokuSolver.py
+++ 37-SudokuSolver/37-SudokuSolver.py
@@ -1,43 +1,57 @@
-# Last updated: 21/7/2025, 3:00:30 pm
+# Last updated: 31/8/2025, 5:29:33 pm
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        boxes = [0] * 9
+        n = 9
         rows = [0] * 9
         cols = [0] * 9
-        need = []
-        n = 9
+        boxes = [0] * 9
+
+        emptyCells = []
         for i in range(n):
             for j in range(n):
-                if board[i][j] == '.': need.append((i, j))
-                else:
-                    pos = 1 << (int(board[i][j]) - 1)
-                    box = (i // 3) * 3 + (j // 3)
-                    rows[i] |= pos
-                    cols[j] |= pos
-                    boxes[box] |= pos
+                if board[i][j] == '.':
+                    emptyCells.append((i, j))
         
-        def dfs(index):
-            if index == len(need):
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == '.': continue
+                cell = 1 << (int(board[i][j]) - 1)
+                box = (i // 3) * 3 + (j // 3)
+                rows[i] |= cell
+                cols[j] |= cell
+                boxes[box] |= cell
+            
+        # print([format(rows[i], '09b') for i in range(n)])
+        # print([format(cols[i], '09b') for i in range(n)])
+        # print([format(boxes[i], '09b') for i in range(n)])
+
+        # print(emptyCells)
+        ret = []
+        def rec(index):
+            if index == len(emptyCells):
+                ret = [board[k][:] for k in range(n)]
                 return True
-            i, j = need[index]
+            i, j = emptyCells[index]
             box = (i // 3) * 3 + (j // 3)
-            for val in range(9):
-                pos = 1 << val
-                if rows[i] & pos == 0 and cols[j] & pos == 0 and boxes[box] & pos == 0:
-                    r = rows[i]
-                    c = cols[j]
-                    b = boxes[box]
-                    rows[i] |= pos
-                    cols[j] |= pos
-                    boxes[box] |= pos
-                    board[i][j] = str(val + 1)
-                    if dfs(index + 1): return True
-                    rows[i] = r
-                    cols[j] = c
-                    boxes[box] = b
-                    board[i][j] = '.'
+            for num in range(9):
+                cell = 1 << num
+                if rows[i] & cell == 0 and cols[j] & cell == 0 and boxes[box] & cell == 0:
+                    board[i][j] = str(num + 1)
+                    prev_row = rows[i]
+                    prev_col = cols[j]
+                    prev_box = boxes[box]
+                    rows[i] |= cell
+                    cols[j] |= cell
+                    boxes[box] |= cell
+                    if rec(index + 1): return True
+                    rows[i] = prev_row
+                    cols[j] = prev_col
+                    boxes[box] = prev_box
+            
             return False
-        dfs(0)
+        
+        rec(0)
+        return ret
