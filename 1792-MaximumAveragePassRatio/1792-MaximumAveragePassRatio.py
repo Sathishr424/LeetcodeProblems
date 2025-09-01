adf1396f584e01f95df867910ca6b4412c58433d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-01 22:33:15 +0530

    File updated at 1/9/2025, 10:33:13 pm

--- 1792-MaximumAveragePassRatio/1792-MaximumAveragePassRatio.py
+++ 1792-MaximumAveragePassRatio/1792-MaximumAveragePassRatio.py
@@ -1,10 +1,18 @@
-# Last updated: 1/9/2025, 10:32:56 pm
+# Last updated: 1/9/2025, 10:33:13 pm
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
         n = len(classes)
+
         heap = []
+        students = [0] * n
+        total = [0] * n
+
+        for i in range(n):
+            students[i] = classes[i][0]
+            total[i] = classes[i][1]
 
         for i, (p, t) in enumerate(classes):
+            if p == t: continue
             heapq.heappush(heap, ((p / t - (p + 1) / (t + 1)), p, t, i))
 
         while extraStudents and heap:
@@ -14,9 +22,13 @@ class Solution:
             heapq.heappush(heap, ((p / t - (p + 1) / (t + 1)), p, t, index))
             extraStudents -= 1
         
-        s = 0
         while heap:
             diff, p, t, index = heapq.heappop(heap)
-            s += p / t
+            students[index] = p
+            total[index] = t
+
+        s = 0
+        for i in range(n):
+            s += students[i] / total[i]
 
         return s / n
