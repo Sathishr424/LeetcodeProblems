4efc416e4decfa6e86d92207a60e04a04267115e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-03 16:25:41 +0530

    File updated at 3/9/2025, 4:25:39 pm

--- 3025-FindtheNumberofWaystoPlacePeopleI/3025-FindtheNumberofWaystoPlacePeopleI.py
+++ 3025-FindtheNumberofWaystoPlacePeopleI/3025-FindtheNumberofWaystoPlacePeopleI.py
@@ -1,46 +1,42 @@
-# Last updated: 2/9/2025, 11:40:51 am
+# Last updated: 3/9/2025, 4:25:39 pm
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
         n = len(points)
 
-        points.sort(key=lambda x: (x[0], -x[1]))
-        max_x = 0
-        max_y = 0
+        row = 0
+        col = 0
         for x, y in points:
-            max_x = max(max_x, x)
-            max_y = max(max_y, y)
-        max_x += 1
-        max_y += 1
+            row = max(row, y)
+            col = max(col, x)
+        row += 1
+        col += 1
+        points.sort(key=lambda x: (x[0], -x[1]))
 
-        points_on_grid = [[0] * max_x for _ in range(max_y)]
+        points_on_grid = [[0] * col for _ in range(row)]
         for x, y in points:
             points_on_grid[y][x] = 1
         
-        prefix = [[] for _ in range(max_y)]
-
-        for i in range(max_y):
-            prefix[i].append(0)
+        prefix = [[0] * (col + 1) for _ in range(row + 1)]
+        for i in range(1, row + 1):
             s = 0
-            for j in range(max_x):
-                if points_on_grid[i][j] == 1:
-                    s += 1
-                prefix[i].append(s)
-
-        def isValid(y, y2, x, x2):
-            if y2 < y: return False
-            for k in range(y + 1, y2):
-                if prefix[k][x + 1] - prefix[k][x2] > 0: return False
-            
-            if y == y2:
-                return prefix[y][x + 1] - prefix[y][x2] == 2
-            return prefix[y][x + 1] - prefix[y][x2] == 1 and prefix[y2][x + 1] - prefix[y2][x2] == 1
+            for j in range(1, col + 1):
+                s += points_on_grid[i-1][j-1]
+                prefix[i][j] = s + prefix[i - 1][j]
 
         count = 0
         for i in range(n):
-            x, y = points[i]
+            x, y = points[i] # lower right
             for j in range(i):
-                x2, y2 = points[j]
-                if isValid(y, y2, x, x2):
+                x2, y2 = points[j] # Upper left
+
+                if y2 < y: continue
+                
+                full = prefix[y2 + 1][x + 1]
+                bottom_left = prefix[y2 + 1][x2]
+                top_right = prefix[y][x + 1]
+                center = prefix[y][x2]
+                
+                if full - (bottom_left + top_right - center) == 2:
                     count += 1
-        
+            
         return count
