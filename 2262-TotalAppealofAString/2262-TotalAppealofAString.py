a092a8a1a707e15cfb6c48682369e72e8bc8393b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-15 05:09:13 +0530

    File updated at 15/9/2025, 5:09:10 am

--- 2262-TotalAppealofAString/2262-TotalAppealofAString.py
+++ 2262-TotalAppealofAString/2262-TotalAppealofAString.py
@@ -1,15 +1,15 @@
-# Last updated: 15/9/2025, 5:08:17 am
+# Last updated: 15/9/2025, 5:09:10 am
 class Solution:
     def appealSum(self, s: str) -> int:
-        # s = [chr(random.randrange(26) + ord('a')) for _ in range(10 ** 5)]
         n = len(s)
 
-        indexes = {}
+        indexes = [-1] * 26
         cnt = 0
         for i in range(n):
-            left = 0 if s[i] not in indexes else indexes[s[i]] + 1
+            a = ord(s[i]) - ord('a')
+            left = indexes[a] + 1
             
             cnt += (n - i) * (i - left + 1)
-            indexes[s[i]] = i
+            indexes[a] = i
 
         return cnt
