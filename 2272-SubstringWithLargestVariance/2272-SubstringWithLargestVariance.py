b42b7a8068218a147bdfe24b8a9fb55411a3d9a0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-15 16:46:14 +0530

    Create 2272-SubstringWithLargestVariance

---
+++ 2272-SubstringWithLargestVariance/2272-SubstringWithLargestVariance.py
@@ -0,0 +1,49 @@
+# Last updated: 15/9/2025, 4:46:14 pm
+cmax = lambda x, y: x if x > y else y
+cmin = lambda x, y: x if x < y else y
+class Solution:
+    def largestVariance(self, s: str) -> int:
+        # s = [chr(random.randrange(26) + ord('a')) for _ in range(10000)]
+        n = len(s)
+        s = [ord(char) - ord('a') for char in s]
+        max_ans = 0
+        there = [0] * 26
+        for a in s:
+            there[a] = 1
+        
+        inf = 10**5
+
+        for i in range(26):
+            for j in range(26):
+                if i == j or not (there[i] and there[j]): continue
+
+                minor_count = 0
+                major_count = 0
+
+                global_max = 0
+                local_max = 0
+                rem_min_count = 0
+
+                for char in s:
+                    if char == j:
+                        rem_min_count += 1
+                
+                for char in s:
+                    if char == i:
+                        major_count += 1
+                    elif char == j:
+                        minor_count += 1
+                        rem_min_count -= 1
+                    
+                    local_max = major_count - minor_count
+                    if rem_min_count and 0 > local_max:
+                        local_max = 0
+                        major_count = 0
+                        minor_count = 0
+                    
+                    if local_max > global_max and minor_count:
+                        global_max = local_max
+                
+                max_ans = max(global_max, max_ans)
+                
+        return max_ans
