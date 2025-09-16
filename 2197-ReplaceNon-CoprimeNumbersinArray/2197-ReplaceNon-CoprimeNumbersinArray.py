df1ca64fee2a5c77dea1f4a970b742807e9fd7fe
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-16 06:45:38 +0530

    File updated at 16/9/2025, 6:45:37 am

--- 2197-ReplaceNon-CoprimeNumbersinArray/2197-ReplaceNon-CoprimeNumbersinArray.py
+++ 2197-ReplaceNon-CoprimeNumbersinArray/2197-ReplaceNon-CoprimeNumbersinArray.py
@@ -1,43 +1,12 @@
-# Last updated: 27/8/2025, 10:16:09 pm
-class DLinkedList:
-    def __init__(self, val):
-        self.val = val
-        self.prev = None
-        self.next = None
-
+# Last updated: 16/9/2025, 6:45:37 am
 class Solution:
     def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
-        head = DLinkedList(nums[0])
-        node = head
-        for i in range(1, len(nums)):
-            dl = DLinkedList(nums[i])
-            dl.prev = node
-            node.next = dl
-            node = node.next
+        stack = []
 
-        node = head
-        while node.next:
-            if gcd(node.val, node.next.val) > 1:
-                new_dl = DLinkedList(lcm(node.val, node.next.val))
-                prev = node.prev
-                next = node.next.next
-                new_dl.prev = prev
-                new_dl.next = next
-                if next:
-                    next.prev = new_dl
-                
-                if prev:
-                    prev.next = new_dl
-                    node = prev
-                else:
-                    head = new_dl
-                    node = new_dl
-                continue
-            node = node.next
-
-        ret = []
-        while head:
-            ret.append(head.val)
-            head = head.next
-
-        return ret 
+        for num in nums:
+            stack.append(num)
+            while len(stack) > 1 and gcd(stack[-2], stack[-1]) > 1:
+                y = stack.pop()
+                stack[-1] = lcm(stack[-1], y)
+        
+        return stack
