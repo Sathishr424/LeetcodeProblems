dfa5c6d6f5f54a2717e8ca1e8a30281c740e203b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-16 21:47:45 +0530

    File updated at 16/9/2025, 9:47:43 pm

--- 2312-SellingPiecesofWood/2312-SellingPiecesofWood.py
+++ 2312-SellingPiecesofWood/2312-SellingPiecesofWood.py
@@ -1,12 +1,14 @@
-# Last updated: 16/9/2025, 9:46:33 pm
+# Last updated: 16/9/2025, 9:47:43 pm
 class Solution:
     def sellingWood(self, m: int, n: int, prices: List[List[int]]) -> int:
         there = defaultdict(int)
         for h, w, p in prices:
             there[(h, w)] = p
 
-        @cache
+        dp = [[-1] * (n + 1) for _ in range(m + 1)]
+
         def rec(h, w):
+            if dp[h][w] != -1: return dp[h][w]
             ans = there[(h, w)]
 
             for half_h in range(1, h // 2 + 1):
@@ -15,8 +17,7 @@ class Solution:
             for half_w in range(1, w // 2 + 1):
                 ans = max(ans, rec(h, half_w) + rec(h, w - half_w))
             
+            dp[h][w] = ans
             return ans
 
-        ans = rec(m, n)
-        rec.cache_clear()
-        return ans
+        return rec(m, n)
