55e4f82add79f677173818563adce1cf44f6c325
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-18 16:58:11 +0530

    File updated at 18/9/2025, 4:58:08 pm

--- 2334-SubarrayWithElementsGreaterThanVaryingThreshold/2334-SubarrayWithElementsGreaterThanVaryingThreshold.py
+++ 2334-SubarrayWithElementsGreaterThanVaryingThreshold/2334-SubarrayWithElementsGreaterThanVaryingThreshold.py
@@ -1,4 +1,4 @@
-# Last updated: 18/9/2025, 4:56:43 pm
+# Last updated: 18/9/2025, 4:58:08 pm
 class Solution:
     def validSubarraySize(self, nums: List[int], threshold: int) -> int:
         n = len(nums)
@@ -19,34 +19,21 @@ class Solution:
 
             stack.append(i)
         
-        # print(left)
-        # print(right)
-
-        length = [0] * n
         arr = []
         for i in range(n):
-            length[i] = (i - left[i] + 1) + (right[i] - i)
             arr.append((nums[i], (i - left[i] + 1) + (right[i] - i)))
-        
-        # print(length)
-
         arr.sort(key=lambda x: (x[0], -x[1]))
-        # print(arr)
 
         prefix = [0] * (n + 1)
         maxi = 0
         for i in range(n-1, -1, -1):
             maxi = max(arr[i][1], maxi)
             prefix[i] = maxi
-        
-        # print([a for a, _ in arr])
-        # print(prefix)
 
-        for window in range(1, n + 1):
+        for window in range(n, 0, -1):
             maxi = threshold / window
 
             index = bisect_right(arr, (maxi, inf))
-            # print(window, maxi, index)
 
             if prefix[index] >= window:
                 return window
