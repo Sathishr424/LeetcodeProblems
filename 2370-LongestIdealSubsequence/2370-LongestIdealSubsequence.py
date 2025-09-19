d4d2a525defcdccc60b5e87d4f962f136171deec
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-20 00:56:11 +0530

    Create 2370-LongestIdealSubsequence

---
+++ 2370-LongestIdealSubsequence/2370-LongestIdealSubsequence.py
@@ -0,0 +1,17 @@
+# Last updated: 20/9/2025, 12:56:06 am
+class Solution:
+    def longestIdealString(self, s: str, k: int) -> int:
+        n = len(s)
+        alp = [ord(char) - ord('a') for char in s]
+
+        dp = [[0] * 26 for _ in range(n + 1)]
+        
+        for i in range(1, n + 1):
+            a = alp[i - 1]
+            for j in range(max(0, a - k), min(26, a + k + 1)):
+                dp[i][j] = max(dp[i - 1][a] + 1, dp[i - 1][j])
+
+            for j in range(26):
+                dp[i][j] = max(dp[i][j], dp[i - 1][j])
+
+        return max(dp[-1])
