d83d5610f29dc1fea4d9a8a4f0656e3cabc18a57
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-20 19:01:53 +0530

    Create 2381-ShiftingLettersII

---
+++ 2381-ShiftingLettersII/2381-ShiftingLettersII.py
@@ -0,0 +1,22 @@
+# Last updated: 20/9/2025, 7:01:52 pm
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        n = len(s)
+
+        diff = [0] * (n + 1)
+
+        for start, end, d in shifts:
+            val = 1 if d == 1 else -1
+
+            diff[start] += val
+            diff[end + 1] -= val
+
+        curr = 0
+        final_s = ''
+        for i in range(n):
+            curr += diff[i]
+            a = ord(s[i]) - ord('a')
+            new_a = (a + curr) % 26
+            final_s += chr(new_a + ord('a'))
+
+        return final_s
