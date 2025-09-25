d2524c8d58b55a09345fbefef8971c2aa3dd7a27
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-26 00:32:24 +0530

    File updated at 26/9/2025, 12:32:24 am

--- 2448-MinimumCosttoMakeArrayEqual/2448-MinimumCosttoMakeArrayEqual.py
+++ 2448-MinimumCosttoMakeArrayEqual/2448-MinimumCosttoMakeArrayEqual.py
@@ -1,42 +1,24 @@
-# Last updated: 18/7/2025, 4:41:11 pm
+# Last updated: 26/9/2025, 12:32:24 am
 class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
         n = len(nums)
 
-        new_nums = []
-        for i, num in enumerate(nums):
-            new_nums.append((num, cost[i]))
-        new_nums.sort()
+        def calc(num):
+            c = 0
+            for i in range(n):
+                diff = abs(num - nums[i])
+                c += diff * cost[i]
+            return c
+
+        l = min(nums)
+        r = max(nums)
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if calc(mid) <= calc(mid + 1):
+                r = mid
+            else:
+                l = mid + 1
         
-        prefix = [0]
-        s = new_nums[0][1]
-
-        for i in range(1, n):
-            num, c = new_nums[i]
-
-            diff = num - new_nums[i-1][0]
-            prev_cost = new_nums[i-1][1]
-
-            curr_cost = prev_cost * diff + (s - prev_cost) * diff
-            prefix.append(prefix[-1] + curr_cost)
-
-            s += c
-        
-        right_c = 0
-        s = new_nums[n-1][1]
-        ret = prefix[n-1]
-
-        for i in range(n-2, -1, -1):
-            num, c = new_nums[i]
-
-            diff = new_nums[i+1][0] - num
-            prev_cost = new_nums[i+1][1]
-
-            curr_cost = prev_cost * diff + (s - prev_cost) * diff
-            right_c += curr_cost
-
-            ret = min(ret, prefix[i] + right_c)
-            s += c
-
-        return ret
-        
+        return calc(l)
