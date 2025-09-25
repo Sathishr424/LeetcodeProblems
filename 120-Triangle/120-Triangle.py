9b0c2773bd3524125f51262518581817047fbe60
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-25 12:12:49 +0530

    File updated at 25/9/2025, 12:12:50 pm

--- 120-Triangle/120-Triangle.py
+++ 120-Triangle/120-Triangle.py
@@ -1,13 +1,17 @@
-# Last updated: 12/6/2025, 5:52:49 am
+# Last updated: 25/9/2025, 12:12:50 pm
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        dp = [float('inf')] * (n+1)
-        dp[1] = 0
-        for i in range(1, n+1):
-            tmp = dp[0]
-            for j in range(1, i+1):
-                curr = dp[j]
-                dp[j] = min(dp[j], tmp) + triangle[i-1][j-1]
-                tmp = curr
+
+        dp = [0] * n
+        dp[0] = triangle[0][0]
+        for i in range(1, n):
+            prev = dp[0]
+            dp[0] += triangle[i][0]
+            for j in range(1, i):
+                new_prev = dp[j]
+                dp[j] = min(dp[j] + triangle[i][j], prev + triangle[i][j])
+                prev = new_prev
+            dp[i] = prev + triangle[i][i]
+        
         return min(dp)
