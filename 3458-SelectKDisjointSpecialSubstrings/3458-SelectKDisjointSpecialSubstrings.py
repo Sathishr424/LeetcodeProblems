ad5ecfd8307a0ef955021daa3f6380df82ba5779
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-28 02:22:56 +0530

    File updated at 28/9/2025, 2:22:56 am

--- 3458-SelectKDisjointSpecialSubstrings/3458-SelectKDisjointSpecialSubstrings.py
+++ 3458-SelectKDisjointSpecialSubstrings/3458-SelectKDisjointSpecialSubstrings.py
@@ -1,43 +1,4 @@
-# Last updated: 28/9/2025, 2:22:00 am
-class Node:
-    def __init__(self):
-        self.left = 0
-        self.right = 0
-
-class SegmentTree:
-    def __init__(self, s, indexes):
-        self.s = s
-        self.n = len(s)
-        self.indexes = indexes
-        self.tree = [Node() for _ in range (self.n * 4)]
-
-        self.build(0, self.n-1, 0)
-    
-    def build(self, l, r, index):
-        if l == r:
-            self.tree[index].left = self.indexes[self.s[l]][0]
-            self.tree[index].right = self.indexes[self.s[l]][-1]
-            return self.tree[index]
-        
-        mid = (l + r) // 2
-        left = self.build(l, mid, index * 2 + 1)
-        right = self.build(mid + 1, r, index * 2 + 2)
-
-        self.tree[index].left = min(left.left, right.left)
-        self.tree[index].right = max(left.right, right.right)
-        return self.tree[index]
-    
-    def query(self, l, r, index, left, right):
-        if l > right or r < left:
-            return True
-        
-        if l >= left and r <= right:
-            return self.tree[index].left >= left and self.tree[index].right <= right
-        
-        mid = (l + r) // 2
-
-        return self.query(l, mid, index * 2 + 1, left, right) and self.query(mid + 1, r, index * 2 + 2, left, right)
-
+# Last updated: 28/9/2025, 2:22:56 am
 class Solution:
     def maxSubstringLength(self, s: str, k: int) -> bool:
         if k == 0: return True
@@ -48,7 +9,6 @@ class Solution:
         for i in range(n):
             indexes[s[i]].append(i)
 
-        segTree = SegmentTree(s, indexes)
         used = [0] * n
         for char in sorted(indexes.keys(), key=lambda x: indexes[x][-1] - indexes[x][0]):
             l = indexes[char][0]
