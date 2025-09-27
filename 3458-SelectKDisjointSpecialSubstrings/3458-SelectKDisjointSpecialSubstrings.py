7db6e605605f9386c0380b60a0b43fca91988fa0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-28 02:22:00 +0530

    File updated at 28/9/2025, 2:22:00 am

--- 3458-SelectKDisjointSpecialSubstrings/3458-SelectKDisjointSpecialSubstrings.py
+++ 3458-SelectKDisjointSpecialSubstrings/3458-SelectKDisjointSpecialSubstrings.py
@@ -1,66 +1,69 @@
-# Last updated: 17/5/2025, 3:42:22 pm
-class Solution:
-    def maxSubstringLength(self, st: str, k: int) -> bool:
-        if k == 0: return True
-        n = len(st)
+# Last updated: 28/9/2025, 2:22:00 am
+class Node:
+    def __init__(self):
+        self.left = 0
+        self.right = 0
 
-        alp_to_index = [ord(a) - 97 for a in st]
-        start = [n] * 26
-        end = [-1] * 26
+class SegmentTree:
+    def __init__(self, s, indexes):
+        self.s = s
+        self.n = len(s)
+        self.indexes = indexes
+        self.tree = [Node() for _ in range (self.n * 4)]
 
-        prefix = [[0] * 26 for _ in range(n+1)]
-        
-        for i, char in enumerate(alp_to_index):
-            if start[char] == n:
-                start[char] = i
-            end[char] = i
-
-            for j in range(26):
-                prefix[i+1][j] = prefix[i][j]
-            
-            prefix[i+1][char] += 1
-
-        stack = []
-        for char in range(26):
-            if start[char] == n: continue
-
-            s = start[char]
-            e = end[char]
-
-            for i in range(s+1, e):
-                e = max(e, end[alp_to_index[i]])
-
-            if e - s + 1 == n: continue
-            stack.append((e-s, s, e))
-        
-        stack.sort(key=lambda x: x[0])
-        visited = SortedList()
-
-        for dis, s, e in stack:
-            index = visited.bisect_left(s)
-            if index < len(visited) and visited[index] < e: continue
-
-            if dis <= 1:
-                k -= 1
-                if k == 0: return True
-                visited.add(s)
-                continue
-
-            if e-s+1 == n: continue
-
-            left = prefix[s]
-            right = prefix[e+1]
-
-            match = True
-            for i in range(26):
-                cnt = right[i] - left[i]
-                if cnt > 0 and (left[i] > 0 or prefix[-1][i] > cnt):
-                    match = False
-                    break
-            
-            if match:
-                k -= 1
-                if k == 0: return True
-                visited.add(s)
+        self.build(0, self.n-1, 0)
     
+    def build(self, l, r, index):
+        if l == r:
+            self.tree[index].left = self.indexes[self.s[l]][0]
+            self.tree[index].right = self.indexes[self.s[l]][-1]
+            return self.tree[index]
+        
+        mid = (l + r) // 2
+        left = self.build(l, mid, index * 2 + 1)
+        right = self.build(mid + 1, r, index * 2 + 2)
+
+        self.tree[index].left = min(left.left, right.left)
+        self.tree[index].right = max(left.right, right.right)
+        return self.tree[index]
+    
+    def query(self, l, r, index, left, right):
+        if l > right or r < left:
+            return True
+        
+        if l >= left and r <= right:
+            return self.tree[index].left >= left and self.tree[index].right <= right
+        
+        mid = (l + r) // 2
+
+        return self.query(l, mid, index * 2 + 1, left, right) and self.query(mid + 1, r, index * 2 + 2, left, right)
+
+class Solution:
+    def maxSubstringLength(self, s: str, k: int) -> bool:
+        if k == 0: return True
+        n = len(s)
+
+        indexes = defaultdict(list)
+
+        for i in range(n):
+            indexes[s[i]].append(i)
+
+        segTree = SegmentTree(s, indexes)
+        used = [0] * n
+        for char in sorted(indexes.keys(), key=lambda x: indexes[x][-1] - indexes[x][0]):
+            l = indexes[char][0]
+            r = indexes[char][-1]
+
+            i = l + 1
+            while i < r:
+                if indexes[s[i]][0] < l or used[indexes[s[i]][-1]]: break
+                r = max(r, indexes[s[i]][-1])
+                i += 1
+            else:
+                if r - l + 1 != n:
+                    for i in range(l, r+1):
+                        used[i] = 1
+                    k -= 1
+                    if k == 0: return True
+        
         return False
