67337b8bbc665bdb0def2e0c04dba7329553067b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-27 13:49:11 +0530

    File updated at 27/9/2025, 1:49:10 pm

--- 812-LargestTriangleArea/812-LargestTriangleArea.py
+++ 812-LargestTriangleArea/812-LargestTriangleArea.py
@@ -1,10 +1,7 @@
-# Last updated: 27/9/2025, 1:48:55 pm
+# Last updated: 27/9/2025, 1:49:10 pm
 class Solution:
     def largestTriangleArea(self, points: List[List[int]]) -> float:
         n = len(points)
-
-        def getDis(a, b):
-            return sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
 
         max_area = 0
         for i in range(n):
@@ -14,10 +11,8 @@ class Solution:
                 for k in range(j+1, n):
                     x3, y3 = points[k]
 
-
                     area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
 
                     max_area = max(max_area, area)
             
         return max_area
-                    
