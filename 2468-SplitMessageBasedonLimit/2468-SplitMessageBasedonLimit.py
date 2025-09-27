90a76d1896feba1ccb9ad0f4cbf62b0256354bd9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-27 19:31:01 +0530

    Create 2468-SplitMessageBasedonLimit

---
+++ 2468-SplitMessageBasedonLimit/2468-SplitMessageBasedonLimit.py
@@ -0,0 +1,40 @@
+# Last updated: 27/9/2025, 7:30:59 pm
+class Solution:
+    def splitMessage(self, message: str, limit: int) -> List[str]:
+        n = len(message)
+        def getTheTotal(part):
+            cnt = 3 + len(str(part))
+            rem = n
+            parts = 0
+            for i in range(0, 5):
+                curr = 10 ** i
+                if curr > part: break
+                next = min(part, 10 ** (i + 1) - 1)
+                elements = next - curr + 1
+                new_length = cnt + (i + 1)
+                rem -= (limit - new_length) * elements
+                parts += elements
+                if parts > part: return False
+                if parts == part and next == part and rem <= -(limit - new_length): return False
+                if parts == part and next == part and rem > -(limit - new_length) and rem <= 0: return True
+            
+            return False
+
+        def getTheString(part):
+            m = len(str(part))
+            ret = []
+            index = 0
+            for i in range(1, part + 1):
+                cnt = 3 + m + len(str(i))
+                need = limit - cnt
+                curr = message[index:index+need]
+                index += need
+                ret.append(f"{curr}<{i}/{part}>")
+            return ret
+        
+        for part in range(1, n + 1):
+            if getTheTotal(part):
+                return getTheString(part)
+        
+        return []
+                
