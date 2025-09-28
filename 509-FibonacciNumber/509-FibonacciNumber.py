453ed7e71d28be42ce3c4a6d65e2d13170064667
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-28 21:21:39 +0530

    File updated at 28/9/2025, 9:21:37 pm

--- 509-FibonacciNumber/509-FibonacciNumber.py
+++ 509-FibonacciNumber/509-FibonacciNumber.py
@@ -1,11 +1,38 @@
-# Last updated: 28/9/2025, 9:14:58 pm
+# Last updated: 28/9/2025, 9:21:37 pm
+def multiplyMatrix(x, y):
+    m = len(x)
+    n = len(y[0])
+    ret = [[0] * n for _ in range(m)]
+    
+    for row in range(m):
+        for j in range(m):
+            curr = 0
+            for i in range(m):
+                curr += x[row][i] * y[i][j]
+            ret[row][j] = curr
+    
+    return ret
+
+def matrixPow(matrix, power):
+    if power == 1: return matrix
+
+    half = power // 2
+
+    ans = matrixPow(matrix, half)
+    ans = multiplyMatrix(ans, ans)
+    if power % 2:
+        ans = multiplyMatrix(ans, matrix)
+    
+    return ans
+
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0: return 0
-        dp = [0] * (n + 1)
-        dp[1] = 1
+        if n <= 1: return n
 
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 2] + dp[i - 1]
-        
-        return dp[n]
+        matrix = [[1, 1], [1, 0]]
+
+        matrix = matrixPow(matrix, n-1)
+
+        # [print(row) for row in matrix]
+
+        return matrix[0][0]
