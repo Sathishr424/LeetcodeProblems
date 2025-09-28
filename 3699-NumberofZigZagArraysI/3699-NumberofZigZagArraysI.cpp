8a60a8f0159dcde45e8abb6f14b70e069555f68d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-28 12:32:56 +0530

    File updated at 28/9/2025, 12:32:55 pm

--- 3699-NumberofZigZagArraysI/3699-NumberofZigZagArraysI.cpp
+++ 3699-NumberofZigZagArraysI/3699-NumberofZigZagArraysI.cpp
@@ -1,4 +1,4 @@
-// Last updated: 28/9/2025, 12:17:47 pm
+// Last updated: 28/9/2025, 12:32:55 pm
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,7 +8,6 @@ public:
         const int MOD = 1e9 + 7;
         int m = r - l + 1;
 
-        // dp[i][j][k] = number of ways at index i, value j, state k
         vector<vector<array<long long, 2>>> dp(n + 1, vector<array<long long, 2>>(m, {0, 0}));
 
         long long prev = 0;
@@ -24,26 +23,25 @@ public:
 
         long long cnt = 0;
         for (int i = 0; i < n; i++) {
-            long long new_prev = 0;
+            long long newPrev = 0;
             for (int j = 0; j < m; j++) {
                 for (int k = 0; k < 2; k++) {
-                    dp[i][j][k] %= MOD;
                     if (k == 1) {
-                        cnt += dp[i][j][k];
+                        cnt = (cnt + dp[i][j][k]) % MOD;
                         if (j + 1 < m) {
-                            dp[i + 1][j + 1][0] += cnt;
-                            new_prev += dp[i + 1][j + 1][0];
+                            dp[i + 1][j + 1][0] = cnt;
+                            newPrev = (newPrev + dp[i + 1][j + 1][0]) % MOD;
                         }
                     } else {
-                        prev -= dp[i][j][k];
-                        dp[i + 1][j][1] += prev;
+                        prev = (prev - dp[i][j][k] + MOD) % MOD;
+                        dp[i + 1][j][1] = prev;
                     }
                 }
             }
-            prev = new_prev;
+            prev = newPrev;
             cnt = 0;
         }
 
-        return (dp[n][0][1] + dp[n][m - 1][0]) % MOD;
+        return (int)((dp[n][0][1] + dp[n][m - 1][0]) % MOD);
     }
 };
