9f473eca798a35058f321bd939282bab26378035
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-28 19:16:20 +0530

    Create 3699-NumberofZigZagArraysI

---
+++ 3699-NumberofZigZagArraysI/3699-NumberofZigZagArraysI.py
@@ -0,0 +1,35 @@
+# Last updated: 28/9/2025, 7:16:18 pm
+class Solution:
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        mod = 10**9 + 7
+
+        m = r - l + 1
+        dp = [[0, 0] for _ in range(m)]
+
+        prefix = 0
+        suffix = 0
+        for i in range(m):
+            if i + 1 < m:
+                dp[i][1] = 1
+            if i > 0:
+                suffix += 1
+                dp[i][0] = 1
+
+        for i in range(n-1):
+            next_suffix = 0
+            for j in range(m):
+                suffix = (suffix - dp[j][0]) % mod
+                dp[j][0] = prefix
+                next_suffix = (next_suffix + prefix) % mod
+
+                prefix = (prefix + dp[j][1]) % mod
+                dp[j][1] = suffix
+
+            suffix = next_suffix
+            prefix = 0
+
+        ans = 0
+        for i in range(m):
+            ans += (dp[i][0] + dp[i][1]) % mod
+            ans %= mod
+        return ans
