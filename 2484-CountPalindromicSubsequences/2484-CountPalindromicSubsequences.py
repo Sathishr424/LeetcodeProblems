18e4835523fd92e95a8db3c8b7084fcdb0ff3640
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-09-30 19:55:15 +0530

    File updated at 30/9/2025, 7:55:12 pm

--- 2484-CountPalindromicSubsequences/2484-CountPalindromicSubsequences.py
+++ 2484-CountPalindromicSubsequences/2484-CountPalindromicSubsequences.py
@@ -1,74 +1,62 @@
-# Last updated: 11/5/2025, 4:31:11 pm
-index = 0
-relation = {}
-index_to_relation = [''] * 100
-opp_relation_index = [0] * 100
-
-for i in range(10):
-    val = '0' + str(i)
-    op = val[::-1]
-    relation[val] = index
-    index_to_relation[index] = val
-    
-    index += 1
-
-for i in range(10, 100):
-    val = str(i)
-    op = val[::-1]
-    
-    relation[str(i)] = index
-    index_to_relation[index] = str(i)
-    index += 1
-
-for num in relation:
-    opp_relation_index[relation[num]] = relation[num[::-1]]
-
-mod = 10**9 + 7
-
-pre = [[''] * 10 for _ in range(10)]
-
-for i in range(10):
-    for j in range(10):
-        pre[i][j] = str(i) + str(j)
-
+# Last updated: 30/9/2025, 7:55:12 pm
 class Solution:
     def countPalindromes(self, s: str) -> int:
+        mod = 10**9 + 7
+        
         n = len(s)
-        ret = 0
-        freq = [0] * 10
-        arr = [int(i) for i in s]
+        s = [int(a) for a in s]
+        prefix_reverse = [[0] * 10 for _ in range(n + 1)]
+        prefix = [[0] * 10 for _ in range(n + 1)]
 
-        dp = [[0] * 100]
-        
-        freq[arr[0]] += 1
+        for i in range(n-1, -1, -1):
+            a = s[i]
+            for j in range(10):
+                prefix_reverse[i][j] += prefix_reverse[i + 1][j]
+            prefix_reverse[i][a] += 1
+
+        prefix[0][s[i]] += 1
         for i in range(1, n):
-            dp.append(dp[-1] + [])
-            val = arr[i]
-            for num in range(10):
-                if freq[num] > 0:
-                    y = pre[num][val]
-                    dp[i][relation[y]] += freq[num]
+            a = s[i]
+            for j in range(10):
+                prefix[i][j] += prefix[i - 1][j]
+            prefix[i][a] += 1
 
-            freq[val] += 1
+        dp = [[-1] * 100 for _ in range(n)]
+        dp_reverse = [[-1] * 100 for _ in range(n)]
 
-        freq = [0] * 10
-        right_dp = [0] * 100
+        def recReverse(index, a, b):
+            if index == 0: return 0
+            if dp_reverse[index][a * 10 + b] != -1:
+                return dp_reverse[index][a * 10 + b]
 
-        for i in range(n-1, 1, -1):
-            
-            if i < n-2:
-                for index in range(100):
-                    left = dp[i-1][opp_relation_index[index]]
-                    right = right_dp[index]
-                    if left and right:
-                        ret = (ret + left * right % mod) % mod
+            ans = recReverse(index - 1, a, b)
+            if s[index] == b:
+                ans += prefix[index - 1][a]
 
-            val = arr[i]
-            for num in range(10):
-                if freq[num] > 0:
-                    y = pre[val][num]
-                    right_dp[relation[y]] += freq[num]
-        
-            freq[val] += 1
-        
-        return ret % mod
+            ans %= mod
+            dp_reverse[index][a * 10 + b] = ans
+            return ans
+
+        def rec(index, a, b):
+            if index == n - 1: return 0
+            if dp[index][a * 10 + b] != -1:
+                return dp[index][a * 10 + b]
+
+            ans = rec(index + 1, a, b)
+            if s[index] == a:
+                ans += prefix_reverse[index + 1][b]
+
+            ans %= mod
+            dp[index][a * 10 + b] = ans
+            return ans
+
+        ans = 0
+        for index in range(2, n-3+1):
+            for i in range(10):
+                for j in range(10):
+                    left = recReverse(index - 1, i, j)
+                    right = rec(index + 1, j, i)
+                    ans += (left * right) % mod
+                    ans %= mod
+
+        return ans
