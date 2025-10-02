34b9ca9cec7a6dd34083cba6a44b87e8a8e0caa4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-02 20:41:08 +0530

    File updated at 2/10/2025, 8:41:07 pm

--- 3116-KthSmallestAmountWithSingleDenominationCombination/3116-KthSmallestAmountWithSingleDenominationCombination.py
+++ 3116-KthSmallestAmountWithSingleDenominationCombination/3116-KthSmallestAmountWithSingleDenominationCombination.py
@@ -1,28 +1,29 @@
-# Last updated: 2/10/2025, 8:39:56 pm
+# Last updated: 2/10/2025, 8:41:07 pm
 class Solution:
     def findKthSmallest(self, coins: List[int], k: int) -> int:
         n = len(coins)
-        coins.sort()
 
         def isGood(mid):
             total = 0
-            for mask in range(1, 1 << n):
-                bits = mask.bit_count()
-
-                LCM = 1
-                for i in range(n):
-                    if mask & (1 << i):
-                        LCM = lcm(LCM, coins[i])
-                
-                if bits % 2:
-                    total += mid // LCM
-                else:
-                    total -= mid // LCM
             
-            return total >= k
+            def rec(index, cnt, l):
+                nonlocal total
+                if index == n:
+                    if cnt == 0: return
+                    if cnt % 2:
+                        total += mid // l
+                    else:
+                        total -= mid // l
+                    return
+                
+                rec(index + 1, cnt + 1, lcm(l, coins[index]))
+                rec(index + 1, cnt, l)
 
+            rec(0, 0, 1)
+            return total >= k    
+            
         l = 1
-        r = coins[0] * k
+        r = coins[-1] * k
 
         while l < r:
             mid = (l + r) // 2
