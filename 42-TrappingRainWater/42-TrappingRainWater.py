3c92fb47122d17ebbed8dec79b90b35913432878
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-03 07:54:43 +0530

    File updated at 3/10/2025, 7:54:43 am

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,17 +1,25 @@
-# Last updated: 12/6/2025, 5:54:36 am
+# Last updated: 3/10/2025, 7:54:43 am
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l = 0
-        r = len(height)-1
-        diff = min( height[l],  height[r] )
-        trap = 0
-        while l < r:
-            if height[l] < height[r]:
-                trap += diff - height[l]
-                l += 1
-            else:
-                trap += diff - height[r]
-                r -= 1
-            diff = max(diff, min(height[l],  height[r]))
+        n = len(height)
 
-        return trap
+        stack = []
+        area = 0
+        for i in range(n):
+            prev = i
+            curr = 0
+            while stack and stack[-1][1] < height[i]:
+                index, h = stack.pop()
+                dis = prev - index
+                curr += (height[i] - h) * dis
+                prev = index
+
+            if len(stack) == 0 and prev != i:
+                dis = i - prev
+                curr -= (height[i] - height[prev]) * dis
+                prev = i
+            
+            area += curr
+            stack.append((prev, height[i]))
+        
+        return area
