5d386d598eb888242b498b399659adec80cdeba9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-04 22:39:58 +0530

    File updated at 4/10/2025, 10:39:59 pm

--- 2508-AddEdgestoMakeDegreesofAllNodesEven/2508-AddEdgestoMakeDegreesofAllNodesEven.py
+++ 2508-AddEdgestoMakeDegreesofAllNodesEven/2508-AddEdgestoMakeDegreesofAllNodesEven.py
@@ -1,9 +1,9 @@
-# Last updated: 4/10/2025, 10:37:17 pm
+# Last updated: 4/10/2025, 10:39:59 pm
 class Solution:
     def isPossible(self, n: int, edges: List[List[int]]) -> bool:
         out_degree = [0] * n
         graph = defaultdict(dict)
-        out = 0
+        out = []
 
         for x, y in edges:
             x -= 1
@@ -15,33 +15,24 @@ class Solution:
         
         for i in range(n):
             if out_degree[i] % 2:
-                out += 1
+                out.append(i)
         
-        def connect(x, y, degree):
+        def connect(x, y):
             if x not in graph[y]: 
                 return 1
             for i in range(n):
-                if degree[i] % 2 == 0 and i not in graph[x] and i not in graph[y]:
+                if out_degree[i] % 2 == 0 and i not in graph[x] and i not in graph[y]:
                     return 2
             return 5
         
-        if out == 0: return True
-        if out % 2 or out > 4: return False
-        if out == 2:
-            nodes = []
-            for i in range(n):
-                if out_degree[i] % 2:
-                    nodes.append(i)
-            x, y = nodes
-            return connect(x, y, out_degree) <= 2
+        if len(out) == 0: return True
+        if len(out) % 2 or len(out) > 4: return False
+        if len(out) == 2:
+            return connect(*out) <= 2
         else:
-            nodes = []
-            for i in range(n):
-                if out_degree[i] % 2:
-                    nodes.append(i)
             for i in range(4):
                 for j in range(i+1, 4):
                     k, l = [m for m in range(4) if m != i and m != j]
-                    if connect(nodes[i], nodes[j], out_degree) + connect(nodes[k], nodes[l], out_degree) <= 2:
+                    if connect(out[i], out[j]) + connect(out[k], out[l]) <= 2:
                         return True
             return False
