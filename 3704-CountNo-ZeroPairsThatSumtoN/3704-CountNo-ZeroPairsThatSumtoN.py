411252030b694170dfe8ef1840ff1997ee95ab31
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-05 22:05:42 +0530

    Create 3704-CountNo-ZeroPairsThatSumtoN

---
+++ 3704-CountNo-ZeroPairsThatSumtoN/3704-CountNo-ZeroPairsThatSumtoN.py
@@ -0,0 +1,62 @@
+# Last updated: 5/10/2025, 10:05:41 pm
+class Solution:
+    def countNoZeroPairs(self, n: int) -> int:
+        nums = [int(num) for num in str(n)]
+        m = len(nums)
+        # print(nums, m)
+        
+        zero = [0] * m
+        z = 0
+        for i in range(m):
+            if nums[i] == 0:
+                z = 1
+            zero[i] = z
+        valid = []
+
+        def check(index, carry):
+            # print(index, carry, tmp)
+            for j in range(index, -1, -1):
+                a = nums[j] - carry
+                if a < 0:
+                    if j == 0: return 0
+                    a = nums[j] + 10 - carry
+                    carry = 1
+                else:
+                    carry = 0
+                if a == 0 and j > 0: return 0
+            return 1
+        
+        @cache
+        def rec(index, carry, last_zero):
+            if index == -1:
+                if carry == 0:
+                    return 1
+                return 0
+            a = nums[index] - carry
+            if last_zero:
+                return check(index, carry)
+
+            ans = 0
+            if index != m-1 and check(index, carry):
+                ans += 1
+            
+            for b in range(1, 10):
+                a = nums[index] - carry
+                new_carry = 0
+                if a < b:
+                    if index == 0: break
+                    a = nums[index] + 10 - carry
+                    new_carry = 1
+                
+                if a == b:
+                    if index != m-1:
+                        ans += rec(index - 1, new_carry, 1)
+                else:
+                    ans += rec(index - 1, new_carry, 0)
+            
+            return ans
+
+        ans = rec(m-1, 0, 0)
+        rec.cache_clear()
+        return ans
+        
