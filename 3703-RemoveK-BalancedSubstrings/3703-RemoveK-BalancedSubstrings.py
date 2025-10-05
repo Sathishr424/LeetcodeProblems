c3167656b13524f29a68ecb15304a8e7d8d2a2b6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-05 11:30:50 +0530

    File updated at 5/10/2025, 11:30:49 am

--- 3703-RemoveK-BalancedSubstrings/3703-RemoveK-BalancedSubstrings.py
+++ 3703-RemoveK-BalancedSubstrings/3703-RemoveK-BalancedSubstrings.py
@@ -1,23 +1,21 @@
-# Last updated: 5/10/2025, 11:27:10 am
+# Last updated: 5/10/2025, 11:30:49 am
 class Solution:
     def removeSubstring(self, s: str, k: int) -> str:
+        # rel = ['(', ')']
+        # s = [rel[random.randrange(2)] for _ in range(10 ** 5)]
         n = len(s)
         stack = []
-
-        open = 0
         close = 0
         for i in range(n):
             if s[i] == '(':
                 if close:
                     stack.append([')', close])
                     close = 0
-                open += 1
                 if stack and stack[-1][0] == '(':
                     stack[-1][1] += 1
                 else:
                     stack.append(['(', 1])
             else:
-                open = 0
                 close += 1
                 if close == k and stack and stack[-1][0] == '(' and stack[-1][1] >= k:
                     stack[-1][1] -= k
@@ -29,7 +27,7 @@ class Solution:
                             close = 0
                     else:
                         close = 0
-            # print(stack, open, close, s[i])
+
         if close:
             stack.append([')', close])
         
