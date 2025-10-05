9a662e4d18ab9a69dc05d9022aae2ff051e387dd
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-05 10:48:40 +0530

    File updated at 5/10/2025, 10:48:39 am

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,4 +1,4 @@
-# Last updated: 5/10/2025, 10:46:56 am
+# Last updated: 5/10/2025, 10:48:39 am
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         m = len(heights)
@@ -8,15 +8,15 @@ class Solution:
         visited = [[0] * n for _ in range(m)]
         stack = deque([])
         for j in range(n):
-            visited[0][j] |= 1 << 0
-            visited[m-1][j] |= 1 << 1
-            stack.append([0, j, 0])
-            stack.append([m-1, j, 1])
+            visited[0][j] |= 1
+            visited[m-1][j] |= 2
+            stack.append([0, j, 1])
+            stack.append([m-1, j, 2])
         for i in range(m):
-            visited[i][0] |= 1 << 0
-            visited[i][n-1] |= 1 << 1
-            stack.append([i, 0, 0])
-            stack.append([i, n-1, 1])
+            visited[i][0] |= 1
+            visited[i][n-1] |= 2
+            stack.append([i, 0, 1])
+            stack.append([i, n-1, 2])
         
         while stack:
             i, j, ocean = stack.popleft()
@@ -25,9 +25,9 @@ class Solution:
                 i2 += i
                 j2 += j
                 if 0 <= i2 < m and 0 <= j2 < n and heights[i2][j2] >= heights[i][j]:
-                    if visited[i2][j2] & (1 << ocean) == 0:
+                    if visited[i2][j2] & ocean == 0:
                         stack.append([i2, j2, ocean])
-                        visited[i2][j2] |= (1 << ocean)
+                        visited[i2][j2] |= ocean
 
         ret = []
         for i in range(m):
