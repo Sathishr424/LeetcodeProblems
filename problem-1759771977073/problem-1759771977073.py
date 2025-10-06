6a38d84a7f86478edcd61ef617ad89fc9ce25389
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-06 23:02:59 +0530

    Create

---
+++ problem-1759771977073/problem-1759771977073.py
@@ -0,0 +1,56 @@
+# Last updated: 6/10/2025, 11:02:57 pm
+class Solution:
+    def maxPower(self, stations: List[int], r: int, k: int) -> int:
+        n = len(stations)
+        powers = [0] * n
+        left = 0
+        power = 0
+        for i in range(n):
+            if i - left > r:
+                power -= stations[left]
+                left += 1
+            power += stations[i]
+            powers[i] += power
+
+        power = 0
+        right = n-1
+        for i in range(n-1, -1, -1):
+            if right - i > r:
+                power -= stations[right]
+                right -= 1
+            powers[i] += power
+            power += stations[i]
+
+        def isGood(mid):
+            need = [0] * n
+            for i in range(n):
+                need[i] = max(0, mid - powers[i])
+            line = [0] * n
+            curr = 0
+            rem = k
+            for i in range(n):
+                curr += line[i]
+                if need[i] > curr:
+                    want = need[i] - curr
+                    if want > rem: return False
+                        
+                    index = i + (r * 2) + 1
+                    if index < n:
+                        line[index] -= want
+                    
+                    rem -= want
+                    curr += want
+            return True
+
+        left = 0
+        right = max(powers) + k
+
+        while left < right:
+            mid = (left + right + 1) // 2
+
+            if isGood(mid):
+                left = mid
+            else:
+                right = mid - 1
+
+        return left
