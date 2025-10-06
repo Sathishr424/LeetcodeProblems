fdf7612c231644b5862cedd29312f04284b743f0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-06 19:11:02 +0530

    File updated at 6/10/2025, 7:10:59 pm

--- 1012-NumbersWithRepeatedDigits/1012-NumbersWithRepeatedDigits.py
+++ 1012-NumbersWithRepeatedDigits/1012-NumbersWithRepeatedDigits.py
@@ -1,37 +1,25 @@
-# Last updated: 6/10/2025, 6:58:29 pm
+# Last updated: 6/10/2025, 7:10:59 pm
 class Solution:
     def numDupDigitsAtMostN(self, n: int) -> int:
         nums = [int(d) for d in str(n)]
         m = len(nums)
 
         @cache
-        def rec(index, is_small, strict, mask, done):
-            if is_small and index == m-1:
-                if done:  return 1
+        def rec(pos, strict, leading, mask, done):
+            if pos == m:
+                if done and not leading:
+                    return 1
                 return 0
             
-            if index == m:
-                if done: return 1
-                return 0
-            
-            ans = 1 if done else 0
-            if strict:
-                for i in range(10):
-                    new_done = done or mask & (1 << i) > 0
-                    if i <= nums[index]:
-                        ans += rec(index + 1, is_small, i == nums[index], mask | (1 << i), new_done)
-                    elif index < m-1:
-                        ans += rec(index + 1, 1, 0, mask | (1 << i), new_done)
-            else:
-                for i in range(10):
-                    new_done = done or mask & (1 << i) > 0
-                    ans += rec(index + 1, is_small, 0, mask | (1 << i), new_done)
-            
+            ans = 0
+            limit = nums[pos] if strict else 9
+            for i in range(0, limit + 1):
+                new_done = done or (not leading and mask & (1 << i) > 0)
+                new_mask = mask if leading and i == 0 else mask | (1 << i)
+
+                ans += rec(pos + 1, strict and i == nums[pos], leading and i == 0, new_mask, new_done)
             return ans
         
-        ans = 0
-        for i in range(1, 10):
-            ans += rec(1, i > nums[0], i == nums[0], 1 << i, 0)
-
+        ans = rec(0, 1, 1, 0, 0)
         rec.cache_clear()
         return ans
