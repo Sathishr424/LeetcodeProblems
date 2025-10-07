e35e6279fc62f3b7ddfcffbb9c357d1e97f54987
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-07 05:48:04 +0530

    Create

---
+++ problem-1759796281596/problem-1759796281596.py
@@ -0,0 +1,32 @@
+# Last updated: 7/10/2025, 5:48:01 am
+class Solution:
+    def avoidFlood(self, rains: List[int]) -> List[int]:
+        n = len(rains)
+
+        nearest = defaultdict(list)
+        for i in range(n):
+            nearest[rains[i]].append(i)
+        
+        ans = [1] * n
+        floods = {}
+        heap = []
+        for i in range(n):
+            lake = rains[i]
+            # print(lake, floods, heap)
+            if lake > 0:
+                if lake in floods: return []
+                floods[lake] = i
+                index = bisect_left(nearest[lake], i + 1)
+                if index != len(nearest[lake]):
+                    heapq.heappush(heap, (nearest[lake][index], lake))
+                ans[i] = -1
+            else:
+                while heap and heap[0][0] < i:
+                    heapq.heappop(heap)
+                
+                if heap:
+                    _, lake_dry = heapq.heappop(heap)
+                    del floods[lake_dry]
+                    ans[i] = lake_dry
+        
+        return ans
