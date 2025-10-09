d4c8de77bf156fd22da7859cdd3ced21b5274c53
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-09 20:36:05 +0530

    Create 2532-TimetoCrossaBridge

---
+++ 2532-TimetoCrossaBridge/2532-TimetoCrossaBridge.py
@@ -0,0 +1,40 @@
+# Last updated: 9/10/2025, 8:36:04 pm
+class Solution:
+    def findCrossingTime(self, n: int, k: int, workers: List[List[int]]) -> int:
+        # 5, 4, 3
+        # l -> r + 2
+
+        left = []
+        right = []
+        picked = []
+        leftFree = []
+        for i, (r, pick, l, put) in enumerate(workers):
+            heapq.heappush(leftFree, (-(l + r), -i))
+
+        elapsed = 0
+        while n or right or picked:
+            while right and right[0][0] <= elapsed:
+                t, c, index = heapq.heappop(right)
+                heapq.heappush(picked, (c, index))
+            while left and left[0][0] <= elapsed:
+                t, c, index = heapq.heappop(left)
+                heapq.heappush(leftFree, (c, index))
+            
+            # print(n, left, right, picked, leftFree, elapsed)
+            if picked:
+                c, index = heapq.heappop(picked)
+                r, pick, l, put = workers[-index]
+                t = elapsed
+                heapq.heappush(left, (t + l + put, c, index))
+                elapsed += l
+            elif n and leftFree:
+                c, index = heapq.heappop(leftFree)
+                r, pick, l, put = workers[-index]
+                t = elapsed
+                heapq.heappush(right, (t + r + pick, c, index))
+                elapsed += r
+                n -= 1
+            else:
+                elapsed += 1
+                
+        return elapsed
