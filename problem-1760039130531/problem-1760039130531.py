80de4c05c38f4bc6d6ea474cbf76d84890137f98
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-10 01:15:32 +0530

    Create

---
+++ problem-1760039130531/problem-1760039130531.py
@@ -0,0 +1,19 @@
+# Last updated: 10/10/2025, 1:15:30 am
+class Solution:
+    def minOperations(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        if k == 0:
+            if nums1 == nums2: return 0
+            return -1
+        n = len(nums1)
+
+        diff = 0
+        op = 0
+        for i in range(n):
+            d = nums2[i] - nums1[i]
+            if d % k != 0: return -1
+            if d > 0: op += d // k
+            diff += d // k
+
+        if diff == 0: return op
+        return -1
+            
