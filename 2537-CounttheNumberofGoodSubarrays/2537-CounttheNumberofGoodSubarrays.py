49fbab9f03f5c37a71693954acdee85b5646020f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-10 00:40:46 +0530

    File updated at 10/10/2025, 12:40:44 am

--- 2537-CounttheNumberofGoodSubarrays/2537-CounttheNumberofGoodSubarrays.py
+++ 2537-CounttheNumberofGoodSubarrays/2537-CounttheNumberofGoodSubarrays.py
@@ -1,4 +1,4 @@
-# Last updated: 10/10/2025, 12:38:53 am
+# Last updated: 10/10/2025, 12:40:44 am
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
         n = len(nums)
@@ -9,15 +9,14 @@ class Solution:
         left = 0
         count = 0
         for i in range(n):
-            num = nums[i]
-            freq[num] += 1
-            pairs += freq[num] - 1
+            pairs += freq[nums[i]]
+            freq[nums[i]] += 1
             while pairs >= k:
                 count += 1
                 freq[nums[left]] -= 1
                 pairs -= freq[nums[left]]
                 left += 1
             
-            # print(i, count, pairs, freq)
             ret += count
+        
         return ret
