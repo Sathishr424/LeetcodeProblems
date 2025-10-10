5c91d8ccf38e033c5babeaba604c262c7db81141
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-10 13:30:48 +0530

    File updated at 10/10/2025, 1:30:46 pm

--- 3147-TakingMaximumEnergyFromtheMysticDungeon/3147-TakingMaximumEnergyFromtheMysticDungeon.py
+++ 3147-TakingMaximumEnergyFromtheMysticDungeon/3147-TakingMaximumEnergyFromtheMysticDungeon.py
@@ -1,18 +1,13 @@
-# Last updated: 10/10/2025, 1:28:10 pm
+# Last updated: 10/10/2025, 1:30:46 pm
 class Solution:
     def maximumEnergy(self, energy: List[int], k: int) -> int:
         n = len(energy)
         
-        cache = [-inf] * n
-        def rec(index):
-            if index >= n: return 0
-            if cache[index] != -inf: return cache[index]
-            
-            cache[index] = rec(index + k) + energy[index]
-            return cache[index]
-        
         best = -inf
-        for i in range(n):
-            best = max(best, rec(i))
+        for i in range(n-1, n-k-1, -1):
+            s = 0
+            for j in range(i, -1, -k):
+                s += energy[j]
+                best = max(s, best)
         
         return best
