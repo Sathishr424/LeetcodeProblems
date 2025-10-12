e0db95b3949cfeeb65d725ac40b54fdb545eab48
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-12 08:05:47 +0530

    Create

---
+++ problem-1760236543212/problem-1760236543212.py
@@ -0,0 +1,25 @@
+# Last updated: 12/10/2025, 8:05:43 am
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        n = len(s)
+
+        def isBalanced(freq):
+            prev = -1
+            for char in freq:
+                cnt = freq[char]
+                if prev != -1 and cnt != prev: return False
+                prev = cnt
+            
+            return True
+
+        best = 1
+        for i in range(n):
+            cnt = 0
+            freq = defaultdict(int)
+            for j in range(i, n):
+                freq[s[j]] += 1
+                if isBalanced(freq):
+                    best = max(best, j - i + 1)
+
+        return best
+                    
