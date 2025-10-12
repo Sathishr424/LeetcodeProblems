e876056232cbe7c6524ee3ac3303f9625ddd0613
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-12 22:36:57 +0530

    File updated at 12/10/2025, 10:36:53 pm

--- 3709-DesignExamScoresTracker/3709-DesignExamScoresTracker.py
+++ 3709-DesignExamScoresTracker/3709-DesignExamScoresTracker.py
@@ -1,18 +1,66 @@
-# Last updated: 11/10/2025, 10:38:12 pm
+# Last updated: 12/10/2025, 10:36:53 pm
+class Node:
+    def __init__(self, l, r):
+        self.score = 0
+        self.left = None
+        self.right = None
+        self.l = l
+        self.r = r
+        # self.lazy = -1
+
+class SegmentTree:
+    def __init__(self, n):
+        self.node = Node(1, n)
+    
+    def processLazy(self, node, l, mid, r):
+        if node.left == None:
+            node.left = Node(l, mid)
+        if node.right == None:
+            node.right = Node(mid +1, r)
+        
+        # if node.lazy != -1:
+        #     node.left.score += node.lazy
+        #     node.right.score += node.lazy
+        #     node.left.lazy = node.lazy
+        #     node.right.lazy = node.lazy
+        #     node.lazy = -1
+        
+    def update(self, node, l, r, index, score):
+        if l == r:
+            node.score = score
+            return
+        
+        mid = (l + r) // 2
+        self.processLazy(node, l, mid, r)
+        if index <= mid:
+            self.update(node.left, l, mid, index, score)
+        else:
+            self.update(node.right, mid + 1, r, index, score)
+        node.score = node.left.score + node.right.score
+    
+    def query(self, node, l, r, left, right):
+        if l > right or r < left:
+            return 0
+
+        if l >= left and r <= right:
+            return node.score
+
+        mid = (l + r) // 2
+        self.processLazy(node, l, mid, r)
+
+        return self.query(node.left, l, mid, left, right) + self.query(node.right, mid + 1, r, left, right)
+
 class ExamTracker:
     def __init__(self):
-        self.times = []
-        self.prefix = [0]
+        self.n = 10 ** 9
+        self.segTree = SegmentTree(self.n)
 
     def record(self, time: int, score: int) -> None:
-        self.times.append(time)
-        self.prefix.append(self.prefix[-1] + score)
+        return self.segTree.update(self.segTree.node, 1, self.n, time, score)
 
     def totalScore(self, startTime: int, endTime: int) -> int:
-        left = bisect_left(self.times, startTime)
-        right = bisect_right(self.times, endTime)
+        return self.segTree.query(self.segTree.node, 1, self.n, startTime, endTime)
 
-        return self.prefix[right] - self.prefix[left]
 
 # Your ExamTracker object will be instantiated and called as such:
 # obj = ExamTracker()
