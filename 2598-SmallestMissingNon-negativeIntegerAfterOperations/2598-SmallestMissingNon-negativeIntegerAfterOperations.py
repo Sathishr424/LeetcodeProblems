448bc9ba6524f4bcd6a20a5cb1e1da5c39d0ac0a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-16 16:24:47 +0530

    File updated at 16/10/2025, 4:24:45 pm

--- 2598-SmallestMissingNon-negativeIntegerAfterOperations/2598-SmallestMissingNon-negativeIntegerAfterOperations.py
+++ 2598-SmallestMissingNon-negativeIntegerAfterOperations/2598-SmallestMissingNon-negativeIntegerAfterOperations.py
@@ -1,36 +1,15 @@
-# Last updated: 16/10/2025, 4:04:02 pm
+# Last updated: 16/10/2025, 4:24:45 pm
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
         n = len(nums)
-        if value == 1: return n
-        N = 10**9
-        for i in range(n):
-            l = -N
-            r = N
-            num = nums[i]
+        counter = [0 for _ in range(value)]
 
-            while l < r:
-                mid = (l + r) // 2
-
-                if num + (value * mid) >= value:
-                    r = mid
-                else:
-                    l = mid + 1
-            nums[i] += (value * (l - 1))
-        nums.sort()
-
-        # print(nums)
-        heap = []
         for num in nums:
-            heapq.heappush(heap, num)
+            counter[num % value] += 1
         
-        prev = heapq.heappop(heap)
-        if prev != 0: return 0
-        while heap:
-            num = heapq.heappop(heap)
-            if num == prev:
-                heapq.heappush(heap, num + value)
-            elif num != prev + 1: return prev + 1
-            prev = num
-        
-        return n
+        current = 0
+        while counter[current % value] > 0:
+            counter[current % value] -= 1
+            current += 1
+    
+        return current
