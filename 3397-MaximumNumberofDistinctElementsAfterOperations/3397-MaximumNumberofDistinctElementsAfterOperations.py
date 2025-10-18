59fc5711bfb215021c8834c5b12414ebc7ce0399
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-18 14:17:47 +0530

    File updated at 18/10/2025, 2:17:47 pm

--- 3397-MaximumNumberofDistinctElementsAfterOperations/3397-MaximumNumberofDistinctElementsAfterOperations.py
+++ 3397-MaximumNumberofDistinctElementsAfterOperations/3397-MaximumNumberofDistinctElementsAfterOperations.py
@@ -1,26 +1,17 @@
-# Last updated: 18/10/2025, 2:09:03 pm
+# Last updated: 18/10/2025, 2:17:47 pm
 class Solution:
     def maxDistinctElements(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        if k * 2 >= n: return n
 
-        k_range = [i for i in range(-k, k+1)]
-        used = {}
         nums.sort()
+        arr = []
+        arr.append(nums[0] - k)
 
-        for num in nums:
-            l = 0
-            r = len(k_range) - 1
-
-            while l < r:
-                mid = (l + r) // 2
-
-                if (num + k_range[mid]) not in used:
-                    r = mid
-                else:
-                    l = mid + 1
-            
-            if (num + k_range[l]) not in used:
-                used[(num + k_range[l])] = 1
-
-        return len(used)
+        for i in range(1, n):
+            if nums[i] - k <= arr[-1]:
+                num = min(nums[i] + k, arr[-1] + 1)
+            else:
+                num = nums[i] - k
+            arr.append(num)
+        
+        return len(set(arr))
