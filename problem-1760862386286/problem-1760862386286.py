75a7cd93de6c66665fbaef99507ad3d301ca399c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-19 13:56:27 +0530

    Create

---
+++ problem-1760862386286/problem-1760862386286.py
@@ -0,0 +1,28 @@
+# Last updated: 19/10/2025, 1:56:26 pm
+class Solution:
+    def lexGreaterPermutation(self, s: str, target: str) -> str:
+        n = len(s)
+
+        arr = sorted(list(s))
+        stack = deque([('', 0, arr, False)])
+
+        while stack:
+            st, st_index, arr, larger = stack.popleft()
+            # print(st, st_index, arr)
+            if st_index == n:
+                if larger: return st
+                continue
+            if larger:
+                stack.append((st + arr[0], st_index + 1, arr[1:], larger))
+                continue
+            index = bisect_left(arr, target[st_index])
+            if index < len(arr):
+                if arr[index] == target[st_index]:
+                    stack.append((st + arr[index], st_index + 1, arr[:index] + arr[index+1:], False))
+                    index = bisect_right(arr, target[st_index])
+                    if index < len(arr):
+                        stack.append((st + arr[index], st_index + 1, arr[:index] + arr[index+1:], True))
+                elif arr[index] > target[st_index]:
+                    stack.append((st + arr[index], st_index + 1, arr[:index] + arr[index+1:], True))
+
+        return ''
