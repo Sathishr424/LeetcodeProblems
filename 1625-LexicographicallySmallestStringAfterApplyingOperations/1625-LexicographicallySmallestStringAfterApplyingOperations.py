405158843d5d14c9fcb54d74e97e623cca36326f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-19 21:51:06 +0530

    File updated at 19/10/2025, 9:51:06 pm

--- 1625-LexicographicallySmallestStringAfterApplyingOperations/1625-LexicographicallySmallestStringAfterApplyingOperations.py
+++ 1625-LexicographicallySmallestStringAfterApplyingOperations/1625-LexicographicallySmallestStringAfterApplyingOperations.py
@@ -1,4 +1,4 @@
-# Last updated: 19/10/2025, 9:46:19 pm
+# Last updated: 19/10/2025, 9:51:06 pm
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
         n = len(s)
@@ -9,7 +9,6 @@ class Solution:
 
         while stack:
             new_s = stack.pop()
-            # print(new_s)
             if new_s in visited: continue
             
             left = new_s[:n-b]
@@ -19,16 +18,6 @@ class Solution:
             visited[new_s] = 1
             best = min(best, new_s)
 
-            curr = [int(d) for d in new_s]
-            seen = {}
-            num = curr[1]
-            while num not in seen:
-                seen[num] = 1
-                num = (num + a) % 10
-
-                for i in range(1, n, 2):
-                    curr[i] = (curr[i] + a) % 10
-                
-                stack.append(''.join([str(num) for num in curr]))
-        
+            stack.append(''.join([str((int(num) + a) % 10) if i % 2 else num for i, num in enumerate(new_s)]))
+    
         return best
