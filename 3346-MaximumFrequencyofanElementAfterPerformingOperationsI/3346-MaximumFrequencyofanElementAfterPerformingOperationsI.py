e2066f8829211e5ed3c42d2cbc26ca48f617a78e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-21 13:33:54 +0530

    File updated at 21/10/2025, 1:33:51 pm

--- 3346-MaximumFrequencyofanElementAfterPerformingOperationsI/3346-MaximumFrequencyofanElementAfterPerformingOperationsI.py
+++ 3346-MaximumFrequencyofanElementAfterPerformingOperationsI/3346-MaximumFrequencyofanElementAfterPerformingOperationsI.py
@@ -1,47 +1,31 @@
-# Last updated: 21/10/2025, 1:27:08 pm
+# Last updated: 21/10/2025, 1:33:51 pm
 class Solution:
-    def maxFrequency(self, nums: List[int], k: int, op: int) -> int:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
         n = len(nums)
-
         nums.sort()
-        # print(nums)
-        ret = 1
-        freq = defaultdict(int)
 
-        i = 0
-        while i < n:
-            freq[nums[i]] += 1
-            j = i + 1
-            while j < n and nums[j] == nums[i]:
-                j += 1
-            
-            dup = j - i - 1
-            right = bisect_left(nums, nums[i] + k + 1)
+        cnts = [0] * n
+        cnt = 1
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                cnt += 1
+            else:
+                cnt = 1
+            cnts[i-cnt+1] = cnt
+
+        best = 1
+        for i in range(n):
+            if i > 0 and nums[i-1] == nums[i]: continue
+
             left = bisect_left(nums, nums[i] - k)
+            right = bisect_right(nums, nums[i] + k)
 
-            left = min(op, i - left)
-            right = min(op + dup, right - i - 1)
-            ret = max(ret, left + 1, right + 1, min(left + right + 1, op + dup + 1))
-
-            i = j
+            best = max(best, min(right - left, numOperations + cnts[i]))
         
-        if op < 2: return ret
-        op -= 1
-        i = 0
-        while i < n:
-            num = nums[i] + k
-            dup = freq[num]
-            right = bisect_left(nums, num + k + 1)
-            left = bisect_left(nums, num - k)
+        for i in range(n):
+            left = bisect_left(nums, nums[i] - (k * 2))
+            right = bisect_right(nums, nums[i] + (k * 2))
 
-            # print(i, num, (left, right, dup))
-
-            left = min(op, i - left)
-            right = min(op + dup, right - i - 1)
-            # print(left, right)
-
-            ret = max(ret, left + 1, right + 1, min(left + right + 1, op + dup + 1))
-
-            i += 1
+            best = max( best, min(numOperations, i - left + 1), min(numOperations, right - i) )
         
-        return ret
+        return best
