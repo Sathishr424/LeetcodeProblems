2451fae8d0da3f2c0d936b354f387f572c0c3209
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-24 06:05:57 +0530

    File updated at 24/10/2025, 6:05:55 am

--- 2048-NextGreaterNumericallyBalancedNumber/2048-NextGreaterNumericallyBalancedNumber.py
+++ 2048-NextGreaterNumericallyBalancedNumber/2048-NextGreaterNumericallyBalancedNumber.py
@@ -1,9 +1,9 @@
-# Last updated: 24/10/2025, 5:53:27 am
+# Last updated: 24/10/2025, 6:05:55 am
 class Solution:
     def nextBeautifulNumber(self, n: int) -> int:
         num = n + 1
         while True:
-            uniq = defaultdict(int)
+            uniq = [0] * 10
             tmp = num
             while tmp:
                 uniq[tmp % 10] += 1
@@ -15,4 +15,3 @@ class Solution:
             else:
                 return num
             num += 1
-        
