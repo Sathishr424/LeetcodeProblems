065662eadc6cbd82e4689ac2f82d39235ad86014
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-28 02:39:43 +0530

    Create

---
+++ problem-1761599382183/problem-1761599382183.py
@@ -0,0 +1,48 @@
+# Last updated: 28/10/2025, 2:39:42 am
+class Solution:
+    def maximizeWin(self, price: List[int], k: int) -> int:
+        n = len(price)
+
+        prefix = []
+        for i in range(n):
+            num = price[i]
+            right = bisect_right(price, num + k) - 1
+            prefix.append(right)
+            
+        right_max = [0] * (n + 1)
+        right_max[n-1] = 1
+        for i in range(n-2, -1, -1):
+            right_max[i] = max(right_max[i + 1], prefix[i] - i + 1)
+        
+        def getCan(mid, right):
+            l = mid
+            r = prefix[mid]
+
+            diff = max(0, right - l + 1)
+
+            can = r - l + 1
+            return max(0, can - diff)
+
+        best = 0
+        for i in range(n):
+            num = price[i]
+            right = prefix[i]
+            tot = right - i + 1
+            
+            l = i
+            r = right
+
+            while l < r:
+                mid = (l + r) // 2
+
+                if getCan(mid, right) > getCan(mid + 1, right):
+                    r = mid
+                else:
+                    l = mid + 1
+
+            best = max(best, tot + getCan(l, right))
+            best = max(best, tot + right_max[right + 1])
+
+        return best
+
+                
