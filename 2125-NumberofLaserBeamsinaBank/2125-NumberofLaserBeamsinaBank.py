acca8a6250db114f31b40ff1fd75d17230417209
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-27 14:51:35 +0530

    File updated at 27/10/2025, 2:51:35 pm

--- 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.py
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.py
@@ -1,4 +1,4 @@
-# Last updated: 27/10/2025, 2:51:17 pm
+# Last updated: 27/10/2025, 2:51:35 pm
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
         m = len(bank)
@@ -9,7 +9,6 @@ class Solution:
         for i in range(m):
             cam = bank[i].count('1')
             if cam == 0: continue
-            # print(i, (prev, cam))
             if prev != -1:
                 ans += cam * prev
             prev = cam
