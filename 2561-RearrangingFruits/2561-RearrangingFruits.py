5553a4c07f3c78cb2884ebc038848b5c4722f57b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-28 19:41:56 +0530

    File updated at 28/10/2025, 7:41:54 pm

--- 2561-RearrangingFruits/2561-RearrangingFruits.py
+++ 2561-RearrangingFruits/2561-RearrangingFruits.py
@@ -1,22 +1,31 @@
-# Last updated: 2/8/2025, 7:48:28 pm
+# Last updated: 28/10/2025, 7:41:54 pm
 class Solution:
     def minCost(self, basket1: List[int], basket2: List[int]) -> int:
-        freq = Counter()
-        m = float("inf")
-        for b1 in basket1:
-            freq[b1] += 1
-            m = min(m, b1)
-        for b2 in basket2:
-            freq[b2] -= 1
-            m = min(m, b2)
+        n = len(basket1)
 
-        merge = []
-        for k, c in freq.items():
-            if c % 2 != 0:
-                return -1
-            merge.extend([k] * (abs(c) // 2))
+        freq1 = defaultdict(int)
+        freq2 = defaultdict(int)
+        both = set()
+        for i in range(n):
+            both.add(basket1[i])
+            both.add(basket2[i])
+            
+            freq1[basket1[i]] += 1
+            freq2[basket2[i]] += 1
 
-        if not merge:
-            return 0
-        merge.sort()
-        return sum(min(2 * m, x) for x in merge[: len(merge) // 2])
+        for num in both:
+            if (freq1[num] + freq2[num]) % 2: return -1
+            
+        arr = []
+        mini = inf
+        for num in both:
+            a = freq1[num]
+            b = freq2[num]
+
+            d = abs(a - b) // 2
+            arr += [num] * d
+
+            mini = min(num, mini)
+        
+        arr.sort()
+        return sum([ min(mini * 2, num) for num in arr[:len(arr) // 2] ])
