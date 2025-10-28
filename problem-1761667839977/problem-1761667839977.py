d1b3cd66f49eb87882de63e8b2c593ae03ffedf8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-10-28 21:40:41 +0530

    Create

---
+++ problem-1761667839977/problem-1761667839977.py
@@ -0,0 +1,49 @@
+# Last updated: 28/10/2025, 9:40:39 pm
+class Solution:
+    def minimumScore(self, s: str, t: str) -> int:
+        m = len(s)
+        n = len(t)
+
+        left = [m] * n
+        index = 0
+        for i in range(m):
+            if s[i] == t[index]:
+                left[index] = i
+                index += 1
+                if index == n:
+                    return 0
+
+        right = [-1] * n
+        index = n-1
+        for i in range(m-1, -1, -1):
+            if s[i] == t[index]:
+                right[index] = i
+                index -= 1
+                if index == -1:
+                    return 0
+        
+        def isGood(mid):
+            if right[mid] != -1: return True
+            if left[n-mid-1] != m: return True
+            for i in range(mid + 1, n):
+                l = i - mid - 1
+                r = i
+                
+                if left[l] < right[r]:
+                    return True
+            
+            return False
+        
+        l = 1
+        r = n
+        
+        while l < r:
+            mid = (l + r) // 2
+
+            if isGood(mid):
+                r = mid
+            else:
+                l = mid + 1
+
+        return l
+            
