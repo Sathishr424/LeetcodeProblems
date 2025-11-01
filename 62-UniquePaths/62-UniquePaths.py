29bbe9986cf1bfcb6da154aaa68552c384327ae0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-01 22:21:08 +0530

    File updated at 1/11/2025, 10:21:06 pm

--- 62-UniquePaths/62-UniquePaths.py
+++ 62-UniquePaths/62-UniquePaths.py
@@ -1,4 +1,13 @@
-# Last updated: 1/11/2025, 10:12:40 pm
+# Last updated: 1/11/2025, 10:21:06 pm
+def fact(i, r):
+    if i > r: return 1
+    return i * fact(i + 1, r)
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return factorial(m+n-2) // (factorial(n-1) * factorial(m-1))
+        if n > m:
+            n, m = m, n
+        n -= 1
+        m -= 1
+        
+        return fact(m + 1, (m + n)) // factorial(n)
