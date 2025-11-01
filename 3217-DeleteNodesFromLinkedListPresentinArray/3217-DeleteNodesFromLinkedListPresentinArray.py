981e9fdefcdc3c231897a3d7d4201004badf3a2e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-01 12:42:58 +0530

    File updated at 1/11/2025, 12:42:57 pm

--- 3217-DeleteNodesFromLinkedListPresentinArray/3217-DeleteNodesFromLinkedListPresentinArray.py
+++ 3217-DeleteNodesFromLinkedListPresentinArray/3217-DeleteNodesFromLinkedListPresentinArray.py
@@ -1,4 +1,4 @@
-# Last updated: 1/11/2025, 12:40:08 pm
+# Last updated: 1/11/2025, 12:42:57 pm
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -8,12 +8,14 @@ class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
         nums = set(nums)
 
-        def rec(node):
-            if node == None: return None
-
-            if node.val in nums:
-                return rec(node.next)
-            node.next = rec(node.next)
-            return node
+        node = head
+        while node and node.val in nums:
+            node = node.next
+        head = node
+        while node.next:
+            if node.next.val in nums:
+                node.next = node.next.next
+                continue
+            node = node.next
         
-        return rec(head)
+        return head
