ee098dddfe83d74855757767761ee788efffc575
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-02 07:58:48 +0530

    File updated at 2/11/2025, 7:58:45 am

--- 1483-KthAncestorofaTreeNode/1483-KthAncestorofaTreeNode.py
+++ 1483-KthAncestorofaTreeNode/1483-KthAncestorofaTreeNode.py
@@ -1,23 +1,25 @@
-# Last updated: 14/5/2025, 2:22:02 pm
+# Last updated: 2/11/2025, 7:58:45 am
 class TreeAncestor:
     def __init__(self, n: int, parent: List[int]):
-        self.N = int(log2(n) + 1)
-        self.logs = [[-1] * n for _ in range(self.N)]
+        self.parent = parent
+        self.k = floor(log2(n)) + 1
+
+        self.logs = [[-1] * n for _ in range(self.k)]
 
         for i in range(n):
             self.logs[0][i] = parent[i]
         
-        for i in range(1, self.N):
+        for i in range(1, self.k):
             for j in range(n):
-                if self.logs[i-1][j] == -1: continue
-                self.logs[i][j] = self.logs[i-1][self.logs[i-1][j]]
-    
+                if self.logs[i - 1][j] == -1: continue
+                self.logs[i][j] = self.logs[i-1][ self.logs[i - 1][j] ]
+
     def getKthAncestor(self, node: int, k: int) -> int:
-        for i in range(self.N-1, -1, -1):
+        for i in range(31, -1, -1):
             if (1 << i) <= k:
-                if self.logs[i][node] == -1: return -1
-                k -= (1 << i)
                 node = self.logs[i][node]
+                if node == -1: return -1
+                k -= (1 << i)
         return node
 
 # Your TreeAncestor object will be instantiated and called as such:
