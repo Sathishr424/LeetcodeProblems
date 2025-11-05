9688c971b54d3ad0ce78892a6441ffa1fd366a08
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-05 22:42:53 +0530

    File updated at 5/11/2025, 10:42:51 pm

--- 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
+++ 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
@@ -1,19 +1,26 @@
-# Last updated: 5/11/2025, 10:29:49 pm
+# Last updated: 5/11/2025, 10:42:51 pm
 class MedianFinder:
     def __init__(self):
         self.max = []
         self.min = []
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.min, num)
-        
-        if self.min and self.max and -self.max[0] > self.min[0]:
-            num = -heapq.heappop(self.max)
+        if len(self.min) == 0:
+            heapq.heappush(self.max, -num)
+        elif num > self.min[0]:
             heapq.heappush(self.min, num)
+        else:
+            heapq.heappush(self.max, -num)
         
         while len(self.min) > len(self.max):
             num = heapq.heappop(self.min)
             heapq.heappush(self.max, -num)
+        
+        while len(self.max) > len(self.min) + 1:
+            num = -heapq.heappop(self.max)
+            heapq.heappush(self.min, num)
+        
+        # print(self.max, self.min)
 
     def findMedian(self) -> float:
         if len(self.min) != len(self.max):
