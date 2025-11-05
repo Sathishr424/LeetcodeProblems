e2991901f7557bd0ccf119439408b0e231cca9bf
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-06 01:24:33 +0530

    File updated at 6/11/2025, 1:24:31 am

--- 3321-FindX-SumofAllK-LongSubarraysII/3321-FindX-SumofAllK-LongSubarraysII.py
+++ 3321-FindX-SumofAllK-LongSubarraysII/3321-FindX-SumofAllK-LongSubarraysII.py
@@ -1,11 +1,10 @@
-# Last updated: 6/11/2025, 1:18:24 am
+# Last updated: 6/11/2025, 1:24:31 am
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
         n = len(nums)
         ret = []
 
         indexes = defaultdict(list)
-
         for i in range(n):
             indexes[nums[i]].append(i)
 
@@ -16,12 +15,9 @@ class Solution:
         right = []
         left_there = set()
 
-        # print(indexes)
-
-        def withinBounds(num, cnt, index, left_limit):
-            # return index > left_limit
+        def isValid(num, cnt, index, left_limit):
+            if freq[num] != cnt: return False
             num_index = bisect_left(indexes[num], index)
-            # print(num, cnt, (index, num_index), left_limit)
             return indexes[num][num_index - cnt + 1] > left_limit
 
         def go(i):
@@ -36,12 +32,19 @@ class Solution:
             freq[num] += 1
             heapq.heappush(right, (-freq[num], -num, i))
 
-            while left and (freq[left[0][1]] != left[0][0] or not withinBounds(left[0][1], left[0][0], left[0][2], i - k)):
+            while left and not isValid(left[0][1], left[0][0], left[0][2], i - k):
                 heapq.heappop(left)
                 extra -= 1
             
-            while right and (len(left) - extra < x or ( -right[0][0] > left[0][0] or ( -right[0][0] == left[0][0] and -right[0][1] > left[0][1] ) ) ):
-                if freq[-right[0][1]] != -right[0][0] or not withinBounds(-right[0][1], -right[0][0], right[0][2], i - k):
+            while right and \
+                (
+                    len(left) - extra < x or ( 
+                        -right[0][0] > left[0][0] or ( 
+                            -right[0][0] == left[0][0] and -right[0][1] > left[0][1] 
+                        ) 
+                    ) 
+                ):
+                if not isValid(-right[0][1], -right[0][0], right[0][2], i - k):
                     heapq.heappop(right)
                     continue
                 c, num, index = heapq.heappop(right)
@@ -53,15 +56,13 @@ class Solution:
 
             while len(left) - extra > x:
                 c, num, index = heapq.heappop(left)
-                if freq[num] != c or not withinBounds(num, c, index, i - k):
+                if not isValid(num, c, index, i - k):
                     extra -= 1
                 else:
                     heapq.heappush(right, (-c, -num, index))
                     if freq[num] == c:
                         s -= num * c
                     left_there.discard(num)
-                
-            # print(i, s, extra, left, right, dict(freq))
     
         for i in range(k):
             go(i)
@@ -77,7 +78,6 @@ class Solution:
             
             freq[prev_num] -= 1
             if freq[prev_num]:
-                # index = i
                 index = bisect_right(indexes[prev_num], i)
                 index = indexes[prev_num][index - 1]
                 heapq.heappush(right, (-freq[prev_num], -prev_num, index))
