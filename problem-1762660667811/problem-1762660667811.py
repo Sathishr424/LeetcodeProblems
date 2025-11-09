e96bf8736c73b2ae73081df5a249f36e54667cda
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-09 09:27:48 +0530

    Create

---
+++ problem-1762660667811/problem-1762660667811.py
@@ -0,0 +1,41 @@
+# Last updated: 9/11/2025, 9:27:47 am
+cmax = lambda x, y: x if x > y else y
+cmin = lambda x, y: x if x < y else y
+class Solution:
+    def maxPathScore(self, grid: List[List[int]], k: int) -> int:
+        # k = 1000
+        # grid = [[random.randint(0, 3) for _ in range(200)] for _ in range(200)]
+        
+        m = len(grid)
+        n = len(grid[0])
+
+        dp = [[defaultdict(lambda: -inf) for _ in range(n)] for _ in range(m)]
+        dp[0][0][k] = 0
+        
+        def getCostCell(cell):
+            return 1 if cell > 0 else 0
+        
+        def getCost(i, j):
+            return getCostCell(grid[i][j])
+
+        cost = 0
+        for i in range(m):
+            for j in range(n):
+                for rem in dp[i][j]:
+                    if i + 1 < m:
+                        cell = grid[i+1][j]
+                        cost = getCostCell(cell)
+                        if rem >= cost:
+                            cost = rem - cost
+                            dp[i + 1][j][cost] = cmax(dp[i + 1][j][cost], dp[i][j][rem] + cell)
+                    if j + 1 < n:
+                        cell = grid[i][j+1]
+                        cost = getCostCell(cell)
+                        if rem >= cost:
+                            cost = rem - cost
+                            dp[i][j + 1][cost] = cmax(dp[i][j + 1][cost], dp[i][j][rem] + cell)
+        # [print(row) for row in dp]
+        ans = -inf
+        for rem in dp[m-1][n-1]:
+            ans = max(ans, dp[m-1][n-1][rem])
+        return -1 if ans == -inf else ans
