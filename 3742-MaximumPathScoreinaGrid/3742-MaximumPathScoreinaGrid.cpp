17e25e30aae20d8113c9388249fb9c14d9067122
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-10 20:33:34 +0530

    File updated at 10/11/2025, 8:33:32 pm

--- 3742-MaximumPathScoreinaGrid/3742-MaximumPathScoreinaGrid.cpp
+++ 3742-MaximumPathScoreinaGrid/3742-MaximumPathScoreinaGrid.cpp
@@ -1,4 +1,5 @@
-// Last updated: 10/11/2025, 7:58:53 pm
+// Last updated: 10/11/2025, 8:33:32 pm
+#include <cstring>
 #include <iostream>
 #include <vector>
 
@@ -7,37 +8,49 @@ using namespace std;
 int inf = INT_MAX;
 class Solution {
 public:
-    int DIR[2][2];
-    Solution() : DIR{{1, 0}, {0, 1}} {
-
-    }
-    int rec(int i, int j, int m, int n, int rem, vector<vector<int>>& grid, vector<vector<vector<int>>>& memo) {
-        if (rem < 0) return -inf;
-        if (i == m - 1 && j == n - 1) return 0;
-        if (memo[i][j][rem] != -1) return memo[i][j][rem];
-
-        int ans = -inf;
-        for (int d = 0; d < 2; d++) {
-            int i2 = i + DIR[d][0];
-            int j2 = j + DIR[d][1];
-
-            if (i2 >= 0 && i2 < m && j2 >= 0 && j2 < n && (grid[i2][j2] == 0 || rem >= 1)) {
-                ans = max( ans, rec(i2, j2, m, n, rem - (grid[i2][j2] == 0 ? 0 : 1), grid, memo) + grid[i2][j2] );
-            }
-        }
-        memo[i][j][rem] = ans;
-        return ans;
-    }
-
     int maxPathScore(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
+        k = min(k, 402);
 
-        vector<vector<vector<int>>> memo(
-            m, vector<vector<int>>(n, vector<int>(k + 1, -1))
-        );
-        int ans = rec(0, 0, m, n, k, grid, memo);
+        int dp[m][n][k + 1];
 
-        return ans <= -(inf - 1000) ? -1 : ans;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int l = 0; l <= k; ++l) {
+                    dp[i][j][l] = -inf;
+                }
+            }
+        }
+
+        dp[0][0][k] = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int rem = 0; rem <= k; rem++) {
+
+                    for (int i2=0; i2<=1; i2++) {
+                        for (int j2=0; j2<=1; j2++) {
+                            if (i2 == j2) continue;
+                            int i3 = i2 + i;
+                            int j3 = j2 + j;
+                            if (i3 < m && j3 < n && (grid[i3][j3] == 0 || rem >= 1)) {
+                                int new_rem = rem - (grid[i3][j3] == 0 ? 0 : 1);
+                                // cout << i3 << " " << j3 << " " << rem << endl;
+                                dp[i3][j3][new_rem] = max(dp[i3][j3][new_rem], dp[i][j][rem] + grid[i3][j3]);
+                                // cout << "ANS: " << dp[i][j][rem] << " " << dp[i3][j3][new_rem] << endl;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+        
+        int ans = -1;
+        for (int rem=0; rem<=k; rem++) {
+            ans = max(ans, dp[m - 1][n - 1][rem]);
+        }
+        return ans;
     }
 };
