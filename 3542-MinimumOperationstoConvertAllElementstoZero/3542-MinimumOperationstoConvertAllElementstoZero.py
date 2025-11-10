d70363959e81514bb3fda4e1c80a4d2d76772dc2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-10 15:55:37 +0530

    File updated at 10/11/2025, 3:55:36 pm

--- 3542-MinimumOperationstoConvertAllElementstoZero/3542-MinimumOperationstoConvertAllElementstoZero.py
+++ 3542-MinimumOperationstoConvertAllElementstoZero/3542-MinimumOperationstoConvertAllElementstoZero.py
@@ -1,23 +1,48 @@
-# Last updated: 10/5/2025, 10:24:50 pm
+# Last updated: 10/11/2025, 3:55:36 pm
+from math import floor, log2
+
+class sparseTable:
+    def __init__(self, nums):
+        self.nums = nums
+        n = len(nums)
+        self.n = n
+        k_log = floor(log2(n)) + 1
+                
+        self.logs = [[0] * n for _ in range(k_log)]
+
+        for i in range(n):
+            self.logs[0][i] = nums[i]
+
+        for power in range(1, k_log):
+            m = 1 << power
+            prev_m = m >> 1
+
+            for i in range(n-m+1):
+                self.logs[power][i] = min(self.logs[power - 1][i], self.logs[power - 1][i + prev_m])
+    
+    def query(self, l, r):
+        dis = r - l + 1
+        power = floor(log2(dis))
+        return min(self.logs[power][l], self.logs[power][r - (1 << power) + 1])
+
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        stack = []
-        ret = 0
+        n = len(nums)
 
-        for num in nums:
-            prev = -1
-            while stack and stack[-1] > num:
-                p = stack.pop()
-                ret += p != prev
-                prev = p
-            stack.append(num)
-
-        prev = -1
-        for num in stack:
-            if num == prev or num == 0: continue
-            ret += 1
-            prev = num
-
-        return ret
-
-            
+        indexes = defaultdict(list)
+        for i, num in enumerate(nums):
+            indexes[num].append(i)
+        
+        table = sparseTable(nums)
+        op = 0
+        for num in sorted(indexes.keys()):
+            if num == 0: continue
+            prev = indexes[num][0]
+            for index in indexes[num]:
+                min_num = table.query(prev, index)
+                if min_num < num:
+                    op += 1
+                    prev = index
+            op += 1
+        
+        return op
