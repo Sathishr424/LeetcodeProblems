25d75807e9890e62bfa44e9e387ea642db2bc68b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-14 16:24:06 +0530

    Create 2536-IncrementSubmatricesbyOne

---
+++ 2536-IncrementSubmatricesbyOne/2536-IncrementSubmatricesbyOne.cpp
@@ -0,0 +1,62 @@
+// Last updated: 14/11/2025, 4:24:05 pm
+int diff[501][501];
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        int q = queries.size();
+        vector<vector<int>> ret(n, vector<int>(n, 0));
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                diff[i][j] = 0;
+            }
+        }
+
+        for (int i=0; i<q; i++) {
+            int i1 = queries[i][0];
+            int j1 = queries[i][1];
+            int i2 = queries[i][2];
+            int j2 = queries[i][3];
+
+            diff[i1][j1] += 1;
+            diff[i1][j2 + 1] -= 1;
+            diff[i2 + 1][j1] -= 1;
+            diff[i2 + 1][j2 + 1] += 1;
+        }
+
+        // for (int i=0; i<n+1; i++) {
+        //     for (int j=0; j<n+1; j++) {
+        //         cout << diff[i][j] << ", ";
+        //     }
+        //     cout << endl;
+        // }
+
+        for (int i=0; i<n; i++) {
+            int curr=0;
+            for (int j=0; j<n; j++) {
+                curr += diff[i][j];
+                ret[i][j] += curr;
+            }
+        }
+
+        // cout << endl;
+        
+        // for (int i=0; i<n; i++) {
+        //     for (int j=0; j<n; j++) {
+        //         cout << ret[i][j] << ", ";
+        //     }
+        //     cout << endl;
+        // }
+
+        for (int j=0; j<n; j++) {
+            int curr=0;
+            for (int i=0; i<n; i++) {
+                int tmp = ret[i][j];
+                ret[i][j] += curr;
+                curr += tmp;
+            }
+        }
+
+        return ret;
+    }
+};
