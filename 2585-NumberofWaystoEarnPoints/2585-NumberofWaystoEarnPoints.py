7d0fe309aea217586d96ba86d701757d81fdb1b9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-18 02:25:36 +0530

    Create 2585-NumberofWaystoEarnPoints

---
+++ 2585-NumberofWaystoEarnPoints/2585-NumberofWaystoEarnPoints.py
@@ -0,0 +1,19 @@
+# Last updated: 18/11/2025, 2:25:34 am
+class Solution:
+    def waysToReachTarget(self, target: int, types: List[List[int]]) -> int:
+        n = len(types)
+        mod = 10**9 + 7
+
+        dp = [0 for _ in range(target + 1)]
+        dp[0] = 1
+        for i in range(n):
+            cnt, mark = types[i]
+            for rem in range(target, -1, -1):
+                curr = dp[rem]
+                for _ in range(cnt):
+                    if mark + rem > target: break
+                    rem += mark
+                    dp[rem] += curr
+                    dp[rem] %= mod
+
+        return dp[target]
