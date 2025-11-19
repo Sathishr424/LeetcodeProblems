9c1f40898d121e468ed70dad9bd0525db6aaec15
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-19 06:44:14 +0530

    Create 2154-KeepMultiplyingFoundValuesbyTwo

---
+++ 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.cpp
@@ -0,0 +1,16 @@
+// Last updated: 19/11/2025, 6:44:13 am
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> set;
+        for (int num: nums) {
+            set.insert(num);
+        }
+
+        while (set.find(original) != set.end()) {
+            original *= 2;
+        }
+
+        return original;
+    }
+};
