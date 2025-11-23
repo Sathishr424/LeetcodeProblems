323b8c8d05c5e6510a9f680fec050bbc222f2528
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-23 06:06:30 +0530

    File updated at 23/11/2025, 6:06:29 am

--- 1262-GreatestSumDivisiblebyThree/1262-GreatestSumDivisiblebyThree.py
+++ 1262-GreatestSumDivisiblebyThree/1262-GreatestSumDivisiblebyThree.py
@@ -1,4 +1,6 @@
-# Last updated: 23/11/2025, 6:05:40 am
+# Last updated: 23/11/2025, 6:06:29 am
+cmax = lambda x, y: x if x > y else y
+
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
         n = len(nums)
@@ -9,7 +11,7 @@ class Solution:
         max_ans = 0
         for num in nums:
             if num % 3 == need:
-                max_ans = max(max_ans, total - num)
+                max_ans = cmax(max_ans, total - num)
         
         nums.sort()
         need = (3 - total) % 3
@@ -17,7 +19,7 @@ class Solution:
             if num % 3 == need:
                 total -= num
                 if total % 3 == 0:
-                    max_ans = max(max_ans, total)
+                    max_ans = cmax(max_ans, total)
                     break
         
         return max_ans
