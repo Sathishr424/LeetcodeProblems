5ed08b73ddfe16d6d3553686cfc57639903f4131
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-23 08:34:06 +0530

    Create

---
+++ problem-1763867044777/problem-1763867044777.py
@@ -0,0 +1,29 @@
+# Last updated: 23/11/2025, 8:34:04 am
+class Solution:
+    def maxBalancedSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        def findAns(nums):
+            ans = 0
+            hash = defaultdict(dict)
+            hash[0][0] = -1
+            even = 0
+            odd = 0
+            xor = 0
+            for i in range(n):
+                if nums[i] % 2 == 0:
+                    even += 1
+                else:
+                    odd += 1
+                xor ^= nums[i]
+                diff = even - odd
+                if diff in hash and xor in hash[diff]:
+                    ans = max(ans, i - hash[diff][xor])
+                if xor not in hash[diff]:
+                    hash[diff][xor] = i
+
+                # print(i, nums, diff, xor, dict(hash))
+            
+            return ans
+        
+        return max(findAns(nums), findAns(nums[::-1]))
