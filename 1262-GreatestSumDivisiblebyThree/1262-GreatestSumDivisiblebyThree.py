b58d6e3cd26c656ce2f85f1a86994e128fc9a627
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-23 06:00:01 +0530

    File updated at 23/11/2025, 6:00:00 am

--- 1262-GreatestSumDivisiblebyThree/1262-GreatestSumDivisiblebyThree.py
+++ 1262-GreatestSumDivisiblebyThree/1262-GreatestSumDivisiblebyThree.py
@@ -1,16 +1,18 @@
-# Last updated: 23/11/2025, 5:57:05 am
+# Last updated: 23/11/2025, 6:00:00 am
+cmax = lambda x, y: x if x > y else y
+
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
         n = len(nums)
-
+        inf = 10**20
         dp = [[-inf] * (n + 1) for _ in range(3)]
         dp[0][0] = 0
 
         for i in range(n):
             for rem in range(3):
-                dp[rem][i + 1] = max(dp[rem][i + 1], dp[rem][i])
+                dp[rem][i + 1] = cmax(dp[rem][i + 1], dp[rem][i])
 
                 new_rem = (rem + nums[i]) % 3
-                dp[new_rem][i + 1] = max(dp[new_rem][i + 1], dp[rem][i] + nums[i])
+                dp[new_rem][i + 1] = cmax(dp[new_rem][i + 1], dp[rem][i] + nums[i])
         
         return dp[0][n]
