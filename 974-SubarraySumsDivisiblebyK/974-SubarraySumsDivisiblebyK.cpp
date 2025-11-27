7bd41a697367e449dd0aa1242fb5757362b25c66
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-27 09:30:50 +0530

    File updated at 27/11/2025, 9:30:50 am

--- 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.cpp
+++ 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.cpp
@@ -1,4 +1,4 @@
-// Last updated: 27/11/2025, 9:27:20 am
+// Last updated: 27/11/2025, 9:30:50 am
 1class Solution {
 2public:
 3    int subarraysDivByK(vector<int>& nums, int k) {
@@ -10,15 +10,12 @@
 9        int curr = 0;
 10        last_seen[curr]++;
 11        for (int i=0; i<n; i++) {
-12            curr = (curr + nums[i]) % k;
-13            int rem = (curr + k) % k;
-14            if (last_seen.find(rem) != last_seen.end()) {
-15                ans+=last_seen[rem];
-16            }
-17            last_seen[rem]++;
-18            // cout << i << " " << rem << ", " << ans << endl;
-19        }
-20
-21        return ans;
-22    }
-23};
+12            curr = ((curr + nums[i]) % k + k) % k;
+13            ans+=last_seen[curr];
+14            last_seen[curr]++;
+15            cout << curr << " " << ans << endl;
+16        }
+17
+18        return ans;
+19    }
+20};
