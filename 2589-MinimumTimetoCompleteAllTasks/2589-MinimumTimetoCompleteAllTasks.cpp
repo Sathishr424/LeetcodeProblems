db2f5aac9d0e37f63fe2526f312af158874b8120
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-30 04:22:03 +0530

    File updated at 30/11/2025, 4:22:02 am

--- 2589-MinimumTimetoCompleteAllTasks/2589-MinimumTimetoCompleteAllTasks.cpp
+++ 2589-MinimumTimetoCompleteAllTasks/2589-MinimumTimetoCompleteAllTasks.cpp
@@ -1,4 +1,4 @@
-// Last updated: 30/11/2025, 4:21:17 am
+// Last updated: 30/11/2025, 4:22:02 am
 1class Solution {
 2public:
 3    int findMinimumTime(vector<vector<int>>& tasks) {
@@ -6,35 +6,28 @@
 5        sort(tasks.begin(), tasks.end(), [](auto& a, auto& b) {
 6            return a[0] < b[0];
 7        });
-8        unordered_map<int, int> freq;
-9
-10        for (auto task: tasks) {
-11            for (int i=task[0]; i<=task[1]; i++) {
-12                freq[i]++;
-13            }
-14        }
-15
-16        unordered_set<int> used;
-17
-18        for (int i=n-1; i>=0; i--) {
-19            auto& task = tasks[i];
+8
+9        unordered_set<int> used;
+10
+11        for (int i=n-1; i>=0; i--) {
+12            auto& task = tasks[i];
+13
+14            int rem = task[2];
+15            for (int time=task[0]; time<=task[1]; time++) {
+16                if (used.find(time) != used.end()) {
+17                    rem--;
+18                }
+19            }
 20
-21            int rem = task[2];
-22            for (int time=task[0]; time<=task[1]; time++) {
-23                if (used.find(time) != used.end()) {
-24                    rem--;
-25                }
-26            }
-27
-28            for (int time=task[0]; time<=task[1]; time++) {
-29                if (rem <= 0) break;
-30                if (used.find(time) == used.end()) {
-31                    used.insert(time);
-32                    rem--;
-33                }
-34            }
-35        }
-36
-37        return used.size();
-38    }
-39};
+21            for (int time=task[0]; time<=task[1]; time++) {
+22                if (rem <= 0) break;
+23                if (used.find(time) == used.end()) {
+24                    used.insert(time);
+25                    rem--;
+26                }
+27            }
+28        }
+29
+30        return used.size();
+31    }
+32};
