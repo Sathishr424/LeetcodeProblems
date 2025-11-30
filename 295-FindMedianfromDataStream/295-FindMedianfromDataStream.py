d9f01282accb41eabe3120d0db619c3072985711
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-01 01:39:05 +0530

    File updated at 1/12/2025, 1:39:02 am

--- 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
+++ 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
@@ -1,4 +1,4 @@
-# Last updated: 1/12/2025, 1:38:28 am
+# Last updated: 1/12/2025, 1:39:02 am
 1class MedianFinder:
 2    def __init__(self):
 3        self.left = []
@@ -9,27 +9,25 @@
 8        self.n += 1
 9        heapq.heappush(self.left, -num)
 10
-11        while self.left and self.right and -self.left[0] > self.right[0]:
+11        if self.left and self.right and -self.left[0] > self.right[0]:
 12            heapq.heappush(self.right, -heapq.heappop(self.left))
 13
 14        half = ceil(self.n / 2)
 15
-16        while len(self.left) > half:
+16        if len(self.left) > half:
 17            heapq.heappush(self.right, -heapq.heappop(self.left))
 18        
-19        while len(self.right) > self.n // 2:
+19        if len(self.right) > self.n // 2:
 20            heapq.heappush(self.left, -heapq.heappop(self.right))
 21
-22
-23    def findMedian(self) -> float:
-24        # print(self.n, self.left, self.right)
-25        if self.n % 2:
-26            return -self.left[0]
-27        else:
-28            return (-1 * self.left[0] + self.right[0]) / 2  
-29
-30
-31# Your MedianFinder object will be instantiated and called as such:
-32# obj = MedianFinder()
-33# obj.addNum(num)
-34# param_2 = obj.findMedian()
+22    def findMedian(self) -> float:
+23        if self.n % 2:
+24            return -self.left[0]
+25        else:
+26            return (-1 * self.left[0] + self.right[0]) / 2  
+27
+28
+29# Your MedianFinder object will be instantiated and called as such:
+30# obj = MedianFinder()
+31# obj.addNum(num)
+32# param_2 = obj.findMedian()
