9c6eb3c6a80aea422e91f3e87d9fa4de11f98c70
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-11-30 10:19:22 +0530

    File updated at 30/11/2025, 10:19:21 am

--- 3760-MaximumSubstringsWithDistinctStart/3760-MaximumSubstringsWithDistinctStart.py
+++ 3760-MaximumSubstringsWithDistinctStart/3760-MaximumSubstringsWithDistinctStart.py
@@ -1,5 +1,4 @@
-# Last updated: 30/11/2025, 10:19:12 am
+# Last updated: 30/11/2025, 10:19:21 am
 1class Solution:
 2    def maxDistinct(self, s: str) -> int:
-3        n = len(s)
-4        return len(set(s))
+3        return len(set(s))
