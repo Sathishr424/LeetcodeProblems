6497504da12c642c49e52537f2d9a555a309cd0b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-01 15:04:08 +0530

    File updated at 1/12/2025, 3:04:05 pm

--- 2141-MaximumRunningTimeofNComputers/2141-MaximumRunningTimeofNComputers.py
+++ 2141-MaximumRunningTimeofNComputers/2141-MaximumRunningTimeofNComputers.py
@@ -1,20 +1,22 @@
-# Last updated: 25/7/2025, 3:32:14 pm
-class Solution:
-    def maxRunTime(self, n: int, batteries: List[int]) -> int:
-        def isGood(maxTime):
-            s = 0
-            for bat in batteries:
-                s += min(maxTime, bat)
-            return s // n >= maxTime
-        
-        l = 0
-        r = sum(batteries)
-
-        while l < r:
-            mid = (l + r + 1) // 2
-            if isGood(mid):
-                l = mid
-            else:
-                r = mid - 1
-        
-        return l
+# Last updated: 1/12/2025, 3:04:05 pm
+1class Solution:
+2    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+3        def isGood(mid):
+4            s = 0
+5            for bat in batteries:
+6                s += min(bat, mid)
+7            
+8            return s // mid >= n
+9
+10        l = 1
+11        r = sum(batteries)
+12
+13        while l <= r:
+14            mid = (l + r) // 2
+15
+16            if isGood(mid):
+17                l = mid + 1
+18            else:
+19                r = mid - 1
+20
+21        return r
