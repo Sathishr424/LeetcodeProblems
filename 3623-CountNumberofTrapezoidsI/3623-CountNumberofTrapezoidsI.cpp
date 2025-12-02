a665b27e314f1b19a98e976ada59c25bdf94b2a6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-02 17:46:09 +0530

    File updated at 2/12/2025, 5:46:06 pm

--- 3623-CountNumberofTrapezoidsI/3623-CountNumberofTrapezoidsI.cpp
+++ 3623-CountNumberofTrapezoidsI/3623-CountNumberofTrapezoidsI.cpp
@@ -1,4 +1,4 @@
-// Last updated: 2/12/2025, 4:41:57 pm
+// Last updated: 2/12/2025, 5:46:06 pm
 1class Solution {
 2public:
 3    int countTrapezoids(vector<vector<int>>& points) {
@@ -12,12 +12,11 @@
 11        int ans = 0;
 12        int prev = 0;
 13        for (auto& it: slopes) {
-14            if (it.second <= 1) continue;
-15            long long curr = it.second * 1LL * (it.second - 1) / 2;
-16            ans = (ans + prev * curr % mod) % mod;
-17            prev += curr;
-18        }
-19
-20        return ans;
-21    }
-22};
+14            long long curr = it.second * 1LL * (it.second - 1) / 2;
+15            ans = (ans + prev * curr % mod) % mod;
+16            prev += curr;
+17        }
+18
+19        return ans;
+20    }
+21};
