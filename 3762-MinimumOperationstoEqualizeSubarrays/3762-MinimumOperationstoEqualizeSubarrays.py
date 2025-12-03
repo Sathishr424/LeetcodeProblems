48d8a0cfa9191d4eef13ecac6de07120bbf4001a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-03 15:34:30 +0530

    File updated at 3/12/2025, 3:34:30 pm

--- 3762-MinimumOperationstoEqualizeSubarrays/3762-MinimumOperationstoEqualizeSubarrays.py
+++ 3762-MinimumOperationstoEqualizeSubarrays/3762-MinimumOperationstoEqualizeSubarrays.py
@@ -1,4 +1,4 @@
-# Last updated: 3/12/2025, 3:33:36 pm
+# Last updated: 3/12/2025, 3:34:30 pm
 1class PSegmentTree:
 2    def __init__(self, n, nums, compressed):
 3        self.n = n
@@ -28,96 +28,93 @@
 27        mid = (l + r) // 2
 28
 29        return self.getNewNode(self.build(l, mid), self.build(mid + 1, r))
-30    
-31    def nodeAt(self, index):
-32        return self.nodes[index]
-33
-34    def update(self, node, l, r, pos, sum):
-35        if l == r:
-36            return self.getNewNode(cnt=self.cnts[node] + 1, sum=self.sums[node] + sum)
-37        
-38        mid = (l + r) // 2
-39
-40        if pos <= mid:
-41            return self.getNewNode(self.update(self.lefts[node], l, mid, pos, sum), self.rights[node])
-42        return self.getNewNode(self.lefts[node], self.update(self.rights[node], mid + 1, r, pos, sum))
-43    
-44    def _getKthSmallest(self, left, right, l, r, k):
-45        if l == r:
-46            return l
-47        
-48        mid = (l + r) // 2
-49        left_count = self.cnts[self.lefts[right]]- self.cnts[self.lefts[left]]
-50        if left_count >= k:
-51            return self._getKthSmallest(self.lefts[left], self.lefts[right], l, mid, k)
-52        return self._getKthSmallest(self.rights[left], self.rights[right], mid + 1, r, k - left_count)
-53    
-54    def _getFirstKSum(self, left, right, l, r, k):
-55        if l == r:
-56            return self.sums[right] - self.sums[left]
-57        
-58        mid = (l + r) // 2
-59        left_count = self.cnts[self.lefts[right]]- self.cnts[self.lefts[left]]
-60        if left_count >= k:
-61            return self._getFirstKSum(self.lefts[left], self.lefts[right], l, mid, k)
-62        return (self.sums[self.lefts[right]] - self.sums[self.lefts[left]]) + self._getFirstKSum(self.rights[left], self.rights[right], mid + 1, r, k - left_count)
-63
-64    def getKthSmallest(self, l, r, k):
-65        return self._getKthSmallest(self.roots[l], self.roots[r+1], 0, self.n-1, k)
-66    
-67    def getFirstKSum(self, l, r, k):
-68        return self._getFirstKSum(self.roots[l], self.roots[r+1], 0, self.n-1, k)
-69
-70class Solution:
-71    def minOperations(self, nums: List[int], k: int, queries: List[List[int]]) -> List[int]:
-72        n = len(nums)
-73        divided = []
-74        prefix = [0]
-75        new_nums = []
-76        for i, num in enumerate(nums):
-77            divided.append(num // k)
-78            prefix.append(prefix[-1] + divided[-1])
-79            new_nums.append((divided[-1], i))
-80        compressed = {}
-81        uncompressed = [0] * n
-82        new_nums.sort()
-83        index = 0
-84        for num, i in new_nums:
-85            compressed[(num, i)] = index
-86            uncompressed[index] = num
-87            index += 1
-88        
-89        same_group = [0] * n
-90        i = 0
-91        group = 0
-92        while i < n:
-93            rem = nums[i] % k
-94            while i < n and nums[i] % k == rem:
-95                same_group[i] = group
-96                i += 1
-97            group += 1
-98
-99        segTree = PSegmentTree(index, divided, compressed)
-100        ret = []
-101        for l, r in queries:
-102            if same_group[l] != same_group[r]:
-103                ret.append(-1)
-104                continue
-105            
-106            window = r - l + 1
-107            half = window // 2
-108            if window % 2:
-109                med = segTree.getKthSmallest(l, r, half + 1)
-110                median = uncompressed[med]
-111                leftSum = segTree.getFirstKSum(l, r, half + 1)
-112                remSum = (prefix[r + 1] - prefix[l]) - leftSum
-113                ret.append((median * (half + 1) - leftSum) + (remSum - median * half))
-114            else:
-115                median_l = uncompressed[segTree.getKthSmallest(l, r, half)]
-116                median_r = uncompressed[segTree.getKthSmallest(l, r, half + 1)]
-117                median = (median_l + median_r) // 2
-118                leftSum = segTree.getFirstKSum(l, r, half)
-119                remSum = (prefix[r + 1] - prefix[l]) - leftSum
-120                ret.append((median * half - leftSum) + (remSum - median * half))
-121        
-122        return ret
+30
+31    def update(self, node, l, r, pos, sum):
+32        if l == r:
+33            return self.getNewNode(cnt=self.cnts[node] + 1, sum=self.sums[node] + sum)
+34        
+35        mid = (l + r) // 2
+36
+37        if pos <= mid:
+38            return self.getNewNode(self.update(self.lefts[node], l, mid, pos, sum), self.rights[node])
+39        return self.getNewNode(self.lefts[node], self.update(self.rights[node], mid + 1, r, pos, sum))
+40    
+41    def _getKthSmallest(self, left, right, l, r, k):
+42        if l == r:
+43            return l
+44        
+45        mid = (l + r) // 2
+46        left_count = self.cnts[self.lefts[right]]- self.cnts[self.lefts[left]]
+47        if left_count >= k:
+48            return self._getKthSmallest(self.lefts[left], self.lefts[right], l, mid, k)
+49        return self._getKthSmallest(self.rights[left], self.rights[right], mid + 1, r, k - left_count)
+50    
+51    def _getFirstKSum(self, left, right, l, r, k):
+52        if l == r:
+53            return self.sums[right] - self.sums[left]
+54        
+55        mid = (l + r) // 2
+56        left_count = self.cnts[self.lefts[right]]- self.cnts[self.lefts[left]]
+57        if left_count >= k:
+58            return self._getFirstKSum(self.lefts[left], self.lefts[right], l, mid, k)
+59        return (self.sums[self.lefts[right]] - self.sums[self.lefts[left]]) + self._getFirstKSum(self.rights[left], self.rights[right], mid + 1, r, k - left_count)
+60
+61    def getKthSmallest(self, l, r, k):
+62        return self._getKthSmallest(self.roots[l], self.roots[r+1], 0, self.n-1, k)
+63    
+64    def getFirstKSum(self, l, r, k):
+65        return self._getFirstKSum(self.roots[l], self.roots[r+1], 0, self.n-1, k)
+66
+67class Solution:
+68    def minOperations(self, nums: List[int], k: int, queries: List[List[int]]) -> List[int]:
+69        n = len(nums)
+70        divided = []
+71        prefix = [0]
+72        new_nums = []
+73        for i, num in enumerate(nums):
+74            divided.append(num // k)
+75            prefix.append(prefix[-1] + divided[-1])
+76            new_nums.append((divided[-1], i))
+77        compressed = {}
+78        uncompressed = [0] * n
+79        new_nums.sort()
+80        index = 0
+81        for num, i in new_nums:
+82            compressed[(num, i)] = index
+83            uncompressed[index] = num
+84            index += 1
+85        
+86        same_group = [0] * n
+87        i = 0
+88        group = 0
+89        while i < n:
+90            rem = nums[i] % k
+91            while i < n and nums[i] % k == rem:
+92                same_group[i] = group
+93                i += 1
+94            group += 1
+95
+96        segTree = PSegmentTree(index, divided, compressed)
+97        ret = []
+98        for l, r in queries:
+99            if same_group[l] != same_group[r]:
+100                ret.append(-1)
+101                continue
+102            
+103            window = r - l + 1
+104            half = window // 2
+105            if window % 2:
+106                med = segTree.getKthSmallest(l, r, half + 1)
+107                median = uncompressed[med]
+108                leftSum = segTree.getFirstKSum(l, r, half + 1)
+109                remSum = (prefix[r + 1] - prefix[l]) - leftSum
+110                ret.append((median * (half + 1) - leftSum) + (remSum - median * half))
+111            else:
+112                median_l = uncompressed[segTree.getKthSmallest(l, r, half)]
+113                median_r = uncompressed[segTree.getKthSmallest(l, r, half + 1)]
+114                median = (median_l + median_r) // 2
+115                leftSum = segTree.getFirstKSum(l, r, half)
+116                remSum = (prefix[r + 1] - prefix[l]) - leftSum
+117                ret.append((median * half - leftSum) + (remSum - median * half))
+118        
+119        return ret
