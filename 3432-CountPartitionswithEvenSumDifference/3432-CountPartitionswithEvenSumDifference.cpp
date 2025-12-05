f5c32019ebc9d1b71b7a093ff29e50dd141127c0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-05 11:29:59 +0530

    Create 3432-CountPartitionswithEvenSumDifference

---
+++ 3432-CountPartitionswithEvenSumDifference/3432-CountPartitionswithEvenSumDifference.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/12/2025, 11:29:58 am
+1class Solution {
+2public:
+3    int countPartitions(vector<int>& nums) {
+4        int right = accumulate(nums.begin(), nums.end(), 0);
+5        int left = 0;
+6        int count = 0;
+7        for (int i=0; i<nums.size() - 1; i++) {
+8            left += nums[i];
+9            right -= nums[i];
+10            
+11            if (abs(left - right) % 2 == 0) {
+12                count++;
+13            }
+14        }
+15        return count;
+16    }
+17};
