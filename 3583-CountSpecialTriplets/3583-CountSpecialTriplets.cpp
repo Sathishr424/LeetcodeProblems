bce4010a0625a8b50d4aafe127c84a46f186f44f
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-09 06:16:34 +0530

    Create 3583-CountSpecialTriplets

---
+++ 3583-CountSpecialTriplets/3583-CountSpecialTriplets.cpp
@@ -0,0 +1,27 @@
+// Last updated: 12/9/2025, 6:16:34 AM
+1class Solution {
+2public:
+3    int specialTriplets(vector<int>& nums) {
+4        int n = nums.size();
+5        int mod = 1e9 + 7;
+6
+7        unordered_map<int, int> right;
+8        unordered_map<int, int> left;
+9
+10        for (int num: nums) {
+11            right[num]++;
+12        }
+13
+14        int ans = 0;
+15        for (int num: nums) {
+16            right[num]--;
+17
+18            int need = num * 2;
+19            ans += (left[need] * 1LL * right[need]) % mod;
+20            ans %= mod;
+21            left[num]++;
+22        }
+23
+24        return ans;
+25    }
+26};
