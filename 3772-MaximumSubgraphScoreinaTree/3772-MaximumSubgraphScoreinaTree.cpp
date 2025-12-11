236fcc36ec81195386dd75d7ba5a4350d35c0be8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-11 19:35:31 +0530

    File updated at 12/11/2025, 7:35:30 PM

--- 3772-MaximumSubgraphScoreinaTree/3772-MaximumSubgraphScoreinaTree.cpp
+++ 3772-MaximumSubgraphScoreinaTree/3772-MaximumSubgraphScoreinaTree.cpp
@@ -1,50 +1,49 @@
-// Last updated: 12/11/2025, 7:31:37 PM
+// Last updated: 12/11/2025, 7:35:30 PM
 1class Solution {
 2public:
-3    vector<int> maxSubgraphScore(int n, vector<vector<int>>& edges, vector<int>& good) {
-4        unordered_map<int, vector<int>> graph;
-5        for (int i=0; i<n; i++) {
-6            good[i] = good[i] == 0 ? -1 : 1;
-7        }
-8
-9        vector<int> costs(n, 0);
-10
-11        for (auto& edge: edges) {
-12            graph[edge[0]].push_back(edge[1]);
-13            graph[edge[1]].push_back(edge[0]);
-14        }
-15
-16        function<int(int,int)> dfs = [&](int x, int par) -> int {
-17            int total_cost = good[x];
-18            for (int y: graph[x]) {
-19                if (y == par) continue;
-20                int cost = dfs(y, x);
-21                total_cost += max(0, cost);
-22            }
-23            costs[x] = total_cost;
-24            return total_cost;
-25        };
-26
-27        dfs(0, -1);
-28        vector<int> ret(n, 0);
+3    int dfs(unordered_map<int, vector<int>>& graph, vector<int>& good, vector<int>& costs, int x, int par) {
+4        int total_cost = good[x];
+5        for (int y: graph[x]) {
+6            if (y == par) continue;
+7            int cost = dfs(graph, good, costs, y, x);
+8            total_cost += max(0, cost);
+9        }
+10        costs[x] = total_cost;
+11        return total_cost;
+12    }
+13
+14    void dfs2(unordered_map<int, vector<int>>& graph, vector<int>& costs, vector<int>& ret, int x, int par, int cost_so_far) {
+15        int cost = costs[x];
+16            
+17        if (cost <= 0) {
+18            cost_so_far += cost;
+19            ret[x] = max(cost, cost_so_far);
+20        } else {
+21            ret[x] = max(cost, cost_so_far);
+22        }
+23
+24        for (int y: graph[x]) {
+25            if (y == par) continue;
+26            dfs2(graph, costs, ret, y, x, max(cost_so_far, cost));
+27        }
+28    }
 29
-30        function<void(int,int,int)> dfs2 = [&](int x, int par, int cost_so_far) {
-31            int cost = costs[x];
-32            
-33            if (cost <= 0) {
-34                cost_so_far += cost;
-35                ret[x] = max(cost, cost_so_far);
-36            } else {
-37                ret[x] = max(cost, cost_so_far);
-38            }
-39
-40            for (int y: graph[x]) {
-41                if (y == par) continue;
-42                dfs2(y, x, max(cost_so_far, cost));
-43            }
-44        };
+30    vector<int> maxSubgraphScore(int n, vector<vector<int>>& edges, vector<int>& good) {
+31        vector<int> ret(n, 0);
+32        vector<int> costs(n, 0);
+33        unordered_map<int, vector<int>> graph;
+34        for (int i=0; i<n; i++) {
+35            good[i] = good[i] == 0 ? -1 : 1;
+36        }
+37
+38        for (auto& edge: edges) {
+39            graph[edge[0]].push_back(edge[1]);
+40            graph[edge[1]].push_back(edge[0]);
+41        }
+42
+43        dfs(graph, good, costs, 0, -1);
+44        dfs2(graph, costs, ret, 0, -1, costs[0]);
 45        
-46        dfs2(0, -1, costs[0]);
-47        return ret;
-48    }
-49};
+46        return ret;
+47    }
+48};
