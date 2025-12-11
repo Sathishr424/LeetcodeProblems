0c3ff65b8a954f8c5d7330c4827aea1108ca9b36
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-11 11:32:12 +0530

    File updated at 12/11/2025, 11:32:12 AM

--- 3531-CountCoveredBuildings/3531-CountCoveredBuildings.cpp
+++ 3531-CountCoveredBuildings/3531-CountCoveredBuildings.cpp
@@ -1,31 +1,35 @@
-// Last updated: 12/11/2025, 11:29:09 AM
+// Last updated: 12/11/2025, 11:32:12 AM
 1class Solution {
 2public:
 3    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-4        unordered_map<int, int> v_min, v_max, h_min, h_max;
-5
-6        for (auto cord: buildings) {
-7            int x = cord[0];
-8            int y = cord[1];
+4        n++;
+5        vector<int> h_min(n, INT_MAX);
+6        vector<int> h_max(n, 0);
+7        vector<int> v_min(n, INT_MAX);
+8        vector<int> v_max(n, 0);
 9
-10            h_min[x] = min(h_min[x] == 0 ? y : h_min[x], y);
-11            h_max[x] = max(h_max[x], y);
-12
-13            v_min[y] = min(v_min[y] == 0 ? x : v_min[y], x);
-14            v_max[y] = max(v_max[y], x);
-15            
-16        }
-17
-18        int count = 0;
-19        for (auto cord: buildings) {
-20            int x = cord[0];
-21            int y = cord[1];
-22
-23            if (h_min[x] < y && h_max[x] > y && v_min[y] < x && v_max[y] > x) {
-24                count++;
-25            }
-26        }
-27
-28        return count;
-29    }
-30};
+10        for (auto cord: buildings) {
+11            int x = cord[0];
+12            int y = cord[1];
+13
+14            h_min[x] = min(h_min[x], y);
+15            h_max[x] = max(h_max[x], y);
+16
+17            v_min[y] = min(v_min[y], x);
+18            v_max[y] = max(v_max[y], x);
+19            
+20        }
+21
+22        int count = 0;
+23        for (auto cord: buildings) {
+24            int x = cord[0];
+25            int y = cord[1];
+26
+27            if (h_min[x] < y && h_max[x] > y && v_min[y] < x && v_max[y] > x) {
+28                count++;
+29            }
+30        }
+31
+32        return count;
+33    }
+34};
