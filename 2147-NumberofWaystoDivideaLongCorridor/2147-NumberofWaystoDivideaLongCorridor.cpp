99f18d590c5aff74e46b1f166d942fb262d40715
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-14 14:01:27 +0530

    File updated at 12/14/2025, 2:01:27 PM

--- 2147-NumberofWaystoDivideaLongCorridor/2147-NumberofWaystoDivideaLongCorridor.cpp
+++ 2147-NumberofWaystoDivideaLongCorridor/2147-NumberofWaystoDivideaLongCorridor.cpp
@@ -1,4 +1,4 @@
-// Last updated: 12/14/2025, 2:01:00 PM
+// Last updated: 12/14/2025, 2:01:27 PM
 1const int N = 1e5 + 1;
 2int dp[N][3];
 3
@@ -8,34 +8,32 @@
 7        int mod = 1e9 + 7;
 8        int n = corridor.length();
 9        for (int i=0; i<=n; i++) {
-10            dp[i][0] = 0;
-11            dp[i][1] = 0;
-12            dp[i][2] = 0;
-13        }
-14        dp[0][0] = 1;
-15
-16        for (int i=0; i<n; i++) {
-17            char c = corridor[i];
-18            for (int s=0; s<=2; s++) {
-19                if (c == 'S') {
-20                    if (s == 2) {
-21                        dp[i + 1][1] += dp[i][s];
-22                        dp[i + 1][1] %= mod;
-23                    } else {
-24                        dp[i + 1][s + 1] += dp[i][s];
-25                        dp[i + 1][s + 1] %= mod;
-26                    }
-27                } else {
-28                    if (s == 2) {
-29                        dp[i + 1][0] += dp[i][s];
-30                        dp[i + 1][0] %= mod;
-31                    }
-32                    dp[i + 1][s] += dp[i][s];
-33                    dp[i + 1][s] %= mod;
-34                }
-35            }
-36        }
-37
-38        return dp[n][2];
-39    }
-40};
+10            memset(dp[i], 0, sizeof(dp[0]));
+11        }
+12        dp[0][0] = 1;
+13
+14        for (int i=0; i<n; i++) {
+15            char c = corridor[i];
+16            for (int s=0; s<=2; s++) {
+17                if (c == 'S') {
+18                    if (s == 2) {
+19                        dp[i + 1][1] += dp[i][s];
+20                        dp[i + 1][1] %= mod;
+21                    } else {
+22                        dp[i + 1][s + 1] += dp[i][s];
+23                        dp[i + 1][s + 1] %= mod;
+24                    }
+25                } else {
+26                    if (s == 2) {
+27                        dp[i + 1][0] += dp[i][s];
+28                        dp[i + 1][0] %= mod;
+29                    }
+30                    dp[i + 1][s] += dp[i][s];
+31                    dp[i + 1][s] %= mod;
+32                }
+33            }
+34        }
+35
+36        return dp[n][2];
+37    }
+38};
