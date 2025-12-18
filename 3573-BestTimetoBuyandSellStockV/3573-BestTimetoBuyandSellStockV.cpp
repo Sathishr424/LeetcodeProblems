7b6c7f6bdcb943141c29315f6ddc2b6a4146a4f4
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-18 13:38:37 +0530

    File updated at 12/18/2025, 1:38:37 PM

--- 3573-BestTimetoBuyandSellStockV/3573-BestTimetoBuyandSellStockV.cpp
+++ 3573-BestTimetoBuyandSellStockV/3573-BestTimetoBuyandSellStockV.cpp
@@ -1,33 +1,29 @@
-// Last updated: 12/18/2025, 1:19:17 PM
-1const int N = 1e5 + 1;
-2const long inf = LONG_MAX;
-3long long dp[N][2];
-4
-5class Solution {
-6public:
-7    long long rec(int index, int done, vector<long long>& prefix, vector<int>& prices, vector<int>& strategy, int k, int n) {
-8        if (dp[index][done] != inf) return dp[index][done];
-9        if (index == n) {
-10            return 0;
-11        }
-12
-13        long long ans = rec(index + 1, done, prefix, prices, strategy, k, n) + (strategy[index] * prices[index]);
-14        if (done == 0 && index + k <= n) {
-15            ans = max(ans, rec(index + k, 1, prefix, prices, strategy, k, n) + (prefix[index + k] - prefix[index + (k / 2)]));
-16        }
-17
-18        dp[index][done] = ans;
-19        return ans;
-20    }
-21
-22    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
-23        int n = prices.size();
-24        vector<long long> prefix(n+1, 0);
-25        for (int i=0; i<n; i++) {
-26            prefix[i + 1] = prefix[i] + prices[i];
-27        }
-28        fill(&dp[0][0], &dp[0][0] + 0LL + N * 2, inf);
-29
-30        return rec(0, 0, prefix, prices, strategy, k, n);
-31    }
-32};
+// Last updated: 12/18/2025, 1:38:37 PM
+1class Solution {
+2public:
+3    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+4        int n = prices.size();
+5        vector<long long> prefix(n+1, 0);
+6        long long best = 0;
+7        for (int i=0; i<n; i++) {
+8            prefix[i + 1] = prefix[i] + prices[i];
+9            best += prices[i] * strategy[i];
+10        }
+11
+12        int half = k / 2;
+13        long long left = 0;
+14        long long right = 0;
+15        for (int i=k; i<n; i++) {
+16            right += (strategy[i] * prices[i]);
+17        }
+18
+19        for (int i=0; i<n-k+1; i++) {
+20            best = max(best, left + (prefix[i + k] - prefix[i + half]) + right);
+21            left += (strategy[i] * prices[i]);
+22            if (i + k < n)
+23                right -= (strategy[i + k] * prices[i + k]);
+24        }
+25        
+26        return best;
+27    }
+28};
