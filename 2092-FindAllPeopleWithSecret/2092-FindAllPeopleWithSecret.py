cd10001ba38d5845e0ff94201214a81d21b74131
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-19 13:56:40 +0530

    File updated at 12/19/2025, 1:56:40 PM

--- 2092-FindAllPeopleWithSecret/2092-FindAllPeopleWithSecret.py
+++ 2092-FindAllPeopleWithSecret/2092-FindAllPeopleWithSecret.py
@@ -1,74 +1,27 @@
-# Last updated: 12/8/2025, 3:36:54 pm
-class Union:
-    def __init__(self, n):
-        self.parents = [i for i in range(n)]
-        self.sizes = [1] * n
-
-    def find(self, x):
-        if x != self.parents[x]:
-            self.parents[x] = self.find(self.parents[x])
-
-        return self.parents[x]
-
-    def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-
-        if x == y: return True
-
-        if self.sizes[y] > self.sizes[x]:
-            x, y = y, x
-
-        self.sizes[x] += self.sizes[y]
-        self.parents[y] = x
-        
-        return False
-
-class Solution:
-    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        meetings.sort(key=lambda x: x[2])
-
-        un = Union(n)
-        un.union(0, firstPerson)
-
-        linked = []
-        last_added = defaultdict(list)
-        prev = -1
-
-        def dfs(x, vis):
-            if x in vis: return
-            vis[x] = 1
-            for y in last_added[x]:
-                un.union(x, y)
-                dfs(y, vis)
-        
-        for x, y, t in meetings:
-            if t != prev:
-                vis = {}
-                for node in linked:
-                    dfs(node, vis)
-                linked = []
-                last_added = defaultdict(list)
-                    
-            if un.find(x) == 0 or un.find(y) == 0:
-                un.union(x, y)
-                linked.append(x)
-                linked.append(y)
-            else:
-                last_added[x].append(y)
-                last_added[y].append(x)
-
-            prev = t
-
-        vis = {}
-        for node in linked:
-            dfs(node, vis)
-        
-        ret = []
-
-        for i in range(n):
-            if un.find(i) == 0:
-                ret.append(i)
-
-        return ret
-        
+# Last updated: 12/19/2025, 1:56:40 PM
+1class Solution:
+2    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+3
+4        heaps = [[] for _ in range(n)]
+5        for x, y, t in meetings:
+6            heaps[x].append((t, y))
+7            heaps[y].append((t, x))
+8
+9        for i in range(n):
+10            heaps[i].sort()
+11
+12        heap = heaps[firstPerson] + heaps[0]
+13        heapq.heapify(heap)
+14        ret = set()
+15        ret.add(0)
+16        ret.add(firstPerson)
+17        while heap:
+18            t, y = heapq.heappop(heap)
+19            ret.add(y)
+20            while len(heaps[y]) > 0 and heaps[y][-1][0] >= t:
+21                tmp = heaps[y].pop()
+22                heapq.heappush(heap, tmp)
+23
+24        return list(ret)
+25
+26
