d3d50e0d5543f928a8018a7c9427349df10b239e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-19 13:58:49 +0530

    File updated at 12/19/2025, 1:58:48 PM

--- 2092-FindAllPeopleWithSecret/2092-FindAllPeopleWithSecret.py
+++ 2092-FindAllPeopleWithSecret/2092-FindAllPeopleWithSecret.py
@@ -1,27 +1,26 @@
-# Last updated: 12/19/2025, 1:56:40 PM
+# Last updated: 12/19/2025, 1:58:48 PM
 1class Solution:
 2    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-3
-4        heaps = [[] for _ in range(n)]
-5        for x, y, t in meetings:
-6            heaps[x].append((t, y))
-7            heaps[y].append((t, x))
-8
-9        for i in range(n):
-10            heaps[i].sort()
-11
-12        heap = heaps[firstPerson] + heaps[0]
-13        heapq.heapify(heap)
-14        ret = set()
-15        ret.add(0)
-16        ret.add(firstPerson)
-17        while heap:
-18            t, y = heapq.heappop(heap)
-19            ret.add(y)
-20            while len(heaps[y]) > 0 and heaps[y][-1][0] >= t:
-21                tmp = heaps[y].pop()
-22                heapq.heappush(heap, tmp)
-23
-24        return list(ret)
+3        relations = [[] for _ in range(n)]
+4        for x, y, t in meetings:
+5            relations[x].append((t, y))
+6            relations[y].append((t, x))
+7
+8        for i in range(n):
+9            relations[i].sort()
+10
+11        heap = relations[firstPerson] + relations[0]
+12        heapq.heapify(heap)
+13        ret = set()
+14        ret.add(0)
+15        ret.add(firstPerson)
+16        while heap:
+17            t, y = heapq.heappop(heap)
+18            ret.add(y)
+19            while len(relations[y]) > 0 and relations[y][-1][0] >= t:
+20                tmp = relations[y].pop()
+21                heapq.heappush(heap, tmp)
+22
+23        return list(ret)
+24
 25
-26
