dd8ccf9349564038ec4f2668aec2a4b82e9a95ad
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-22 15:48:14 +0530

    File updated at 22/12/2025, 3:48:14 pm

--- 960-DeleteColumnstoMakeSortedIII/960-DeleteColumnstoMakeSortedIII.py
+++ 960-DeleteColumnstoMakeSortedIII/960-DeleteColumnstoMakeSortedIII.py
@@ -1,24 +1,27 @@
-# Last updated: 22/12/2025, 3:45:59 pm
+# Last updated: 22/12/2025, 3:48:14 pm
 1class Solution:
 2    def minDeletionSize(self, st: List[str]) -> int:
 3        n = len(st)
 4        m = len(st[0])
 5
-6        @cache
-7        def rec(index, prev_index, prev_prev_index):
-8            if index == m: return 0
-9            
-10            ans = rec(index + 1, prev_index, prev_prev_index) + 1
-11            for i in range(n):
-12                if st[i][index] < st[i][prev_index]:
-13                    if prev_prev_index != -1:
-14                        for i2 in range(n):
-15                            if st[i2][index] < st[i2][prev_prev_index]:
-16                                return ans
-17                    return min(ans, rec(index + 1, index, prev_prev_index) + 1)
-18            
-19            return min(ans, rec(index + 1, index, prev_index))
-20        
-21        ans = rec(1, 0, -1)
-22        rec.cache_clear()
-23        return ans
+6        def isAllSmaller(index, prev_index):
+7            for i in range(n):
+8                if st[i][index] < st[i][prev_index]: return False
+9            return True
+10
+11        @cache
+12        def rec(index, prev_index, prev_prev_index):
+13            if index == m: return 0
+14            
+15            ans = rec(index + 1, prev_index, prev_prev_index) + 1
+16            for i in range(n):
+17                if st[i][index] < st[i][prev_index]:
+18                    if prev_prev_index != -1 and not isAllSmaller(index, prev_prev_index):
+19                        return ans
+20                    return min(ans, rec(index + 1, index, prev_prev_index) + 1)
+21            
+22            return min(ans, rec(index + 1, index, prev_index))
+23        
+24        ans = rec(1, 0, -1)
+25        rec.cache_clear()
+26        return ans
