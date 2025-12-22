d43617f81e10946a9a28b8ceed72e48dc2fc053e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-22 15:59:28 +0530

    File updated at 22/12/2025, 3:59:28 pm

--- 960-DeleteColumnstoMakeSortedIII/960-DeleteColumnstoMakeSortedIII.cpp
+++ 960-DeleteColumnstoMakeSortedIII/960-DeleteColumnstoMakeSortedIII.cpp
@@ -1,16 +1,16 @@
-// Last updated: 22/12/2025, 3:57:25 pm
+// Last updated: 22/12/2025, 3:59:28 pm
 1int dp[101][101][102];
 2
 3class Solution {
 4public:
-5    bool isCurrentIndexLargerThanPrev(int index, int prev_index, vector<string>& strs, int n) {
+5    bool isCurrentIndexLargerThanPrev(int index, int prev_index, vector<string>& strs, int& n) {
 6        for (int i=0; i<n; i++) {
 7            if (strs[i][index] < strs[i][prev_index]) return false;
 8        }
 9        return true;
 10    }
 11
-12    int rec(int index, int prev_index, int prev_prev_index, vector<string>& strs, int n, int m) {
+12    int rec(int index, int prev_index, int prev_prev_index, vector<string>& strs, int& n, int& m) {
 13        if (dp[index][prev_index][prev_prev_index] != -1) return dp[index][prev_index][prev_prev_index];
 14        if (index == m) return 0;
 15
