a002e2f73ebed4d7b29c5d67894fd79a4cee5a82
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-25 19:08:31 +0530

    Create 4069-CountWaysToChooseCoprimeIntegersFromRows

---
+++ 4069-CountWaysToChooseCoprimeIntegersFromRows/4069-CountWaysToChooseCoprimeIntegersFromRows.py
@@ -0,0 +1,23 @@
+# Last updated: 12/25/2025, 7:08:31 PM
+class Solution:
+    def countCoprime(self, mat: List[List[int]]) -> int:
+        m = len(mat)
+        n = len(mat[0])
+        maxi = max( [max(mat[i]) for i in range(m)] ) + 1
+        
+        mod = 10**9 + 7
+        dp = [[-1] * maxi for _ in range(m)]
+
+        def rec(i, g):
+            if i == m:
+                return 1 if g == 1 else 0
+            if dp[i][g] != -1: return dp[i][g]
+
+            ans = 0
+            for j in range(n):
+                ans += rec(i + 1, gcd(g, mat[i][j]))
+
+            dp[i][g] = ans % mod
+            return dp[i][g]
+
+        return rec(0, 0)
