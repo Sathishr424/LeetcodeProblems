e412895346951e3a73285072d2f0df7291e469b0
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-25 19:09:56 +0530

    Create 3959-MaximumTotalFromOptimalActivationOrder

---
+++ 3959-MaximumTotalFromOptimalActivationOrder/3959-MaximumTotalFromOptimalActivationOrder.py
@@ -0,0 +1,33 @@
+# Last updated: 12/25/2025, 7:09:55 PM
+class Solution:
+    def maxTotal(self, value: List[int], limit: List[int]) -> int:
+        n = len(value)
+        new_arr = []
+
+        for i in range(n):
+            new_arr.append((limit[i], value[i], i))
+        new_arr.sort(key=lambda x: (x[0], -x[1]))
+
+        heap = []
+        for i in range(n):
+            heapq.heappush(heap, (limit[i], i))
+        
+        can_use = [1] * n
+        active = 0
+        ret = 0
+        used = [0] * n
+
+        for i in range(n):
+            if can_use[new_arr[i][2]] and new_arr[i][0] > active:
+                ret += new_arr[i][1]
+                used[new_arr[i][2]] = 1
+                active += 1
+            
+            old_active = active
+            while heap and heap[0][0] <= old_active:
+                _, index = heapq.heappop(heap)
+                can_use[index] = 0
+                if used[index]: active -= 1
+        
+        return ret
+        
