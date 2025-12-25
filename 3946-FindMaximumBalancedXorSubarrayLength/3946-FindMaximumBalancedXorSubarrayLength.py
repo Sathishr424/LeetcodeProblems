90e9160f654309b1ebb8be36e8ee2b76cc93cfd2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-25 19:10:28 +0530

    Create 3946-FindMaximumBalancedXorSubarrayLength

---
+++ 3946-FindMaximumBalancedXorSubarrayLength/3946-FindMaximumBalancedXorSubarrayLength.py
@@ -0,0 +1,29 @@
+# Last updated: 12/25/2025, 7:10:28 PM
+class Solution:
+    def maxBalancedSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        def findAns(nums):
+            ans = 0
+            hash = defaultdict(dict)
+            hash[0][0] = -1
+            even = 0
+            odd = 0
+            xor = 0
+            for i in range(n):
+                if nums[i] % 2 == 0:
+                    even += 1
+                else:
+                    odd += 1
+                xor ^= nums[i]
+                diff = even - odd
+                if diff in hash and xor in hash[diff]:
+                    ans = max(ans, i - hash[diff][xor])
+                if xor not in hash[diff]:
+                    hash[diff][xor] = i
+
+                # print(i, nums, diff, xor, dict(hash))
+            
+            return ans
+        
+        return findAns(nums)
