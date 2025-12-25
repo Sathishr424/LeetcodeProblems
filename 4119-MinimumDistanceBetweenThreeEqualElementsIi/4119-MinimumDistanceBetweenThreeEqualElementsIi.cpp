1a11580e8deabc2411ef73778d489405f6f2b6c5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-25 19:08:08 +0530

    Create 4119-MinimumDistanceBetweenThreeEqualElementsIi

---
+++ 4119-MinimumDistanceBetweenThreeEqualElementsIi/4119-MinimumDistanceBetweenThreeEqualElementsIi.cpp
@@ -0,0 +1,29 @@
+// Last updated: 12/25/2025, 7:08:08 PM
+#include <iostream>
+#include <ostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    static int minimumDistance(vector<int>& nums) {
+        unordered_map<int, vector<int>> indexes;
+
+        int min_diff = INT_MAX;
+        for (int index = 0; index < nums.size(); index++) {
+            vector<int>& curr_nums = indexes[nums[index]];
+            curr_nums.push_back(index);
+            if (curr_nums.size() >= 3) {
+                int n = curr_nums.size();
+                int k = curr_nums[n - 1];
+                int j = curr_nums[n - 2];
+                int i = curr_nums[n - 3];
+                min_diff = min(min_diff, (j - i) + (k - j) + (k - i));
+            }
+        }
+
+        return min_diff == INT_MAX ? -1 : min_diff;
+    }
+};
