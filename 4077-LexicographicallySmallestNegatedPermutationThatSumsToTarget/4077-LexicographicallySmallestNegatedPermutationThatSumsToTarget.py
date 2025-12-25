0c32bfdca2669bcf31fb2703f98e865fd5b32cd5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-25 19:08:27 +0530

    Create 4077-LexicographicallySmallestNegatedPermutationThatSumsToTarget

---
+++ 4077-LexicographicallySmallestNegatedPermutationThatSumsToTarget/4077-LexicographicallySmallestNegatedPermutationThatSumsToTarget.py
@@ -0,0 +1,28 @@
+# Last updated: 12/25/2025, 7:08:27 PM
+class Solution:
+    def lexSmallestNegatedPerm(self, n: int, target: int) -> List[int]:
+        total = n * (n + 1) / 2
+
+        if total < target: return []
+        if -total > target: return []
+        if total == target: return [i for i in range(1, n+1)]
+
+        rem = 0
+        ret = []
+        used = {}
+        other = total
+        for i in range(n, 0, -1):
+            if (rem - i) + (other - i) >= target:
+                rem -= i
+                used[i] = 1
+                other -= i
+
+        if rem+other != target: return []
+        for num in used:
+            ret.append(-num)
+
+        for i in range(1, n + 1):
+            if i not in used:
+                ret.append(i)
+
+        return ret
