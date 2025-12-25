d78b411385ce08ab0e2847841aa55e851f1962be
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-25 19:08:40 +0530

    Create 4057-TotalWavinessOfNumbersInRangeI

---
+++ 4057-TotalWavinessOfNumbersInRangeI/4057-TotalWavinessOfNumbersInRangeI.py
@@ -0,0 +1,15 @@
+# Last updated: 12/25/2025, 7:08:40 PM
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        ans = 0
+        for num in range(num1, num2+1):
+            num = str(num)
+            w = 0
+            for i in range(1, len(num) - 1):
+                if num[i] > num[i-1] and num[i] > num[i + 1]:
+                    w += 1
+                elif num[i] < num[i-1] and num[i] < num[i + 1]:
+                    w += 1
+            ans += w
+
+        return ans
