2220da0fe2f2b09a5c16b57e669f7f275fd54df2
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-27 20:35:05 +0530

    File updated at 12/27/2025, 8:35:05 PM

--- 3785-MinimumSwapstoAvoidForbiddenValues/3785-MinimumSwapstoAvoidForbiddenValues.py
+++ 3785-MinimumSwapstoAvoidForbiddenValues/3785-MinimumSwapstoAvoidForbiddenValues.py
@@ -1,4 +1,4 @@
-# Last updated: 12/27/2025, 8:32:19 PM
+# Last updated: 12/27/2025, 8:35:05 PM
 1class Solution:
 2    def minSwaps(self, nums: List[int], forbidden: List[int]) -> int:
 3        n = len(nums)
@@ -8,25 +8,24 @@
 7        both = defaultdict(lambda: defaultdict(int))
 8        bottom = defaultdict(int)
 9
-10        for i in range(n):
-11            num = nums[i]
-12            fb = forbidden[i]
-13
-14            if num == fb:
-15                need[num] += 1
-16            
-17            top[num] += 1
-18            bottom[fb] += 1
-19            both[num][fb] += 1
-20    
-21        for num in need:
-22            cnt = top[num] + bottom[num] - both[num][num]
-23            rem = n - cnt
-24            if rem < need[num]: return -1
-25
-26        op = 0
-27        arr = [need[num] for num in need]
-28        if len(arr) == 0: return op
+10        s = 0
+11        maxi = 0
+12        for i in range(n):
+13            num = nums[i]
+14            fb = forbidden[i]
+15
+16            if num == fb:
+17                s += 1
+18                need[num] += 1
+19                maxi = max(maxi, need[num])
+20            
+21            top[num] += 1
+22            bottom[fb] += 1
+23            both[num][fb] += 1
+24    
+25        for num in need:
+26            cnt = top[num] + bottom[num] - both[num][num]
+27            rem = n - cnt
+28            if rem < need[num]: return -1
 29
-30        s = sum(arr)
-31        return max(max(arr), (s + 1) // 2)
+30        return max(maxi, (s + 1) // 2)
