3a6854fad515c305609878b041a8d070247f9407
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2025-12-31 13:20:03 +0530

    File updated at 12/31/2025, 1:20:03 PM

--- 1970-LastDayWhereYouCanStillCross/1970-LastDayWhereYouCanStillCross.cpp
+++ 1970-LastDayWhereYouCanStillCross/1970-LastDayWhereYouCanStillCross.cpp
@@ -1,4 +1,4 @@
-// Last updated: 12/31/2025, 1:13:24 PM
+// Last updated: 12/31/2025, 1:20:03 PM
 1class UnionFind {
 2public:
 3    vector<int> parents;
@@ -42,49 +42,51 @@
 41    vector<pair<int, int>> dirs = {{1, 0}, {0, 1}};
 42    bool isGood(int& n, int& day, int& row, int& col, vector<vector<int>>& cells) {
 43        UnionFind uf(n);
-44        vector<vector<int>> grid(row, vector<int>(col, 0));
+44        int *grid = (int*)malloc(row * col * sizeof(int));
 45
 46        for (int i=0; i<day; i++) {
-47            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
-48        }
-49
-50        for (int i=0; i<row; i++) {
-51            for (int j=0; j<col; j++) {
-52                if (grid[i][j] == 1) continue;
+47            int pos = (cells[i][0] - 1) * col + (cells[i][1] - 1);
+48            grid[pos] = 1;
+49        }
+50
+51        for (int i=0; i<row; i++) {
+52            for (int j=0; j<col; j++) {
 53                int pos = i * col + j;
-54                for (auto& d: dirs) {
-55                    int i2 = i + d.first;
-56                    int j2 = j + d.second;
-57                    if (i2 >= 0 && i2 < row && j2 >= 0 && j2 < col && grid[i2][j2] != 1) {
-58                        uf.join(pos, i2 * col + j2);
-59                    }
-60                }
-61            }
-62        }
-63
-64        int last = (row  - 1) * col;
-65        for (int i=1; i<col; i++) {
-66            uf.join(i, i-1);
-67            uf.join(last + i, last + i - 1);
-68        }
-69        return uf.find(0) == uf.find(n-1);
-70    }
-71
-72    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-73        int l = 0;
-74        int r = cells.size();
-75        int n = row * col;
-76
-77        while (l < r) {
-78            int mid = (l + r + 1) / 2;
-79
-80            if (isGood(n, mid, row, col, cells)) {
-81                l = mid;
-82            } else {
-83                r = mid - 1;
-84            }
-85        }
-86
-87        return l;
-88    }
-89};
+54                if (grid[pos] == 1) continue;
+55                for (auto& d: dirs) {
+56                    int i2 = i + d.first;
+57                    int j2 = j + d.second;
+58                    if (i2 >= 0 && i2 < row && j2 >= 0 && j2 < col && grid[i2 * col + j2] != 1) {
+59                        uf.join(pos, i2 * col + j2);
+60                    }
+61                }
+62            }
+63        }
+64        free(grid);
+65
+66        int last = (row  - 1) * col;
+67        for (int i=1; i<col; i++) {
+68            uf.join(i, i-1);
+69            uf.join(last + i, last + i - 1);
+70        }
+71        return uf.find(0) == uf.find(n-1);
+72    }
+73
+74    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+75        int l = 0;
+76        int r = cells.size();
+77        int n = row * col;
+78
+79        while (l < r) {
+80            int mid = (l + r + 1) / 2;
+81
+82            if (isGood(n, mid, row, col, cells)) {
+83                l = mid;
+84            } else {
+85                r = mid - 1;
+86            }
+87        }
+88
+89        return l;
+90    }
+91};
