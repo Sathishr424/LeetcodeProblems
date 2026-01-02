fec9291ab44231cadc1179dc11dc89832c09588c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-01-02 11:45:00 +0530

    File updated at 1/2/2026, 11:44:59 AM

--- 66-PlusOne/66-PlusOne.cpp
+++ 66-PlusOne/66-PlusOne.cpp
@@ -1,26 +1,22 @@
-// Last updated: 1/1/2026, 12:29:04 PM
+// Last updated: 1/2/2026, 11:44:59 AM
 1class Solution {
 2public:
 3    vector<int> plusOne(vector<int>& digits) {
 4        int n = digits.size();
-5        int cnt = 0;
-6        for (int i=n-1; i>=0; i--) {
-7            if (digits[i] != 9) {
-8                digits[i] += 1;
-9                break;
-10            }
-11            digits[i] = 0;
-12            cnt += 1;
-13        }
-14
-15        if (cnt == n) {
-16            digits.push_back(0);
-17            for (int i=n-1; i>=0; i--) {
-18                digits[i + 1] = digits[i];
-19            }
-20            digits[0] = 1;
-21        }
-22
-23        return digits;
-24    }
-25};
+5        for (int i=n-1; i>=0; i--) {
+6            if (digits[i] != 9) {
+7                digits[i] += 1;
+8                return digits;
+9            }
+10            digits[i] = 0;
+11        }
+12
+13        digits.push_back(0);
+14        for (int i=n-1; i>=0; i--) {
+15            digits[i + 1] = digits[i];
+16        }
+17        digits[0] = 1;
+18
+19        return digits;
+20    }
+21};
