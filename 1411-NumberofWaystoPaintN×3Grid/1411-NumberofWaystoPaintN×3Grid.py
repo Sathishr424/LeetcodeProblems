e5eb7ff3218d8b527a19c877d68be2079ba87d8a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-01-03 14:04:50 +0530

    File updated at 1/3/2026, 2:04:50 PM

--- 1411-NumberofWaystoPaintN×3Grid/1411-NumberofWaystoPaintN×3Grid.py
+++ 1411-NumberofWaystoPaintN×3Grid/1411-NumberofWaystoPaintN×3Grid.py
@@ -1,42 +1,40 @@
-# Last updated: 1/3/2026, 2:04:15 PM
-1class Solution:
-2    def numOfWays(self, n: int) -> int:
-3        mod = 10**9 + 7
-4        vals = []
-5        stack = ["0", "1", "2"]
-6        while stack:
-7            prev = stack.pop()
-8            if len(prev) == 3:
-9                vals.append(prev)
-10                continue
-11            for i in range(3):
-12                if str(i) != prev[-1]:
-13                    stack.append(prev + str(i))
-14            
-15        graph = defaultdict(list)
-16        m = len(vals)
-17        for i in range(m):
-18            x = vals[i]
-19            for j in range(i+1, m):
-20                y = vals[j]
-21
-22                if x[0] != y[0] and x[1] != y[1] and x[2] != y[2]:
-23                    graph[x].append(y)
-24                    graph[y].append(x)
-25
-26
-27        @cache
-28        def rec(x, rem):
-29            if rem == 0: return 1
-30            ans = 0
-31            for y in graph[x]:
-32                ans += rec(y, rem - 1)
-33            return ans % mod
-34
-35        ans = 0
-36        for val in vals:
-37            ans += rec(val, n-1)
-38            ans %= mod
-39        
-40        rec.cache_clear()
-41        return ans
+# Last updated: 1/3/2026, 2:04:50 PM
+1mod = 10**9 + 7
+2vals = []
+3stack = ["0", "1", "2"]
+4while stack:
+5    prev = stack.pop()
+6    if len(prev) == 3:
+7        vals.append(prev)
+8        continue
+9    for i in range(3):
+10        if str(i) != prev[-1]:
+11            stack.append(prev + str(i))
+12    
+13graph = defaultdict(list)
+14m = len(vals)
+15for i in range(m):
+16    x = vals[i]
+17    for j in range(i+1, m):
+18        y = vals[j]
+19
+20        if x[0] != y[0] and x[1] != y[1] and x[2] != y[2]:
+21            graph[x].append(y)
+22            graph[y].append(x)
+23
+24@cache
+25def rec(x, rem):
+26    if rem == 0: return 1
+27    ans = 0
+28    for y in graph[x]:
+29        ans += rec(y, rem - 1)
+30    return ans % mod
+31
+32class Solution:
+33    def numOfWays(self, n: int) -> int:
+34        ans = 0
+35        for val in vals:
+36            ans += rec(val, n-1)
+37            ans %= mod
+38        
+39        return ans
