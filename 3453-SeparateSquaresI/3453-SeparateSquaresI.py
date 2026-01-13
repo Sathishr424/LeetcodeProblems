0c6de54995bd028eadd760543d175107d48c565b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-01-14 00:33:32 +0530

    File updated at 1/14/2026, 12:33:32 AM

--- 3453-SeparateSquaresI/3453-SeparateSquaresI.py
+++ 3453-SeparateSquaresI/3453-SeparateSquaresI.py
@@ -1,35 +1,35 @@
-# Last updated: 2/4/2025, 2:02:47 am
-class Solution:
-    def separateSquares(self, squares: List[List[int]]) -> float:
-        n = len(squares)
-        left = 0
-        right = 0
-
-        for x, y, l in squares:
-            left = min(y, left)
-            right = max(y+l, right)
-        
-        precision = 1 / (10**5)
-        while right - left > precision:
-            mid = (left+right) / 2
-
-            top = 0
-            bottom = 0
-
-            for x, y, l in squares:
-                if y < mid and y+l > mid:
-                    b = mid-y
-                    t = l - b
-                    top += t * l
-                    bottom += b * l
-                elif y < mid:
-                    bottom += l*l
-                else:
-                    top += l*l
-            
-            if top > bottom:
-                left = mid
-            else:
-                right = mid
-
-        return left
+# Last updated: 1/14/2026, 12:33:32 AM
+1class Solution:
+2    def separateSquares(self, squares: List[List[int]]) -> float:
+3        n = len(squares)
+4        left = 0
+5        right = 0
+6
+7        for x, y, l in squares:
+8            left = min(y, left)
+9            right = max(y+l, right)
+10        
+11        precision = 1 / (10**5)
+12        while right - left > precision:
+13            mid = (left+right) / 2
+14
+15            top = 0
+16            bottom = 0
+17
+18            for x, y, l in squares:
+19                if y < mid and y+l > mid:
+20                    b = mid-y
+21                    t = l - b
+22                    top += t * l
+23                    bottom += b * l
+24                elif y < mid:
+25                    bottom += l*l
+26                else:
+27                    top += l*l
+28            
+29            if top > bottom:
+30                left = mid
+31            else:
+32                right = mid
+33
+34        return left
