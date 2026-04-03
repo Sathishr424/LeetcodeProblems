ec68f806722f41d3a137c612cf2c40908b40fd28
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-04-03 14:30:34 +0530

    File updated at 4/3/2026, 2:30:34 PM

--- 3418-MaximumAmountofMoneyRobotCanEarn/3418-MaximumAmountofMoneyRobotCanEarn.py
+++ 3418-MaximumAmountofMoneyRobotCanEarn/3418-MaximumAmountofMoneyRobotCanEarn.py
@@ -1,28 +1,27 @@
-# Last updated: 4/2/2026, 11:07:33 PM
-1class Solution:
-2    def maximumAmount(self, coins: List[List[int]]) -> int:
-3        m = len(coins)
-4        n = len(coins[0])
-5
-6        @cache
-7        def rec(i, j, rem):
-8            if i == m-1 and j == n-1: return 0
-9
-10            ans = -inf
-11            if i+1 < m:
-12                if coins[i + 1][j] < 0 and rem:
-13                    ans = max(ans, rec(i + 1, j, rem - 1))
-14                ans = max(ans, rec(i + 1, j, rem) + coins[i + 1][j])
-15            if j+1 < n:
-16                if coins[i][j + 1] < 0 and rem:
-17                    ans = max(ans, rec(i, j + 1, rem - 1))    
-18                ans = max(ans, rec(i, j + 1, rem) + coins[i][j + 1])
-19            
-20            return ans
-21
-22        ans = rec(0, 0, 2) + coins[0][0]
-23        if coins[0][0] < 0:
-24            ans = max(rec(0, 0, 1), ans)
-25        
-26        rec.cache_clear()
-27        return ans
+# Last updated: 4/3/2026, 2:30:34 PM
+1from typing import List
+2
+3inf = 10**20
+4class Solution:
+5    def maximumAmount(self, coins: List[List[int]]) -> int:
+6        m = len(coins)
+7        n = len(coins[0])
+8
+9        dp = [[[-inf] * 3 for _ in range(n)] for _ in range(m)]
+10        dp[0][0][2] = coins[0][0]
+11        dp[0][0][1] = 0
+12        
+13        for i in range(m):
+14            for j in range(n):
+15                for rem in range(2, -1, -1):
+16                    if i + 1 < m:
+17                        dp[i + 1][j][rem] = max(dp[i + 1][j][rem], dp[i][j][rem] + coins[i + 1][j])
+18                        if rem: 
+19                            dp[i + 1][j][rem - 1] = max(dp[i + 1][j][rem - 1], dp[i][j][rem])
+20                    if j + 1 < n:
+21                        dp[i][j + 1][rem] = max(dp[i][j + 1][rem], dp[i][j][rem] + coins[i][j + 1])
+22                        if rem: 
+23                            dp[i][j + 1][rem - 1] = max(dp[i][j + 1][rem - 1], dp[i][j][rem])
+24
+25        return max(dp[-1][-1])
+26
