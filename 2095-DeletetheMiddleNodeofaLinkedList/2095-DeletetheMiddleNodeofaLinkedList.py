e5b22fb15d509fe833df7a0f95765ab9e61cec9c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-06-15 12:10:15 +0530

    File updated at 6/15/2026, 12:10:14 PM

--- 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
+++ 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
@@ -1,20 +1,27 @@
-# Last updated: 12/8/2025, 9:54:41 pm
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head.next == None: return None
-        slow = head
-        fast = head.next
-
-        while fast.next and fast.next.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        slow.next = slow.next.next
-        return head
-
-        
+# Last updated: 6/15/2026, 12:10:14 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+8        slow = head
+9        fast = head
+10        prev_slow = head
+11
+12        while fast and fast.next:
+13            prev_slow = slow
+14            slow = slow.next
+15            fast = fast.next.next
+16        
+17        if prev_slow and prev_slow.next:
+18            prev_slow.next = prev_slow.next.next
+19        else:
+20            return None
+21        
+22        return head
+23
+24
+25
+26
