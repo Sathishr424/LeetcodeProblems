beb88908576467fb0d6dc16e4b46117e93636f70
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-06-17 17:38:31 +0530

    File updated at 6/17/2026, 5:38:31 PM

--- 3614-ProcessStringwithSpecialOperationsII/3614-ProcessStringwithSpecialOperationsII.py
+++ 3614-ProcessStringwithSpecialOperationsII/3614-ProcessStringwithSpecialOperationsII.py
@@ -1,29 +1,44 @@
-# Last updated: 14/7/2025, 5:40:43 pm
-class Solution:
-    def processStr(self, s: str, k: int) -> str:
-        n = len(s)
-        cnt = 0
-
-        for char in s:
-            if char == '*':
-                cnt = max(0, cnt - 1)
-            elif char == '#':
-                cnt += cnt
-            elif char != '%':
-                cnt += 1
-        
-        if cnt <= k: return '.'
-
-        for i in range(n-1, -1, -1):
-            char = s[i]
-            if char == '*':
-                cnt += 1
-            elif char == '#':
-                cnt //= 2
-                if k >= cnt:
-                    k -= cnt
-            elif char == '%':
-                k = cnt - k - 1
-            else:
-                if k == cnt - 1: return char
-                cnt -= 1
+# Last updated: 6/17/2026, 5:38:31 PM
+1class Solution:
+2    def processStr(self, s: str, k: int) -> str:
+3        n = len(s)
+4
+5        cnt = 0
+6        chars = set()
+7        for char in s:
+8            if char == "*":
+9                cnt = max(0, cnt - 1)
+10            elif char == '%':
+11                pass
+12            elif char == '#':
+13                cnt *= 2
+14            else:
+15                chars.add(char)
+16                cnt += 1    
+17        
+18        # print(cnt)
+19        if k >= cnt: return '.'
+20        # print(list(chars))
+21        s = s[::-1]
+22
+23        for a in chars:
+24            pos = k
+25            length = cnt
+26            for char in s:
+27                if char == "*":
+28                    length += 1
+29                elif char == '%':
+30                    pos = length - pos - 1
+31                elif char == '#':
+32                    half = length // 2
+33                    if pos >= half:
+34                        pos -= half
+35                    length = half
+36                else:
+37                    if pos == length - 1: 
+38                        if char == a: return a
+39                        break
+40                    length -= 1
+41                # print(a, char, pos, length)
+42        
+43        return '.'
