17f1967a7ee75b49664a801e97515c1bc2308364
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-07-03 16:10:21 +0530

    File updated at 7/3/2026, 4:10:20 PM

--- 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.py
+++ 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.py
@@ -1,38 +1,44 @@
-# Last updated: 24/7/2025, 1:45:41 pm
-class Solution:
-    def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
-        graph = defaultdict(list)
-        n = len(online)
-        max_cost = 0
-        for x, y, c in edges:
-            graph[x].append((y, c))
-            max_cost = max(max_cost, c)
-        
-        l = -1
-        r = max_cost
-        while l < r:
-            mid = (l + r + 1) // 2
-
-            stack = [(0, 0)]
-            visited = [inf] * n
-            can = False
-            while stack:
-                dis, x = heapq.heappop(stack)
-                if x == n-1:
-                    can = True
-                    break
-
-                if visited[x] <= dis: continue
-                visited[x] = dis
-                
-                for y, c in graph[x]:
-                    if not online[y] or c < mid: continue
-                    if dis + c > k: continue
-                    heapq.heappush(stack, (dis + c, y))
-            
-            if can:
-                l = mid
-            else:
-                r = mid - 1
-            
-        return l
+# Last updated: 7/3/2026, 4:10:20 PM
+1inf = 10**20
+2class Solution:
+3    def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
+4        n = len(online)
+5
+6        graph = [[] for _ in range(n)]
+7        l = -1
+8        r = -1
+9        for u, v, c in edges:
+10            if online[u] and online[v]:
+11                graph[u].append((v, c))
+12                r = max(r, c)
+13        
+14        for x in range(n):
+15            graph[x].sort(key=lambda y: y[1])
+16
+17        def isGood(target):
+18            dis = [-inf] * n
+19            stack = [(-k, 0)]
+20
+21            while stack:
+22                rem, x = heappop(stack)
+23                if x == n-1: return True
+24                rem = -rem
+25                if dis[x] >= rem: continue
+26                dis[x] = rem
+27
+28                for y, c in graph[x]:
+29                    if c < target or c > rem: continue
+30                    heappush(stack, (-(rem - c), y))
+31
+32            return False
+33            
+34        while l < r:
+35            mid = (l + r + 1) // 2
+36
+37            if isGood(mid):
+38                l = mid
+39            else:
+40                r = mid - 1
+41
+42        return l
+43
