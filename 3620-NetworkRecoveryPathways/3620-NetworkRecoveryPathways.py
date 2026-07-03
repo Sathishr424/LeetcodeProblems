62e69a5cd2dcaea22d3207869bc1e1d5c839803b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-07-03 16:13:05 +0530

    File updated at 7/3/2026, 4:13:05 PM

--- 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.py
+++ 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.py
@@ -1,4 +1,4 @@
-# Last updated: 7/3/2026, 4:10:35 PM
+# Last updated: 7/3/2026, 4:13:05 PM
 1inf = 10**20
 2class Solution:
 3    def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
@@ -12,30 +12,36 @@
 11                graph[u].append((v, c))
 12                r = max(r, c)
 13        
-14        def isGood(target):
-15            dis = [-inf] * n
-16            stack = [(-k, 0)]
-17
-18            while stack:
-19                rem, x = heappop(stack)
-20                if x == n-1: return True
-21                rem = -rem
-22                if dis[x] >= rem: continue
-23                dis[x] = rem
-24
-25                for y, c in graph[x]:
-26                    if c < target or c > rem: continue
-27                    heappush(stack, (-(rem - c), y))
+14        for x in range(n):
+15            graph[x].sort(key=lambda y: y[1])
+16
+17        def isGood(target):
+18            dis = [-inf] * n
+19
+20            stack = [(-k, 0)]
+21            dis[0] = k
+22
+23            while stack:
+24                rem, x = heappop(stack)
+25                if x == n-1: return True
+26                rem = -rem
+27                if dis[x] > rem: continue
 28
-29            return False
-30            
-31        while l < r:
-32            mid = (l + r + 1) // 2
-33
-34            if isGood(mid):
-35                l = mid
-36            else:
-37                r = mid - 1
-38
-39        return l
+29                for y, c in graph[x]:
+30                    if c < target or c > rem: continue
+31                    new_rem = rem - c
+32                    if new_rem > dis[y]:
+33                        dis[y] = new_rem
+34                        heappush(stack, (-new_rem, y))
+35
+36            return False
+37            
+38        while l < r:
+39            mid = (l + r + 1) // 2
 40
+41            if isGood(mid):
+42                l = mid
+43            else:
+44                r = mid - 1
+45
+46        return l
