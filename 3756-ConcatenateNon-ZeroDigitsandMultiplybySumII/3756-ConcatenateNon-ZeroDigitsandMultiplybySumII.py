497d40f212b28dbd528bae278299dda77f0e78b1
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-07-08 06:47:36 +0530

    File updated at 7/8/2026, 6:47:35 AM

--- 3756-ConcatenateNon-ZeroDigitsandMultiplybySumII/3756-ConcatenateNon-ZeroDigitsandMultiplybySumII.py
+++ 3756-ConcatenateNon-ZeroDigitsandMultiplybySumII/3756-ConcatenateNon-ZeroDigitsandMultiplybySumII.py
@@ -1,75 +1,45 @@
-# Last updated: 27/11/2025, 1:23:00 pm
-1mod = 10**9 + 7
-2
-3class Node:
-4    def __init__(self):
-5        self.sum = 0
-6        self.x = 0
-7        self.n = 0
-8
-9    def __repr__(self):
-10        return f"{self.sum}, {self.x}, {self.n}"
-11
-12class SegmentTree:
-13    def __init__(self, nums):
-14        self.nums = nums
-15        self.n = len(nums)
-16        self.tree = [Node() for _ in range(self.n * 4)]
-17
-18        self.build(0, self.n-1, 0)
-19    
-20    def build(self, l, r, index):
-21        if l == r:
-22            self.tree[index].sum += self.nums[l]
-23            if self.nums[l] > 0:
-24                self.tree[index].n += 1
-25                self.tree[index].x = self.nums[l]
-26            return self.tree[index]
-27        
-28        mid = (l + r) // 2
-29        left = self.build(l, mid, index * 2 + 1)
-30        right = self.build(mid + 1, r, index * 2 + 2)
-31
-32        self.tree[index].sum = left.sum + right.sum
-33        x = (left.x * pow(10, right.n, mod) % mod) + right.x
-34        x %= mod
-35        self.tree[index].x = x
-36        self.tree[index].n = left.n + right.n
-37        return self.tree[index]
-38    
-39    def query(self, l, r, index, left, right):
-40        if l > right or r < left:
-41            return Node()
-42        
-43        # print((left, right), l, r, index)
-44        if l >= left and r <= right:
-45            return self.tree[index]
-46        
-47        mid = (l + r) // 2
-48        left_node = self.query(l, mid, index * 2 + 1, left, right)
-49        right_node = self.query(mid + 1, r, index * 2 + 2, left, right)
-50
-51        new_node = Node()
-52        x = (left_node.x * pow(10, right_node.n, mod) % mod) + right_node.x
-53        x %= mod
-54        new_node.sum = left_node.sum + right_node.sum
-55        new_node.x = x
-56        new_node.n = left_node.n + right_node.n
-57        return new_node
-58
-59class Solution:
-60    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
-61        n = len(s)
-62
-63        nums = [int(d) for d in s]
-64        ret = []
-65        
-66        segTree = SegmentTree(nums)
-67        # print(segTree.tree)
-68
-69        for l, r in queries:
-70            node = segTree.query(0, n-1, 0, l, r)
-71            # print((l, r), node)
-72            ret.append(node.sum * node.x % mod)
-73
-74        return ret
+# Last updated: 7/8/2026, 6:47:35 AM
+1MOD = 10**9 + 7
+2N = 10**5 + 1
+3
+4pows = [1] * N
+5for i in range(1, N):
+6    pows[i] = pows[i - 1] * 10 % MOD
+7
+8inv10 = pow(10, MOD - 2, MOD)   # inverse of 10
+9
+10inv_pows = [1] * N
+11for i in range(1, N):
+12    inv_pows[i] = inv_pows[i - 1] * inv10 % MOD
+13
+14class Solution:
+15    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
+16        n = len(s)
+17        s = [int(d) for d in s]
+18
+19        sums = [0]
+20        for i in range(n):
+21            sums.append(sums[-1] + s[i])
+22
+23
+24        powers = [0] * (n+1)
+25        vals = [0] * (n+1)
+26        cnt = 0
+27        for i in range(n-1, -1, -1):
+28            vals[i] = s[i] * pows[cnt] % MOD + vals[i + 1] 
+29            vals[i] %= MOD
+30            if s[i] > 0:
+31                cnt += 1
+32            powers[i] = cnt
+33
+34        ret = []
+35        for l, r in queries:
+36            sum = sums[r + 1] - sums[l]
+37            val = vals[l]
+38            p = powers[r + 1]
+39
+40            curr = val - vals[r + 1]
+41
+42            ans = curr * inv_pows[p] % MOD * sum % MOD
+43            ret.append(ans)
+44        return ret
