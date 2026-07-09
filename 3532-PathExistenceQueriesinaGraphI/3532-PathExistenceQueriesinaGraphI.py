c554650ab46880d0ade1f74d9cf2ab8f92279342
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-07-09 15:50:47 +0530

    File updated at 7/9/2026, 3:50:46 PM

--- 3532-PathExistenceQueriesinaGraphI/3532-PathExistenceQueriesinaGraphI.py
+++ 3532-PathExistenceQueriesinaGraphI/3532-PathExistenceQueriesinaGraphI.py
@@ -1,21 +1,40 @@
-# Last updated: 28/4/2025, 9:00:00 pm
-class Solution:
-    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
-        parents = [i for i in range(n)]
-
-        def find(x):
-            return parents[x]
-        
-        def union(x, y):
-            parents[find(y)] = find(x)
-        
-        for i in range(1, n):
-            if nums[i] - nums[i-1] <= maxDiff:
-                union(i-1, i)
-        
-        ret = []
-
-        for x, y in queries:
-            ret.append(find(x) == find(y))
-        
-        return ret
+# Last updated: 7/9/2026, 3:50:46 PM
+1class Union:
+2    def __init__(self, n):
+3        self.parents = [i for i in range(n)]
+4        self.sizes = [1] * n
+5
+6    def find(self, x):
+7        if x != self.parents[x]:
+8            self.parents[x] = self.find(self.parents[x])
+9
+10        return self.parents[x]
+11
+12    def union(self, x, y):
+13        x = self.find(x)
+14        y = self.find(y)
+15
+16        if x == y: return True
+17
+18        if self.sizes[y] > self.sizes[x]:
+19            x, y = y, x
+20
+21        self.sizes[x] += self.sizes[y]
+22        self.parents[y] = x
+23        
+24        return False
+25
+26class Solution:
+27    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+28        un = Union(n)
+29
+30        left = 0
+31        for i in range(n):
+32            while nums[i] - nums[left] > maxDiff:
+33                left += 1
+34            un.union(left, i)
+35
+36        ret = []
+37        for l, r in queries:
+38            ret.append(un.find(l) == un.find(r))
+39        return ret
