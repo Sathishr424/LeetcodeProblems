c2098afc5b9a3aaa8ab73451839c75065e497c92
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-07-11 02:18:01 +0530

    File updated at 7/11/2026, 2:18:01 AM

--- 3532-PathExistenceQueriesinaGraphI/3532-PathExistenceQueriesinaGraphI.py
+++ 3532-PathExistenceQueriesinaGraphI/3532-PathExistenceQueriesinaGraphI.py
@@ -1,40 +1,19 @@
-# Last updated: 7/9/2026, 3:59:12 PM
-1class Union:
-2    def __init__(self, n):
-3        self.parents = [i for i in range(n)]
-4        self.sizes = [1] * n
-5
-6    def find(self, x):
-7        if x != self.parents[x]:
-8            self.parents[x] = self.find(self.parents[x])
+# Last updated: 7/11/2026, 2:18:01 AM
+1class Solution:
+2    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+3        indexes = [-1]
+4        for i in range(1, n):
+5            if nums[i] - nums[i - 1] > maxDiff:
+6                indexes.append(i)
+7        indexes.append(n)
+8        # print(indexes)
 9
-10        return self.parents[x]
-11
-12    def union(self, x, y):
-13        x = self.find(x)
-14        y = self.find(y)
-15
-16        if x == y: return True
+10        m = len(indexes) - 1
+11        ret = []
+12        for u, v in queries:
+13            left = bisect_right(indexes, u)
+14            right = bisect_right(indexes, v)
+15            # print((u, v), (left, right))
+16            ret.append(left == right)
 17
-18        if self.sizes[y] > self.sizes[x]:
-19            x, y = y, x
-20
-21        self.sizes[x] += self.sizes[y]
-22        self.parents[y] = x
-23        
-24        return False
-25
-26class Solution:
-27    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
-28        un = Union(n)
-29
-30        left = 0
-31        for i in range(n):
-32            while nums[i] - nums[left] > maxDiff:
-33                left += 1
-34            un.union(left, i)
-35
-36        ret = []
-37        for l, r in queries:
-38            ret.append(un.find(l) == un.find(r))
-39        return ret
+18        return ret
