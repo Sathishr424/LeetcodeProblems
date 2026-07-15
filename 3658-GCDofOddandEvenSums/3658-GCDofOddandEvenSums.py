4f8d318025ee09b4ddd4f0bc32f14c2abc5e988b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-07-15 12:02:09 +0530

    File updated at 7/15/2026, 12:02:09 PM

--- 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.py
+++ 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.py
@@ -1,4 +1,4 @@
-# Last updated: 7/15/2026, 12:01:47 PM
+# Last updated: 7/15/2026, 12:02:09 PM
 1def gcd(a, b):
 2    rem = a % b
 3    if rem == 0: return b
@@ -6,13 +6,14 @@
 5
 6class Solution:
 7    def gcdOfOddEvenSums(self, n: int) -> int:
-8        odd = 0
-9        even = 0
-10
-11        for i in range(1, n * 2 + 1, 2):
-12            odd += i
-13        for i in range(2, n * 2 + 1, 2):
-14            even += i
-15
-16        return gcd(odd, even)
-17
+8        return n
+9        odd = 0
+10        even = 0
+11
+12        for i in range(1, n * 2 + 1, 2):
+13            odd += i
+14        for i in range(2, n * 2 + 1, 2):
+15            even += i
+16
+17        return gcd(odd, even)
+18
