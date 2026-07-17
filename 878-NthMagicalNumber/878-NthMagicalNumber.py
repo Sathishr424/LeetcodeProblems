310522a2c3cf6b6a01ae3b01c90d0001bd4e1ece
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-07-17 19:07:48 +0530

    File updated at 7/17/2026, 7:07:47 PM

--- 878-NthMagicalNumber/878-NthMagicalNumber.py
+++ 878-NthMagicalNumber/878-NthMagicalNumber.py
@@ -1,27 +1,31 @@
-# Last updated: 7/17/2026, 7:07:24 PM
+# Last updated: 7/17/2026, 7:07:47 PM
 1class Solution:
 2    def nthMagicalNumber(self, n: int, a: int, b: int) -> int:
 3        mod = 10**9 + 7
 4        LCM = lcm(a, b)
 5
-6        def getCnt(val):
-7            x_cnt = val // a
-8            y_cnt = val // b
-9            lcm_cnt = val // LCM
-10
-11            return x_cnt + (y_cnt - lcm_cnt)
-12
-13        l = 1
-14        r = min(a, b) * n
-15
-16        while l < r:
-17            mid = (l + r) // 2
-18
-19            cnt = getCnt(mid)
-20
-21            if cnt >= n:
-22                r = mid
-23            else:
-24                l = mid + 1
-25        
-26        return l % mod
+6        def getCnt(x, y, cnt):
+7            val = x * cnt
+8
+9            y_cnt = val // y
+10            lcm_cnt = val // LCM
+11
+12            return cnt + (y_cnt - lcm_cnt)
+13
+14        def getIndex(x, y):
+15            l = 1
+16            r = n
+17            while l < r:
+18                mid = (l + r) // 2
+19                tot = getCnt(x, y, mid)
+20    
+21    
+22                if tot >= n:
+23                    r = mid
+24                else:
+25                    l = mid + 1
+26            return l
+27
+28        index = getIndex(a, b)
+29        if getCnt(a, b, index) == n: return index * a % mod
+30        return getIndex(b, a) * b % mod
