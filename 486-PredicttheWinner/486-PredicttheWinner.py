772b90a20fa59806197a7f0b1d48e1d55c8278f1
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-01 22:57:18 +0530

    File updated at 8/1/2026, 10:57:17 PM

--- 486-PredicttheWinner/486-PredicttheWinner.py
+++ 486-PredicttheWinner/486-PredicttheWinner.py
@@ -1,16 +1,16 @@
-# Last updated: 17/7/2025, 4:02:26 pm
-class Solution:
-    def predictTheWinner(self, nums: List[int]) -> bool:
-        tot = sum(nums)
-        @cache
-        def rec(l, r, p1_s, p1_turn):
-            if l > r:
-                return p1_s >= tot - p1_s
-            if p1_turn:
-                return rec(l+1, r, p1_s + nums[l], not p1_turn) or rec(l, r-1, p1_s + nums[r], not p1_turn)
-            else:
-                return rec(l+1, r, p1_s, not p1_turn) and rec(l, r-1, p1_s, not p1_turn)
-        
-        ans = rec(0, len(nums)-1, 0, True)
-        rec.cache_clear()
-        return ans
+# Last updated: 8/1/2026, 10:57:17 PM
+1class Solution:
+2    def predictTheWinner(self, nums: List[int]) -> bool:
+3        tot = sum(nums)
+4        @cache
+5        def rec(l, r, p1_s, p1_turn):
+6            if l > r:
+7                return p1_s >= tot - p1_s
+8            if p1_turn:
+9                return rec(l+1, r, p1_s + nums[l], not p1_turn) or rec(l, r-1, p1_s + nums[r], not p1_turn)
+10            else:
+11                return rec(l+1, r, p1_s, not p1_turn) and rec(l, r-1, p1_s, not p1_turn)
+12        
+13        ans = rec(0, len(nums)-1, 0, True)
+14        rec.cache_clear()
+15        return ans
