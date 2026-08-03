a9a3cb226b1018257758aff0f390ae0a0736d386
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-03 19:51:09 +0530

    File updated at 8/3/2026, 7:51:09 PM

--- 486-PredicttheWinner/486-PredicttheWinner.py
+++ 486-PredicttheWinner/486-PredicttheWinner.py
@@ -1,19 +1,19 @@
-# Last updated: 8/3/2026, 7:21:14 PM
+# Last updated: 8/3/2026, 7:51:09 PM
 1class Solution:
 2    def predictTheWinner(self, nums: List[int]) -> bool:
 3        n = len(nums)
 4
 5        @cache
-6        def rec(l, r, score, turn):
+6        def rec(l, r, turn):
 7            if l > r:
-8                return 1 if score >= 0 else -1
+8                return 0
 9
 10            add = 1 if turn else -1
-11            left = rec(l+1, r, score + nums[l] * add, not turn)
-12            right = rec(l, r-1, score + nums[r] * add, not turn)
+11            left = rec(l+1, r, not turn) + (nums[l] * add)
+12            right = rec(l, r-1, not turn) + (nums[r] * add)
 13
 14            return max(left, right) if turn else min(left, right)
 15
-16        ans = rec(0, n-1, 0, True)
+16        ans = rec(0, n-1, True)
 17        rec.cache_clear()
 18        return ans >= 0
