04a214e0f94fe42954a5b264a6cd7a6a52cab957
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-03 19:18:44 +0530

    File updated at 8/3/2026, 7:18:43 PM

--- 486-PredicttheWinner/486-PredicttheWinner.py
+++ 486-PredicttheWinner/486-PredicttheWinner.py
@@ -1,16 +1,19 @@
-# Last updated: 8/1/2026, 10:57:17 PM
+# Last updated: 8/3/2026, 7:18:43 PM
 1class Solution:
 2    def predictTheWinner(self, nums: List[int]) -> bool:
-3        tot = sum(nums)
-4        @cache
-5        def rec(l, r, p1_s, p1_turn):
-6            if l > r:
-7                return p1_s >= tot - p1_s
-8            if p1_turn:
-9                return rec(l+1, r, p1_s + nums[l], not p1_turn) or rec(l, r-1, p1_s + nums[r], not p1_turn)
-10            else:
-11                return rec(l+1, r, p1_s, not p1_turn) and rec(l, r-1, p1_s, not p1_turn)
-12        
-13        ans = rec(0, len(nums)-1, 0, True)
-14        rec.cache_clear()
-15        return ans
+3        n = len(nums)
+4
+5        @cache
+6        def rec(l, r, score, turn):
+7            if l > r:
+8                return 1 if score >= 0 else -1
+9
+10            add = 1 if turn else -1
+11            left = rec(l+1, r, score + nums[l] * add, not turn)
+12            right = rec(l, r-1, score + nums[r] * add, not turn)
+13
+14            return max(left, right) if turn else min(left, right)
+15
+16        ans = rec(0, n-1, 0, True)
+17        rec.cache_clear()
+18        return ans >= 0
