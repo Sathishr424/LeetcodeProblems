984bfb4948a2358378f05e80633d7f66d8dde08a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-03 20:58:01 +0530

    File updated at 8/3/2026, 8:58:01 PM

--- 486-PredicttheWinner/486-PredicttheWinner.py
+++ 486-PredicttheWinner/486-PredicttheWinner.py
@@ -1,19 +1,27 @@
-# Last updated: 8/3/2026, 7:51:09 PM
+# Last updated: 8/3/2026, 8:58:01 PM
 1class Solution:
-2    def predictTheWinner(self, nums: List[int]) -> bool:
-3        n = len(nums)
+2    def stoneGameIII(self, stoneValue: List[int]) -> str:
+3        n = len(stoneValue)
 4
 5        @cache
-6        def rec(l, r, turn):
-7            if l > r:
-8                return 0
-9
-10            add = 1 if turn else -1
-11            left = rec(l+1, r, not turn) + (nums[l] * add)
-12            right = rec(l, r-1, not turn) + (nums[r] * add)
-13
-14            return max(left, right) if turn else min(left, right)
-15
-16        ans = rec(0, n-1, True)
-17        rec.cache_clear()
-18        return ans >= 0
+6        def rec(index, turn):
+7            add = 1 if turn else -1
+8            if index == n:
+9                return 0
+10
+11            compare = max if turn else min
+12            curr = stoneValue[index]
+13            ans = rec(index + 1, not turn) + curr * add
+14            for i in range(index + 1, min(n, index + 3)):
+15                curr += stoneValue[i]
+16                ans = compare(ans, rec(i + 1, not turn) + curr * add)
+17
+18            return ans
+19
+20        ans = rec(0, True)
+21        rec.cache_clear()
+22        if ans > 0:
+23            return "Alice"
+24        elif ans < 0:
+25            return "Bob"
+26        return "Tie"
