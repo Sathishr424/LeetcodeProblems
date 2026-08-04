69b5659682d1a07a9905221f759b8b1f769ba4d1
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-04 21:06:06 +0530

    File updated at 8/4/2026, 9:06:06 PM

--- 3984-DivisibleGame/3984-DivisibleGame.py
+++ 3984-DivisibleGame/3984-DivisibleGame.py
@@ -1,4 +1,4 @@
-# Last updated: 8/4/2026, 9:00:20 PM
+# Last updated: 8/4/2026, 9:06:06 PM
 1mod = 10**9 + 7
 2N = 10**6 + 1
 3is_prime = [1] * N
@@ -11,45 +11,46 @@
 10        is_prime[j] = 0
 11
 12primes = []
-13for i in range(N):
-14    if is_prime[i]: primes.append(i)
-15
-16class Solution:
-17    def divisibleGame(self, nums: list[int]) -> int:
-18        n = len(nums)
-19
-20        factors = defaultdict(set)
-21
-22        for num in set(nums):
-23            r_num = num
-24            for p in primes:
-25                if p * p > num:
-26                    if num > 1: factors[r_num].add(num)
-27                    break
-28                if num % p == 0:
-29                    factors[r_num].add(p)
-30                    while num % p == 0:
-31                        num //= p
+13factors = [[] for _ in range(N)]
+14for i in range(N):
+15    if is_prime[i]: primes.append(i)
+16
+17for num in range(1, N):
+18    r_num = num
+19    for p in primes:
+20        if p * p > num:
+21            if num > 1: factors[r_num].append(num)
+22            break
+23        if num % p == 0:
+24            factors[r_num].append(p)
+25            while num % p == 0:
+26                num //= p
+27
+28inf = 10**20
+29class Solution:
+30    def divisibleGame(self, nums: list[int]) -> int:
+31        n = len(nums)
 32
-33
-34        # print(dict(factors))
-35        found = []
-36        for i in range(n):
-37            tot = 0
-38            fac = defaultdict(int)
-39            for j in range(i, n):
-40                tot += nums[j]
+33        best = -inf
+34        best_k = inf
+35        for i in range(n):
+36            tot = 0
+37            fac = defaultdict(int)
+38            for j in range(i, n):
+39                num = nums[j]
+40                tot += num
 41                mx = 2
-42                for d in factors[nums[j]]:
-43                    fac[d] += nums[j]
+42                for d in factors[num]:
+43                    fac[d] += num
 44                    if fac[d] > fac[mx] or (fac[d] == fac[mx] and d < mx):
 45                        mx = d
-46                # print(nums[i:j+1], (i, j), dict(fac), (mx, fac[mx], tot))
-47                found.append((fac[mx] - (tot - fac[mx]), -mx))
-48        found.sort(reverse=True)
-49        if len(found) == 0: return -sum(nums) * 2 % mod
-50
-51        diff, k = found[0]
-52        return diff * (-k) % mod
+46
+47                curr = fac[mx] - (tot - fac[mx])
+48                k = mx
+49
+50                if curr > best or (curr == best and k < best_k):
+51                    best = curr
+52                    best_k = k
 53
-54
+54        if best_k == inf: return -sum(nums) * 2 % mod
+55        return best * best_k % mod
