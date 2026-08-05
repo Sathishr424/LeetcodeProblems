0ed63c2e6864d8823b483fbb044f85503c2f3bcf
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-05 11:46:01 +0530

    File updated at 8/5/2026, 11:46:00 AM

--- 3310-RemoveMethodsFromProject/3310-RemoveMethodsFromProject.py
+++ 3310-RemoveMethodsFromProject/3310-RemoveMethodsFromProject.py
@@ -1,4 +1,4 @@
-# Last updated: 8/5/2026, 11:43:04 AM
+# Last updated: 8/5/2026, 11:46:00 AM
 1class Solution:
 2    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
 3        graph = defaultdict(list)
@@ -6,23 +6,22 @@
 5        for u, v in invocations:
 6            graph[u].append(v)
 7
-8        sus = set()
-9        vis = set()
+8        vis = [0] * n
+9        sus = [0] * n
 10        def rec(x):
-11            if x in sus: return True
-12            if x in vis: return False
-13            vis.add(x)
+11            if sus[x]: return True
+12            if vis[x]: return False
+13            vis[x] = 1
 14            for y in graph[x]:
 15                if rec(y): return True
 16
 17            return False
 18
 19        rec(k)
-20        sus = vis
-21        vis = set()
-22
-23        for x in range(n):
-24            if x not in sus:
-25                if rec(x): return [i for i in range(n)]
-26
-27        return [i for i in range(n) if i not in sus]
+20        sus, vis = vis, sus
+21
+22        for x in range(n):
+23            if sus[x] == 0:
+24                if rec(x): return [i for i in range(n)]
+25
+26        return [i for i in range(n) if sus[i] == 0]
