77ba2d866909b6bdc98405ce44ce7a9bd93afe0a
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-10 13:53:47 +0530

    File updated at 8/10/2026, 1:53:47 PM

--- 1510-StoneGameIV/1510-StoneGameIV.py
+++ 1510-StoneGameIV/1510-StoneGameIV.py
@@ -1,25 +1,22 @@
-# Last updated: 8/10/2026, 1:52:14 PM
+# Last updated: 8/10/2026, 1:53:47 PM
 1squares = []
 2num = 1
 3N = 10**5
 4while num * num <= N:
 5    squares.append(num * num)
 6    num += 1
-7class Solution:
-8    def winnerSquareGame(self, n: int) -> bool:
-9
-10        @cache
-11        def rec(rem, alice):
-12            if rem == 0: return not alice
-13
-14            for num in squares:
-15                if num > rem: break
-16                if rec(rem - num, not alice) == alice: return alice
+7
+8@cache
+9def rec(rem, alice):
+10    if rem == 0: return not alice
+11
+12    for num in squares:
+13        if num > rem: break
+14        if rec(rem - num, not alice) == alice: return alice
+15
+16    return not alice
 17
-18            return not alice
-19
-20
-21        ans = rec(n, True)
-22        rec.cache_clear()
-23
-24        return ans
+18class Solution:
+19    def winnerSquareGame(self, n: int) -> bool:
+20        return rec(n, True)
+21
