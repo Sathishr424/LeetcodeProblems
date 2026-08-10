d158774ad88555fe9c6c60a97b2c477ff01432df
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-10 14:09:41 +0530

    File updated at 8/10/2026, 2:09:41 PM

--- 1510-StoneGameIV/1510-StoneGameIV.py
+++ 1510-StoneGameIV/1510-StoneGameIV.py
@@ -1,4 +1,4 @@
-# Last updated: 8/10/2026, 1:53:47 PM
+# Last updated: 8/10/2026, 2:09:41 PM
 1squares = []
 2num = 1
 3N = 10**5
@@ -6,17 +6,19 @@
 5    squares.append(num * num)
 6    num += 1
 7
-8@cache
-9def rec(rem, alice):
-10    if rem == 0: return not alice
-11
-12    for num in squares:
-13        if num > rem: break
-14        if rec(rem - num, not alice) == alice: return alice
-15
-16    return not alice
-17
-18class Solution:
-19    def winnerSquareGame(self, n: int) -> bool:
-20        return rec(n, True)
-21
+8dp = [[True, False] for _ in range(N+1)]
+9
+10for rem in range(N + 1):
+11    for alice in range(2):
+12        opp = 0 if alice else 1
+13        for num in squares:
+14            if rem + num > N: break
+15            if alice:
+16                dp[rem + num][opp] = dp[rem + num][opp] and dp[rem][alice]
+17            else:
+18                dp[rem + num][opp] = dp[rem + num][opp] or dp[rem][alice]
+19
+20class Solution:
+21    def winnerSquareGame(self, n: int) -> bool:
+22        return dp[n][1]
+23
