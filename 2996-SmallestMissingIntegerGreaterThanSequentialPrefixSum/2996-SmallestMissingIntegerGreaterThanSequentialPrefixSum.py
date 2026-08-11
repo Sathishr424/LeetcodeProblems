d1d0f767648e6c51eea8a893f1d68849e0069f90
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-11 18:17:22 +0530

    File updated at 8/11/2026, 6:17:22 PM

--- 2996-SmallestMissingIntegerGreaterThanSequentialPrefixSum/2996-SmallestMissingIntegerGreaterThanSequentialPrefixSum.py
+++ 2996-SmallestMissingIntegerGreaterThanSequentialPrefixSum/2996-SmallestMissingIntegerGreaterThanSequentialPrefixSum.py
@@ -1,19 +1,17 @@
-# Last updated: 8/11/2026, 2:25:02 PM
+# Last updated: 8/11/2026, 6:17:22 PM
 1class Solution:
 2    def missingInteger(self, nums: List[int]) -> int:
 3        n = len(nums)
 4
 5        there = set(nums)
-6        prev = nums[0]
-7        s = prev
-8        for i in range(1, n):
-9            if nums[i] == prev + 1:
-10                s += nums[i]
-11                prev = nums[i]
-12            else:
-13                break
-14
-15        while s in there:
-16            s += 1
-17
-18        return s
+6        s = nums[0]
+7        for i in range(1, n):
+8            if nums[i] == nums[i-1] + 1:
+9                s += nums[i]
+10            else:
+11                break
+12
+13        while s in there:
+14            s += 1
+15
+16        return s
