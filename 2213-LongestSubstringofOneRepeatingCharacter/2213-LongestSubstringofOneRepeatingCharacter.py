7c8c2ca0b576f1a6dd1242a744b2ee8d1b233a67
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-13 13:30:10 +0530

    File updated at 8/13/2026, 1:30:10 PM

--- 2213-LongestSubstringofOneRepeatingCharacter/2213-LongestSubstringofOneRepeatingCharacter.py
+++ 2213-LongestSubstringofOneRepeatingCharacter/2213-LongestSubstringofOneRepeatingCharacter.py
@@ -1,82 +1,88 @@
-# Last updated: 4/9/2025, 4:02:53 pm
-class Node:
-    def __init__(self):
-        self.max = 1
-        self.left = 1
-        self.right = 1
-        self.left_a = ''
-        self.right_a = ''
-        self.window = 1
-
-class SegmentTree:
-    def __init__(self, alp):
-        self.alp = alp
-        self.n = len(alp)
-        self.tree = [Node() for _ in range(self.n * 4)]
-        self.indexes = [0] * self.n
-
-        self.build(0, self.n-1, 0)
-
-    def assignTreeIndex(self, index, left, right):
-        self.tree[index].left_a = left.left_a
-        self.tree[index].right_a = right.right_a
-        if left.right_a == right.left_a:
-            max_cnt = left.right + right.left
-            self.tree[index].max = max(left.max, right.max, max_cnt)
-    
-            if left.left == left.window:
-                self.tree[index].left = max_cnt
-            else:
-                self.tree[index].left = left.left
-    
-            if right.right == right.window:
-                self.tree[index].right = max_cnt
-            else:
-                self.tree[index].right = right.right
-        else:
-            self.tree[index].max = max(left.max, right.max)
-    
-            self.tree[index].left = left.left
-            self.tree[index].right = right.right
-    
-    def build(self, l, r, index):
-        if l == r:
-            self.indexes[l] = index
-            self.tree[index].left_a = self.alp[l]
-            self.tree[index].right_a = self.alp[l]
-            return self.tree[index]
-
-        mid = (l + r) // 2
-        left = self.build(l, mid, index * 2 + 1)
-        right = self.build(mid + 1, r, index * 2 + 2)
-
-        self.tree[index].window = left.window + right.window
-        self.assignTreeIndex(index, left, right)
-
-        return self.tree[index]
-    
-    def update(self, index, new_val):
-        index = self.indexes[index]
-        self.tree[index].left_a = new_val
-        self.tree[index].right_a = new_val
-
-        while index:
-            index = (index - 1) // 2
-
-            left = self.tree[index * 2  + 1]
-            right = self.tree[index * 2  + 2]
-
-            self.assignTreeIndex(index, left, right)
-
-class Solution:
-    def longestRepeating(self, s: str, queryCharacters: str, queryIndices: List[int]) -> List[int]:
-        n = len(queryCharacters)
-        segTree = SegmentTree(s)
-        ret = []
-        for i in range(n):
-            segTree.update(queryIndices[i], queryCharacters[i])
-
-            ret.append(segTree.tree[0].max)
-
-        return ret
-        
+# Last updated: 8/13/2026, 1:30:10 PM
+1from typing import *
+2from collections import defaultdict, deque, Counter
+3from functools import lru_cache, cache
+4from itertools import accumulate, combinations, permutations, product
+5from bisect import bisect_left, bisect_right, insort
+6from heapq import *
+7from math import *
+8from string import ascii_lowercase, ascii_uppercase
+9from sortedcontainers import SortedList
+10import random
+11import re
+12
+13class Node:
+14    def __init__(self, left=0, right=0, best=0):
+15        self.left = left
+16        self.right = right
+17        self.best = best
+18
+19class SegmentTree:
+20    def __init__(self, nums):
+21        self.nums = nums
+22        self.n = len(nums)
+23        self.m = self.n * 4
+24
+25        self.indexes = [-1 for _ in range(self.n)]
+26        self.tree = [None for _ in range(self.m)]
+27        self.build(0, 0, self.n-1)
+28
+29    def mergeNodes(self, left, right, l, mid, r):
+30        node = Node(left.left, right.right, max(left.best, right.best))
+31
+32        if self.nums[mid] == self.nums[mid + 1]:
+33            node.best = max(node.best, left.right + right.left)
+34
+35            if left.left == (mid - l + 1):
+36                node.left = left.left + right.left
+37            if right.left == (r - mid):
+38                node.right = right.left + left.right
+39
+40        return node
+41
+42    def build(self, index, l, r):
+43        if l == r:
+44            self.tree[index] = Node(1, 1, 1)
+45            self.indexes[l] = index
+46            return self.tree[index]
+47
+48        mid = (l + r) // 2
+49
+50        left = self.build(index * 2 + 1, l, mid)
+51        right = self.build(index * 2 + 2, mid + 1, r)
+52
+53        self.tree[index] = self.mergeNodes(left, right, l, mid, r)
+54        return self.tree[index]
+55
+56    def update(self, index, l, r, left, right):
+57        if r < left or l > right:
+58            return self.tree[index]
+59
+60        if l >= left and r <= right:
+61            return self.tree[index]
+62
+63        mid = (l + r) // 2
+64        left_node = self.update(index * 2 + 1, l, mid, left, right)
+65        right_node = self.update(index * 2 + 2, mid + 1, r, left, right)
+66
+67        self.tree[index] = self.mergeNodes(left_node, right_node, l, mid, r)
+68        return self.tree[index]
+69
+70class Solution:
+71    def longestRepeating(self, s: str, queryCharacters: str, queryIndices: List[int]) -> List[int]:
+72        n = len(s)
+73        k = len(queryCharacters)
+74        s = list(s)
+75
+76        ans = []
+77        segTree = SegmentTree(s)
+78
+79        for i in range(k):
+80            index = queryIndices[i]
+81            s[index] = queryCharacters[i]
+82
+83            node = segTree.update(0, 0, n-1, index, index)
+84            ans.append(node.best)
+85
+86        return ans
+87
