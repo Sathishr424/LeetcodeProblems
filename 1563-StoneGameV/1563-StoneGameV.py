0a94da5d2e2c1dfc5ca166ccaea2a44112095b26
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-17 13:16:33 +0530

    File updated at 8/17/2026, 1:16:33 PM

--- 1563-StoneGameV/1563-StoneGameV.py
+++ 1563-StoneGameV/1563-StoneGameV.py
@@ -1,32 +1,33 @@
-# Last updated: 8/17/2026, 1:14:26 PM
-1class Solution:
-2    def stoneGameV(self, stones: List[int]) -> int:
-3        n = len(stones)
-4
-5        prefix = [0]
-6        for stone in stones:
-7            prefix.append(prefix[-1] + stone)
-8
-9        dp = [[-1] * n for _ in range(n)]
-10        def rec(l, r):
-11            if l == r:
-12                return 0
-13
-14            if dp[l][r] != -1: return dp[l][r]
-15            ans = 0
-16            for i in range(l, r):
-17                left = prefix[i + 1] - prefix[l]
-18                right = prefix[r + 1] - prefix[i + 1]
-19
-20                if left > right:
-21                    ans = max(ans, rec(i + 1, r) + right)
-22                elif right > left:
-23                    ans = max(ans, rec(l, i) + left)
-24                else:
-25                    ans = max(ans, max(rec(l, i), rec(i+1, r)) + left)
-26
-27            dp[l][r] = ans
-28            return ans
-29
-30        ans = rec(0, n-1)
-31        return ans
+# Last updated: 8/17/2026, 1:16:33 PM
+1cmax = lambda x, y: x if x > y else y
+2class Solution:
+3    def stoneGameV(self, stones: List[int]) -> int:
+4        n = len(stones)
+5
+6        prefix = [0]
+7        for stone in stones:
+8            prefix.append(prefix[-1] + stone)
+9
+10        dp = [[-1] * n for _ in range(n)]
+11        def rec(l, r):
+12            if l == r:
+13                return 0
+14
+15            if dp[l][r] != -1: return dp[l][r]
+16            ans = 0
+17            for i in range(l, r):
+18                left = prefix[i + 1] - prefix[l]
+19                right = prefix[r + 1] - prefix[i + 1]
+20
+21                if left > right:
+22                    ans = cmax(ans, rec(i + 1, r) + right)
+23                elif right > left:
+24                    ans = cmax(ans, rec(l, i) + left)
+25                else:
+26                    ans = cmax(ans, cmax(rec(l, i), rec(i+1, r)) + left)
+27
+28            dp[l][r] = ans
+29            return ans
+30
+31        ans = rec(0, n-1)
+32        return ans
