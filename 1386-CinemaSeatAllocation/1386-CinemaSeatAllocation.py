b9941cf8a3308a75a800b09e33bc62f8b7414df5
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-19 05:58:28 +0530

    File updated at 8/19/2026, 5:58:28 AM

--- 1386-CinemaSeatAllocation/1386-CinemaSeatAllocation.py
+++ 1386-CinemaSeatAllocation/1386-CinemaSeatAllocation.py
@@ -1,22 +1,22 @@
-# Last updated: 8/19/2026, 5:58:05 AM
-1class Solution:
-2    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
-3        rows = defaultdict(lambda: [0] * 10)
-4        for row, seat in reservedSeats:
-5            rows[row][seat - 1] = 1
-6
-7        sorted_rows = sorted(rows.keys())
-8        prev = 0
-9        ans = 0
-10        for row in sorted_rows:
-11            diff = max(0, row - prev - 1)
-12            ans += diff * 2
-13            prev = row
-14
-15        diff = max(0, n - prev)
-16        ans += diff * 2
-17
-18        choices = [[1, 2, 3, 4], [3, 4, 5, 6], [5, 6, 7, 8]]
+# Last updated: 8/19/2026, 5:58:28 AM
+1choices = [[1, 2, 3, 4], [3, 4, 5, 6], [5, 6, 7, 8]]
+2class Solution:
+3    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+4        rows = defaultdict(lambda: [0] * 10)
+5        for row, seat in reservedSeats:
+6            rows[row][seat - 1] = 1
+7
+8        sorted_rows = sorted(rows.keys())
+9        prev = 0
+10        ans = 0
+11        for row in sorted_rows:
+12            diff = max(0, row - prev - 1)
+13            ans += diff * 2
+14            prev = row
+15
+16        diff = max(0, n - prev)
+17        ans += diff * 2
+18
 19        for row in sorted_rows:
 20            c = 0
 21            while c < 3:
