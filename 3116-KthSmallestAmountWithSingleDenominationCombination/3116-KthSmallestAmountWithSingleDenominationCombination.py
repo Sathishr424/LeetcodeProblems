f27688f45fa23c2cf0264b3c153fae634aae5aa6
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-21 15:12:18 +0530

    File updated at 8/21/2026, 3:12:18 PM

--- 3116-KthSmallestAmountWithSingleDenominationCombination/3116-KthSmallestAmountWithSingleDenominationCombination.py
+++ 3116-KthSmallestAmountWithSingleDenominationCombination/3116-KthSmallestAmountWithSingleDenominationCombination.py
@@ -1,4 +1,4 @@
-# Last updated: 8/21/2026, 3:10:36 PM
+# Last updated: 8/21/2026, 3:12:18 PM
 1class Solution:
 2    def findKthSmallest(self, coins: List[int], k: int) -> int:
 3        n = len(coins)
@@ -10,33 +10,28 @@
 9        2, 3, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21
 10        """
 11
-12        mn = min(coins)
-13        l = 1
-14        r = mn * k + 1
-15        while l < r:
-16            mid = (l + r) // 2
-17
-18            curr = 0
-19            for mask in range(1, 1 << n):
-20                lcm_ = 1
-21                cnt = 0
-22                for i in range(n):
-23                    if mask & (1 << i):
-24                        lcm_ = lcm(lcm_, coins[i])
-25                        cnt += 1
-26
-27                tmp = mid // lcm_
-28                # print(bin(mask), lcm_, cnt, tmp)
-29                if cnt % 2 == 0:
-30                    curr -= mid // lcm_
-31                else:
-32                    curr += mid // lcm_
-33
-34            # print((l, r), mid, curr)
-35            
-36            if curr >= k:
-37                r = mid
-38            else:
-39                l = mid + 1
-40
-41        return l
+12        l = 1
+13        r = min(coins) * k + 1
+14        while l < r:
+15            mid = (l + r) // 2
+16
+17            curr = 0
+18            for mask in range(1, 1 << n):
+19                lcm_ = 1
+20                cnt = 0
+21                for i in range(n):
+22                    if mask & (1 << i):
+23                        lcm_ = lcm(lcm_, coins[i])
+24                        cnt += 1
+25
+26                if cnt % 2:
+27                    curr += mid // lcm_
+28                else:
+29                    curr -= mid // lcm_
+30            
+31            if curr >= k:
+32                r = mid
+33            else:
+34                l = mid + 1
+35
+36        return l
