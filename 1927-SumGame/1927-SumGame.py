7658d376993a0fbab11d89cbe3dadc88e4c30462
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-25 14:35:42 +0530

    File updated at 8/25/2026, 2:35:42 PM

--- 1927-SumGame/1927-SumGame.py
+++ 1927-SumGame/1927-SumGame.py
@@ -1,4 +1,4 @@
-# Last updated: 8/25/2026, 2:32:03 PM
+# Last updated: 8/25/2026, 2:35:42 PM
 1class Solution:
 2    def stoneGameVIII(self, stones: List[int]) -> int:
 3        n = len(stones)
@@ -10,19 +10,14 @@
 9        @cache
 10        def rec(index, alice):
 11            if index == n-1:
-12                if alice:
-13                    return prefix[index + 1]
-14                else:
-15                    return -prefix[index + 1]
-16
-17            ans = rec(index + 1, alice)
-18            if alice:
-19                ans = max(ans, rec(index + 1, not alice) + prefix[index + 1])
-20            else:
-21                ans = min(ans, rec(index + 1, not alice) - prefix[index + 1])
-22
-23            return ans
-24
-25        ans = rec(1, True)
-26        rec.cache_clear()
-27        return ans
+12                return prefix[index + 1] * (1 if alice else -1)
+13
+14            ans = rec(index + 1, alice)
+15            if alice:
+16                return max(ans, rec(index + 1, alice ^ 1) + prefix[index + 1])
+17            else:
+18                return min(ans, rec(index + 1, alice ^ 1) - prefix[index + 1])
+19
+20        ans = rec(1, 1)
+21        rec.cache_clear()
+22        return ans
