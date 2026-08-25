87cd6729898c273f037c2beb80fd4959012a0332
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-25 14:32:04 +0530

    File updated at 8/25/2026, 2:32:03 PM

--- 1927-SumGame/1927-SumGame.py
+++ 1927-SumGame/1927-SumGame.py
@@ -1,61 +1,28 @@
-# Last updated: 8/25/2026, 2:01:03 PM
+# Last updated: 8/25/2026, 2:32:03 PM
 1class Solution:
-2    def sumGame(self, num: str) -> bool:
-3        n = len(num)
+2    def stoneGameVIII(self, stones: List[int]) -> int:
+3        n = len(stones)
 4
-5        mid = n // 2
-6
-7        left = 0
-8        left_q = 0
-9        right = 0
-10        right_q = 0
-11        for i in range(mid):
-12            if num[i] == '?':
-13                left_q += 1
-14            else:
-15                left += int(num[i])
+5        prefix = [0]
+6        for stone in stones:
+7            prefix.append(prefix[-1] + stone)
+8
+9        @cache
+10        def rec(index, alice):
+11            if index == n-1:
+12                if alice:
+13                    return prefix[index + 1]
+14                else:
+15                    return -prefix[index + 1]
 16
-17        for i in range(mid, n):
-18            if num[i] == '?':
-19                right_q += 1
+17            ans = rec(index + 1, alice)
+18            if alice:
+19                ans = max(ans, rec(index + 1, not alice) + prefix[index + 1])
 20            else:
-21                right += int(num[i])
+21                ans = min(ans, rec(index + 1, not alice) - prefix[index + 1])
 22
-23        if left == right:
-24            if left_q == right_q: return False
-25
-26        rem = left_q + right_q
-27        if rem % 2 == 0:
-28            alice = True
-29            while rem:
-30                if left != right:
-31                    condition = (left > right and alice) or (not alice and right > left)
-32                    if condition:
-33                        if left_q:
-34                            left += 9
-35                            left_q -= 1
-36                        else:
-37                            right_q -= 1
-38                    else:
-39                        if right_q:
-40                            right += 9
-41                            right_q -= 1
-42                        else:
-43                            left_q -= 1
-44                else:
-45                    if alice:
-46                        if left_q:
-47                            left += 9
-48                            left_q -= 1
-49                        else:
-50                            right += 9
-51                            right_q -= 1
-52                    else:
-53                        if left_q: left_q -= 1
-54                        else: right_q -= 1
-55                rem -= 1
-56                alice = not alice
-57
-58            return left != right
-59
-60        return True
+23            return ans
+24
+25        ans = rec(1, True)
+26        rec.cache_clear()
+27        return ans
