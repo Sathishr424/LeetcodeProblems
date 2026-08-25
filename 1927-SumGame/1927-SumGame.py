0f64b750d46cf7ed52b3323a9a5d97b00cdaacfb
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-25 14:38:06 +0530

    File updated at 8/25/2026, 2:38:06 PM

--- 1927-SumGame/1927-SumGame.py
+++ 1927-SumGame/1927-SumGame.py
@@ -1,23 +1,10 @@
-# Last updated: 8/25/2026, 2:35:42 PM
+# Last updated: 8/25/2026, 2:38:06 PM
 1class Solution:
-2    def stoneGameVIII(self, stones: List[int]) -> int:
-3        n = len(stones)
+2    def missingMultiple(self, nums: List[int], k: int) -> int:
+3        there = set(nums)
 4
-5        prefix = [0]
-6        for stone in stones:
-7            prefix.append(prefix[-1] + stone)
+5        multi = k
+6        while multi in there:
+7            multi += k
 8
-9        @cache
-10        def rec(index, alice):
-11            if index == n-1:
-12                return prefix[index + 1] * (1 if alice else -1)
-13
-14            ans = rec(index + 1, alice)
-15            if alice:
-16                return max(ans, rec(index + 1, alice ^ 1) + prefix[index + 1])
-17            else:
-18                return min(ans, rec(index + 1, alice ^ 1) - prefix[index + 1])
-19
-20        ans = rec(1, 1)
-21        rec.cache_clear()
-22        return ans
+9        return multi
