ad30bff4946e1cf5f8d92389eb405c8639de5cc7
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-28 01:26:41 +0530

    File updated at 8/28/2026, 1:26:41 AM

--- 3720-LexicographicallySmallestPermutationGreaterThanTarget/3720-LexicographicallySmallestPermutationGreaterThanTarget.py
+++ 3720-LexicographicallySmallestPermutationGreaterThanTarget/3720-LexicographicallySmallestPermutationGreaterThanTarget.py
@@ -1,45 +1,43 @@
-# Last updated: 8/28/2026, 1:23:48 AM
+# Last updated: 8/28/2026, 1:26:41 AM
 1class Solution:
 2    def lexGreaterPermutation(self, s: str, target: str) -> str:
 3        n = len(s)
 4
-5        freq = [0] * 26
-6        for char in s:
-7            freq[ord(char) - ord('a')] += 1
-8
-9        z = "z" * (n + 1)
+5        z = "z" * (n + 1)
+6        freq = [0] * 26
+7        for char in s:
+8            freq[ord(char) - ord('a')] += 1
+9
 10
 11        def buildSmall(index, curr):
-12            tmp = freq[:]
-13            for i in range(index, n):
-14                for c in range(26):
-15                    if tmp[c]:
-16                        curr += chr(c + ord('a'))
-17                        tmp[c] -= 1
-18                        break
-19            return curr
-20
-21        def rec(index, curr):
-22            if index == n:
-23                return z
-24            
-25            c = ord(target[index]) - ord('a')
-26            best = z
-27            if freq[c]:
-28                freq[c] -= 1
-29                best = rec(index + 1, curr + chr(c + ord('a')))
-30                freq[c] += 1
-31
-32            c += 1
-33            while c < 26 and freq[c] == 0:
-34                c += 1
-35
-36            if c < 26:
-37                freq[c] -= 1
-38                best = min(best, buildSmall(index + 1, curr + chr(c + ord('a'))))
-39                freq[c] += 1
+12            while index < n:
+13                for c in range(26):
+14                    for _ in range(freq[c]):
+15                        curr += chr(c + ord('a'))
+16                        index += 1
+17            return curr
+18
+19        def rec(index, curr):
+20            if index == n:
+21                return z
+22            
+23            c = ord(target[index]) - ord('a')
+24            best = z
+25            if freq[c]:
+26                freq[c] -= 1
+27                best = rec(index + 1, curr + chr(c + ord('a')))
+28                freq[c] += 1
+29
+30            c += 1
+31            while c < 26 and freq[c] == 0:
+32                c += 1
+33
+34            if c < 26:
+35                freq[c] -= 1
+36                best = min(best, buildSmall(index + 1, curr + chr(c + ord('a'))))
+37                freq[c] += 1
+38
+39            return best
 40
-41            return best
-42
-43        ans = rec(0, "")
-44        return ans if ans != z else ""
+41        ans = rec(0, "")
+42        return ans if ans != z else ""
