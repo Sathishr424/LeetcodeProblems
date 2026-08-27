68af6164980aeb7451d56a59388518db18e9897e
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-28 01:32:38 +0530

    File updated at 8/28/2026, 1:32:38 AM

--- 3720-LexicographicallySmallestPermutationGreaterThanTarget/3720-LexicographicallySmallestPermutationGreaterThanTarget.py
+++ 3720-LexicographicallySmallestPermutationGreaterThanTarget/3720-LexicographicallySmallestPermutationGreaterThanTarget.py
@@ -1,4 +1,4 @@
-# Last updated: 8/28/2026, 1:31:55 AM
+# Last updated: 8/28/2026, 1:32:38 AM
 1class Solution:
 2    def lexGreaterPermutation(self, s: str, target: str) -> str:
 3        n = len(s)
@@ -7,32 +7,31 @@
 6        for char in s:
 7            freq[ord(char) - ord('a')] += 1
 8
-9        def buildSmall(index, curr):
+9        def buildSmall(curr):
 10            for c in range(26):
 11                for _ in range(freq[c]):
 12                    curr += chr(c + ord('a'))
-13                    index += 1
-14            return curr
-15
-16        def rec(index, curr):
-17            if index == n:
-18                return ""
-19            
-20            c = ord(target[index]) - ord('a')
-21            if freq[c]:
-22                freq[c] -= 1
-23                best = rec(index + 1, curr + chr(c + ord('a')))
-24                if best != "": return best
-25                freq[c] += 1
-26
-27            c += 1
-28            while c < 26 and freq[c] == 0:
-29                c += 1
-30
-31            if c < 26:
-32                freq[c] -= 1
-33                return buildSmall(index + 1, curr + chr(c + ord('a')))
-34
-35            return ""
-36
-37        return rec(0, "")
+13            return curr
+14
+15        def rec(index, curr):
+16            if index == n:
+17                return ""
+18            
+19            c = ord(target[index]) - ord('a')
+20            if freq[c]:
+21                freq[c] -= 1
+22                best = rec(index + 1, curr + chr(c + ord('a')))
+23                if best != "": return best
+24                freq[c] += 1
+25
+26            c += 1
+27            while c < 26 and freq[c] == 0:
+28                c += 1
+29
+30            if c < 26:
+31                freq[c] -= 1
+32                return buildSmall(curr + chr(c + ord('a')))
+33
+34            return ""
+35
+36        return rec(0, "")
