0fb21dae6ff3cc90f05b94a22135bab2dd9de57d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-29 16:34:17 +0530

    File updated at 8/29/2026, 4:34:17 PM

--- 2948-MakeLexicographicallySmallestArraybySwappingElements/2948-MakeLexicographicallySmallestArraybySwappingElements.py
+++ 2948-MakeLexicographicallySmallestArraybySwappingElements/2948-MakeLexicographicallySmallestArraybySwappingElements.py
@@ -1,4 +1,4 @@
-# Last updated: 8/29/2026, 4:33:04 PM
+# Last updated: 8/29/2026, 4:34:17 PM
 1class Solution:
 2    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
 3        n = len(nums)
@@ -6,26 +6,25 @@
 5        sorted_nums = []
 6        for i, num in enumerate(nums):
 7            sorted_nums.append((num, i))
-8
-9        sorted_nums.sort()
-10
-11        group_relation = [i for i in range(n)]
-12        groups = defaultdict(list)
-13        group_id = 0
-14        prev = -10**20
-15        for num, i in sorted_nums:
-16            if num - prev > limit:
-17                group_id += 1
-18
-19            groups[group_id].append(num)
-20            group_relation[i] = group_id
-21            prev = num
-22
-23        for group in groups:
-24            groups[group].sort(reverse=True)
-25
-26        ret = []
-27        for i in range(n):
-28            ret.append(groups[group_relation[i]].pop())
-29
-30        return ret
+8        sorted_nums.sort()
+9
+10        group_relation = [i for i in range(n)]
+11        groups = defaultdict(list)
+12        group_id = 0
+13        prev = -limit
+14        for num, i in sorted_nums:
+15            if num - prev > limit:
+16                group_id += 1
+17
+18            groups[group_id].append(num)
+19            group_relation[i] = group_id
+20            prev = num
+21
+22        for group in groups:
+23            groups[group].sort(reverse=True)
+24
+25        ret = []
+26        for i in range(n):
+27            ret.append(groups[group_relation[i]].pop())
+28
+29        return ret
