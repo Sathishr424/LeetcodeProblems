a1a77ff641a419d9289f27df2cc573522f085368
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-30 14:49:03 +0530

    File updated at 8/30/2026, 2:49:03 PM

--- 2091-RemovingMinimumandMaximumFromArray/2091-RemovingMinimumandMaximumFromArray.py
+++ 2091-RemovingMinimumandMaximumFromArray/2091-RemovingMinimumandMaximumFromArray.py
@@ -1,19 +1,21 @@
-# Last updated: 8/30/2026, 2:47:06 PM
+# Last updated: 8/30/2026, 2:49:03 PM
 1class Solution:
 2    def minimumDeletions(self, nums: List[int]) -> int:
 3        n = len(nums)
-4        mn = min(nums)
-5        mx = max(nums)
-6
-7        mn_index = 0
-8        mx_index = 0
+4        mn = nums[0]
+5        mx = nums[0]
+6        mn_index = 0
+7        mx_index = 0
+8
 9        for i in range(n):
-10            if nums[i] == mn:
-11                mn_index = i
-12            elif nums[i] == mx:
-13                mx_index = i
-14
-15        if mn_index > mx_index:
-16            mn_index, mx_index = mx_index, mn_index
-17
-18        return min(mx_index + 1, n - mn_index, mn_index + 1 + (n - mx_index))
+10            if nums[i] > mx:
+11                mx = nums[i]
+12                mx_index = i
+13            elif nums[i] < mn:
+14                mn = nums[i]
+15                mn_index = i
+16
+17        if mn_index > mx_index:
+18            mn_index, mx_index = mx_index, mn_index
+19
+20        return min(mx_index + 1, n - mn_index, mn_index + 1 + (n - mx_index))
