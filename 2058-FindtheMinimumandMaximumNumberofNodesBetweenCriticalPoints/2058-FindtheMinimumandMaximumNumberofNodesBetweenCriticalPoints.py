0541a96957e90a668a03beb3cf3788d631aac557
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-08-31 16:31:59 +0530

    File updated at 8/31/2026, 4:31:58 PM

--- 2058-FindtheMinimumandMaximumNumberofNodesBetweenCriticalPoints/2058-FindtheMinimumandMaximumNumberofNodesBetweenCriticalPoints.py
+++ 2058-FindtheMinimumandMaximumNumberofNodesBetweenCriticalPoints/2058-FindtheMinimumandMaximumNumberofNodesBetweenCriticalPoints.py
@@ -1,25 +1,27 @@
-# Last updated: 8/31/2026, 4:30:44 PM
+# Last updated: 8/31/2026, 4:31:58 PM
 1class Solution:
 2    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
 3        node = head
-4        prev = None
-5        stack = []
-6        index = -1
-7        min_dis = inf
-8        while node:
-9            if prev and node.next:
-10                if (node.val > prev.val and node.val > node.next.val) or (node.val < prev.val and node.val < node.next.val):
-11                    if stack:
-12                        prev_index = stack.pop()
-13                        min_dis = min(index - prev_index, min_dis)
-14
-15                        if len(stack) == 0:
-16                            stack.append(prev_index)
-17                    stack.append(index)
-18
-19            index += 1
-20            prev = node
-21            node = node.next
-22
-23        if len(stack) < 2: return [-1, -1]
-24        return [min_dis, stack[-1] - stack[0]]
+4        prev = node
+5        node = node.next
+6
+7        stack = []
+8        index = -1
+9        min_dis = inf
+10
+11        while node.next:
+12            if (node.val > prev.val and node.val > node.next.val) or (node.val < prev.val and node.val < node.next.val):
+13                if stack:
+14                    prev_index = stack.pop()
+15                    min_dis = min(index - prev_index, min_dis)
+16
+17                    if len(stack) == 0:
+18                        stack.append(prev_index)
+19                stack.append(index)
+20
+21            index += 1
+22            prev = node
+23            node = node.next
+24
+25        if len(stack) < 2: return [-1, -1]
+26        return [min_dis, stack[-1] - stack[0]]
