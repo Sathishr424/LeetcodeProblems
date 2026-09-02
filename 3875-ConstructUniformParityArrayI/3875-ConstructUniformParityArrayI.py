c3df63ed18a6c21b794d7072854788bdcf9ae8b8
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-02 07:37:20 +0530

    File updated at 9/2/2026, 7:37:20 AM

--- 3875-ConstructUniformParityArrayI/3875-ConstructUniformParityArrayI.py
+++ 3875-ConstructUniformParityArrayI/3875-ConstructUniformParityArrayI.py
@@ -1,28 +1,23 @@
-# Last updated: 9/2/2026, 7:36:32 AM
+# Last updated: 9/2/2026, 7:37:20 AM
 1class Solution:
 2    def uniformArray(self, nums: list[int]) -> bool:
-3        n = len(nums)
-4
-5        odd = 0
-6        even = 0
-7        for num in nums:
-8            if num % 2:
-9                odd += 1
-10            else:
-11                even += 1
-12
-13        # odd
-14        for num in nums:
-15            if num % 2 == 0:
-16                if odd == 0: break
-17        else:
-18            return True
-19
-20        # even
-21        for num in nums:
-22            if num % 2:
-23                if odd == 1: break
-24        else:
-25            return True
-26
-27        return False
+3        odd = 0
+4        for num in nums:
+5            if num % 2:
+6                odd += 1
+7
+8        # odd
+9        for num in nums:
+10            if num % 2 == 0:
+11                if odd == 0: break
+12        else:
+13            return True
+14
+15        # even
+16        for num in nums:
+17            if num % 2:
+18                if odd == 1: break
+19        else:
+20            return True
+21
+22        return False
