a70f05ceb32cad498a5235d9d2f70e4038c34262
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-08 14:25:18 +0530

    File updated at 9/8/2026, 2:25:18 PM

--- 3870-CountCommasinRange/3870-CountCommasinRange.py
+++ 3870-CountCommasinRange/3870-CountCommasinRange.py
@@ -1,8 +1,6 @@
-# Last updated: 9/8/2026, 2:24:52 PM
+# Last updated: 9/8/2026, 2:25:18 PM
 1class Solution:
 2    def countCommas(self, n: int) -> int:
 3        # 100,000
-4        if n > 999:
-5            return n - 999
-6        return 0
-7
+4        return max(0, n - 999)
+5
