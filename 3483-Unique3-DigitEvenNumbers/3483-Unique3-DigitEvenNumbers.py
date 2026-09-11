6f0976933db1c44e52d51fb158866787e0fbe506
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-11 15:54:44 +0530

    File updated at 9/11/2026, 3:54:43 PM

--- 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.py
@@ -1,26 +1,15 @@
-# Last updated: 12/5/2025, 2:09:56 pm
-class Solution:
-    def totalNumbers(self, digits: List[int]) -> int:
-        freq = [0] * 10
-        for num in digits:
-            freq[num] += 1
-        
-        ret = 0
-
-        for i in range(1, 10):
-            if freq[i] == 0: continue
-            for j in range(10):
-                if freq[j] == 0: continue
-                for k in range(0, 10, 2):
-                    if freq[k] == 0: continue
-                    
-                    if i == j and j == k:
-                        if freq[i] < 3: continue
-                    elif i == j or i == k:
-                        if freq[i] < 2: continue
-                    elif j == k:
-                        if freq[j] < 2: continue
-                    
-                    ret += 1
-        return ret
-        
+# Last updated: 9/11/2026, 3:54:43 PM
+1class Solution:
+2    def totalNumbers(self, digits: List[int]) -> int:
+3        n = len(digits)
+4
+5        uniq = set()
+6        for i in range(n):
+7            if digits[i] == 0: continue
+8            for j in range(n):
+9                if i == j: continue
+10                for k in range(n):
+11                    if k == i or k == j or digits[k] % 2: continue
+12                    uniq.add(str(digits[i]) + str(digits[j]) + str(digits[k]))
+13
+14        return len(uniq)
