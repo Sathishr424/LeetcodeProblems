e378762e6e4c2d8d6032d325b8a79ce01e7e7e3d
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-12 20:11:22 +0530

    Create

---
+++ problem-1789224082347/problem-1789224082347.py
@@ -0,0 +1,22 @@
+# Last updated: 9/12/2026, 8:11:22 PM
+1class Solution:
+2    def countSpecialIntegers(self, nums: list[int]) -> int:
+3        indexes = defaultdict(list)
+4        ans = 0
+5
+6        for i, num in enumerate(nums):
+7            indexes[num].append(i)
+8
+9        for num in indexes:
+10            if len(indexes[num]) < 3: continue
+11
+12            m = len(indexes[num])
+13            ind = indexes[num]
+14            diff = ind[1] - ind[0]
+15
+16            for i in range(2, m):
+17                if ind[i] - ind[i - 1] != diff: break
+18            else:
+19                ans += 1
+20
+21        return ans
