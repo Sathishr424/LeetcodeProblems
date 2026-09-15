bcf29c015ea49a314a447fb707743dda3641d86b
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-15 16:25:42 +0530

    File updated at 9/15/2026, 4:25:42 PM

--- 2484-CountPalindromicSubsequences/2484-CountPalindromicSubsequences.py
+++ 2484-CountPalindromicSubsequences/2484-CountPalindromicSubsequences.py
@@ -1,62 +1,18 @@
-# Last updated: 30/9/2025, 7:55:12 pm
-class Solution:
-    def countPalindromes(self, s: str) -> int:
-        mod = 10**9 + 7
-        
-        n = len(s)
-        s = [int(a) for a in s]
-        prefix_reverse = [[0] * 10 for _ in range(n + 1)]
-        prefix = [[0] * 10 for _ in range(n + 1)]
-
-        for i in range(n-1, -1, -1):
-            a = s[i]
-            for j in range(10):
-                prefix_reverse[i][j] += prefix_reverse[i + 1][j]
-            prefix_reverse[i][a] += 1
-
-        prefix[0][s[i]] += 1
-        for i in range(1, n):
-            a = s[i]
-            for j in range(10):
-                prefix[i][j] += prefix[i - 1][j]
-            prefix[i][a] += 1
-
-        dp = [[-1] * 100 for _ in range(n)]
-        dp_reverse = [[-1] * 100 for _ in range(n)]
-
-        def recReverse(index, a, b):
-            if index == 0: return 0
-            if dp_reverse[index][a * 10 + b] != -1:
-                return dp_reverse[index][a * 10 + b]
-
-            ans = recReverse(index - 1, a, b)
-            if s[index] == b:
-                ans += prefix[index - 1][a]
-
-            ans %= mod
-            dp_reverse[index][a * 10 + b] = ans
-            return ans
-
-        def rec(index, a, b):
-            if index == n - 1: return 0
-            if dp[index][a * 10 + b] != -1:
-                return dp[index][a * 10 + b]
-
-            ans = rec(index + 1, a, b)
-            if s[index] == a:
-                ans += prefix_reverse[index + 1][b]
-
-            ans %= mod
-            dp[index][a * 10 + b] = ans
-            return ans
-
-        ans = 0
-        for index in range(2, n-3+1):
-            for i in range(10):
-                for j in range(10):
-                    left = recReverse(index - 1, i, j)
-                    right = rec(index + 1, j, i)
-                    ans += (left * right) % mod
-                    ans %= mod
-
-        return ans
+# Last updated: 9/15/2026, 4:25:42 PM
+1class Solution:
+2    def longestPalindrome(self, s: str) -> str:
+3        n = len(s)
+4
+5        dp = [[False] * n for _ in range(n + 1)]
+6        for k in range(2):
+7            for i in range(n):
+8                dp[k][i] = True
+9
+10        best = [0, 0]
+11        for k in range(2, n + 1):
+12            for i in range(k-1, n):
+13                dp[k][i] = s[i] == s[i-k+1] and dp[k-2][i-1]
+14                if dp[k][i] and k > best[1] - best[0] + 1:
+15                    best = [i-k+1, i]
+16
+17        return s[best[0]:best[1] + 1]
