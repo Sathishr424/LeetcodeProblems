0f399661789ad61babc322ef5ab43563de1cd7fd
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-16 16:24:50 +0530

    File updated at 9/16/2026, 4:24:50 PM

--- 1621-NumberofSetsofKNon-OverlappingLineSegments/1621-NumberofSetsofKNon-OverlappingLineSegments.py
+++ 1621-NumberofSetsofKNon-OverlappingLineSegments/1621-NumberofSetsofKNon-OverlappingLineSegments.py
@@ -1,20 +1,20 @@
-# Last updated: 9/16/2026, 4:22:09 PM
+# Last updated: 9/16/2026, 4:24:50 PM
 1class Solution:
 2    def numberOfSets(self, n: int, k: int) -> int:
 3        mod = 10**9 + 7
 4
 5        @cache
-6        def rec(index, rem, start):
-7            if rem == 0 and not start:
+6        def rec(index, rem, started):
+7            if rem == 0 and not started:
 8                return 1
 9            if index == n: return 0
 10
-11            ans = rec(index + 1, rem, start)
-12            if start:
+11            ans = rec(index + 1, rem, started)
+12            if started:
 13                ans += rec(index + 1, rem, False)
 14                if rem:
 15                    ans += rec(index + 1, rem - 1, True)
-16            elif not start and rem:
+16            elif not started and rem:
 17                ans += rec(index + 1, rem - 1, True)
 18            return ans % mod
 19
