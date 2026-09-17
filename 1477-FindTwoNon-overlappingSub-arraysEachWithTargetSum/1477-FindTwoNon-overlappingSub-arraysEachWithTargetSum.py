769c58d038232c39bf91b8e350c855d9266a4189
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-17 15:46:24 +0530

    File updated at 9/17/2026, 3:46:23 PM

--- 1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.py
+++ 1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.py
@@ -1,25 +1,21 @@
-# Last updated: 9/17/2026, 3:26:27 PM
+# Last updated: 9/17/2026, 3:46:23 PM
 1class Solution:
 2    def minSumOfLengths(self, arr: List[int], target: int) -> int:
 3        n = len(arr)
-4        prefix = [0]
-5        for num in arr:
-6            prefix.append(prefix[-1] + num)
-7
-8        @cache
-9        def rec(index, status):
-10            if status == 2:
-11                return 0
-12            if index == n:
-13                return inf
-14
-15            ans = rec(index + 1, status)
-16            need = prefix[index] + target
-17            right = bisect_left(prefix, need, lo=index + 1)
-18            if right <= n and prefix[right] == need:
-19                ans = min(ans, rec(right, status + 1) + (right - index))
-20            return ans
-21
-22        ans = rec(0, 0)
-23        rec.cache_clear()
-24        return -1 if ans == inf else ans
+4
+5        best = [inf] * (n + 2)
+6        tot = 0
+7        left = 0
+8        ans = inf
+9        for i in range(n):
+10            tot += arr[i]
+11            best[i] = min(best[i], best[i - 1])
+12            while tot > target:
+13                tot -= arr[left]
+14                left += 1
+15
+16            if tot == target:
+17                best[i] = min(i - left + 1, best[i])
+18                ans = min(ans, (i - left + 1) + best[left - 1])
+19
+20        return -1 if ans == inf else ans
