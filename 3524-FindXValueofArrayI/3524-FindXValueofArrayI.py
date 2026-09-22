9cb4ef53996546f2349909f2f279de15886166ca
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-22 17:13:55 +0530

    File updated at 9/22/2026, 5:13:54 PM

--- 3524-FindXValueofArrayI/3524-FindXValueofArrayI.py
+++ 3524-FindXValueofArrayI/3524-FindXValueofArrayI.py
@@ -1,28 +1,23 @@
-# Last updated: 9/22/2026, 3:41:36 PM
+# Last updated: 9/22/2026, 5:13:54 PM
 1class Solution:
 2    def resultArray(self, nums: List[int], k: int) -> List[int]:
 3        n = len(nums)
 4
-5        @cache
-6        def rec(index, rem, used):
-7            ans = [0] * k
-8            if used and rem < k:
-9                ans[rem] += 1
-10
-11            if index == n:
-12                return ans
-13
-14            if not used:
-15                curr = rec(index + 1, rem, used)
-16                for x in range(k):
-17                    ans[x] += curr[x]
-18
-19            curr = rec(index + 1, (rem * nums[index]) % k, True)
-20            for x in range(k):
-21                ans[x] += curr[x]
-22
-23            return ans
-24
-25        ans = rec(0, 1, False)
-26        rec.cache_clear()
-27        return ans
+5        dp = [defaultdict(lambda: [0] * k) for _ in range(n + 1)]
+6
+7        for i in range(n):
+8            dp[i + 1][nums[i] % k][nums[i] % k] += 1
+9
+10        ans = [0] * k
+11        for i in range(n):
+12            ni = i + 1
+13            for rem in dp[i]:
+14                nrem = (rem * nums[i]) % k
+15                dp[ni][nrem][nrem] += dp[i][rem][rem]
+16        
+17        for i in range(1, n + 1):
+18            for rem in dp[i]:
+19                for x in range(k):
+20                    ans[x] += dp[i][rem][x]
+21
+22        return ans
