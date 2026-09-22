3f1a2913dbd20ff72fa0e1c210131d89a759582c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-22 15:41:37 +0530

    File updated at 9/22/2026, 3:41:36 PM

--- 3524-FindXValueofArrayI/3524-FindXValueofArrayI.py
+++ 3524-FindXValueofArrayI/3524-FindXValueofArrayI.py
@@ -1,22 +1,28 @@
-# Last updated: 9/22/2026, 2:38:55 AM
+# Last updated: 9/22/2026, 3:41:36 PM
 1class Solution:
 2    def resultArray(self, nums: List[int], k: int) -> List[int]:
 3        n = len(nums)
-4        result = [0] * k
-5        # Initial state: no elements have been processed, so no non-empty subarray exists.
-6        dp = [0] * k
-7
-8        for i in range(n):
-9            ndp = [0] * k  # Current state (rolling array).
+4
+5        @cache
+6        def rec(index, rem, used):
+7            ans = [0] * k
+8            if used and rem < k:
+9                ans[rem] += 1
 10
-11            ndp[nums[i] % k] += 1
-12
-13            for r in range(k):
-14                ndp[(r * nums[i]) % k] += dp[r]
-15
-16            dp = ndp  # Update the state.
-17
-18            # Accumulate the answer.
-19            for r in range(k):
-20                result[r] += dp[r]
-21        return result
+11            if index == n:
+12                return ans
+13
+14            if not used:
+15                curr = rec(index + 1, rem, used)
+16                for x in range(k):
+17                    ans[x] += curr[x]
+18
+19            curr = rec(index + 1, (rem * nums[index]) % k, True)
+20            for x in range(k):
+21                ans[x] += curr[x]
+22
+23            return ans
+24
+25        ans = rec(0, 1, False)
+26        rec.cache_clear()
+27        return ans
