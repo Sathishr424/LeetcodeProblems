1247ab2584c631bfdc155e9237de1dff5021ab87
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-23 08:00:54 +0530

    File updated at 9/23/2026, 8:00:53 AM

--- 1658-MinimumOperationstoReduceXtoZero/1658-MinimumOperationstoReduceXtoZero.py
+++ 1658-MinimumOperationstoReduceXtoZero/1658-MinimumOperationstoReduceXtoZero.py
@@ -1,27 +1,21 @@
-# Last updated: 9/23/2026, 7:48:36 AM
+# Last updated: 9/23/2026, 8:00:53 AM
 1class Solution:
 2    def minOperations(self, nums: list[int], x: int) -> int:
 3        n = len(nums)
-4        suffix = defaultdict(int)
-5        suffix[0] = 0
-6        suf = 0
+4        suffix = [0] * (n + 1)
+5        total = 0
+6        best = n + 1
 7        for i in range(n-1, -1, -1):
-8            suf += nums[i]
-9            suffix[suf] = n - i
-10
-11        prefix = 0
-12        best = n + 1
-13        if x in suffix: best = suffix[x]
-14        suffix[suf] = n + 1
+8            total += nums[i]
+9            suffix[n-i] = total
+10            if total == x: best = min(best, n - i)
+11
+12        total = 0
+13        for i in range(n):
+14            total += nums[i]
 15
-16        for i in range(n):
-17            suf -= nums[i]
-18            suffix[suf] = n + 1
+16            index = bisect_left(suffix, x - total, hi=n-i)
+17            if index < n and suffix[index] + total == x:
+18                best = min(best, i + 1 + index)
 19
-20            prefix += nums[i]
-21
-22            need = x - prefix
-23            if need in suffix:
-24                best = min(best, (i + 1) + suffix[need])
-25
-26        return best if best <= n else -1
+20        return best if best <= n else -1
