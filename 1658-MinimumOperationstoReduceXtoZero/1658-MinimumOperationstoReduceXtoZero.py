f3b6f53455ab0368085e9380f69fcb817b6627c9
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-23 08:09:16 +0530

    File updated at 9/23/2026, 8:09:16 AM

--- 1658-MinimumOperationstoReduceXtoZero/1658-MinimumOperationstoReduceXtoZero.py
+++ 1658-MinimumOperationstoReduceXtoZero/1658-MinimumOperationstoReduceXtoZero.py
@@ -1,30 +1,31 @@
-# Last updated: 9/23/2026, 8:08:33 AM
-1class Solution:
-2    def minOperations(self, nums: list[int], x: int) -> int:
-3        n = len(nums)
-4
-5        best = n + 1
-6        total = 0
-7        for i in range(n-1, -1, -1):
-8            total += nums[i]
-9            if total == x:
-10                best = min(best, n - i)
-11
-12        right = n-1
-13        suffix = 0
-14        prefix = 0
-15        for i in range(n):
-16            prefix += nums[i]
-17
-18            while right + 1 < n and prefix + suffix > x:
-19                right += 1
-20                suffix -= nums[right]
-21
-22            while right > i and prefix + suffix + nums[right] <= x:
-23                suffix += nums[right]
-24                right -= 1
-25
-26            if prefix + suffix == x:
-27                best = min(best, i + 1 + (n - right - 1))
-28
-29        return best if best <= n else -1
+# Last updated: 9/23/2026, 8:09:16 AM
+1cmin = lambda x, y: x if x < y else y
+2class Solution:
+3    def minOperations(self, nums: list[int], x: int) -> int:
+4        n = len(nums)
+5
+6        best = n + 1
+7        total = 0
+8        for i in range(n-1, -1, -1):
+9            total += nums[i]
+10            if total == x:
+11                best = cmin(best, n - i)
+12
+13        right = n-1
+14        suffix = 0
+15        prefix = 0
+16        for i in range(n):
+17            prefix += nums[i]
+18
+19            while right + 1 < n and prefix + suffix > x:
+20                right += 1
+21                suffix -= nums[right]
+22
+23            while right > i and prefix + suffix + nums[right] <= x:
+24                suffix += nums[right]
+25                right -= 1
+26
+27            if prefix + suffix == x:
+28                best = cmin(best, i + 1 + (n - right - 1))
+29
+30        return best if best <= n else -1
