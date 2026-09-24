199b9ef4998490317fc22da71de3b7710025ca1c
Sathish R <50843894+Sathishr424@users.noreply.github.com>
2026-09-24 19:02:27 +0530

    File updated at 9/24/2026, 7:02:27 PM

--- 3550-SmallestIndexWithDigitSumEqualtoIndex/3550-SmallestIndexWithDigitSumEqualtoIndex.py
+++ 3550-SmallestIndexWithDigitSumEqualtoIndex/3550-SmallestIndexWithDigitSumEqualtoIndex.py
@@ -1,11 +1,11 @@
-# Last updated: 18/5/2025, 5:18:21 pm
-class Solution:
-    def smallestIndex(self, nums: List[int]) -> int:
-        for i, num in enumerate(nums):
-            s = 0
-            while num:
-                s += num % 10
-                num //= 10
-            if s == i: return i
-        
-        return -1
+# Last updated: 9/24/2026, 7:02:27 PM
+1class Solution:
+2    def smallestIndex(self, nums: List[int]) -> int:
+3        for i, num in enumerate(nums):
+4            s = 0
+5            while num:
+6                s += num % 10
+7                num //= 10
+8            if s == i: return i
+9
+10        return -1
